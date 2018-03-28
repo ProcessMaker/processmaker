34d5f07798f212badbf2618e41a2921d8be038b2
Alan Bollinger <velkymx@gmail.com>
2018-03-28 16:40:26 -0700

    Webpack update

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,7 @@ const { mix } = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
- */
+
 
 mix.js([
   'resources/assets/js/app-layout.js',
@@ -28,3 +28,7 @@ mix.js([
     .sass('resources/assets/sass/base.scss', 'public/css')
     .sass('resources/assets/sass/layouts-app.scss', 'public/css')
     .copy('node_modules/font-awesome/css/font-awesome.css', 'public/css');
+*/
+mix.js('resources/assets/js/app.js', 'public/js')
+   .extract(['vue','jquery','bootstrap-vue','axios','popper.js','lodash','bootstrap'])
+   .sass('resources/assets/sass/app.scss', 'public/css');
