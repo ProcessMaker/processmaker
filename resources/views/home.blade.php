6c4373c2e43d7c2744e2dd8677b8c757a945175b
Mila Endo <milal.alim@gmail.com>
2018-03-29 15:20:42 -0700

    test for dynamic rendering of table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,109 +8,15 @@
       <input class="form-control" type="search" placeholder="Search..." aria-label="Search">
     </form>
   </div>
-  <table class="table  table-hover">
+  <table class="table table-sm table-hover" id="fourthTable">
   <thead>
     <tr>
-      <th class="text-dark" scope="col">CASE   <i class="fa fa-sort-desc" aria-hidden="true"></i></i></th>
-      <th class="text-muted" scope="col">PROCESS   <i class="fa fa-sort" aria-hidden="true"></i></th>
-      <th class="text-muted" scope="col">TASK   <i class="fa fa-sort" aria-hidden="true"></i></th>
-      <th class="text-muted" scope="col">SENT BY   <i class="fa fa-sort" aria-hidden="true"></i></th>
-      <th class="text-muted" scope="col">DUE   <i class="fa fa-sort" aria-hidden="true"></i></th>
-      <th class="text-muted" scope="col">MODIFIED   <i class="fa fa-sort" aria-hidden="true"></i></th>
-      <th class="text-muted" scope="col">PRIORITY   <i class="fa fa-sort" aria-hidden="true"></i></th>
-      <th class="text-muted" scope="col">INFO   <i class="fa fa-sort" aria-hidden="true"></i></th>
+      <th class="text-dark" scope="col" v-for="col in columns" v-on:click="sortTable(col)"> @{{col}}   <i class="fa fa-sort-desc" aria-hidden="true"></i></i></th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>4</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>5</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>6</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>7</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>8</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>icon</td>
-    </tr>
-    <tr>
-      <td>9</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>icon</td>
+    <tr v-for="row in rows">
+      <td v-for="col in columns">@{{row[col]}}</td>
     </tr>
   </tbody>
 </table>
@@ -122,5 +28,50 @@
 @endsection
 
 @section('js')
+  <script>
+  var fourthTable = new Vue({
+    el: '#fourthTable',
+    data: {
+      ascending: false,
+      sortColumn: '',
+      rows: [
+        { id: 1, name: "Chandler Bing", phone: '305-917-1301', profession: 'IT Manager' },
+        { id: 2, name: "Ross Geller", phone: '210-684-8953', profession: 'Paleontologist' },
+        { id: 3, name: "Rachel Green", phone: '765-338-0312', profession: 'Waitress'},
+        { id: 4, name: "Monica Geller", phone: '714-541-3336', profession: 'Head Chef' },
+        { id: 5, name: "Joey Tribbiani", phone: '972-297-6037', profession: 'Actor' },
+        { id: 6, name: "Phoebe Buffay", phone: '760-318-8376', profession: 'Masseuse' }
+      ]
+    },
+    methods: {
+      "sortTable": function sortTable(col) {
+        if (this.sortColumn === col) {
+          this.ascending = !this.ascending;
+        } else {
+          this.ascending = true;
+          this.sortColumn = col;
+        }
 
+        var ascending = this.ascending;
+
+        this.rows.sort(function(a, b) {
+          if (a[col] > b[col]) {
+            return ascending ? 1 : -1
+          } else if (a[col] < b[col]) {
+            return ascending ? -1 : 1
+          }
+          return 0;
+        })
+      }
+    },
+    computed: {
+      "columns": function columns() {
+        if (this.rows.length == 0) {
+          return [];
+        }
+        return Object.keys(this.rows[0])
+      }
+    }
+  });
+</script>
 @endsection
