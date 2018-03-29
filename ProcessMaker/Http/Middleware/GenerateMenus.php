53811d7514e81ee8466594d9649db12ea3a1ee2a
Alan Bollinger <velkymx@gmail.com>
2018-03-29 12:58:54 -0700

    Sitebar menu generation enabled

--- ProcessMaker/Http/Middleware/GenerateMenus.php
+++ ProcessMaker/Http/Middleware/GenerateMenus.php
@@ -19,45 +19,43 @@ class GenerateMenus
     {
         // Build the menu
         Menu::make('main', function ($menu) {
-            $execute = $menu->raw('Execute')->data([
-                'icon' => 'glyphicon glyphicon-briefcase'
-            ]);
-            $tasks = $execute->add('Tasks');
-            $tasks->add('Pending');
-            $tasks->add('Unclaimed');
-            $tasks->add('Completed');
+            $execute = $menu;
+            $tasks = $execute->add('Tasks',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-list-ul"></i> ');
+            $tasks->add('Pending',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Unclaimed',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Completed',['route'  => 'home', 'id' => 'home']);
 
-            $cases = $execute->add('Cases');
+            $cases = $execute->add('Cases',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-briefcase"></i> ');
             $cases->add('New');
             $cases->add('Drafts');
             $cases->add('Mine');
             $cases->add('Find');
-
-            $build = $menu->raw('Build')->data([
-                'icon' => 'glyphicon glyphicon-wrench'
-            ]);
-            $app = $build->add('Application');
-            $app->add('Processes');
-            $app->add('Tables');
-            $app->add('Forms');
-            $app->add('Scripts');
-
-            $manage = $menu->raw('Manage')->data([
-               'icon' => 'glyphicon glyphicon-cog'
-            ]);
-            $org = $manage->add('Organization');
-            $org->add('Users', ['route' => 'management-users-index']);
-            $org->add('Groups');
-            $org->add('Roles');
-
-            $appearance = $manage->add('Appearance');
-            $appearance->add('Themes');
-            $appearance->add('Logo Management');
-
-            $manage->add('Localization and Internationalization');
-            $manage->add('Notifications');
-            $manage->add('Plugin Manager');
-            $manage->add('Logs & Audit');
+            //
+            // $build = $menu->raw('Build',['class' => 'sidebar-header'])
+            // ->prepend('<i class="fa fa-wrench"></i> ');
+            // $app = $build->add('Application');
+            // $app->add('Processes');
+            // $app->add('Tables');
+            // $app->add('Forms');
+            // $app->add('Scripts');
+            //
+            // $manage = $menu->raw('Manage',['class' => 'sidebar-header'])
+            // ->prepend('<i class="fa fa-list-ul"></i> ');
+            // $org = $manage->add('Organization',['class' => 'sidebar-header']);
+            // $org->add('Users', ['route' => 'management-users-index']);
+            // $org->add('Groups');
+            // $org->add('Roles');
+            //
+            // $appearance = $manage->add('Appearance');
+            // $appearance->add('Themes');
+            // $appearance->add('Logo Management');
+            //
+            // $manage->add('Localization and Internationalization');
+            // $manage->add('Notifications');
+            // $manage->add('Plugin Manager');
+            // $manage->add('Logs & Audit');
         });
 
         return $next($request);
