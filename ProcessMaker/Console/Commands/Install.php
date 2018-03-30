413d61ba74cb715464271134e4a6e258b7a595ff
Taylor Dondich <taylor@dondich.com>
2018-03-30 12:41:42 -0700

    Fix up system so main namespace directory points to ProcessMaker and ensure namespace resolution works properly in php artisan generate commands.

---
+++ ProcessMaker/Console/Commands/Install.php
@@ -0,0 +1,41 @@
+<?php
+namespace ProcessMaker\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bpm:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install and configure ProcessMaker BPM';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
