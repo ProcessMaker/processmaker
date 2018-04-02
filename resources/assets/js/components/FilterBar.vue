034fc49abcb891936169fd311f32f6f77c2934f7
Mila Endo <milal.alim@gmail.com>
2018-04-02 16:13:13 -0700

    set char limit to search

--- resources/assets/js/components/FilterBar.vue
+++ resources/assets/js/components/FilterBar.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="filter-bar">
-      <div>
-        <form class="form-inline justify-content-between">
-            <label class="table-header"><strong>Completed Tasks<small class="font-weight-light">  (100)</small></strong></label>
-            <input type="text" v-model="filterText" class="form-control" @keyup.enter="doFilter" placeholder="search...">
-        </form>
-      </div>
+    <div class="filter-bar justify-content-between">
+      <form class="form-inline ">
+        <div class="form-group ">
+          <label class="table-header"><strong>Completed Tasks<small class="font-weight-light">  (100)</small></strong></label>
+          <input type="text" v-model="filterText" class="form-control" @keyup="doFilter" placeholder="search...">
+          <i @click.prevent="doFilter"></i>
+        </div>
+      </form>
     </div>
 </template>
-
 <script>
   export default {
     data () {
@@ -18,7 +18,9 @@
     },
     methods: {
       doFilter () {
-        this.$events.fire('filter-set', this.filterText)
+        if (this.filterText.length > 3) {
+         this.$events.fire('filter-set', this.filterText)
+        }
       },
       resetFilter () {
         this.filterText = ''
