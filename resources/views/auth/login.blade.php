17a1c335b444fa1aa43d330102cd329b9824fb24
Mila Endo <milal.alim@gmail.com>
2018-04-03 15:39:31 -0700

    updated classes and format

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.minimal')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="row">
+    <div class="col">
+      <img src="../img/building.png" class="img-fluid">
+    </div>
+    <div class="container col">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="container">
+                <div class="col-sm-4 col-form-label text-md-right">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div>
                     <form method="POST" action="{{ route('login') }}">
                       {{ csrf_field() }}
 
-                        <div class="form-group row">
-                            <label for="username" class="col-sm-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
+                        <div class="form-group">
+                            <label for="username">{{ __('Username') }}</label>
                             <div class="col-md-6">
                                 <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
 
@@ -25,9 +27,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
+                        <div class="form-group">
+                            <label for="password">{{ __('Password') }}</label>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
@@ -39,26 +40,19 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">
+                            {{ __('Login') }}
+                        </button>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
+                        <div class="form-inline">
+                          <div class="checkbox">
+                              <label>
+                                  <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                              </label>
+                          </div>
+                          <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                          </a>
                         </div>
                     </form>
                 </div>
