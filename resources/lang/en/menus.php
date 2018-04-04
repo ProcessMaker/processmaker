295037fba1e16649e4434657a2dbaa39116e8621
Mila Endo <milal.alim@gmail.com>
2018-04-04 10:50:53 -0700

    localization

---
+++ resources/lang/en/menus.php
@@ -0,0 +1,18 @@
+<?php
+
+return[
+  'execute' => [
+    'tasks' => 'Tasks',
+    'pending' => 'Pending',
+    'unclaimed' => 'Unclaimed',
+    'completed' => 'Completed',
+    'cases' => 'Cases',
+    'new' => 'New',
+    'draft' => 'Draft',
+    'mine' => 'Mine',
+    'find' => 'Find',
+  ],
+  'build' => [
+
+  ]
+];
