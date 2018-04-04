e3e5592aee0bc042cbc12337e9aa36ac535360bf
Mila Endo <milal.alim@gmail.com>
2018-04-04 10:49:55 -0700

    localization

--- ProcessMaker/Http/Middleware/GenerateMenus.php
+++ ProcessMaker/Http/Middleware/GenerateMenus.php
@@ -17,21 +17,25 @@ class GenerateMenus
      */
     public function handle(Request $request, Closure $next)
     {
+          //set’s application’s locale
+        app()->setLocale('en');
+
+
         // Build the menu
         Menu::make('main', function ($menu) {
             $execute = $menu;
-            $tasks = $execute->add('Tasks',['class' => 'sidebar-header'])
+            $tasks = $execute->add(__('menus.execute.tasks'),['class' => 'sidebar-header'])
             ->prepend('<i class="fa fa-list-ul"></i> ');
-            $tasks->add('Pending',['route'  => 'home', 'id' => 'home']);
-            $tasks->add('Unclaimed',['route'  => 'home', 'id' => 'home']);
-            $tasks->add('Completed',['route'  => 'home', 'id' => 'home']);
+            $tasks->add(__('menus.execute.pending'),['route'  => 'home', 'id' => 'home']);
+            $tasks->add(__('menus.execute.unclaimed'),['route'  => 'home', 'id' => 'home']);
+            $tasks->add(__('menus.execute.completed'),['route'  => 'home', 'id' => 'home']);
 
-            $cases = $execute->add('Cases',['class' => 'sidebar-header'])
+            $cases = $execute->add(__('menus.execute.cases'),['class' => 'sidebar-header'])
             ->prepend('<i class="fa fa-briefcase"></i> ');
-            $cases->add('New');
-            $cases->add('Drafts');
-            $cases->add('Mine');
-            $cases->add('Find');
+            $cases->add(__('menus.execute.new'));
+            $cases->add(__('menus.execute.draft'));
+            $cases->add(__('menus.execute.mine'));
+            $cases->add(__('menus.execute.find'));
         });
 
         Menu::make('execute', function ($menu) {
