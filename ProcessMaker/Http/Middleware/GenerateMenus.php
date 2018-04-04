f2a870217ea40178347d8eadaee60e3f94a246e8
Alan Bollinger <velkymx@gmail.com>
2018-04-04 09:35:28 -0700

    Swap sidebar to use generated menus

--- ProcessMaker/Http/Middleware/GenerateMenus.php
+++ ProcessMaker/Http/Middleware/GenerateMenus.php
@@ -32,30 +32,54 @@ class GenerateMenus
             $cases->add('Drafts');
             $cases->add('Mine');
             $cases->add('Find');
-            //
-            // $build = $menu->raw('Build',['class' => 'sidebar-header'])
-            // ->prepend('<i class="fa fa-wrench"></i> ');
-            // $app = $build->add('Application');
-            // $app->add('Processes');
-            // $app->add('Tables');
-            // $app->add('Forms');
-            // $app->add('Scripts');
-            //
-            // $manage = $menu->raw('Manage',['class' => 'sidebar-header'])
-            // ->prepend('<i class="fa fa-list-ul"></i> ');
-            // $org = $manage->add('Organization',['class' => 'sidebar-header']);
-            // $org->add('Users', ['route' => 'management-users-index']);
-            // $org->add('Groups');
-            // $org->add('Roles');
-            //
-            // $appearance = $manage->add('Appearance');
-            // $appearance->add('Themes');
-            // $appearance->add('Logo Management');
-            //
-            // $manage->add('Localization and Internationalization');
-            // $manage->add('Notifications');
-            // $manage->add('Plugin Manager');
-            // $manage->add('Logs & Audit');
+        });
+
+        Menu::make('execute', function ($menu) {
+            $execute = $menu;
+            $tasks = $execute->add('Execute',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-list-ul"></i> ');
+            $tasks->add('Pending',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Unclaimed',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Completed',['route'  => 'home', 'id' => 'home']);
+
+            $cases = $execute->add('Cases',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-briefcase"></i> ');
+            $cases->add('New');
+            $cases->add('Drafts');
+            $cases->add('Mine');
+            $cases->add('Find');
+        });
+
+        Menu::make('manage', function ($menu) {
+            $execute = $menu;
+            $tasks = $execute->add('Manage',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-list-ul"></i> ');
+            $tasks->add('Pending',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Unclaimed',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Completed',['route'  => 'home', 'id' => 'home']);
+
+            $cases = $execute->add('Cases',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-briefcase"></i> ');
+            $cases->add('New');
+            $cases->add('Drafts');
+            $cases->add('Mine');
+            $cases->add('Find');
+        });
+
+        Menu::make('build', function ($menu) {
+            $execute = $menu;
+            $tasks = $execute->add('Build',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-list-ul"></i> ');
+            $tasks->add('Pending',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Unclaimed',['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Completed',['route'  => 'home', 'id' => 'home']);
+
+            $cases = $execute->add('Cases',['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-briefcase"></i> ');
+            $cases->add('New');
+            $cases->add('Drafts');
+            $cases->add('Mine');
+            $cases->add('Find');
         });
 
         return $next($request);
