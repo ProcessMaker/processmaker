1f9d271c6b26947d13d8d916d6370cebc425d3d0
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-04-09 16:26:57 -0400

    Feature/28

---
+++ ProcessMaker/Model/TaskUser.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ProcessMaker\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Watson\Validating\ValidatingTrait;
+
+/**
+ * Represents an Eloquent model of a Task User
+ *
+ * @package ProcessMaker\Model
+ *
+ * @property int TAS_USER_ID
+ * @property string TAS_UID
+ * @property string USR_UID
+ * @property int TU_TYPE
+ * @property int TU_RELATION
+ */
+class TaskUser extends Model
+{
+    use ValidatingTrait;
+
+    protected $table = 'TASK_USER';
+    protected $primaryKey = 'TAS_USER_ID';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'TAS_ID',
+        'TAS_UID',
+        'USR_UID',
+        'TU_TYPE',
+        'TU_RELATION'
+    ];
+
+    protected $attributes = [
+        'TAS_ID' => '',
+        'TAS_UID' => null,
+        'USR_UID' => null,
+        'TU_TYPE' => '',
+        'TU_RELATION' => ''
+    ];
+    protected $casts = [
+        'TAS_UID' => 'string',
+        'USR_UID' => 'string',
+        'TU_TYPE' => 'int',
+        'TU_RELATION' => 'int'
+    ];
+
+    protected $rules = [
+        'TAS_UID' => 'required|max:32',
+        'USR_UID' => 'required|max:32',
+        'TU_TYPE' => 'required',
+        'TU_RELATION' => 'required'
+    ];
+
+    /**
+     * Get information user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne(User::class, 'USR_UID', 'USR_UID');
+    }
+
+    /**
+     * Get information user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function group()
+    {
+        return $this->hasOne(Group::class, 'GRP_UID', 'USR_UID');
+    }
+
+}
