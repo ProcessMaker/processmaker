13d15d07adc8da986e5202137d8f495f412d9279
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-04-11 16:40:20 -0400

    Add test save assignment

--- ProcessMaker/Managers/TaskManager.php
+++ ProcessMaker/Managers/TaskManager.php
@@ -47,31 +47,41 @@ class TaskManager
      * @param Task $task
      * @param array $options
      *
-     * @return array
+     * @throws TaskAssignedException
      */
     public function saveAssignee(Process $process, Task $task, array $options)
     {
-        try
-        {
-            //todo validate Process and task
-            switch (strtoupper($options['aas_type'])) {
-                case 'USER':
-                    $check = User::where('USR_UID', $options['ass_uid'])->get()->toArray();
-                    break;
-                case 'GROUP':
-                    $check = Group::where('GRP_UID', $options['ass_uid'])->get()->toArray();
-                    break;
-                default:
-                    $check = [];
-                    break;
-            }
-            if (!$check) {
-                throw new TaskAssignedException(__('This id: {uid} does not correspond to a registered {type}', ['uid' => $options['ass_uid'], 'type' => $options['aas_type']]));
-            }
-
-        } catch (TaskAssignedException $exception) {
-            throw $exception;
+        //todo validate Process and task
+        $query= TaskUser::where('TAS_UID', $task->TAS_UID)->type(1);
+        $type = 1;
+        switch (strtoupper($options['aas_type'])) {
+            case 'USER':
+                $check = User::where('USR_UID', $options['aas_uid'])->get()->toArray();
+                $query->onlyUsers();
+                break;
+            case 'GROUP':
+                $check = Group::where('GRP_UID', $options['aas_uid'])->get()->toArray();
+                $query->onlyGroups();
+                $type = 2;
+                break;
+            default:
+                $check = [];
+                break;
         }
+        if (!$check) {
+            throw new TaskAssignedException(__('This id :uid does not correspond to a registered :type', ['uid' => $options['aas_uid'], 'type' => $options['aas_type']]));
+        }
+        $exist = $query->where('USR_UID', $options['aas_uid'])->get()->toArray();
+        if ($exist) {
+            throw new TaskAssignedException(__('This ID: :user is already assigned to task: :task', ['user' => $options['aas_uid'], 'task' => $task->TAS_UID]));
+        }
+        $assigned = new TaskUser();
+        $assigned->TAS_UID = $task->TAS_UID;
+        $assigned->USR_UID = $options['aas_uid'];
+        $assigned->TU_RELATION = $type;
+        $assigned->TU_TYPE = 1;
+        $assigned->saveOrFail();
+
     }
 
 }
