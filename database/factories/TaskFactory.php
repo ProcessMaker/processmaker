4a6701d8772ce50235103068f6907adaa42dd34f
Alan Bollinger <velkymx@gmail.com>
2018-04-23 11:04:52 -0700

    Task Factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,22 @@
+<?php
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Model factory for a Task
+ */
+
+use Faker\Generator as Faker;
+
+$factory->define(\ProcessMaker\Model\Task::class, function (Faker $faker) {
+
+    return [
+
+      'PRO_UID' => function () {
+            return factory(\ProcessMaker\Model\Process::class)->create()->PRO_UID;
+        },
+      'TAS_UID' => str_replace('-', '', Uuid::uuid4()),
+      'TAS_TITLE' => $faker->sentence(4),
+      'TAS_DESCRIPTION' => $faker->paragraph,
+
+    ];
+});
