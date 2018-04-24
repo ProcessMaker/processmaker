4dce8a4ff379946babb1938994376c7ffcb55b62
Mila Endo <milal.alim@gmail.com>
2018-04-24 15:59:54 -0700

    rm unneeded imgs

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,9 +1,6 @@
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light mt-0">
+  <a class="navbar-brand" href="#"><img src="/img/logo.png" style="max-width:40px"></a>
   <div class="mr-auto collapse navbar-collapse " id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
