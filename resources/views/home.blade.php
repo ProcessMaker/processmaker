9b238b7fc05b011625d543dbb2e80f67bdc61b10
Mila Endo <milal.alim@gmail.com>
2018-04-26 10:12:07 -0700

    forms added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,19 @@
 
 @section('content')
 
-<h1>Execute</h1>
-<div class="container">
-                <my-vuetable
-                  api-url="/test"></my-vuetable>
-            </div>
+<h3>User Profile</h1>
+<div class="container bg-light">
+  <form>
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="First name">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Last name">
+    </div>
+  </div>
+</form>
+</div>
 @endsection
 
 @section('sidebar')
