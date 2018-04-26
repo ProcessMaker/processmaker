08639e423339e12b01658f2303b9000f0326b748
Taylor Dondich <taylor@dondich.com>
2018-04-26 15:16:42 -0700

    Update to work in progress with DbSource.

--- ProcessMaker/Model/DbSource.php
+++ ProcessMaker/Model/DbSource.php
@@ -3,6 +3,7 @@ namespace ProcessMaker\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use ProcessMaker\Facades\DatabaseManager;
+use ProcessMaker\Model\Traits\Uuid;
 use Watson\Validating\ValidatingTrait;
 
 /**
@@ -14,24 +15,22 @@ class DbSource extends Model
     use ValidatingTrait {
         isValid as public validatingIsValid;
     }
+    use Uuid;
 
     // We do not store timestamps
     public $timestamps = false;
-    protected $table = 'DB_SOURCE';
-    protected $primaryKey = 'DBS_UID';
-    public $incrementing = false;
 
     protected $appends = [
-        'DBS_DATABASE_DESCRIPTION',
+        'description',
         //this attr. is rewritten
-        'DBS_SERVER',
+        'server',
         //this attr. is rewritten
-        'DBS_DATABASE_NAME'
+        'database_name'
     ];
 
     protected $rules = [
-        'DBS_TYPE' => 'required',
-        'DBS_ENCODE' => 'required'
+        'type' => 'required',
+        'encode' => 'required'
     ];
 
     // List of supported engines
@@ -57,8 +56,8 @@ class DbSource extends Model
     public function getDbsDatabaseDescriptionAttribute()
     {
         return $this->isTns()
-            ? '[' . $this->DBS_TNS . ']' . $this->DBS_DESCRIPTION
-            : $this->DBS_DESCRIPTION;
+            ? '[' . $this->tns . ']' . $this->description
+            : $this->description;
     }
 
     /**
@@ -71,7 +70,7 @@ class DbSource extends Model
     public function getDbsServerAttribute($value)
     {
         $server = $this->isTns()
-            ? '[' . $this->DBS_TNS . ']'
+            ? '[' . $this->tns . ']'
             : $value;
         return $server;
     }
@@ -86,7 +85,7 @@ class DbSource extends Model
     public function getDbsDatabaseNameAttribute($value)
     {
         $server = $this->isTns()
-            ? '[' . $this->DBS_TNS . ']'
+            ? '[' . $this->tns . ']'
             : $value;
         return $server;
     }
@@ -108,41 +107,41 @@ class DbSource extends Model
             $typesExists[] = $val['id'];
         }
 
-        if (!in_array($this->DBS_TYPE, $typesExists)) {
-            $this->validationErrors->add('dbs_type', __('ID_DBC_TYPE_INVALID'));
+        if (!in_array($this->type, $typesExists)) {
+            $this->validationErrors->add('type', __('ID_DBC_TYPE_INVALID'));
         }
 
-        if (isset($this->DBS_SERVER) && $this->DBS_SERVER == '' && !$this->isTns()) {
-            $this->validationErrors->add('dbs_server', __('ID_DBC_SERVER_INVALID'));
+        if (isset($this->server) && $this->server == '' && !$this->isTns()) {
+            $this->validationErrors->add('server', __('ID_DBC_SERVER_INVALID'));
         }
 
-        if (isset($this->DBS_DATABASE_NAME) && $this->DBS_DATABASE_NAME == '' && !$this->isTns()) {
-            $this->validationErrors->add('dbs_database_name', __('ID_DBC_DBNAME_INVALID'));
+        if (isset($this->database_name) && $this->database_name == '' && !$this->isTns()) {
+            $this->validationErrors->add('database_name', __('ID_DBC_DBNAME_INVALID'));
         }
 
-        if (isset($this->DBS_PORT) &&
-            ($this->DBS_PORT == '' || $this->DBS_PORT == 0)
+        if (isset($this->port) &&
+            ($this->port == '' || $this->port == 0)
         ) {
             if (!$this->isTns()) {
-                $this->validationErrors->add('dbs_port', __('ID_DBC_PORT_INVALID'));
+                $this->validationErrors->add('port', __('ID_DBC_PORT_INVALID'));
             }
         }
 
-        if (isset($this->DBS_TNS) && $this->DBS_TNS == '' && $this->isTns()) {
-            $this->validationErrors->add('dbs_tns', __('ID_DBC_TNS_NOT_EXIST'));
+        if (isset($this->tns) && $this->tns == '' && $this->isTns()) {
+            $this->validationErrors->add('tns', __('ID_DBC_TNS_NOT_EXIST'));
         }
 
-        if (isset($this->DBS_ENCODE)) {
+        if (isset($this->encode)) {
             $encodingExists = false;
-            $dbEncodes = DatabaseManager::getEncodingList($this->DBS_TYPE);
+            $dbEncodes = DatabaseManager::getEncodingList($this->type);
             foreach ($dbEncodes as $encoding) {
-                if (strtolower($encoding[0]) == strtolower($this->DBS_ENCODE)) {
+                if (strtolower($encoding[0]) == strtolower($this->encode)) {
                     $encodingExists = true;
                     break;
                 }
             }
             if (!$encodingExists) {
-                $this->validationErrors->add('dbs_encode', __('ID_DBC_ENCODE_INVALID'));
+                $this->validationErrors->add('encode', __('ID_DBC_ENCODE_INVALID'));
             }
         }
 
@@ -157,7 +156,7 @@ class DbSource extends Model
      */
     public function isTns()
     {
-        return $this->DBS_TYPE === 'oracle'
-            && $this->DBS_CONNECTION_TYPE === 'TNS';
+        return $this->type === 'oracle'
+            && $this->connection_type === 'TNS';
     }
 }
