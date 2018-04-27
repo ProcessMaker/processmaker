2bb69bd78b8f4ed0db494178e3f16729bf48074d
Alan Bollinger <velkymx@gmail.com>
2018-04-27 15:44:31 -0700

    Factory utilize relationships

--- database/factories/AppDelegationFactory.php
+++ database/factories/AppDelegationFactory.php
@@ -4,21 +4,27 @@ use Faker\Generator as Faker;
 use Carbon\Carbon;
 use ProcessMaker\Model\Application;
 use ProcessMaker\Model\Task;
+use \ProcessMaker\Model\User;
 
 $factory->define(\ProcessMaker\Model\Delegation::class, function (Faker $faker) {
 
-    $task = factory(Task::class)->create();
-    $application = factory(Application::class)->create();
-
     return [
-        'APP_UID' => $application->APP_UID,
-        'PRO_UID' => $task->PRO_UID,
-        'TAS_UID' => $task->TAS_UID,
+        'APP_UID' => function () {
+            return factory(Application::class)->create()->APP_UID;
+        },
+        'TAS_UID' => function () {
+            return factory(Task::class)->create()->TAS_UID;
+        },
+        'PRO_UID' => function (array $task) {
+            return Task::where('TAS_UID',$task['TAS_UID'])->first()->PRO_UID;
+        },        
         'DEL_THREAD_STATUS' => $faker->randomElement(["OPEN"]),
         'DEL_LAST_INDEX' => $faker->randomElement(["0","1"]),
-        'APP_NUMBER' => $application->APP_NUMBER,
+        'APP_NUMBER' => function (array $delegation) {
+            return Application::where('APP_UID',$delegation['APP_UID'])->first()->APP_NUMBER;
+        },
         'USR_UID' => function () {
-              return factory(\ProcessMaker\Model\User::class)->create()->USR_UID;
+              return factory(User::class)->create()->USR_UID;
           },
         'DEL_DELEGATE_DATE' => Carbon::now(),
         'DEL_DATA' => $faker->paragraph,
