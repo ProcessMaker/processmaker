652a62d1d0862841920835af7f57f786bb97ee61
Mila Endo <milal.alim@gmail.com>
2018-05-17 09:32:09 -0700

    setData successful

--- resources/assets/js/components/inbox.vue
+++ resources/assets/js/components/inbox.vue
@@ -53,12 +53,12 @@ export default {
     return {
       fields: [
         {
-          name: 'name',
-          sortField: 'name',
+          name: 'task',
+          sortField: 'task',
         },
         {
-          name: 'username',
-          sortField: 'username',
+          name: 'user',
+          sortField: 'user',
         },
         {
           name: 'title',
@@ -103,8 +103,7 @@ export default {
     }
   },
   mounted() {
-    // Maybe fetch is a method you define that does the ajax calls.  By calling it
-    // in the mounted handler, it'll do the initial load
+
 
     function getusers() {
       return axios.get('https://jsonplaceholder.typicode.com/users');
@@ -124,58 +123,48 @@ export default {
           return task
         })
       }))
-    },
-        //possibly use map?
-        //loop through users
-        //loop through tasks
-        //find matching userid to user
-        //append the user data to the tasks array
-        //then log the new array
-
-
 
 
     // Fetch our users and then populate the data
     // Here, you'd want to potentially do your various axious calls, merge the
     // data and then make sure you build up the object that vuetable expects.
     // See: https://ratiw.github.io/vuetable-2/#/Data-Format-JSON
-  //   this.$refs.vuetable.setData({
-  //   "links": {
-  //     "pagination": {
-  //       "total": 20,
-  //       "per_page": 2,
-  //       "current_page": 1,
-  //       "last_page": 2,
-  //       "next_page_url": "...",
-  //       "prev_page_url": "...",
-  //       "from": 1,
-  //       "to": 2,
-  //     }
-  //   },
-  //   " ": [
-  //     {
-  //       "id": 1,
-  //       "name": "xxxxxxxxx",
-  //       "nickname": "xxxxxxx",
-  //       "email": "xxx@xxx.xxx",
-  //       "birthdate": "xxxx-xx-xx",
-  //       "gender": "X",
-  //       "group_id": 1,
-  //     },
-  //     {
-  //       "id": 50,
-  //       "name": "xxxxxxxxx",
-  //       "nickname": "xxxxxxx",
-  //       "email": "xxx@xxx.xxx",
-  //       "birthdate": "xxxx-xx-xx",
-  //       "gender": "X",
-  //       "group_id": 3,
-  //     }
-  //   ]
-  // }),
 
-
-
+    this.$refs.vuetable.setData({
+    "links": {
+      "pagination": {
+        "total": 20,
+        "per_page": 2,
+        "current_page": 1,
+        "last_page": 2,
+        "next_page_url": "...",
+        "prev_page_url": "...",
+        "from": 1,
+        "to": 2,
+      }
+    },
+    " ": [
+      {
+        "id": 1,
+        "name": "xxxxxxxxx",
+        "nickname": "xxxxxxx",
+        "email": "xxx@xxx.xxx",
+        "birthdate": "xxxx-xx-xx",
+        "gender": "X",
+        "group_id": 1,
+      },
+      {
+        "id": 50,
+        "name": "xxxxxxxxx",
+        "nickname": "xxxxxxx",
+        "email": "xxx@xxx.xxx",
+        "birthdate": "xxxx-xx-xx",
+        "gender": "X",
+        "group_id": 3,
+      }
+    ]
+  })
+},
   methods: {
     allcap (value) {
       return value.toUpperCase()
