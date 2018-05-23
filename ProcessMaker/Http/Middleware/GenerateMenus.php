8907d215a3fd8207603d90663b861ccd9564f18b
Mila Endo <milal.alim@gmail.com>
2018-05-23 12:01:50 -0700

    #149 fixed errors in generate menus

--- ProcessMaker/Http/Middleware/GenerateMenus.php
+++ ProcessMaker/Http/Middleware/GenerateMenus.php
@@ -1,14 +1,10 @@
 <?php
 namespace ProcessMaker\Http\Middleware;
-
 use Closure;
 use Illuminate\Http\Request;
 use Lavary\Menu\Facade as Menu;
-
-
 class GenerateMenus
 {
-
     /**
      * Generate the core menus that are used in web requests for our application
      *
@@ -20,7 +16,6 @@ class GenerateMenus
     {
         //set’s application’s locale
         app()->setLocale('en');
-
         // Build the menu
         Menu::make('sidebar_admin', function ($menu) {
             $task_items = [
@@ -165,7 +160,6 @@ class GenerateMenus
                 'id' => 'homeid'
               ],
             ];
-
             $tasks = $menu;
             foreach ($task_items as $item) {
                 if ($item['header'] === false) {
@@ -175,7 +169,6 @@ class GenerateMenus
                 }
             }
         });
-
         Menu::make('sidebar_task', function ($menu) {
             $task_items = [
               [
@@ -214,7 +207,6 @@ class GenerateMenus
               'id' => 'homeid'
             ]
           ];
-
             $tasks = $menu;
             foreach ($task_items as $item) {
                 if ($item['header'] === false) {
@@ -224,54 +216,32 @@ class GenerateMenus
                 }
             }
         });
-
-        Menu::make('request', function ($menu) {
-          $task_items = [
-            [
-              'label' => 'In Progress',
-              'route' => 'home',
-              'icon' => 'fa-user',
-              'id' => 'homeid'
-            ],
-            [
-              'label' => 'Draft',
-              'route' => 'home',
-              'icon' => 'fa-users',
-              'id' => 'homeid'
-            ],
-            [
-              'label' => 'Completed',
-              'route' => 'home',
-              'icon' => 'fa-user-plus',
-              'id' => 'homeid'
-            ],
-            [
-              'label' => 'Paused',
-              'route' => 'home',
-              'icon' => 'fa-user-plus',
-              'id' => 'homeid'
-            ]
-          ];
-
+        Menu::make('manage', function ($menu) {
             $execute = $menu;
-
-
-            $tasks = $execute->add('request',['class' => 'h5 text-muted font-weight-light']);
-
-
-            foreach ($task_items as $item) {
-              $tasks->add($item['label'],['route' => $item['route'], 'id' => $item['id'], 'icon' => $item['icon']]);
-            }
-
             $tasks = $execute->add('Manage', ['class' => 'sidebar-header'])->prepend('<i class="fa fa-list-ul"></i> ');
             $tasks->add('Pending', ['route'  => 'home', 'id' => 'home']);
             $tasks->add('Unclaimed', ['route'  => 'home', 'id' => 'home']);
             $tasks->add('Completed', ['route'  => 'home', 'id' => 'home']);
-
             $cases = $execute->add('Cases', ['class' => 'sidebar-header'])->prepend('<i class="fa fa-briefcase"></i> ');
             $cases->add('New');
             $cases->add('Drafts');
             $cases->add('Mine');
             $cases->add('Find');
-
+        });
+        Menu::make('build', function ($menu) {
+            $execute = $menu;
+            $tasks = $execute->add('Build', ['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-list-ul"></i> ');
+            $tasks->add('Pending', ['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Unclaimed', ['route'  => 'home', 'id' => 'home']);
+            $tasks->add('Completed', ['route'  => 'home', 'id' => 'home']);
+            $cases = $execute->add('Cases', ['class' => 'sidebar-header'])
+            ->prepend('<i class="fa fa-briefcase"></i> ');
+            $cases->add('New');
+            $cases->add('Drafts');
+            $cases->add('Mine');
+            $cases->add('Find');
+        });
+        return $next($request);
+    }
 }
