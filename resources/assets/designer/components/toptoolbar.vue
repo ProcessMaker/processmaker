5a710c3a627b82d59294999fed68ffa272d13aae
Mila Endo <milal.alim@gmail.com>
2018-05-23 15:08:10 -0700

    #153 added icons and navbar

--- resources/assets/designer/components/toptoolbar.vue
+++ resources/assets/designer/components/toptoolbar.vue
@@ -1,11 +1,41 @@
 <template>
-  <nav class="nav nav-pills flex-column flex-sm-row position-absolute mt-1">
-    <a  href="#"><i class="fas fa-expand-arrows-alt"></i></a>
-    <a  href="#"><i class="fas fa-undo"></i></a>
-    <a  href="#"><i class="fas fa-redo"></i></a>
-    <a  href="#"><i class="fas fa-search-plus"></i></a>
-    <a  href="#"><i class="fas fa-save"></i></a>
-    <a  href="#"><i class="fas fa-info"></i></a>
-    <a  href="#"><i class="fas fa-times"></i></a>
-  </nav>
+  <ul id="designer-nav" class="nav flex-column flex-md-row position-absolute mt--1 text-light">
+  <li class="mr-auto">
+    <h4 id="designer-nav-header" class="font-weight-light">Invite New Employee</h4>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link active" href="#"><i id="designer-icons" class="fas fa-expand-arrows-alt"></i></a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="#"><i id="designer-icons" class="fas fa-undo"></i></a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="#"><i id="designer-icons" class="fas fa-redo"></i></a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="#"><i id="designer-icons" class="fas fa-search-plus"></i></a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="#"><i id="designer-icons" class="fas fa-save"></i></a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="#"><i id="designer-icons" class="fas fa-info"></i></a>
+  </li>
+  <li class="nav-item ">
+    <a class="nav-link" href="#"><i id="designer-icons" class="fas fa-times"></i></a>
+  </li>
+</ul>
 </template>
+
+<style>
+  #designer-nav{
+    width:97%;
+    background-color:#b6bfc6;
+  }
+  #designer-icons{
+    color:white;
+  }
+  #designer-nav-header{
+    margin: 7px;
+  }
+</style>
