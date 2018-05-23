148bebab56e6429b72594b0bf87db350a02eb520
Mila Endo <milal.alim@gmail.com>
2018-05-23 15:41:43 -0700

    #146 changed html to echo out error message

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -47,7 +47,7 @@
     @endif
     @if(session('error_message'))
       <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+        <strong>{{session('error_message')}}</strong> You should check in on some of those fields below.
           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
