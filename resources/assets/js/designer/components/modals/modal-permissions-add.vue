20024c06805e899bd3199c06d9c5285dded7ee1b
Mila Endo <milal.alim@gmail.com>
2018-05-31 11:58:52 -0700

    added vmodel to form

--- resources/assets/js/designer/components/modals/modal-permissions-add.vue
+++ resources/assets/js/designer/components/modals/modal-permissions-add.vue
@@ -2,44 +2,32 @@
   <b-modal ref="modal" size="lg" centered title="Create Permissions">
       <form>
         <div class="form-group">
-          <label for="casesStatus">Cases Status</label>
-          <select class="form-control" id="casesStatus">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <label for="casesStatus" v-model="casesStatus">{{casesStatus}}</label>
+          <select class="form-control" id="casesStatus" v-model="casesSelect">
+          <option  v-for="select in casesSelectOptions">{{select}}</option>
           </select>
         </div>
 
         <div class="form-group">
-          <label for="targetTask">Target Task</label>
-          <select class="form-control" id="targetTask">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <label for="targetTask" v-model="targetTask">{{targetTask}}</label>
+          <select class="form-control" id="targetTask" v-model="targetSelect">
+          <option v-for="select in targetSelectOptions">{{select}}</option>
           </select>
         </div>
 
         <div class="form-group">
-          <label for="groupOrUser">Group or User</label>
+          <label for="groupOrUser" v-model="groupOrUser">{{groupOrUser}}</label>
           <input type="text" class="form-control" id="groupOrUser">
         </div>
 
         <div class="form-group">
-        <label for="originTask">Origin Task</label>
-        <select class="form-control" id="originTask">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+        <label for="originTask" v-model="originTask">{{originTask}}</label>
+        <select class="form-control" id="originTask" v-model="originSelect">
+          <option v-for="select in targetSelectOptions">{{select}}</option>
         </select>
         </div>
         <div class="form-group">
-          <label>Participation Required?</label>
+          <label v-model="participationRequired">{{participationRequired}}</label>
           <div>
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
@@ -53,24 +41,16 @@
           </div>
         </div>
         <div class="form-group">
-        <label for="typeSelect">Type</label>
-        <select class="form-control" id="typeSelect">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+        <label for="typeSelect" v-model="type">{{type}}</label>
+        <select class="form-control" id="typeSelect" v-model="typeSelect">
+          <option v-for="select in typeSelectOptions">{{select}}</option>
         </select>
         </div>
 
         <div class="form-group">
-        <label for="PermissionsSelect">Permission</label>
-        <select class="form-control" id="PermissionsSelect">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+        <label for="PermissionsSelect" v-model="permission">{{permission}}</label>
+        <select class="form-control" id="PermissionsSelect" v-model="permissionSelect">
+          <option v-for="select in typeSelectOptions">{{select}}</option>
         </select>
         </div>
     </form>
@@ -83,12 +63,38 @@ export default {
   data() {
     return {
       // form models here
+      'casesStatus': "Cases Status",
+      'casesSelect': 1,
+      'casesSelectOptions':[
+        1,2,3,4,5
+      ],
+      'targetTask': "Target Task",
+      'targetSelect': 1,
+      'targetSelectOptions':[
+        1,2,3,4,5
+      ],
+      'groupOrUser':"Group Or User",
+      'originTask':"Origin Task",
+      'originSelect': 1,
+      'originSelectOptions':[
+        1,2,3,4,5
+      ],
+      'participationRequired':"Participation Required?",
+      'type':"Type",
+      'typeSelect': 1,
+      'typeSelectOptions':[
+        1,2,3,4,5
+      ],
+      'permission':"Permission",
+      'permissionSelect':1,
+      'permissionSelectOptions':[
+        1,2,3,4,5
+      ]
     }
-  }
+  },
   mounted() {
     // Show our modal as soon as we're created
     this.$refs.modal.show();
   }
 };
 </script>
-
