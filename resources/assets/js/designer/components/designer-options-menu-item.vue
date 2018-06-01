b5a74ed51cdf60b483dcb74d12c23837a4c8905e
Mila Endo <milal.alim@gmail.com>
2018-06-01 15:40:56 -0700

    #178 rm icons from rows that do not have it in zeplin

--- resources/assets/js/designer/components/designer-options-menu-item.vue
+++ resources/assets/js/designer/components/designer-options-menu-item.vue
@@ -1,18 +1,22 @@
 <template>
-
-<a href="#" @click="titleClick" class="object-menu-item">
-  {{title}}
-  <i v-if="buttonvisible == false" class="plus-icon fas fa-plus-circle"></i>
-  <button @click.stop="buttonClick" type="button" class="add-button btn btn-secondary btn-sm float-right"><i class="fas fa-plus fa-sm"></i> Add</button>
-</a>
-
+<div>
+  <a href="#" @click="titleClick" v-if="hasadd == true" class="object-menu-item">
+    {{title}}
+    <i v-if="buttonvisible == false" class="plus-icon fas fa-plus-circle"></i>
+    <button @click.stop="buttonClick" type="button" class="add-button btn btn-secondary btn-sm float-right"><i class="fas fa-plus fa-sm"></i> Add</button>
+  </a>
+  <a href="#" @click="titleClick" v-else class="object-menu-item">
+    {{title}}
+  </a>
+</div>
 </template>
 
 
 <script>
 export default{
   props:[
-    'title'
+    'title',
+    'hasadd'
   ],
   data(){
     return{
