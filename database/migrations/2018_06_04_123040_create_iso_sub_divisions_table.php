1b8f5391e1e980e50490c3600aaeaf96412db07f
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-06-04 11:29:09 -0400

    Add countries

---
+++ database/migrations/2018_06_04_123040_create_iso_sub_divisions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIsoSubDivisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('iso_sub_divisions', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->unsignedInteger('country_id');
+            $table->string('uid', 2)->unique();
+            $table->string('name')->default('');
+
+            // setup relationship for Country we belong to
+            $table->foreign('country_id')->references('id')->on('iso_countries')->ondelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('iso_sub_divisions');
+    }
+}
