f096f803dd9d5c3326ee5cfc5dde176b67bb9feb
Mila Endo <milal.alim@gmail.com>
2018-06-04 12:19:03 -0700

    #187 added Editor

--- resources/assets/js/designer/main.js
+++ resources/assets/js/designer/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Designer from './Designer'
+import tinymce from 'tinymce/tinymce'
+import Editor from '@tinymce/tinymce-vue'
+
 
 // Bootstrap our Designer application
 new Vue({
