d014ff8e7fdf238a95975cc1b57dd46f0f71230a
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-06-04 11:29:44 -0400

    Add Table roles and permissions

---
+++ database/migrations/2018_06_04_123300_create_permission_role_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_role', function(Blueprint $table)
+        {
+            $table->unsignedInteger('role_id');
+            $table->unsignedInteger('permission_id');
+            $table->primary(['role_id','permission_id']);
+
+            // setup relationship for Rol we belong to
+            $table->foreign('role_id')->references('id')->on('roles')->ondelete('cascade');
+            // setup relationship for Permission we belong to
+            $table->foreign('permission_id')->references('id')->on('permissions')->ondelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_roles');
+    }
+}
