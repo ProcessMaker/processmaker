f7fe0b7d9e22fd0ff487906550643a65e4af58c0
Marco A. Nina Mena <marcoantonina@gmail.com>
2018-06-05 00:12:42 -0400

    Fix emailServer

--- database/factories/ProcessFileFactory.php
+++ database/factories/ProcessFileFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 use Ramsey\Uuid\Uuid;
 use Faker\Generator as Faker;
 
@@ -7,20 +8,20 @@ use Faker\Generator as Faker;
  */
 $factory->define(\ProcessMaker\Model\ProcessFile::class, function (Faker $faker) {
     //was need base_path for complete to relative path.
-    $filepath = $faker->file('/tmp',base_path('tests/shared/public'));
+    $filepath = $faker->file('/tmp', base_path('tests/shared/public'));
     return [
-        'PRF_UID'            => str_replace('-', '', Uuid::uuid4()),
-        'process_id'            => function () {
+        'PRF_UID' => str_replace('-', '', Uuid::uuid4()),
+        'process_id' => function () {
             $pro = factory(\ProcessMaker\Model\Process::class)->create();
             return $pro->id;
         },
-        'USR_UID'            => str_replace('-', '', Uuid::uuid4()),
+        'USR_UID' => str_replace('-', '', Uuid::uuid4()),
         'PRF_UPDATE_USR_UID' => str_replace('-', '', Uuid::uuid4()),
-        'PRF_PATH'           => $filepath,
-        'PRF_TYPE'            => 'file',
-        'PRF_DRIVE'           => 'public',
+        'PRF_PATH' => $filepath,
+        'PRF_TYPE' => 'file',
+        'PRF_DRIVE' => 'public',
         'PRF_PATH_FOR_CLIENT' => basename($filepath),
-        'PRF_CREATE_DATE'    => $faker->date(),
-        'PRF_UPDATE_DATE'    => $faker->date(),
+        'PRF_CREATE_DATE' => $faker->date(),
+        'PRF_UPDATE_DATE' => $faker->date(),
     ];
 });
