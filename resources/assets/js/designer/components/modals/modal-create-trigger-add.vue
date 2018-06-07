19377978c49be383061e752accd06dbf620985e4
Mila Endo <milal.alim@gmail.com>
2018-06-07 13:01:20 -0700

    #183 trigger modal created

--- resources/assets/js/designer/components/modals/modal-create-trigger-add.vue
+++ resources/assets/js/designer/components/modals/modal-create-trigger-add.vue
@@ -1,58 +1,22 @@
 <template>
-  <b-modal ref="modal" size="md" @hidden="onHidden" centered title="Create Permission">
+  <b-modal ref="modal" size="md" @hidden="onHidden" centered title="Create Custom Trigger">
       <form>
         <div class="form-group">
-          <label for="casesStatus" v-model="casesStatus">{{casesStatus}}</label>
-          <select class="form-control" id="casesStatus" v-model="casesSelect">
-          <option  v-for="select in casesSelectOptions">{{select}}</option>
-          </select>
+          <label for="title" v-model="title">{{title}}</label>
+          <input type="text" class="form-control" id="title">
         </div>
+        <div class="form-group">
+          <label for="description">{{description}}</label>
+          <textarea class="form-control" id="description" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="code">{{code}}</label>
+          <textarea class="form-control bottom-label-form" id="code" rows="3"></textarea>
+          <label class="form-group bottom-label">Press ctrl+space to get the function list</label>
+        </div>
+        <button type="submit" class="btn variable-buttons text-light">@@</button>
+        <button type="submit" class="btn variable-buttons text-light">OPEN EDITOR</button>
 
-        <div class="form-group">
-          <label for="targetTask" v-model="targetTask">{{targetTask}}</label>
-          <select class="form-control" id="targetTask" v-model="targetSelect">
-          <option v-for="select in targetSelectOptions">{{select}}</option>
-          </select>
-        </div>
-
-        <div class="form-group">
-          <label for="groupOrUser" v-model="groupOrUser">{{groupOrUser}}</label>
-          <input type="text" class="form-control" id="groupOrUser">
-        </div>
-
-        <div class="form-group">
-        <label for="originTask" v-model="originTask">{{originTask}}</label>
-        <select class="form-control" id="originTask" v-model="originSelect">
-          <option v-for="select in targetSelectOptions">{{select}}</option>
-        </select>
-        </div>
-        <div class="form-group">
-          <label v-model="participationRequired">{{participationRequired}}</label>
-          <div>
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-            <label class="form-check-label" for="inlineRadio1">Yes</label>
-          </div>
-
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-            <label class="form-check-label" for="inlineRadio2">No</label>
-          </div>
-          </div>
-        </div>
-        <div class="form-group">
-        <label for="typeSelect" v-model="type">{{type}}</label>
-        <select class="form-control" id="typeSelect" v-model="typeSelect">
-          <option v-for="select in typeSelectOptions">{{select}}</option>
-        </select>
-        </div>
-
-        <div class="form-group">
-        <label for="PermissionsSelect" v-model="permission">{{permission}}</label>
-        <select class="form-control" id="PermissionsSelect" v-model="permissionSelect">
-          <option v-for="select in permissionSelectOptions">{{select}}</option>
-        </select>
-        </div>
     </form>
 
     <template slot="modal-footer">
@@ -72,33 +36,9 @@ export default {
   data() {
     return {
       // form models here
-      'casesStatus': "Cases Status",
-      'casesSelect': "All",
-      'casesSelectOptions':[
-        'All','Assigned','Unassigned','Completed','Paused'
-      ],
-      'targetTask': "Target Task",
-      'targetSelect': 'All',
-      'targetSelectOptions':[
-        'All','Task 1', 'Task 2'
-      ],
-      'groupOrUser':"Group Or User",
-      'originTask':"Origin Task",
-      'originSelect': "All",
-      'originSelectOptions':[
-        'All','Task 1', 'Task 2'
-      ],
-      'participationRequired':"Participation Required?",
-      'type':"Type",
-      'typeSelect': 'Form',
-      'typeSelectOptions':[
-        'Form', 'Attachment', 'Status', 'Summary', 'Comments','Input Document', 'Output Document'
-      ],
-      'permission':"Permission",
-      'permissionSelect': 'View',
-      'permissionSelectOptions':[
-        'View', 'Block'
-      ]
+      'title':"Title",
+      'description':"Description",
+      'code': "Code"
     }
   },
   methods:{
@@ -116,5 +56,15 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-
+.variable-buttons{
+  background-color: rgb(109,124,136);
+  font-size: 14px;
+  font-weight: 300;
+}
+.bottom-label-form{
+  margin-bottom: -6px;
+}
+.bottom-label{
+  font-size: 10px;
+}
 </style>
