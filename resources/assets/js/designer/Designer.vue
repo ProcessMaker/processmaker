aa68ce8afaa42c756bcb06c31364171ff7ca1a19
Mila Endo <milal.alim@gmail.com>
2018-06-07 15:57:09 -0700

    #185 create database modal created

--- resources/assets/js/designer/Designer.vue
+++ resources/assets/js/designer/Designer.vue
@@ -35,6 +35,8 @@ import designerobjectsmenu from "./components/designer-objects-menu";
 import modalPermissionsAdd from "./components/modals/modal-permissions-add";
 
 import modalPublicFileAdd from "./components/modals/modal-public-file-add";
+
+import modalCreateDatabaseAdd from "./components/modals/modal-create-database-add";
 //
 import modalFormsAdd from "./components/modals/modal-forms-add";
 
@@ -51,6 +53,7 @@ export default {
     ///camel case because vue will transform it into dash's
     modalPublicFileAdd,
     modalFormsAdd,
+    modalCreateDatabaseAdd,
     crown
   },
   data() {
@@ -75,6 +78,9 @@ export default {
           break;
         case 'forms':
           this.modalComponent = 'modal-forms-add'
+          break;
+        case 'database-connections':
+          this.modalComponent = 'modal-create-database-add'
           break;
         default:
           alert(key + ' add modal not yet implemented.')
