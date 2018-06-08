31802fe26833e1e8662154f4fb56ba2fdbcc091d
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-06-08 09:27:55 -0400

    Change test dusk task

--- tests/Browser/TasksListTest.php
+++ tests/Browser/TasksListTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Browser;
 
+use Faker\Factory as Faker;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Artisan;
 use ProcessMaker\Model\Process;
 use ProcessMaker\Model\Task;
 use ProcessMaker\Model\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
+
 
 class TasksListTest extends DuskTestCase
 {
@@ -20,23 +20,65 @@ class TasksListTest extends DuskTestCase
      *
      * @throws \Throwable
      */
-    public function testPageTask()
+    public function testListTask()
     {
-        $this->browse(function (Browser $browser) {
-            $this->artisan('db:seed');
+        //populate data for test
+        $this->artisan('db:seed');
 
-            $user = User::find(1);
-            $process = factory(Process::class)->create([
-                'creator_user_id' => $user->id
-            ]);
+        $user = User::find(1);
+        $process = factory(Process::class)->create([
+            'creator_user_id' => $user->id
+        ]);
 
-            factory(Task::class, 10)->create([
-                'process_id' => $process->id
-            ]);
+        $items = Faker::create()->numberBetween(1,9);
+        $tasks = factory(Task::class, $items)->create([
+            'process_id' => $process->id
+        ]);
+        $titleSearch = 'Task title of Search...';
+        factory(Task::class)->create([
+            'title' => $titleSearch,
+            'process_id' => $process->id
+        ]);
+        $items++;
 
-            $browser->loginAs($user)
+        $this->browse(function (Browser $browser) use ($user, $process, $tasks, $items, $titleSearch) {
+
+            $tableId = '#tasks-listing';
+            $selectTableRows = $tableId . ' tbody tr';
+            $inputSearch = 'input.form-control.col-sm-3';
+
+            $elements = $browser->loginAs($user)
                 ->visit('/process/' . $process->uid . '/tasks')
-                ->assertSee('Tasks');
+                ->waitFor($tableId)
+                ->assertSee('Tasks')
+                ->elements($selectTableRows);
+
+            //Rows(Tasks introduced)
+            $this->assertCount($items, $elements);
+
+            //name of columns
+            $browser->whenAvailable($tableId, function ($grid) {
+                $grid->assertSee('TITLE');
+                $grid->assertSee('DESCRIPTION');
+                $grid->assertSee('TYPE');
+                $grid->assertSee('CREATED AT');
+                $grid->assertSee('UPDATED AT');
+            });
+
+            //Set input search
+            $browser->type($inputSearch, $titleSearch)
+                ->pause(500);
+
+            //validating result of search
+            $elements =  $browser->waitFor($tableId)
+                ->assertSee('Tasks')
+                ->elements($selectTableRows);
+
+            //rows in search
+            $this->assertCount(1, $elements);
+            $browser->assertSee($titleSearch);
+            $browser->assertDontSee($tasks[0]->title);
+
         });
     }
 }
