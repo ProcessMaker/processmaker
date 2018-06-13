7e57edf9383ba2981bef88c9d1d575a776e708b1
hjonathan <jonathan.quispe@processmaker.com>
2018-06-13 11:59:52 -0400

    update min-width in crown.vue

--- resources/assets/js/designer/components/crown.vue
+++ resources/assets/js/designer/components/crown.vue
@@ -2,25 +2,25 @@
     <div v-show="visible" class="designer-container-crown" v-bind:style="{ top: y+'px', left:x+'px' }">
         <div class="d-flex flex-row">
             <div class="item-crown">
-                <img id="bpmn:Task" src="../img/task.svg" height="18"
+                <img id="bpmn:Task" src="../img/task.svg" height="20"
                      @click="createAction($event)">
             </div>
             <div class="item-crown">
-                <img id="bpmn:ExclusiveGateway" src="../img/exclusive-gateway.svg" height="25"
+                <img id="bpmn:ExclusiveGateway" src="../img/exclusive-gateway.svg" height="27"
                      @click="createAction($event)">
             </div>
             <div class="item-crown">
                 <img id="bpmn:IntermediateEmailEvent" name="IntermediateEmailEvent"
-                     src="../img/intermediate-email-event.svg" height="25" @click="createAction($event)">
+                     src="../img/intermediate-email-event.svg" height="27" @click="createAction($event)">
             </div>
         </div>
         <div class="d-flex flex-row">
             <div class="item-crown">
                 <img id="bpmn:EndEvent" name="EndEvent"
-                     src="../img/end-event.svg" height="25" @click="createAction($event)">
+                     src="../img/end-event.svg" height="27" @click="createAction($event)">
             </div>
             <div class="item-crown">
-                <img id="bpmn:EndEvent" src="../img/corona-flow.png" height="25"
+                <img id="bpmn:EndEvent" src="../img/corona-flow.png" height="28"
                      @click="createFlow($event)">
             </div>
             <div class="item-crown">
@@ -94,7 +94,8 @@
         display: table-cell;
         padding: 3px;
         text-align: center;
-        font-size: 19px;
+        font-size: 22px;
+        min-width: 35px;
     }
 
     .icon-crown {
