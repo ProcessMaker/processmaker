c7007bdd31b31b1fa36b84e92c02f79a66cfcd81
Leah Miller <killahdillah@Leahs-MacBook-Pro.local>
2018-06-19 13:50:49 -0700

    icon now displaying

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.layout')
 @section('content')
-<div id="404-error">
-    <div></div>
+<div class="error-404">
+    <div class="error-404-icon">
+        <img src="/img/robot.png"/>
+    </div>
     <div class="error-content">
         <h1>Oops!</h1>
         <p>The page you are looking for could not be found</p>
     </div>
 </div>
+@endsection
+
+@section('css')
+
+<style lang="scss" scoped>
+    .error-404 {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .error-404-icon {
+        /* height: 200px; */
+        margin: auto;
+    }
+    img {
+        margin-left: -33px;
+    }
+
+</style>
 @endsection
