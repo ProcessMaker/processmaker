cb9ce1b8a7dc15c25b62a14750dfed6bbd824f86
Leah Miller <killahdillah@Leahs-MacBook-Pro.local>
2018-06-19 11:48:12 -0700

    robot img not displaying

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 @section('content')
-<div>
-    <div id="404-error"></div>
+<div id="404-error">
+    <div></div>
     <div class="error-content">
         <h1>Oops!</h1>
         <p>The page you are looking for could not be found</p>
