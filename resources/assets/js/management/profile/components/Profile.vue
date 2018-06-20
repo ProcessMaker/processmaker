f651613f939522e7d265be6d4d723d521bad9a52
Mila Endo <milal.alim@gmail.com>
2018-06-20 11:05:31 -0700

    #292 modal added

--- resources/assets/js/management/profile/components/Profile.vue
+++ resources/assets/js/management/profile/components/Profile.vue
@@ -5,38 +5,44 @@
       <div class="custom-file">
         <div v-if="!image" class="profile-avatar-none text-light">JB</div>
         <div v-else ><img :src="image" class="profile-avatar"></div>
-        <label for="customFile"><img class="profile-overlay" align="center" src="/img/avatar-profile-overlay.png"></label>
+        <img class="profile-overlay" align="center" src="/img/avatar-profile-overlay.png" @click="openModal()">
         <input type="file" class="custom-file-input" id="customFile" @change="onFileChange">
       </div>
     </div>
+    <b-modal ref="myModalRef" hide-footer title="Using Component Methods">
+      <div class="d-block text-center">
+        <h3>Hello From My Modal!</h3>
+      </div>
+      <b-btn class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-btn>
+    </b-modal>
     <form class="pl-5 pr-5">
       <div class="row form-group">
         <div class="col">
-          <label for="inputAddress">Address</label>
+          <label for="inputAddress">First Name</label>
           <input type="text" class="form-control" placeholder="First name">
         </div>
         <div class="col">
-          <label for="inputAddress">Address</label>
+          <label for="inputAddress">Last Name</label>
           <input type="text" class="form-control" placeholder="Last name">
         </div>
       </div>
       <div class="row form-group">
         <div class="col">
-          <label for="inputAddress">Address</label>
+          <label for="inputAddress">User Name</label>
           <input type="text" class="form-control" placeholder="First name">
         </div>
         <div class="col">
-          <label for="inputAddress">Address</label>
+          <label for="inputAddress">Email</label>
           <input type="text" class="form-control" placeholder="Last name">
         </div>
       </div>
       <div class="row form-group">
         <div class="col">
-          <label for="inputAddress">Address</label>
+          <label for="inputAddress">New Password</label>
           <input type="text" class="form-control" placeholder="First name">
         </div>
         <div class="col">
-          <label for="inputAddress">Address</label>
+          <label for="inputAddress">Change Password</label>
           <input type="text" class="form-control" placeholder="Last name">
         </div>
       </div>
@@ -49,12 +55,9 @@
      </div>
      <div class="row form-group">
        <div class="col">
-        <label for="inputState">State</label>
-        <select id="inputState" class="form-control">
-          <option selected>Choose...</option>
-          <option>...</option>
-        </select>
-        </div>
+         <label for="inputAddress">City</label>
+         <input type="text" class="form-control" placeholder="First name">
+       </div>
         <div class="col">
           <label for="inputState">State</label>
           <select id="inputState" class="form-control">
@@ -109,9 +112,10 @@
 <script>
 
 import VueCroppie from 'vue-croppie';
+
 export default{
   components:{
-    VueCroppie
+    VueCroppie,
   },
   data(){
     return{
@@ -119,6 +123,12 @@ export default{
     }
   },
   methods: {
+    openModal () {
+      this.$refs.myModalRef.show()
+    },
+    hideModal () {
+     this.$refs.myModalRef.hide()
+    },
     onFileChange(e) {
       var files = e.target.files || e.dataTransfer.files;
       if (!files.length)
@@ -140,6 +150,9 @@ export default{
 </script>
 
 <style lang="scss" scoped>
+  form{
+    margin-top: -34px;
+  }
   .form-wrap{
     max-width: 620px;
   }
@@ -161,6 +174,6 @@ export default{
   .profile-overlay{
     position: absolute;
     margin-left: 220px;
-    margin-top: -92px;
+    margin-top: -82px;
   }
 </style>
