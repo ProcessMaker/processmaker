f86b7142405bd6e99c35824716a912911b1eefb9
Mila Endo <milal.alim@gmail.com>
2018-06-25 14:56:58 -0700

    #328 created vue components for different form types

---
+++ resources/assets/js/components/common/forms/FormInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="form-group">
+    <label v-uni-for="label">{{label}}</label>
+    <input v-uni-id="label" :placeholder="placeholder" class="form-control" :class="{'is-invalid': error}" @input="updateValue">
+    <div v-if="error" class="invalid-feedback">{{error}}</div>
+  </div>
+</template>
+
+
+<script>
+import { createUniqIdsMixin } from 'vue-uniq-ids'
+
+// Create the mixin
+const uniqIdsMixin = createUniqIdsMixin()
+
+export default {
+  mixins: [uniqIdsMixin],
+  props: [
+    'label',
+    'error',
+    'placeholder',
+    'value',
+  ],
+  data() {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    updateValue(e) {
+      this.content = e.target.value;
+      this.$emit('input', this.content)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/assets/js/components/common/forms/FormSelect.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="form-group">
+    <label v-uni-for="label">{{label}}</label>
+    <select v-uni-id="label" class="form-control" :class="{'is-invalid': error}" @input="updateValue">
+        <option :selected="selected" v-for="option in options">{{option}}</option>
+    </select>
+    <div v-if="error" class="invalid-feedback">{{error}}</div>
+  </div>
+</template>
+
+
+<script>
+import { createUniqIdsMixin } from 'vue-uniq-ids'
+
+// Create the mixin
+const uniqIdsMixin = createUniqIdsMixin()
+
+export default {
+  mixins: [uniqIdsMixin],
+  props: [
+    'label',
+    'error',
+    'selected',
+    'value',
+    'options'
+  ],
+  data() {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    updateValue(e) {
+      this.content = e.target.value;
+      this.$emit('input', this.content)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
