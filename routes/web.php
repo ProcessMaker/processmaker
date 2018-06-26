04e1860bd1e44de20a3e57a08d6810380fcd760b
Alan Bollinger <velkymx@gmail.com>
2018-06-26 11:25:13 -0700

    Test route for new password

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Auth::routes();
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
 $this->post('login', 'Auth\LoginController@login');
 $this->get('logout', 'Auth\LoginController@logout')->name('logout');
+$this->get('password/newpass', function(){
+  return view('auth.passwords.reset',['title' => __('Reset Your Password')]);
+})->name('password-reset');
 
 // Password Reset Routes...
 // $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
