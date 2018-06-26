4ef8f52b937fbeb33266427eb89da724413c2f7a
Alan Bollinger <velkymx@gmail.com>
2018-06-26 10:52:57 -0700

    Added missing controllers + route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+Auth::routes();
 
 // Authentication Routes...
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -7,10 +8,14 @@ $this->post('login', 'Auth\LoginController@login');
 $this->get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Password Reset Routes...
-$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+// $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+// $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+// $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+// $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+// $this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+// $this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+// $this->post('password/reset', 'Auth\PasswordController@reset');
 
 $this->middleware(['auth', 'apitoken'])->group(function() {
 
