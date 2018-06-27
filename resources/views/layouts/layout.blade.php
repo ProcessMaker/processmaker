0d317953cf7e2f48e9535453357de497b0356427
Alan Bollinger <velkymx@gmail.com>
2018-06-27 13:19:52 -0700

    Add translation to title

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api-token" content="{{ session('apiToken')['access_token']}}">
-    <title>{{ $title or 'Welcome' }} - {{__('ProcessMaker')}}</title>
+    <title>{{ $title or __('Welcome') }} - {{__('ProcessMaker')}}</title>
     <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico" />
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @yield('css')
