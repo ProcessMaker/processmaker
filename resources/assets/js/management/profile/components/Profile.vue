3b50841539cf32148c1dbcac55b11c8e58371273
Taylor Dondich <taylor@dondich.com>
2018-06-27 12:01:25 -0700

    Update to begin pulling and pushing to profile api endpoint.

--- resources/assets/js/management/profile/components/Profile.vue
+++ resources/assets/js/management/profile/components/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="form-wrap container bg-light mt-3 p-5">
+  <div v-if="loaded" class="form-wrap container bg-light mt-3 p-5">
     <h3 class="pl-5">Profile</h3>
     <div class="modal-wrapper">
       <avatar :uid="uid" class="avatar-wrapper">
@@ -14,21 +14,21 @@
       <div class="row form-group">
         <div class="col">
           <label for="inputAddress">First Name</label>
-          <input type="text" class="form-control">
+          <input type="text" v-model="data.firstname" class="form-control">
         </div>
         <div class="col">
           <label for="inputAddress">Last Name</label>
-          <input type="text" class="form-control">
+          <input type="text" v-model="data.lastname" class="form-control">
         </div>
       </div>
       <div class="row form-group">
         <div class="col">
           <label for="inputAddress">User Name</label>
-          <input type="text" class="form-control">
+          <input type="text" v-model="data.username" class="form-control">
         </div>
         <div class="col">
           <label for="inputAddress">Email</label>
-          <input type="text" class="form-control">
+          <input type="text" v-model="data.email" class="form-control">
         </div>
       </div>
       <div class="row form-group">
@@ -45,32 +45,32 @@
       <div class="row form-group">
         <div class="col">
          <label for="inputAddress">Address</label>
-         <input type="text" class="form-control" id="inputAddress" >
+         <input type="text" v-model="data.address" class="form-control" id="inputAddress" >
        </div>
      </div>
      <div class="row form-group">
        <div class="col">
          <label for="inputAddress">City</label>
-         <input type="text" class="form-control">
+         <input type="text" v-model="data.city" class="form-control">
        </div>
         <div class="col">
           <label for="inputState">State or Region</label>
-          <select id="inputState" class="form-control">
-            <option selected>Choose...</option>
-            <option>...</option>
+          <select id="inputState" v-model="data.state" class="form-control">
+            <option :selected="data.state === null">Choose...</option>
+            <option :key="code" :selected="code == data.state" v-for="(state, code) in states" :value="code">{{state}}</option>
           </select>
         </div>
       </div>
      <div class="row form-group">
        <div class="col">
          <label for="inputAddress">Zip Code</label>
-         <input type="text" class="form-control">
+         <input type="text" v-model="data.postal" class="form-control">
        </div>
         <div class="col">
           <label for="inputState">Country</label>
-          <select id="inputState" class="form-control">
-            <option selected>Choose...</option>
-            <option>...</option>
+          <select id="inputState" v-model="data.country" class="form-control">
+            <option :selected="data.country === null">Choose...</option>
+            <option v-for="country in countries" :key="country.abbreviation" :value="country.abbreviation" :selected="data.country == country.abbreviation">{{country.country}}</option>
           </select>
         </div>
       </div>
@@ -80,10 +80,10 @@
          <input type="text" class="form-control">
        </div>
        <div class="col">
-         <label for="inputState">Default Time Zone</label>
-         <select id="inputState" class="form-control">
-           <option selected>Choose...</option>
-           <option>...</option>
+         <label for="inputTimezone">Default Time Zone</label>
+         <select id="inputTimezone" class="form-control">
+           <option :selected="data.time_zone === null">Choose...</option>
+           <option v-for="(zone, index) in timezones" :key="index" :value="zone.abbr" :selected="data.time_zone == zone.abbr">{{zone.text}}</option>
          </select>
        </div>
       </div>
@@ -91,15 +91,13 @@
        <div class="col-6">
          <label for="inputState">Language</label>
          <select id="inputState" class="form-control">
-           <option selected>Choose...</option>
-           <option>...</option>
+           <option value="en" :selected="data.lang == 'en'">English</option>
          </select>
        </div>
       </div>
       <div class="row form-group float-right mt-3">
         <div class="col">
-          <button type="button" class="btn btn-outline-secondary">Cancel</button>
-          <button type="button" class="btn btn-secondary text-light">Save</button>
+          <button @click="save" type="button" class="btn btn-secondary text-light">Save</button>
         </div>
       </div>
     </form>
@@ -110,9 +108,12 @@
 
 import VueCroppie from 'vue-croppie';
 import modalProfileAvatar from './modal-profile-avatar.vue'
-import avatar from '../../../../js/components/common/avatar.vue'
+import avatar from '../../../components/common/avatar.vue'
+import states from '../../../data/states_hash.json'
+import timezones from 'timezones.json'
+let countries = require('country-json/src/country-by-abbreviation.json')
 
-export default{
+export default {
   components:{
     VueCroppie,
     modalProfileAvatar,
@@ -120,12 +121,38 @@ export default{
   },
   data(){
     return{
+      loaded: false,
       // Points to a url of the image
        image: '',
        uid: window.ProcessMaker.user.uid,
+       data: {},
+       states: states,
+       timezones: timezones,
+       countries: countries
     }
   },
+  mounted() {
+    this.load()
+  },
   methods: {
+    // Loads data from our profile api to fetch data and populate fields
+    load() {
+      ProcessMaker.apiClient.get('admin/profile')
+        .then((response) => {
+          // Copy everything into our data
+          this.data = response.data
+          this.loaded = true;
+        });
+    },
+    save() {
+      ProcessMaker.apiClient.put('admin/profile', this.data)
+        .then((response) => {
+          // @todo Show success alert/modal
+          console.log(response)
+          alert("saved")
+
+        });
+    },
     updateImage (newImage) {
       this.image = newImage;
     },
--- resources/assets/js/management/profile/index.js
+++ resources/assets/js/management/profile/index.js
@@ -4,9 +4,6 @@ import Profile from './components/Profile'
 
 new Vue({
   el: '#profile',
-  data:{
-
-  },
   components:{
      Profile,
   }
