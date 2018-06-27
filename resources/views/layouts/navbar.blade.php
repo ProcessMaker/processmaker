5dda3cfe5674033988be55d51567ec59533a2376
Alan Bollinger <velkymx@gmail.com>
2018-06-27 09:55:54 -0700

    Revamp flash messaging

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,6 +12,40 @@
       <li class="nav-item">
         <a class="nav-link" href="{{ url('admin') }}">Admin</a>
       </li>
+      <li>
+        @if(Session::has('message_error'))
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <i class="fas fa-times-circle"></i> {{Session::get('message_error')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        @endif
+        @if(Session::has('message_info'))
+          <div class="alert alert-info alert-dismissible fade show" role="alert">
+            <i class="fas fa-info-circle"></i> {{Session::get('message_info')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        @endif
+        @if(Session::has('message_success'))
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <i class="fas fa-check"></i> {{Session::get('message_success')}}
+          </div>
+        @endif
+        @if(Session::has('message_warning'))
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <i class="fas fa-exclamation-triangle"></i> {{Session::get('message_warning')}}
+          </div>
+        @endif
+      </li>
     </ul>
 
     <component id="navbar-request-button" v-bind:is="'request-modal'"></component>
@@ -25,7 +59,7 @@
       <li class="dropdown">
         <img class="avatar dropdown-toggle " id="navbarDropdown" src="/img/avatar.png" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-           <a class="dropdown-item drop-header"><img class="avatar-small" src="/img/avatar.png">{{__('John Bunton')}}</a>
+           <a class="dropdown-item drop-header"><img class="avatar-small" src="/img/avatar.png">{{\Auth::user()->firstname}} {{\Auth::user()->lastname}}</a>
            @foreach($dropdown_nav->items as $row)
               <a class="dropdown-item" href="{{ url($row->link->path['route']) }}"><i class="fas {{$row->attr('icon')}} fa-fw fa-lg"></i>{{$row->title}}</a>
            @endforeach
@@ -34,3 +68,7 @@
     </li>
   </ul>
 </nav>
+<style media="screen">
+  .alert { padding: 5px 15px; margin-bottom: 0; margin-top:3px; min-width: 300px}
+  .alert button { margin-top: -7px }
+</style>
