1e9024ad222d8b3ec8e6413f1ea34cab758c272a
Mila Endo <milal.alim@gmail.com>
2018-07-06 14:14:01 -0700

    Update index.blade.php

--- resources/views/request/index.blade.php
+++ resources/views/request/index.blade.php
@@ -11,7 +11,7 @@
 @endsection
 
 @section('sidebar')
-  @include('sidebars.default', ['sidebar'=> $sidebar_request])
+  @include('sidebars.default', ['sidebar' => Menu::get('sidebar_request')])
 @endsection
 
 @section('js')
