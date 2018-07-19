8b8e48e58dcb1c5c046d73ace04dc24b29ca2cff
Taylor Dondich <taylor@dondich.com>
2018-07-18 20:19:03 -0700

    Add support for creating a new user. Not all fields are shown in modal until product decision.

--- resources/assets/js/management/users/index.js
+++ resources/assets/js/management/users/index.js
@@ -1,11 +1,60 @@
 import Vue from 'vue'
 import UsersListing from './components/UsersListing'
 
-// Bootstrap our Designer application
+import {
+  FormInput
+} from '@processmaker/vue-form-elements/src/components'
+
+// Bootstrap our User listing
 new Vue({
   data: {
-    filter: ''
+    filter: '',
+    addUser: {
+      username: '',
+      firstname: '',
+      lastname: '',
+      password: ''
+    },
+    passwordConfirmation: ''
   },
   el: '#users-listing',
-  components: { UsersListing }
+  methods: {
+    openAddUserModal() {
+      this.$refs.addModal.show()
+    },
+    hideAddModal() {
+      this.$refs.addModal.hide()
+      this.resetAddUser()
+    },
+    resetAddUser() {
+      // Reset form data:
+      this.addUser.username = '';
+      this.addUser.firstname = '';
+      this.addUser.lastname = '';
+      this.addUser.password = '';
+      this.passwordConfirmation = '';
+    },
+    submitAdd() {
+      ProcessMaker.apiClient.post('users', this.addUser)
+        .then((response) => {
+          // Add notification to the top
+          this.$refs.addModal.hide()
+          this.resetAddUser()
+          // @todo Use new notifications
+          // Refresh data grid
+          this.$refs.listing.sortOrder = [
+            {
+              field: "created_at",
+              sortField: "created_at",
+              direction: "desc"
+            }
+          ];
+          this.$refs.listing.fetch();
+        });
+    }
+  },
+  components: { 
+    UsersListing ,
+    FormInput
+  }
 })
--- resources/views/management/users/index.blade.php
+++ resources/views/management/users/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <div class="container page-content" id="users-listing">
+    <div class="container page-content" id="users-listing" v-cloak>
     <div class="row">
         <div class="col-sm-12">
             <div class="row">
@@ -14,13 +14,29 @@
                 <input v-model="filter" class="form-control col-sm-3" placeholder="{{__('Search')}}...">
                 </div>
                 <div class="col-md-4 d-flex justify-content-end align-items-center col-sm-12 actions">
-                    <a href="#" class="btn btn-action"><i class="fas fa-plus"></i> {{__('User')}}</a>
+                    <a @click="openAddUserModal" href="#" class="btn btn-action"><i class="fas fa-plus"></i> {{__('User')}}</a>
                 </div>
             </div>
-            <users-listing :filter="filter"></users-listing>
+            <users-listing ref="listing" :filter="filter"></users-listing>
         </div>
     </div>
-    </div>
+    <b-modal ref="addModal" size="md" centered title="Create New User" v-cloak>
+        <form-input v-model="addUser.username" label="Username" helper="Username must be distinct"></form-input>
+        <form-input v-model="addUser.firstname" label="First Name"></form-input>
+        <form-input v-model="addUser.lastname" label="Last Name"></form-input>
+        <form-input type="password" v-model="addUser.password" label="Password"></form-input>
+        <form-input type="password" v-model="passwordConfirmation" label="Confirm Password"></form-input>
+
+        <template slot="modal-footer">
+        <b-button @click="hideAddModal" class="btn-outline-secondary btn-md">
+            Cancel
+        </b-button>
+        <b-button @click="submitAdd" class="btn-secondary text-light btn-md">
+            Save
+        </b-button>
+        </template>
+    </b-modal>
+</div>
 @endsection
 
 @section('js')
