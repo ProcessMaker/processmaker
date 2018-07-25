da064d9d290201fc2dce49f7d161e1638c3e5a5e
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-07-25 16:20:54 -0400

    Change form with processmaker vue form builder

--- resources/assets/js/processes/components/modal/modal-process-add.vue
+++ resources/assets/js/processes/components/modal/modal-process-add.vue
@@ -1,24 +1,11 @@
 <template>
-    <b-modal id="createProcess" ref="modal" size="md" @hidden="onHidden" title="Create New Process">
-        <form>
-            <div class="form-group">
-                <label for="title">{{titleLabel}}</label>
-                <input id="title" type="text" class="form-control" v-model="title">
-            </div>
-
-            <div class="form-group">
-                <label for="description">{{descriptionLabel}}</label>
-                <textarea id="description" class="form-control" v-model="description" rows="3"></textarea>
-            </div>
-
-            <div class="form-group">
-                <label for="category">{{categoryLabel}}</label>
-                <select class="form-control" id="category" v-model="categorySelect">
-                    <option v-for="select in categorySelectOptions" :value="select.cat_uid">{{select.cat_name}}</option>
-                </select>
-            </div>
-
-        </form>
+    <b-modal id="createProcess" ref="modal" size="md" centered @hidden="onHidden" title="Create New Process" v-cloak>
+        <b-alert dismissable :show="validationError != null" variant="danger">@{{validationError}}</b-alert>
+        <form-input :error="errors.name" v-model="name" label="Title" helper="Process Name must be distinct"
+                    required="required"></form-input>
+        <form-text-area :error="errors.description" :rows="3" v-model="description" label="Description"></form-text-area>
+        <form-select :error="errors.process_category_id" label="Category" name="category" v-model="categorySelect"
+                     :options="categorySelectOptions"></form-select>
 
         <template slot="modal-footer">
             <b-button @click="onCancel" class="btn-outline-secondary btn-md">
@@ -33,16 +20,24 @@
 </template>
 
 <script>
+    import FormTextArea from "@processmaker/vue-form-elements/src/components/FormTextArea";
+    import FormSelect from "@processmaker/vue-form-elements/src/components/FormSelect";
+    import FormInput from "@processmaker/vue-form-elements/src/components/FormInput";
+
     export default {
+        components: {FormInput, FormSelect, FormTextArea},
         data() {
             return {
-                'titleLabel': 'Title',
-                'categoryLabel': 'Category',
-                'descriptionLabel': 'Description',
-                'title': '',
+                'name': '',
                 'description': '',
                 'categorySelect': null,
-                'categorySelectOptions': []
+                'categorySelectOptions': [],
+                'validationError': null,
+                'errors': {
+                    'name': null,
+                    'description': null,
+                    'process_category_id': null,
+                }
             }
         },
         methods: {
@@ -52,39 +47,55 @@
             onCancel() {
                 this.$refs.modal.hide()
             },
+            reset() {
+                this.name = '';
+                this.description = '';
+                this.categorySelect = null;
+                this.validationError = null;
+                this.errors.name = null;
+                this.errors.description = null;
+                this.errors.process_category_id = null;
+            },
             onShow() {
+                //load Process Categories
                 window.ProcessMaker.apiClient.get('categories')
                     .then((response) => {
-                        response.data.unshift({cat_uid: null, cat_name: 'None'});
-                        this.categorySelectOptions = response.data;
-                        this.title = '';
-                        this.description = '';
-                        this.categorySelect = null;
-                        //display modal create process
+
+                        let options = [
+                            {
+                                value: null, content: 'None'
+                            }
+                        ];
+                        response.data.map(function (category) {
+                            options.push({
+                                value: category.cat_uid,
+                                content: category.cat_name
+                            })
+                        });
+                        this.categorySelectOptions = options;
+
+                        this.reset();
                         this.$refs.modal.show()
                     })
                     .catch((error) => {
-                        //define how display errors
+                        this.validationError = error;
+                        if (error.response.status === 422) {
+                            this.validationError = error.response.data.message;
+                        }
                     })
             },
             onSave() {
-                if (this.cancelToken) {
-                    this.cancelToken();
-                    this.cancelToken = null;
-                }
-                const CancelToken = ProcessMaker.apiClient.CancelToken;
-
-                // Load from our api client
                 ProcessMaker.apiClient
                     .post(
                         'processes/create',
                         {
-                            name: this.title,
+                            name: this.name,
                             description: this.description,
                             category_uid: this.categorySelect
                         }
                     )
                     .then(response => {
+                        ProcessMaker.alert('New Process Successfully Created', 'success');
                         this.$refs.modal.hide();
                         if (response.data && response.data.uid) {
                             //Change way to open the designer
@@ -93,6 +104,15 @@
                     })
                     .catch(error => {
                         //define how display errors
+                        if (error.response.status === 422) {
+                            // Validation error
+                            this.validationError = error.response.data.message;
+                            let fields = Object.keys(error.response.data.errors);
+                            for (var field of fields) {
+                                this.errors[field] = error.response.data.errors[field][0];
+
+                            }
+                        }
                     });
             }
         },
