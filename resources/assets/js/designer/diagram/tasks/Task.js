2a4bb237546e051af7edc2234e1966f77dedf03d
hjonathan <jonathan.quispe@processmaker.com>
2018-07-30 12:56:40 -0400

    update task file for set id in backbone view

--- resources/assets/js/designer/diagram/tasks/Task.js
+++ resources/assets/js/designer/diagram/tasks/Task.js
@@ -24,7 +24,7 @@ export default class extends Shape {
      * Render the Task Based in options config
      */
     render() {
-        this.shape = new JointElements.Task();
+        this.shape = new JointElements.Task({id: this.options.id});
         this.shape.position(this.options.bounds.x, this.options.bounds.y)
         this.shape.resize(this.options.bounds.width, this.options.bounds.height)
         this.shape.addTo(this.graph)
