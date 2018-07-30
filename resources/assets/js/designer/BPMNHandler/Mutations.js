b56db7f051e55913b02f4fee3036d1e47d4d9dd2
hjonathan <jonathan.quispe@processmaker.com>
2018-07-30 08:54:08 -0400

    update mutations in BPMN Handle, update functions to model

--- resources/assets/js/designer/BPMNHandler/Mutations.js
+++ resources/assets/js/designer/BPMNHandler/Mutations.js
@@ -1,32 +1,77 @@
 import actions from "../actions"
-
-
+import _ from "lodash"
+/**
+ * Function to update participant object
+ * @param data
+ * @param elements
+ */
 function participantUpdate(data, elements) {
     if (elements[data.id] && data.bounds) {
         elements[data.id].diagram.elements[0].attributes = data.bounds
     }
 }
 
+/**
+ * Function to update shape
+ * @param data
+ * @param elements
+ */
 function shapeUpdate(data, elements) {
     if (elements[data.id] && data.bounds) {
         elements[data.id].diagram.elements[0].attributes = data.bounds
     }
 }
 
-function participantCreate(data, elements) {
-    let participant = {
+/**
+ * Function to create participant
+ * @param data
+ * @param elements
+ * @param arrayElements
+ * @param processes
+ * @param collaborations
+ */
+function participantCreate(data, elements, arrayElements, processes, collaborations) {
+    let process = {
         "type": "element",
         "name": "bpmn:participant",
-        "attributes": {"id": "Participant_18loy4z", "name": "Approval Link", "processRef": "approval_link"},
+        "attributes": {"id": data.id, "name": "Approval Link", "processRef": "approval_link"},
         "elements": []
     }
-}
 
-function shapeCreate(data, elements, arrayElements, processes) {
     let diagram = {
         "type": "element",
         "name": "bpmndi:BPMNShape",
-        "attributes": {"id": "_BPMNShape_" + data.id, "bpmnElement": data.id},
+        "attributes": {"id": data.id + "_di", "bpmnElement": data.id},
+        "elements": [{
+            "type": "element",
+            "name": "dc:Bounds",
+            "attributes": data.bounds,
+            "elements": []
+        }]
+    }
+
+    elements[data.id] = {
+        diagram,
+        process
+    }
+    arrayElements.push(diagram)
+    collaborations.push(process)
+}
+
+/**
+ * Function to create shape
+ * @param data
+ * @param elements
+ * @param arrayElements
+ * @param processes
+ */
+function shapeCreate(data, elements, arrayElements, processes) {
+    let eventDefinition = data.eventDefinition ? createEventDefinition(data.eventDefinition) : null
+    let arrEvent = eventDefinition ? [eventDefinition] : []
+    let diagram = {
+        "type": "element",
+        "name": "bpmndi:BPMNShape",
+        "attributes": {"id": data.id + "_di", "bpmnElement": data.id},
         "elements": [{
             "type": "element",
             "name": "dc:Bounds",
@@ -39,6 +84,52 @@ function shapeCreate(data, elements, arrayElements, processes) {
         "type": "element",
         "name": "bpmn:" + data.type,
         "attributes": {"id": data.id},
+        "elements": arrEvent
+    }
+
+    elements[data.id] = {
+        diagram,
+        process
+    }
+    arrayElements.push(diagram)
+    processes[0].elements.push(process)
+}
+
+/**
+ * Function to create object definition
+ * @param def
+ * @returns {{elements: Array, name: string, type: string}}
+ */
+function createEventDefinition(def) {
+    let event = {
+        elements: [],
+        name: "bpmn:" + def,
+        type: "element"
+    }
+    return event
+}
+
+/**
+ * Function to create flow in model
+ * @param data
+ * @param elements
+ * @param arrayElements
+ * @param processes
+ */
+function flowCreate(data, elements, arrayElements, processes) {
+    let bounds = createBounds(data.bounds)
+
+    let diagram = {
+        "type": "element",
+        "name": "bpmndi:BPMNEdge",
+        "attributes": {"id": data.id + "_di", "bpmnElement": data.id},
+        "elements": bounds
+    }
+
+    let process = {
+        "type": "element",
+        "name": "bpmn:sequenceFlow",
+        "attributes": {"id": data.id, "sourceRef": data.sourceRef, "targetRef": data.targetRef},
         "elements": []
     }
 
@@ -50,31 +141,51 @@ function shapeCreate(data, elements, arrayElements, processes) {
     processes[0].elements.push(process)
 }
 
+/**
+ * Function to update flow in model
+ * @param data
+ * @param elements
+ */
+function flowUpdate(data, elements) {
+    if (elements[data.id]) {
+        elements[data.id].process.attributes = {
+            "id": data.id,
+            "sourceRef": data.sourceRef,
+            "targetRef": data.targetRef
+        }
+
+        elements[data.id].diagram.attributes = {
+            "id": data.id + "_di",
+            "bpmnElement": data.id
+        }
+
+        elements[data.id].diagram.elements = createBounds(data.bounds)
+    }
+}
+
+/**
+ * Function to return object bounds
+ * @param data
+ * @returns {Array}
+ */
+function createBounds(data) {
+    let bounds = []
+    _.each(data, (value) => {
+        bounds.push({
+            "type": "element",
+            "name": "di:waypoint",
+            "attributes": value,
+            "elements": []
+        })
+    })
+    return bounds
+}
+
 export default {
     [actions.bpmn.shape.update]: shapeUpdate,
     [actions.bpmn.participant.update]: participantUpdate,
+    [actions.bpmn.flow.update]: flowUpdate,
     [actions.bpmn.shape.create]: shapeCreate,
-    [actions.bpmn.participant.create]: participantCreate,
-
+    [actions.bpmn.flow.create]: flowCreate,
+    [actions.bpmn.participant.create]: participantCreate
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
