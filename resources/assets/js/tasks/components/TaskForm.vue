1264059f1ae251f3bb84ad181bbd7b87c68dea18
David Callizaya <davidcallizaya@gmail.com>
2018-07-31 13:48:33 -0400

    Create view to load a task/form

---
+++ resources/assets/js/tasks/components/TaskForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        TODO<!-- TODO: component to load the form -->
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: [
+            'processUid',
+            'instanceUid',
+            'tokenUid',
+            'formUid',
+
+            'data',
+        ],
+        data() {
+            return {
+            };
+        },
+        mounted() {
+        },
+        methods: {
+            submit() {
+                ProcessMaker.apiClient.post(
+                    'processes/' + this.processUid +
+                    '/instances/' + this.instanceUid +
+                    '/tokens/' + this.tokenUid +
+                    '/complete', 
+                    this.data
+                )
+            }
+        }
+
+    }
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ resources/assets/js/tasks/show.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import TaskForm from './components/TaskForm'
+
+new Vue({
+    el: '#task',
+    data: {
+    },
+    components: {
+        TaskForm
+    }
+});
