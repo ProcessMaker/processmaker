111e7b8e468f5de994ed9307bf3700970ebee014
David Callizaya <davidcallizaya@gmail.com>
2018-08-01 10:54:05 -0400

    Show notifications during task execution.

--- resources/assets/js/tasks/show.js
+++ resources/assets/js/tasks/show.js
@@ -7,5 +7,13 @@ new Vue({
     },
     components: {
         TaskForm
-    }
+    },
+    mounted() {
+        // Listen for notifications
+        let userId = document.head.querySelector('meta[name="user-id"]').content;
+        Echo.private(`ProcessMaker.Model.User.${userId}`)
+            .notification((token) => {
+                ProcessMaker.pushNotification(token);
+            });
+    },
 });
