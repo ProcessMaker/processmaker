40d55ae56c5cbcaaf77cbd74c8575936525407d8
David Callizaya <davidcallizaya@gmail.com>
2018-08-01 10:15:56 -0400

    Initial forms for the test processes.

--- database/seeds/ProcessSeeder.php
+++ database/seeds/ProcessSeeder.php
@@ -7,38 +7,6 @@ use ProcessMaker\Model\Form;
 class ProcessSeeder extends Seeder
 {
 
-    private $approvalFormUid = '200f95eb-76d8-459d-b56a-ea605bea4e3f';
-    private $hrFormUid = '300f95eb-76d8-459d-b56a-ea605bea4e3f';
-    private $requestFormUid = '100f95eb-76d8-459d-b56a-ea605bea4e3f';
-    private $requestForm = [
-        [
-            "type" => "FormInput",
-            "field" => "label",
-            "config" => [
-                "label" => "Text Label",
-                "helper" => "The text to display",
-            ]
-        ],
-        [
-            "type" => "FormSelect",
-            "field" => "fontWeight",
-            "config" => [
-                "label" => "Font Weight",
-                "helper" => "The weight of the text",
-                "options" => [
-                    [
-                        "value" => 'normal',
-                        "content" => 'Normal'
-                    ],
-                    [
-                        "value" => 'bold',
-                        "content" => 'Bold'
-                    ]
-                ]
-            ]
-        ],
-    ];
-
     /**
      * Run the database seeds.
      *
@@ -67,29 +35,41 @@ class ProcessSeeder extends Seeder
                 }
             }
             $process->save();
-            
+
             $definitions = $process->getDefinitions();
 
             //Add forms to the process
             factory(Form::class)->create([
                 'uid' => $definitions->getActivity('request')->getProperty('formRef'),
-                'content' => $this->requestForm,
+                'content' => $this->loadForm('request'),
                 'process_id' => $process->id,
             ]);
 
             factory(Form::class)->create([
                 'uid' => $definitions->getActivity('approve')->getProperty('formRef'),
-                'content' => $this->requestForm,
+                'content' => $this->loadForm('approve'),
                 'process_id' => $process->id,
             ]);
 
             $form = factory(Form::class)->create([
                 'uid' => $definitions->getActivity('validate')->getProperty('formRef'),
-                'content' => $this->requestForm,
+                'content' => $this->loadForm('validate'),
                 'process_id' => $process->id,
             ]);
 
             echo 'Process created: ', $process->uid, "\n";
         }
+    }
+
+    /**
+     * Load the JSON of a form.
+     *
+     * @param string $name
+     *
+     * @return object
+     */
+    private function loadForm($name)
+    {
+        return json_decode(file_get_contents(database_path('processes/forms/' . $name)));
     }
 }
