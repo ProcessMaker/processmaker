4da03f910eea9e1fde54d481793ac9403ce9309e
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-08-02 12:48:19 -0400

    Add processUid in modal list OutputDocument

--- resources/assets/js/designer/components/modalList/modal-output-documents.vue
+++ resources/assets/js/designer/components/modalList/modal-output-documents.vue
@@ -1,128 +1,124 @@
 <template>
-  <b-modal class="output-docs" ref="modal" size="lg" @hidden="onHidden" title="Output Documents" hide-footer>
-    <div class="form-group">
-      <div class="d-flex justify-content-between">
-        <filter-bar></filter-bar>
-        <button type="submit" class="btn btn-secondary"><i class="fas fa-plus fa-md"></i> Create</button>
-      </div>
-      <div class="data-table">
-        <vuetable :dataManager="dataManager" :sortOrder="sortOrder" :css="css" :api-mode="false" :fields="fields" :data="data" data-path="data" pagination-path="meta">
-          <template slot="actions" slot-scope="props"> 
-            <div class="actions">
-              <i class="fas fa-ellipsis-h"></i>
-              <div class="popout">
-                <b-btn variant="action" @click="onAction('edit-item', props.rowData, props.rowIndex)" v-b-tooltip.hover title="Edit"><i class="fas fa-edit"></i></b-btn>
-                <b-btn variant="action" @click="onAction('remove-item', props.rowData, props.rowIndex)" v-b-tooltip.hover title="Remove"><i class="fas fa-trash-alt"></i></b-btn>
-              </div>
+    <b-modal class="output-docs" ref="modal" size="lg" @hidden="onHidden" title="Output Documents" hide-footer>
+        <div class="form-group">
+            <div class="d-flex justify-content-between">
+                <filter-bar></filter-bar>
+                <button type="submit" class="btn btn-secondary"><i class="fas fa-plus fa-md"></i> Create</button>
             </div>
-          </template>  
-        </vuetable> 
-      </div>
-    </div>
-  </b-modal>
+            <div class="data-table">
+                <vuetable :dataManager="dataManager" :sortOrder="sortOrder" :css="css" :api-mode="false"
+                          :fields="fields" :data="data" data-path="data" pagination-path="meta">
+                    <template slot="actions" slot-scope="props">
+                        <div class="actions">
+                            <i class="fas fa-ellipsis-h"></i>
+                            <div class="popout">
+                                <b-btn variant="action" @click="onAction('edit-item', props.rowData, props.rowIndex)"
+                                       v-b-tooltip.hover title="Edit"><i class="fas fa-edit"></i></b-btn>
+                                <b-btn variant="action" @click="onAction('remove-item', props.rowData, props.rowIndex)"
+                                       v-b-tooltip.hover title="Remove"><i class="fas fa-trash-alt"></i></b-btn>
+                            </div>
+                        </div>
+                    </template>
+                </vuetable>
+            </div>
+        </div>
+    </b-modal>
 </template>
 
 <script>
-import FilterBar from "../../../components/FilterBar";
-Vue.component('filter-bar', FilterBar);
-import Vuetable from "vuetable-2/src/components/Vuetable";
-import datatableMixin from "../../../components/common/mixins/datatable";
+    import FilterBar from "../../../components/FilterBar";
 
-export default {
-    mixins: [datatableMixin],
-    props: ["filter"],
-  data() {
-    return {
-      // form models here
-      'messageFieldName': "Name",
-          items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ],
-    orderBy: "code",
+    Vue.component('filter-bar', FilterBar);
+    import datatableMixin from "../../../components/common/mixins/datatable";
 
-      sortOrder: [
-        {
-          field: "ID",
-          sortField: "id",
-          direction: "asc"
-        }
-      ],
-      fields: [
-        {
-          title: "Date Uploaded",
-          name: "created_at",
-          sortField: "created_at"
-        },
-        {
-          title: "Title",
-          name: "title",
-          sortField: "title"
-        },
-        {
-          title: "Type",
-          name: "type",
-          sortField: "type"
-        },
-        {
-          name: "__slot:actions",
-          title: ""
-        }
-      ],
-      data: [
+    export default {
+        mixins: [datatableMixin],
+        props: ['filter', 'processUid'],
+        data() {
+            return {
+                // form models here
+                'messageFieldName': "Name",
+                items: [
+                    {message: 'Foo'},
+                    {message: 'Bar'}
+                ],
+                orderBy: "code",
 
-      ]
+                sortOrder: [
+                    {
+                        field: "ID",
+                        sortField: "id",
+                        direction: "asc"
+                    }
+                ],
+                fields: [
+                    {
+                        title: "Date Uploaded",
+                        name: "created_at",
+                        sortField: "created_at"
+                    },
+                    {
+                        title: "Title",
+                        name: "title",
+                        sortField: "title"
+                    },
+                    {
+                        title: "Type",
+                        name: "type",
+                        sortField: "type"
+                    },
+                    {
+                        name: "__slot:actions",
+                        title: ""
+                    }
+                ],
+                data: []
+            };
+        },
+        methods: {
+            onHidden() {
+                this.$emit('hidden')
+            },
+            onCancel() {
+                this.$refs.modal.hide()
+            },
+            fetch() {
+                this.loading = true;
+                if (this.cancelToken) {
+                    this.cancelToken();
+                    this.cancelToken = null;
+                }
+                const CancelToken = ProcessMaker.apiClient.CancelToken;
+                // Load from our api client
+                ProcessMaker.apiClient
+                    .get('process/' + this.processUid + '/output-documents',
+                        "roles?page=" +
+                        this.page +
+                        "&per_page=" +
+                        this.perPage +
+                        "&filter=" +
+                        this.filter +
+                        "&order_by=" +
+                        this.orderBy +
+                        "&order_direction=" +
+                        this.orderDirection,
+                        {
+                            cancelToken: new CancelToken(c => {
+                                this.cancelToken = c;
+                            })
+                        }
+                    )
+                    .then(response => {
+                        this.data = this.transform(response.data);
+                    });
+            }
+        },
+        mounted() {
+            // Show our modal as soon as we're created
+            this.$refs.modal.show();
+        },
+
     };
-  },
-  methods:{
-    onHidden() {
-      this.$emit('hidden')
-    },
-    onCancel() {
-      this.$refs.modal.hide()
-    },
-        fetch() {
-      this.loading = true;
-      if (this.cancelToken) {
-        this.cancelToken();
-        this.cancelToken = null;
-      }
-      const CancelToken = ProcessMaker.apiClient.CancelToken;
-      // Load from our api client
-      ProcessMaker.apiClient
-        .get('process/e4e3a48e-fec7-4ce0-8f68-1ae68e7e316c/output-documents',
-          "roles?page=" +
-            this.page +
-            "&per_page=" +
-            this.perPage +
-            "&filter=" +
-            this.filter +
-            "&order_by=" +
-            this.orderBy +
-            "&order_direction=" +
-            this.orderDirection,
-          {
-            cancelToken: new CancelToken(c => {
-              this.cancelToken = c;
-            })
-          }
-        )
-        .then(response => {
-          console.log(response.data)
-          this.data = this.transform(response.data);
-          // this.loading = false;
-        })
-        .catch(error => {
-          // Undefined behavior currently, show modal?
-        });
-    }
-  },
-  mounted() {
-    // Show our modal as soon as we're created
-    this.$refs.modal.show();
-  },
-
-};
 
 
 </script>
