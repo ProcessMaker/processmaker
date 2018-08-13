9e08d818828864de7e6e831195c49072ae9ccf84
David Callizaya <davidcallizaya@gmail.com>
2018-08-13 11:49:23 -0400

    Add a script task to the leave absence request.

--- database/seeds/ProcessSeeder.php
+++ database/seeds/ProcessSeeder.php
@@ -3,9 +3,17 @@
 use Illuminate\Database\Seeder;
 use ProcessMaker\Model\Process;
 use ProcessMaker\Model\Form;
+use ProcessMaker\Model\Script;
+use ProcessMaker\Providers\WorkflowServiceProvider;
 
 class ProcessSeeder extends Seeder
 {
+
+    const mimeTypes = [
+        'javascript' => 'application/javascript',
+        'lua' => 'application/x-lua',
+        'php' => 'application/x-php',
+    ];
 
     /**
      * Run the database seeds.
@@ -34,6 +42,28 @@ class ProcessSeeder extends Seeder
                     $process->name = $collaborationDefinition->getName();
                 }
             }
+            $process->save();
+
+            //Create scripts from the BPMN process definition
+            $scriptTasks = $process->getDefinitions()->getElementsByTagName('scriptTask');
+            foreach ($scriptTasks as $scriptTaskNode) {
+                $scriptTask = $scriptTaskNode->getBpmnElementInstance();
+                //Create a row in the Scripts table
+                $script = factory(Script::class)->create([
+                    'title' => $scriptTask->getName('name') . ' Script',
+                    'code' => $scriptTaskNode->getElementsByTagName('script')->item(0)->nodeValue,
+                    'language' => $this->languageOfMimeType($scriptTask->getScriptFormat()),
+                    'process_id' => $process->id,
+                ]);
+                $scriptTaskNode->setAttributeNS(
+                    WorkflowServiceProvider::PROCESS_MAKER_NS, 'scriptRef', $script->uid
+                );
+                $scriptTaskNode->setAttributeNS(
+                    WorkflowServiceProvider::PROCESS_MAKER_NS, 'scriptConfiguration', '{}'
+                );
+            }
+            //Update the script references in the BPMN of the process
+            $process->bpmn = $process->getDefinitions()->saveXML();
             $process->save();
 
             $definitions = $process->getDefinitions();
@@ -78,5 +108,10 @@ class ProcessSeeder extends Seeder
     private function loadForm($name)
     {
         return json_decode(file_get_contents(database_path('processes/forms/' . $name)));
+    }
+
+    private function languageOfMimeType($mime)
+    {
+        return in_array($mime, self::mimeTypes) ? array_search($mime, self::mimeTypes) : '';
     }
 }
