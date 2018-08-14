5de3bbe86469926802ce45f15841a2af6963081b
David Callizaya <davidcallizaya@gmail.com>
2018-08-14 10:27:01 -0400

    Assignment of a script to a Script Task.

--- resources/assets/js/designer/components/modals/modal-script-task.vue
+++ resources/assets/js/designer/components/modals/modal-script-task.vue
@@ -1,164 +1,184 @@
 <template>
-  <b-modal ref="modal" size="lg" @hidden="onHidden" centered v-bind:title="popupTitle">
-    <form>
-      <div class="form-group">
-        <label>{{titleLabel}}</label>
-        <input type="text" class="form-control" v-model="title">
+  <b-modal class="scripts-list" ref="modal" size="lg" @hidden="onHidden" title="Script Task" hide-footer>
+    <form-input v-model="name" label="Name" helper="Name of the script task"></form-input>
+    <form-input :readonly="true" :disabled="true" v-model="assignedScript.title" label="Assigned script" helper="Assigned script"></form-input>
+    <div class="form-group">
+      <div class="d-flex justify-content-between">
+        <input v-model="filter" class="form-control  col-sm-3" placeholder="Search..." @keyup="fetch" >
+        <button type="submit" class="btn btn-secondary"><i class="fas fa-plus fa-md"></i> Create</button>
       </div>
-
-      <div class="form-group">
-        <label>{{descriptionLabel}}</label>
-        <textarea class="form-control" v-model="description"></textarea>
+      <div class="data-table">
+        <vuetable :dataManager="dataManager" :sortOrder="sortOrder" :css="css" :api-mode="false"
+            @vuetable:pagination-data="onPaginationData" :fields="fields" :data="data" data-path="data"
+            pagination-path="meta">
+          <template slot="actions" slot-scope="props">
+            <div class="actions">
+              <i class="fas fa-ellipsis-h"></i>
+              <div class="popout">
+                <b-btn variant="action" @click="onAssign(props.rowData, props.rowIndex)"
+                       v-b-tooltip.hover title="Assign"><i class="fas fa-check-circle"></i></b-btn>
+              </div>
+            </div>
+          </template>
+        </vuetable>
+        <pagination single="Script" plural="Scripts" :perPageSelectEnabled="true" @changePerPage="changePerPage"
+                    @vuetable-pagination:change-page="onPageChange" ref="pagination"></pagination>
       </div>
-
-      <div class="form-group">
-        <div class="code-variable">
-          <div>
-            <div><label>{{codeLabel}}</label></div>
-            <div><label>{{variableLabel}}</label></div>
-          </div>
-          <div>
-            <div class="code-variable-editor">
-              <monaco-editor
-                  height="300"
-                  language="php"
-                  :code="code"
-                  :editorOptions="editorOptions"
-                  @mounted="onMounted"
-                  @codeChange="onCodeChange"
-                  >
-              </monaco-editor>
-            </div>
-            <div>
-              <select size="10">
-                <option v-for="variable in variables" draggable="true" @dragstart="dragVariable(variable, $event)">{{variable.attributes.name}}</option>
-              </select>
-            </div>
-          </div>
-          <div>
-            <div><button type="button">{{testButtonLabel}}</button></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="form-group">
-        <div class="code-variable">
-          <div>
-            <div><label for="variableType">{{inputVariablesJsonLabel}}</label></div>
-            <div></div>
-            <div><label for="variableType">{{outputVariablesJsonLabel}}</label></div>
-          </div>
-          <div>
-            <div class="code-variable-editor">
-              <vue-json-pretty
-                  :data="inputVariablesJson"></vue-json-pretty>
-            </div>
-            <div class="code-variable-play"><i class="fas fa-play-circle"></i></div>
-            <div>
-              <vue-json-pretty
-                  :data="outputVariablesJson"></vue-json-pretty>
-            </div>
-          </div>
-        </div>
-      </div>
-    </form>
-
-    <template slot="modal-footer">
-      <b-button @click="onCancel" class="btn-outline-secondary btn-md">
-        CANCEL
-      </b-button>
-      <b-button @click="onSave" class="btn-secondary text-light btn-md">
-        SAVE
-      </b-button>
-    </template>
-
+    </div>
   </b-modal>
 </template>
 
 <script>
-  import MonacoEditor from 'vue-monaco-editor'
-          import VueJsonPretty from 'vue-json-pretty';
+  import dataTableMixin from "../../../components/common/mixins/datatable";
+  import Pagination from "../../../components/common/Pagination";
+  import MonacoEditor from "vue-monaco";
+  import actions from '../../actions';
+  import EventBus from '../../lib/event-bus';
+  import {FormInput} from '@processmaker/vue-form-elements/src/components';
 
   export default {
       components: {
           MonacoEditor,
-          VueJsonPretty
+          Pagination,
+          FormInput
+      },
+      mixins: [dataTableMixin],
+      props: {
+          'processUid': String,
+          'selectedElement': Object
       },
       data() {
           return {
-              // form models here
-              'popupTitle': 'Create Custom Script',
-              'titleLabel': 'Title',
-              'descriptionLabel': 'Description',
-              'codeLabel': 'Code',
-              'variableLabel': 'Variables',
-              'testButtonLabel': 'TEST CODE',
-              'inputVariablesJsonLabel': 'Input Variables (JSON)',
-              'outputVariablesJsonLabel': 'Output Variables (JSON)',
+              items: [],
+              filter: '',
+              orderBy: "title",
 
-              'editorOptions': {},
-
-              'title': '',
-              'description': '',
-              'code': '',
-              'variables': [],
-              'inputVariablesJson': {},
-              'outputVariablesJson': {},
+              sortOrder: [
+                  {
+                      field: "title",
+                      sortField: "title",
+                      direction: "asc"
+                  }
+              ],
+              fields: [
+                  {
+                      title: "Title",
+                      name: "title",
+                      sortField: "title"
+                  },
+                  {
+                      title: "Description",
+                      name: "description",
+                      sortField: "description"
+                  },
+                  {
+                      title: "Type",
+                      name: "type",
+                      sortField: "type"
+                  },
+                  {
+                      name: "__slot:actions",
+                      title: ""
+                  }
+              ],
+              data: [],
+              name: this.selectedElement.attributes.name,
+              configuration: this.selectedElement.attributes.configuration,
+              assignedScriptName: '',
+              assignedScript: {
+                  title: '...'
+              }
+          };
+      },
+      watch: {
+          name(name) {
+              this.updateElement({
+                  name: name,
+              });
+          },
+          assignedScript(data) {
+              this.updateElement({
+                  script: data.code,
+                  scriptFormat: this.getMimeType(data.language),
+                  scriptRef: data.uid,
+                  scriptConfiguration: this.configuration,
+              });
           }
       },
       methods: {
+          getMimeType(language) {
+              let mimeTypes = {
+                  'php': "application/x-php",
+                  'javascript': "application/javascript",
+                  'lua': "application/x-lua"
+              };
+              return mimeTypes[language] === undefined ? "application/x-" + language
+                      : mimeTypes[language];
+          },
           onHidden() {
               this.$emit('hidden');
           },
           onCancel() {
               this.$refs.modal.hide();
           },
-          onSave() {
-              this.$refs.modal.hide();
+          onAssign(data, index) {
+              this.assignedScript = data;
           },
-          dragVariable(variable, event) {
-              event.dataTransfer.effectAllowed = "copy";
-              event.dataTransfer.dropEffect = "copy";
-              event.dataTransfer.setData("text", variable.attributes.name);
-              return false;
+          /**
+           * Update the script task element.
+           *
+           * @param Object data
+           */
+          updateElement(data) {
+              data.id = this.selectedElement.id;
+              let action = actions.bpmn.task.update(data);
+              EventBus.$emit(action.type, action.payload);
           },
-          onMounted(editor) {
-              this.editor = editor;
-          },
-          onCodeChange(editor) {
-              this.code = this.editor.getValue();
+          fetch() {
+              this.loading = true;
+              const CancelToken = ProcessMaker.apiClient.CancelToken;
+              //Get assigned script
+              let scriptRef = this.selectedElement.attributes['pm:scriptRef'];
+              if (scriptRef) {
+                  ProcessMaker.apiClient
+                          .get('process/' + this.processUid + '/script/' + scriptRef)
+                          .then(response => {
+                              this.assignedScript = response.data;
+                              console.log(response.data);
+                          });
+              } else {
+                  this.assignedScript = {
+                      title: '(unassigned)'
+                  };
+              }
+              //Get the list of scripts
+              ProcessMaker.apiClient
+                      .get('process/' +
+                              this.processUid +
+                              '/scripts?page=' +
+                              this.page +
+                              '&per_page=' +
+                              this.perPage +
+                              '&filter=' +
+                              this.filter +
+                              '&order_by=' +
+                              this.orderBy +
+                              '&order_direction=' +
+                              this.orderDirection,
+                              {
+                                  cancelToken: new CancelToken(c => {
+                                      this.cancelToken = c;
+                                  })
+                              }
+                      )
+                      .then(response => {
+                          this.data = this.transform(response.data);
+                      });
           }
       },
       mounted() {
-          // Show our modal as soon as we're created
           this.$refs.modal.show();
       }
   };
-</script>
-<style lang="scss" scoped>
-  .form-check-input{
-      margin-top: 8px;
-  }
-  .bottom-label{
-      font-size: 10px;
-  }
-  .bottom-label-form{
-      margin-bottom: -6px;
-  }
-  .code-variable {
-      display: table;
-      min-height: 12em;
-  }
-  .code-variable > div {
-      display: table-row;
-  }
-  .code-variable > div > div {
-      display: table-cell;
-  }
-  .code-variable-editor {
-      min-width: 40em;
-  }
-  .code-variable-play {
 
-  }
-</style>
+</script>
