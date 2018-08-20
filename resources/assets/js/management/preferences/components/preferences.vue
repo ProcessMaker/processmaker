b81eb24977f9a0d104fa198f0bf0932e4452a24d
Mila Endo <milal.alim@gmail.com>
2018-08-20 10:59:06 -0700

    #536 bringing in vue-form-controls and setting up data

--- resources/assets/js/management/preferences/components/preferences.vue
+++ resources/assets/js/management/preferences/components/preferences.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    hello
+  <div class="container">
+    <h1>Preferences</h1>
+    <form>
+
+    </form>
   </div>
 </template>
+
+<script>
+import FormInput from "@processmaker/vue-form-elements/src/components/FormInput";
+import FormSelect from "@processmaker/vue-form-elements/src/components/FormSelect";
+import FormCheckbox from "@processmaker/vue-form-elements/src/components/FormCheckbox";
+
+export default {
+  components: { FormSelect, FormInput, FormCheckbox },
+  data() {
+    return {
+      timezone: {
+        title: "Time Zone",
+        helper: "Default Time Zone"
+      },
+      format: {
+        title: "Format",
+        nameHelper: "User Name Display Format",
+        globalHelper: "Global Date Format"
+      },
+      Languages: {
+        title: "Languages",
+        helper: "When the domain is loaded the default app language is "
+      }
+    };
+  }
+};
+</script>
