b06d62b5997524f28f8c5069cf23fd6dc7cef501
Leah <leah.miller@processmaker.com>
2018-08-21 12:59:54 -0700

    form inputs, close to mockup, needs onclick event for color picker

--- resources/views/management/admin/index.blade.php
+++ resources/views/management/admin/index.blade.php
@@ -5,15 +5,38 @@
 @endsection
 
 @section('content')
-<div id="uicustomize">
-    <h1 class="page-title">{{__('UI Customization')}}</h1>
-    <div class="container page-content">
-        <p>Add your company logo</p>
-    </div>
+<div id="uicustomize" class="container page-content">
+    <h4>UI Customization</h4>
+    <h5>Add your company logo</h5>
+    <form>
+        <div class="custom-file">
+            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+            <small class="">Logo size must be 400x100. File format .jpg or .png</small>
+        </div>
+
+        <div class="custom-file">
+            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+            <small class="">Logo size must be 400x100. File format .jpg or .png</small>
+        </div>
+        <h5 style="mt-5">Create a color scheme to customize your UI</h5>
+        <div class="form-group">
+            <label>Enter hex color or chose a color for the left navigation bar</label>
+            <input type="text" class="form-control inline-input">
+        </div>
+
+        <div class="form-group">
+            <label>Enter hex color or chose a color for the action buttons</label>
+            <input type="text" class="form-control inline-input">
+        </div>
+        
+    </form>
     <div>
         <p>Create a color scheme to customize your UI</p>
         <span>
             <div>
+
             </div>
         </span>
         <customize-color></customize-color>
