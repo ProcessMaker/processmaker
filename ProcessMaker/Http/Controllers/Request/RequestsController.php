64dfa2c15e313c70fb4e3709f761822bc5100d83
dante <dante@danteloayza.localdomain>
2018-08-21 15:24:30 -0400

    Creating a new route and controller that adds actions (like alert messages) after a successful routing.

--- ProcessMaker/Http/Controllers/Request/RequestsController.php
+++ ProcessMaker/Http/Controllers/Request/RequestsController.php
@@ -15,4 +15,18 @@ class RequestsController extends Controller
     {
         return view('requests.index');
     }
+
+    /**
+     * Actions to execute after a request was submitted.
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function requestSubmitted()
+    {
+        // We set the alert message and type in the session, so the next page will show it
+        Session::put('_alert', '["success", "The request was completed successfully."]');
+
+        // Redirect to the tasks list
+        return redirect('/tasks');
+    }
 }
