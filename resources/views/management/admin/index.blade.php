3cbf24f68518fdbf0aa7eb3d9dfd67acb0edb1ff
Leah <leah.miller@processmaker.com>
2018-08-21 13:18:44 -0700

    minor CSS changes, use of important needs to be removed

--- resources/views/management/admin/index.blade.php
+++ resources/views/management/admin/index.blade.php
@@ -7,20 +7,22 @@
 @section('content')
 <div id="uicustomize" class="container page-content">
     <h4>UI Customization</h4>
-    <h5>Add your company logo</h5>
+    <br/>
+    <h6>Add your company logo</h6>
     <form>
         <div class="custom-file">
             <input type="file" class="custom-file-input" id="validatedCustomFile" required>
             <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-            <small class="">Logo size must be 400x100. File format .jpg or .png</small>
+            <small class="text-mute">Logo size must be 400x100. File format .jpg or .png</small>
         </div>
 
         <div class="custom-file">
             <input type="file" class="custom-file-input" id="validatedCustomFile" required>
             <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-            <small class="">Logo size must be 400x100. File format .jpg or .png</small>
+            <small class="text-mute">Logo size must be 400x100. File format .jpg or .png</small>
         </div>
-        <h5 style="mt-5">Create a color scheme to customize your UI</h5>
+    </form>
+        <h6 style="mt-5">Create a color scheme to customize your UI</h6>
         <div class="form-group">
             <label>Enter hex color or chose a color for the left navigation bar</label>
             <input type="text" class="form-control inline-input">
@@ -30,10 +32,8 @@
             <label>Enter hex color or chose a color for the action buttons</label>
             <input type="text" class="form-control inline-input">
         </div>
-        
-    </form>
+    
     <div>
-        <p>Create a color scheme to customize your UI</p>
         <span>
             <div>
 
@@ -42,11 +42,27 @@
         <customize-color></customize-color>
     </div>
 </div>
-
-
-
 @endsection
 
 @section('js')
     <script src="{{mix('js/management/admin/index.js')}}"></script>
 @endsection
+
+<style lang="scss" scoped>
+.inline-button {
+  background-color: rgb(109,124,136);
+  font-weight: 100;
+}
+.text-mute {
+    color: #788793;
+}
+.custom-file {
+    height: auto !important;
+    margin-bottom: 15px !important;
+}
+.custom-file-label::after {
+    content: "CHOOSE" !important;
+    color: white !important;
+    background-color: rgb(173, 183, 191) !important;
+}
+</style>
