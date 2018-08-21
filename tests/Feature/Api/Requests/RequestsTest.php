bd4d7f07741cb429ce96127433ea3555b5a10840
dante <dante@danteloayza.localdomain>
2018-08-21 08:17:30 -0400

    Unit Test and renaming methods to camelcase

--- tests/Feature/Api/Requests/RequestsTest.php
+++ tests/Feature/Api/Requests/RequestsTest.php
@@ -2,22 +2,24 @@
 namespace Tests\Feature\Api\Requests;
 
 use Illuminate\Support\Facades\Hash;
-use ProcessMaker\Model\Application;
+use ProcessMaker\Model\ProcessCategory;
+use ProcessMaker\Model\Process;
 use ProcessMaker\Model\Role;
 use ProcessMaker\Model\User;
 use Tests\Feature\Api\ApiTestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class RequestsTest extends ApiTestCase
 {
     use DatabaseTransactions;
 
+    const URL_USER_PROCESSES = '/api/1.0/user/processes';
+
     /**
      * Test to check that the route is protected
      */
 
-    public function test_route_token_missing()
+    public function testRouteTokenMissing()
     {
         $this->assertFalse(isset($this->token));
     }
@@ -26,7 +28,7 @@ class RequestsTest extends ApiTestCase
      * Test to check that the route is protected
      */
 
-    public function test_api_result_failed()
+    public function restApiResultFailed()
     {
         $response = $this->api('GET', '/api/1.0/requests');
         $response->assertStatus(401);
@@ -36,7 +38,7 @@ class RequestsTest extends ApiTestCase
      * Test to check that the route returns the correct response
      */
 
-    public function test_api_access()
+    public function testApiAccess()
     {
         $this->login();
 
@@ -59,6 +61,46 @@ class RequestsTest extends ApiTestCase
             'meta'
       ]);
     }
+
+    /**
+     * Tests that the sorting and default sorting works.
+     */
+    public function testUserProcessesListSorting()
+    {
+        $this->login();
+
+        // Create some categories
+        $category1 = factory(ProcessCategory::class)->create([ 'name' => 'A category']);
+        $category2 = factory(ProcessCategory::class)->create([ 'name' => 'X category']);
+
+        // Create two processes with different categories and names
+        factory(Process::class)->create([
+            'name' => 'X name',
+            'process_category_id' => $category1->id
+        ]);
+
+        factory(Process::class)->create([
+            'name' => 'A name',
+            'process_category_id' => $category2->id
+        ]);
+
+        // We call the process list endpoint with sort conditions
+        $response = $this->api('GET', self::URL_USER_PROCESSES . '?order_by=name&order_direction=desc');
+
+        // Assert that the response is correct and the sorting is correct
+        $response->assertStatus(200);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals('X name', $data['data'][0]['name']);
+
+        // We call the process list endpoint without sort conditions
+        $response = $this->api('GET', self::URL_USER_PROCESSES);
+
+        // Assert that the response is correct when no sorting is applied
+        $response->assertStatus(200);
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals('A category', $data['data'][0]['category']);
+    }
+
 
     private function login()
     {
