a7aff60598ca8da501b6562a46d7768538abbc6b
Mila Endo <milal.alim@gmail.com>
2018-08-22 12:28:38 -0700

    #536 preferences controller added

---
+++ ProcessMaker/Http/Controllers/Management/PreferencesController.php
@@ -0,0 +1,19 @@
+<?php
+namespace ProcessMaker\Http\Controllers\Management;
+
+use ProcessMaker\Http\Controllers\Controller;
+use ProcessMaker\Model\User;
+
+class UsersController extends Controller
+{
+
+    /**
+     * Get the list of users.
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View
+     */
+    public function index()
+    {
+        return view('preferences');
+    }
+}
