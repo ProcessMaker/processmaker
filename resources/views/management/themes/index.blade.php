af0a7caabc5c2c022898cd61a91ce65dc4364a2d
Mila Endo <milal.alim@gmail.com>
2018-08-22 16:05:27 -0700

    #290 replaced file input with b-form-file

--- resources/views/management/themes/index.blade.php
+++ resources/views/management/themes/index.blade.php
@@ -6,39 +6,24 @@
 
 @section('content')
 <div id="uicustomize" class="container">
+  <div class="container">
     <h1>UI Customization</h1>
     <div class="row">
-        <div class="col-8">
-        <h6>Add your company logo</h6>
+      <div class="col-8">
+        <div class="card card-body">
             <form>
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="validatedCustomFile" required>
-                    <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-                    <small class="text-mute">Logo size must be 400x100. File format .jpg or .png</small>
-                </div>
-
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="validatedCustomFile" required>
-                    <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
-                    <small class="text-mute">Logo size must be 400x100. File format .jpg or .png</small>
-                </div>
+                <b-form-file v-model="file" :state="Boolean(file)" placeholder="Logo size must be 400x100. File format .jpg or .png"></b-form-file>
+                <b-form-file v-model="file" :state="Boolean(file)" placeholder="Logo size must be 400x100. File format .jpg or .png"></b-form-file>
             </form>
-            <h6 style="mt-5">Create a color scheme to customize your UI</h6>
-            <div class="form-group">
-                <label>Enter hex color or chose a color for the left navigation bar</label>
-                <input type="text" class="form-control inline-input dropdown-toggle" id="pickColor1" @click=keepPickColor data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <div class="dropdown-menu" aria-labelledby="pickColor1">
-                    <customize-color ></customize-color>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label>Enter hex color or chose a color for the action buttons</label>
-                <input type="text" class="form-control inline-input dropdown show" id="pickColor2"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <customize-color class="dropdown-menu p-0" aria-labelledby="pickColor2" multiple  ></customize-color>
-            </div>
-        </div>
-    </div>    
+      </div>
+    </div>
+      <div class="col-4">
+        <div class="card card-body">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
+  </div>
+</div>  
 </div>
 @endsection
 
