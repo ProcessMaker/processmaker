a28fa8cf9fcca53e7a00224601738adef19445f2
Mila Endo <milal.alim@gmail.com>
2018-08-23 15:39:06 -0700

    #290 removed unused data

--- resources/assets/js/management/themes/components/CustomizeColor.vue
+++ resources/assets/js/management/themes/components/CustomizeColor.vue
@@ -1,16 +1,11 @@
 <template>
   <div id="app">
     <chrome-picker v-model="colors" />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 import { Chrome } from "vue-color";
-
-var colors = { r: 255, g: 0, b: 0 };
 
 export default {
   name: "app",
@@ -21,26 +16,7 @@ export default {
   data() {
     return {
       colors: {
-        hex: "#194d33",
-        hsl: {
-          h: 150,
-          s: 0.5,
-          l: 0.2,
-          a: 1
-        },
-        hsv: {
-          h: 150,
-          s: 0.66,
-          v: 0.3,
-          a: 1
-        },
-        rgba: {
-          r: 25,
-          g: 77,
-          b: 51,
-          a: 1
-        },
-        a: 1
+        hex: "#3397e1"
       }
     };
   }
