d2a5a96de00c31a40899391e8a82c1539daa4585
Mila Endo <milal.alim@gmail.com>
2018-08-23 13:05:03 -0700

    #290 added colors to data

--- resources/assets/js/management/themes/components/CustomizeColor.vue
+++ resources/assets/js/management/themes/components/CustomizeColor.vue
@@ -8,20 +8,41 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
-import { Chrome } from 'vue-color'
+import { Chrome } from "vue-color";
 
-var colors = { r: 255, g: 0, b: 0 }
+var colors = { r: 255, g: 0, b: 0 };
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     // HelloWorld,
-    'chrome-picker': Chrome,
+    "chrome-picker": Chrome
   },
-  data () {
+  data() {
     return {
-      colors
-    }
+      colors: {
+        hex: "#194d33",
+        hsl: {
+          h: 150,
+          s: 0.5,
+          l: 0.2,
+          a: 1
+        },
+        hsv: {
+          h: 150,
+          s: 0.66,
+          v: 0.3,
+          a: 1
+        },
+        rgba: {
+          r: 25,
+          g: 77,
+          b: 51,
+          a: 1
+        },
+        a: 1
+      }
+    };
   }
-}
+};
 </script>
