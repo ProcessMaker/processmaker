b6f5726ed066f683b4f23ec36e7102b75bf3c98f
Mila Endo <milal.alim@gmail.com>
2018-08-23 11:03:56 -0700

    #290 changed layout and replaced old forms with bootstrap forms and added a modal

--- resources/views/management/themes/index.blade.php
+++ resources/views/management/themes/index.blade.php
@@ -5,42 +5,70 @@
 @endsection
 
 @section('content')
-<div id="uicustomize" class="container">
-  <div class="container">
-    <h1>UI Customization</h1>
-    <div class="row">
-      <div class="col-8">
-        <div class="card card-body">
-            <form>
-                <label>Add your full size company logo</label>
-                <b-form-file v-model="file" :state="Boolean(file)" placeholder="Logo size must be 400x100. File format .jpg or .png"></b-form-file>
-                <label>Add your company icon</label>
-                <b-form-file v-model="file" :state="Boolean(file)" placeholder="Logo size must be 400x100. File format .jpg or .png"></b-form-file>
-            </form>
-            <h6 style="mt-5">Create a color scheme to customize your UI</h6>
-            <div class="form-group">
-                <label>Enter hex color or chose a color for the left navigation bar</label>
-                <input type="text" class="form-control inline-input dropdown-toggle" id="pickColor1" @click=keepPickColor data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <div class="dropdown-menu" aria-labelledby="pickColor1">
+  <div id="uicustomize" class="container">
+    <div class="container">
+      <h1>UI Customization</h1>
+      <div class="row">
+        <div class="col-8">
+          <div class="card card-body">
+             <h4>Add your company logos</h4>
+             <b-form>
+              <b-form-group>
+                <label>Logo size must be 400x100. File format .jpg or .png</label>
+                <b-form-file v-model="file1" placeholder="Choose a file..."></b-form-file>
+                <div class="mt-3">Selected file: @{{file1 && file1.name}}</div>
+              </b-form-group>
+              <b-form-group>
+                <label>Logo size must be 100x100. File format .jpg or .png</label>
+                <b-form-file v-model="file2" placeholder="Choose a file..."></b-form-file>
+                <div class="mt-3">Selected file: @{{file2 && file2.name}}</div>
+              </b-form-group>
+              <h4>Create a color scheme to customize your UI</h4>
+              <div class="row">
+                <div class="col-2">
+                  <button class="btn btn-primary" @click="showPrimaryModal">
+                    primary
+                  </button>
+                  <b-modal ref="myModalRef" hide-footer title="Using Component Methods" align="center">
                     <customize-color ></customize-color>
+                    <div slot="modal-footer">
+                      <button @click="hidePrimaryModal" class="btn btn-outline-success btn-sm text-uppercase">
+                          Cancel
+                      </button>
+                      <button @click="showPrimaryModal" class="btn btn-success btn-sm text-uppercase">
+                          Save
+                      </button>
+                    </div>
+                  </b-modal>
                 </div>
+                <div class="col-2">
+                  <button class="btn btn-secondary" @click="showSecondaryModal">
+                    secondary
+                  </button>
+                  <b-modal ref="myModalRef" hide-footer title="Using Component Methods" align="center">
+                    <customize-color ></customize-color>
+                    <div slot="modal-footer">
+                      <button @click="hideSecondaryModal" class="btn btn-outline-success btn-sm text-uppercase">
+                          Cancel
+                      </button>
+                      <button @click="showSecondaryModal" class="btn btn-success btn-sm text-uppercase">
+                          Save
+                      </button>
+                    </div>
+                  </b-modal>
+                </div>
+             </b-form>
             </div>
-
-            <div class="form-group">
-                <label>Enter hex color or chose a color for the action buttons</label>
-                <input type="text" class="form-control inline-input dropdown show" id="pickColor2"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <customize-color class="dropdown-menu p-0" aria-labelledby="pickColor2" multiple  ></customize-color>
-            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
       </div>
-    </div>
-      <div class="col-4">
-        <div class="card card-body">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-    </div>
+    </div>  
   </div>
-</div>  
-</div>
 @endsection
 
 @section('js')
