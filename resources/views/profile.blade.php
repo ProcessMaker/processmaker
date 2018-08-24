afbbeac53c67824d418db4c5c920d871d9a96b57
Alan Bollinger <velkymx@gmail.com>
2018-08-23 22:15:18 -0700

    Update for profile layout
    TODO: Convert json to proper arrays
    TODO: Hook up forms

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.layout')
 
 @section('content')
-  <div id="profile">
-    <profile></profile>
-  </div>
+<div id="profile">
+  <profile></profile>
+</div>
 @endsection
 
 @section('sidebar')
-    @include('layouts.sidebar', ['sidebar' => Menu::get('sidebar_designer')])
+@include('layouts.sidebar', ['sidebar' => Menu::get('sidebar_designer')])
 @endsection
 
 @section('js')
-  <script src="{{mix('js/management/profile/index.js')}}"></script>
+<script src="{{mix('js/management/profile/index.js')}}"></script>
 @endsection
