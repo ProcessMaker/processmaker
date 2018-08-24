bfa3d3b5b8fa52e653dd8acec0729e0b55516447
dante <dante@danteloayza.localdomain>
2018-08-24 15:49:37 -0400

    Modifying test to validate that application in state TO_DO are returne

--- tests/Feature/Api/Requests/RequestsTest.php
+++ tests/Feature/Api/Requests/RequestsTest.php
@@ -44,7 +44,8 @@ class RequestsTest extends TestCase
 
         factory(\ProcessMaker\Model\Application::class, 1)->create([
             'id' => 10,
-            'creator_user_id' => $this->user->id
+            'creator_user_id' => $this->user->id,
+            'APP_STATUS' => Application::STATUS_TO_DO
         ]);
 
 
@@ -52,14 +53,21 @@ class RequestsTest extends TestCase
             'application_id' => 10
         ]);
 
+        // We create an instance with status completed
+        factory(\ProcessMaker\Model\Application::class, 1)->create([
+            'creator_user_id' => $this->user->id,
+            'APP_STATUS' => Application::STATUS_COMPLETED
+        ]);
+
         $response = $this->actingAs($this->user, 'api')->json('GET', '/api/1.0/requests?delay=overdue');
-
+        $parsedResponse = json_decode($response->getContent());
         $response->assertStatus(200);
-
         $response->assertJsonStructure([
             'data',
             'meta'
-      ]);
+        ]);
+        $this->assertCount(1, $parsedResponse->data,
+            'The number of results should be 1, because just one application has status TO_DO ');
     }
 
     private function login()
