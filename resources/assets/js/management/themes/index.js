794e35232d308bf1b6f458792fb8f0ec3bbd9103
Mila Endo <milal.alim@gmail.com>
2018-08-27 13:30:28 -0700

    #290 added second method for img upload

--- resources/assets/js/management/themes/index.js
+++ resources/assets/js/management/themes/index.js
@@ -28,7 +28,20 @@ new Vue({
         changeColor(val) {
             this.colors = val
         },
-        onImgUpload(input) {
+        onImgUpload1(input) {
+            let file = event.target.files[0];
+            let reader = new FileReader();
+            reader.onload = function (input) {
+                // The file's text will be printed here
+                // console.log("pic", event.target.result)
+                $('#file1Img')
+                    .attr('src', input.target.result);
+                console.log(input.target.result)
+            };
+
+            reader.readAsText(file);
+        },
+        onImgUpload2(input) {
             let file = event.target.files[0];
             let reader = new FileReader();
             reader.onload = function (input) {
