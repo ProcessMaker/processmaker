8fc366a3cca8fa6c6b0b6d5b25c25ae23a28ed93
Mila Endo <milal.alim@gmail.com>
2018-08-27 15:54:05 -0700

    #527 added help links

--- resources/assets/js/management/about/components/about.vue
+++ resources/assets/js/management/about/components/about.vue
@@ -4,9 +4,12 @@
     <div class="row">
       <div class="col-8">
         <div class="card card-body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <img class="logo" src="/img/lg-blk-logo.png">
+        <hr>
+        <a href="https://github.com/ProcessMaker/bpm/issues">Report an issue  <i class="fas fa-bug float-right"></i></a>
+        <hr>
+        <a href="https://github.com/ProcessMaker/bpm">Get Help  <i class="fas fa-info float-right"></i></a>
+        <hr>
       </div>
     </div>
       <div class="col-4">
@@ -27,4 +30,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.logo {
+  width: 400px;
+  height: 100px;
+}
 </style>
