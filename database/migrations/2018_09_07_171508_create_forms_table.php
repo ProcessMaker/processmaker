9ab9c5675ff9b1d2b275af105c781a114232aeeb
Nolan Ehrstrom <github@nolanpro.com>
2018-09-07 10:48:43 -0700

    Add migrations for forms and scripts

---
+++ database/migrations/2018_09_07_171508_create_forms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->uuid('uuid');
+            $table->primary('uuid');
+            $table->text('title');
+            $table->text('description')->nullable();
+            $table->string('type', 20)->default('FORM');
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
