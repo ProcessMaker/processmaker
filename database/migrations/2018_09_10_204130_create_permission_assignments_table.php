eff458034bec6dcf4c506e09ec78701fe24e8590
Alan Bollinger <velkymx@gmail.com>
2018-09-10 13:47:31 -0700

    application permission tables for guards

---
+++ database/migrations/2018_09_10_204130_create_permission_assignments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_assignments', function (Blueprint $table) {
+          $table->uuid('uuid');
+          $table->primary('uuid');
+          $table->uuid('permission_uuid');
+          $table->uuid('assignment_uuid');
+          $table->enum('assignment_type',['user','group']);
+          $table->timestamps();
+
+          $table->foreign('permission_uuid')->references('uuid')->on('processes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_assignments');
+    }
+}
