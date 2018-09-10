dfca372ade829b8cc9bd60d2c76b5a10dccf2ef1
David Callizaya <davidcallizaya@gmail.com>
2018-09-10 15:35:41 -0400

    Remove debug code.

--- routes/console.php
+++ routes/console.php
@@ -15,20 +15,3 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
-
-Artisan::command('david', function () {
-    Illuminate\Support\Facades\Artisan::call('migrate:fresh');
-    foreach (glob(app_path('Models') . '/*') as $file) {
-        $class = 'ProcessMaker\Models\\' . basename($file, '.php');
-        try {
-            echo $class, "\n", str_repeat('=', strlen($class)), "\n";
-            $model = factory($class)->create();
-            $array = $model->toArray();
-            dump($array);
-            $model2 = new $class($array);
-            dump($model2->toArray());
-        } catch (Throwable $e) {
-            dump($e->getMessage());
-        }
-    }
-})->describe('Display an inspiring quote');
