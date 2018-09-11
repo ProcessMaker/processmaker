c0bc5aca44c48acfdf2739c125ccce4e663371b2
Alan Bollinger <velkymx@gmail.com>
2018-09-11 09:55:11 -0700

    Route clean up

---
+++ ProcessMaker/Http/Controllers/Api/GroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ProcessMaker\Http\Controllers\Api;
+
+use ProcessMaker\Group;
+use Illuminate\Http\Request;
+
+class ApiGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \ProcessMaker\Group  $group
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Group $group)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \ProcessMaker\Group  $group
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Group $group)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \ProcessMaker\Group  $group
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Group $group)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \ProcessMaker\Group  $group
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Group $group)
+    {
+        //
+    }
+}
