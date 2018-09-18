94581a1af4d5dd2fe6dcd2f332b7e9453a60d03f
Alan Bollinger <velkymx@gmail.com>
2018-09-18 10:14:21 -0700

    add: group member route

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,12 @@ Route::group(
 
     Route::apiResource('users', 'UserController');
     Route::apiResource('groups', 'GroupController');
+    Route::apiResource('group_members', 'GroupMemberController');
     Route::apiResource('forms', 'FormController');
     Route::apiResource('scripts', 'ScriptController');
     Route::apiResource('processes', 'ProcessController');
     Route::apiResource('process_categories', 'ProcessCategoryController');
+
     Route::apiResource('requests.tokens', 'ProcessRequestTokenController')->only([
         'index', 'show'
     ]);
