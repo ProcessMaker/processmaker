d38817f3a1ae2cac6cf2b824737d828583b224ef
Mila Endo <milal.alim@gmail.com>
2018-09-19 10:58:22 -0700

    #650 cleaned up admin folder structure

---
+++ resources/viewsNew/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container ml-2">
+  <h1>Admin</h1>
+
+</div>
+@endsection
+
+@section('sidebar')
+  @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_admin')])
+@endsection
+
+@section('js')
+
+@endsection
