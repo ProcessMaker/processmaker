fddb10f2bae287521a8ea36fe56749743fd3fc66
Mila Endo <milal.alim@gmail.com>
2018-09-19 11:15:13 -0700

    #650 restructured requests folder

---
+++ resources/viewsNew/requests/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.layout', ['title' => 'Requests'])
+
+@section('sidebar')
+    @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_request')])
+@endsection
+
+@section('content')
+    <div class="container" id="requests-listing">
+    <div class="row">
+          <div class="col-3">
+                      <h1>{{__($title)}}</h1>
+                      </div>
+                        @if ($status == ProcessMaker\Model\Application::STATUS_TO_DO)
+                          <div class="col-5" style="margin-top:20px">
+                            <a href="#" class="btn btn-danger" @click="loadRequestsOverdue"> OVERDUE </a>
+                            &nbsp;
+                            <a href="#" class="btn btn-warning" @click="loadRequestsAtRisk"> AT RISK </a>
+                            &nbsp;
+                            <a href="#" class="btn btn-info" @click="loadRequestsOnTime"> ON TIME </a>
+                          </div>
+                        @endif
+                <div class="col-4" style="margin-top:20px">
+                    <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
+                </div>
+            </div>
+            <div style="margin-top:-20px;">
+            <requests-listing :filter="filter" status="{{ $status }}"></requests-listing>
+          </div>
+        </div>
+@endsection
+
+@section('js')
+<script src="{{mix('js/requests/index.js')}}"></script>
+@endsection
