85179020e679ecbb44bdfb645e2dd264d4e1a728
Mila Endo <milal.alim@gmail.com>
2018-09-19 11:48:54 -0700

    #650 brough in tasks folder from processes

--- resources/viewsNew/tasks/edit.blade.php
+++ resources/viewsNew/tasks/edit.blade.php
@@ -1,15 +1,27 @@
-@extends('layouts.layout',['title'=>'Task Detail'])
+@extends('layouts.layout', ['title' => __('Tasks Management')])
+
 @section('sidebar')
-    @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_task')])
+    @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_admin')])
 @endsection
+
 @section('content')
-<div id="task">
-    <task-form
-        process-uid="{{$process->uid}}" instance-uid="{{$instance->uid}}" token-uid="{{$token->uid}}" form-uid="{{$token->definition['formRef']}}"
-        :data="{{json_encode($data)}}"></task-form>
-</div>
+    <div class="container page-content" id="tasks-listing">
+        <!-- Task Add Dialog -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-md-8 d-flex align-items-center col-sm-12">
+                        <h1 class="page-title">{{__('Tasks')}}</h1>
+                        <input id="tasks-listing-search" v-model="filter" class="form-control col-sm-3"
+                               placeholder="{{__('Search')}}...">
+                    </div>
+                </div>
+                <tasks-listing uid="{{$process->uid}}" :filter="filter"></tasks-listing>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('js')
-<script src="{{mix('js/tasks/show.js')}}"></script>
+    <script src="{{mix('js/processes/tasks/index.js')}}"></script>
 @endsection
