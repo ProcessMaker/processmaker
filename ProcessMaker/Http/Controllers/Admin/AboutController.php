88adc40b756ce3fa11374e7fe6713f81cec3e8ff
Mila Endo <milal.alim@gmail.com>
2018-09-25 13:42:42 -0700

    added index

--- ProcessMaker/Http/Controllers/Admin/AboutController.php
+++ ProcessMaker/Http/Controllers/Admin/AboutController.php
@@ -7,5 +7,13 @@ use ProcessMaker\Http\Controllers\Controller;
 
 class AboutController extends Controller
 {
-    //
+   /**
+   * Get the list of users.
+   *
+   * @return \Illuminate\View\View|\Illuminate\Contracts\View
+   */
+  public function index()
+  {
+      return view('admin.about.index');
+  }
 }
