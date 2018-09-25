7589eb30e87b1a8469df3263aea4e7930f07fe66
David Callizaya <davidcallizaya@gmail.com>
2018-09-25 12:43:28 -0400

    Clean BPMN Nayra Job classes.

--- ProcessMaker/Jobs/RunScriptTask.php
+++ ProcessMaker/Jobs/RunScriptTask.php
@@ -7,7 +7,7 @@ use ProcessMaker\Model\Script;
 use ProcessMaker\Nayra\Contracts\Bpmn\ScriptTaskInterface;
 use ProcessMaker\Nayra\Contracts\Bpmn\TokenInterface;
 
-class RunScriptTask extends TokenAction implements ShouldQueue
+class RunScriptTask extends BpmnAction implements ShouldQueue
 {
 
     /**
@@ -15,14 +15,14 @@ class RunScriptTask extends TokenAction implements ShouldQueue
      *
      * @return void
      */
-    public function action(TokenInterface $token, ScriptTaskInterface $activity)
+    public function action(TokenInterface $token, ScriptTaskInterface $element)
     {
-        $scriptRef = $activity->getProperty('scriptRef');
+        $scriptRef = $element->getProperty('scriptRef');
         Log::info('Script started: ' . $scriptRef);
-        $configuration = json_decode($activity->getProperty('scriptConfiguration'), true);
+        $configuration = json_decode($element->getProperty('scriptConfiguration'), true);
 
         // Check to see if we've failed parsing.  If so, let's convert to empty array.
-        if($configuration === null) {
+        if ($configuration === null) {
             $configuration = [];
         }
         $dataStore = $token->getInstance()->getDataStore();
@@ -34,7 +34,7 @@ class RunScriptTask extends TokenAction implements ShouldQueue
             foreach ($response['output'] as $key => $value) {
                 $dataStore->putData($key, $value);
             }
-            $activity->complete($token);
+            $element->complete($token);
             Log::info('Script completed: ' . $scriptRef);
         } else {
             $token->setStatus(ScriptTaskInterface::TOKEN_STATE_FAILING);
