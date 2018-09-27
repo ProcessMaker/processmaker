de5208cfd85fc04b9fcc5d0a2b55874f0cdaaf07
Leah <leah.miller@processmaker.com>
2018-09-26 17:11:32 -0700

    processtest created

---
+++ tests/Feature/ProcessTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Tests\Feature\Shared\RequestHelper;
+
+class TasksTest extends TestCase
+{
+    use DatabaseTransactions;
+    use RequestHelper;
+
+    const PROCESS_URL = '/processes';
+    const DEFAULT_PASS = 'password';
+
+    protected $structure = [
+        'uuid',
+        'updated_at',
+        'created_at',
+    ];
+
+    public function testIndex() {
+       
+    }
+}
