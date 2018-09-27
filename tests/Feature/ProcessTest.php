6d2f1cb49041affa8f677f4aa56cbd21a3131255
Leah <leah.miller@processmaker.com>
2018-09-27 12:47:08 -0700

    protected funtion setUp works with added Models\User

--- tests/Feature/ProcessTest.php
+++ tests/Feature/ProcessTest.php
@@ -4,11 +4,18 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Tests\Feature\Shared\RequestHelper;
+use ProcessMaker\Models\User;
 
-class TasksTest extends TestCase
+class ProcessTest extends TestCase
 {
     use DatabaseTransactions;
     use RequestHelper;
+
+    // protected function setUp()
+    // {
+    //     parent::setUp();
+    //     $this->user = factory(User::class)->create();
+    // }
 
     protected $structure = [
         'uuid',
@@ -18,6 +25,7 @@ class TasksTest extends TestCase
 
     public function testIndex() {
         $response = $this->webGet('/processes');
+        $response->assertStatus(200);
         $response->assertViewIs('processes.index');
         $response->assertSee('id="processIndex"');
     }
