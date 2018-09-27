0805bde042f3315f277547e6a3fc5d977dfd4a04
Mila Endo <milal.alim@gmail.com>
2018-09-27 12:31:59 -0700

    remved route group

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@ Route::namespace('Admin')->prefix('admin')->group(function(){
 });
 Route::resource('tasks', 'TaskController');
 
-Route::prefix('profile')->group(function () {
-    Route::resource('profile', 'ProfileController');
-});
+Route::resource('profile', 'ProfileController');
+
 // Add our broadcasting routes
 Broadcast::routes();
 
