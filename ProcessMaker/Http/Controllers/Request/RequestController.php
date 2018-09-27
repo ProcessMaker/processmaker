19cb99253145b73d76e73aa679a88de85860dbf4
Mila Endo <milal.alim@gmail.com>
2018-09-27 15:55:49 -0700

    added index

--- ProcessMaker/Http/Controllers/Request/RequestController.php
+++ ProcessMaker/Http/Controllers/Request/RequestController.php
@@ -7,5 +7,13 @@ use ProcessMaker\Http\Controllers\Controller;
 
 class RequestController extends Controller
 {
-    //
+   /**
+   * Get the list of requests.
+   *
+   * @return \Illuminate\View\View|\Illuminate\Contracts\View
+   */
+  public function index()
+  {
+      return view('requests.index');
+  }
 }
