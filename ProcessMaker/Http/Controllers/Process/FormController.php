5b478505ffc96bd2a96b6f082780b33cdcafedbb
Mila Endo <milal.alim@gmail.com>
2018-09-28 13:37:04 -0700

    added index method

--- ProcessMaker/Http/Controllers/Process/FormController.php
+++ ProcessMaker/Http/Controllers/Process/FormController.php
@@ -7,5 +7,13 @@ use ProcessMaker\Http\Controllers\Controller;
 
 class FormController extends Controller
 {
-    //
+     /**
+     * Get the list of environment variables
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View
+     */
+    public function index()
+    {
+        return view('processes.forms.index');
+    }
 }
