ae59b5e8b34a72716c4986353305d25b1195e8b4
Mila Endo <milal.alim@gmail.com>
2018-09-28 13:34:55 -0700

    added form index route test

---
+++ tests/Feature/Processes/FormTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Processes;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Feature\Shared\RequestHelper;
+
+class FormTest extends TestCase
+{
+    use RequestHelper;
+    
+    /**
+     * Test to make sure the controller and route work with the view
+     *
+     * @return void
+     */
+    public function testIndexRoute()
+    {
+
+      // get the URL
+      $response = $this->apiCall('GET', '/processes/forms');
+      // check the correct view is called
+      $response->assertViewIs('processes.forms.index');
+
+      $response->assertStatus(200);
+
+    }
+}
