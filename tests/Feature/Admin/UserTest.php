efae53ab5f382cfb65981b2a4195bf00345a9bc2
Mila Endo <milal.alim@gmail.com>
2018-10-01 12:29:50 -0700

    replaced apicall with webcall

--- tests/Feature/Admin/UserTest.php
+++ tests/Feature/Admin/UserTest.php
@@ -31,7 +31,7 @@ class UserTest extends TestCase
     {
 
       // get the URL
-      $response = $this->apiCall('GET', '/admin/users');
+      $response = $this->webCall('GET', '/admin/users');
       // check the correct view is called
       $response->assertViewIs('admin.users.index');
 
@@ -50,7 +50,7 @@ class UserTest extends TestCase
 
       $user_uuid = factory(User::class)->create()->uuid_text;
       // get the URL
-      $response = $this->apiCall('GET', '/admin/users/'.$user_uuid . '/edit');
+      $response = $this->webCall('GET', '/admin/users/'.$user_uuid . '/edit');
 
       $response->assertStatus(200);
       // check the correct view is called
@@ -67,7 +67,7 @@ class UserTest extends TestCase
     {
 
       // get the URL
-      $response = $this->apiCall('GET', '/admin/users/create');
+      $response = $this->webCall('GET', '/admin/users/create');
 
       $response->assertStatus(200);
       // check the correct view is called
@@ -83,7 +83,7 @@ class UserTest extends TestCase
     {
       $user_uuid = factory(User::class)->create()->uuid_text;
       // get the URL
-      $response = $this->apiCall('GET', '/admin/users/'. $user_uuid);
+      $response = $this->webCall('GET', '/admin/users/'. $user_uuid);
 
       $response->assertStatus(200);
       // check the correct view is called
