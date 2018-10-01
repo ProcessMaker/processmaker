d7098b59f6654dd2231f0c2ec07d31e88e5601e4
Mila Endo <milal.alim@gmail.com>
2018-10-01 12:18:41 -0700

    added edit page

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,6 +5,56 @@
 @endsection
 
 @Section('content')
+    <div class="container">
+      <h1>Edit User</h1>
+      <div class="row">
+        <div class="col-8">
+          <div class="card card-body">
+            <div class="form-group">
+              <label for="exampleInputEmail1">Username</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <small id="emailHelp" class="form-text text-muted">Username must be distinct</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">First Name</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Last Name</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Status</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>ACTIVE</option>
+                <option>INACTIVE</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Password</label>
+              <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Confirm Password</label>
+              <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="d-flex justify-content-end mt-2">
+              <button type="button" class="btn btn-outline-success" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-success ml-2">Save</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
 
 @Section('js')
