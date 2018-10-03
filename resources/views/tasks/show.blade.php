fd66d18e7c3a4c14d9cf63c71e5f78e8843e68bc
Leah <leah.miller@processmaker.com>
2018-10-03 12:08:19 -0700

    #666 - converted nav to grid

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,19 +2,17 @@
 
 @section('content')
 <div>
-    <nav>
-        <ul class="nav row taskNav">
-            <li class="nav-item col-3 align-self-center">
-                <a class="nav-link active text-light p-0 ml-3" href="#"><i class="fas fa-long-arrow-alt-left fa-lg mr-2"></i>BACK TO REQUEST DETAILS</a>
-            </li>
-            <li class="nav-item col-3 align-self-center text-right">
-                <a class="nav-link text-light p-0" href="#"><h4 class="m-0">Task: Approve Leave of Absence</h4></a>
-            </li>
-            <li class="nav-item col align-self-center">
-                <a class="nav-link p-0" href="#" ><span class="pill badge-light p-1 pl-2 pr-2"><i class="fas fa-circle text-primary mr-2"></i><span>Pending</span></a>
-            </li>
-        </ul>
-    </nav>
+    <div class="nav row taskNav">
+        <div class="col-3 align-self-center">
+            <a class="nav-link active text-light p-0 ml-3" href="#"><i class="fas fa-long-arrow-alt-left fa-lg mr-2"></i>BACK TO REQUEST DETAILS</a>
+        </div>
+        <div class="col-3 align-self-center text-right">
+            <a class="nav-link text-light p-0" href="#"><h4 class="m-0">Task: Approve Leave of Absence</h4></a>
+        </div>
+        <div class="col align-self-center">
+            <a class="nav-link p-0" href="#" ><span class="pill badge-light p-1 pl-2 pr-2"><i class="fas fa-circle text-primary mr-2"></i><span>Pending</span></a>
+        </div>
+    </div>
     <br>
     <div class="d-flex container">
         <br>
@@ -29,28 +27,26 @@
             <br>
             <br>
             <br>
-            <div>
-                <div class="row">
-                    <div class="col">
-                        <div class="card card-body border-0">
-                            <div align="center" class="text-muted">
-                            You have not posted any comments yet.
+            <div class="row">
+                <div class="col">
+                    <div class="card card-body border-0">
+                        <div align="center" class="text-muted">
+                        You have not posted any comments yet.
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-1 align-items-center">
+                                <img class="mr-2" src="{{ asset('img/avatar-placeholder.gif') }}"/>
                             </div>
-                            <br>
-                            <div class="row">
-                                <div class="col-1 align-items-center">
-                                    <img class="mr-2 avatar_placeholder" src="{{ asset('img/avatar-placeholder.gif') }}"/>
-                                </div>
-                                <div class="form-group col">
+                            <div class="form-group col">
                                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
                             </div>
-                            <div class="row">
-                                <div class="col-10"></div>
-                                <div class="form-group col text-right">
-                                <button class="btn btn-success">comment</button>
-                                </div>
-                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-10"></div>
+                            <div class="form-group col text-right">
+                            <button class="btn btn-success">comment</button>
+                        </div>
                         </div>
                     </div>
                 </div>
@@ -103,7 +99,7 @@
 @section('css')
 <style lang="scss" scoped>
 
-.avatar_placeholder {
+img {
   height: 45px; 
   border-radius: 50%;
 }
