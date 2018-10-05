fd7decd45102ec7b9b129abbc48b3590ae468915
Mila Endo <milal.alim@gmail.com>
2018-10-05 14:57:31 -0700

    updated titles for documents

--- resources/views/processes/documents/edit.blade.php
+++ resources/views/processes/documents/edit.blade.php
@@ -1,4 +1,8 @@
-@extends('layouts.layout', ['title' => 'Edit Documents'])
+@extends('layouts.layout')
+
+@section('title')
+  {{__('Edit Documents')}}
+@endsection
 
 @Section('sidebar')
 @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_admin')])
