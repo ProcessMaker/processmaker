38bb6e20f98de32ecfe9e9360796a1fc21427c15
Mila Endo <milal.alim@gmail.com>
2018-10-05 15:18:54 -0700

    updated titles for processes

--- resources/views/processes/edit.blade.php
+++ resources/views/processes/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.layout', ['title' => __('Processes Management')])
 
+@section('title')
+  {{__('Edit Processes')}}
+@endsection
+
 @section('sidebar')
     @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_processes')])
 @endsection
