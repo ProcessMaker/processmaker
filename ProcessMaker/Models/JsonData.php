e978bd2bd0a1a097c400e90dc5afe45342359049
Mila Endo <milal.alim@gmail.com>
2018-10-08 11:19:34 -0700

    added jsondata model to have global access to json

---
+++ ProcessMaker/Models/JsonData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ProcessMaker\Models;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Application Data
+ *
+ * @package ProcessMaker\Models
+ */
+class JsonData extends Model
+{    
+     public static function timezones(){
+     $temp = json_decode(file_get_contents('../resources/js/data/timeszones.json'), true);
+      $timezones=[];
+      foreach($temp['data'] as $t){
+        $timezones[$t['value']] = $t['content'];
+      }
+      return $timezones;
+     }
+}
+     
+     
+     
