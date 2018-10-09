0b0a004a0baa5cb366e16e0dea21d7490096e6a6
dante <dante@danteloayza.localdomain>
2018-10-09 15:11:18 -0400

    Add function fot the http task edit endpoint

--- ProcessMaker/Http/Controllers/TaskController.php
+++ ProcessMaker/Http/Controllers/TaskController.php
@@ -4,15 +4,22 @@ namespace ProcessMaker\Http\Controllers;
 
 use Illuminate\Http\Request;
 use ProcessMaker\Http\Controllers\Controller;
+use ProcessMaker\Models\ProcessRequestToken;
 
 class TaskController extends Controller
 {
     public function index()
-  {
-      return view('tasks.index');
-  }
-  public function show()
-  {
-    return view('tasks.show');
-  }
+    {
+        return view('tasks.index');
+    }
+
+    public function show()
+    {
+        return view('tasks.show');
+    }
+
+    public function edit(ProcessRequestToken $task)
+    {
+        return view('tasks.edit', ['task' => $task]);
+    }
 }
