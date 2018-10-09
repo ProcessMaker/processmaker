4777a05f6622b2c4dbd9522cb69099c46bc6b1e8
Leah <leah.miller@processmaker.com>
2018-10-09 16:55:46 -0700

    #707 - Modal layout and CSS changes

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('content')
-    <div class="container page-content" id="users-listing" v-cloak>
+<div class="container page-content" id="users-listing" v-cloak>
     <div class="row">
         <div class="col-sm-12">
             <div class="row">
@@ -18,78 +18,65 @@
                 <input v-model="filter" class="form-control col-sm-3" placeholder="{{__('Search')}}...">
                 </div>
                 <div class="col-md-4 d-flex justify-content-end align-items-center col-sm-12 actions">
-                    <button type="button" href="#" class="btn btn-action" data-toggle="modal" data-target="#create-user-modal"><i class="fas fa-plus"></i> {{__('User')}}</button>
+                    <button type="button" href="#" class="btn btn-action text-white" data-toggle="modal" data-target="#create-user-modal"><i class="fas fa-plus"></i> {{__('User')}}</button>
 
                     <div class="modal fade" id="create-user-modal" tabindex="-1" role="dialog" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            
-                            <div class="modal-body">
-                                <div class="container">
+                                <div class="modal-header">
                                     <h1>Create New User</h1>
-                                    <div class="row">
-                                        <!-- <div class="col-8"> -->
-                                            <div class="card card-body">
-                                                <div class="form-group">
-                                                    <label for="exampleInputEmail1">Username</label>
-                                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                    <small id="emailHelp" class="form-text text-muted">Username must be distinct</small>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="exampleInputEmail1">First Name</label>
-                                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="exampleInputEmail1">Last Name</label>
-                                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="exampleInputEmail1">Email Address</label>
-                                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="exampleInputEmail1">Password</label>
-                                                    <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="exampleInputEmail1">Confirm Password</label>
-                                                    <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                </div>
-                                                <div id="app" class="form-group">
-                                                    <label>Groups</label>
-                                                    <multiselect
-                                                    v-model="value"
-                                                    :options="options"
-                                                    :multiple="true"
-                                                    track-by="title"
-                                                    :custom-label="customLabel"
-                                                    >
-                                                    </multiselect>
-                                                </div>
-                                                <div class="d-flex justify-content-end mt-2">
-                                                    <button type="button" class="btn btn-outline-success" data-dismiss="modal">Close</button>
-                                                    <button type="button" class="btn btn-success ml-2">Save</button>
-                                                </div>
-                                            </div>
-                                        <!-- </div> -->
-                                        
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                
+                                <div class="modal-body">
+                                    <div class="">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Username</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                            <small id="emailHelp" class="form-text text-muted">Username must be distinct</small>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">First Name</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Last Name</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Email Address</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Password</label>
+                                            <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Confirm Password</label>
+                                            <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        </div>
+                                        <div id="app" class="form-group">
+                                            <label>Groups</label>
+                                            <multiselect
+                                            v-model="value"
+                                            :options="options"
+                                            :multiple="true"
+                                            track-by="title"
+                                            :custom-label="customLabel"
+                                            >
+                                            </multiselect>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-success-outline" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-success">Save changes</button>
-                            </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-outline-success" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-success ml-2">Save</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
             <users-listing ref="listing" :filter="filter"></users-listing>
