dd1db112c0aecaffc51d5d52b4708050648f1c9d
Leah <leah.miller@processmaker.com>
2018-10-10 12:14:12 -0700

    #707 - removed Create Test, no longer needed, tests pass

--- tests/Feature/Admin/UserTest.php
+++ tests/Feature/Admin/UserTest.php
@@ -29,15 +29,12 @@ class UserTest extends TestCase
      */
     public function testIndexRoute()
     {
-
       // get the URL
       $response = $this->webCall('GET', '/admin/users');
       // check the correct view is called
       $response->assertViewIs('admin.users.index');
-
       $response->assertStatus(200);
       $response->assertSee('Users');
-
     }
 
     /**
@@ -47,7 +44,6 @@ class UserTest extends TestCase
      */
     public function testEditRoute()
     {
-
       $user_uuid = factory(User::class)->create()->uuid_text;
       // get the URL
       $response = $this->webCall('GET', '/admin/users/'.$user_uuid . '/edit');
@@ -56,22 +52,6 @@ class UserTest extends TestCase
       // check the correct view is called
       $response->assertViewIs('admin.users.edit');
       $response->assertSee('Edit User');
-    }
-
-    /**
-     * Test to make sure the controller and route work wiht the view
-     *
-     * @return void
-     */
-    public function testCreateRoute()
-    {
-      // get the URL
-      $response = $this->webCall('GET', '/admin/users/create');
-
-      $response->assertStatus(200);
-      // check the correct view is called
-      $response->assertViewIs('admin.users.create');
-
     }
     /**
      * Test to make sure the controller and route work with the view
@@ -87,7 +67,5 @@ class UserTest extends TestCase
       // check the correct view is called
       $response->assertViewIs('admin.users.show');
       $response->assertSee($user->firstname);
-
     }
-    
 }
