23ba3094afbcdb25beb0f1c06e311ed8c2dd4bed
Alan Bollinger <velkymx@gmail.com>
2018-10-11 11:16:48 -0700

    Update test for order of ops test

--- tests/Feature/Admin/GroupTest.php
+++ tests/Feature/Admin/GroupTest.php
@@ -21,13 +21,9 @@ class GroupTest extends TestCase
      */
     public function testIndexRoute()
     {
-      factory(Group::class)->create(['name'=>'Test Group']);
-      factory(Group::class)->create(['name'=>'Another group']);
       $response = $this->webCall('GET', '/admin/groups');
       $response->assertStatus(200);
       $response->assertViewIs('admin.groups.index');
-      $response->assertSee('Test Group');
-      $response->assertSee('Another group');
     }
      /**
      * Test to make sure the controller and route work with the view
@@ -43,12 +39,12 @@ class GroupTest extends TestCase
       $response->assertSee('Test Edit');
     }
 
-    public function testShowRoute() 
+    public function testShowRoute()
     {
         $group = factory(Group::class)->create(['name'=>'Test show']);
         $response = $this->webGet('/admin/groups/'. $group->uuid_text );
-        $response->assertViewIs('admin.groups.show');
         $response->assertStatus(200);
+        $response->assertViewIs('admin.groups.show');
         $response->assertSee('Test show');
     }
 }
