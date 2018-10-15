e0f83330b998a7a23f34560fc40eadf173549679
Mila Endo <milal.alim@gmail.com>
2018-10-15 16:19:36 -0700

    brought in modal and changed toggle method

--- resources/views/processes/index.blade.php
+++ resources/views/processes/index.blade.php
@@ -19,18 +19,47 @@
                                placeholder="{{__('Search')}}...">
                     </div>
                     <div class="col-md-4 d-flex justify-content-end align-items-center col-sm-12 actions">
-                        <a href="#" @click="show" class="btn btn-action">
-                            <i class="fas fa-plus"></i> {{__('Process')}}
-                        </a>
+                        <button type="button" class="btn btn-action text-light" data-toggle="modal" data-target="#addProcess"><i class="fas fa-plus"></i> {{__('Process')}}</button>
                     </div>
                 </div>
-                <modal-create-process :show="processModal" @close="processModal=false" :process-uuid="processUuid"
-                                      v-on:reload="reload"></modal-create-process>
                 <processes-listing ref="processListing" :filter="filter" v-on:edit="edit"
                                    v-on:reload="reload"></processes-listing>
             </div>
         </div>
     </div>
+      <div class="modal" tabindex="-1" role="dialog" id="addProcess">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">{{__('Add A Process')}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          {!!Form::label('title', 'Title');!!}
+          {!!Form::text('title', null, ['class'=> 'form-control'])!!}
+          <div class="invalid-feedback"></div>
+        </div>
+        <div class="form-group">
+          {!!Form::label('description', 'Description');!!}
+          {!!Form::textarea('description', null, ['class'=> 'form-control', 'rows' => '3'])!!}
+          <div class="invalid-feedback"></div>
+        </div>
+        <div class="form-group">
+          {!!Form::label('category', 'Category');!!}
+          {!!Form::select('category', [], null, ['class'=> 'form-control'])!!}
+          <div class="invalid-feedback"></div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" id="disabledForNow" @click="onSubmit" :disabled="submitted">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
 
 @section('js')
