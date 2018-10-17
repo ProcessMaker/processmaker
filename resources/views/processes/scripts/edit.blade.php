1a1588e41fbc6b2cf91406a7507dc14b6eca80bb
Leah <leah.miller@processmaker.com>
2018-10-17 14:52:50 -0700

    merge 650 to 770

--- resources/views/processes/scripts/edit.blade.php
+++ resources/views/processes/scripts/edit.blade.php
@@ -4,29 +4,17 @@
   {{__('Scripts Editor')}}
 @endsection
 
-<<<<<<< HEAD
 @section('sidebar')
-@include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_admin')])
-@endsection
-
-@section('content')
-@endsection
-
-@section('js')
-@endsection
-=======
-@Section('sidebar')
 @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_processes')])
 @endsection
 
-@Section('content')
+@section('content')
 <div class="container" id="script-container">
     <script-editor :script="{{$script}}"> </script-editor>
 </div>
 @endsection
 
-@Section('js')
+@section('js')
     <script src="{{mix('js/designer/ScriptEditor/main.js')}}"></script>
 @endsection
 
->>>>>>> feature/650
