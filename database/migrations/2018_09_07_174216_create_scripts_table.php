ec64ba6eb08fd6d33150a86b4434a93ae1f40cbd
Alan Bollinger <velkymx@gmail.com>
2018-10-18 10:30:26 -0700

    Set script description to nullable

--- database/migrations/2018_09_07_174216_create_scripts_table.php
+++ database/migrations/2018_09_07_174216_create_scripts_table.php
@@ -17,7 +17,7 @@ class CreateScriptsTable extends Migration
             $table->uuid('uuid');
             $table->primary('uuid');
             $table->text('title');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('language', 20)->default('PHP');
             $table->text('code');
             $table->timestamps();
