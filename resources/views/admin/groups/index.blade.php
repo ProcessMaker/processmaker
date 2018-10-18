1e41370948052cbc3a8ca71e4afd089456a2539b
Mila Endo <milal.alim@gmail.com>
2018-10-18 09:44:47 -0700

    translation for groups add

--- resources/views/admin/groups/index.blade.php
+++ resources/views/admin/groups/index.blade.php
@@ -89,7 +89,7 @@
                     this.resetErrors();
                     ProcessMaker.apiClient.post('groups', this.formData)
                         .then(response => {
-                            ProcessMaker.alert('Create Group Successfully', 'success');
+                            ProcessMaker.alert('{{_('Create Group Successfully')}}', 'success');
                             this.onClose();
                             //redirect show group
                             window.location = "/admin/groups/" + response.data.uuid
