cc7ebeca3194ccbc2402cf3d58a5f690dd3fd8ea
Alan Bollinger <velkymx@gmail.com>
2018-10-18 10:13:17 -0700

    Fix for redit port default value error

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 'REDISPORT'),
+            'port' => env('REDIS_PORT', '6379'),
             'database' => 0,
         ],
     ],
