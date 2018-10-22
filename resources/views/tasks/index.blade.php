be915795f2a1b24b70bb5779186bbb7ff81c98d1
Mila Endo <milal.alim@gmail.com>
2018-10-22 13:00:35 -0700

    updated grid for tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.layout')
 
 @section('title')
-  {{__('Tasks')}}
+{{__('Tasks')}}
 @endsection
 
 @section('sidebar')
-  @include('layouts.sidebar', ['sidebar' => Menu::get('sidebar_task')])
+@include('layouts.sidebar', ['sidebar' => Menu::get('sidebar_task')])
 @endsection
 
 @section('content')
-<div class="container" id="tasks">
+<div class="container mt-2" id="tasks">
     <div class="row">
         <div class="col">
-            <h1>{{__('Tasks')}}</h1>
+            <h1 class="page-title">{{__('Tasks')}}</h1>
         </div>
-        <div class="col-4" style="margin-top:20px">
-            <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
+        <div class="col mt-2">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">
+                        <i class="fas fa-search"></i>
+                    </span>
+                </div>
+                <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
+            </div>
         </div>
     </div>
     <div style="margin-top:-20px;">
@@ -25,5 +32,5 @@
 @endsection
 
 @section('js')
-    <script src="{{mix('js/tasks/index.js')}}"></script>
+<script src="{{mix('js/tasks/index.js')}}"></script>
 @endsection
