794586a140fecce96ffabe220b56590879ae76ae
Mila Endo <milal.alim@gmail.com>
2018-10-22 15:29:47 -0700

    fixed layout for tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,25 +9,27 @@
 @endsection
 
 @section('content')
-<div class="container mt-2" id="tasks">
-    <div class="row">
-        <div class="col">
-            <h1 class="page-title">{{__('Tasks')}}</h1>
+<div class="container page-content" id="tasks">
+  <h1>{{__('Tasks')}}</h1>
+  <div class="row">
+    <div class="col">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">
+            <i class="fas fa-search"></i>
+          </span>
         </div>
-        <div class="col mt-2">
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="basic-addon1">
-                        <i class="fas fa-search"></i>
-                    </span>
-                </div>
-                <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
-            </div>
-        </div>
+        <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
+      </div>
+
     </div>
-    <div style="margin-top:-20px;">
-        <tasks-list :filter="filter"></tasks-list>
+    <div class="col-8" align="right">
+      
     </div>
+  </div>
+  <div class="container-fluid">
+    <tasks-list :filter="filter"></tasks-list>
+  </div>
 </div>
 @endsection
 
