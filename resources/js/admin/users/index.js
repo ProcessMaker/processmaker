84a668709c92522427065ba44ca017687cbddfab
Mila Endo <milal.alim@gmail.com>
2018-10-23 15:12:25 -0700

    merge fixes

--- resources/js/admin/users/index.js
+++ resources/js/admin/users/index.js
@@ -5,27 +5,20 @@ import Multiselect from 'vue-multiselect/src/Multiselect';
 Vue.component('multiselect', Multiselect)
 
 new Vue({
-            el: '#users-listing',
-            data: {
-                filter: '',
-                userId: null,
-                userModal: false
-            },
-            components: {
-                UsersListing,
-                ModalCreateUser,
-            },
-            methods: {
-                show() {
-                    this.userId = null;
-                    this.userModal = true;
-                },
-                methods: {
-                    reload() {
-                        this.$refs.listing.dataManager([{
-                            field: 'updated_at',
-                            direction: 'desc'
-                        }]);
-                    },
-                }
-            });
+    el: '#users-listing',
+    data: {
+        filter: '',
+        userId: null,
+    },
+    components: {
+        UsersListing,
+    },
+    methods: {
+        reload() {
+            this.$refs.listing.dataManager([{
+                field: 'updated_at',
+                direction: 'desc'
+            }]);
+        },
+    }
+});
