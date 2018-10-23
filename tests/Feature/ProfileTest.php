1421895de9c35abd4721685da37f1121ae76833e
Mila Endo <milal.alim@gmail.com>
2018-10-23 13:13:10 -0700

    fixed profile test

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -10,21 +10,6 @@ use Tests\Feature\Shared\RequestHelper;
 class ProfileTest extends TestCase
 {
     use RequestHelper;
-    /**
-     * Test to make sure the controller and route work with the view
-     *
-     * @return void
-     */
-    public function testIndexRoute()
-    {
-      // get the URL
-      $response = $this->webCall('GET', '/profile');
-      // check the correct view is called
-      $response->assertViewIs('profile.index');
-
-      $response->assertStatus(200);
-
-    }
 
       /**
      * Test to make sure the controller and route work with the view
@@ -36,7 +21,7 @@ class ProfileTest extends TestCase
 
       $user_uuid = factory(User::class)->create()->uuid_text;
       // get the URL
-      $response = $this->webCall('GET', '/profile/'. $user_uuid . '/edit');
+      $response = $this->webCall('GET', '/profile/edit');
 
       $response->assertStatus(200);
       // check the correct view is called
