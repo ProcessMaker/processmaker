6bb226ebd4a5511952e6c9febaf6c3e766f2e748
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-10-25 08:24:40 -0400

    Add include processCount

--- resources/js/processes/categories/components/CategoriesListing.vue
+++ resources/js/processes/categories/components/CategoriesListing.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="data-table">
-        <vuetable :dataManager="dataManager" :sortOrder="sortOrder" :css="css" :api-mode="false"  @vuetable:pagination-data="onPaginationData" :fields="fields" :data="data" data-path="data" pagination-path="meta">
+        <vuetable :dataManager="dataManager" :sortOrder="sortOrder" :css="css" :api-mode="false"
+                  @vuetable:pagination-data="onPaginationData" :fields="fields" :data="data" data-path="data"
+                  pagination-path="meta">
             <template slot="actions" slot-scope="props">
-            <div class="actions">
-                <i class="fas fa-ellipsis-h"></i>
-                <div class="popout">
-                    <b-btn variant="action" @click="onAction('edit-item', props.rowData, props.rowIndex)" v-b-tooltip.hover title="Edit"><i class="fas fa-edit"></i></b-btn>
-                    <b-btn variant="action" @click="onAction('remove-item', props.rowData, props.rowIndex)" v-b-tooltip.hover title="Remove"><i class="fas fa-trash-alt"></i></b-btn>
+                <div class="actions">
+                    <div class="popout">
+                        <b-btn variant="action" @click="onAction('edit-item', props.rowData, props.rowIndex)"
+                               v-b-tooltip.hover title="Edit"><i class="fas fa-edit"></i></b-btn>
+                        <b-btn variant="action" @click="onAction('remove-item', props.rowData, props.rowIndex)"
+                               v-b-tooltip.hover title="Remove"><i class="fas fa-trash-alt"></i></b-btn>
+                    </div>
                 </div>
-          </div>
             </template>
         </vuetable>
-        <pagination single="Category" plural="Categdories" :perPageSelectEnabled="true" @changePerPage="changePerPage" @vuetable-pagination:change-page="onPageChange" ref="pagination"></pagination>
+        <pagination single="Category" plural="Categdories" :perPageSelectEnabled="true" @changePerPage="changePerPage"
+                    @vuetable-pagination:change-page="onPageChange" ref="pagination"></pagination>
     </div>
 </template>
 
@@ -62,28 +66,23 @@
                 // Load from our api client
                 ProcessMaker.apiClient
                     .get(
-                        "process_categories?current_page=" +
+                        'process_categories?current_page=' +
                         this.page +
-                        "&per_page=" +
+                        '&per_page=' +
                         this.perPage +
-                        "&filter=" +
+                        '&filter=' +
                         this.filter +
-                        "&order_by=" +
+                        '&order_by=' +
                         this.orderBy +
-                        "&order_direction=" +
-                        this.orderDirection
+                        '&order_direction=' +
+                        this.orderDirection +
+                        '&include=processesCount'
                     )
                     .then(response => {
                         this.data = this.transform(response.data);
                         this.loading = false;
                     });
             },
-            transform(data) {
-                // format in a way vuetable is expecting
-                data = Object.assign({}, data, data.meta, { meta: null })
-                return data;
-            },
-            onPaginationData() { },
             onAction(action, data, index) {
                 switch (action) {
                     case "edit-item":
