1a846cfc1874d6fb83bf5beb682fa6ddb92b6e44
Mila Endo <milal.alim@gmail.com>
2018-10-29 13:46:20 -0700

    added conditional rendering for phones and address

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -5,7 +5,6 @@
 @endsection
 
 @section('content')
-<div class="container">
   <div class="container" id="profileForm">
     <h1>{{$user->firstname}} {{$user->lastname}}</h1>
     <div class="row">
@@ -13,11 +12,19 @@
         <div class="card card-body">
           <h4 class="mt-2">{{__('Contact Information')}}</h4>
           <div><i class="fas fa-envelope fa-lg text-secondary pr-1"></i><a href="mailto:{{$user->email}}">{{$user->email}}</a> </div>        
-          <div><i class="fas fa-phone fa-lg text-secondary pr-1"></i>{{$user->phone}}</div>
+          <div><i class="fas fa-phone fa-lg text-secondary pr-1"></i>{{__('Phone')}}  {{$user->phone}}</div>
+          @if ($user->fax)
+          <div><i class="fas fa-phone fa-lg text-secondary pr-1"></i>{{__('Fax')}}  {{$user->fax}}</div>
+          @endif
+          @if ($user->cell)
+          <div><i class="fas fa-phone fa-lg text-secondary pr-1"></i>{{__('Cell')}}  {{$user->cell}}</div>
+          @endif
+          @if($user->address)
           <h4 class="mt-2">{{__('Address')}}</h4>
           <div>{{$user->address}}</div>
           <div>{{$user->city}}, {{$user->state}} </div>
           <div>{{$user->postal}} {{$user->country}}</div>
+          @endif
           <h4 class="mt-2">{{__('Current Local Time')}}</h4>
           <div><i class="far fa-calendar-alt fa-lg text-secondary pr-1"></i>{!! Carbon\Carbon::now()->setTimezone($user->timezone)->format('m/d/Y H:i'); !!}</div>        
         </div>
@@ -26,13 +33,13 @@
         <div class="card card-body">
           <div align="center" data-toggle="modal" data-target="#exampleModal">
             <img src="https://via.placeholder.com/150x150" style="border-radius: 50%">
+             <h1>{{$user->firstname}} {{$user->lastname}}</h1>
+             <h4>{{$user->title}}</h4>
           </div>
-
         </div>
       </div>
     </div>
   </div>
-</div>
 @endsection
 
 @section('sidebar')
