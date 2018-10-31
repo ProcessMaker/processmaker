206a4f285e04828c7119b79f3e82333992c212dd
dante <dante@danteloayza.localdomain>
2018-10-31 09:24:42 -0400

    Enabling the filtering by the status passed in the Url

--- resources/js/tasks/components/TasksList.vue
+++ resources/js/tasks/components/TasksList.vue
@@ -30,160 +30,163 @@
 </template>
 
 <script>
-import datatableMixin from "../../components/common/mixins/datatable";
-import moment from "moment";
+    import datatableMixin from "../../components/common/mixins/datatable";
+    import moment from "moment";
 
-export default {
-  mixins: [datatableMixin],
-  props: ["filter"],
-  data() {
-    return {
-      orderBy: "due_at",
+    export default {
+        mixins: [datatableMixin],
+        props: ["filter"],
+        data() {
+            return {
+                orderBy: "due_at",
 
-      sortOrder: [
-        {
-          field: "due_at",
-          sortField: "due_at",
-          direction: "asc"
+                sortOrder: [
+                    {
+                        field: "due_at",
+                        sortField: "due_at",
+                        direction: "asc"
+                    }
+                ],
+                fields: [
+                    {
+                        title: "ID",
+                        name: "id",
+                        sortField: "id"
+                    },
+                    {
+                        title: "TASK",
+                        name: "__slot:name",
+                        field: "element_name",
+                        sortField: "element_name"
+                    },
+                    {
+                        title: "Request",
+                        name: "process.name",
+                        sortField: "process.name"
+                    },
+                    {
+                        title: "Assignee",
+                        name: "user",
+                        callback: this.formatName,
+                        sortField: "user.lastname"
+                    },
+                    {
+                        title: "DUE DATE",
+                        name: "due_at",
+                        callback: this.formatDueDate,
+                        sortField: "due_at"
+                    }
+                ]
+            };
+        },
+        mounted: function mounted() {
+            let params = new URL(document.location).searchParams;
+            let successRouting = params.get("successfulRouting") === "true";
+            if (successRouting) {
+                ProcessMaker.alert("The request was completed successfully.", "success");
+            }
+        },
+        methods: {
+            onAction(action, rowData, index) {
+                if (action === "edit") {
+                    let link = "/tasks/" + rowData.id + "/edit";
+                    window.location = link;
+                }
+            },
+            formatName(user) {
+                if (user === "undefined" || user === null) {
+                    return "";
+                }
+                let name = "<span>" + user.firstname + " " + user.lastname + "</span>";
+
+                let initials =
+                    '<span class="avatar-initials-list">' +
+                    user.firstname.charAt(0).toUpperCase() +
+                    user.lastname.charAt(0).toUpperCase() +
+                    "</span>";
+
+                return user.avatar
+                    ? '<img class="avatar-image-list avatar-circle-list" src="' +
+                    user.avatar +
+                    '"> ' +
+                    name
+                    : '<button type="button" class="avatar-circle-list">' +
+                    initials +
+                    "</button> " +
+                    name;
+            },
+            formatDueDate(value) {
+                let duedate = moment(value);
+                let now = moment();
+                let diff = duedate.diff(now, "hours");
+                let color =
+                    diff < 0 ? "text-danger" : diff <= 1 ? "text-warning" : "text-primary";
+                return '<span class="' + color + '">' + value + "</span>";
+            },
+            formatDate(value) {
+                return moment(value).fromNow();
+            },
+            getTaskStatus() {
+                let path = new URL(location.href);
+                let status = path.searchParams.get('status');
+                return ((status === null) ? 'ACTIVE' : status);
+            },
+            fetch() {
+                this.loading = true;
+                if (this.cancelToken) {
+                    this.cancelToken();
+                    this.cancelToken = null;
+                }
+                const CancelToken = ProcessMaker.apiClient.CancelToken;
+
+
+                // Load from our api client
+                ProcessMaker.apiClient
+                    .get(
+                        "tasks?page=" +
+                        this.page +
+                        "&include=process,processRequest,processRequest.user,user" +
+                        "&status=" + this.getTaskStatus() +
+                        "&per_page=" +
+                        this.perPage +
+                        "&filter=" +
+                        this.filter +
+                        "&order_by=" +
+                        this.orderBy +
+                        "&order_direction=" +
+                        this.orderDirection,
+                        {
+                            cancelToken: new CancelToken(c => {
+                                this.cancelToken = c;
+                            })
+                        }
+                    )
+                    .then(response => {
+                        this.data = this.transform(response.data);
+                        this.loading = false;
+                    });
+            }
         }
-      ],
-      fields: [
-        {
-          title: "ID",
-          name: "id",
-          sortField: "id"
-        },
-        {
-          title: "TASK",
-          name: "__slot:name",
-          field: "element_name",
-          sortField: "element_name"
-        },
-        {
-          title: "Request",
-          name: "process.name",
-          sortField: "process.name"
-        },
-        {
-          title: "Assignee",
-          name: "user",
-          callback: this.formatName,
-          sortField: "user.lastname"
-        },
-        {
-          title: "DUE DATE",
-          name: "due_at",
-          callback: this.formatDueDate,
-          sortField: "due_at"
-        }
-      ]
     };
-  },
-  mounted: function mounted() {
-    let params = new URL(document.location).searchParams;
-    let successRouting = params.get("successfulRouting") === "true";
-    if (successRouting) {
-      ProcessMaker.alert("The request was completed successfully.", "success");
-    }
-  },
-  methods: {
-    onAction(action, rowData, index) {
-      if (action === "edit") {
-        let link = "/tasks/" + rowData.id + "/edit";
-        window.location = link;
-      }
-    },
-    formatName(user) {
-      if (user === "undefined" || user === null) {
-        return "";
-      }
-      let name = "<span>" + user.firstname + " " + user.lastname + "</span>";
-
-      let initials =
-        '<span class="avatar-initials-list">' +
-        user.firstname.charAt(0).toUpperCase() +
-        user.lastname.charAt(0).toUpperCase() +
-        "</span>";
-
-      return user.avatar
-        ? '<img class="avatar-image-list avatar-circle-list" src="' +
-            user.avatar +
-            '"> ' +
-            name
-        : '<button type="button" class="avatar-circle-list">' +
-            initials +
-            "</button> " +
-            name;
-    },
-    formatDueDate(value) {
-      let duedate = moment(value);
-      let now = moment();
-      let diff = duedate.diff(now, "hours");
-      let color =
-        diff < 0 ? "text-danger" : diff <= 1 ? "text-warning" : "text-primary";
-      return '<span class="' + color + '">' + value + "</span>";
-    },
-    formatDate(value) {
-      return moment(value).fromNow();
-    },
-    fetch() {
-      this.loading = true;
-      if (this.cancelToken) {
-        this.cancelToken();
-        this.cancelToken = null;
-      }
-      const CancelToken = ProcessMaker.apiClient.CancelToken;
-
-      //@todo change the method to obtain ID of the process.
-      var path = location.pathname.split("/");
-
-      // Load from our api client
-      ProcessMaker.apiClient
-        .get(
-          "tasks?page=" +
-            this.page +
-            "&include=process,processRequest,processRequest.user,user" +
-            "&status=ACTIVE" +
-            "&per_page=" +
-            this.perPage +
-            "&filter=" +
-            this.filter +
-            "&order_by=" +
-            this.orderBy +
-            "&order_direction=" +
-            this.orderDirection,
-          {
-            cancelToken: new CancelToken(c => {
-              this.cancelToken = c;
-            })
-          }
-        )
-        .then(response => {
-          this.data = this.transform(response.data);
-          this.loading = false;
-        });
-    }
-  }
-};
 </script>
 
 <style lang="scss">
-/deep/ th#_total_users {
-  width: 150px;
-  text-align: center;
-}
+    /deep/ th#_total_users {
+        width: 150px;
+        text-align: center;
+    }
 
-/deep/ th#_description {
-  width: 250px;
-}
+    /deep/ th#_description {
+        width: 250px;
+    }
 
-/deep/ i.fa-circle {
-  &.active {
-    color: green;
-  }
-  &.inactive {
-    color: red;
-  }
-}
+    /deep/ i.fa-circle {
+        &.active {
+            color: green;
+        }
+        &.inactive {
+            color: red;
+        }
+    }
 </style>
 
