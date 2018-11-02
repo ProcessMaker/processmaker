1a03847dc8c0ae722845c549d446a220656f7899
Mila Endo <milal.alim@gmail.com>
2018-11-02 09:09:23 -0700

    fixed merge issues

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,16 +7,6 @@
 @section('content')
 <div class="container" id="profileForm">
     <h1>{{__('Profile')}}</h1>
-    <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab"
-                aria-controls="nav-home" aria-selected="true">Information</a>
-            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab"
-                aria-controls="nav-profile" aria-selected="false">Permissions</a>
-            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab"
-                aria-controls="nav-contact" aria-selected="false">Groups</a>
-        </div>
-    </nav>
     <div class="row">
         <div class="col-8">
             <nav>
@@ -57,7 +47,6 @@
                             <td><a href="{{'tel:' . $user->cell}}">{{$user->cell}}</a></td>
                         </tr>
                         @endif
-<<<<<<< HEAD
 
 
                         @if($user->address)
@@ -90,8 +79,6 @@
                             </tr>
                         </thead>
                         <tbody>
-=======
->>>>>>> develop
 
                             <tr>
                                 <td>Add</td>
@@ -133,7 +120,6 @@
                                 <td align="center"><input type="checkbox"></td>
                                 <td align="center"><input type="checkbox"></td>
 
-<<<<<<< HEAD
                             </tr>
                             <tr>
                                 <td>Move</td>
@@ -148,24 +134,6 @@
                         </tbody>
                     </table>
                 </div>
-=======
-                        @if($user->address)
-                        <tr>
-                            <td colspan="3">
-                                <h4 class="mb-0 mt-3">{{__('Address')}}</h4>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="3">
-                                {{$user->address}}<br>
-                                {{$user->city}}, {{$user->state}} {{$user->postal}} {{$user->country}}
-                            </td>
-                        </tr>
-                        @endif
-                    </table>
-                </div>
-                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...cee</div>
->>>>>>> develop
                 <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...vvv</div>
             </div>
         </div>
