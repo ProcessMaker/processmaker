844d22c72f3a0822b50c2b90d325858438e65bdc
dante <dante@danteloayza.localdomain>
2018-11-06 14:52:50 -0400

    Removing old and unused preferences's index.js from webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -31,7 +31,6 @@ mix.webpackConfig({
     .js('resources/js/admin/groups/index.js', 'public/js/admin/groups')
     .js('resources/js/admin/queues/index.js', 'public/js/admin/queues')
 
-    .js('resources/js/admin/preferences/index.js', 'public/js/admin/preferences')
     .js('resources/js/processes/index.js', 'public/js/processes')
     .js('resources/js/processes/categories/index.js', 'public/js/processes/categories')
     .js('resources/js/processes/scripts/index.js', 'public/js/processes/scripts')
