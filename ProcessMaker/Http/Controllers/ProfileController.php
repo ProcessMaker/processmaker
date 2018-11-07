8e68a17400ecfd96d1b1f536f96d020911cb7cb5
Mila Endo <milal.alim@gmail.com>
2018-11-07 13:46:43 -0800

    removed update

--- ProcessMaker/Http/Controllers/ProfileController.php
+++ ProcessMaker/Http/Controllers/ProfileController.php
@@ -38,38 +38,7 @@ class ProfileController extends Controller
         $users_permission_ids = $this->user_permission_ids($user);
         return view('profile.show', compact('user', 'all_permissions', 'users_permission_ids'));
     }
-    public function update(Request $request, $id) 
-    {
-        // find the user
-        $user = User::findOrFail($id);
-        // assign the users permissions ids
-        $users_permission_ids = $this->user_permission_ids($user);
-        foreach(Permission::all() as $permission) {
-            //if the request has the ids present
-            if ($request->has('permission_'.$permission->id)) {
-                // and the id is not in the array
-                if(!in_array($permission->id,$users_permission_ids)){
-                    // the user needs to add permissions 
-                    PermissionAssignment::create([
-                        'permission_id' => $permission->id, 
-                        'assignable_type' => User::class, 
-                        'assignable_id' => $user->id
-                    ]);
-                    return redirect()->back();
-                }
-            } else { 
-                if(in_array($permission->id,$users_permission_ids)){
-                    //user needs to delete this permission 
-                    PermissionAssignment::where([
-                        'permission_id' => $permission->id, 
-                        'assignable_type' => User::class, 
-                        'assignable_id' => $user->id
-                    ])->delete();
-                    return redirect()->back();
-                }
-            }
-        }
-    }
+
     private function user_permission_ids($user) 
     {
         return $user->permissionAssignments()->pluck('permission_id')->toArray();
