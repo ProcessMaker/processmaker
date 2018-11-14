c99588a0294e2045f1a2d1633e19ffa369d7c68e
Taylor Dondich <taylor@processmaker.com>
2018-11-14 08:21:48 -0800

    Set default screen type to form when adding a new screen.

--- resources/views/processes/screens/index.blade.php
+++ resources/views/processes/screens/index.blade.php
@@ -53,7 +53,7 @@
                     </div>
                     <div class="form-group">
                         {!! Form::label('type', 'Type') !!}
-                        {!! Form::select('type', ['DISPLAY' => 'Display', 'FORM' => 'Form'], 'null', ['id' => 'type','class'=> 'form-control', 'v-model' => 'formData.type',
+                        {!! Form::select('type', ['DISPLAY' => 'Display', 'FORM' => 'Form'], 'FORM', ['id' => 'type','class'=> 'form-control', 'v-model' => 'formData.type',
                         'v-bind:class' => '{"form-control":true, "is-invalid":errors.type}']) !!}
                         <div class="invalid-feedback" v-for="type in errors.type">@{{type}}</div>
                     </div>
@@ -96,7 +96,8 @@
                 resetFormData() {
                     this.formData = Object.assign({}, {
                         title: null,
-                        type: null,
+                        // Default to form type
+                        type: 'FORM',
                         description: null,
                     });
                 },
