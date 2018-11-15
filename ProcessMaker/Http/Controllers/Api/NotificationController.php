79a7879615fdfe78aa4647a843a1a66556deddaf
dante <dante@danteloayza.localdomain>
2018-11-15 12:51:58 -0400

    Added annotations for Notifications endpoints

--- ProcessMaker/Http/Controllers/Api/NotificationController.php
+++ ProcessMaker/Http/Controllers/Api/NotificationController.php
@@ -13,18 +13,86 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class NotificationController extends Controller
 {
+    /**
+     * Returns a list of notifications not read by the authenticated user
+     *
+     * @param Request $request
+     *
+     * @return ApiCollection
+     *
+     * @OA\Get(
+     *     path="/notifications",
+     *     summary="Returns a list of notifications not read by the authenticated user",
+     *     tags={"Notifications"},
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="list of notifications",
+     *         @OA\JsonContent(
+     *             type="array",
+     *              @OA\Items (
+     *                      @OA\Property(
+     *                          property="dateTime",
+     *                          type="date",
+     *                          description="date when the notification has been created"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="id",
+     *                          type="string",
+     *                          description="message id"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="message",
+     *                          type="string",
+     *                          description="message text"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="url",
+     *                          type="string",
+     *                          description="associated url of the message"
+     *                      ),
+     *              )
+     *             ),
+     *         ),
+     *     )
+     */
     public function index()
     {
         return response(\Auth::user()->activeNotifications(), 200);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update notifications
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param Media $file
+     * @param Request $request
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
+     *
+     *     @OA\Put(
+     *     path="/notification",
+     *     summary="Update notifications",
+     *     tags={"Notifications"},
+     *
+     *     @OA\RequestBody(
+     *       required=true,
+     *       @OA\JsonContent(
+     *          @OA\Property(
+     *              property="message_ids",
+     *              type="array",
+     *              description="list of message ids that will be marked as read",
+     *              @OA\Items (type="string")),
+     *          @OA\Property(
+     *              property="routes",
+     *              type="array",
+     *              description="all messages that has an url that is in this list will be marked as read",
+     *              @OA\Items (type="string"))
+     *       ),
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="success",
+     *     ),
+     * )
      */
     public function update(Request $request)
     {
