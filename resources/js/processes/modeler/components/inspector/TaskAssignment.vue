9eded107a773670f398c169939b9ab3133bf7df8
David Callizaya <davidcallizaya@gmail.com>
2018-11-15 16:05:36 -0400

    Add properties to task.

---
+++ resources/js/processes/modeler/components/inspector/TaskAssignment.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="form-group">
+        <label>{{label}}</label>
+        <select class="form-control" :value="assignmentGetter" @input="assignmentSetter">
+            <option value=""></option>
+            <option value="requestor">To requestor</option>
+        </select>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ["value", "label", "helper", "property"],
+    data() {
+        return {};
+    },
+    computed: {
+        /**
+         * Get the value of the edited property
+         */
+        assignmentGetter() {
+            const node = this.$parent.$parent.inspectorNode;
+            const value = _.get(node, 'assignment');
+            return value;
+        },
+        node () {
+            return this.$parent.$parent.inspectorNode;
+        }
+    },
+    methods: {
+        /**
+         * Update the event of the editer property
+         */
+        assignmentSetter (event) {
+            Vue.set(this.node, 'assignment', event.target.value);
+            this.$emit('input', this.value);
+        },
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- resources/js/processes/modeler/initialLoad.js
+++ resources/js/processes/modeler/initialLoad.js
@@ -22,18 +22,38 @@ let moddle = new BpmnModdle({
 // Bring in our screen selector
 import ModelerScreenSelect from './components/inspector/ScreenSelect';
 import ExpressionEditor from './components/inspector/ExpressionEditor';
+import TaskAssignment from './components/inspector/TaskAssignment';
 
 Vue.component('ModelerScreenSelect', ModelerScreenSelect);
 Vue.component('ExpressionEditor', ExpressionEditor);
+Vue.component('TaskAssignment', TaskAssignment);
 
 // Append to the inspector config of task
 task.inspectorConfig[0].items.push({
-  component: 'ModelerScreenSelect',
-  config: {
-    label: 'Screen For Input',
-    helper: 'What Screen Should Be Used For Rendering This Task',
-    name: 'screenRef'
-  }
+    component: 'ModelerScreenSelect',
+    config: {
+        label: 'Screen For Input',
+        helper: 'What Screen Should Be Used For Rendering This Task',
+        name: 'screenRef'
+    }
+});
+task.inspectorConfig[0].items.push({
+    component: "FormInput",
+    config: {
+        type: "number",
+        label: "Due In",
+        placeholder: "72 hours",
+        helper: "Time when the task will due (hours)",
+        name: "dueIn"
+    }
+});
+task.inspectorConfig[0].items.push({
+    component: "TaskAssignment",
+    config: {
+        label: "Task Assignment",
+        helper: "",
+        name: "id"
+    }
 });
 
 // Watcher:
@@ -41,7 +61,6 @@ task.inspectorConfig[0].items.push({
 sequenceFlow.inspectorHandler = (value, definition, component) => {
     // Go through each property and rebind it to our data
     Object.keys(value).forEach((key) => {
-        console.log(key, value[key]);
         if (definition[key] !== value[key]) {
             definition[key] = value[key];
         }
