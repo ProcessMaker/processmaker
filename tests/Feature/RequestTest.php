c4fdd1c3855f27a870c0ea6228dd7aa8cea124b1
Mila Endo <milal.alim@gmail.com>
2018-11-19 15:43:35 -0800

    testing route with permissons to view all requests

--- tests/Feature/RequestTest.php
+++ tests/Feature/RequestTest.php
@@ -5,6 +5,10 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Tests\Feature\Shared\RequestHelper;
 use ProcessMaker\Models\ProcessRequest;
+use ProcessMaker\Models\User;
+use ProcessMaker\Models\PermissionAssignment;
+use ProcessMaker\Models\Permission;
+use \PermissionSeeder;
 
 class RequestTest extends TestCase
 {
@@ -19,6 +23,27 @@ class RequestTest extends TestCase
     {
       // get the URL
       $response = $this->webCall('GET', '/requests');
+      $response->assertStatus(200);
+      // check the correct view is called
+      $response->assertViewIs('requests.index');
+
+    }
+    /**
+     * Test to make sure the controller and route work with the view and show_all_requests permissions
+     *
+     * @return void
+     */
+    public function testRequestAllRouteWithShowAllRequestsPermission()
+    {
+      (new PermissionSeeder)->run($this->user);
+      factory(PermissionAssignment::class)->create([
+            'assignable_type' => get_class($this->user),
+            'assignable_id' => $this->user->id,
+            'permission_id' => Permission::byGuardName('show_all_requests')
+        ]);
+      $this->user->giveDirectPermission('show_all_requests');
+      // get the URL
+      $response = $this->webCall('GET', '/requests/all');
       $response->assertStatus(200);
       // check the correct view is called
       $response->assertViewIs('requests.index');
