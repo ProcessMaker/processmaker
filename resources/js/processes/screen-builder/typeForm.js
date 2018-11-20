924b6a2c920194d8619e4b233fe5c1a839dde514
Taylor Dondich <taylor@processmaker.com>
2018-11-19 16:41:51 -0800

    Work in progress of file upload component.

--- resources/js/processes/screen-builder/typeForm.js
+++ resources/js/processes/screen-builder/typeForm.js
@@ -1,7 +1,38 @@
 import initialControls from "@processmaker/vue-form-builder/src/form-builder-controls";
 
+import FileUpload from "./components/form/file-upload"
+
+initialControls[initialControls.length] = {
+    rendererComponent: FileUpload,
+    rendererBinding: 'FileUpload',
+    builderComponent: FileUpload,
+    builderBinding: 'FileUpload',
+    control: {
+        label: 'File Upload',
+        component: 'FileUpload',
+        'editor-component': 'FileUpload',
+        'editor-icon': require('./components/form/file-upload.png'),
+        config: {
+            label: 'New File Upload'
+        },
+        inspector: [
+                {
+                    type: "FormInput",
+                    field: "label",
+                    config: {
+                        label: "Text Label",
+                        helper: "The text to display",
+                    }
+                },
+        ]
+    }
+}
+
 ProcessMaker.EventBus.$on('screen-builder-init', (manager) => {
     for (var i = 0; i < initialControls.length; i++) {
+        console.log("Adding:");
+        console.log(initialControls[i].builderComponent);
+        console.log(initialControls[i].builderBinding);
         manager.addControl(
             initialControls[i].control,
             initialControls[i].rendererComponent,
