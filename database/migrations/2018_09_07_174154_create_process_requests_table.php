a8c6a7e25197575f8025d354b5bafb22a4d33905
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-11-20 09:25:10 -0400

    Add status CANCELED to table process_requests

--- database/migrations/2018_09_07_174154_create_process_requests_table.php
+++ database/migrations/2018_09_07_174154_create_process_requests_table.php
@@ -24,7 +24,7 @@ class CreateProcessRequestsTable extends Migration
             $table->string('participant_id')->nullable();
             // The callable id is the text id of the bpmn element
             $table->string('callable_id');
-            $table->enum('status', ['DRAFT','ACTIVE', 'COMPLETED', 'ERROR']);
+            $table->enum('status', ['DRAFT','ACTIVE', 'COMPLETED', 'ERROR', 'CANCELED']);
             $table->json('data');
             $table->string('name');
             $table->timestamp('completed_at')->nullable();
