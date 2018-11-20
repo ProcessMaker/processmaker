8c3b5b0dfc9a41d8927a0e11a1ca42a365ab745d
Mila Endo <milal.alim@gmail.com>
2018-11-19 16:40:30 -0800

    new api endpoint for request files

---
+++ ProcessMaker/Http/Controllers/Api/ProcessRequestFileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ProcessMaker\Http\Controllers\Api;
+
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use ProcessMaker\Http\Controllers\Controller;
+use ProcessMaker\Http\Resources\ApiCollection;
+use ProcessMaker\Http\Resources\ProcessRequests;
+use ProcessMaker\Models\ProcessRequest;
+use ProcessMaker\Http\Resources\ProcessRequests as ProcessRequestResource;
+
+
+class ProcessRequestFileController extends Controller
+{
+    //
+}
