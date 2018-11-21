a825f07fa10a689a2c808de12c3f19314ceb5c21
Mila Endo <milal.alim@gmail.com>
2018-11-20 16:50:21 -0800

    store method added

--- ProcessMaker/Http/Controllers/Api/ProcessRequestFileController.php
+++ ProcessMaker/Http/Controllers/Api/ProcessRequestFileController.php
@@ -11,8 +11,10 @@ use ProcessMaker\Http\Resources\ApiCollection;
 use ProcessMaker\Models\ProcessRequest;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use ProcessMaker\Http\Resources\ApiResource;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\JsonResponse;
 
 
 class ProcessRequestFileController extends Controller
@@ -25,4 +27,13 @@ class ProcessRequestFileController extends Controller
      {
         return new ResourceCollection($request->getMedia());
      }
+
+    /**
+     * save media file to db
+    */
+    public function store(Request $laravel_request, ProcessRequest $request)
+    {
+        $file = $request->addMedia($laravel_request->file)->toMediaCollection();
+        return new JsonResponse(['message' => 'file successfully uploaded'], 200);
+    }
 }
