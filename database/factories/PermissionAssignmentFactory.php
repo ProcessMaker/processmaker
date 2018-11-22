9ddc61bcf20168c6a50fe2db18a9f2042b166a57
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-11-22 15:58:57 -0400

    Update factory Permission Assignments

--- database/factories/PermissionAssignmentFactory.php
+++ database/factories/PermissionAssignmentFactory.php
@@ -1,8 +1,45 @@
 <?php
 
 use Faker\Generator as Faker;
+use ProcessMaker\Models\Group;
+use ProcessMaker\Models\Permission;
+use ProcessMaker\Models\PermissionAssignment;
+use ProcessMaker\Models\User;
 
-$factory->define(ProcessMaker\Models\PermissionAssignment::class, function (Faker $faker) {
+$factory->define(PermissionAssignment::class, function (Faker $faker) {
+
+    $model = factory($faker->randomElement([
+        User::class,
+        Group::class,
+    ]))->create();
+
     return [
+        'permission_id' => function () {
+            return factory(Permission::class)->create()->getKey();
+        },
+        'assignable_id' => $model->getKey(),
+        'assignable_type' => get_class($model)
     ];
+});
+
+$factory->defineAs(PermissionAssignment::class, 'user', function () use ($factory) {
+    $follow = $factory->raw(PermissionAssignment::class);
+    $extras = [
+        'id' => function () {
+            return factory(User::class)->create()->getKey();
+        },
+        'assignable_type' => User::class
+    ];
+    return array_merge($follow, $extras);
+});
+
+$factory->defineAs(PermissionAssignment::class, 'group', function () use ($factory) {
+    $follow = $factory->raw(PermissionAssignment::class);
+    $extras = [
+        'id' => function () {
+            return factory(Group::class)->create()->getKey();
+        },
+        'assignable_type' => Group::class
+    ];
+    return array_merge($follow, $extras);
 });
