b4e00bfe515f472ec6dc15f8971c3f148c9538d9
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-11-22 15:35:48 -0400

    Add list of user and groups in page edit processes

--- ProcessMaker/Http/Controllers/ProcessController.php
+++ ProcessMaker/Http/Controllers/ProcessController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace ProcessMaker\Http\Controllers;
+
+use ProcessMaker\Http\Resources\Groups;
+use ProcessMaker\Models\Group;
+use ProcessMaker\Models\Permission;
+use ProcessMaker\Models\PermissionAssignment;
 use ProcessMaker\Models\Process;
 use Illuminate\Http\Request;
 use ProcessMaker\Models\ProcessCategory;
 use ProcessMaker\Models\Screen;
+use ProcessMaker\Models\User;
 
 class ProcessController extends Controller
 {
@@ -13,24 +19,48 @@ class ProcessController extends Controller
         $processes = Process::all(); //what will be in the database = Model
         $processCategories = ProcessCategory::all();
         $processCategoryArray = ['' => 'None'];
-        foreach($processCategories as $pc){
+        foreach ($processCategories as $pc) {
             $processCategoryArray[$pc->id] = $pc->name;
         }
-        return view('processes.index', ["processes"=>$processes, "processCategories"=>$processCategoryArray]);
+        return view('processes.index', ["processes" => $processes, "processCategories" => $processCategoryArray]);
     }
+
     public function edit(Process $process)
     {
         $categories = ProcessCategory::orderBy('name')
-                        ->get()
-                        ->pluck('name', 'id')
-                        ->toArray();
+            ->get()
+            ->pluck('name', 'id')
+            ->toArray();
 
         $screens = Screen::orderBy('title')
-                    ->get()
-                    ->pluck('title', 'id')
-                    ->toArray();
+            ->get()
+            ->pluck('title', 'id')
+            ->toArray();
 
-        return view('processes.edit', compact('process', 'categories', 'screens'));
+        //list users and group with permissions processes.cancel
+        $assignments = [
+            'Users' => [],
+            'Groups' => [],
+        ];
+
+        $groups = PermissionAssignment::where('permission_id', Permission::byGuardName('processes.cancel')->id)
+            ->where('assignable_type', Group::class)
+            ->get();
+
+        $users = PermissionAssignment::where('permission_id', Permission::byGuardName('processes.cancel')->id)
+            ->where('assignable_type', User::class)
+            ->get();
+
+        foreach ($groups as $assigned) {
+            $group = Group::find($assigned->assignable_id);
+            $assignments['Groups'][$group->id] = $group->name;
+        }
+        foreach ($users as $assigned) {
+            $user = User::find($assigned->assignable_id);
+            $assignments['Users'][$user->id] = $user->fullname;
+        }
+
+        return view('processes.edit', compact('process', 'categories', 'screens', 'assignments'));
     }
 
     public function create() // create new process
@@ -48,11 +78,13 @@ class ProcessController extends Controller
         $process->saveOrFail();
         return redirect('/processes');
     }
+
     public function show(Process $process) // show new process to UI
     {
         // Redirect to our modeler
         return redirect()->to(route('modeler'));
     }
+
     public function update(Process $process, Request $request) // update existing process to DB
     {
         $request->validate(Process::rules($request));
@@ -60,6 +92,7 @@ class ProcessController extends Controller
         $process->saveOrFail();
         return redirect('/processes');
     }
+
     public function destroy(Process $process) // destory existing process to DB / UI
     {
         $process->delete();
--- resources/views/processes/edit.blade.php
+++ resources/views/processes/edit.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.layout', ['title' => __('Processes Management')])
 
 @section('title')
-  {{__('Edit Process')}}
+{{__('Edit Process')}}
 @endsection
 
 @section('sidebar')
-    @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_processes')])
+@include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_processes')])
 @endsection
 
 @section('content')
-<!doctype html>
+        <!doctype html>
 
 <div class="container" id="editProcess">
     <h1>{{__('Edit Process')}}</h1>
     <div class="row">
         <div class="col-8">
             <div class="card card-body">
-                
+
                 <div class="form-group">
                     {!!Form::label('processTitle', __('Process title'))!!}
                     {!!Form::text('processTitle', null,
@@ -41,7 +41,7 @@
                     <div class="invalid-feedback" v-if="errors.description">@{{errors.description[0]}}</div>
                 </div>
                 <div class="form-group p-0">
-                    {!! Form::label('category', 'Category') !!}
+                    {!! Form::label('category', __('Category')) !!}
                     {!! Form::select('category', $categories, null,
                         ['id' => 'process_category_id',
                             'class' => 'form-control',
@@ -52,11 +52,20 @@
                     <div class="invalid-feedback" v-if="errors.category">@{{errors.category[0]}}</div>
                 </div>
                 <div class="form-group p-0">
-                    {!! Form::label('summaryScreen', 'Summary screen') !!}
+                    {!! Form::label('summaryScreen', __('Summary screen')) !!}
                     {!! Form::select('summaryScreen', ['null' => '- No screen -'] + $screens, null,
                         ['id' => 'summary_screen_id',
                             'class' => 'form-control',
                             'v-model' => 'formData.summary_screen_id',
+                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.screen}'
+                        ])
+                    !!}
+                </div>
+                <div class="form-group p-0">
+                    {!! Form::label('cancelRequest', __('Cancel Request')) !!}
+                    {!! Form::select('cancelRequest', $assignments, null, [
+                            'class' => 'form-control',
+                            'v-model' => 'formData.cancel_request_id',
                             'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.screen}'
                         ])
                     !!}
@@ -78,10 +87,15 @@
                 </div>
             </div>
 
+
         </div>
         <div class="col-4">
             <div class="card card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
             </div>
         </div>
     </div>
@@ -91,60 +105,60 @@
 
 @section('js')
     <script>
-    test = new Vue({
-        el: '#editProcess',
-        data() {
-            return {
-                formData: @json($process),
-                dataGroups: [],
-                value: [],
-                errors: {
-                    name: null,
-                    description: null,
-                    category: null,
-                    status: null,
-                    screen: null
+        test = new Vue({
+            el: '#editProcess',
+            data() {
+                return {
+                    formData: @json($process),
+                    dataGroups: [],
+                    value: [],
+                    errors: {
+                        name: null,
+                        description: null,
+                        category: null,
+                        status: null,
+                        screen: null
+                    }
+                }
+            },
+            methods: {
+                resetErrors() {
+                    this.errors = Object.assign({}, {
+                        name: null,
+                        description: null,
+                        category: null,
+                        status: null,
+                        screen: null
+                    });
+                },
+                onClose() {
+                    window.location.href = '/processes';
+                },
+                onUpdate() {
+                    this.resetErrors();
+                    let that = this;
+
+                    //if the summary screen id is not a number (e.g. null string)
+                    // is set to null
+                    this.formData.summary_screen_id =
+                        isNaN(this.formData.summary_screen_id)
+                            ? null
+                            : this.formData.summary_screen_id;
+
+                    ProcessMaker.apiClient.put('processes/' + that.formData.id, that.formData)
+                        .then(response => {
+                            ProcessMaker.alert('{{__('Update User Successfully')}}', 'success');
+                            that.onClose();
+                        })
+                        .catch(error => {
+                            //define how display errors
+                            if (error.response.status && error.response.status === 422) {
+                                // Validation error
+                                that.errors = error.response.data.errors;
+                            }
+                        });
                 }
             }
-        },
-        methods: {
-            resetErrors() {
-                this.errors = Object.assign({}, {
-                    name: null,
-                    description: null,
-                    category: null,
-                    status: null,
-                    screen: null
-                });
-            },
-            onClose() {
-                window.location.href = '/processes';
-            },
-            onUpdate() {
-                this.resetErrors();
-                let that = this;
-
-                //if the summary screen id is not a number (e.g. null string)
-                // is set to null
-                this.formData.summary_screen_id =
-                    isNaN(this.formData.summary_screen_id)
-                        ? null
-                        : this.formData.summary_screen_id;
-
-                ProcessMaker.apiClient.put('processes/' + that.formData.id, that.formData)
-                    .then(response => {
-                        ProcessMaker.alert('{{__('Update User Successfully')}}', 'success');
-                        that.onClose();
-                    })
-                    .catch(error => {
-                    //define how display errors
-                        if (error.response.status && error.response.status === 422) {
-                            // Validation error
-                            that.errors = error.response.data.errors;
-                        }
-                    });
-            }
-        }
-    });
-</script>
+        });
+    </script>
 @endsection
