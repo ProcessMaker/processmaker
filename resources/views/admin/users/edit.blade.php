eab5c4cd729e0bc840050f2ecee88a6c561b33a2
Mila Endo <milal.alim@gmail.com>
2018-11-27 14:52:55 -0800

    rm lorem ipsum

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -127,15 +127,6 @@
                     </div>
                 </div>
             </div>
-            <div class="col-4">
-                <div class="card card-body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </div>
-            </div>
         </div>
     </div>
 @endsection
