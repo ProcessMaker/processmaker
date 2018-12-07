2d9e2ec01f54c9f21ec7c2649d1361b906dbdb40
Mila Endo <milal.alim@gmail.com>
2018-12-07 13:40:17 -0800

    inital for file-download

---
+++ resources/js/processes/screen-builder/components/file-download.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <i class="fas fa-download"></i>
+    Download {{file}}
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      file: ""
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
