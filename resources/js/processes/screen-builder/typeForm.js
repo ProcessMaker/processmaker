148d3fe26d698d7d615e7e841a34ed7026b52e72
Mila Endo <milal.alim@gmail.com>
2018-12-11 11:20:56 -0800

    downloader added in form

--- resources/js/processes/screen-builder/typeForm.js
+++ resources/js/processes/screen-builder/typeForm.js
@@ -1,8 +1,10 @@
 import initialControls from "@processmaker/vue-form-builder/src/form-builder-controls";
+import FormText from "@processmaker/vue-form-builder/src/components/renderer/form-text";
+import FileDownload from "./components/file-download"
 
 import FileUpload from "./components/form/file-upload"
 
-initialControls[initialControls.length] = {
+initialControls.push({
     rendererComponent: FileUpload,
     rendererBinding: 'FileUpload',
     builderComponent: FileUpload,
@@ -33,7 +35,39 @@ initialControls[initialControls.length] = {
             }
         ]
     }
-}
+})
+initialControls.push({
+    rendererComponent: FormText,
+    rendererBinding: 'FormText',
+    builderComponent: FileDownload,
+    builderBinding: 'FileDownload',
+    control: {
+        label: 'File Download',
+        component: 'FileDownload',
+        'editor-component': 'FormText',
+        'editor-icon': require('./components/file-download.png'),
+        config: {
+            label: 'New File Download'
+        },
+        inspector: [{
+                type: "FormInput",
+                field: "label",
+                config: {
+                    label: "Text Label",
+                    helper: "The text to display",
+                }
+            },
+            {
+                type: "FormInput",
+                field: "name",
+                config: {
+                    label: 'Download Name',
+                    helper: "The name of the Download",
+                }
+            }
+        ]
+    }
+})
 
 ProcessMaker.EventBus.$on('screen-builder-init', (manager) => {
     for (var i = 0; i < initialControls.length; i++) {
@@ -45,4 +79,4 @@ ProcessMaker.EventBus.$on('screen-builder-init', (manager) => {
             initialControls[i].builderBinding
         );
     }
-});
+}, );
