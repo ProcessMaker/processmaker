77e8b0dc1acac0372bff698b15edf8b1438543c2
David Callizaya <davidcallizaya@gmail.com>
2018-12-21 12:21:33 -0400

    Improve assignment code.

--- ProcessMaker/Traits/ProcessTaskAssignmentsTrait.php
+++ ProcessMaker/Traits/ProcessTaskAssignmentsTrait.php
@@ -2,6 +2,7 @@
 
 namespace ProcessMaker\Traits;
 
+use DOMElement;
 use ProcessMaker\Models\Process;
 use ProcessMaker\Models\User;
 use ProcessMaker\Providers\WorkflowServiceProvider as PM;
@@ -19,35 +20,45 @@ trait ProcessTaskAssignmentsTrait
      */
     public static function bootProcessTaskAssignmentsTrait()
     {
-        Process::saved(function(Process $process) {
-            $process->assignments()->delete();
-            $definitions = $process->getDefinitions();
-            if ($definitions) {
-                $assignments = [];
-                foreach ($definitions->getElementsByTagName('task') as $node) {
-                    $assignment = $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignment');
-                    $users = explode(',', $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignedUsers'));
-                    if ($assignment === 'user') {
-                        $assignments[] = [
-                            'process_task_id' => $node->getAttribute('id'),
-                            'assignment_id' => $users[0],
-                            'assignment_type' => User::class,
-                        ];
-                    }
-                }
-                foreach ($definitions->getElementsByTagName('userTask') as $node) {
-                    $assignment = $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignment');
-                    $users = explode(',', $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignedUsers'));
-                    if ($assignment === 'user') {
-                        $assignments[] = [
-                            'process_task_id' => $node->getAttribute('id'),
-                            'assignment_id' => $users[0],
-                            'assignment_type' => User::class,
-                        ];
-                    }
-                }
-                $process->assignments()->createMany($assignments);
+        static::saved([static::class, 'updateTaskAssignments']);
+    }
+
+    public static function updateTaskAssignments(Process $process)
+    {
+        $process->assignments()->delete();
+        $definitions = $process->getDefinitions();
+        if ($definitions) {
+            $assignments = [];
+            foreach ($definitions->getElementsByTagName('task') as $node) {
+                $assignments = static::setAssignments($node, $assignments);
             }
-        });
+            foreach ($definitions->getElementsByTagName('userTask') as $node) {
+                $assignments = static::setAssignments($node, $assignments);
+            }
+            $process->assignments()->createMany($assignments);
+        }
+    }
+
+    /**
+     * Populates the assignments array.
+     *
+     * @param DOMElement $node
+     * @param array $assignments
+     *
+     * @return array
+     */
+    private static function setAssignments(DOMElement $node, array $assignments)
+    {
+        $assignment = $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignment');
+        $users = explode(',',
+            $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignedUsers'));
+        if ($assignment === 'user') {
+            $assignments[] = [
+                'process_task_id' => $node->getAttribute('id'),
+                'assignment_id' => $users[0],
+                'assignment_type' => User::class,
+            ];
+        }
+        return $assignments;
     }
 }
