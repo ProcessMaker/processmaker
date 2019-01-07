f5f3f851c97dd872d7ab9acee6ee6ee7d35785c6
Ryan Cooley <ryancooley@gmail.com>
2019-01-07 14:21:45 -0800

    Removed old permissions tables & replaced

---
+++ database/migrations/2019_01_07_221241_recreate_permission_assignments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RecreatePermissionAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_assignments', function (Blueprint $table) {
+          $table->increments('id');
+          $table->unsignedInteger('permission_id');
+          $table->morphs('assignable');
+          $table->timestamps();
+
+          $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('permission_assignments');
+    }
+}
