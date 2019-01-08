dbec1fd29e5692f133a5aeb461f58ce2b2c1040e
Ryan Cooley <ryancooley@gmail.com>
2019-01-08 10:56:35 -0800

    Cleaning up AuthServiceProvider, adding new gates

--- ProcessMaker/Providers/AuthServiceProvider.php
+++ ProcessMaker/Providers/AuthServiceProvider.php
@@ -2,36 +2,13 @@
 
 namespace ProcessMaker\Providers;
 
+use ProcessMaker\Models\Permission;
 use Illuminate\Auth\RequestGuard;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
-use ProcessMaker\Model\Application;
-use ProcessMaker\Model\Screen;
-use ProcessMaker\Model\Delegation;
-use ProcessMaker\Model\InputDocument;
-use ProcessMaker\Model\OutputDocument;
-use ProcessMaker\Model\PmTable;
-use ProcessMaker\Model\Process;
-use ProcessMaker\Model\ProcessCategory;
-use ProcessMaker\Model\ProcessVariable;
-use ProcessMaker\Model\ReportTable;
-use ProcessMaker\Model\Role;
-use ProcessMaker\Model\Task;
-use ProcessMaker\Model\TaskUser;
-use ProcessMaker\Model\Script;
-use ProcessMaker\Policies\ApplicationPolicy;
-use ProcessMaker\Policies\AssigneeTaskPolicy;
-use ProcessMaker\Policies\FormPolicy;
-use ProcessMaker\Policies\InputDocumentPolicy;
-use ProcessMaker\Policies\OutputDocumentPolicy;
-use ProcessMaker\Policies\PmTablePolicy;
-use ProcessMaker\Policies\ProcessCategoryPolicy;
-use ProcessMaker\Policies\ProcessPolicy;
-use ProcessMaker\Policies\ProcessVariablePolicy;
-use ProcessMaker\Policies\ReportTablePolicy;
-use ProcessMaker\Policies\TaskPolicy;
+use ProcessMaker\Models\Script;
 use ProcessMaker\Policies\ScriptPolicy;
 
 /**
@@ -46,7 +23,8 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array
      */
-    protected $policies = [
+    protected $policies = [        
+        Script::class => ScriptPolicy::class,
     ];
 
     /**
@@ -58,6 +36,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        
+        $permissions = Permission::all();
+        
+        $permissions->each(function($permission) {
+            Gate::define($permission->name, function ($user, $model = false) use($permission) {
+                return $user->hasPermission($permission->name);
+            });
+        });
     }
 
 }
