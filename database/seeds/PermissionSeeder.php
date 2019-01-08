573f8f194a32b9a3386184a8541d190bd1262466
Ryan Cooley <ryancooley@gmail.com>
2019-01-08 10:21:50 -0800

    Updating Permission factory and seeder

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -11,58 +11,10 @@ class PermissionSeeder extends Seeder
 {
 
     private $permissions = [
-        'documents.index',
-        'documents.create',
-        'documents.destroy',
-        'documents.edit',
-        'documents.show',
-        'environment_variables.index',
-        'environment_variables.create',
-        'environment_variables.destroy',
-        'environment_variables.edit',
-        'environment_variables.show',
-        'screens.index',
-        'screens.create',
-        'screens.destroy',
-        'screens.edit',
-        'screens.show',
-        'group_members.index',
-        'group_members.create',
-        'group_members.destroy',
-        'group_members.edit',
-        'group_members.show',
-        'groups.index',
-        'groups.create',
-        'groups.destroy',
-        'groups.edit',
-        'groups.show',
-        'process_categories.index',
-        'process_categories.create',
-        'process_categories.destroy',
-        'process_categories.edit',
-        'process_categories.show',
-        'processes.index',
-        'processes.create',
-        'processes.destroy',
-        'processes.edit',
-        'processes.show',
-        'requests.index',	
-        'requests.create',	
-        'requests.destroy',	
-        'requests.cancel',	
-        'requests.edit',	
-        'requests.show',
-        'scripts.index',
-        'scripts.create',
-        'scripts.destroy',
-        'scripts.edit',
-        'scripts.show',
-        'users.index',
-        'users.create',
-        'users.destroy',
-        'users.edit',
-        'users.show',
-        'modeler.show'
+        'view-script',
+        'create-script',
+        'edit-script',
+        'delete-script',
     ];
 
     private $resourcePermissions = [
@@ -89,18 +41,12 @@ class PermissionSeeder extends Seeder
         ]);
 
         foreach ($this->permissions as $permissionString) {
-            $type = 'ROUTE';
-            $resource = explode('.', $permissionString)[0];
-            if (in_array($resource, $this->resourcePermissions)) {
-                $type = 'RESOURCE';
-            }
-
             $permission = factory(Permission::class)->create([
-                'name' => ucwords(preg_replace('/(\.|_)/', ' ',
+                'title' => ucwords(preg_replace('/(\-|_)/', ' ',
                         $permissionString)),
-                'type' => $type,
-                'guard_name' => $permissionString,
+                'name' => $permissionString,
             ]);
+            
             factory(PermissionAssignment::class)->create([
                 'permission_id' => $permission->id,
                 'assignable_type' => Group::class,
