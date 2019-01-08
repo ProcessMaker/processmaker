a57d8741c3eca4b2e742b01b0eafeebc21c56517
David Callizaya <davidcallizaya@gmail.com>
2019-01-08 16:47:32 -0400

    Execution of assignment rules

--- ProcessMaker/Traits/ProcessTaskAssignmentsTrait.php
+++ ProcessMaker/Traits/ProcessTaskAssignmentsTrait.php
@@ -26,6 +26,9 @@ trait ProcessTaskAssignmentsTrait
 
     public static function updateTaskAssignments(Process $process)
     {
+        if (!$process->exists) {
+            return;
+        }
         $process->assignments()->delete();
         $definitions = $process->getDefinitions(true);
         if ($definitions) {
