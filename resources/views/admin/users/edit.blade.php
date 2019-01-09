10a2dba7c50192189daa96447068bd038016731d
Mila Endo <milal.alim@gmail.com>
2019-01-09 14:46:57 -0800

    refactor to add sub view

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -247,24 +247,7 @@
                                     </div>
                                 </div>
                             </div>
-                            @foreach($groupedPermissions as $groupTitle => $permissions)
-                            <div class="card">
-                                <div class="card-header">
-                                    <h5 class="mb-0">
-                                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#{{camel_case($groupTitle)}}">
-                                            {{ $groupTitle }}
-                                        </button>
-                                    </h5>
-                                </div>
-                                <div id="{{camel_case($groupTitle)}}" class="collapse" >
-                                    <div class="card-body">
-                                        @foreach ($permissions as $permission)
-                                        <div><input type="checkbox" name="{{$groupTitle}}" value="{{$permission}}">  {{ $permission->title }}</div>
-                                        @endforeach
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
+                           @include('admin.shared.permissions')
                         </div>
                         <div class="text-right mt-2">
                             {!! Form::button('Cancel', ['class'=>'btn btn-outline-success', '@click' => 'onClose'])!!}
