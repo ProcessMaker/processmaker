6d38bcb61212b7e22c96e58c55de4602ed32b485
Mila Endo <milal.alim@gmail.com>
2019-01-09 14:45:18 -0800

    subview added for permissions

---
+++ resources/views/admin/shared/permissions.blade.php
@@ -0,0 +1,18 @@
+ @foreach($groupedPermissions as $groupTitle => $permissions)
+    <div class="card">
+        <div class="card-header">
+            <h5 class="mb-0">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#{{camel_case($groupTitle)}}">
+                    {{ $groupTitle }}
+                </button>
+            </h5>
+        </div>
+        <div id="{{camel_case($groupTitle)}}" class="collapse" >
+            <div class="card-body">
+                @foreach ($permissions as $permission)
+                <div><input type="checkbox" name="{{$groupTitle}}" value="{{$permission}}">  {{ $permission->title }}</div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endforeach
