69bdcd105e8e619864c22443cc641a82547f334e
Ryan Cooley <ryancooley@gmail.com>
2019-01-08 16:01:07 -0800

    Adding static methods to Permission model

--- ProcessMaker/Models/Permission.php
+++ ProcessMaker/Models/Permission.php
@@ -12,6 +12,29 @@ class Permission extends Model
         'title',
         'name',
     ];
+    
+    static public function for($resource)
+    {
+        return self::byResource($resource)->pluck('name');
+    }
+    
+    static public function byResource($resource)
+    {
+        //Grab all of our permissions
+        $all = self::all();
+        
+        //Filter them by the name of the resource
+        $filtered = $all->filter(function ($value, $key) use($resource) {
+            $match = preg_match("/(.+)-{$resource}/", $value->name);
+            if ($match === 1) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        
+        return $filtered;
+    }
 
     static public function byName($name)
     {
