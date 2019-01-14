05c77f8ec73bcae325c052bed3e5eca9691e9c98
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-01-14 16:30:18 -0400

    Add table comments

---
+++ database/migrations/2019_01_14_201209_create_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->morphs('comment');
+            $table->text('subject')->nullable();
+            $table->text('body')->nullable();
+            $table->boolean('hidden')->default(false);
+            $table->enum('type', ['LOG', 'MESSAGE']);
+            $table->timestamps();
+
+            $table->index('user_id');
+            $table->index(['comment_id','comment_type']);
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
