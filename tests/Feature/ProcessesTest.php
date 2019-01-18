8e5a1e72cd699c84631634be1212be26b532d075
Ryan Cooley <ryancooley@gmail.com>
2019-01-17 16:28:20 -0800

    Updating process permissions tests

--- tests/Feature/ProcessesTest.php
+++ tests/Feature/ProcessesTest.php
@@ -54,15 +54,6 @@ class ProcessesTest extends TestCase
         $response->assertSee('Processes');
     }
 
-    public function testIndexWithOutPermission()
-    {
-        $this->user = factory(User::class)->create([
-            'is_administrator' => false,
-        ]);
-        $response = $this->webGet('/processes');
-        $response->assertStatus(403);
-    }
-
     public function testEdit()
     {
         $process = factory(Process::class)->create(['name' => 'Test Edit']);
@@ -89,79 +80,112 @@ class ProcessesTest extends TestCase
         $response->assertSee('Test Edit');
     }
 
-    public function testEditWithOutPermission()
-    {
-        
-        $this->user = factory(User::class)->create([
-            'is_administrator' => false,
-        ]);
-        (new \PermissionSeeder())->run($this->user);
-
-        $process = factory(Process::class)->create(['name' => 'Test Edit']);
-        $response = $this->webGet('processes/' . $process->id . '/edit');
-        $response->assertStatus(403);
-    }
-
     public function testCreate()
     {
         $process = factory(Process::class)->create(['name' => 'Test Create']);
-        $response = $this->webGet('/processes/create');
-        $response->assertViewIs('processes.create');
-        $response->assertStatus(200);
-    }
 
-    public function testCreateWithOutPermission()
-    {
-        
         $this->user = factory(User::class)->create([
             'is_administrator' => false,
         ]);
-        $process = factory(Process::class)->create(['name' => 'Test Create']);
-        $response = $this->webGet('/processes/create');
+        // Set the URL & permission to test.
+        $url = 'processes/create';
+        $permission = 'create-processes';
+        
+        // Our user has no permissions, so this should return 403.
+        $response = $this->webCall('GET', $url);
         $response->assertStatus(403);
-    }
 
+        // Attach the permission to our user.
+        $this->user->permissions()->attach(Permission::byName($permission)->id);
+        $this->user->refresh();
+
+        // Our user now has permissions, so this should return 200.
+        $response = $this->webCall('GET', $url);
+        $response->assertStatus(200);
+        $response->assertViewIs('processes.create');
+    }
+    
     public function testStore()
     {
-        $this->withoutExceptionHandling();
-        $response = $this->webCall('POST', '/processes', [
-            'name' => 'Stored new user',
-            'description' => 'descript',
-            'status' => 'ACTIVE'
+        $process = factory(Process::class)->create(['name' => 'Test Edit']);
+
+        $this->user = factory(User::class)->create([
+            'is_administrator' => false,
         ]);
+        // Set the URL, permission, and data with which to test.
+        $url = 'processes';
+        $permission = 'edit-processes';
+        $data = [
+            'name' => 'Stored new process',
+            'description' => 'My description',
+            'status' => 'ACTIVE'
+        ];
+        
+        // Our user has no permissions, so this should return 403.
+        $response = $this->webCall('POST', $url, $data);
+        $response->assertStatus(403);
 
+        // Attach the permission to our user.
+        $this->user->permissions()->attach(Permission::byName($permission)->id);
+        $this->user->refresh();
+
+        // Our user now has permissions, so this should return 200.
+        $response = $this->webCall('POST', $url, $data);
         $response->assertStatus(302);
-        $this->assertDatabaseHas('processes', ['name' => 'Stored new user']);  // how do I verify DB table name?
-
+        $this->assertDatabaseHas('processes', ['name' => 'Stored new process']);
     }
-
+    
     public function testUpdate()
     {
         $process = factory(Process::class)->create(['name' => 'Test Update']);
-        $response = $this->webCall('PUT', 'processes/' . $process->id . '', [
-            'name' => 'Update Name',
-            'description' => 'Descriptionnnnn'
-        ]);
-        $this->assertDatabaseHas('processes', ['name' => 'Update Name']);
-        $response->assertRedirect('/processes');
-    }
 
-    public function testDestroy()
-    {
-        $process = factory(Process::class)->create();
-        $response = $this->webCall('DELETE', 'processes/' . $process->id . '');
-        $this->assertDatabaseMissing('processes', ['id' => $process->id, 'deleted_at' => null]);
-        $response->assertRedirect('/processes');
-    }
-
-    public function testArchiveWithOutPermission()
-    {
-        
         $this->user = factory(User::class)->create([
             'is_administrator' => false,
         ]);
-        $process = factory(Process::class)->create();
-        $response = $this->webCall('DELETE', 'processes/' . $process->id . '');
+        // Set the URL, permission, and data with which to test.
+        $url = 'processes/' . $process->id;
+        $permission = 'edit-processes';
+        $data = [
+            'name' => 'Updated Name',
+            'description' => 'My description',
+        ];
+        
+        // Our user has no permissions, so this should return 403.
+        $response = $this->webCall('PUT', $url, $data);
         $response->assertStatus(403);
+
+        // Attach the permission to our user.
+        $this->user->permissions()->attach(Permission::byName($permission)->id);
+        $this->user->refresh();
+
+        // Our user now has permissions, so this should return 200.
+        $response = $this->webCall('PUT', $url, $data);
+        $response->assertRedirect('/processes');
+        $this->assertDatabaseHas('processes', ['name' => 'Updated Name']);
+    }
+
+    public function testArchive()
+    {
+        $process = factory(Process::class)->create(['name' => 'Test Archive']);
+
+        $this->user = factory(User::class)->create([
+            'is_administrator' => false,
+        ]);
+        // Set the URL & permission to test.
+        $url = 'processes/' . $process->id;
+        $permission = 'archive-processes';
+
+        // Our user has no permissions, so this should return 403.
+        $response = $this->webCall('DELETE', $url);
+        $response->assertStatus(403);
+
+        // Attach the permission to our user.
+        $this->user->permissions()->attach(Permission::byName($permission)->id);
+        $this->user->refresh();
+
+        // Our user now has permissions, so this should return 200.
+        $response = $this->webCall('DELETE', $url);
+        $response->assertRedirect('/processes');
+        $this->assertDatabaseMissing('processes', ['id' => $process->id, 'deleted_at' => null]);
     }
 }
