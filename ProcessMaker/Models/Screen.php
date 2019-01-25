61be7519e90be23b6c9072bee65313d59334d11a
Ryan Cooley <ryan.cooley@processmaker.com>
2019-01-25 15:04:51 -0800

    Fix Screen validation rules

--- ProcessMaker/Models/Screen.php
+++ ProcessMaker/Models/Screen.php
@@ -66,21 +66,13 @@ class Screen extends Model
      */
     public static function rules($existing = null)
     {
-        $rules = [];
-        if ($existing) {
-            //ignore the unique rule for this id
-            $rules['title'] = [
-                'required',
-                Rule::unique('screens')->ignore($existing->id, 'id')
-            ];
-        } else {
-            $rules = [
-                'title' => 'required|unique:screens,title',
-                'description' => 'required',
-                'type' => 'required'
-            ];
-        }
-        return $rules;
+        $unique = Rule::unique('screens')->ignore($existing);
+
+        return [
+            'title' => ['required', $unique],
+            'description' => 'required',
+            'type' => 'required'
+        ];
     }
 
     /**
