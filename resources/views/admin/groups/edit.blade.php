a0ec9798a56554f4593fe1c4c8650761e944e56d
Ryan Cooley <ryan.cooley@processmaker.com>
2019-01-30 08:45:10 -0800

    Removing old references to disabledForNow

--- resources/views/admin/groups/edit.blade.php
+++ resources/views/admin/groups/edit.blade.php
@@ -127,8 +127,7 @@
                         <div class="modal-footer">
                             <button type="button" class="btn btn-outline-success"
                                     data-dismiss="modal" @click="onCloseAddUser">{{__('Close')}}</button>
-                            <button type="button" class="btn btn-success ml-2" @click="onSave"
-                                    id="disabledForNow">{{__('Save')}}</button>
+                            <button type="button" class="btn btn-success ml-2" @click="onSave">{{__('Save')}}</button>
                         </div>
                     </div>
                 </div>
