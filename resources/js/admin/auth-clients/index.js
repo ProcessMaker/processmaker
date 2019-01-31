45342f1b63a180e59227c335bb9fb562faff06b0
Nolan Ehrstrom <github@nolanpro.com>
2019-01-30 18:10:45 -0800

    Refactor auth clients modal

--- resources/js/admin/auth-clients/index.js
+++ resources/js/admin/auth-clients/index.js
@@ -1,18 +1,3 @@
 import Vue from "vue";
 import AuthClientsListing from "./components/AuthClientsListing";
-
-new Vue({
-    el: "#listAuthClients",
-    components: {
-        AuthClientsListing
-    },
-    methods: {
-        resetValues() {
-            this.errors = {
-                name: null,
-                redirect: null
-            }
-        }
-    },
-
-});
+Vue.component('auth-clients-listing', AuthClientsListing);
--- resources/views/admin/auth-clients/index.blade.php
+++ resources/views/admin/auth-clients/index.blade.php
@@ -9,62 +9,57 @@
 @endsection
 
 @section('content')
-@include('shared.breadcrumbs', ['routes' => [
+    @include('shared.breadcrumbs', ['routes' => [
         __('Admin') => route('admin.index'),
         __('Auth Clients') => null,
     ]])
-    <div class="container page-content" id="listAuthClients">
-        <div class="row align-items-center">
-            <div class="col-8">
-                
-            </div>
-            <div class="col-4" align="right">
-                <button class="btn btn-secondary" type="button" class="btn btn-secondary" data-toggle="modal"
-                    data-target="#createAuthClient">
-                    <i class="fas fa-plus"></i>
-                    {{__('Auth Client')}}</a>
-                </button>
+    <div id="authClients">
+
+        <div class="modal" role="dialog" ref="createEditAuthClient" id="createEditAuthClient">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">{{__('Create Environment Variable')}}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            {!!Form::label('name', __('Name'))!!}
+                            {!!Form::text('name', null, ['class'=> 'form-control', 'v-model'=> 'authClient.name',
+                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.name}'])!!}
+                            <small class="form-text text-muted">{{ __('Name must be distinct') }}</small>
+                            <div class="invalid-feedback" v-if="errors.name">@{{ errors.name[0] }}</div>
+                        </div>
+                        <div class="form-group">
+                            {!!Form::label('redirect', __('Redirect URL'))!!}
+                            {!!Form::text('redirect', null, ['class'=> 'form-control', 'v-model'=> 'authClient.redirect',
+                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.redirect}','rows'=>3])!!}
+                            <div class="invalid-feedback" v-if="errors.redirect">@{{ errors.redirect[0] }}</div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary"
+                                data-dismiss="modal">{{__('Close')}}</button>
+                        <button type="button" class="btn btn-secondary ml-2" @click="save">{{__('Save')}}</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <auth-clients-listing ref="authClientList"/>
-    </div>
 
-    <div class="modal" tabindex="-1" role="dialog" id="createAuthClient">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">{{__('Create Environment Variable')}}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+        <div class="container page-content">
+            <div class="row">
+                <div class="col" align="right">
+                    <button class="btn btn-secondary" type="button" class="btn btn-secondary" data-toggle="modal" data-target="#createEditAuthClient">
+                        <i class="fas fa-plus"></i>
+                        {{__('Auth Client')}}</a>
                     </button>
                 </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        {!!Form::label('name', __('Name'))!!}
-                        {!!Form::text('name', null, ['class'=> 'form-control', 'v-model'=> 'authClient.name',
-                        'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.name}'])!!}
-                        <small class="form-text text-muted">{{ __('Name must be distinct') }}</small>
-                        <div class="invalid-feedback" v-if="errors.name">@{{ errors.name[0] }}</div>
-                    </div>
-                    <div class="form-group">
-                        {!!Form::label('redirect', __('Redirect URL'))!!}
-                        {!!Form::text('redirect', null, ['class'=> 'form-control', 'v-model'=> 'authClient.redirect',
-                        'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.redirect}','rows'=>3])!!}
-                        <div class="invalid-feedback" v-if="errors.redirect">@{{ errors.redirect[0] }}</div>
-                    </div>
-                    <div class="form-group">
-                        {!!Form::label('value', __('Value'))!!}
-                        {!!Form::text('value', null, ['class'=> 'form-control', 'v-model'=> 'authClient.secret'])!!}
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary"
-                            data-dismiss="modal">{{__('Close')}}</button>
-                    <button type="button" class="btn btn-secondary ml-2" @click="save">{{__('Save')}}</button>
-                </div>
             </div>
-
+            <auth-clients-listing ref="authClientList" @edit="edit"/>
         </div>
+
     </div>
 @endsection
 
@@ -73,20 +68,20 @@
 
     <script>
         new Vue({
-            el: '#createAuthClient',
+            el: '#authClients',
             data: {
-                authClient: {
-                    id: null,
-                    name: "",
-                    redirect: "",
-                    secret: "",
-                },
-                errors: {
-                    name: null,
-                    redirect: null,
-                },
+                authClient: null,
+                errors: null,
             },
-             methods: {
+            beforeMount() {
+                this.resetValues();
+            },
+            mounted() {
+                $('#createEditAuthClient').on('hidden.bs.modal', () => {
+                    this.resetValues();
+                });
+            },
+            methods: {
                 save() {
                     event.preventDefault()
                     this.loading = true
@@ -103,16 +98,29 @@
                         baseURL: '/',
                         data: this.authClient,
                     }).then(response => {
-                        this.$refs.createEditAuthClient.hide()
+                        $('#createEditAuthClient').modal('hide')
                         this.$refs.authClientList.fetch()
-                        this.resetValues();
                         this.loading = false
+                        ProcessMaker.alert("Auth Client Updated", "success")
                     }).catch(error => {
-                        this.errors = error.response.data.errors
+                        this.errors = error.response.data.errors;
                     });
                 },
                 resetValues() {
-                    this.errors = { name: null, redirect: null }
+                    this.authClient = {
+                        id: null,
+                        name: "",
+                        redirect: "",
+                        secret: ""
+                    };
+                    this.errors = {
+                        name: null,
+                        redirect: null
+                    }
+                },
+                edit(item) {
+                    this.authClient = item
+                    $('#createEditAuthClient').modal('show');
                 }
             },
         })
