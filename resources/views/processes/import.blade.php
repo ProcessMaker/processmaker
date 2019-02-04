106cc9408902767b9283913487ee3a56f435d54b
Mila Endo <milal.alim@gmail.com>
2019-02-04 13:15:45 -0800

    update import functionality

--- resources/views/processes/import.blade.php
+++ resources/views/processes/import.blade.php
@@ -13,7 +13,7 @@
         __('Processes') => route('processes.index'),
         __('Import') => null,
     ]])
-<div class="container" id="editProcess">
+<div class="container" id="importProcess">
     <div class="row">
         <div class="col">
             <div class="card text-center">
@@ -23,13 +23,35 @@
                 <div class="card-body">
                     <h5 class="card-title">You are about to import a Process</h5>
                     <p class="card-text">You will need to fix hecka stuff</p> 
+                    <input type="file" ref="file" class="d-none" @change="handleFile">
+                    <button @click="$refs.file.click()" class="btn btn-secondary ml-2">
+                        <i class="fas fa-upload"></i>
+                        Browse
+                    </button>
                 </div>
                 <div class="card-footer bg-light" align="right">
-                    <button type="button" class="btn btn-outline-secondary">Close</button>
-    			    <button type="button" class="btn btn-secondary ml-2" >Save</button>
+                    <button type="button" class="btn btn-outline-secondary">Cancel</button>
+    			    <button type="button" class="btn btn-secondary ml-2" >Import</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('js')
+    <script>
+        new Vue({
+            el: '#importProcess',
+            data: {
+                
+            },
+            methods: {
+                handleFile(e) {
+                    console.log(e.target.files[0])
+                     this.$emit('input', e.target.files[0])
+                }
+            }
+        })
+    </script>
 @endsection
