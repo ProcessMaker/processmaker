99603aa5ecc268de64c70a07224e862e36678083
dante <dante@danteloayza.localdomain>
2019-02-08 09:38:28 -0400

    Improve of error message when no user/group is selected

--- resources/lang/en/exceptions.php
+++ resources/lang/en/exceptions.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'TaskDoesNotHaveUsersException' => 'The task ":task" does not have any assigned user',
+    'TaskDoesNotHaveUsersException' => 'The task ":task" has an incomplete assignment. You should select one user or group.',
     'ScriptLanguageNotSupported' => 'The ":language" language is not supported',
     'SyntaxErrorException' => 'Syntax error. :error',
     'ExpressionFailedException' => 'Failed to evaluate expression. :error',
