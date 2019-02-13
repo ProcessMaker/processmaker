14913508db2ba9103dbdc94ee485d9448cbe5bc5
Nolan Ehrstrom <github@nolanpro.com>
2019-02-13 09:54:05 -0800

    Add OpenApi annotations for webhooks

--- ProcessMaker/Http/Controllers/Api/ProcessWebhookController.php
+++ ProcessMaker/Http/Controllers/Api/ProcessWebhookController.php
@@ -12,6 +12,43 @@ use Ramsey\Uuid\Uuid;
 
 class ProcessWebhookController extends Controller
 {
+    /**
+     * Display the specified webhook.
+     *
+     * @param Request $request
+     * @param Process $process
+     *
+     * @return \ProcessMaker\Http\Resources\ProcessWebhook
+     * 
+     * @OA\Get(
+     *     path="/processes/{process_id}/webhooks/",
+     *     summary="Get the webhook for a start node",
+     *     operationId="getProcessWebhook",
+     *     tags={"Process Webhooks"},
+     *     @OA\Parameter(
+     *         name="process_id",
+     *         description="ID of process",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="node",
+     *         description="Start event node ID",
+     *         in="query",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successfully found the webhook",
+     *     ),
+     * )
+     */
     public function show(Request $request, Process $process)
     {
         $node = $request->input('node');
@@ -23,6 +60,43 @@ class ProcessWebhookController extends Controller
         return new WebhookResource($result);
     }
 
+    /**
+     * Save a new webhook.
+     *
+     * @param Request $request
+     * @param Process $process
+     *
+     * @return \ProcessMaker\Http\Resources\ProcessWebhook
+     * 
+     * @OA\Post(
+     *     path="/processes/{process_id}/webhooks/",
+     *     summary="Save a new webhook for a start node",
+     *     operationId="createProcessWebhook",
+     *     tags={"Process Webhooks"},
+     *     @OA\Parameter(
+     *         name="process_id",
+     *         description="ID of process",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="node",
+     *         description="Start event node ID",
+     *         in="query",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successfully saved the webhook",
+     *     ),
+     * )
+     */
     public function store(Request $request, Process $process)
     {
         $result = ProcessWebhook::create([
@@ -33,6 +107,43 @@ class ProcessWebhookController extends Controller
         return new WebhookResource($result);
     }
 
+    /**
+     * Delete a webhook.
+     *
+     * @param Request $request
+     * @param Process $process
+     *
+     * @return \ProcessMaker\Http\Resources\ProcessWebhook
+     * 
+     * @OA\Delete(
+     *     path="/processes/{process_id}/webhooks/",
+     *     summary="Delete (revoke) a webhook for a start node",
+     *     operationId="deleteProcessWebhook",
+     *     tags={"Process Webhooks"},
+     *     @OA\Parameter(
+     *         name="process_id",
+     *         description="ID of process",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="node",
+     *         description="Start event node ID",
+     *         in="query",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Successfully deleted the webhook",
+     *     ),
+     * )
+     */
     public function destroy(Request $request, Process $process)
     {
         $node = $request->input('node');
--- ProcessMaker/Models/ProcessWebhook.php
+++ ProcessMaker/Models/ProcessWebhook.php
@@ -3,10 +3,30 @@
 namespace ProcessMaker\Models;
 
 use Illuminate\Database\Eloquent\Model;
-// use Illuminate\Validation\Rule;
-// use ProcessMaker\Models\Process;
-// use ProcessMaker\Traits\SerializeToIso8601;
-
+/**
+ * Represents a webhook for a process and start event node
+ *
+ * @property string $id
+ * @property integer $process_id
+ * @property string $node
+ * @property string $token
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $created_at
+ *
+ * @OA\Schema(
+ *   schema="ProcessWebhookEditable",
+ * ),
+ * @OA\Schema(
+ *   schema="ProcessWebhook",
+ *   allOf={@OA\Schema(ref="#/components/schemas/ProcessWebhookEditable")},
+ *   @OA\Property(property="id", type="integer", format="id"),
+ *   @OA\Property(property="process_id", type="integer", format="id"),
+ *   @OA\Property(property="node", type="string"),
+ *   @OA\Property(property="token", type="string"),
+ *   @OA\Property(property="created_at", type="string", format="date-time"),
+ *   @OA\Property(property="updated_at", type="string", format="date-time"),
+ * )
+ */
 class ProcessWebhook extends Model
 {
     protected $fillable = [
