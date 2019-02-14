f9cb94def371d2afb36eb6721e7a99291b2758e6
dante <dante@danteloayza.localdomain>
2019-02-14 17:01:20 -0400

    Process Timer events trait

--- ProcessMaker/Traits/ProcessTimerEventsTrait.php
+++ ProcessMaker/Traits/ProcessTimerEventsTrait.php
@@ -3,7 +3,9 @@
 namespace ProcessMaker\Traits;
 
 use DOMElement;
+use Illuminate\Support\Facades\Log;
 use ProcessMaker\Exception\TaskDoesNotHaveUsersException;
+use ProcessMaker\Managers\TaskSchedulerManager;
 use ProcessMaker\Models\Process;
 use ProcessMaker\Models\User;
 use ProcessMaker\Models\Group;
@@ -14,70 +16,18 @@ use ProcessMaker\Providers\WorkflowServiceProvider as PM;
  *
  * @package ProcessMaker\Traits
  */
-trait ProcessTaskAssignmentsTrait
+trait ProcessTimerEventsTrait
 {
 
-    /**
-     * Create the process task assignments.
-     */
-    public static function bootProcessTaskAssignmentsTrait()
+    public static function bootProcessTimerEventsTrait()
     {
-        static::saving([static::class, 'updateTaskAssignments']);
+        static::saved([static::class, 'saveStartEvents']);
     }
 
-    public static function updateTaskAssignments(Process $process)
+    public static function saveStartEvents(Process $process)
     {
-        if (!$process->exists) {
-            return;
-        }
-        $process->assignments()->delete();
-        $definitions = $process->getDefinitions(true);
-        if ($definitions) {
-            $assignments = [];
-            foreach ($definitions->getElementsByTagName('task') as $node) {
-                $assignments = static::setAssignments($node, $assignments);
-            }
-            foreach ($definitions->getElementsByTagName('userTask') as $node) {
-                $assignments = static::setAssignments($node, $assignments);
-            }
-            $process->assignments()->createMany($assignments);
-        }
-    }
-
-    /**
-     * Populates the assignments array.
-     *
-     * @param DOMElement $node
-     * @param array $assignments
-     *
-     * @return array
-     */
-    private static function setAssignments(DOMElement $node, array $assignments)
-    {
-        $assignment = $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignment');
-        if ($assignment === 'user') {
-            $users = explode(',',
-                $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignedUsers'));
-            if (empty($users[0])) {
-                throw new TaskDoesNotHaveUsersException($node->getAttribute('id'));
-            }
-            $assignments[] = [
-                'process_task_id' => $node->getAttribute('id'),
-                'assignment_id' => $users[0],
-                'assignment_type' => User::class,
-            ];
-
-        } elseif ($assignment === 'group') {
-            $group = $node->getAttributeNS(PM::PROCESS_MAKER_NS, 'assignedGroups');
-            if (empty($group)) {
-                throw new TaskDoesNotHaveUsersException($node->getAttribute('id'));
-            }
-            $assignments[] = [
-                'process_task_id' => $node->getAttribute('id'),
-                'assignment_id' => $group,
-                'assignment_type' => Group::class,
-            ];
-        }
-        return $assignments;
+        Log::info('pepepepepep');
+        $manager = new TaskSchedulerManager();
+        $manager->registerTimerEvents($process);
     }
 }
