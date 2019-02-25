1af5058264bb2c2172faebca238c38e92716a2ca
Ryan Cooley <ryan.cooley@processmaker.com>
2019-02-25 10:27:19 -0800

    Send notifications
    - Request started
    - Request canceled
    - Request completed
    - Task assigned
    - Task completed

---
+++ ProcessMaker/Notifications/ActivityCompletedNotification.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace ProcessMaker\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use ProcessMaker\Models\Process;
+use ProcessMaker\Models\ProcessRequestToken as Token;
+use ProcessMaker\Nayra\Contracts\Bpmn\TokenInterface;
+
+class ActivityCompletedNotification extends Notification
+{
+    use Queueable;
+
+    private $processUid;
+    private $instanceUid;
+    private $tokenUid;
+    private $tokenElement;
+    private $tokenStatus;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(TokenInterface $token)
+    {
+        $this->processUid = $token->processRequest->process->getKey();
+        $this->instanceUid = $token->processRequest->getKey();
+        $this->tokenUid = $token->getKey();
+        $this->tokenElement = $token->element_id;
+        $this->tokenStatus = $token->status;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['broadcast', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the database representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return $this->toArray($notifiable);
+    }
+
+    /*
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        $process = Process::find($this->processUid);
+        $definitions = $process->getDefinitions();
+        $activity = $definitions->getActivity($this->tokenElement);
+        $token = Token::find($this->tokenUid);
+        $request = $token->processRequest;
+        return [
+            'type' => 'TASK_COMPLETED' ,
+            'message' => sprintf('Task completed: %s', $activity->getName()),
+            'name' => $activity->getName(),
+            'processName' => $process->name,
+            'request_id' => $request->getKey(),
+            'userName' => $token->user->getFullName(),
+            'user_id' => $token->user->id,
+            'dateTime' => $token->completed_at->toIso8601String(),
+            'uid' => $this->tokenUid,
+            'url' => sprintf(
+                '/tasks/%s/edit',
+                $token->id
+            )
+        ];
+    }
+
+    /*
+     * Get the broadcast representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage($this->toArray($notifiable));
+    }
+
+}
