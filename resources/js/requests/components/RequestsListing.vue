a935d421d6ef80cb8bd98b79dd90801e09c0ac15
Mila Endo <milal.alim@gmail.com>
2019-03-05 10:35:20 -0800

    translations for requests and tasks

--- resources/js/requests/components/RequestsListing.vue
+++ resources/js/requests/components/RequestsListing.vue
@@ -32,7 +32,7 @@
                 variant="link"
                 @click="onAction('edit-designer', props.rowData, props.rowIndex)"
                 v-b-tooltip.hover
-                title="Open Request"
+                :title="__('Open Request')"
               >
                 <i class="fas fa-caret-square-right fa-lg fa-fw"></i>
               </b-btn>
@@ -56,6 +56,7 @@
 import datatableMixin from "../../components/common/mixins/datatable";
 import AvatarImage from "../../components/AvatarImage";
 import moment from "moment";
+import __ from "../../modules/lang";
 
 Vue.component("avatar-image", AvatarImage);
 
@@ -76,26 +77,26 @@ export default {
       fields: [
         {
           name: "__slot:ids",
-          title: "Name",
+          title: __("Name"),
           field: "id",
           sortField: "id"
         },
         {
-          title: "Status",
+          title: __("Status"),
           name: "status",
           sortField: "status"
         },
         {
-          title: "Participants",
+          title: __("Participants"),
           name: "__slot:participants"
         },
         {
-          title: "Started",
+          title: __("Started"),
           name: "initiated_at",
           sortField: "initiated_at"
         },
         {
-          title: "Completed",
+          title: __("Completed"),
           name: "completed_at",
           sortField: "completed_at"
         },
@@ -107,6 +108,9 @@ export default {
     };
   },
   methods: {
+    __(variable) {
+      return __(variable);
+    },
     onAction(action, data, index) {
       switch (action) {
         case "edit-designer":
