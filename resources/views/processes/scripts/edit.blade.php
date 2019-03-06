896d7e26263149b66e92bf7b327171c40f4861e6
Mila Endo <milal.alim@gmail.com>
2019-03-06 11:27:23 -0800

    new select for users

--- resources/views/processes/scripts/edit.blade.php
+++ resources/views/processes/scripts/edit.blade.php
@@ -33,6 +33,10 @@
                         <div class="invalid-feedback" v-for="language in errors.language">@{{language}}</div>
                     </div>
                     <div class="form-group">
+                        <label class="typo__label">{{__('Run script as')}}</label>
+                        <multiselect v-model="selectedUser" label="fullname" :options="options" :searchable="true"></multiselect>
+                    </div>
+                    <div class="form-group">
                         {!! Form::label('description', 'Description') !!}
                         {!! Form::textarea('description', null, ['id' => 'description', 'rows' => 4, 'class'=> 'form-control',
                         'v-model' => 'formData.description', 'v-bind:class' => '{"form-control":true, "is-invalid":errors.description}']) !!}
@@ -57,6 +61,8 @@
             data() {
                 return {
                     formData: @json($script),
+                    options:@json($users),
+                    selectedUser:"",
                     errors: {
                         'title': null,
                         'language': null,
