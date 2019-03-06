f568e71916bc73f986eb7130c980c03518771bef
Nolan Ehrstrom <github@nolanpro.com>
2019-03-06 14:26:17 -0800

    Add constant for API_HOST when generating the swagger doc

--- ProcessMaker/Http/Controllers/Api/swagger-v3.php
+++ ProcessMaker/Http/Controllers/Api/swagger-v3.php
@@ -13,7 +13,7 @@
  *             url="http://www.apache.org/licenses/LICENSE-2.0.html"
  *         )
  *     ),
- *     @OA\Server(url="/api/1.0"),
+ *     @OA\Server(url=API_HOST),
  *     @OA\Components(
  *         @OA\Parameter(
  *             parameter="filter",
