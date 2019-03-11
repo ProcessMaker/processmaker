c6184347f2fdac26d034cb8d6e806fa254bba552
Ryan Cooley <ryan.cooley@processmaker.com>
2019-03-11 13:13:47 -0700

    Adding name & mime type to script config

--- config/script-runners.php
+++ config/script-runners.php
@@ -10,10 +10,14 @@ return [
     |
     */
     'php' => [
+        'name' => 'PHP',
+        'mime_type' => 'application/x-php',
         'runner' => ProcessMaker\ScriptRunners\PhpRunner::class,
         'image' => env('SCRIPTS_PHP_IMAGE', 'processmaker/executor:php'),
     ],
     'lua' => [
+        'name' => 'Lua',
+        'mime_type' => 'application/x-lua',
         'runner' => ProcessMaker\ScriptRunners\LuaRunner::class,
         'image' => env('SCRIPTS_LUA_IMAGE', 'processmaker/executor:lua'),
     ],
