df7608520bf4490025fcc4f44177ff0c161e7731
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-03-11 16:07:56 -0400

    git validation single click- import process

--- resources/views/processes/import.blade.php
+++ resources/views/processes/import.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.layout')
 
 @section('title')
-{{__('Import Process')}}
+    {{__('Import Process')}}
 @endsection
 
 @section('sidebar')
-@include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_processes')])
+    @include('layouts.sidebar', ['sidebar'=> Menu::get('sidebar_processes')])
 @endsection
 
 @section('content')
@@ -13,67 +13,77 @@
         __('Processes') => route('processes.index'),
         __('Import') => null,
     ]])
-<div class="container" id="importProcess">
-    <div class="row">
-        <div class="col">
-            <div class="card text-center">
-                <div class="card-header bg-light" align="left">
-                    <h5>{{__('Import Process')}}</h5>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">{{__('You are about to import a Process.')}}</h5>
-                    <p class="card-text">{{__('User assignments and sensitive environment variables will not be imported.')}}</p> 
-                    <input type="file" ref="file" class="d-none" @change="handleFile" accept=".bpm4">
-                    <button @click="$refs.file.click()" class="btn btn-secondary ml-2">
-                        <i class="fas fa-upload"></i>
-                        {{__('Browse')}}
-                    </button>
-                </div>
-                <div class="card-footer bg-light" align="right">
-                    <button type="button" class="btn btn-outline-secondary" @click="onCancel">{{__('Cancel')}}</button>
-    			    <button type="button" class="btn btn-secondary ml-2" @click="importFile" :disabled="uploaded == false">{{__('Import')}}</button>
+    <div class="container" id="importProcess">
+        <div class="row">
+            <div class="col">
+                <div class="card text-center">
+                    <div class="card-header bg-light" align="left">
+                        <h5>{{__('Import Process')}}</h5>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{__('You are about to import a Process.')}}</h5>
+                        <p class="card-text">{{__('User assignments and sensitive environment variables will not be imported.')}}</p>
+                        <input type="file" ref="file" class="d-none" @change="handleFile" accept=".bpm4">
+                        <button @click="$refs.file.click()" class="btn btn-secondary ml-2">
+                            <i class="fas fa-upload"></i>
+                            {{__('Browse')}}
+                        </button>
+                    </div>
+                    <div class="card-footer bg-light" align="right">
+                        <button type="button" class="btn btn-outline-secondary" @click="onCancel">
+                            {{__('Cancel')}}
+                        </button>
+                        <button type="button" class="btn btn-secondary ml-2" @click="importFile" :disabled="uploaded == false">
+                            {{__('Import')}}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('js')
     <script>
-        new Vue({
-            el: '#importProcess',
-            data: {
-                file: '',
-                uploaded: false
-            },
-            methods: {
-                handleFile(e) {
-                    this.file = this.$refs.file.files[0];
-                    this.uploaded = true
-                },
-                onCancel() {
-                    window.location = '{{ route("processes.index") }}';
-                },
-                importFile() {
-                    let formData = new FormData();
-                    formData.append('file', this.file);
-                    ProcessMaker.apiClient.post( '/processes/import',
-                        formData,
-                        {
-                            headers: {
-                                'Content-Type': 'multipart/form-data'
-                            }
-                        }
-                        ).then(response => {
-                            ProcessMaker.alert('{{__('The process was imported.')}}', 'success')
-                            window.location = '{{ route("processes.index") }}';
-                        })
-                        .catch(error => {
-                            ProcessMaker.alert('{{__('Unable to import the process.')}}', 'danger')
-                        });
-                }
+      new Vue({
+        el: '#importProcess',
+        data: {
+          file: '',
+          uploaded: false,
+          submitted: false
+        },
+        methods: {
+          handleFile(e) {
+            this.file = this.$refs.file.files[0];
+            this.uploaded = true
+          },
+          onCancel() {
+            window.location = '{{ route("processes.index") }}';
+          },
+          importFile() {
+            let formData = new FormData();
+            formData.append('file', this.file);
+            if (this.submitted) {
+              return
             }
-        })
+            this.submitted = true;
+            ProcessMaker.apiClient.post('/processes/import',
+              formData,
+              {
+                headers: {
+                  'Content-Type': 'multipart/form-data'
+                }
+              }
+            ).then(response => {
+              ProcessMaker.alert('{{__('The process was imported.')}}', 'success')
+              window.location = '{{ route("processes.index") }}';
+            })
+              .catch(error => {
+                this.submitted = false;
+                ProcessMaker.alert('{{__('Unable to import the process.')}}', 'danger')
+              });
+          }
+        }
+      })
     </script>
 @endsection
