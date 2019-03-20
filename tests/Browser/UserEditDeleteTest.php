4df36a1c84429d0e961baf027c031988f3ff9c55
PMQABen <benjamin.fields@processmaker.com>
2019-03-19 17:14:14 -0700

    Adding new Dusk browser tests
    These tests cover the adding, editing, and deleting of Auth Clients, Categories, Groups, and Users. As far as I know they will run headless, and should be fine with Sauce Labs.

---
+++ tests/Browser/UserEditDeleteTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+
+class UserAddEditDeleteTest extends DuskTestCase
+{
+    /**
+     * @throws \Throwable
+     */
+    public function testUserAddEditDelete()
+    {
+        $this->markTestSkipped('Skipping Dusk tests temporarily');
+
+        //Create Admin User
+        Artisan::call('migrate:fresh', []);
+        $user = factory(User::class)->create([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'email' => 'any@gmail.com',
+            'firstname' => 'admin',
+            'lastname' => 'admin',
+            'timezone' => null,
+            'datetime_format' => null,
+            'status' => 'ACTIVE',
+            'is_administrator' => true,
+        ]);
+
+        $this->browse(function ($browser) {
+            //Login
+            $browser->visit("https://bpm4.local.processmaker.com")
+                ->assertSee("Username")
+                ->type("#username", "admin")
+                ->type("#password", "admin")
+                ->press(".btn")
+                ->assertMissing(".invalid-feedback")
+                ->clickLink('Admin')
+                ->waitUntilMissing(".vuetable-empty-result");
+            //Add User
+            $browser->press("#addUserBtn")
+                ->type("#username", "user1")
+                ->type("#firstname", "user1")
+                ->type("#lastname", "last1")
+                ->select("select[name='size']", "ACTIVE")
+                ->type("#email", "user1@hotmail.com")
+                ->type("#password", "password123")
+                ->type("#confpassword", "password123");
+            $browser->whenAvailable(".modal-footer", function ($modal) { //A funky work-around to let me click the save modal button 
+                $modal->press(".ml-2");
+            })
+                ->pause(750)   //No choice here, we have to pause here to wait for either the error message or the success alert.
+                ->assertMissing(".invalid-feedback")
+                ->waitFor(".alert-dismissible", 10)
+                ->assertSee("successfully created");
+            //Edit User
+            $browser->clickLink("Admin")
+                ->waitUntilMissing(".vuetable-empty-result");
+            $browser->driver->findElement(WebDriverBy::xpath("//*[@id='users-listing']/div[2]/div/div/table/tbody/tr[2]/td[7]/div/div/button[1]/i"))
+                ->click();  //The edit button lacks a unique ID
+            $browser->waitFor("#navbar-request-button",10) //when this loads, the whole page is loaded
+                ->type("#firstname","foo")
+                ->type("#lastname","bar")
+                ->press(".ml-2")
+                ->waitFor(".vuetable-body", 10)
+                ->waitUntilMissing(".vuetable-empty-result", 10)
+                ->assertSee("foo bar");
+            //Delete User
+            $browser->driver->findElement(WebDriverBy::xpath("//*[@id='users-listing']/div[2]/div/div/table/tbody/tr[2]/td[7]/div/div/button[2]/i"))
+                ->click();  //The delete button lacks a unique ID
+            $browser->waitFor('#confirmModal', 10)
+                ->press("#confirm")
+                ->waitFor("#alertBox", 10)
+                ->assertSee("The user was deleted");
+        });
+
+    }
+}
