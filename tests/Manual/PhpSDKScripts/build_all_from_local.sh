7426f0c141f57b25ba11c8765d15cfe5f10a1cfd
Nolan Ehrstrom <github@nolanpro.com>
2019-03-20 14:49:40 -0700

    Update build-all script to conform to folder structure

--- tests/Manual/PhpSDKScripts/build_all_from_local.sh
+++ tests/Manual/PhpSDKScripts/build_all_from_local.sh
@@ -1,6 +1,6 @@
 set -ex
 
-for LANG in lua php node
+for LANG in node
 do
     EXECDIR=/home/vagrant/bpm-plugins/ProcessMaker/pm4-docker-executor-$LANG
     SDKDIR=/home/vagrant/processmaker/storage/api/${LANG}-sdk
@@ -12,19 +12,8 @@ do
 
     COPY_TO=pm4-sdk-$LANG
     pushd $EXECDIR
-        if [ "$LANG" == "php" ]; then
-            echo "GOT PWD ${PWD}"
-            rm -rf src/$COPY_TO
-            cp -r $SDKDIR src/$COPY_TO
-        elif [ "$LANG" == "node" ]; then
-            rm -rf $COPY_TO
-            cp -r $SDKDIR $COPY_TO
-        elif [ "$LANG" == "lua" ]; then
-            rm -rf $COPY_TO
-            cp -r $SDKDIR $COPY_TO
-        else
-            echo "NONE"
-        fi
+        rm -rf src/$COPY_TO
+        cp -r $SDKDIR src/$COPY_TO
         ./build.sh
     popd
 done
