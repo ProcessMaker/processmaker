c7aec805930b18e35dcce6c3d738ae70f58bc1db
Tuan N. Truong <t.truong@vehikl.com>
2019-04-30 11:54:24 -0400

    Remove comments

--- resources/js/processes/scripts/components/ScriptEditor.vue
+++ resources/js/processes/scripts/components/ScriptEditor.vue
@@ -92,39 +92,6 @@
                         </b-list-group>
                     </b-col>
                 </b-row>
-
-                <!-- <div class="editor" v-if="resizing"></div>
-                <div class="preview border-top">
-                    <div class="data border-right">
-                        <div class=" p-1 bg-secondary border-bottom text-white">Input Data JSON</div>
-                        <monaco-editor :options="monacoOptions" v-model="preview.data" language="json" class="editor" :class="{hidden: resizing}"></monaco-editor>
-                    </div>
-                    <div class="config border-right">
-                        <div class="p-1 bg-secondary border-bottom text-white">Script Config JSON</div>
-                        <monaco-editor :options="monacoOptions" v-model="preview.config" language="json" class="editor" :class="{hidden: resizing}"></monaco-editor>
-
-                    </div>
-                    <div class="output">
-                        <div class="p-1 bg-secondary border-bottom text-white">Script Output</div>
-                        <div class="btn-group" role="group">
-                            <button :disabled="preview.executing" @click="execute" class="btn btn-primary"><i class="fas fa-play-circle"></i></button>
-                            <div class="col"></div>
-                            <div class="col p-2 text-right">
-                                <i v-if="preview.executing" class="fas fa-spinner fa-spin"></i>
-                                <i v-if="preview.success" class="fas fa-check text-success"></i>
-                                <i v-if="preview.failure" class="fas fa-times-circle text-danger"></i>
-                            </div>
-                        </div>
-                        <div class="content" style="overflow: auto; width: 100%;">
-                            <pre v-if="preview.success" v-text="preview.output"></pre>
-                            <div v-if="preview.failure" class="w-100">
-                                <div class="text-light bg-danger">{{preview.error.exception}}</div>
-                                <div class="text-light text-monospace small">{{preview.error.message}}</div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div> -->
             </b-card-body>
 
             <b-card-footer>
@@ -251,81 +218,5 @@
 .output {
     min-height: 300px;
 }
-
-    /* .container {
-        max-width: 100%;
-        padding: 0 0 0 0;
-    }
-
-    #editor-container {
-        height: calc(100vh - 60px);
-        display: flex;
-        flex-direction: column;
-
-        .editor {
-            flex-grow: 1;
-
-            &.hidden {
-                display: none;
-            }
-        }
-        .preview {
-            height: 300px;
-            display: flex;
-
-            .content {
-                flex-grow: 1;
-                display: flex;
-
-                .editor {
-                    flex-grow: 1;
-                }
-            }
-
-            .data,
-            .config {
-                width: 300px;
-                display: flex;
-                flex-direction: column;
-            }
-
-            .output {
-                display: flex;
-                flex-direction: column;
-                flex-grow: 1;
-
-                .content {
-                    padding: 8px;
-                    background-color: black;
-
-                    pre {
-                        color: white;
-                        font-weight: bold;
-                        font-size: 12px;
-                        font-family: monospace;
-                    }
-                }
-            }
-        }
-
-        .toolbar .override {
-            background-color: #b6bfc6;
-            padding: 10px;
-            height: 40px;
-            font-family: "Open Sans";
-            font-size: 18px;
-            font-weight: 600;
-            font-style: normal;
-            font-stretch: normal;
-            line-height: normal;
-            letter-spacing: normal;
-            text-align: left;
-            color: #ffffff;
-            a {
-                color: white;
-                padding-right: 15px;
-            }
-        }
-    } */
 </style>
 
