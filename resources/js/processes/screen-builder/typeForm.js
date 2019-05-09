267c3d6d94bd41f979ed0807aa585a4d677f97f3
dante <dante@danteloayza.localdomain>
2019-05-09 14:17:20 -0400

    Removing $ char from validatons regualr expression

--- resources/js/processes/screen-builder/typeForm.js
+++ resources/js/processes/screen-builder/typeForm.js
@@ -99,7 +99,7 @@ ProcessMaker.EventBus.$on('screen-builder-init', (manager) => {
         for (let j = 0; j < item.control.inspector.length; j++) {
             let config = item.control.inspector[j].config;
             if (config.label === 'Field Name') {
-                config.validation = 'regex:/^(?:[\$A-Z_a-z])(?:[\$0-9A-Z_a-z])*$/|required';
+                config.validation = 'regex:/^(?:[A-Z_a-z])(?:[0-9A-Z_a-z])*$/|required';
             }
         }
     }
