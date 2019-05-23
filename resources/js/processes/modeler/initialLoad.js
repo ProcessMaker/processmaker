7c3fd2e616bcc5003753d85b4689b0bf19bfcd85
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-05-23 14:27:39 -0400

    Add Screen select to Manual Task

--- resources/js/processes/modeler/initialLoad.js
+++ resources/js/processes/modeler/initialLoad.js
@@ -10,6 +10,7 @@ import {
     startEvent,
     task,
     scriptTask,
+    manualTask,
     pool,
     poolLane,
     textAnnotation,
@@ -46,6 +47,7 @@ let nodeTypes = [
     endEvent,
     task,
     scriptTask,
+    manualTask,
     callActivity,
     exclusiveGateway,
     inclusiveGateway,
@@ -59,7 +61,7 @@ let nodeTypes = [
     textAnnotation,
     eventBasedGateway,
     intermediateMessageCatchEvent,
-]
+];
 
 ProcessMaker.nodeTypes.push(startEvent);
 ProcessMaker.nodeTypes.push(...nodeTypes);
@@ -173,6 +175,15 @@ ProcessMaker.EventBus.$on('modeler-init', ({ registerNode, registerBpmnExtension
         }
     });
     registerInspectorExtension(endEvent, {
+        component: 'ModelerScreenSelect',
+        config: {
+            label: 'Summary screen',
+            helper: 'Summary screen that will be displayed when process finish with this End event.',
+            name: 'screenRef',
+            params: { type: 'DISPLAY' }
+        }
+    });
+    registerInspectorExtension(manualTask, {
         component: 'ModelerScreenSelect',
         config: {
             label: 'Summary screen',
