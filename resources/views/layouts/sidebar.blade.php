32f1adcdb698d1b10c8bb66214b130167be77c96
Mila Endo <milal.alim@gmail.com>
2019-06-21 09:38:22 -0700

    fix dark theme support

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,7 +6,7 @@
             <img src={{asset(env('MAIN_LOGO_PATH', '/img/processmaker_logo.png'))}}>
         </a>
       </li>
-      <li v-else class="logo-closed">
+      <li v-else v-cloak class="logo-closed">
         <a href="#">
             <img src={{asset(env('ICON_PATH_PATH', '/img/processmaker_icon.png'))}}>
         </a>
