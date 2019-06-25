a1e1a69cc45bb6db91faba40fb9d472a5af7e020
Ryan Cooley <ryan.cooley@processmaker.com>
2019-06-25 07:40:21 -0700

    Adding count badge & realtime count updates

--- resources/js/components/Sidebaricon.vue
+++ resources/js/components/Sidebaricon.vue
@@ -1,26 +1,36 @@
 <template>
     <div class="filter-bar justify-content-between" id="Sidebaricon">
       <li class="nav-item">
-        <a href="item.url()" class="nav-link" title="item.title" v-show="item.attributes.icon">
-          <i class="fas nav-icon" :class="item.attributes.icon" ></i> <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}</span>
+        <a :href="url" class="nav-link" title="item.title" v-show="item.attributes.icon">
+          <i class="fas nav-icon" :class="item.attributes.icon" ></i> <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}<span v-if="count !== null" class="nav-badge float-right">{{ count }}</span></span>
         </a>
-        <a href="item.url()" class="nav-link" title="item.title" v-show="item.attributes.file">
-          <img :src="item.attributes.file" class="nav-icon" id="custom_icon"><span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}</span>
+        <a :href="url" class="nav-link" title="item.title" v-show="item.attributes.file">
+          <img :src="item.attributes.file" class="nav-icon" id="custom_icon"><span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}<span v-if="count !== null" class="nav-badge float-right">{{ count }}</span></span>
         </a>
       </li>
     </div>
 </template>
 
-
-
 <script>
   export default {
     props: [
-      'item'
+      'item',
+      'url',
     ],
+    mounted() {
+      if (this.item.attributes.count !== undefined) {
+        this.count = this.item.attributes.count;
+      }
+      
+      if (this.item.attributes.countId !== undefined) {
+        ProcessMaker.EventBus.$on('sidebar-count-updated-' + this.item.attributes.countId, count => {
+          this.count = count;
+        });
+      }
+    },
     data () {
       return {
-       
+        count: null,
       }
     },
     methods: {
@@ -30,6 +40,7 @@
     }
   }
 </script>
+
 <style>
 
 </style>
