75d773d0bedfe372845620f9f6f78e78cc46e213
Ryan Cooley <ryan.cooley@processmaker.com>
2019-07-02 11:29:08 -0700

    Bugfix to fix failing test

--- resources/js/components/Sidebaricon.vue
+++ resources/js/components/Sidebaricon.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="filter-bar justify-content-between" id="Sidebaricon">
       <li class="nav-item">
-        <a :href="url" class="nav-link" title="item.title" v-show="item.attributes.icon">
+        <a :href="url" class="nav-link" :title="item.title" v-show="item.attributes.icon">
           <i class="fas nav-icon" :class="item.attributes.icon" ></i> <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}<span v-if="count !== null" class="nav-badge float-right">{{ count }}</span></span>
         </a>
-        <a :href="url" class="nav-link" title="item.title" v-show="item.attributes.file">
+        <a :href="url" class="nav-link" :title="item.title" v-show="item.attributes.file">
           <img :src="item.attributes.file" class="nav-icon" id="custom_icon"><span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}<span v-if="count !== null" class="nav-badge float-right">{{ count }}</span></span>
         </a>
       </li>
