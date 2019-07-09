2ed6a73fdcd3223b70f0315d2c87ad2edebdeea8
Tuan N. Truong <t.truong@vehikl.com>
2019-07-09 15:21:51 -0400

    Add missing design controls for text

--- resources/js/processes/screen-builder/typeDisplay.js
+++ resources/js/processes/screen-builder/typeDisplay.js
@@ -12,6 +12,89 @@ const {
 
 const TableControl = FormBuilderControls.find(control => control.editorComponent === FormMultiColumn);
 const RichTextControl = FormBuilderControls.find(control => control.editorComponent === FormHtmlEditor);
+const bgcolorProperty = {
+    type: "ColorSelect",
+    field: "bgcolor",
+    config: {
+        label: "Element Background color",
+        helper: "Set the element's background color",
+        options: [{
+            value: "alert alert-primary",
+            content: "primary"
+        },
+        {
+            value: "alert alert-secondary",
+            content: "secondary"
+        },
+        {
+            value: "alert alert-success",
+            content: "success"
+        },
+        {
+            value: "alert alert-danger",
+            content: "danger"
+        },
+        {
+            value: "alert alert-warning",
+            content: "warning"
+        },
+        {
+            value: "alert alert-info",
+            content: "info"
+        },
+        {
+            value: "alert alert-light",
+            content: "light"
+        },
+        {
+            value: "alert alert-dark",
+            content: "dark"
+        }
+        ]
+    }
+};
+
+const colorProperty = {
+    type: "ColorSelect",
+    field: "color",
+    config: {
+        label: "Text color",
+        helper: "Set the element's text color",
+        options: [{
+            value: "text-primary",
+            content: "primary"
+        },
+        {
+            value: "text-secondary",
+            content: "secondary"
+        },
+        {
+            value: "text-success",
+            content: "success"
+        },
+        {
+            value: "text-danger",
+            content: "danger"
+        },
+        {
+            value: "text-warning",
+            content: "warning"
+        },
+        {
+            value: "text-info",
+            content: "info"
+        },
+        {
+            value: "text-light",
+            content: "light"
+        },
+        {
+            value: "text-dark",
+            content: "dark"
+        }
+        ]
+    }
+};
 
 let initialControls = [{
     builderComponent: FormText,
@@ -23,63 +106,114 @@ let initialControls = [{
         component: "FormText",
         "editor-component": "FormText",
         config: {
+            icon: "fas fa-align-justify",
             label: "New Text",
             fontSize: "1em",
             fontWeight: "normal",
-            icon: "fas fa-align-justify"
+            textAlign: "left",
+            verticalAlign: "top",
+            name: ""
         },
-        inspector: [{
-            type: "FormInput",
-            field: "label",
-            config: {
-                label: "Text Label",
-                helper: "The text to display"
-            }
-        },
-        {
-            type: "FormSelect",
-            field: "fontWeight",
-            config: {
-                label: "Font Weight",
-                helper: "The weight of the text",
-                options: [{
-                    value: "normal",
-                    content: "Normal"
-                },
-                {
-                    value: "bold",
-                    content: "Bold"
+        inspector: [
+            {
+                type: "FormTextArea",
+                field: "label",
+                config: {
+                    rows: 5,
+                    label: "Text Content",
+                    helper: "The text to display"
                 }
-                ]
-            }
-        },
-
-        {
-            type: "FormSelect",
-            field: "fontSize",
-            config: {
-                label: "Font Size",
-                helper: "The size of the text in em",
-                options: [{
-                    value: "0.5em",
-                    content: "0.5"
-                },
-                {
-                    value: "1em",
-                    content: "1"
-                },
-                {
-                    value: "1.5em",
-                    content: "1.5"
-                },
-                {
-                    value: "2em",
-                    content: "2"
+            },
+            {
+                type: "FormMultiselect",
+                field: "fontWeight",
+                config: {
+                    label: "Font Weight",
+                    helper: "The weight of the text",
+                    options: [{
+                        value: "normal",
+                        content: "Normal"
+                    },
+                    {
+                        value: "bold",
+                        content: "Bold"
+                    }
+                    ]
                 }
-                ]
-            }
-        }
-
+            },
+            {
+                type: "FormMultiselect",
+                field: "textAlign",
+                config: {
+                    label: "Text Horizontal Alignment",
+                    helper: "Horizontal alignment of the text",
+                    options: [{
+                        value: "center",
+                        content: "Center"
+                    },
+                    {
+                        value: "left",
+                        content: "Left"
+                    },
+                    {
+                        value: "right",
+                        content: "Right"
+                    },
+                    {
+                        value: "justify",
+                        content: "Justify"
+                    }
+                    ]
+                }
+            },
+            {
+                type: "FormMultiselect",
+                field: "verticalAlign",
+                config: {
+                    label: "Text Vertical Alignment",
+                    helper: "Vertical alignment of the text",
+                    options: [{
+                        value: "top",
+                        content: "Top"
+                    },
+                    {
+                        value: "middle",
+                        content: "Middle"
+                    },
+                    {
+                        value: "bottom",
+                        content: "Bottom"
+                    }
+                    ]
+                }
+            },
+            {
+                type: "FormMultiselect",
+                field: "fontSize",
+                config: {
+                    label: "Font Size",
+                    helper: "The size of the text in em",
+                    options: [{
+                        value: "0.5em",
+                        content: "0.5"
+                    },
+                    {
+                        value: "1em",
+                        content: "1"
+                    },
+                    {
+                        value: "1.5em",
+                        content: "1.5"
+                    },
+                    {
+                        value: "2em",
+                        content: "2"
+                    }
+                    ]
+                }
+            },
+            bgcolorProperty,
+            colorProperty
         ]
     }
 },
