6bc08e595a32552cdfc94bcf147e3821aafaf0cb
dante <dante@danteloayza.localdomain>
2019-07-17 15:43:29 -0400

    Test to validate task sorting by request name

--- tests/Feature/Api/TasksTest.php
+++ tests/Feature/Api/TasksTest.php
@@ -61,7 +61,7 @@ class TasksTest extends TestCase
         //Verify the structure
         $response->assertJsonStructure(['data' => ['*' => $this->structure]]);
     }
-    
+
     /**
      * You only see tasks that belong to you if you are not admin
      */
@@ -173,6 +173,36 @@ class TasksTest extends TestCase
         //Verify the first row
         $firstRow = $response->json('data')[0];
         $this->assertArraySubset(['completed_at' => null], $firstRow);
+    }
+
+    public function testSortByRequestName()
+    {
+        //$request = factory(ProcessRequest::class)->create();
+
+        factory(ProcessRequestToken::class, 5)->create([
+            'user_id' => $this->user->id,
+            'completed_at' => Carbon::now()
+        ]);
+
+        // Order by process_request_id
+        $route = route('api.' . $this->resource . '.index',[
+                'order_by' => 'process_request_id',
+                'order_direction' =>'asc'
+        ]);
+        $response = $this->apiCall('GET', $route);
+        $response->assertStatus(200);
+        $firstRow = $response->json('data')[0];
+        $this->assertEquals(1, $firstRow['process_request_id']);
+
+        // Order by the request name (id + name)
+        $route = route('api.' . $this->resource . '.index',[
+            'order_by' => 'process_requests.id, process_requests.name',
+            'order_direction' =>'desc'
+        ]);
+        $response = $this->apiCall('GET', $route);
+        $response->assertStatus(200);
+        $firstRow = $response->json('data')[0];
+        $this->assertEquals(5, $firstRow['process_request_id']);
     }
 
     /**
