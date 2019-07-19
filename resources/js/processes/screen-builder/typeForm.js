c235719fd50b335b66312d8c5f75fd22b0dd9a47
dante <dante@danteloayza.localdomain>
2019-07-19 09:08:05 -0400

    fix to field name to be validated

--- resources/js/processes/screen-builder/typeForm.js
+++ resources/js/processes/screen-builder/typeForm.js
@@ -100,7 +100,7 @@ ProcessMaker.EventBus.$on("screen-builder-init", (manager) => {
 
         for (let j = 0; j < item.control.inspector.length; j++) {
             let config = item.control.inspector[j].config;
-            if (config.label === "Field Name") {
+            if (config.label === "Key Name") {
                 config.validation = "regex:/^(?:[A-Z_a-z])(?:[0-9A-Z_a-z])*$/|required";
             }
         }
