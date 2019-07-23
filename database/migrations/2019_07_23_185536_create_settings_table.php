5a78a6d01b490a053db68077aa8a84385b7eb4b8
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-07-23 15:38:17 -0400

    Add model settings

--- database/migrations/2019_07_23_185536_create_settings_table.php
+++ database/migrations/2019_07_23_185536_create_settings_table.php
@@ -15,7 +15,7 @@ class CreateSettingsTable extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('key');
+            $table->string('key')->index();
             $table->json('config')->nullable();
             $table->timestamps();
         });
