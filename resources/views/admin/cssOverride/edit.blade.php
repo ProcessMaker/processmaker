b632d1c2a2447e2077b78e83f8049e65dcf81564
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-07-26 12:26:53 -0400

    Add modal loading

--- resources/views/admin/cssOverride/edit.blade.php
+++ resources/views/admin/cssOverride/edit.blade.php
@@ -103,6 +103,34 @@
                 </div>
             </div>
         </div>
+
+        <b-modal id="modalLoading"
+                 ref="modalLoading"
+                 v-bind:hide-header="true"
+                 v-bind:hide-footer="true"
+                 v-bind:no-close-on-backdrop="true"
+                 v-bind:no-close-on-esc="true"
+                 v-bind:hide-header-close="true">
+            <div class="container text-center">
+                <div class="icon-container m-4">
+                    <svg class="lds-gear" width="100%" height="50%" xmlns="http://www.w3.org/2000/svg"
+                         xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100"
+                         preserveAspectRatio="xMidYMid">
+                        <g transform="translate(50 50)">
+                            <g transform="rotate(248.825)">
+                                <animateTransform attributeName="transform" type="rotate" values="0;360"
+                                                  keyTimes="0;1" dur="4.7s"
+                                                  repeatCount="indefinite">
+                                </animateTransform>
+                                <path d="M37.43995192304605 -6.5 L47.43995192304605 -6.5 L47.43995192304605 6.5 L37.43995192304605 6.5 A38 38 0 0 1 35.67394948182593 13.090810836924174 L35.67394948182593 13.090810836924174 L44.33420351967032 18.090810836924174 L37.83420351967032 29.34914108612188 L29.17394948182593 24.34914108612188 A38 38 0 0 1 24.34914108612188 29.17394948182593 L24.34914108612188 29.17394948182593 L29.34914108612188 37.83420351967032 L18.090810836924184 44.33420351967032 L13.090810836924183 35.67394948182593 A38 38 0 0 1 6.5 37.43995192304605 L6.5 37.43995192304605 L6.500000000000001 47.43995192304605 L-6.499999999999995 47.43995192304606 L-6.499999999999996 37.43995192304606 A38 38 0 0 1 -13.09081083692417 35.67394948182593 L-13.09081083692417 35.67394948182593 L-18.09081083692417 44.33420351967032 L-29.34914108612187 37.834203519670325 L-24.349141086121872 29.173949481825936 A38 38 0 0 1 -29.17394948182592 24.34914108612189 L-29.17394948182592 24.34914108612189 L-37.83420351967031 29.349141086121893 L-44.33420351967031 18.0908108369242 L-35.67394948182592 13.090810836924193 A38 38 0 0 1 -37.43995192304605 6.5000000000000036 L-37.43995192304605 6.5000000000000036 L-47.43995192304605 6.500000000000004 L-47.43995192304606 -6.499999999999993 L-37.43995192304606 -6.499999999999994 A38 38 0 0 1 -35.67394948182593 -13.090810836924167 L-35.67394948182593 -13.090810836924167 L-44.33420351967032 -18.090810836924163 L-37.834203519670325 -29.34914108612187 L-29.173949481825936 -24.34914108612187 A38 38 0 0 1 -24.349141086121893 -29.17394948182592 L-24.349141086121893 -29.17394948182592 L-29.349141086121897 -37.834203519670304 L-18.0908108369242 -44.334203519670304 L-13.090810836924195 -35.67394948182592 A38 38 0 0 1 -6.500000000000005 -37.43995192304605 L-6.500000000000005 -37.43995192304605 L-6.500000000000007 -47.43995192304605 L6.49999999999999 -47.43995192304606 L6.499999999999992 -37.43995192304606 A38 38 0 0 1 13.090810836924149 -35.67394948182594 L13.090810836924149 -35.67394948182594 L18.090810836924142 -44.33420351967033 L29.349141086121847 -37.83420351967034 L24.349141086121854 -29.17394948182595 A38 38 0 0 1 29.17394948182592 -24.349141086121893 L29.17394948182592 -24.349141086121893 L37.834203519670304 -29.349141086121897 L44.334203519670304 -18.0908108369242 L35.67394948182592 -13.090810836924197 A38 38 0 0 1 37.43995192304605 -6.500000000000007 M0 -20A20 20 0 1 0 0 20 A20 20 0 1 0 0 -20"></path>
+                            </g>
+                        </g>
+                    </svg>
+                </div>
+                <h3 class="display-6">{{ __('Regenerating CSS Files') }}</h3>
+                <p class="lead">{{ __('Please wait while the files are generated. The screen will be updated when finished.') }}</p>
+            </div>
+        </b-modal>
     </div>
 @endsection
 
@@ -278,7 +306,7 @@
         mounted() {
           let userID = document.head.querySelector('meta[name="user-id"]');
           window.Echo.private(
-              `ProcessMaker.Models.User.${userID.content}`
+            `ProcessMaker.Models.User.${userID.content}`
           ).notification(response => {
             if (response.type == "ProcessMaker\\Notifications\\SassCompiledNotification") {
               ProcessMaker.alert('{{ __('The styles were recompiled.') }}', 'success');
@@ -315,8 +343,8 @@
           onCreate(data) {
             ProcessMaker.apiClient.post('css_settings', data)
               .then(response => {
-                ProcessMaker.alert('{{ __('The styles were saved. Please wait while the styles are regenerated...')
-                }}', 'success');
+                ProcessMaker.alert('{{ __('The styles were saved. Please wait while the styles are regenerated...') }}', 'success');
+                this.$refs.modalLoading.show();
               })
               .catch(error => {
                 if (error.response.status && error.response.status === 422) {
@@ -328,6 +356,7 @@
             ProcessMaker.apiClient.put('css_settings', data)
               .then(response => {
                 ProcessMaker.alert('{{ __('The styles were updated.') }}', 'danger');
+                this.$refs.modalLoading.show();
               })
               .catch(error => {
                 if (error.response.status && error.response.status === 422) {
@@ -364,4 +393,23 @@
         }
       });
     </script>
+
+    <style lang="scss" scoped>
+        .icon-container {
+            display: inline-block;
+            width: 8em;
+            margin-bottom: 1em;
+
+        .i {
+            color: #96b0aa;
+            font-size: 5em;
+        }
+
+        .svg {
+            fill: #aeb5bb;
+        }
+
+        }
+    </style>
 @endsection
+
