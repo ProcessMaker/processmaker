6c2e06718bfba7f2a0ddb9f352aa265c7942d6a6
Alan Bollinger <velkymx@gmail.com>
2019-07-30 14:23:08 -0700

    add faker config

--- database/factories/SettingFactory.php
+++ database/factories/SettingFactory.php
@@ -1,12 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+
 /**
  * Model factory for a settings.
  */
 $factory->define(ProcessMaker\Models\Setting::class, function (Faker $faker) {
     return [
-        'key' => $faker->sentence(1),
-        'config' => json_encode([])
+        'key' => $faker->word(),
+        'config' => json_encode(
+            ['test' => $faker->sentence(1)]
+        )
     ];
 });
