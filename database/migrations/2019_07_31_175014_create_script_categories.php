47cf3984f8975080d8492107765935cd26c48b25
Alan Bollinger <velkymx@gmail.com>
2019-07-31 11:04:57 -0700

    Add script category table

---
+++ database/migrations/2019_07_31_175014_create_script_categories.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScriptCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('script_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum('status', ['ACTIVE', 'INACTIVE'])->default('ACTIVE');
+            $table->timestamps();
+        });
+
+        Schema::table('scripts', function (Blueprint $table) {
+            $table->unsignedInteger('script_category_id')->nullable();
+            $table->foreign('script_category_id')->references('id')->on('script_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('scripts', function (Blueprint $table) {
+            $table->dropForeign(['script_category_id']);
+        });
+
+        Schema::dropIfExists('script_categories');
+    }
+}
