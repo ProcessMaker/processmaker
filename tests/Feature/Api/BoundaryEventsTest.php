ff55a2cbf073df9649ddfcb52da6f5459ecaec32
David Callizaya <davidcallizaya@gmail.com>
2019-07-31 14:35:33 -0400

    Add non interrupting boundary event tests

--- tests/Feature/Api/BoundaryEventsTest.php
+++ tests/Feature/Api/BoundaryEventsTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Feature\Api;
 
+use ProcessMaker\Managers\TaskSchedulerManager;
+use ProcessMaker\Models\ProcessRequest;
 use Tests\Feature\Shared\ProcessTestingTrait;
 use Tests\Feature\Shared\RequestHelper;
 use Tests\TestCase;
-use ProcessMaker\Managers\TaskSchedulerManager;
 
 class BoundaryEventsTest extends TestCase
 {
@@ -120,5 +121,221 @@ class BoundaryEventsTest extends TestCase
         // and continue to Task 2
         $this->assertCount(1, $activeTokens);
         $this->assertEquals('Task 2', $activeTokens[0]->element_name);
+    }
+
+    /**
+     * Tests a process with a cycle timer boundary event attached to a CallActivity
+     *
+     */
+    public function testCycleTimerBoundaryEventCallActivity()
+    {
+        // Mock current date for TaskSchedulerManager
+        $now = TaskSchedulerManager::fakeToday('2018-05-01T00:00:00Z');
+
+        // Create a process
+        $process = $this->createProcess(file_get_contents(__DIR__ . '/processes/Timer_BoundaryEvent_CallActivity.bpmn'));
+
+        // Start a process instance
+        $instance = $this->startProcess($process, '_4');
+
+        // Get active tokens
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Call Activity" must be active in the main instance
+        $this->assertCount(1, $activeTokens);
+        $this->assertEquals('Call Activity', $activeTokens[0]->element_name);
+
+        // Get active tokens in sub process
+        $subInstance = ProcessRequest::orderBy('id', 'desc')->first();
+        $activeTokensSub = $subInstance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Sub Task" must be active in the sub process instance
+        $this->assertCount(1, $activeTokensSub);
+        $this->assertEquals('Sub Task', $activeTokensSub[0]->element_name);
+
+        // Trigger timer event
+        $now->modify('+1 minute');
+        TaskSchedulerManager::fakeToday($now);
+        $this->runScheduledTasks();
+
+        // Get active tokens
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Task 2" must be active
+        $this->assertCount(1, $activeTokens);
+        $this->assertEquals('Task 2', $activeTokens[0]->element_name);
+
+        // SubProcess instance must be COMPLETED
+        $subInstance->refresh();
+        $this->assertEquals('COMPLETED', $subInstance->status);
+    }
+
+    /**
+     * Tests the a process with a signal boundary event attached to a CallActivity
+     */
+    public function testSignalBoundaryEventCallActivity()
+    {
+        // Create a process
+        $process = $this->createProcess(file_get_contents(__DIR__ . '/processes/Signal_BoundaryEvent_CallActivity.bpmn'));
+
+        // Start a process instance
+        $instance = $this->startProcess($process, '_4');
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')
+            ->get();
+
+        // The sub process trigger a Signal that is catch by Boundary Event
+        // and continue to Task 2, closing the CallActivity
+        $this->assertCount(1, $activeTokens);
+        $this->assertEquals('Task 2', $activeTokens[0]->element_name);
+        $subInstance = ProcessRequest::orderBy('id', 'desc')->first();
+        $this->assertEquals('COMPLETED', $subInstance->status);
+    }
+
+    /**
+     * Tests a process with a cycle timer boundary non interrupting event attached to a task
+     */
+    public function testCycleTimerBoundaryEventNonInterrupting()
+    {
+        // Mock current date for TaskSchedulerManager
+        $now = TaskSchedulerManager::fakeToday('2018-05-01T00:00:00Z');
+
+        // Create a process
+        $process = $this->createProcess(file_get_contents(__DIR__ . '/processes/Timer_BoundaryEvent_Cycle_NonInterrupting.bpmn'));
+
+        // Start a process instance
+        $instance = $this->startProcess($process, '_2');
+
+        // Get active tokens
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Task 1" must be active
+        $this->assertCount(1, $activeTokens);
+        $this->assertEquals('Task 1', $activeTokens[0]->element_name);
+
+        // Trigger timer event
+        $now->modify('+1 minute');
+        TaskSchedulerManager::fakeToday($now);
+        $this->runScheduledTasks();
+
+        // Get active tokens
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Task 1" and "Task 2" must be active
+        $this->assertCount(2, $activeTokens);
+        $this->assertEquals('Task 1', $activeTokens[0]->element_name);
+        $this->assertEquals('Task 2', $activeTokens[1]->element_name);
+    }
+
+    /**
+     * Tests a process with an error boundary non interrupting event attached to a script task
+     */
+    public function testErrorBoundaryEventScriptTaskNonInterrupting()
+    {
+        // Create a process
+        $process = $this->createProcess(file_get_contents(__DIR__ . '/processes/Error_BoundaryEvent_ScriptTask_NonInterrupting.bpmn'));
+
+        // Start a process instance
+        $instance = $this->startProcess($process, '_2');
+
+        // Get active tokens
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+
+        // Since "Task 1" failed, BoundaryEvent catch the error and activate "Task 2"
+        $this->assertCount(1, $activeTokens);
+        $this->assertEquals('Task 2', $activeTokens[0]->element_name);
+
+        // And keeps "Task 1" in FAILING status
+        $failingTokens = $instance->tokens()->where('status', 'FAILING')->get();
+        $this->assertCount(1, $failingTokens);
+        $this->assertEquals('Task 1', $failingTokens[0]->element_name);
+    }
+
+    /**
+     * Tests a process with an error boundary non interrupting event attached to a CallActivity
+     */
+    public function testErrorBoundaryEventCallActivityNonInterrupting()
+    {
+        // Create a process
+        $process = $this->createProcess(file_get_contents(__DIR__ . '/processes/Error_BoundaryEvent_CallActivity_NonInterrupting.bpmn'));
+
+        // Start a process instance
+        $instance = $this->startProcess($process, '_4');
+
+        // The sub process have thrown an ErrorEvent that is catch by Boundary Event
+        // which activates Task 2 and keeps the CallActivity in FAILING status
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+        $this->assertCount(1, $activeTokens);
+        $this->assertEquals('Task 2', $activeTokens[0]->element_name);
+
+        $failingTokens = $instance->tokens()->where('status', 'FAILING')->get();
+        $this->assertCount(1, $failingTokens);
+        $this->assertEquals('Call Activity', $failingTokens[0]->element_name);
+    }
+
+    /**
+     * Tests a process with a cycle timer boundary non interrupting event attached to a CallActivity
+     */
+    public function testCycleTimerBoundaryEventCallActivityNonInterrupting()
+    {
+        // Mock current date for TaskSchedulerManager
+        $now = TaskSchedulerManager::fakeToday('2018-05-01T00:00:00Z');
+
+        // Create a process
+        $process = $this->createProcess(file_get_contents(__DIR__ . '/processes/Timer_BoundaryEvent_CallActivity_NonInterrupting.bpmn'));
+
+        // Start a process instance
+        $instance = $this->startProcess($process, '_4');
+
+        // Get active tokens
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Call Activity" must be active in the main instance
+        $this->assertCount(1, $activeTokens);
+        $this->assertEquals('Call Activity', $activeTokens[0]->element_name);
+
+        // Get active tokens in sub process
+        $subInstance = ProcessRequest::orderBy('id', 'desc')->first();
+        $activeTokensSub = $subInstance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Sub Task" must be active in the sub process instance
+        $this->assertCount(1, $activeTokensSub);
+        $this->assertEquals('Sub Task', $activeTokensSub[0]->element_name);
+
+        // Trigger timer event
+        $now->modify('+1 minute');
+        TaskSchedulerManager::fakeToday($now);
+        $this->runScheduledTasks();
+
+        // Get active tokens
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')->get();
+
+        // "Call Activity" and "Task 2" must be active
+        $this->assertCount(2, $activeTokens);
+        $this->assertEquals('Call Activity', $activeTokens[0]->element_name);
+        $this->assertEquals('Task 2', $activeTokens[1]->element_name);
+
+        // SubProcess instance must keep ACTIVE
+        $subInstance->refresh();
+        $this->assertEquals('ACTIVE', $subInstance->status);
+    }
+
+    /**
+     * Tests the a process with a signal boundary non interrupting event attached to a CallActivity
+     */
+    public function testSignalBoundaryEventCallActivityNonInterrupting()
+    {
+        // Create a process
+        $process = $this->createProcess(file_get_contents(__DIR__ . '/processes/Signal_BoundaryEvent_CallActivity_NonInterrupting.bpmn'));
+
+        // Start a process instance
+        $instance = $this->startProcess($process, '_4');
+        $activeTokens = $instance->tokens()->where('status', 'ACTIVE')
+            ->get();
+
+        // The sub process trigger a Signal that is catch by Boundary Event
+        // which activates Task 2 and keeps the CallActivity activated
+        $this->assertCount(2, $activeTokens);
+        $this->assertEquals('Call Activity', $activeTokens[0]->element_name);
+        $this->assertEquals('Task 2', $activeTokens[1]->element_name);
     }
 }
