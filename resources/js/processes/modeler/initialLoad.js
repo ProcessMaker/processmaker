f83df3812c75b9584900272c4a825f1246c26717
Alan Bollinger <velkymx@gmail.com>
2019-08-02 14:38:11 -0700

    Remove boundary events

--- resources/js/processes/modeler/initialLoad.js
+++ resources/js/processes/modeler/initialLoad.js
@@ -1,29 +1,26 @@
 /* eslint-disable func-names */
 // Our initial node types to register with our modeler
 import {
-    association,
-    endEvent,
-    exclusiveGateway,
-    inclusiveGateway,
-    parallelGateway,
-    sequenceFlow,
-    startEvent,
-    task,
-    scriptTask,
-    manualTask,
-    pool,
-    poolLane,
-    textAnnotation,
-    messageFlow,
-    serviceTask,
-    startTimerEvent,
-    intermediateTimerEvent,
-    callActivity,
-    eventBasedGateway,
-    intermediateMessageCatchEvent,
-    boundaryTimerEvent,
-    boundaryErrorEvent,
-    boundarySignalEvent
+  association,
+  endEvent,
+  exclusiveGateway,
+  inclusiveGateway,
+  parallelGateway,
+  sequenceFlow,
+  startEvent,
+  task,
+  scriptTask,
+  manualTask,
+  pool,
+  poolLane,
+  textAnnotation,
+  messageFlow,
+  serviceTask,
+  startTimerEvent,
+  intermediateTimerEvent,
+  callActivity,
+  eventBasedGateway,
+  intermediateMessageCatchEvent
 } from '@processmaker/modeler';
 import bpmnExtension from '@processmaker/processmaker-bpmn-moddle/resources/processmaker.json';
 import ModelerScreenSelect from './components/inspector/ScreenSelect';
@@ -47,26 +44,23 @@ Vue.component('ScriptSelect', ScriptSelect);
 Vue.component('StartPermission', StartPermission);
 
 let nodeTypes = [
-    endEvent,
-    task,
-    scriptTask,
-    manualTask,
-    callActivity,
-    exclusiveGateway,
-    inclusiveGateway,
-    parallelGateway,
-    sequenceFlow,
-    association,
-    pool,
-    poolLane,
-    messageFlow,
-    serviceTask,
-    textAnnotation,
-    eventBasedGateway,
-    intermediateMessageCatchEvent,
-    boundaryTimerEvent,
-    boundaryErrorEvent,
-    boundarySignalEvent
+  endEvent,
+  task,
+  scriptTask,
+  manualTask,
+  callActivity,
+  exclusiveGateway,
+  inclusiveGateway,
+  parallelGateway,
+  sequenceFlow,
+  association,
+  pool,
+  poolLane,
+  messageFlow,
+  serviceTask,
+  textAnnotation,
+  eventBasedGateway,
+  intermediateMessageCatchEvent
 ];
 
 ProcessMaker.nodeTypes.push(startEvent);
@@ -74,52 +68,64 @@ ProcessMaker.nodeTypes.push(...nodeTypes);
 
 // Implement user list and group list for intermediate catch event
 // eslint-disable-next-line func-names
-(function () {
-    intermediateMessageCatchEvent.inspectorConfig[0].items[0].items[4] = {
-        component: 'UserSelect',
-        config: {
-            label: 'Allowed User',
-            helper: 'Select allowed user',
-            name: 'allowedUsers',
-        }
-    };
-    intermediateMessageCatchEvent.inspectorConfig[0].items[0].items[5] = {
-        component: 'GroupSelect',
-        config: {
-            label: 'Allowed Group',
-            helper: 'Select allowed group',
-            name: 'allowedGroups',
-        }
-    };
+(function() {
+  intermediateMessageCatchEvent.inspectorConfig[0].items[0].items[4] = {
+    component: 'UserSelect',
+    config: {
+      label: 'Allowed User',
+      helper: 'Select allowed user',
+      name: 'allowedUsers'
+    }
+  };
+  intermediateMessageCatchEvent.inspectorConfig[0].items[0].items[5] = {
+    component: 'GroupSelect',
+    config: {
+      label: 'Allowed Group',
+      helper: 'Select allowed group',
+      name: 'allowedGroups'
+    }
+  };
 })();
 
 // Set default properties for task
 task.definition = function definition(moddle) {
-    return moddle.create('bpmn:Task', {
-        name: window.ProcessMaker.events.$t('New Task'),
-        assignment: 'requester',
-    });
+  return moddle.create('bpmn:Task', {
+    name: window.ProcessMaker.events.$t('New Task'),
+    assignment: 'requester'
+  });
 };
 
-ProcessMaker.EventBus.$on('modeler-init', ({ registerNode, registerBpmnExtension, registerInspectorExtension }) => {
+ProcessMaker.EventBus.$on(
+  'modeler-init',
+  ({ registerNode, registerBpmnExtension, registerInspectorExtension }) => {
     // Register start events
     registerNode(startEvent);
     registerNode(startTimerEvent, definition => {
-        const eventDefinitions = definition.get('eventDefinitions');
-        if (definition.$type === 'bpmn:StartEvent' && eventDefinitions && eventDefinitions.length && eventDefinitions[0].$type === 'bpmn:TimerEventDefinition') {
+      const eventDefinitions = definition.get('eventDefinitions');
+      if (
+        definition.$type === 'bpmn:StartEvent' &&
+        eventDefinitions &&
+        eventDefinitions.length &&
+        eventDefinitions[0].$type === 'bpmn:TimerEventDefinition'
+      ) {
         return startTimerEvent.id;
-        }
+      }
     });
     registerNode(intermediateTimerEvent, definition => {
-        const eventDefinitions = definition.get('eventDefinitions');
-        if (definition.$type === 'bpmn:IntermediateCatchEvent' && eventDefinitions && eventDefinitions.length && eventDefinitions[0].$type === 'bpmn:TimerEventDefinition') {
+      const eventDefinitions = definition.get('eventDefinitions');
+      if (
+        definition.$type === 'bpmn:IntermediateCatchEvent' &&
+        eventDefinitions &&
+        eventDefinitions.length &&
+        eventDefinitions[0].$type === 'bpmn:TimerEventDefinition'
+      ) {
         return intermediateTimerEvent.id;
-        }
+      }
     });
 
     /* Register basic node types */
     for (const node of nodeTypes) {
-        registerNode(node);
+      registerNode(node);
     }
 
     /* Add a BPMN extension */
@@ -127,90 +133,93 @@ ProcessMaker.EventBus.$on('modeler-init', ({ registerNode, registerBpmnExtension
 
     /* Register extension for start permission */
     registerInspectorExtension(startEvent, {
-        component: 'StartPermission',
-        config: {
-            label: 'Permission To Start',
-            helper: '',
-            name: 'startPermission',
-        }
+      component: 'StartPermission',
+      config: {
+        label: 'Permission To Start',
+        helper: '',
+        name: 'startPermission'
+      }
     });
 
     /* Register the inspector extensions for tasks */
     registerInspectorExtension(task, {
-        component: 'ModelerScreenSelect',
-        config: {
-            label: 'Screen For Input',
-            helper: 'What Screen Should Be Used For Rendering This Task',
-            name: 'screenRef',
-            type: 'FORM'
-        }
+      component: 'ModelerScreenSelect',
+      config: {
+        label: 'Screen For Input',
+        helper: 'What Screen Should Be Used For Rendering This Task',
+        name: 'screenRef',
+        type: 'FORM'
+      }
     });
     registerInspectorExtension(task, {
-        component: "TaskAssignment",
-        config: {
-            label: "Task Assignment",
-            helper: "",
-            name: "taskAssignment"
-        }
+      component: 'TaskAssignment',
+      config: {
+        label: 'Task Assignment',
+        helper: '',
+        name: 'taskAssignment'
+      }
     });
     registerInspectorExtension(task, {
-        component: "TaskNotifications",
-        config: {
-            label: "Task Notifications",
-            helper: "Users that should be notified about task events",
-        }
+      component: 'TaskNotifications',
+      config: {
+        label: 'Task Notifications',
+        helper: 'Users that should be notified about task events'
+      }
     });
 
     /* Register the inspector extensions for script tasks */
     registerInspectorExtension(scriptTask, {
-        component: 'ScriptSelect',
-        config: {
-            label: 'Script',
-            helper: 'Script that will be executed by the task',
-            name: 'scriptRef'
-        }
+      component: 'ScriptSelect',
+      config: {
+        label: 'Script',
+        helper: 'Script that will be executed by the task',
+        name: 'scriptRef'
+      }
     });
 
     registerInspectorExtension(scriptTask, {
-        component: 'ConfigEditor',
-        config: {
-            label: 'Script Configuration',
-            helper: 'Configuration JSON for the script task',
-            name: 'scriptConfiguration',
-            property: 'config',
-        }
+      component: 'ConfigEditor',
+      config: {
+        label: 'Script Configuration',
+        helper: 'Configuration JSON for the script task',
+        name: 'scriptConfiguration',
+        property: 'config'
+      }
     });
     registerInspectorExtension(endEvent, {
-        component: 'ModelerScreenSelect',
-        config: {
-            label: 'Summary screen',
-            helper: 'Summary screen that will be displayed when process finish with this End event.',
-            name: 'screenRef',
-            params: { type: 'DISPLAY' }
-        }
+      component: 'ModelerScreenSelect',
+      config: {
+        label: 'Summary screen',
+        helper:
+          'Summary screen that will be displayed when process finish with this End event.',
+        name: 'screenRef',
+        params: { type: 'DISPLAY' }
+      }
     });
     registerInspectorExtension(manualTask, {
-        component: 'ModelerScreenSelect',
-        config: {
-            label: 'Summary screen',
-            helper: 'Summary screen that will be displayed when process finish with this End event.',
-            name: 'screenRef',
-            params: { type: 'DISPLAY' }
-        }
+      component: 'ModelerScreenSelect',
+      config: {
+        label: 'Summary screen',
+        helper:
+          'Summary screen that will be displayed when process finish with this End event.',
+        name: 'screenRef',
+        params: { type: 'DISPLAY' }
+      }
     });
     registerInspectorExtension(manualTask, {
-        component: "TaskAssignment",
-        config: {
-            label: "Task Assignment",
-            helper: "",
-            name: "taskAssignment"
-        }
+      component: 'TaskAssignment',
+      config: {
+        label: 'Task Assignment',
+        helper: '',
+        name: 'taskAssignment'
+      }
     });
     registerInspectorExtension(manualTask, {
-        component: "TaskNotifications",
-        config: {
-            label: "Task Notifications",
-            helper: "Users that should be notified about task events"
-        }
+      component: 'TaskNotifications',
+      config: {
+        label: 'Task Notifications',
+        helper: 'Users that should be notified about task events'
+      }
     });
-});
+  }
+);
