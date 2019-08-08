aebfa47b39c1758cf8835b123c7dc49f19741bed
Nolan Ehrstrom <github@nolanpro.com>
2019-08-08 14:59:06 -0700

    Modify collections in place

--- ProcessMaker/Http/Resources/ProcessRequestsCollection.php
+++ ProcessMaker/Http/Resources/ProcessRequestsCollection.php
@@ -6,10 +6,13 @@ class ProcessRequestsCollection extends ApiCollection
 {
     public function toResponse($request)
     {
-        $this->resource = $this->resource->reject(function($processRequest) {
-            return $processRequest->process->category &&
-                $processRequest->process->category->is_system;
-            return false;
+        $this->resource->each(function($processRequest, $key) {
+            if(
+                $processRequest->process->category &&
+                $processRequest->process->category->is_system
+            ) {
+                $this->resource->forget($key);
+            }
         });
 
         return parent::toResponse($request);
