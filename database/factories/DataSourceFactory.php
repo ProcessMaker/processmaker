d191c55f84586af0b6d494b7ccdca40e2c30ce1d
Alan Bollinger <velkymx@gmail.com>
2019-08-26 16:16:45 -0700

    Create DataSourceFactory.php

---
+++ database/factories/DataSourceFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use ProcessMaker\Models\DataSource;
+use ProcessMaker\Models\DataSourceCategory;
+use Faker\Generator as Faker;
+
+$factory->define(DataSource::class, function (Faker $faker) {
+
+    $auth = $faker->randomElement(['NONE', 'BASIC', 'BEARER']);
+
+    $creds = '[]';
+
+    if ($auth == 'BASIC') {
+        $creds = '[{"username":"' . $faker->userName . '"},{"password":"' . $faker->password . '"}]';
+    }
+
+    if ($auth == 'BEARER') {
+        $creds = '[{"bearer_token":"' . $faker->text . '"}]';
+    }
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(),
+        'endpoints' => '[
+                    {"create": "' . $faker->url . '"},
+                    {"read": "' . $faker->url . '"},
+                    {"update": "' . $faker->url . '"},
+                    {"delete": "' . $faker->url . '"},
+                    {"list": "' . $faker->url . '"}
+                    ]',
+        'credentials' => $creds,
+        'status' => $faker->randomElement(['ACTIVE', 'INACTIVE']),
+        'authtype' => $auth,
+        'data_source_category_id' => function () {
+            return factory(DataSourceCategory::class)->create()->getKey();
+        }
+    ];
+});
