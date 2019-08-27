ce545b0d26e53d9af583bfa3895999af5c5b8a7f
Alan Bollinger <velkymx@gmail.com>
2019-08-27 12:44:27 -0700

    Stub index method

---
+++ ProcessMaker/Http/Controllers/Api/DataSourceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ProcessMaker\Http\Controllers\Api;
+
+use ProcessMaker\Http\Controllers\Controller;
+use ProcessMaker\Http\Resources\ApiResource;
+use Illuminate\Http\Request;
+
+class DataSourceController extends Controller
+{
+    public function index(Request $request)
+    {
+        return [];
+    }
+}
