0a804574b02afca6e03fa4b9898449c007287b34
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-08-28 12:07:33 -0400

    Add test script categories

--- ProcessMaker/Models/ScriptCategory.php
+++ ProcessMaker/Models/ScriptCategory.php
@@ -5,11 +5,35 @@ namespace ProcessMaker\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Validation\Rule;
 use ProcessMaker\Models\Script;
+use ProcessMaker\Traits\HideSystemResources;
+use ProcessMaker\Traits\SerializeToIso8601;
 
 class ScriptCategory extends Model
 {
+    use SerializeToIso8601;
+    use HideSystemResources;
+
     protected $connection = 'processmaker';
 
+    protected $fillable = [
+        'name',
+        'status',
+        'is_system'
+    ];
+
+    public static function rules()
+    {
+        return [
+            'name' => 'required|string|max:100|unique:script_categories,name',
+            'status' => 'required|string|in:ACTIVE,INACTIVE'
+        ];
+    }
+
+    /**
+     * Get screens
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function scripts()
     {
         return $this->hasMany(Script::class);
