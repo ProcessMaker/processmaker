480d9164e9221d556ca7050686fdc0e5170a69c2
David Callizaya <davidcallizaya@gmail.com>
2019-08-28 20:06:26 -0400

    Do migration of cataegories

--- database/migrations/2019_08_28_221332_remove_field_category_screen_script.php
+++ database/migrations/2019_08_28_221332_remove_field_category_screen_script.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use ProcessMaker\Models\Screen;
+use ProcessMaker\Models\ScreenCategory;
+use ProcessMaker\Models\Script;
+use ProcessMaker\Models\ScriptCategory;
 
 class RemoveFieldCategoryScreenScript extends Migration
 {
@@ -13,10 +17,20 @@ class RemoveFieldCategoryScreenScript extends Migration
      */
     public function up()
     {
+        foreach(Script::all() as $script) {
+            if ($script->category) {
+                $category = ScriptCategory::firstOrCreate(['name' => $script->category ]);
+                $script->category()->associate($category);
+            }
+        }
+        foreach(Screen::all() as $screen) {
+            if ($screen->category) {
+                $category = ScreenCategory::firstOrCreate(['name' => $screen->category]);
+                $screen->category()->associate($category);
+            }
+        }
         Schema::table('scripts', function (Blueprint $table) {
             $table->dropColumn('category');
-
-            //$table->foreign('script_category_id')->references('id')->on('script_categories')->onDelete('cascade');
         });
         Schema::table('script_versions', function (Blueprint $table) {
             $table->dropColumn('category');
@@ -24,8 +38,6 @@ class RemoveFieldCategoryScreenScript extends Migration
 
         Schema::table('screens', function (Blueprint $table) {
             $table->dropColumn('category');
-
-            //$table->foreign('screen_category_id')->references('id')->on('screen_categories')->onDelete('cascade');
         });
         Schema::table('screen_versions', function (Blueprint $table) {
             $table->dropColumn('category');
@@ -40,18 +52,24 @@ class RemoveFieldCategoryScreenScript extends Migration
     public function down()
     {
         Schema::table('scripts', function (Blueprint $table) {
-            //$table->dropForeign('script_category_id');
             $table->string('category', '100')->after('status')->nullable()->default(null);
         });
         Schema::table('script_versions', function (Blueprint $table) {
             $table->string('category', '100')->after('status')->nullable()->default(null);
         });
         Schema::table('screens', function (Blueprint $table) {
-            //$table->dropForeign('screen_category_id');
             $table->string('category', '100')->after('status')->nullable()->default(null);
         });
         Schema::table('screen_versions', function (Blueprint $table) {
             $table->string('category', '100')->after('status')->nullable()->default(null);
         });
+        foreach(Script::all() as $script) {
+            $category = $script->category()->first();
+            !$category ?: $script->category = $category->name;
+        }
+        foreach(Screen::all() as $screen) {
+            $category = $screen->category()->first();
+            !$category ?: $screen->category = $category->name;
+        }
     }
 }
