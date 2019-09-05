ad6a7aff1e8833d8bf17b826d24385ed2f26c774
Marco A. Nina Mena <marcoantonina@gmail.com>
2019-09-05 15:18:31 -0400

    Add api Datasources

--- ProcessMaker/Http/Controllers/Api/DataSourceController.php
+++ ProcessMaker/Http/Controllers/Api/DataSourceController.php
@@ -2,14 +2,266 @@
 
 namespace ProcessMaker\Http\Controllers\Api;
 
-use ProcessMaker\Http\Controllers\Controller;
-use ProcessMaker\Http\Resources\ApiResource;
+use Exception;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use ProcessMaker\Http\Controllers\Controller;
+use ProcessMaker\Http\Resources\ApiCollection;
+use ProcessMaker\Http\Resources\ApiResource;
+use ProcessMaker\Models\DataSource;
+use ProcessMaker\Models\ProcessRequest;
+use Throwable;
 
 class DataSourceController extends Controller
 {
+    /**
+     * Get the list of records of a dataSource
+     *
+     * @param Request $request
+     *
+     * @return ApiCollection
+     *
+     * @OA\Get(
+     *     path="/datasources",
+     *     summary="Returns all Data Sources that the user has access to",
+     *     operationId="getDataSources",
+     *     tags={"DataSources"},
+     *     @OA\Parameter(ref="#/components/parameters/filter"),
+     *     @OA\Parameter(ref="#/components/parameters/order_by"),
+     *     @OA\Parameter(ref="#/components/parameters/order_direction"),
+     *     @OA\Parameter(ref="#/components/parameters/per_page"),
+     *     @OA\Parameter(ref="#/components/parameters/include"),
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="list of Data Sources,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 @OA\Items(ref="#/components/schemas/dataSource"),
+     *             ),
+     *             @OA\Property(
+     *                 property="meta",
+     *                 type="object",
+     *                 allOf={@OA\Schema(ref="#/components/schemas/metadata")},
+     *             ),
+     *         ),
+     *     ),
+     * )
+     */
     public function index(Request $request)
     {
-        return [];
+        $query = DataSource::query();
+
+        $filter = $request->input('filter', '');
+
+        if (!empty($filter)) {
+            $filter = '%' . $filter . '%';
+            $query->where(function ($query) use ($filter) {
+                $query->where('name', 'like', $filter)
+                    ->orWhere('description', 'like', $filter)
+                    ->orWhere('authtype', 'like', $filter);
+            });
+        }
+        $response =
+            $query->orderBy(
+                $request->input('order_by', 'id'),
+                $request->input('order_direction', 'ASC')
+            )->paginate($request->input('per_page', 10));
+
+        return new ApiCollection($response);
+    }
+
+    /**
+     * Get a single DataSource.
+     *
+     * @param DataSource $dataSource
+     *
+     * @return ApiResource
+     *
+     * @OA\Get(
+     *     path="/datasources/datasource_id",
+     *     summary="Get single Data Source by ID",
+     *     operationId="getDataSourceById",
+     *     tags={"DataSources"},
+     *     @OA\Parameter(
+     *         description="ID of Data Source to return",
+     *         in="path",
+     *         name="datasource_id",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successfully found the Data Source",
+     *         @OA\JsonContent(ref="#/components/schemas/dataSource")
+     *     ),
+     * )
+     */
+    public function show(DataSource $dataSource)
+    {
+        return new ApiResource($dataSource);
+    }
+
+    /**
+     * Create a new DataSource.
+     *
+     * @param Request $request
+     *
+     * @return ApiResource
+     * @throws Throwable
+     *
+     * @OA\Post(
+     *     path="/datasources",
+     *     summary="Save a new Data Source",
+     *     operationId="createDataSource",
+     *     tags={"DataSources"},
+     *     @OA\RequestBody(
+     *       required=true,
+     *       @OA\JsonContent(ref="#/components/schemas/dataSourceEditable")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="success",
+     *         @OA\JsonContent(ref="#/components/schemas/dataSource")
+     *     ),
+     * )
+     */
+    public function store(Request $request)
+    {
+        $request->validate(DataSource::rules());
+
+        $dataSource = new DataSource;
+        $dataSource->fill($request->input());
+        $dataSource->saveOrFail();
+        return new ApiResource($dataSource);
+    }
+
+    /**
+     * Update a DataSource.
+     *
+     * @param DataSource $dataSource
+     * @param Request $request
+     *
+     * @return ResponseFactory|Response
+     *
+     * @throws Throwable
+     *
+     * @OA\Put(
+     *     path="/datasources/datasource_id",
+     *     summary="Update a Data Source",
+     *     operationId="updateDataSource",
+     *     tags={"DataSources"},
+     *     @OA\Parameter(
+     *         description="ID of Data Source to return",
+     *         in="path",
+     *         name="datasource_id",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *       required=true,
+     *       @OA\JsonContent(ref="#/components/schemas/dataSourceEditable")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="success",
+     *         @OA\JsonContent(ref="#/components/schemas/dataSource")
+     *     ),
+     * )
+     */
+    public function update(DataSource $dataSource, Request $request)
+    {
+        $request->validate(DataSource::rules($dataSource));
+        $dataSource->fill($request->input());
+        $dataSource->saveOrFail();
+
+        return response([], 204);
+    }
+
+    /**
+     * Delete a DataSource.
+     *
+     * @param DataSource $dataSource
+     *
+     * @return ResponseFactory|Response
+     *
+     * @throws Exception
+     *
+     * @OA\Delete(
+     *     path="/datasources/datasource_id",
+     *     summary="Delete a Data Source",
+     *     operationId="deleteDataSource",
+     *     tags={"DataSources"},
+     *     @OA\Parameter(
+     *         description="ID of Data Source to return",
+     *         in="path",
+     *         name="datasource_id",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="success",
+     *         @OA\JsonContent(ref="#/components/schemas/dataSource")
+     *     ),
+     * )
+     */
+    public function destroy(DataSource $dataSource)
+    {
+        //TODO Check if there are processes that use it.
+        $dataSource->delete();
+        return response([], 204);
+    }
+
+    /**
+     * Send a DataSource request.
+     *
+     * @param DataSource $datasource
+     * @param ProcessRequest $processRequest
+     *
+     * @param Request $request
+     *
+     * @return ResponseFactory|Response
+     *
+     * @OA\Put(
+     *     path="/datasources/datasource_id/send",
+     *     summary="Send a Data Source request",
+     *     operationId="sendDataSource",
+     *     tags={"DataSources"},
+     *     @OA\Parameter(
+     *         description="ID of Data Source to return",
+     *         in="path",
+     *         name="datasource_id",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *       required=true,
+     *       @OA\JsonContent(ref="#/components/schemas/dataSourceEditable")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="success",
+     *         @OA\JsonContent(ref="#/components/schemas/dataSource")
+     *     ),
+     * )
+     */
+    public function request(DataSource $datasource, ProcessRequest $processRequest, Request $request)
+    {
+        $response = $datasource->request($processRequest->data, $request->json('config', []));
+
+        return response($response, 200);
     }
 }
--- ProcessMaker/Models/DataSource.php
+++ ProcessMaker/Models/DataSource.php
@@ -6,6 +6,43 @@ use Illuminate\Validation\Rule;
 use Illuminate\Database\Eloquent\Model;
 use ProcessMaker\Traits\Encryptable;
 
+/**
+ * Class DataSource
+ *
+ * @package ProcessMaker\Packages\Connectors\DataSources\Models
+ *
+ * @property integer id
+ * @property string name
+ * @property string description
+ * @property array endpoints
+ * @property array mappings
+ * @property string authtype
+ * @property string credentials
+ * @property string status
+ * @property integer data_source_category_id
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $created_at
+ *
+ * @OA\Schema(
+ *   schema="dataSourceEditable",
+ *   @OA\Property(property="id", type="string", format="id"),
+ *   @OA\Property(property="name", type="string"),
+ *   @OA\Property(property="description", type="string"),
+ *   @OA\Property(property="endpoints", type="string"),
+ *   @OA\Property(property="mappings", type="string"),
+ *   @OA\Property(property="authtype", type="string"),
+ *   @OA\Property(property="credentials", type="string"),
+ *   @OA\Property(property="status", type="string"),
+ *   @OA\Property(property="data_source_category_id", type="string"),
+ * ),
+ * @OA\Schema(
+ *   schema="dataSource",
+ *   allOf={@OA\Schema(ref="#/components/schemas/dataSourceEditable")},
+ *   @OA\Property(property="created_at", type="string", format="date-time"),
+ *   @OA\Property(property="updated_at", type="string", format="date-time"),
+ * )
+ *
+ */
 class DataSource extends Model
 {
 
@@ -16,6 +53,7 @@ class DataSource extends Model
     protected $encryptable = [
         'credentials'
     ];
+
     /**
      * Validation rules
      *
