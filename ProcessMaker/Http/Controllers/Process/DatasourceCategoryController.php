d33bf8229165c2ca185013f92eb0a7442c5bb130
Marco A. Nina Mena <marcoantonina@gmail.com>
2019-09-06 11:38:13 -0400

    Add web routes Datasources and categories

---
+++ ProcessMaker/Http/Controllers/Process/DatasourceCategoryController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ProcessMaker\Http\Controllers\Process;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use ProcessMaker\Http\Controllers\Controller;
+use ProcessMaker\Models\DatasourceCategory;
+
+class DatasourceCategoryController extends Controller
+{
+    /**
+     * Get list of Datasource Categories
+     *
+     * @return Factory|View
+     */
+    public function index()
+    {
+        $title = __('Datasource Categories');
+        $btnCreate = __('Category');
+        $titleMenu = __('Datasources');
+        $routeMenu = 'datasources.index';
+        $titleModal = __('Create Datasource Category');
+        $fieldName = __('Category Datasource Name');
+        $distinctName = __('The category name must be distinct.');
+        $permissions = Auth::user()->hasPermissionsFor('categories');
+        $route = 'datasource_categories';
+        $location = '/designer/datasources/categories';
+        $create = 'create-categories';
+        $include = 'datasourcesCount';
+        $labelCount = __('# Datasources');
+        $count = 'datasources_count';
+
+        return view('categories.index', compact('title', 'btnCreate', 'titleMenu', 'routeMenu', 'permissions', 'titleModal', 'fieldName', 'distinctName', 'route', 'location', 'create', 'include', 'labelCount', 'count'));
+    }
+
+    /**
+     * Get a specific datasource category
+     *
+     * @param DatasourceCategory $datasourceCategory
+     *
+     * @return Factory|View
+     */
+    public function edit(DatasourceCategory $datasourceCategory)
+    {
+        $category = $datasourceCategory;
+        $titleMenu = __('Categories');
+        $routeMenu = 'datasource-categories.index';
+        $route = 'datasource_categories';
+        $location = '/designer/datasources/categories';
+        return view('categories.edit', compact('category', 'route', 'location', 'titleMenu', 'routeMenu'));
+    }
+}
