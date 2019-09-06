0475b0cac470ba649e953b9b41e5a5aa2a00b5c2
Marco A. Nina Mena <marcoantonina@gmail.com>
2019-09-06 09:28:09 -0400

    Add model Datasource Category

--- ProcessMaker/Models/DataSourceCategory.php
+++ ProcessMaker/Models/DataSourceCategory.php
@@ -3,8 +3,41 @@
 namespace ProcessMaker\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Validation\Rule;
+use ProcessMaker\Traits\HideSystemResources;
+use ProcessMaker\Traits\SerializeToIso8601;
 
 class DataSourceCategory extends Model
 {
-    //
+    use SerializeToIso8601;
+    use HideSystemResources;
+
+    protected $connection = 'processmaker';
+
+    protected $fillable = [
+        'name',
+        'status',
+        'is_system'
+    ];
+
+    public static function rules($existing = null)
+    {
+        $unique = Rule::unique('data_source_categories')->ignore($existing);
+
+        return [
+            'name' => ['required', 'string', 'max:100', $unique],
+            'status' => 'required|string|in:ACTIVE,INACTIVE'
+        ];
+    }
+
+    /**
+     * Get screens
+     *
+     * @return HasMany
+     */
+    public function datasources()
+    {
+        return $this->hasMany(DataSource::class);
+    }
 }
