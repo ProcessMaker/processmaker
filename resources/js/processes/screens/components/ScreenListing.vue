79b499b98aa4ffb409b8cc71750e9788fd26bb33
sanjacornelius <sanja.cornelius@processmaker.com>
2019-09-16 12:28:13 -0700

    Fix category sorting

--- resources/js/processes/screens/components/ScreenListing.vue
+++ resources/js/processes/screens/components/ScreenListing.vue
@@ -165,7 +165,7 @@ export default {
         {
           title: this.$t("Category"),
           name: "category.name",
-          sortField: "category.name"
+          sortField: "screen_category_id"
         },
         {
           title: this.$t("Type"),
