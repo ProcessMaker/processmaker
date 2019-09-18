86957cf071bd76675e3b7569ed603f187d6459b9
Marco A. Nina Mena <marcoantonina@gmail.com>
2019-09-18 12:54:56 -0400

    Display tab category in first time

--- resources/views/components/categorized_resource.blade.php
+++ resources/views/components/categorized_resource.blade.php
@@ -1,13 +1,24 @@
 <div class="px-3 page-content" id="categorizedList">
+    @php
+        $firstTab = $secondTab = 'nav-item nav-link';
+        $firstContent = $secondContent = 'tab-pane fade show';
+        if ($countCategories === 0) {
+            $secondTab.=' active';
+            $secondContent.=' active';
+        } else {
+            $firstTab.=' active';
+            $firstContent.=' active';
+        }
+    @endphp
     <ul class="nav nav-tabs">
         <li class="nav-item">
-            <a class="nav-item nav-link" :class="{'active': !tabActive}" id="nav-sources-tab" data-toggle="tab" href="#nav-sources" role="tab"
+            <a class="{{$firstTab}}" id="nav-sources-tab" data-toggle="tab" href="#nav-sources" role="tab"
                aria-controls="nav-sources" aria-selected="true">
                 {{ $tabs[0] ?? __('Resources') }}
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-item nav-link" :class="{'active': tabActive}" id="nav-categories-tab" data-toggle="tab" href="#nav-categories"
+            <a class="{{$secondTab}}" id="nav-categories-tab" data-toggle="tab" href="#nav-categories"
                role="tab"
                aria-controls="nav-categories" aria-selected="true">
                 {{ $tabs[1] ?? __('Categories') }}
@@ -17,13 +28,12 @@
 
     <div class="mt-3">
         <div class="tab-content">
-            <div class="tab-pane fade show" :class="{'active': !tabActive}" id="nav-sources" role="tabpanel" aria-labelledby="nav-sources-tab">
+            <div class="{{$firstContent}}" id="nav-sources" role="tabpanel" aria-labelledby="nav-sources-tab">
                 <div class="card card-body">
                     {{ $itemList }}
                 </div>
             </div>
-            <div class="tab-pane fade show " :class="{'active': tabActive}" id="nav-categories" role="tabpanel"
-                 aria-labelledby="nav-categories-tab">
+            <div class="{{$secondContent}}" id="nav-categories" role="tabpanel" aria-labelledby="nav-categories-tab">
                 <div class="card card-body">
                     {{ $categoryList }}
                 </div>
@@ -31,23 +41,3 @@
         </div>
     </div>
 </div>
-
-@section('js')
-    <script>
-      new Vue({
-        el: "#categorizedList",
-        data: {
-          count: @json($countCategories)
-        },
-        computed: {
-            tabActive() {
-                return this.count === 0;
-            }
-        },
-        methods: {
-        }
-      });
-    </script>
-
-
-@append
