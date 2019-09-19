e6777cb65a4d7defad697c0add79f253318c02e7
Marco A. Nina Mena <marcoantonina@gmail.com>
2019-09-19 12:51:52 -0400

    Fix style categories filter

--- resources/views/categories/list.blade.php
+++ resources/views/categories/list.blade.php
@@ -1,129 +1,127 @@
-    <div class="px-3 page-content" id="categories-listing">
-        <div class="row">
-            <div class="col">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">
-                        <i class="fas fa-search"></i>
-                        </span>
-                    </div>
+<div class="px-3 page-content" id="categories-listing">
+    <div id="search-bar" class="search mt-2 bg-light p-2" vcloak>
+        <div class="d-flex">
+            <div class="flex-grow-1">
+                <div id="search" class="pr-2">
                     <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
                 </div>
             </div>
-            <div class="col-8" align="right">
+            <div class="flex-shrink-0">
+                <button title="" type="button" class="btn btn-primary" data-original-title="Search"><i class="fas fa-search"></i></button>
                 @can($create)
                     <button type="button" id="create_category" class="btn btn-secondary" data-toggle="modal"
                             data-target="#createCategory">
-                        <i class="fas fa-plus"></i> {{ $btnCreate }}
+                        <i class="fas fa-plus"></i> {{ $btnCreate ?? __('Category') }}
                     </button>
                 @endcan
             </div>
         </div>
-        <categories-listing ref="list"
-                            @edit="editCategory"
-                            @delete="deleteCategory"
-                            :filter="filter"
-                            api-route="{{$route}}"
-                            :permission="{{$permissions}}"
-                            load-on-start="{{$showCategoriesTab}}"
-                            location="{{$location}}"
-                            include="{{$include}}"
-                            label-count="{{$labelCount}}"
-                            count="{{$count}}">
-        </categories-listing>
     </div>
+    <categories-listing
+        ref="list"
+        @edit="editCategory"
+        @delete="deleteCategory"
+        :filter="filter"
+        api-route="{{$route}}"
+        :permission="{{$permissions}}"
+        load-on-start="{{$showCategoriesTab ?? true}}"
+        location="{{$location}}"
+        include="{{$include}}"
+        label-count="{{$labelCount}}"
+        count="{{$count}}">
+    </categories-listing>
+</div>
 
-    @can($create)
-        <div class="modal" tabindex="-1" role="dialog" id="createCategory">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">{{ $titleModal }}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="onClose">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+@can($create)
+    <div class="modal" tabindex="-1" role="dialog" id="createCategory">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ $titleModal ?? __('Create Category')}}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="onClose">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        {!!Form::label('name', $fieldName ?? __('Category Name'))!!}
+                        {!!Form::text('name', null, ['class'=> 'form-control', 'v-model'=> 'name',
+                        'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.name}'])!!}
+                        <small class="form-text text-muted" v-if="! errors.name">
+                            {{ $distinctName ?? __('The category name must be distinct.') }}
+                        </small>
+                        <div class="invalid-feedback" v-for="name in errors.name">@{{name}}</div>
                     </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            {!!Form::label('name', $fieldName)!!}
-                            {!!Form::text('name', null, ['class'=> 'form-control', 'v-model'=> 'name',
-                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.name}'])!!}
-                            <small class="form-text text-muted" v-if="! errors.name">
-                                {{ $distinctName }}
-                            </small>
-                            <div class="invalid-feedback" v-for="name in errors.name">@{{name}}</div>
-                        </div>
-                        <div class="form-group">
-                            {!! Form::label('status', __('Status')) !!}
-                            {!! Form::select('status', ['ACTIVE' => __('active'), 'INACTIVE' => __('inactive')], null, ['id' => 'status',
-                            'class' => 'form-control', 'v-model' => 'status', 'v-bind:class' => '{"form-control":true, "is-invalid":errors.status}']) !!}
-                            <div class="invalid-feedback" v-for="status in errors.status">@{{status}}</div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal" @click="onClose">
-                            {{ __('Cancel') }}
-                        </button>
-                        <button type="button" class="btn btn-secondary ml-2" @click="onSubmit" :disabled="disabled">
-                            {{ __('Save') }}
-                        </button>
+                    <div class="form-group">
+                        {!! Form::label('status', __('Status')) !!}
+                        {!! Form::select('status', ['ACTIVE' => __('active'), 'INACTIVE' => __('inactive')], null, ['id' => 'status',
+                        'class' => 'form-control', 'v-model' => 'status', 'v-bind:class' => '{"form-control":true, "is-invalid":errors.status}']) !!}
+                        <div class="invalid-feedback" v-for="status in errors.status">@{{status}}</div>
                     </div>
                 </div>
-
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-dismiss="modal" @click="onClose">
+                        {{ __('Cancel') }}
+                    </button>
+                    <button type="button" class="btn btn-secondary ml-2" @click="onSubmit" :disabled="disabled">
+                        {{ __('Save') }}
+                    </button>
+                </div>
             </div>
+
         </div>
-    @endcan
+    </div>
+@endcan
 
-    @section('js')
+@section('js')
+    <script>
+      //Data needed for default search
+      window.Processmaker.route = '{{ $route }}';
+    </script>
+    <script src="{{mix('js/processes/categories/index.js')}}"></script>
+
+    @can($create)
         <script>
-          //Data needed for default search
-          window.Processmaker.route = '{{ $route }}';
-        </script>
-        <script src="{{mix('js/processes/categories/index.js')}}"></script>
-
-        @can($create)
-            <script>
-              new Vue({
-                el: '#createCategory',
-                data: {
-                  errors: {},
-                  name: '',
-                  status: 'ACTIVE',
-                  disabled: false,
-                  route: @json($route),
-                  location: @json($location),
-                },
-                methods: {
-                  onClose() {
-                    this.name = '';
-                    this.status = 'ACTIVE';
-                    this.errors = {};
-                  },
-                  onSubmit() {
-                    this.errors = {};
-                    //single click
-                    if (this.disabled) {
-                      return
-                    }
-                    this.disabled = true;
-                    ProcessMaker.apiClient.post(this.route, {
-                      name: this.name,
-                      status: this.status
-                    })
-                        .then(response => {
-                          ProcessMaker.alert('{{__('The category was created.')}}', 'success', 5, true);
-                          window.location = this.location;
-                        })
-                        .catch(error => {
-                          this.disabled = false;
-                          if (error.response.status === 422) {
-                            this.errors = error.response.data.errors
-                          }
-                        });
-                  }
+          new Vue({
+            el: "#createCategory",
+            data: {
+              errors: {},
+              name: "",
+              status: "ACTIVE",
+              disabled: false,
+              route: @json($route),
+              location: @json($location),
+            },
+            methods: {
+              onClose () {
+                this.name = "";
+                this.status = "ACTIVE";
+                this.errors = {};
+              },
+              onSubmit () {
+                this.errors = {};
+                //single click
+                if (this.disabled) {
+                  return;
                 }
-              })
-            </script>
-        @endcan
-    @append
-
+                this.disabled = true;
+                ProcessMaker.apiClient.post(this.route, {
+                  name: this.name,
+                  status: this.status
+                })
+                  .then(response => {
+                    ProcessMaker.alert('{{__('The category was created.')}}', "success", 5, true);
+                    window.location = this.location;
+                  })
+                  .catch(error => {
+                    this.disabled = false;
+                    if (error.response.status === 422) {
+                      this.errors = error.response.data.errors;
+                    }
+                  });
+              }
+            }
+          });
+        </script>
+    @endcan
+@append
