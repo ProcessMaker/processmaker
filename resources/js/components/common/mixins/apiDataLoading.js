d13d0b3be71783e64340fdd0a43e2a4db717d194
Marco A. Nina Mena <marcoantonina@gmail.com>
2019-09-24 12:20:27 -0400

    Fix data loading

--- resources/js/components/common/mixins/apiDataLoading.js
+++ resources/js/components/common/mixins/apiDataLoading.js
@@ -1,26 +1,26 @@
 import DataLoading from "../../../components/common/DataLoading";
 
 export default {
-    components: {
-        DataLoading,
-    },
-    data() {
-        return {
-            apiDataLoading: true,
-            apiNoResults: false,
-        }
-    },
-    computed: {
-        shouldShowLoader() {
-            return this.apiDataLoading
-        }
-    },
-    mounted() {
-        ProcessMaker.EventBus.$on('api-data-loading', (val) => {
-            this.apiDataLoading = val
-        });
-        ProcessMaker.EventBus.$on('api-data-no-results', (val) => {
-            this.apiNoResults = val
-        });
+  components: {
+    DataLoading
+  },
+  data () {
+    return {
+      apiDataLoading: true,
+      apiNoResults: false
+    };
+  },
+  computed: {
+    shouldShowLoader () {
+      return this.apiDataLoading || this.apiNoResults;
     }
-}
+  },
+  mounted () {
+    ProcessMaker.EventBus.$on("api-data-loading", (val) => {
+      this.apiDataLoading = val;
+    });
+    ProcessMaker.EventBus.$on("api-data-no-results", (val) => {
+      this.apiNoResults = val;
+    });
+  }
+};
