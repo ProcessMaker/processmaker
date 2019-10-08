c148b5c38f92989be22b82a6b01187bb4ec6264d
sanjacornelius <sanja.cornelius@processmaker.com>
2019-10-08 16:31:42 -0700

    Convert the add user modal to vue-bootstrap

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -30,10 +30,10 @@
             </div>
             <div class="col-8" align="right">
                 @can('create-users')
-                    <button type="button" class="btn btn-action text-light" data-toggle="modal" data-target="#addUser"
-                            id="addUserBtn">
+                    <b-button @click="showModal()"> 
                         <i class="fas fa-plus"></i>
-                        {{__('User')}}</button>
+                        {{__('User')}}
+                    </b-button>
                 @endcan
             </div>
         </div>
@@ -41,256 +41,94 @@
             <users-listing ref="listing" :filter="filter" :permission="{{ \Auth::user()->hasPermissionsFor('users') }}"
                            v-on:reload="reload"></users-listing>
         </div>
+        @can('create-users')
+        <add-user-modal ref="addUserModal">
+                <template v-slot:modal-title>
+                    <h5>{{__('Create User')}}</h5>
+                </template>
+    
+                <template v-slot:default>
+                    <div class="form-group">
+                        {!!Form::label('username', __('Username'))!!}<small class="ml-1">*</small>
+                        {!!Form::text('username', null, ['class'=> 'form-control', 'v-model'=> 'config.username', 'v-bind:class'
+                        => '{\'form-control\':true, \'is-invalid\':config.addError.username}', 'autocomplete' => 'off']) !!}
+                        <div class="invalid-feedback" v-for="username in config.addError.username">
+                            <div v-if="username !== 'userExists'"> 
+                                @{{username}} 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!!Form::label('firstname', __('First Name'))!!}<small class="ml-1">*</small>
+                        {!!Form::text('firstname', null, ['class'=> 'form-control', 'v-model'=> 'config.firstname', 'v-bind:class'
+                        => '{\'form-control\':true, \'is-invalid\':config.addError.firstname}'])!!}
+                        <div class="invalid-feedback" v-for="firstname in config.addError.firstname">@{{firstname}}</div>
+                    </div>
+                    <div class="form-group">
+                        {!!Form::label('lastname', __('Last Name'))!!}<small class="ml-1">*</small>
+                        {!!Form::text('lastname', null, ['class'=> 'form-control', 'v-model'=> 'config.lastname', 'v-bind:class'
+                        => '{\'form-control\':true, \'is-invalid\':config.addError.lastname}'])!!}
+                        <div class="invalid-feedback" v-for="lastname in config.addError.lastname">@{{lastname}}</div>
+                    </div>
+                    <div class="form-group">
+                        {!!Form::label('title', __('Job Title'))!!}
+                        {!!Form::text('title', null, ['class'=> 'form-control', 'v-model'=> 'config.title', 'v-bind:class'
+                        => '{\'form-control\':true, \'is-invalid\':config.addError.title}'])!!}
+                        <div class="invalid-feedback" v-for="title in config.addError.title">@{{title}}</div>
+                    </div>
+                    <div class="form-group">
+                        {!!Form::label('status', __('Status'));!!}<small class="ml-1">*</small>
+                        {!!Form::select('size',[null => __('Select')]+['ACTIVE' => __('Active'), 'INACTIVE' => __('Inactive')], 'Active',
+                        [
+                        'class'=> 'form-control', 'v-model'=> 'config.status',
+                        'v-bind:class' => '{\'form-control\':true, \'is-invalid\':config.addError.status}']);!!}
+                        <div class="invalid-feedback" v-for="status in config.addError.status">@{{status}}</div>
+                    </div>
+                    <div class="form-group">
+                        {!!Form::label('email', __('Email'))!!}<small class="ml-1">*</small>
+                        {!!Form::email('email', null, ['class'=> 'form-control', 'v-model'=> 'config.email', 'v-bind:class' =>
+                        '{\'form-control\':true, \'is-invalid\':config.addError.email}', 'autocomplete' => 'off'])!!}
+                        <div class="invalid-feedback" v-for="email in config.addError.email">
+                            <div v-if="email !== 'userExists'">
+                                @{{email}}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {!!Form::label('password', __('Password'))!!}<small class="ml-1">*</small>
+                        <vue-password v-model="config.password" :disable-toggle=true ref="passwordStrength">
+                            <div slot="password-input" slot-scope="props">
+                                {!!Form::password('password', ['class'=> 'form-control', 'v-model'=> 'config.password',
+                                '@input' => 'props.updatePassword($event.target.value)', 'autocomplete' => 'new-password',
+                                'v-bind:class' => '{\'form-control\':true, \'is-invalid\':config.addError.password}'])!!}
+                            </div>
+                        </vue-password>
+                    </div>
+                    <div class="form-group">
+                        {!!Form::label('confpassword', __('Confirm Password'))!!}<small class="ml-1">*</small>
+                        {!!Form::password('confpassword', ['class'=> 'form-control', 'v-model'=> 'config.confpassword',
+                        'v-bind:class' => '{\'form-control\':true, \'is-invalid\':config.addError.password}', 'autocomplete' => 'new-password'])!!}
+                        <div class="invalid-feedback" v-for="password in config.addError.password">@{{password}}</div>
+                    </div>
+                </template>
+    
+                <template v-slot:modal-footer>
+                    <div class="text-right pb-5 mt-3">
+                        <button type="button" class="btn btn-outline-secondary" @click="hideModal">
+                            {{__('Cancel')}}
+                        </button>
+                        <button type="button" class="btn btn-secondary ml-2" @click="onSubmit" :disabled="config.disabled">
+                            {{__('Save')}}
+                        </button>
+                    </div>
+                </template>
+            </add-user-modal>
+        @endcan
     </div>
-
-    @can('create-users')
-        <div class="modal" tabindex="-10" role="dialog" id="addUser">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">{{__('Create User')}}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            {!!Form::label('username', __('Username'))!!}<small class="ml-1">*</small>
-                            {!!Form::text('username', null, ['class'=> 'form-control', 'v-model'=> 'username', 'v-bind:class'
-                            => '{\'form-control\':true, \'is-invalid\':addError.username}', 'autocomplete' => 'off']) !!}
-                            <div class="invalid-feedback" v-for="username in addError.username">
-                                <div v-if="username !== 'userExists'"> 
-                                    @{{username}} 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            {!!Form::label('firstname', __('First Name'))!!}<small class="ml-1">*</small>
-                            {!!Form::text('firstname', null, ['class'=> 'form-control', 'v-model'=> 'firstname', 'v-bind:class'
-                            => '{\'form-control\':true, \'is-invalid\':addError.firstname}'])!!}
-                            <div class="invalid-feedback" v-for="firstname in addError.firstname">@{{firstname}}</div>
-                        </div>
-                        <div class="form-group">
-                            {!!Form::label('lastname', __('Last Name'))!!}<small class="ml-1">*</small>
-                            {!!Form::text('lastname', null, ['class'=> 'form-control', 'v-model'=> 'lastname', 'v-bind:class'
-                            => '{\'form-control\':true, \'is-invalid\':addError.lastname}'])!!}
-                            <div class="invalid-feedback" v-for="lastname in addError.lastname">@{{lastname}}</div>
-                        </div>
-                        <div class="form-group">
-                            {!!Form::label('title', __('Job Title'))!!}
-                            {!!Form::text('title', null, ['class'=> 'form-control', 'v-model'=> 'title', 'v-bind:class'
-                            => '{\'form-control\':true, \'is-invalid\':addError.title}'])!!}
-                            <div class="invalid-feedback" v-for="title in addError.title">@{{title}}</div>
-                        </div>
-                        <div class="form-group">
-                            {!!Form::label('status', __('Status'));!!}<small class="ml-1">*</small>
-                            {!!Form::select('size',[null => __('Select')]+['ACTIVE' => __('Active'), 'INACTIVE' => __('Inactive')], 'Active',
-                            [
-                            'class'=> 'form-control', 'v-model'=> 'status',
-                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':addError.status}']);!!}
-                            <div class="invalid-feedback" v-for="status in addError.status">@{{status}}</div>
-                        </div>
-                        <div class="form-group">
-                            {!!Form::label('email', __('Email'))!!}<small class="ml-1">*</small>
-                            {!!Form::email('email', null, ['class'=> 'form-control', 'v-model'=> 'email', 'v-bind:class' =>
-                            '{\'form-control\':true, \'is-invalid\':addError.email}', 'autocomplete' => 'off'])!!}
-                            <div class="invalid-feedback" v-for="email in addError.email">
-                                <div v-if="email !== 'userExists'">
-                                    @{{email}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            {!!Form::label('password', __('Password'))!!}<small class="ml-1">*</small>
-                            <vue-password v-model="password" :disable-toggle=true ref="passwordStrength">
-                                <div slot="password-input" slot-scope="props">
-                                    {!!Form::password('password', ['class'=> 'form-control', 'v-model'=> 'password',
-                                    '@input' => 'props.updatePassword($event.target.value)', 'autocomplete' => 'new-password',
-                                    'v-bind:class' => '{\'form-control\':true, \'is-invalid\':addError.password}'])!!}
-                                </div>
-                            </vue-password>
-                        </div>
-                        <div class="form-group">
-                            {!!Form::label('confpassword', __('Confirm Password'))!!}<small class="ml-1">*</small>
-                            {!!Form::password('confpassword', ['class'=> 'form-control', 'v-model'=> 'confpassword',
-                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':addError.password}', 'autocomplete' => 'new-password'])!!}
-                            <div class="invalid-feedback" v-for="password in addError.password">@{{password}}</div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">
-                            {{__('Cancel')}}
-                        </button>
-                        <button type="button" class="btn btn-secondary ml-2" @click="onSubmit" :disabled="disabled">
-                            {{__('Save')}}
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="modal" tabindex="-10" role="dialog" id="restoreUser">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">{{__('Deleted User Found')}}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>{{ __('An existing user has been found with the ')}} @{{ restoreAttribute + ' "' + restoreValue + '"' }} {{__(' would you like to save and reactivate their account?') }}</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary" @click="onCancelRestore">
-                            {{__('Cancel')}}
-                        </button>
-                        <button class="btn btn-secondary ml-2" @click="onSaveRestore">
-                            {{__('Save')}}
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endcan
 @endsection
 
 @section('js')
     <script src="{{mix('js/admin/users/index.js')}}"></script>
-
-    @can('create-users')
-        <script>
-        var addUserModal = new Vue({
-            el: '#addUser',
-            data: {
-              username: '',
-              firstname: '',
-              lastname: '',
-              title: '',
-              status: '',
-              email: '',
-              password: '',
-              confpassword: '',
-              addError: {},
-              submitted: false,
-              disabled: false
-            },
-            methods: {
-              onClose() {
-                this.username = '';
-                this.firstname = '';
-                this.lastname = '';
-                this.status = '';
-                this.title = '',
-                this.email = '';
-                this.password = '';
-                this.confpassword = '';
-                this.addError = {};
-                this.submitted = false;
-              },
-              validatePassword() {
-                if (this.password.trim().length > 0 && this.password.trim().length < 8) {
-                  this.addError.password = ['Password must be at least 8 characters']
-                  this.$refs.passwordStrength.updatePassword('')
-                  this.password = ''
-                  this.confpassword = ''
-                  this.submitted = false
-                  return false
-                }
-                if (this.password !== this.confpassword) {
-                  this.addError.password = ['Passwords must match']
-                  this.$refs.passwordStrength.updatePassword('')
-                  this.password = ''
-                  this.confpassword = ''
-                  this.submitted = false
-                  return false
-                }
-                return true
-              },
-              onSubmit() {
-                this.submitted = true;
-                if (this.validatePassword()) {
-                  //single click
-                  if (this.disabled) {
-                    return
-                  }
-                  this.disabled = true;
-                  ProcessMaker.apiClient.post("/users", {
-                    username: this.username,
-                    firstname: this.firstname,
-                    lastname: this.lastname,
-                    title: this.title,
-                    status: this.status,
-                    email: this.email,
-                    password: this.password
-                  }).then(function (response) {
-                    window.location = "/admin/users/" + response.data.id + '/edit?created=true'
-                  }).catch(error => {
-                    this.addError = error.response.data.errors;
-                    this.disabled = false;
-                    if (!Object.values(this.addError).some(field => field.includes('userExists'))) {
-                        if (this.addError.email) {
-                            restoreUserModal.emailToRestore = restoreUserModal.restoreValue = this.email;
-                            restoreUserModal.restoreAttribute = 'email address';
-                        } else if (this.addError.username) {
-                            restoreUserModal.usernameToRestore = restoreUserModal.restoreValue= this.username;
-                            restoreUserModal.restoreAttribute = 'username';
-                        }
-                        $('#addUser').modal('hide');
-                        $('#restoreUser').modal('show');
-                    }
-                  });
-                }
-              }
-            },
-            mounted() {
-              $('#addUser').on('hidden.bs.modal', () => {
-                this.onClose();
-              });
-            }
-        });
-
-        var restoreUserModal = new Vue({
-            el: '#restoreUser',
-            data(){
-                return {
-                    restoreValue: '',
-                    restoreAttribute: '',
-                    emailToRestore: '',
-                    usernameToRestore: ''
-                }
-            },
-            methods: {
-                onCancelRestore() {
-                    $('#restoreUser').modal('hide');
-                },
-                onSaveRestore() {
-                    let data = [];
-                    if (this.emailToRestore !== '') {
-                        this.data = {
-                            email: this.emailToRestore
-                        };
-                    }
-
-                    if (this.usernameToRestore !== '') {
-                        this.data = {
-                            username: this.usernameToRestore
-                        };
-                    }
-                    ProcessMaker.apiClient.put('/users/restore', this.data)
-                    .then(response => {
-                        $('#restoreUser').modal('hide');
-                        ProcessMaker.alert(this.$t("The user was restored."), "success");
-                        location.reload();
-                    })
-                    .catch(error => {
-                        ProcessMaker.alert(error, "danger");
-                    });
-                }
-            },
-        });
-        </script>
-    @endcan
 @endsection
 @section('css')
     <style>
