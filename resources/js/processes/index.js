9ff013e456befce1b0b9e3182f96984c0fb909e1
Nolan Ehrstrom <github@nolanpro.com>
2019-11-13 14:18:24 -0800

    Refactor add process modal to use bootstrap vue

--- resources/js/processes/index.js
+++ resources/js/processes/index.js
@@ -10,6 +10,19 @@ new Vue({
         filter: "",
         processModal: false,
         processId: null,
+        processForm: {
+            name: "",
+            selectedFile: "",
+            file: null,
+            categoryOptions: "",
+            description: "",
+            process_category_id: "",
+            addError: {},
+            status: "",
+            bpmn: "",
+            disabled: false,
+            errors: {},
+        }
     },
     components: {
         ProcessesListing
@@ -31,6 +44,70 @@ new Vue({
                 field: "updated_at",
                 direction: "desc"
             }]);
-        }
+        },
+
+
+
+          browse () {
+            this.$refs.customFile.click();
+          },
+          onFileChange (e) {
+            let files = e.target.files || e.dataTransfer.files;
+
+            if (!files.length) {
+              return;
+            }
+
+            this.processForm.selectedFile = files[0].name;
+            this.processForm.file = this.$refs.customFile.files[0];
+          },
+          onClose () {
+            this.processForm.name = "";
+            this.processForm.description = "";
+            this.processForm.process_category_id = "";
+            this.processForm.status = "";
+            this.processForm.addError = {};
+          },
+          onSubmit (event) {
+            event.preventDefault();
+            this.processForm.errors = Object.assign({}, {
+              name: null,
+              description: null,
+              process_category_id: null,
+              status: null
+            });
+            if (this.processForm.process_category_id === "") {
+              this.processForm.addError = {"process_category_id": [this.$t('The category field is required.')]};
+              return;
+            }
+            //single click
+            if (this.processForm.disabled) {
+              return;
+            }
+            this.processForm.disabled = true;
+
+            let formData = new FormData();
+            formData.append("name", this.processForm.name);
+            formData.append("description", this.processForm.description);
+            formData.append("process_category_id", this.processForm.process_category_id);
+            if (this.processForm.file) {
+              formData.append("file", this.processForm.file);
+            }
+
+            ProcessMaker.apiClient.post("/processes", formData,
+              {
+                headers: {
+                  "Content-Type": "multipart/form-data"
+                }
+              })
+              .then(response => {
+                ProcessMaker.alert(this.$t('The process was created.'), "success");
+                window.location = "/modeler/" + response.data.id;
+              })
+              .catch(error => {
+                this.processForm.disabled = false;
+                this.processForm.addError = error.response.data.errors;
+              });
+          }
     }
 });
--- resources/views/processes/list.blade.php
+++ resources/views/processes/list.blade.php
@@ -15,14 +15,13 @@
                     </a>
                 @endcan
                 @can('create-processes')
-                    <a href="#" id="create_process" class="btn btn-secondary" data-toggle="modal" data-target="#addProcess">
+                    <a href="#" id="create_process" class="btn btn-secondary" @click="$refs.addProcess.show()">
                         <i class="fas fa-plus"></i> {{__('Process')}}
                     </a>
                 @endcan
             </div>
         </div>
     </div>
-
 
     <div class="container-fluid">
         <processes-listing
@@ -34,165 +33,65 @@
             :permission="{{ \Auth::user()->hasPermissionsFor('processes') }}"
         ></processes-listing>
     </div>
-</div>
 
-@can('create-processes')
-    <div class="modal" tabindex="-1" role="dialog" id="addProcess">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">{{__('Create Process')}}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="onClose">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+    <b-modal
+        hidden
+        ref="addProcess"
+        title="{{__('Create Process')}}"
+        ok-title="{{__('Save')}}"
+        @ok="onSubmit",
+        @hidden="onClose"
+    >
                 @if ($config->countCategories!== 0)
-                    <div class="modal-body">
                         <div class="form-group">
                             {!! Form::label('name', __('Name')) !!}
                             {!! Form::text('name', null, [
                             'autocomplete' => 'off',
                             'class'=> 'form-control',
-                            'v-model'=> 'name',
-                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':addError.name}']) !!}
+                            'v-model'=> 'processForm.name',
+                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':processForm.addError.name}']) !!}
                             <small class="form-text text-muted"
-                                   v-if="! addError.name">{{ __('The process name must be distinct.') }}</small>
-                            <div class="invalid-feedback" v-for="name in addError.name">@{{name}}</div>
+                                   v-if="! processForm.addError.name">{{ __('The process name must be distinct.') }}</small>
+                            <div class="invalid-feedback" v-for="name in processForm.addError.name">@{{name}}</div>
                         </div>
                         <div class="form-group">
                             {!! Form::label('description', __('Description')) !!}
                             {!! Form::textarea('description', null, [
                             'class'=> 'form-control',
                             'rows' => '3',
-                            'v-model'=> 'description',
-                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':addError.description}']) !!}
-                            <div class="invalid-feedback" v-for="description in addError.description">
+                            'v-model'=> 'processForm.description',
+                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':processForm.addError.description}']) !!}
+                            <div class="invalid-feedback" v-for="description in processForm.addError.description">
                                 @{{description}}
                             </div>
                         </div>
                         <category-select :label="$t('Category')" api-get="process_categories"
-                                         api-list="process_categories" v-model="process_category_id"
-                                         :errors="addError.process_category_id">
+                                         api-list="process_categories" v-model="processForm.process_category_id"
+                                         :errors="processForm.addError.process_category_id">
                         </category-select>
                         <div class="form-group">
                             {!! Form::label('fileName', __('Upload BPMN File (optional)')) !!}
                             <div class="input-group">
-                                <input type="text" name="fileName" class="form-control" v-model="selectedFile">
+                                <input type="text" name="fileName" class="form-control" v-model="processForm.selectedFile">
                                 <button type="button" @click="browse" class="btn btn-secondary"><i
                                         class="fas fa-upload"></i>
                                     {{__('Upload file')}}
                                 </button>
                                 <input type="file" class="custom-file-input"
-                                       :class="{'is-invalid': addError.bpmn && addError.bpmn.length}"
+                                       :class="{'is-invalid': processForm.addError.bpmn && processForm.addError.bpmn.length}"
                                        ref="customFile" @change="onFileChange" accept=".bpmn" style="height: 1em;">
-                                <div class="invalid-feedback" v-for="error in addError.bpmn">@{{error}}</div>
+                                <div class="invalid-feedback" v-for="error in processForm.addError.bpmn">@{{error}}</div>
                             </div>
                         </div>
-                    </div>
                 @else
-                    <div class="modal-body">
                         <div>{{__('Categories are required to create a process')}}</div>
                         <a href="{{ url('designer/processes/categories') }}" class="btn btn-primary container mt-2">
                             {{__('Add Category')}}
                         </a>
-                    </div>
                 @endif
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-dismiss="modal"
-                            @click="onClose">{{__('Cancel')}}</button>
-                    @if ($config->countCategories!== 0)
-                        <button type="button" class="btn btn-secondary ml-2" @click="onSubmit" :disabled="disabled">
-                            {{__('Save')}}
-                        </button>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-@endcan
+    </b-modal>
+</div>
 
 @section('js')
     <script src="{{mix('js/processes/index.js')}}"></script>
-    @can('create-processes')
-        <script>
-          new Vue({
-            el: "#addProcess",
-            data: {
-              name: "",
-              selectedFile: "",
-              categoryOptions: "",
-              description: "",
-              process_category_id: "",
-              addError: {},
-              status: "",
-              bpmn: "",
-              countCategories: @json($config->countCategories),
-              disabled: false
-            },
-            methods: {
-              browse () {
-                this.$refs.customFile.click();
-              },
-              onFileChange (e) {
-                let files = e.target.files || e.dataTransfer.files;
-
-                if (!files.length) {
-                  return;
-                }
-
-                this.selectedFile = files[0].name;
-                this.file = this.$refs.customFile.files[0];
-              },
-              onClose () {
-                this.name = "";
-                this.description = "";
-                this.process_category_id = "";
-                this.status = "";
-                this.addError = {};
-              },
-              onSubmit () {
-                this.errors = Object.assign({}, {
-                  name: null,
-                  description: null,
-                  process_category_id: null,
-                  status: null
-                });
-                if (this.process_category_id === "") {
-                  this.addError = {"process_category_id": ["{{__('The category field is required.')}}"]};
-                  return;
-                }
-                //single click
-                if (this.disabled) {
-                  return;
-                }
-                this.disabled = true;
-
-                let formData = new FormData();
-                formData.append("name", this.name);
-                formData.append("description", this.description);
-                formData.append("process_category_id", this.process_category_id);
-                if (this.file) {
-                  formData.append("file", this.file);
-                }
-
-                ProcessMaker.apiClient.post("/processes", formData,
-                  {
-                    headers: {
-                      "Content-Type": "multipart/form-data"
-                    }
-                  })
-                  .then(response => {
-                    ProcessMaker.alert('{{__('The process was created.')}}', "success");
-                    window.location = "/modeler/" + response.data.id;
-                  })
-                  .catch(error => {
-                    this.disabled = false;
-                    this.addError = error.response.data.errors;
-                  });
-              }
-            }
-          });
-        </script>
-    @endcan
-
 @append
