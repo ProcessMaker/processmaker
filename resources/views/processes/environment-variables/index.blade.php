59189184f5b9f40b625d930dc552acd835a00451
Ryan Cooley <ryan.cooley@processmaker.com>
2019-11-18 11:20:03 -0800

    Update env variable listing with new search bar

--- resources/views/processes/environment-variables/index.blade.php
+++ resources/views/processes/environment-variables/index.blade.php
@@ -16,23 +16,25 @@
 @endsection
 @section('content')
     <div class="px-3 page-content" id="process-variables-listing">
-        <div class="row">
-            <div class="col">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text">
-                        <i class="fas fa-search"></i>
-                        </span>
+        <div id="search-bar" class="search mt-2 bg-light" vcloak>
+            <div class="d-flex flex-column flex-md-row">
+                <div class="flex-grow-1">
+                    <div id="search" class="mb-3 mb-md-0">
+                        <div class="input-group w-100">
+                            <input v-model="filter" class="form-control" placeholder="{{__('Search')}}">
+                            <div class="input-group-append">
+                                <button type="button" class="btn btn-primary" data-original-title="Search"><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
                     </div>
-                    <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
                 </div>
-            </div>
-            <div class="col-8" align="right">
                 @can('create-environment_variables')
-                    <button type="button" id="create_envvar" class="btn btn-secondary" data-toggle="modal"
-                            data-target="#createEnvironmentVariable">
-                        <i class="fas fa-plus"></i> {{__('Environment Variable')}}
-                    </button>
+                    <div class="d-flex ml-md-2 flex-column flex-md-row">
+                        <button type="button" id="create_envvar" class="btn btn-secondary" data-toggle="modal"
+                                data-target="#createEnvironmentVariable">
+                            <i class="fas fa-plus"></i> {{__('Environment Variable')}}
+                        </button>
+                    </div>
                 @endcan
             </div>
         </div>
