b183796aed861bacdc35830e226414dc89e4e146
Ryan Cooley <ryan.cooley@processmaker.com>
2019-11-18 11:12:16 -0800

    Update script listing with new search bar

--- resources/views/processes/scripts/list.blade.php
+++ resources/views/processes/scripts/list.blade.php
@@ -1,20 +1,24 @@
     <div class="px-3 page-content" id="scriptIndex">
-        <div id="search-bar" class="search mt-2 bg-light p-2" vcloak>
-            <div class="d-flex">
+        <div id="search-bar" class="search mt-2 bg-light" vcloak>
+            <div class="d-flex flex-column flex-md-row">
                 <div class="flex-grow-1">
-                    <div id="search" class="pr-2">
-                        <input v-model="filter" class="form-control" placeholder="{{__('Search')}}...">
+                    <div id="search" class="mb-3 mb-md-0">
+                        <div class="input-group w-100">
+                            <input v-model="filter" class="form-control" placeholder="{{__('Search')}}">
+                            <div class="input-group-append">
+                                <button type="button" class="btn btn-primary" data-original-title="Search"><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="flex-shrink-0">
-                    <button title="" type="button" class="btn btn-primary" data-original-title="Search"><i class="fas fa-search"></i></button>
-                    @can('create-scripts')
+                @can('create-scripts')
+                    <div class="d-flex ml-md-2 flex-column flex-md-row">
                         <a href="#" id="create_script" class="btn btn-secondary" data-toggle="modal"
                            data-target="#addScript"><i
                                     class="fas fa-plus"></i>
                             {{__('Script')}}</a>
-                    @endcan
-                </div>
+                    </div>
+                @endcan
             </div>
         </div>
 
