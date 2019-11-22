d08f880aea540975647d55ad1d24533fe1d49bef
David Callizaya <davidcallizaya@gmail.com>
2019-11-22 17:39:25 -0400

    Add iframe component

--- resources/js/processes/screen-builder/advancedScreen.vue
+++ resources/js/processes/screen-builder/advancedScreen.vue
@@ -1,32 +1,71 @@
 <template>
   <div class="h-100">
-    <monaco-editor :options="monacoOptions" v-model="code" language="html" class="editor"></monaco-editor>
+    <div class="p-2">
+      <b-button-group size="sm">
+        <b-button
+          :variant="mode == 'editor' ? 'secondary' : 'outline-secondary'"
+          @click="mode = 'editor'"
+          class="text-capitalize"
+        >
+          <i class="fas fa-drafting-compass pr-1"></i>
+          {{ $t('Design') }}
+        </b-button>
+        <b-button
+          :variant="mode == 'preview' ? 'secondary' : 'outline-secondary'"
+          @click="mode = 'preview'"
+          class="text-capitalize"
+        >
+          <i class="fas fa-cogs pr-1"></i>
+          {{ $t('Preview') }}
+        </b-button>
+      </b-button-group>
+    </div>
+    <monaco-editor
+      v-show="mode == 'editor'"
+      :options="monacoOptions"
+      v-model="config.html"
+      language="html"
+      class="editor"
+    ></monaco-editor>
+    <advanced-screen-frame v-show="mode == 'preview'" :config="config"></advanced-screen-frame>
   </div>
 </template>
 
 <script>
 import MonacoEditor from "vue-monaco";
+import AdvancedScreenFrame from "./advancedScreenFrame";
 
 export default {
   props: ["process", "screen", "permission"],
   data() {
     return {
       monacoOptions: {
-        automaticLayout: true,
+        automaticLayout: true
       },
-      code: "",
+      config: { html: "" },
+      mode: "editor",
       showPopup: false
     };
   },
   components: {
-    MonacoEditor
+    MonacoEditor,
+    AdvancedScreenFrame
+  },
+  watch: {
+    screen: {
+      deep: true,
+      immediate: true,
+      handler(screen) {
+        screen.config ? (this.config = screen.config) : null;
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-  .editor {
-    min-height: 200px;
-    height: 100%;
-  }
+.editor {
+  min-height: 200px;
+  height: 100%;
+}
 </style>
---
+++ resources/js/processes/screen-builder/advancedScreenFrame.vue
@@ -0,0 +1,32 @@
+<template>
+  <iframe @load="loadIFrame" class="border-0 w-100 h-100"></iframe>
+</template>
+
+<script>
+export default {
+  props: ["config"],
+  data() {
+    return {};
+  },
+  methods: {
+    loadIFrame() {
+      const html = this.config.html && this.config.html ? this.config.html : "";
+      const document = this.$el.contentDocument;
+      if (document) {
+        document.open("text/html", "replace");
+        document.write(html);
+        document.close();
+      }
+    }
+  },
+  watch: {
+    config: {
+      deep: true,
+      immediate: true,
+      handler() {
+        this.loadIFrame();
+      }
+    }
+  }
+};
+</script>
