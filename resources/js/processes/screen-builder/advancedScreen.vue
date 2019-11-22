b4a26a8c43b4f298340f0a39adb0e821b68fc012
David Callizaya <davidcallizaya@gmail.com>
2019-11-22 16:04:20 -0400

    Add advanced editor component

---
+++ resources/js/processes/screen-builder/advancedScreen.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="h-100">
+    <monaco-editor :options="monacoOptions" v-model="code" language="html" class="editor"></monaco-editor>
+  </div>
+</template>
+
+<script>
+import MonacoEditor from "vue-monaco";
+
+export default {
+  props: ["process", "screen", "permission"],
+  data() {
+    return {
+      monacoOptions: {
+        automaticLayout: true,
+      },
+      code: "",
+      showPopup: false
+    };
+  },
+  components: {
+    MonacoEditor
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .editor {
+    min-height: 200px;
+    height: 100%;
+  }
+</style>
