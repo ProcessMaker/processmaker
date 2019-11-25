224ae10c790e455f10bfdc7eb8ca2e3b0a941027
dante <dante@danteloayza.localdomain>
2019-11-25 09:49:56 -0400

    Placeholders for setting token and request data in new Advanced Form template

--- ProcessMaker/Models/Screen.php
+++ ProcessMaker/Models/Screen.php
@@ -147,6 +147,12 @@ class Screen extends Model
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        ProcessMaker = {
+            apiToken: \'PM_API_TOKEN\',
+            requestData: PM_REQUEST_DATA
+        }        
+    </script>
     <!--Do Not Delete Above -->
 </head>
 
