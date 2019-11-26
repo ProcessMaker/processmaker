281d4316a909009890418b0e864df5cdf0faa8fe
Marco A. Nina Mena <marcoantonina@gmail.com>
2019-11-26 09:11:11 -0400

    Vue in modal

--- resources/views/categories/list.blade.php
+++ resources/views/categories/list.blade.php
@@ -81,9 +81,84 @@
 </div>
 
 @section('js')
-    <script>
-      ProcessMaker.user.category = {};
-      ProcessMaker.user.category.route = @json(route($config->routes->categoryListApi));
-    </script>
     <script src="{{mix('js/processes/categories/index.js')}}"></script>
+    <script>
+      ProcessMaker.CategoriesIndex = new Vue({
+        el: "#categories-listing",
+        data: {
+          filter: "",
+          formData: null,
+          errors: {},
+          id: "",
+          name: "",
+          status: "ACTIVE",
+          disabled: false,
+          route: @json(route($config->routes->categoryListApi))
+        },
+        methods: {
+          emptyData () {
+            this.id = "";
+            this.name = "";
+            this.status = "ACTIVE";
+            this.disabled = false;
+            this.errors = {};
+          },
+          getTitle () {
+            return this.id ? this.$t("Edit Category") : this.$t("Create Category");
+          },
+          reload () {
+            this.$refs.list.fetch();
+          },
+          edit (value) {
+            this.emptyData();
+            this.id = value.id;
+            this.name = value.name;
+            this.status = value.status;
+            $("#createCategory").modal("show");
+          },
+          onClose () {
+            this.emptyData();
+          },
+          onSubmit () {
+            this.errors = {};
+            // single click
+            if (this.disabled) {
+              return;
+            }
+            this.disabled = true;
+            let method = "POST",
+              url = this.route;
+            if (this.id) {
+              // Do an update
+              method = "PUT";
+              url = `${url}/${this.id}`;
+            }
+            ProcessMaker.apiClient({
+              method,
+              url,
+              baseURL: "/",
+              data: {
+                name: this.name,
+                status: this.status
+              }
+            })
+              .then((response) => {
+                $("#createCategory").modal("hide");
+                let message = "The category was created.";
+                if (this.id) {
+                  message = "The category was saved.";
+                }
+                ProcessMaker.alert(this.$t(message), "success");
+                this.emptyData();
+                this.reload();
+              }).catch((error) => {
+              this.disabled = false;
+              if (error.response.status === 422) {
+                this.errors = error.response.data.errors;
+              }
+            });
+          }
+        }
+      });
+    </script>
 @append
