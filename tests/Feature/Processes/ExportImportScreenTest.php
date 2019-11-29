190f718e5967f53069bb53f8a2ea8179805d877c
danloa <danloa@gmail.com>
2019-11-29 14:50:32 -0400

    Fix to test because now export respects the screen name case

--- tests/Feature/Processes/ExportImportScreenTest.php
+++ tests/Feature/Processes/ExportImportScreenTest.php
@@ -61,7 +61,7 @@ class ExportImportScreenTest extends TestCase
         // Test to ensure we can download the exported file
         $response = $this->webCall('GET', $response->json('url'));
         $response->assertStatus(200);
-        $response->assertHeader('content-disposition', 'attachment; filename=approve.json');
+        $response->assertHeader('content-disposition', 'attachment; filename=Approve.json');
 
         // Get our file contents (we have to do it this way because of
         // Symfony's weird response API)
