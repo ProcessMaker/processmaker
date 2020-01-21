0cc42a1cc29aed61e825e9f4a6e31d16a407f80e
Ryan Cooley <ryan.cooley@processmaker.com>
2020-01-21 09:19:20 -0800

    Improving organization & robustness of test

--- tests/Feature/Console/InstallTest.php
+++ tests/Feature/Console/InstallTest.php
@@ -29,7 +29,7 @@ class InstallTest extends TestCase
     }
     
     /**
-     * Parse the table output of the Artisan command
+     * Generate parameters to feed into the console command
      *
      * @return Illuminate\Support\Collection
      */
@@ -37,19 +37,24 @@ class InstallTest extends TestCase
     {
         $faker = Faker::create();
         
+        $dataDriver = $faker->randomElement(['mysql', 'pgsql', 'sqlsrv']);
+        $dataSchema = $dataDriver == 'pgsql' ? 'public' : null;
+        
         $params = collect([]);
         $params->push(new Param('--pretend'));
         $params->push(new Param('--no-interaction'));
+        $params->push(new Param('--app-debug', 'APP_DEBUG', $faker->boolean));
         $params->push(new Param('--url', 'APP_URL', "https://{$faker->domainName}"));
         $params->push(new Param('--username', null, $faker->userName));
         $params->push(new Param('--password', null, $faker->word));
         $params->push(new Param('--email', null, $faker->safeEmail));
+        $params->push(new Param('--telescope', 'TELESCOPE_ENABLED', $faker->boolean));
         $params->push(new Param('--db-host', 'DB_HOSTNAME', $faker->ipv4));
         $params->push(new Param('--db-port', 'DB_PORT', $faker->numberBetween(1, 9999)));
         $params->push(new Param('--db-name', 'DB_DATABASE', $faker->word));
         $params->push(new Param('--db-username', 'DB_USERNAME', $faker->userName));
         $params->push(new Param('--db-password', 'DB_PASSWORD', $faker->word));
-        $params->push(new Param('--data-driver', 'DATA_DB_DRIVER', $faker->randomElement(['mysql', 'pgsql', 'sqlsrv'])));
+        $params->push(new Param('--data-driver', 'DATA_DB_DRIVER', $dataDriver));
         $params->push(new Param('--data-host', 'DATA_DB_HOST', $faker->ipv4));
         $params->push(new Param('--data-port', 'DATA_DB_PORT', $faker->numberBetween(1, 9999)));
         $params->push(new Param('--data-name', 'DATA_DB_DATABASE', $faker->word));
@@ -58,18 +63,22 @@ class InstallTest extends TestCase
         $params->push(new Param('--redis-client', 'REDIS_CLIENT', $faker->randomElement(['phpredis', 'predis'])));
         $params->push(new Param('--redis-prefix', 'REDIS_PREFIX', "{$faker->word}:"));
         $params->push(new Param('--horizon-prefix', 'HORIZON_PREFIX', "{$faker->word}:"));
-        $params->push(new Param('--telescope', 'TELESCOPE_ENABLED', $faker->boolean));
+        $params->push(new Param('--broadcast-debug', 'LARAVEL_ECHO_SERVER_DEBUG', $faker->boolean));
+        $params->push(new Param('--broadcast-driver', 'BROADCAST_DRIVER', $faker->randomElement(['pusher', 'redis'])));
+        $params->push(new Param('--broadcast-host', 'BROADCASTER_HOST', "https://{$faker->domainName}"));
+        $params->push(new Param('--broadcast-key', 'BROADCASTER_KEY', $faker->md5));
+        $params->push(new Param('--echo-host', 'LARAVEL_ECHO_SERVER_AUTH_HOST', "https://{$faker->domainName}"));
+        $params->push(new Param('--echo-port', 'LARAVEL_ECHO_SERVER_PORT', $faker->numberBetween(1, 9999)));
         $params->push(new Param('--pusher-app-id', 'PUSHER_APP_ID', $faker->word));
         $params->push(new Param('--pusher-app-key', 'PUSHER_APP_KEY', $faker->md5));
         $params->push(new Param('--pusher-app-secret', 'PUSHER_APP_SECRET', $faker->sha1));
         $params->push(new Param('--pusher-cluster', 'PUSHER_CLUSTER', $faker->word));
-        $params->push(new Param('--broadcast-driver', 'BROADCAST_DRIVER', $faker->randomElement(['pusher', 'redis'])));
-        $params->push(new Param('--broadcast-host', 'BROADCASTER_HOST', "https://{$faker->domainName}"));
-        $params->push(new Param('--broadcast-key', 'BROADCASTER_KEY', $faker->md5));
-        $params->push(new Param('--broadcast-debug', 'LARAVEL_ECHO_SERVER_DEBUG', $faker->boolean));
-        $params->push(new Param('--echo-host', 'LARAVEL_ECHO_SERVER_AUTH_HOST', "https://{$faker->domainName}"));
-        $params->push(new Param('--echo-port', 'LARAVEL_ECHO_SERVER_PORT', $faker->numberBetween(1, 9999)));
-
+        $params->push(new Param('--pusher-tls', 'PUSHER_TLS', $faker->boolean));
+        
+        if ($dataSchema) {
+            $params->push(new Param('--data-schema', 'DATA_DB_SCHEMA', $dataSchema));
+        }
+        
         return $params;
     }
     
