0811e30a2b25b32ba1e456e05999a3c0ba0c9477
Nolan Ehrstrom <github@nolanpro.com>
2020-01-27 16:28:54 -0800

    Add more descriptive expresion syntax error

--- ProcessMaker/Exception/SyntaxErrorException.php
+++ ProcessMaker/Exception/SyntaxErrorException.php
@@ -17,8 +17,11 @@ class SyntaxErrorException extends Exception
     /**
      * @param Throwable $previous
      */
-    public function __construct(Throwable $previous)
+    public function __construct(Throwable $previous, String $body)
     {
-        parent::__construct(__('exceptions.SyntaxErrorException', ['error' => $previous->getMessage()]), 0, $previous);
+        parent::__construct(__('exceptions.SyntaxErrorException', [
+            'error' => $previous->getMessage(),
+            'body' => $body
+        ]), 0, $previous);
     }
 }
