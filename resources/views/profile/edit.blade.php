856e184fc2657e9754cdc7eb5ef9676027fdaefa
danloa <dante.loayza@processmaker.com>
2020-01-31 09:27:55 -0400

    Remove code that added addons, now they are in the layout page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -75,15 +75,6 @@
 @section('js')
 	<script src="{{mix('js/admin/profile/edit.js')}}"></script>
 
-    <script>
-        var addons = [];
-    </script>
-    @foreach ($addons as $addon)
-        @if (!empty($addon['script']))
-            {!! $addon['script'] !!}
-        @endif
-    @endforeach
-
 <script>
         let formVueInstance = new Vue({
             el: '#profileForm',
