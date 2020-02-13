da77d2523cbfa3798300cad89618010e03f8ab3c
sanjacornelius <sanja.cornelius@processmaker.com>
2020-02-13 11:22:43 -0800

    Complete Broadcast Testing

--- tests/Feature/BroadcastTest.php
+++ tests/Feature/BroadcastTest.php
@@ -4,7 +4,10 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Tests\Feature\Shared\LoggingHelper;
 use ProcessMaker\Events\ActivityAssigned;
+use ProcessMaker\Events\ActivityCompleted;
+use ProcessMaker\Events\ProcessCompleted;
 use ProcessMaker\Models\ProcessRequestToken as Task;
+use ProcessMaker\Models\ProcessRequest as Request;
 
 class BroadcastTest extends TestCase
 {
@@ -21,5 +24,44 @@ class BroadcastTest extends TestCase
         event(new ActivityAssigned($task));
         $this->assertLogContainsText('ActivityAssigned');
         $this->assertLogContainsText(addcslashes(route('api.tasks.show', ['task' => $task->id]), '/'));
+    }
+
+     /**
+     * Asserts that the ActivityCompleted broadcast event works.
+     *
+     * @return void
+     */
+    public function testActivityCompletedBroadcast()
+    {
+        $task = factory(Task::class)->create();
+        event(new ActivityCompleted($task));
+        $this->assertLogContainsText('ActivityCompleted');
+        $this->assertLogContainsText(addcslashes(route('api.tasks.show', ['task' => $task->id]), '/'));
+    }
+
+     /**
+     * Asserts that the ProcessCompleted broadcast event works.
+     *
+     * @return void
+     */
+    public function testProcessCompletedBroadcast()
+    {
+        $request = factory(Request::class)->create();
+        event(new ProcessCompleted($request));
+        $this->assertLogContainsText('ProcessCompleted');
+        $this->assertLogContainsText(addcslashes(route('api.requests.show', ['task' => $request->id]), '/'));
+    }
+
+    /**
+     * Asserts that the ProcessUpdated broadcast event works.
+     *
+     * @return void
+     */
+    public function testProcessUpdatedBroadcast()
+    {
+        $request = factory(Request::class)->create();
+        event(new ProcessCompleted($request));
+        $this->assertLogContainsText('ProcessCompleted');
+        $this->assertLogContainsText(addcslashes(route('api.requests.show', ['task' => $request->id]), '/'));
     }
 }
