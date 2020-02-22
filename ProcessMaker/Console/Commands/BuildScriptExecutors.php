d36daf3c670a2b44ded53a9fb474038f80d7a448
Nolan Ehrstrom <github@nolanpro.com>
2020-02-21 16:16:11 -0800

    Add artisan command to build executor containers

---
+++ ProcessMaker/Console/Commands/BuildScriptExecutors.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace ProcessMaker\Console\Commands;
+
+use Illuminate\Console\Command;
+use ProcessMaker\BuildSdk;
+use \Exception;
+
+class BuildScriptExecutors extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     *
+     * @var string
+     */
+    protected $signature = 'processmaker:build-script-executor {lang}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $lang = $this->argument('lang');
+        $this->info("Building for language: $lang");
+
+        $this->info("Generating SDK json document");
+        \Artisan::call('l5-swagger:generate');
+
+        $dockerDir = sys_get_temp_dir() . "/pm4-docker-builds/${lang}";
+        $sdkDir = $dockerDir . "/sdk";
+
+        if (!is_dir($sdkDir)) {
+            mkdir($sdkDir, 0755, true);
+        }
+
+        $this->info("Building the SDK");
+        \Artisan::call("processmaker:sdk", [
+            'language' => $lang,
+            'output' => $sdkDir
+        ]);
+
+        $dockerfile = '';
+        $initDockerfile = config('script-runners.' . $lang . '.init_dockerfile');
+        if ($initDockerfile) {
+            $dockerfile .= $initDockerfile;
+        }
+        $dockerfile .= "\n";
+        $userDockerfilePath = storage_path("docker-build-config/Dockerfile-${lang}");
+        if (file_exists($userDockerfilePath)) {
+            $dockerfile .= file_get_contents($userDockerfilePath);
+        }
+
+        $this->info("Dockerfile:\n  " . implode("\n  ", explode("\n", $dockerfile)));
+        file_put_contents($dockerDir . '/Dockerfile', $dockerfile);
+
+        $this->info("Building the docker executor");
+        system("docker build -t processmaker4/executor-${lang}:latest ${dockerDir}");
+    }
+}
