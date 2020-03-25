4bd33e508d943bb897135ca900efa5fe7717b0b3
danloa <danloa@hotmail.com>
2020-03-25 09:06:25 -0400

    Mustache tests

--- tests/Feature/MustacheFEELTest.php
+++ tests/Feature/MustacheFEELTest.php
@@ -14,6 +14,36 @@ class MustacheFEELTest extends TestCase
 {
 
     /**
+     * Test basic mustache templating
+     */
+    public function testMustacheTemplates()
+    {
+        $expresion = new FormalExpression();
+        $expresion->setLanguage('FEEL');
+
+        $expresion->setBody("{{Value}}");
+        $response = $expresion(["Value" => 11]);
+        $this->assertSame(11, $response);
+    }
+
+    /**
+     * Test basic mustache with FEEL string concatenation
+     */
+    public function testStringConcatenation()
+    {
+        $expresion = new FormalExpression();
+        $expresion->setLanguage('FEEL');
+
+        $expresion->setBody("{{Value}}~'Test'");
+        $response = $expresion(["Value" => 11]);
+        $this->assertSame("11Test", $response);
+
+        $expresion->setBody("'{{Value}}'~'{{Other}}'");
+        $response = $expresion(["Value" => 11, "Other" => 22]);
+        $this->assertSame("1122", $response);
+    }
+
+    /**
      * Test formulas with mustache
      */
     public function testFormulaAndMustache()
