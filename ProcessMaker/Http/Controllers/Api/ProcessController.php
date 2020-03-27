1419c5eaca11340d2d8bc17c080d641d89e9dc60
Gerrie Swart <g.swart@vehikl.com>
2020-03-27 17:30:07 -0400

    Remove unused import of Log

--- ProcessMaker/Http/Controllers/Api/ProcessController.php
+++ ProcessMaker/Http/Controllers/Api/ProcessController.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use ProcessMaker\Exception\TaskDoesNotHaveUsersException;
 use ProcessMaker\Facades\WorkflowManager;
