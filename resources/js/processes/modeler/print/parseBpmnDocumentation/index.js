1c23cbc0f94a9d6d23e3b9237cfc2c02ddaf44cb
Gerrie Swart <g.swart@vehikl.com>
2020-03-27 17:33:46 -0400

    Extract filter logic

--- resources/js/processes/modeler/print/parseBpmnDocumentation/index.js
+++ resources/js/processes/modeler/print/parseBpmnDocumentation/index.js
@@ -2,33 +2,49 @@ import getFullyQualifiedNodeType from './typeParser';
 import { getNodeName, hasNonEmptyName } from './nameParser';
 import { nodeDocumentation, nodeText } from './documentationParser';
 
+const allowedName = (bpmnNode) => {
+  const nodesToIncludeAnyway = [
+    'bpmn:textAnnotation',
+    'bpmn:messageFlow',
+    'bpmn:sequenceFlow',
+  ];
+  return hasNonEmptyName(bpmnNode) || nodesToIncludeAnyway.includes(bpmnNode.tagName);
+};
+
+const editorSpecificNodes = (bpmnNode) => {
+  const adonis = 'adonis:';
+
+  const appSpecificNamespaces = [adonis];
+
+  return !appSpecificNamespaces.some((appNamespace) => bpmnNode.tagName.includes(appNamespace));
+};
+
+const undocumentableNodes = (bpmnNode) => {
+  const nodesThatCannotBeDocumented = [
+    'bpmn:process',
+    'bpmn:error',
+    'bpmn:message',
+    'bpmn:signal',
+    'signal',
+  ];
+  return !nodesThatCannotBeDocumented.includes(bpmnNode.tagName);
+};
+
 function documentableBpmnNodes(bpmnString) {
   const bpmnDoc = new DOMParser().parseFromString(bpmnString, 'text/xml');
-  const nodesWithNonEmptyId = Array.from(bpmnDoc.querySelectorAll('*[id]:not([id=""])'));
-
-  const withNonEmptyNames = nodesWithNonEmptyId.filter((bpmnNode) => {
-    const nodesToIncludeAnyway = [
-      'bpmn:textAnnotation',
-      'bpmn:messageFlow',
-      'bpmn:sequenceFlow',
-    ];
-    return hasNonEmptyName(bpmnNode) || nodesToIncludeAnyway.includes(bpmnNode.tagName);
-  });
-
-  const documentableNodes = withNonEmptyNames.filter((bpmnNode) => {
-    const nodesThatCannotBeDocumented = ['bpmn:process', 'bpmn:error', 'bpmn:message', 'bpmn:signal'];
-    return !nodesThatCannotBeDocumented.includes(bpmnNode.tagName);
-  });
-
-  return documentableNodes.map((bpmnNode) => {
-    return {
-      id: bpmnNode.attributes.getNamedItem('id').textContent,
-      type: getFullyQualifiedNodeType(bpmnNode),
-      name: getNodeName(bpmnNode),
-      documentationHtml: nodeDocumentation(bpmnNode),
-      textHtml: nodeText(bpmnNode),
-    };
-  });
+  return Array.from(bpmnDoc.querySelectorAll('*[id]:not([id=""])'))
+      .filter(allowedName)
+      .filter(editorSpecificNodes)
+      .filter(undocumentableNodes)
+      .map((bpmnNode) => {
+        return {
+          id: bpmnNode.attributes.getNamedItem('id').textContent,
+          type: getFullyQualifiedNodeType(bpmnNode),
+          name: getNodeName(bpmnNode),
+          documentationHtml: nodeDocumentation(bpmnNode),
+          textHtml: nodeText(bpmnNode),
+        };
+      });
 }
 
 export default documentableBpmnNodes;
