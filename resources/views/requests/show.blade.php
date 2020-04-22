344c95f67816837716d8cfecb81f207966f46cca
Ryan Cooley <ryan.cooley@processmaker.com>
2020-04-22 11:15:22 -0700

    Remove padding

--- resources/views/requests/show.blade.php
+++ resources/views/requests/show.blade.php
@@ -167,7 +167,7 @@
                         </div>
 
                         <template v-for="{ tab, component } in packages">
-                          <div class="tab-pane fade card card-body border-top-0 p-3" :id="tab.target" role="tabpanel">
+                          <div class="tab-pane fade card card-body border-top-0 p-0" :id="tab.target" role="tabpanel">
                             <component :is="component" :process-request-id="requestId"></component>
                           </div>
                         </template>
