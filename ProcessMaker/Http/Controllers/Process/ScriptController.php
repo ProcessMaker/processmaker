dea493a90e0c4b4fbc04d09dfebb93b1bd6a51f6
Nolan Ehrstrom <github@nolanpro.com>
2020-05-14 10:22:00 -0700

    Filter script executors by set language

--- ProcessMaker/Http/Controllers/Process/ScriptController.php
+++ ProcessMaker/Http/Controllers/Process/ScriptController.php
@@ -55,7 +55,7 @@ class ScriptController extends Controller
     public function edit(Script $script, User $users)
     {
         $selectedUser = $script->runAsUser;
-        $scriptExecutors = ScriptExecutor::list();
+        $scriptExecutors = ScriptExecutor::list($script->scriptExecutor->language);
         $addons = $this->getPluginAddons('edit', compact(['script']));
 
         return view('processes.scripts.edit', compact('script', 'selectedUser', 'scriptExecutors', 'addons'));
