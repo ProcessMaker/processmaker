6f7723070ee4c77786c15c7e42bd06dc6bc5be43
Marco A. Nina Mena <marcoantonina@gmail.com>
2020-06-01 11:18:10 -0400

    Add Pagination

--- resources/js/admin/auth-clients/components/AuthClientsListing.vue
+++ resources/js/admin/auth-clients/components/AuthClientsListing.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="data-table">
     <data-loading
-            :for="/clients/"
-            v-show="shouldShowLoader"
-            :empty="$t('No Data Available')"
-            :empty-desc="$t('')"
-            empty-icon="noData"
+      :for="/clients/"
+      v-show="shouldShowLoader"
+      :empty="$t('No Data Available')"
+      :empty-desc="$t('')"
+      empty-icon="noData"
     />
-    <div v-show="!shouldShowLoader"  class="card card-body table-card">
+    <div v-show="!shouldShowLoader" class="card card-body table-card">
       <vuetable
+        ref="vuetable"
         :dataManager="dataManager"
+        :sortOrder="sortOrder"
         :css="css"
         :api-mode="false"
+        @vuetable:pagination-data="onPaginationData"
         :fields="fields"
         :data="data"
         data-path="data"
+        pagination-path="meta"
         :noDataTemplate="$t('No Data Available')"
       >
         <template slot="actions" slot-scope="props">
@@ -52,6 +56,14 @@
           {{ props.rowData.secret.substr(0, 10) }}...
         </template>
       </vuetable>
+      <pagination
+        :single="$t('Auth Client')"
+        :plural="$t('Auth Clients')"
+        :perPageSelectEnabled="true"
+        @changePerPage="changePerPage"
+        @vuetable-pagination:change-page="onPageChange"
+        ref="pagination"
+      ></pagination>
       <textarea class="copytext" ref="copytext"></textarea>
     </div>
   </div>
@@ -67,32 +79,39 @@ export default {
   data() {
     return {
       copytext: "",
+      sortOrder: [
+        {
+          field: "name",
+          sortField: "name",
+          direction: "asc",
+        },
+      ],
 
       fields: [
         {
           title: () => this.$t("Client ID"),
-          name: "id"
+          name: "id",
         },
         {
           title: () => this.$t("Name"),
-          name: "name"
+          name: "name",
         },
         {
           title: () => this.$t("Redirect"),
           name: "redirect",
           callback(val) {
             return val.substr(0, 20) + "...";
-          }
+          },
         },
         {
           title: () => this.$t("Client Secret"),
-          name: "__slot:secret"
+          name: "__slot:secret",
         },
         {
           name: "__slot:actions",
-          title: ""
-        }
-      ]
+          title: "",
+        },
+      ],
     };
   },
   methods: {
@@ -101,10 +120,46 @@ export default {
       // Load from our api client
       ProcessMaker.apiClient
         .get("/oauth/clients", { baseURL: "/" })
-        .then(response => {
-          this.data = response.data.data;
+        .then((response) => {
+          this.data = this.transform(response.data.data);
           this.loading = false;
         });
+    },
+    transform(data) {
+      let meta = {};
+      meta.total = data.length;
+      meta.current_page = this.page;
+      meta.per_page = this.perPage;
+      meta.from = (meta.current_page - 1) * meta.per_page;
+      meta.total_pages = Math.floor(meta.total / meta.per_page) + 1;
+      meta.last_page = meta.total_pages;
+      meta.to = meta.from + this.perPage;
+      if (meta.to > meta.total) {
+        meta.to = meta.total;
+      }
+
+      let rows = data.slice(meta.from, meta.to);
+      meta.count = rows.length;
+
+      this.$refs.pagination.tablePagination = meta;
+      return rows;
+    },
+    onPageChange(page) {
+      if (page == "next") {
+        this.page = this.page + 1;
+      } else if (page == "prev") {
+        this.page = this.page - 1;
+      } else {
+        this.page = page;
+      }
+      if (this.page <= 0) {
+        this.page = 1;
+      }
+      let meta = this.$refs.pagination.tablePagination;
+      if (this.page > meta.last_page) {
+        this.page = meta.last_page;
+      }
+      this.fetch();
     },
     edit(row) {
       this.$emit("edit", Object.assign({}, row));
@@ -117,7 +172,8 @@ export default {
     doDelete(item) {
       ProcessMaker.confirmModal(
         this.$t("Caution!"),
-        this.$t("Are you sure you want to delete the auth client") + ' ' +
+        this.$t("Are you sure you want to delete the auth client") +
+          " " +
           item.name +
           this.$t("?"),
         "",
@@ -125,13 +181,16 @@ export default {
           ProcessMaker.apiClient
             .delete("/oauth/clients/" + item.id, { baseURL: "/" })
             .then(() => {
-              ProcessMaker.alert(this.$t('The auth client was deleted.'), 'success');
+              ProcessMaker.alert(
+                this.$t("The auth client was deleted."),
+                "success"
+              );
               this.fetch();
             });
         }
       );
-    }
-  }
+    },
+  },
 };
 </script>
 
