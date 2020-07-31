b5bc1de3b84db5026d118743bdf7ca677bb03f74
gustavo-romero <43180174+gustavo-romero@users.noreply.github.com>
2020-07-30 23:04:54 -0400

    Set secure cookie flag

--- config/session.php
+++ config/session.php
@@ -173,7 +173,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
