1a5ec92d90f0d07ad31d414bba2a96b92025bdf7
gustavo-romero <43180174+gustavo-romero@users.noreply.github.com>
2020-07-30 23:03:47 -0400

    Set cookie expiration on browser close

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
