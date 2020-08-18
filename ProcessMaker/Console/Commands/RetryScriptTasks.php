bfa4f356d5a4c3db722a71a2c026163b98b68c1f
danloa <danloa@hotmail.com>
2020-08-18 18:09:52 -0400

    Remove tokens from locks table before attempting run them

--- ProcessMaker/Console/Commands/RetryScriptTasks.php
+++ ProcessMaker/Console/Commands/RetryScriptTasks.php
@@ -5,6 +5,7 @@ namespace ProcessMaker\Console\Commands;
 use Illuminate\Console\Command;
 use ProcessMaker\Jobs\RunScriptTask;
 use ProcessMaker\Models\ProcessRequestToken;
+use ProcessMaker\Models\ProcessRequestLock;
 
 class RetryScriptTasks extends Command
 {
@@ -45,6 +46,9 @@ class RetryScriptTasks extends Command
     {
         $tasks = $this->retrieveTaskList();
 
+   	$requestIds = $tasks->pluck('process_request_id');
+        ProcessRequestLock::whereIn('process_request_id', $requestIds)->delete();
+
         if (!$tasks->count()) {
             exit($this->error('No failing script tasks found.'));
         }
@@ -65,7 +69,8 @@ class RetryScriptTasks extends Command
 
     private function retrieveTaskList()
     {
-        $tasks = ProcessRequestToken::whereIn('status', array('FAILING', 'ACTIVE'))->where('element_type', 'scriptTask');
+        //$tasks = ProcessRequestToken::where('status', 'FAILING')->where('element_type', 'scriptTask');
+	$tasks = ProcessRequestToken::whereIn('status', array('FAILING', 'ACTIVE'))->where('element_type', 'scriptTask');
 
         if ($this->option('process') && $this->option('request')) {
             exit($this->error('Please specify either a Process ID or a Request ID, not both.'));
