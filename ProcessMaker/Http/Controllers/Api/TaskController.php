44790aaeb66d73c818382ddfabc24b015d66946c
Ryan Cooley <ryancooley@gmail.com>
2020-09-25 14:21:52 -0700

    Persist user data upon claiming self-service task

--- ProcessMaker/Http/Controllers/Api/TaskController.php
+++ ProcessMaker/Http/Controllers/Api/TaskController.php
@@ -293,6 +293,7 @@ class TaskController extends Controller
                 // Claim task
                 $task->is_self_service = 0;
                 $task->user_id = $userToAssign;
+                $task->persistUserData($userToAssign);
             } else {
                 // Validate if user can reassign
                 $task->authorizeReassignment(Auth::user());
