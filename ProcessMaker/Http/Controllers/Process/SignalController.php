15915c2c315c0f09cba210c0a0ab2eea0ce7c995
Marco Antonio Nina Mena <marcoantonina@gmail.com>
2020-11-05 10:00:52 -0400

    Add view signal

---
+++ ProcessMaker/Http/Controllers/Process/SignalController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ProcessMaker\Http\Controllers\Process;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use ProcessMaker\Http\Controllers\Controller;
+use ProcessMaker\Models\Screen;
+use ProcessMaker\Traits\HasControllerAddons;
+
+class SignalController extends Controller
+{
+    use HasControllerAddons;
+
+    /**
+     * Get the list of signals
+     *
+     * @param Request $request
+     * 
+     * @return Factory|View
+     */
+    public function index(Request $request)
+    {
+        return view('processes.signals.index');
+    }
+}
