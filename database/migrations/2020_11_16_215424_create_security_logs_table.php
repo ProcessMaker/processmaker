2e892419ec7f670715662eb36aa5fba825315042
Ryan Cooley <ryancooley@gmail.com>
2020-11-16 17:00:56 -0800

    Add permissions to migration

--- database/migrations/2020_11_16_215424_create_security_logs_table.php
+++ database/migrations/2020_11_16_215424_create_security_logs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use ProcessMaker\Models\Permission;
 
 class CreateSecurityLogsTable extends Migration
 {
@@ -23,6 +24,14 @@ class CreateSecurityLogsTable extends Migration
             
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
+        
+        if (! Permission::where('name', 'view-security-logs')->first()) {
+            factory(Permission::class)->create([
+                'title' => 'View Security Logs',
+                'name' => 'view-security-logs',
+                'group' => 'Security Logs',
+            ]);
+        }
     }
 
     /**
@@ -33,5 +42,9 @@ class CreateSecurityLogsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('security_logs');
+        
+        if ($permission = Permission::where('name', 'view-security-logs')->first()) {
+            $permission->delete();
+        }
     }
 }
