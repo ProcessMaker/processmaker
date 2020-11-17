703e7b7915c3a55b240c39f5e7c6c0091a673dc6
Ryan Cooley <ryancooley@gmail.com>
2020-11-17 12:47:55 -0800

    Add testing for logger and API

---
+++ tests/Feature/SecurityLoggerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use PermissionSeeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use ProcessMaker\Models\User;
+use ProcessMaker\Models\SecurityLog;
+use Tests\Feature\Shared\RequestHelper;
+
+class SecurityLoggerTest extends TestCase
+{
+    /**
+     * Create new script in process
+     */
+    public function testLogSecurityEvents()
+    {
+        // Build a user with a specified password
+        $user = factory(User::class)->create([
+            'username' =>'newuser',
+            'password' => Hash::make('password')
+        ]);
+        $this->assertDatabaseMissing('security_logs', ['user_id' => $user->id]);
+        
+        // Attempt to login with incorrect credentials
+        $this->assertFalse(Auth::attempt([
+            'username' => $user->username,
+            'password' => 'invalidpassword'
+        ]));
+        $this->assertDatabaseHas('security_logs', ['event' => 'attempt', 'user_id' => $user->id]);
+        
+        // Attempt to login with correct password
+        $this->assertTrue(Auth::attempt([
+            'username' => $user->username,
+            'password' => 'password'
+        ]));
+        $this->assertDatabaseHas('security_logs', ['event' => 'login', 'user_id' => $user->id]);
+        
+        // Attempt to logout
+        Auth::logout();
+        $this->assertDatabaseHas('security_logs', ['event' => 'logout', 'user_id' => $user->id]);
+    }
+    
+    /**
+     * Do not use transactions for this test
+     */
+    protected function connectionsToTransact()
+    {
+        return [];
+    }
+}
