39336e4b630f59d4b6e0848ce4e38406f63bfc98
danloa <danloa@hotmail.com>
2020-12-11 13:56:40 -0400

    Wait for translations to render modeler

--- resources/js/processes/modeler/index.js
+++ resources/js/processes/modeler/index.js
@@ -1,16 +1,31 @@
 import Vue from "vue";
 import ModelerApp from "./components/ModelerApp";
 
-new Vue({
-    render: h => {
-        let renderWhenTranslationAvailable = (h) => {
-            if(ProcessMaker.i18n.exists('Save') === false) {
-                window.setTimeout(() => renderWhenTranslationAvailable(h), 100);
-            }
-            else {
-                return h(ModelerApp);
-            }
-        };
 
-        return renderWhenTranslationAvailable(h)
-}}).$mount("#modeler-app");
+let renderWhenTranslationAvailable = () => {
+    if(ProcessMaker.i18n.exists('Save') === false) {
+        window.setTimeout(() => renderWhenTranslationAvailable(), 100);
+    }
+    else {
+        new Vue({
+            render: h => h(ModelerApp)
+            }).$mount("#modeler-app");
+    }
+};
+
+
+renderWhenTranslationAvailable();
+
+// new Vue({
+//     render: h => {
+//         let renderWhenTranslationAvailable = (h) => {
+//             if(ProcessMaker.i18n.exists('Save') === false) {
+//                 window.setTimeout(() => renderWhenTranslationAvailable(h), 100);
+//             }
+//             else {
+//                 return h(ModelerApp);
+//             }
+//         };
+//
+//         return renderWhenTranslationAvailable(h)
+// }}).$mount("#modeler-app");
