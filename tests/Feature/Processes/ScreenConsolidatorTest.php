92c951a932392c55d46fc50615ea7bcc8736a58c
David Callizaya <davidcallizaya@gmail.com>
2020-12-16 17:10:16 -0400

    Enable NavButtons from Nested Screens

--- tests/Feature/Processes/ScreenConsolidatorTest.php
+++ tests/Feature/Processes/ScreenConsolidatorTest.php
@@ -255,4 +255,107 @@ class ScreenConsolidatorTest extends TestCase
 
         $this->assertEquals($expectedResponse, $consolidatedScreen);
     }
+
+    /**
+     * Test nav buttons in nested screen
+     *
+     * @return void
+     */
+    public function testNestedNavButtons()
+    {
+        // Create an admin user
+        $adminUser = factory(User::class)->create([
+            'username' => 'admin',
+            'is_administrator' => true,
+        ]);
+
+        // Save the file contents and convert them to an UploadedFile
+        $fileName = realpath(__DIR__ . '/../../Fixtures/nested_with_navbar.json');
+        $file = new UploadedFile($fileName, 'nested_with_navbar.json', null, null, null, true);
+
+        // Test to ensure our standard user cannot import a screen
+        $this->user = $adminUser;
+        $this->apiCall('POST', '/screens/import', [
+            'file' => $file,
+        ]);
+
+        $consolidator = new ScreenConsolidator(Screen::find(2));
+
+        $consolidatedScreen = $consolidator->call();
+
+        $pages = $consolidatedScreen['config'];
+        // Page 0 = Page 0 from Root + Page0 from Nested1
+        // Page 1 = Page 1 from Root + Page0 from Nested2
+        // Page 2 = Page 1 from Nested 1
+        // Page 3 = Page 2 from Nested 1
+        // Page 4 = Page 1 from Nested 2
+        // Page 5 = Page 2 from Nested 2
+
+        // Page 0 has a NavButton to Page 2
+        $this->assertPageHas($pages[0]['items'], [
+            ['editor-control' => 'PageNavigation', 'config' => ['eventData' => 2]],
+        ], 'Page 0 does not have a NavButton to Page 2');
+
+        // Page 0 has a RecordList to Page 1
+        $this->assertPageHas($pages[0]['items'], [
+            ['editor-control' => 'FormRecordList', 'config' => ['form' => 2]],
+        ], 'Page 0 does not have a RecordList to Page 1');
+
+        // Page 1 has a NavButton to Page 4
+        $this->assertPageHas($pages[1]['items'], [
+            ['editor-control' => 'PageNavigation', 'config' => ['eventData' => 4]],
+        ], 'Page 1 does not have a NavButton to Page 4');
+
+        // Page 2 has a NavButton to Page 3
+        $this->assertPageHas($pages[2]['items'], [
+            ['editor-control' => 'PageNavigation', 'config' => ['eventData' => 3]],
+        ], 'Page 2 does not have a NavButton to Page 3');
+
+        // Page 3 has a NavButton to Page 1
+        $this->assertPageHas($pages[3]['items'], [
+            ['editor-control' => 'PageNavigation', 'config' => ['eventData' => 1]],
+        ], 'Page 3 does not have a NavButton to Page 1');
+
+        // Page 4 is the Page 1 from Nested 2
+        $this->assertEquals('Page 1 from Nested', $pages[4]['name'], 'Page 4 must be Page 1 from Nested 2');
+
+        // Page 5 is the Page 2 from Nested 2
+        $this->assertEquals('Page 2 from Nested', $pages[5]['name'], 'Page 5 must be Page 2 from Nested 2');
+    }
+
+    private function assertPageHas($page, $content, $message)
+    {
+        $this->assertTrue($this->doesPageContains($page, $content), $message);
+    }
+
+    private function doesPageContains($page, $content)
+    {
+        foreach ($content as $key => $value) {
+            if (is_numeric($key)) {
+                $found = false;
+                foreach ($page as $item) {
+                    if ($this->doesPageContains($item, $value)) {
+                        $found = true;
+                        break;
+                    }
+                }
+                if (!$found) {
+                    return false;
+                }
+            } else {
+                if (!isset($page[$key])) {
+                    return false;
+                }
+                $item = $page[$key];
+            }
+            if (is_array($value)) {
+                $this->doesPageContains($item, $value);
+            } else {
+                if ($value != $item) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
