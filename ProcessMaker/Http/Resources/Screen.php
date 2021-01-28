fb928549e50f0ceb46d2965861e9c1e7c07d6148
David Callizaya <davidcallizaya@gmail.com>
2021-01-28 16:03:08 -0400

    Add nested screens to screen endpoint

--- ProcessMaker/Http/Resources/Screen.php
+++ ProcessMaker/Http/Resources/Screen.php
@@ -2,8 +2,25 @@
 
 namespace ProcessMaker\Http\Resources;
 
-use ProcessMaker\ScreenConsolidator;
-
 class Screen extends ApiResource
 {
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $screen = parent::toArray($request);
+        $include = explode(',', $request->input('include', ''));
+        if (in_array('nested', $include)) {
+            $nested = [];
+            foreach ($this->nestedScreenIds() as $id) {
+                $nested[] = self::findOrFail($id)->toArray();
+            }
+            $screen['nested'] = $nested;
+        }
+        return $screen;
+    }
 }
