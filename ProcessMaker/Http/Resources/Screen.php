82eefd1fa1e23526516b57d2ebc9731a913c1677
David Callizaya <davidcallizaya@gmail.com>
2021-01-28 13:27:32 -0400

    Return screen without consolidation

--- ProcessMaker/Http/Resources/Screen.php
+++ ProcessMaker/Http/Resources/Screen.php
@@ -6,16 +6,4 @@ use ProcessMaker\ScreenConsolidator;
 
 class Screen extends ApiResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        $screen = parent::toArray($request);
-        $consolidator = new ScreenConsolidator($this);
-        return array_merge($screen, $consolidator->call());
-    }
 }
