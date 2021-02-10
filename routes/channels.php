b21b3a6849fcf85f577104df91e64d00debe34ad
Nolan Ehrstrom <github@nolanpro.com>
2021-02-10 11:24:51 -0800

    Allow admin users to subscribe to all process event channels

--- routes/channels.php
+++ routes/channels.php
@@ -21,11 +21,20 @@ Broadcast::channel('ProcessMaker.Models.ProcessRequest.{id}', function ($user, $
     if ($id === 'undefined' || $user === 'undefined') {
         return;
     }
+
+    if ($user->is_administrator) {
+        return true;
+    }
+
     $request = ProcessRequest::find($id);
     return !empty($request->participants()->where('users.id', $user->getKey())->first());
 });
 
 Broadcast::channel('ProcessMaker.Models.ProcessRequestToken.{id}', function ($user, $id) {
+    if ($user->is_administrator) {
+        return true;
+    }
+
     $token = ProcessRequestToken::find($id);
     return $user->getKey() === $token->user_id;
 });
