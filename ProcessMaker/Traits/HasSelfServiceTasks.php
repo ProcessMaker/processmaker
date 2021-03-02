1d640ba0079251ab5a2dea5832d183941915d437
sanjacornelius <sanja.cornelius@processmaker.com>
2021-03-02 12:43:06 -0800

    Add self service support for Manual Tasks

--- ProcessMaker/Traits/HasSelfServiceTasks.php
+++ ProcessMaker/Traits/HasSelfServiceTasks.php
@@ -20,13 +20,26 @@ trait HasSelfServiceTasks
         }
         $definitions = new BpmnDocument();
         $definitions->loadXML($this->bpmn);
-        $tasks = $definitions->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'task');
-        foreach ($tasks as $task) {
-            $id = $task->getAttribute('id');
-            $assignment = $task->getAttributeNS(WorkflowServiceProvider::PROCESS_MAKER_NS, 'assignment');
-            if ($assignment === 'self_service') {
-                $response[$id] = explode(',', $task->getAttributeNS(WorkflowServiceProvider::PROCESS_MAKER_NS, 'assignedGroups'));
-            }
+        foreach ($definitions->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'task') as $task) {
+            $response = $this->assignSelfService($task);
+        }
+
+        foreach ($definitions->getElementsByTagNameNS(BpmnDocument::BPMN_MODEL, 'manualTask') as $task) {
+            $response = $this->assignSelfService($task);
+        }
+        
+        return $response;
+    }
+
+    /**
+    * Assign self service
+    */
+    private function assignSelfService($task) {
+        $response = [];
+        $id = $task->getAttribute('id');
+        $assignment = $task->getAttributeNS(WorkflowServiceProvider::PROCESS_MAKER_NS, 'assignment');
+        if ($assignment === 'self_service') {
+            $response[$id] = explode(',', $task->getAttributeNS(WorkflowServiceProvider::PROCESS_MAKER_NS, 'assignedGroups'));
         }
         return $response;
     }
