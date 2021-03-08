a8496a5b2cce0c8fe1b24ba87ddd4542f8b034ce
Ryan Cooley <ryan.cooley@processmaker.com>
2021-03-08 13:54:59 -0800

    Refactor text & textarea components to use modals

--- resources/js/admin/settings/components/SettingText.vue
+++ resources/js/admin/settings/components/SettingText.vue
@@ -1,28 +1,44 @@
 <template>
-  <span class="setting-text">
-    <b-input-group>
-      <b-form-input ref="input" v-model="input" :readonly="readonly" @keyup.enter="onSave" class="border-right-0" spellcheck="false" autocomplete="off"></b-form-input>
-      <b-input-group-append>
-        <b-button v-if="!readonly" variant="primary" :disabled="disabled" @click="onSave">
-          <i class="fa fa-save"></i>
-        </b-button>
-        <b-button v-else variant="secondary" @click="onCopy">
-          <i class="fas fa-copy"></i>
-        </b-button>
-      </b-input-group-append>
-    </b-input-group>
-  </span>
+  <div class="setting-text">
+    <div v-if="input === null || !input.length" class="font-italic text-black-50">
+      Empty
+    </div>
+    <div v-else>
+      {{ input }}
+    </div>
+    <b-modal class="setting-object-modal" v-model="showModal" size="lg" @hidden="onModalHidden" @shown="onModalShown">
+      <template v-slot:modal-header class="d-block">
+        <div>
+          <h5 class="mb-0" v-if="setting.name">{{ $t(setting.name) }}</h5>
+          <h5 class="mb-0" v-else>{{ setting.key }}</h5>
+          <small class="form-text text-muted" v-if="setting.helper">{{ $t(setting.helper) }}</small>
+        </div>
+        <button type="button" aria-label="Close" class="close" @click="onCancel">×</button>
+      </template>
+      <b-form-input ref="input" v-model="transformed" @keyup.enter="onSave" spellcheck="false" autocomplete="off"></b-form-input>
+      <div slot="modal-footer" class="w-100 m-0 d-flex">
+        <button type="button" class="btn btn-outline-secondary ml-auto" @click="onCancel">
+            {{ $t('Cancel') }}
+        </button>
+        <button type="button" class="btn btn-secondary ml-3" @click="onSave" :disabled="! changed">
+            {{ $t('Save')}}
+        </button>
+      </div>
+    </b-modal>
+  </div>
 </template>
 
 <script>
+import settingMixin from "../mixins/setting";
+
 export default {
-  props: ['value', 'readonly'],
+  mixins: [settingMixin],
+  props: ['value', 'setting'],
   data() {
     return {
-      disabled: true,
-      firstRun: true,
       input: null,
-      original: null,
+      showModal: false,
+      transformed: null,
     };
   },
   computed: {
@@ -33,46 +49,44 @@ export default {
         return 'success';
       }
     },
+    changed() {
+      return JSON.stringify(this.input) !== JSON.stringify(this.transformed);
+    }
   },
   watch: {
     value: {
       handler: function(value) {
         this.input = value;
       },
-    },
-    input: {
-      handler: function(value) {
-        if (!this.firstRun) {
-          if (this.original !== value) {
-            this.disabled = false;
-          } else {
-            this.disabled = true;
-          }
-        }
-        this.firstRun = false;
-      }
     }
   },
   methods: {
-    onCopy() {
-      this.$refs.input.select();
-      document.execCommand('copy');
+    onCancel() {
+      this.showModal = false;
+    },
+    onEdit() {
+      this.showModal = true;
+    },
+    onModalHidden() {
+      this.transformed = this.copy(this.input);
+    },
+    onModalShown() {
+      this.$refs.input.focus();
     },
     onSave() {
+      this.input = this.copy(this.transformed);
+      this.showModal = false;
       this.$emit('change', this.input);
       this.$emit('input', this.input);
-      this.original = this.input;
-      this.disabled = true;
     },
   },
   mounted() {
     if (this.value === null) {
       this.input = '';
-      this.original = '';
     } else {
       this.input = this.value;
-      this.original = this.value;
     }
+    this.transformed = this.copy(this.input);
   }
 };
 </script>
