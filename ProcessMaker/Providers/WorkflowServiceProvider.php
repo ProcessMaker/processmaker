43d858d170834b7fb854d39ece674e95e8cf876c
David Callizaya <davidcallizaya@gmail.com>
2021-03-15 12:09:58 -0400

    Declare ManualTask

--- ProcessMaker/Providers/WorkflowServiceProvider.php
+++ ProcessMaker/Providers/WorkflowServiceProvider.php
@@ -109,6 +109,8 @@ class WorkflowServiceProvider extends ServiceProvider
             $mapping = $bpmnRepository->getBpmnElementsMapping();
 
             //Initialize custom properties for ProcessMaker
+            $bpmnRepository->setBpmnElementMapping(self::PROCESS_MAKER_NS, '', []);
+            $bpmnRepository->setBpmnElementMapping(BpmnDocument::BPMN_MODEL, 'manualTask', $mapping[BpmnDocument::BPMN_MODEL]['task']);
             $bpmnRepository->setBpmnElementMapping(BpmnDocument::BPMN_MODEL, 'association', BpmnDocument::SKIP_ELEMENT);
             $bpmnRepository->setBpmnElementMapping(BpmnDocument::BPMN_MODEL, 'textAnnotation', BpmnDocument::SKIP_ELEMENT);
 
