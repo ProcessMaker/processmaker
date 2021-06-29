59ad807da2f4856466cfeace1107abf9effec1d6
Marco A. Nina Mena <marcoantonina@gmail.com>
2021-06-29 15:24:43 -0400

    Add aria-label to menu sidebar

--- resources/js/components/Sidebaricon.vue
+++ resources/js/components/Sidebaricon.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="filter-bar justify-content-between" id="Sidebaricon" v-b-tooltip.hover.right="{ animation: false, disabled: expanded(), boundary: 'viewport', delay: { show: 0, hide: 0 } }" :title="item.title">
-      <li class="nav-item">
+      <li class="nav-item" :aria-label="item.title">
         <a :href="item.url" class="nav-link"  @click="toggle" :target="item.attributes.target">
           <i v-if="item.attributes.icon" class="fas nav-icon" :class="item.attributes.icon" ></i>
           <i v-if="item.attributes.customicon" :class="item.attributes.customicon" ></i>
@@ -8,16 +8,22 @@
           <span class="nav-text" v-if="expanded()" v-cloak >
             {{item.title}}
             <i v-if="item.children && item.children.length" class="float-right fas" :class="{'fa-caret-right': !isOpen, 'fa-caret-down': isOpen}"></i>
-            <span v-if="count !== null" class="nav-badge float-right">{{ count }}</span>
+            <span v-if="count !== null" class="nav-badge float-right" :aria-label="item.title +' '+ $t('Count')">{{ count }}</span>
           </span>
         </a>
         <ul v-if="item.children && item.children.length" class="nav nav-list flex-column" v-show="isOpen">
-            <li v-for="item in item.children" :key="item.id" class="nav-item nav-pl">
+            <li v-for="item in item.children" :key="item.id" class="nav-item nav-pl" :aria-label="item.title">
               <a :href="item.url" class="nav-link" v-show="item.attributes.icon">
-                <i class="fas nav-icon" :class="item.attributes.icon" ></i> <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}<span v-if="count !== null" class="nav-badge float-right">{{ count }}</span></span>
+                <i class="fas nav-icon" :class="item.attributes.icon" ></i>
+                <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}
+                  <span v-if="count !== null" class="nav-badge float-right" :aria-label="item.title +' '+ $t('Count')">{{ count }}</span>
+                </span>
               </a>
               <a :href="item.url" class="nav-link" v-show="item.attributes.file">
-                <img :src="item.attributes.file" class="nav-icon" id="custom_icon"><span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}<span v-if="count !== null" class="nav-badge float-right">{{ count }}</span></span>
+                <img :src="item.attributes.file" class="nav-icon" id="custom_icon">
+                <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}
+                  <span v-if="count !== null" class="nav-badge float-right" :aria-label="item.title +' '+ $t('Count')">{{ count }}</span>
+                </span>
               </a>
             </li>
         </ul>
