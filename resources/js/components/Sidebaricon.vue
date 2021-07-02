19f3967ce45e26946e5a4b1e43ce9039dff1b28f
Ryan Cooley <ryan.cooley@processmaker.com>
2021-07-02 16:44:35 -0700

    Refactor sidebar

--- resources/js/components/Sidebaricon.vue
+++ resources/js/components/Sidebaricon.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="filter-bar justify-content-between" id="Sidebaricon" v-b-tooltip.hover.right="{ animation: false, disabled: expanded(), boundary: 'viewport', delay: { show: 0, hide: 0 } }" :title="item.title">
-      <li class="nav-item" :aria-label="item.title">
-        <a :href="item.url" class="nav-link"  @click="toggle" :target="item.attributes.target">
-          <i v-if="item.attributes.icon" class="fas nav-icon" :class="item.attributes.icon" ></i>
-          <i v-if="item.attributes.customicon" :class="item.attributes.customicon" ></i>
-          <img v-if="item.attributes.file" :src="item.attributes.file" class="nav-icon" id="custom_icon">
-          <span class="nav-text" v-if="expanded()" v-cloak >
-            {{item.title}}
-            <i v-if="item.children && item.children.length" class="float-right fas" :class="{'fa-caret-right': !isOpen, 'fa-caret-down': isOpen}"></i>
-            <span v-if="count !== null" class="nav-badge float-right" :aria-label="item.title +' '+ $t('Count')">{{ count }}</span>
-          </span>
-        </a>
-        <ul v-if="item.children && item.children.length" class="nav nav-list flex-column" v-show="isOpen">
-            <li v-for="item in item.children" :key="item.id" class="nav-item nav-pl" :aria-label="item.title">
-              <a :href="item.url" class="nav-link" v-show="item.attributes.icon">
+    <li class="nav-item filter-bar justify-content-between" id="Sidebaricon" v-b-tooltip.hover.right="{ animation: false, disabled: expanded(), boundary: 'viewport', delay: { show: 0, hide: 0 }, title: item.title }">
+      <a :href="item.url" class="nav-link"  @click="toggle" :target="item.attributes.target" :aria-label="ariaLabel">
+        <i v-if="item.attributes.icon" class="fas nav-icon" :class="item.attributes.icon" ></i>
+        <i v-if="item.attributes.customicon" :class="item.attributes.customicon" ></i>
+        <img v-if="item.attributes.file" :src="item.attributes.file" class="nav-icon" id="custom_icon">
+        <span class="nav-text" v-show="expanded()" v-cloak >
+          {{item.title}}
+          <i v-if="item.children && item.children.length" class="float-right fas" :class="{'fa-caret-right': !isOpen, 'fa-caret-down': isOpen}"></i>
+          <span v-if="count !== null" class="nav-badge float-right">{{ count }}</span>
+        </span>
+      </a>
+      <ul v-if="item.children && item.children.length" class="nav nav-list flex-column" v-show="isOpen">
+          <li v-for="item in item.children" :key="item.id" class="nav-item nav-pl">
+            <a :href="item.url" class="nav-link" v-show="item.attributes.icon">
                 <i class="fas nav-icon" :class="item.attributes.icon" ></i>
                 <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}
-                  <span v-if="count !== null" class="nav-badge float-right" :aria-label="item.title +' '+ $t('Count')">{{ count }}</span>
+                  <span v-if="count !== null" class="nav-badge float-right" :aria-label="ariaLabel">{{ count }}</span>
                 </span>
-              </a>
-              <a :href="item.url" class="nav-link" v-show="item.attributes.file">
-                <img :src="item.attributes.file" class="nav-icon" id="custom_icon">
+            </a>
+            <a :href="item.url" class="nav-link" v-show="item.attributes.file">
+              <img :src="item.attributes.file" class="nav-icon" id="custom_icon"><span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}<span v-if="count !== null" class="nav-badge float-right">{{ count }}</span></span>
                 <span class="nav-text" v-if="expanded()" v-cloak>{{item.title}}
-                  <span v-if="count !== null" class="nav-badge float-right" :aria-label="item.title +' '+ $t('Count')">{{ count }}</span>
+                  <span v-if="count !== null" class="nav-badge float-right" :aria-label="ariaLabel">{{ count }}</span>
                 </span>
-              </a>
-            </li>
-        </ul>
-      </li>
-    </div>
+            </a>
+          </li>
+      </ul>
+    </li>
 </template>
 
 <script>
@@ -53,7 +51,23 @@
         isOpen: false,
       }
     },
+    computed: {
+      ariaLabel() {
+        if (this.item.attributes.count !== undefined) {
+          return `${this.item.title}, ${this.count} ${this.plural(this.$t('Item'), this.count)}`;
+        } else {
+          return this.item.title;
+        }
+      },
+    },
     methods: {
+      plural(noun, count) {
+        if (count == 1) {
+          return noun;
+        } else {
+          return noun + 's';
+        }
+      },
       toggle() {
         this.isOpen = !this.isOpen;
       },
