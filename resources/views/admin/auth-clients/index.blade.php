994aa1bde3c36291384e7c303d37f02295b5a10a
Ryan Cooley <ryan.cooley@processmaker.com>
2021-07-06 15:48:16 -0700

    Update Auth Client page to use new modal

--- resources/views/admin/auth-clients/index.blade.php
+++ resources/views/admin/auth-clients/index.blade.php
@@ -16,56 +16,34 @@
 @endsection
 @section('content')
     <div id="authClients">
-        <div class="modal" role="dialog" ref="createEditAuthClient" id="createEditAuthClient">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">@{{title}}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="{{__('Close')}}">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            {!!Form::label('name', __('Name'))!!}<small class="ml-1">*</small>
-                            {!!Form::text('name', null, ['class'=> 'form-control', 'v-model'=> 'authClient.name',
-                            'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.name}'])!!}
-                            <small class="form-text text-muted">{{ __('Name must be distinct') }}</small>
-                            <div class="invalid-feedback" v-if="errors.name">@{{ errors.name[0] }}</div>
-                        </div>
-
-
-                        <b-form-checkbox-group v-model="authClient.types">
-                          <div class="form-group">
-                            <div class="invalid-feedback d-block" v-if="errors.types">@{{ errors.types[0] }}</div>
-                            <b-form-checkbox value="authorization_code_grant">{{__('Enable Authorization Code Grant')}}</b-form-checkbox>
-                            <br />
-                            <template v-if="authClient['types'].includes('authorization_code_grant')">
-                              {!!Form::label('redirect', __('Redirect URL'))!!}
-                              {!!Form::text('redirect', null, ['class'=> 'form-control', 'v-model'=> 'authClient.redirect',
-                              'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.redirect}','rows'=>3])!!}
-                              <div class="invalid-feedback" v-if="errors.redirect">@{{ errors.redirect[0] }}</div>
-                            </template>
-                          </div>
-                          <div class="form-group">
-                            <b-form-checkbox value="password_client">{{__('Enable Password Grant')}}</b-form-checkbox>
-                          </div>
-                          <div class="form-group">
-                            <b-form-checkbox value="personal_access_client">{{__('Enable Personal Access Tokens')}}</b-form-checkbox>
-                          </div>
-                        </b-form-checkbox-group>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">
-                            {{__('Cancel')}}
-                        </button>
-                        <button type="button" class="btn btn-secondary" @click.prevent="save" :disabled="disabled">
-                            {{__('Save')}}
-                        </button>
-                    </div>
-                </div>
+        <pm-modal ref="createEditAuthClient" id="createEditAuthClient" :title="title" @hidden="onClose" @ok.prevent="onSave" style="display: none;">
+            <div class="form-group" required>
+                {!!Form::label('name', __('Name'))!!}
+                {!!Form::text('name', null, ['class'=> 'form-control', 'v-model'=> 'authClient.name',
+                'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.name}'])!!}
+                <small class="form-text text-muted">{{ __('Name must be distinct') }}</small>
+                <div class="invalid-feedback" v-if="errors.name">@{{ errors.name[0] }}</div>
             </div>
-        </div>
+            <b-form-checkbox-group v-model="authClient.types">
+              <div class="form-group">
+                <div class="invalid-feedback d-block" v-if="errors.types">@{{ errors.types[0] }}</div>
+                <b-form-checkbox value="authorization_code_grant">{{__('Enable Authorization Code Grant')}}</b-form-checkbox>
+                <br />
+                <template v-if="authClient['types'].includes('authorization_code_grant')">
+                  {!!Form::label('redirect', __('Redirect URL'))!!}
+                  {!!Form::text('redirect', null, ['class'=> 'form-control', 'v-model'=> 'authClient.redirect',
+                  'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.redirect}','rows'=>3])!!}
+                  <div class="invalid-feedback" v-if="errors.redirect">@{{ errors.redirect[0] }}</div>
+                </template>
+              </div>
+              <div class="form-group">
+                <b-form-checkbox value="password_client">{{__('Enable Password Grant')}}</b-form-checkbox>
+              </div>
+              <div class="form-group">
+                <b-form-checkbox value="personal_access_client">{{__('Enable Personal Access Tokens')}}</b-form-checkbox>
+              </div>
+            </b-form-checkbox-group>
+        </pm-modal>
 
         <div class="px-3 page-content">
             <div id="search-bar" class="search mb-3" vcloak>
@@ -81,10 +59,10 @@
                         </div>
                     </div>
                     <div class="d-flex ml-md-2 flex-column flex-md-row">
-                        <a href="#" id="create_authclients" class="btn btn-secondary" data-toggle="modal" data-target="#createEditAuthClient">
+                        <button class="btn btn-secondary" @click="$refs.createEditAuthClient.show()" aria-label="{{__('Create Auth Client')}}">
                             <i class="fas fa-plus"></i>
                             {{__('Auth Client')}}
-                        </a>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -118,13 +96,11 @@
         beforeMount() {
           this.resetValues();
         },
-        mounted() {
-          $('#createEditAuthClient').on('hidden.bs.modal', () => {
-            this.resetValues();
-          });
-        },
         methods: {
-          save() {
+          onClose() {
+            this.resetValues();
+          },
+          onSave() {
             //single click
             if (this.disabled) {
               return
@@ -147,18 +123,17 @@
               baseURL: '/',
               data: this.authClient,
             }).then(response => {
-              $('#createEditAuthClient').modal('hide');
+              this.$refs.createEditAuthClient.hide();
               this.$refs.authClientList.fetch();
               this.loading = false;
               ProcessMaker.alert(this.$t("The auth client was ") + verb + ".", this.$t("success"))
             }).catch(error => {
               this.disabled = false;
               this.errors = error.response.data.errors;
-              console.log("ERRORS set to ", this.errors)
             });
           },
           resetValues() {
-            this.title = this.$t('Create An Auth-Client')
+            this.title = this.$t('Create Auth-Client')
             this.authClient = {
               id: null,
               name: "",
@@ -176,7 +151,7 @@
           edit(item) {
             this.title = this.$t('Edit Auth Client');
             this.authClient = item;
-            $('#createEditAuthClient').modal('show');
+            this.$refs.createEditAuthClient.show();
           }
         },
       })
