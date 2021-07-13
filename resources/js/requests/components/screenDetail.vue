c4c57476bc7bf5e5600e3a37572f5a0bca272f8e
Marco A. Nina Mena <marcoantonina@gmail.com>
2021-07-13 17:34:11 -0400

    Change spinner to overlay bootstrap

--- resources/js/requests/components/screenDetail.vue
+++ resources/js/requests/components/screenDetail.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="card h-100">
-    <div v-show="disabled" id="cover-spin" class="card-body h-100"></div>
-    <div slot="modal-header" class="w-100 d-print-none" align="right">
+    <b-overlay
+          id="overlay-background"
+          :show="disabled"
+          :variant="variant"
+          :opacity="opacity"
+          :blur="blur"
+          rounded="sm"
+        >
+    <div class="w-100 d-print-none" align="right">
       <button
         type="button"
         @click="print"
@@ -27,7 +34,7 @@
         />
       </div>
     </div>
-    <div slot="modal-header" class="w-100 d-print-none" align="right">
+    <div class="w-100 d-print-none" align="right">
       <button
         type="button"
         @click="print"
@@ -38,6 +45,7 @@
         <i class="fas fa-print"></i> {{ $t("Print") }}
       </button>
     </div>
+  </b-overlay>
   </div>
 </template>
 
@@ -68,6 +76,9 @@
       return {
         interval: null,
         disabled: true,
+        variant: 'transparent',
+        opacity: 0.85,
+        blur: '2px',
       }
     },
     computed: {
@@ -192,38 +203,3 @@
   }
 </script>
 
-<style scoped>
-#cover-spin {
-    position:fixed;
-    width:100%;
-    left:0;right:0;top:0;bottom:0;
-    background-color: rgba(255,255,255,0.7);
-    z-index:9999;
-    display:none;
-}
-
-@-webkit-keyframes spin {
-	from {-webkit-transform:rotate(0deg);}
-	to {-webkit-transform:rotate(360deg);}
-}
-
-@keyframes spin {
-	from {transform:rotate(0deg);}
-	to {transform:rotate(360deg);}
-}
-
-#cover-spin::after {
-    content:'';
-    display:block;
-    position:absolute;
-    left:48%;top:40%;
-    width:40px;height:40px;
-    border-style:solid;
-    border-color:black;
-    border-top-color:transparent;
-    border-width: 4px;
-    border-radius:50%;
-    -webkit-animation: spin .8s linear infinite;
-    animation: spin .8s linear infinite;
-}
-</style>
