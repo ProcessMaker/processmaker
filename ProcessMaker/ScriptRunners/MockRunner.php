6b0ae725b0eca57a3ad77d46298a13eb0e244113
Nolan Ehrstrom <github@nolanpro.com>
2021-07-30 12:00:27 -0700

    Move mock runner to correct location

--- ProcessMaker/ScriptRunners/MockRunner.php
+++ ProcessMaker/ScriptRunners/MockRunner.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace ProcessMaker\ScriptRunners;
 
 class MockRunner
 {
@@ -9,6 +9,9 @@ class MockRunner
     }
 
     public function run($code, $data, $config, $timeout, $user) {
+        if (app()->env !== 'testing') {
+            throw new \Exception("MockRunner is for tests only.");
+        }
         putenv('HOST_URL=' . config('app.docker_host_url'));
         $res = eval(str_replace('<?php', '', $code));
         return ['output' => $res];
