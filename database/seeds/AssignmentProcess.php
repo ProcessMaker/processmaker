125f5be2752e1ffb74623e84dc1fb1f1166458e0
danloa <danloa@gmail.com>
2021-09-01 09:19:54 -0400

    Adjust seeder to update the bpmn definition of the fixed process

--- database/seeds/AssignmentProcess.php
+++ database/seeds/AssignmentProcess.php
@@ -16,30 +16,38 @@ class AssignmentProcess extends Seeder
      */
     public function run()
     {
-        if ($this->processExists()) {
-            return;
-        }
+//        if ($this->processExists()) {
+//            return;
+//        }
 
         $admin = User::where('is_administrator', true)->firstOrFail();
         $processCategory = ProcessCategory::where('is_system', true)->firstOrFail();
         $bpmn = file_get_contents(__DIR__ . '/../processes/' . static::$TEMPLATE_PROCESS_FILE);
 
-        Process::unguard();
-        Process::updateOrCreate([
-            'name' => Process::ASSIGNMENT_PROCESS,
-            'process_category_id' => $processCategory->id,
-            'description' => 'Assignment Process',
-            'bpmn' => $bpmn,
-            'user_id' => $admin->id,
-        ]);
-        Process::reguard();
+        $process = $this->getAssignmentProcess();
+
+        // If the process does not exist
+        if (empty($process)) {
+            Process::unguard();
+            Process::updateOrCreate([
+                'name' => Process::ASSIGNMENT_PROCESS,
+                'process_category_id' => $processCategory->id,
+                'description' => 'Assignment Process',
+                'bpmn' => $bpmn,
+                'user_id' => $admin->id,
+            ]);
+            Process::reguard();
+        }
+
+        //if the process exists and the bpmn has changed, update it
+        if ($process && strcmp($process->bpmn , $bpmn) !== 0) {
+            $process->bpmn = $bpmn;
+            $process->saveOrFail();
+        }
     }
 
-    /**
-     * @return bool
-     */
-    private function processExists(): bool
+    private function getAssignmentProcess()
     {
-        return Process::where('name', Process::ASSIGNMENT_PROCESS)->get()->count() > 0;
+        return Process::where('name', Process::ASSIGNMENT_PROCESS)->first();
     }
 }
