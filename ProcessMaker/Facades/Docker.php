e9aea8c6109ac8fad06be7061e04826f6cc42f90
Enrique Ponce De Leon <enrique@processmaker.com>
2021-09-09 12:39:40 -0400

    Adding documentation to new classes

--- ProcessMaker/Facades/Docker.php
+++ ProcessMaker/Facades/Docker.php
@@ -1,10 +1,17 @@
 <?php
-
 namespace ProcessMaker\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use ProcessMaker\Managers\DockerManager;
 
+/**
+ * @see \ProcessMaker\Managers\DockerManager
+ * 
+ * @method bool hasRemoteDocker()
+ * @method string getDockerHost()
+ * @method string getDockerExecutable(int)
+ * @method mixed command(int)
+ */
 class Docker extends Facade
 {
     /**
