59ccfe787d9865a2f4c2da7c172e338beb4f00c7
AJ <velkymx@gmail.com>
2021-09-10 09:33:05 -0400

    Revert "Removed unique constraint on the db table on install"
    This reverts commit 0cba0350e1e285eca81142b692784596ed42c7a4.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->nullable();
+            $table->string('email')->unique();
             $table->string('password');
 
             $table->string('firstname', 50)->nullable();
