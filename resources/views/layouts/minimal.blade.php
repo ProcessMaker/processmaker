1efcf26644b002458fa0c8e16f86a0896980e833
Agustin Busso <90727999+agustinbusso@users.noreply.github.com>
2021-09-22 15:54:46 -0300

    Fixed issue not finding ProcessMaker i18n when using minimal template

--- resources/views/layouts/minimal.blade.php
+++ resources/views/layouts/minimal.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="i18n-mdate" content='{!! json_encode(ProcessMaker\i18nHelper::mdates()) !!}'>
     <title>@yield('title',__('Welcome')) - ProcessMaker</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
