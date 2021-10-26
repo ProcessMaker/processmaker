9b7b68b89aaddfac37e8d8d71182726d18379a64
David Callizaya <davidcallizaya@gmail.com>
2021-10-26 16:58:51 -0400

    Add test names

--- tests/Feature/Api/ProcessPatternsTest.php
+++ tests/Feature/Api/ProcessPatternsTest.php
@@ -74,18 +74,20 @@ class ProcessPatternsTest extends TestCase
     private function prepareTestCases($bpmnFile, array $tests)
     {
         $file = "{$this->basePath}{$bpmnFile}";
+        $name = basename($bpmnFile, '.bpmn');
         $jsonFile = substr($file, 0, -4) . 'json';
         if (file_exists($jsonFile)) {
             $contexts = json_decode(file_get_contents($jsonFile), true);
             foreach ($contexts as $context) {
-                $tests[] = [
+                $comment = $context['comment'] ?? '';
+                $tests["$name:$comment"] = [
                     'runProcessWithContext',
                     $bpmnFile,
                     $context,
                 ];
             }
         } else {
-            $tests[] = [
+            $tests[$name] = [
                 'runProcessWithoutContext',
                 $bpmnFile,
             ];
