eda57ce2efd3892055a354db285a46ffac3fbcb8
David Callizaya <davidcallizaya@gmail.com>
2021-10-27 09:31:06 -0400

    Standard Loop Task is not a MultiInstance Task

--- ProcessMaker/Models/ProcessRequestToken.php
+++ ProcessMaker/Models/ProcessRequestToken.php
@@ -17,6 +17,7 @@ use ProcessMaker\Models\User;
 use ProcessMaker\Nayra\Bpmn\TokenTrait;
 use ProcessMaker\Nayra\Contracts\Bpmn\ActivityInterface;
 use ProcessMaker\Nayra\Contracts\Bpmn\FlowElementInterface;
+use ProcessMaker\Nayra\Contracts\Bpmn\MultiInstanceLoopCharacteristicsInterface;
 use ProcessMaker\Nayra\Contracts\Bpmn\TokenInterface;
 use ProcessMaker\Traits\ExtendedPMQL;
 use ProcessMaker\Traits\SerializeToIso8601;
@@ -867,7 +868,7 @@ class ProcessRequestToken extends Model implements TokenInterface
         $definition = $this->getDefinition(true);
         if ($definition instanceof ActivityInterface) {
             $loop = $definition->getLoopCharacteristics();
-            return $loop && $loop->isExecutable();
+            return $loop && $loop->isExecutable() && $loop instanceof MultiInstanceLoopCharacteristicsInterface;
         }
         return false;
     }
