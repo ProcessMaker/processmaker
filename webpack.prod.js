4bcdb6fba4cc4d8e67577428f27b64bedc6efa7f
Jose Chirivella <jose.chirivella@processmaker.com>
2021-10-28 15:52:28 -0400

    Added dependencies for webpack

---
+++ webpack.prod.js
@@ -0,0 +1,71 @@
+const {VueLoaderPlugin} = require("vue-loader");
+const path = require("path");
+const MiniCssExtractPlugin = requir("mini-css-extract-plugin");
+
+const autoprefixer = require("autoprefixer");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./resources/js/app.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+        loader: "file-loader",
+        options: {
+          name: "[name][contenthash:8].[ext]",
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "assets",
+          esModule: false,
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin(),
+    // new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.runtime.esm.js",
+    },
+    extensions: ["*", ".js", ".vue", ".json"],
+  },
+};
