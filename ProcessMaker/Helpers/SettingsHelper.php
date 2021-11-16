092034792c28c828f1c05d347f3fee4f89e03469
Alex Runyan <alex@runyan.co>
2021-11-16 13:55:07 -0500

    Adjusted event/config/route re-caching

--- ProcessMaker/Helpers/SettingsHelper.php
+++ ProcessMaker/Helpers/SettingsHelper.php
@@ -56,21 +56,15 @@ if (!function_exists('flush_settings')) {
     {
         cache()->tags('setting')->flush();
 
-        $configuration_is_cached = app()->configurationIsCached();
-        $routes_are_cached = app()->routesAreCached();
-        $events_are_cached = app()->eventsAreCached();
-
-        Artisan::call('optimize:clear');
-
-        if ($configuration_is_cached) {
+        if (app()->configurationIsCached()) {
             Artisan::call('config:cache');
         }
 
-        if ($routes_are_cached) {
+        if (app()->routesAreCached()) {
             Artisan::call('route:cache');
         }
 
-        if ($events_are_cached) {
+        if (app()->eventsAreCached()) {
             Artisan::call('event:cache');
         }
     }
