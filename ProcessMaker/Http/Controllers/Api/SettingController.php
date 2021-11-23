7908e65d9cf106e3b9f56971482863bc3ead5a0e
Agustin Busso <90727999+agustinbusso@users.noreply.github.com>
2021-11-23 12:22:52 -0300

    Added backend validation for variable names

--- ProcessMaker/Http/Controllers/Api/SettingController.php
+++ ProcessMaker/Http/Controllers/Api/SettingController.php
@@ -204,6 +204,7 @@ class SettingController extends Controller
      */
     public function update(Setting $setting, Request $request)
     {
+        $request->validate(Setting::rules($setting), Setting::messages());
         $setting->config = $request->input('config');
         $setting->save();
 
