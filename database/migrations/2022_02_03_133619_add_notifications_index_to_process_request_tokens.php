c5af47c9dca3c89a42360cdd30c6e6b4a8ce2a98
David Callizaya <davidcallizaya@gmail.com>
2022-02-03 09:46:39 -0400

    Add notifications index to process_request_tokens

---
+++ database/migrations/2022_02_03_133619_add_notifications_index_to_process_request_tokens.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNotificationsIndexToProcessRequestTokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('process_request_tokens', function (Blueprint $table) {
+            // Add index by: user_id, status, due_at, due_notified
+            $table->index(['user_id', 'status', 'due_at', 'due_notified']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('process_request_tokens', function (Blueprint $table) {
+            // Remove index by: user_id, status, due_at, due_notified
+            $table->dropIndex(['user_id', 'status', 'due_at', 'due_notified']);
+        });
+    }
+}
