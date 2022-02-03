a5b0df0ec93a8eb4f81db05cea53c14a1826d64b
David Callizaya <davidcallizaya@gmail.com>
2022-02-03 11:30:05 -0400

    Check if Script/Service Job failed

--- ProcessMaker/Jobs/RunServiceTask.php
+++ ProcessMaker/Jobs/RunServiceTask.php
@@ -96,4 +96,26 @@ class RunServiceTask extends BpmnAction implements ShouldQueue
             Log::error($exception->getTraceAsString());
         }
     }
+
+    /**
+     * When Job fails
+     */
+    public function failed(Throwable $exception)
+    {
+        if (!$this->tokenId) {
+            Log::error('Script failed: ' . $exception->getMessage());
+            return;
+        }
+        Log::error('Script (#' . $this->tokenId . ') failed: ' . $exception->getMessage());
+        $token = ProcessRequestToken::find($this->tokenId);
+        if ($token) {
+            $element = $token->getBpmnDefinition();
+            $token->setStatus(ServiceTaskInterface::TOKEN_STATE_FAILING);
+            $error = $element->getRepository()->createError();
+            $error->setName($exception->getMessage());
+            $token->setProperty('error', $error);
+            Log::error($exception->getTraceAsString());
+            $token->save();
+        }
+    }
 }
