6856ccc158416cb0d15421722a074e10ba50f9dc
Agustin Busso <90727999+agustinbusso@users.noreply.github.com>
2022-02-16 09:25:24 -0300

    Added do not sanitize column

---
+++ database/migrations/2022_02_15_161839_add_do_not_sanitize_to_process_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDoNotSanitizeToProcessRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('process_requests', function (Blueprint $table) {
+            $table->json('do_not_sanitize')->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('process_requests', function (Blueprint $table) {
+            $table->dropColumn('do_not_sanitize');
+        });
+    }
+}
