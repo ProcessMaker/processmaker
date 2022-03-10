83a604a31e364b50b892d410836c6bc4d79513b4
Alex Runyan <alex@runyan.co>
2022-03-09 21:02:16 -0500

    Removed files not to be included in PR; restored files not needing changes

--- ProcessMaker/Console/Kernel.php
+++ ProcessMaker/Console/Kernel.php
@@ -12,7 +12,9 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-    protected $commands = [];
+    protected $commands = [
+        // @todo Add our ProcessMaker specific commands to this list
+    ];
 
     /**
      * Define the application's command schedule.
@@ -35,7 +37,6 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
-
         require base_path('routes/console.php');
     }
 }
