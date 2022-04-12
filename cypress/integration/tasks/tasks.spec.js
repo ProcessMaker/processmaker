e99e27d8e654982e5d7ad0f92945e646322029e7
Jose Chirivella <jose.chirivella@processmaker.com>
2022-04-12 12:06:51 -0400

    FOUR-5917 Indent

--- cypress/integration/tasks/tasks.spec.js
+++ cypress/integration/tasks/tasks.spec.js
@@ -1,28 +1,36 @@
-describe('Tasks page', () => {
+describe("Tasks page", () => {
   beforeEach(() => {
     cy.visit("/tasks");
   });
 
   describe("Task Table", () => {
     beforeEach(() => {
-      cy.title().should("include", "To Do Tasks");
+      cy.title()
+        .should("include", "To Do Tasks");
     });
     it("task table shouldn't be visible", function () {
-      cy.get('[data-cy="no-results-message]').should('be.visible').contains('Congratulations');
+      cy.get("[data-cy=\"no-results-message]")
+        .should("be.visible")
+        .contains("Congratulations");
       cy.get("[data-cy='tasks-table']")
         .should("not.be.visible");
-    })
+    });
     xit("task table should be visible", function () {
-      cy.get('[data-cy="no-results-message]').should('not.be.visible');
+      cy.get("[data-cy=\"no-results-message]")
+        .should("not.be.visible");
       cy.get("[data-cy='tasks-table']")
         .should("be.visible");
     });
   });
 
-  describe('Completed tab', () => {
-    it("should go to the completed tab", function () {
-      cy.get('[data-cy="Completed"]').click();
-      cy.title().should("include", "Completed Tasks");
+  describe("Completed tab", () => {
+    beforeEach(() => {
+      cy.title()
+        .should("include", "Completed");
     });
-  })
+    it("should go to the completed tab", function () {
+      cy.get("[data-cy=\"Completed\"]")
+        .click();
+    });
+  });
 });
