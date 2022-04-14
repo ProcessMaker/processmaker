c3178f9a6ae0570a21c5ed1ed69563054705ba56
Ryan Cooley <ryan.cooley@processmaker.com>
2022-04-14 01:38:56 -0700

    Use translation string in primary layouts

--- resources/views/layouts/minimal.blade.php
+++ resources/views/layouts/minimal.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="i18n-mdate" content='{!! json_encode(ProcessMaker\i18nHelper::mdates()) !!}'>
-    <title>@yield('title',__('Welcome')) - ProcessMaker</title>
+    <title>@yield('title',__('Welcome')) - {{ __('ProcessMaker') }}</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
 @yield('css')
