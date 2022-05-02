c48fe4365f2ae7241307366fb0320decdaa4ee5f
Jose Chirivella <jose.chirivella@processmaker.com>
2022-05-02 16:11:54 -0400

    Changed the rules for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,11 @@ module.exports = {
     BpmnModdle: true,
     Snap: true,
     Dispatcher: true,
+    ProcessMaker: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/base", "airbnb-base"],
+  extends: ["eslint:recommended", "plugin:vue/recommended", "airbnb-base"],
   parserOptions: {
     parser: "@babel/eslint-parser",
-    ecmaVersion: 2017,
     sourceType: "module",
     babelOptions: {
       configFile: "./babel.config.json",
@@ -24,81 +24,11 @@ module.exports = {
     "vue",
   ],
   rules: {
-    "comma-dangle": ["error", "always-multiline"],
-    indent: [
-      1,
-      2,
-      {
-        SwitchCase: 1,
-      },
-    ],
-    "linebreak-style": [
-      "error",
-      "unix",
-    ],
-    quotes: [
-      2,
-      "double",
-    ],
-    semi: [
-      1,
-      "always",
-    ],
-    "max-len": [
-      1,
-      {
-        code: 120,
-        comments: 80,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreTemplateLiterals: false,
-        ignoreRegExpLiterals: true,
-      },
-    ],
-    "prefer-const": [
-      0,
-    ],
-    "prefer-destructuring": 1,
-    "object-curly-spacing": [
-      1,
-      "never",
-    ],
-    "no-underscore-dangle": [
-      1,
-      {
-        allowAfterThis: true,
-      },
-    ],
-    "no-use-before-define": [
-      1,
-      {
-        functions: true,
-        classes: true,
-      },
-    ],
-    "no-param-reassign": 0,
-    "no-cond-assign": [
-      "error",
-      "always",
-    ],
-    "no-multi-assign": [
-      "error",
-    ],
-    "no-useless-return": 0,
-    "class-methods-use-this": 0,
-    "generator-star-spacing": [
-      "error",
-      {
-        before: false,
-        after: true,
-      },
-    ],
-    "array-callback-return": [
-      "error",
-      {
-        allowImplicit: true,
-      },
-    ],
+    quotes: ["error", "double"],
+    "max-len": ["error", {
+      code: 140,
+      ignoreComments: true,
+    }],
   },
   // overrides: [
   // 	{
