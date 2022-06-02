7a99dfc9b4ffb86cccd152728a02364fced28c48
Eleazar Resendez <eleazar.resendez@processmaker.com>
2022-06-02 09:57:14 -0500

    Return object instead of null

--- ProcessMaker/Models/Process.php
+++ ProcessMaker/Models/Process.php
@@ -235,7 +235,7 @@ class Process extends Model implements HasMedia, ProcessModelInterface
      */
     public function category()
     {
-        return $this->belongsTo(ProcessCategory::class, 'process_category_id');
+        return $this->belongsTo(ProcessCategory::class, 'process_category_id')->withDefault();
     }
 
     /**
