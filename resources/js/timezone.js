ebf1fa365214b64d065b70d93d342d9f7758d057
Jose Chirivella <jose.chirivella@processmaker.com>
2022-06-27 16:06:56 -0400

    FOUR-6062 Added Timezone service

---
+++ resources/js/timezone.js
@@ -0,0 +1,9 @@
+export function timezone () {
+  if (!window.ProcessMaker.user && !window.ProcessMaker.user.timezone) return throw new Error("User or Timezone hasn't been set");
+  return window.ProcessMaker.user.timezone;
+}
+
+export function timezone_format () {
+  if (!window.ProcessMaker.user && !window.ProcessMaker.user.datetime_format) return throw new Error("User or datetime_format hasn't been set");
+  return window.ProcessMaker.user.datetime_format;
+}
