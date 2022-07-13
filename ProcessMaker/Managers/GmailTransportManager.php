6f6fbde9f71248dff1efd129680320e211d138e7
sanjacornelius <sanja.cornelius@processmaker.com>
2022-07-13 11:07:30 -0700

    Set access token

--- ProcessMaker/Managers/GmailTransportManager.php
+++ ProcessMaker/Managers/GmailTransportManager.php
@@ -13,7 +13,14 @@ class GmailTransportManager extends Transport
 
     public function __construct($config)
     {
-        $this->token = $config['access_token'];
+        $this->token = [
+          "access_token" => $config['access_token'],
+          "expires_in" => $config['expires_in'],
+          "refresh_token" => $config['refresh_token'],
+          "scope" => $config['scope'],
+          "token_type" => $config['token_type'],
+          "created" => $config['created'],
+        ];
     }
     
     public function send(Swift_Mime_SimpleMessage $mime, &$failedRecipients = null)
@@ -21,6 +28,26 @@ class GmailTransportManager extends Transport
         $client = new \Google\Client();
         try {
             $client->setAccessToken($this->token);
+            
+            // TODO::TEST IF REFRESH TOKEN IS WORKING PROPERLY
+            // if ($client->isAccessTokenExpired()) {
+            //     // save refresh token to some variable
+            //     $refreshTokenSaved = $client->getRefreshToken();
+            //     dd($refreshTokenSaved);
+            //     // update access token
+            //     $client->fetchAccessTokenWithRefreshToken($refreshTokenSaved);
+            //     // pass access token to some variable
+            //     $accessTokenUpdated = $client->getAccessToken();
+
+            //     //Set the new acces token
+            //     $accessToken = $refreshTokenSaved;
+            //     $client->setAccessToken($accessToken);
+            //     // dd($accessTokenUpdated);
+            //     //UPDATE CONFIG WITH NEW ACCESS TOKEN
+            //     EmailController::setEnvVar("EMAIL_CONNECTOR_GMAIL_API_ACCESS_TOKEN_2", $accessToken['access_token']);
+            //     // dd($client->isAccessTokenExpired());
+            // }
+            
         
             $gmailService = new \Google\Service\Gmail($client);
             $message = new \Google_Service_Gmail_Message();
