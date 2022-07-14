6c83ff86e120a7d8bb4662243e41e94541669f56
sanjacornelius <sanja.cornelius@processmaker.com>
2022-07-14 10:36:24 -0700

    Set refresh token

--- ProcessMaker/Managers/GmailTransportManager.php
+++ ProcessMaker/Managers/GmailTransportManager.php
@@ -14,12 +14,10 @@ class GmailTransportManager extends Transport
     public function __construct($config)
     {
         $this->token = [
+          "client_id" => $config['key'],
+          "client_secret" => $config['secret'],
           "access_token" => $config['access_token'],
-          "expires_in" => $config['expires_in'],
           "refresh_token" => $config['refresh_token'],
-          "scope" => $config['scope'],
-          "token_type" => $config['token_type'],
-          "created" => $config['created'],
         ];
     }
     
@@ -27,26 +25,14 @@ class GmailTransportManager extends Transport
     {
         $client = new \Google\Client();
         try {
+            $authConfig = array(
+                "web" => array(
+                    'client_id' => $this->token['client_id'], 
+                    'client_secret' => $this->token['client_secret']
+                )
+            );
+            $client->setAuthConfig($authConfig);
             $client->setAccessToken($this->token);
-            
-            // TODO::TEST IF REFRESH TOKEN IS WORKING PROPERLY
-            // if ($client->isAccessTokenExpired()) {
-            //     // save refresh token to some variable
-            //     $refreshTokenSaved = $client->getRefreshToken();
-            //     dd($refreshTokenSaved);
-            //     // update access token
-            //     $client->fetchAccessTokenWithRefreshToken($refreshTokenSaved);
-            //     // pass access token to some variable
-            //     $accessTokenUpdated = $client->getAccessToken();
-
-            //     //Set the new acces token
-            //     $accessToken = $refreshTokenSaved;
-            //     $client->setAccessToken($accessToken);
-            //     // dd($accessTokenUpdated);
-            //     //UPDATE CONFIG WITH NEW ACCESS TOKEN
-            //     EmailController::setEnvVar("EMAIL_CONNECTOR_GMAIL_API_ACCESS_TOKEN_2", $accessToken['access_token']);
-            //     // dd($client->isAccessTokenExpired());
-            // }
             
         
             $gmailService = new \Google\Service\Gmail($client);
