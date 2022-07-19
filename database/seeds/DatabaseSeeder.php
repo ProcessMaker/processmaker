2a27d7d1f6a284f4f389a30a63293b64a97126b4
Nolan Ehrstrom <github@nolanpro.com>
2022-07-19 10:29:09 -0700

    Move create anonymous user to seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            AnonymousUserSeeder::class,
             PermissionSeeder::class,
             ProcessSystemCategorySeeder::class,
             GroupSeeder::class,
