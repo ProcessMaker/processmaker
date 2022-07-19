df0e05d61cb9d079d97fe1679f5cea44e689e16d
sanjacornelius <sanja.cornelius@processmaker.com>
2022-07-19 11:21:43 -0700

    Add Office 365 Option

---
+++ database/migrations/2022_06_22_160857_change_buttons_ui_in_settings_table.php
@@ -0,0 +1,124 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use ProcessMaker\Packages\Connectors\Email\EmailConfig;
+use ProcessMaker\Models\Setting;
+
+class ChangeButtonsUiInSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        $this->updateEmailServerUi();
+        $this->updateAbeUi();
+        $this->updateDocusignUi();
+        $this->updateLdapUi();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        $this->revertEmailServerUi();
+        $this->revertAbeUi();
+        $this->revertDocusignUi();
+        $this->revertLdapUi();
+    }
+
+    private function updateEmailServerUi() {
+        // Update Email Server buttons UI
+        Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_ADD_MAIL_SERVER%')->update([
+            'name' => 'Mail Server', 
+            'ui' => '{"props":{"variant":"primary", "position": "top", "order": "100", "icon": "fas fa-plus"},"handler":"addMailServer"}',
+        ]);
+
+        Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_REMOVE_MAIL_SERVER%')->update([
+            'name' => 'Remove Server', 
+            'ui' => '{"props":{"variant":"outline-danger", "position": "bottom", "order":"100", "icon":"fas fa-trash-alt"},"handler":"removeMailServer"}',
+        ]);
+
+        Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_SEND_TEST_EMAIL%')->update([
+            'ui' => '{"props":{"variant":"outline-secondary", "position": "top", "order":"300"},"handler":"mailTest"}',
+        ]);
+
+        // Add Mail Driver Options
+        Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_MAIL_DRIVER%')->update([
+            'ui' => '{"options": ["smtp", "sendmail", "mailgun", "postmark", "ses", "gmail", "office365"]}',
+        ]);
+    }
+
+    protected function revertEmailServerUi() {
+        // Revert buttons UI
+         Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_ADD_MAIL_SERVER%')->update([
+            'name' => '+ Mail Server',
+        ]);
+
+        Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_REMOVE_MAIL_SERVER%')->update([
+            'name' => '- Remove Server', 
+            'ui' => '{"props":{"variant":"outline-secondary"},"handler":"removeMailServer"}'
+        ]);
+
+        Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_SEND_TEST_EMAIL%')->update([
+            'ui' => '{"props":{"variant":"primary"},"handler":"mailTest"}'
+        ]);
+
+        // Revert Mail Driver Options
+        Setting::where('key', 'LIKE', '%EMAIL_CONNECTOR_MAIL_DRIVER%')->update([
+            'ui' => '{"options": ["smtp", "sendmail", "mailgun", "postmark", "ses"]}',
+        ]);
+
+    }
+
+    protected function updateAbeUi() {
+        // Update ABE Buttons
+        Setting::where('key', 'abe_imap_test_connection')->update([
+            'ui' => '{"props":{"variant":"primary", "position": "top", "order":"100"},"handler":"imapTest"}',
+        ]);
+    }
+
+    protected function revertAbeUi() {
+         // Revert ABE Buttons
+         Setting::where('key', 'abe_imap_test_connection')->update([
+            'ui' => '{"props":{"variant":"primary"},"handler":"imapTest"}'
+        ]);
+    }
+
+    protected function updateDocusignUi() {
+        // Update Docusign Buttons
+        Setting::where('key', 'docusign_grant_access')->update([
+            'ui' => '{"props":{"variant":"primary","href":"/docusign/grant", "position": "top", "order":"100"}}',
+        ]);
+    }
+
+    protected function revertDocusignUi() {
+        // Revert Docusign Buttons
+        Setting::where('key', 'docusign_grant_access')->update([
+            'ui' => '{"props":{"variant":"primary","href":"/docusign/grant"}}',
+        ]);
+    }
+
+    protected function updateLdapUi() {
+        // Update LDAP Buttons
+        Setting::where('key', 'services.ldap.log')->update([
+            'ui' => '{"props":{"variant":"primary","target":"_blank","href":"/admin/ldap-logs", "position": "top", "order":"100"}}',
+        ]);
+    }
+
+    protected function revertLdapUi() {
+        // Revert LDAP Buttons
+        Setting::where('key', 'services.ldap.log')->update([
+            'ui' => '{"props":{"variant":"primary","target":"_blank","href":"/admin/ldap-logs"}}',
+        ]);
+    }
+}
