aad5c6a048a8c5fd7e63653a41528519e99c161a
sanjacornelius <sanja.cornelius@processmaker.com>
2022-07-27 16:27:29 -0700

    Handle expired access tokens

--- ProcessMaker/Managers/OauthTransportManager.php
+++ ProcessMaker/Managers/OauthTransportManager.php
@@ -10,11 +10,21 @@ use ProcessMaker\Packages\Connectors\Email\EmailConfig;
 class OauthTransportManager extends TransportManager
 {
     protected $config = null;
+    
+    private $token = null;
 
 
     public function __construct($config)
     {   
         $this->config = (object) $config;
+        // TODO:: Can maybe pass in the server index here
+        $this->token = [
+            "client_id" => $config->get('services.gmail.key'),
+            "client_secret" => $config->get('services.gmail.secret'),
+            "access_token" => $config->get('services.gmail.access_token'),
+            "refresh_token" => $config->get('services.gmail.refresh_token'),
+            "expires_in" => $config->get('services.gmail.expires_in'),
+        ];
     }
     
     protected function createSmtpDriver()
@@ -27,31 +37,8 @@ class OauthTransportManager extends TransportManager
             switch ($authMethod) {
                 case 'google':
                     $serverIndex = $this->config->get('mail.server_index');
+                    $accessToken = $this->checkForExpiredAccessToken($serverIndex);
                     $fromAddress = $this->config->get('mail.from.address');
-                    
-                    $accessToken = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_ACCESS_TOKEN_' . $serverIndex)->firstOrFail()->value;
-                    $clientId = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_CLIENT_ID_' . $serverIndex)->firstOrFail()->value;
-                    $clientSecret = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_SECRET_' . $serverIndex)->firstOrFail()->value;
-                    $refreshToken = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_REFRESH_TOKEN_' . $serverIndex)->firstOrFail()->value;
-                    
-                    $token = [
-                        "client_id" => $clientId,
-                        "client_secret" => $clientSecret,
-                        "access_token" => $accessToken,
-                        "refresh_token" => $refreshToken
-                    ];
-
-                    // Check if access token has expired
-                    $client = new \Google\Client();
-                    $authConfig = array(
-                        "web" => array(
-                            'client_id' => $clientId, 
-                            'client_secret' => $clientSecret
-                        )
-                    );
-                    $client->setAuthConfig($authConfig);
-                    $client->setAccessToken($token);
-                    
                     // Update Authencation Mode
                     $transport->setAuthMode('XOAUTH2')
                     ->setUsername($fromAddress)
@@ -64,6 +51,47 @@ class OauthTransportManager extends TransportManager
             }
         }
         return $transport;
+    }
+
+    public function checkForExpiredAccessToken($index) 
+    {
+        $client = new \Google\Client();
+        $authConfig = array(
+            "web" => array(
+                'client_id' => $this->token['client_id'], 
+                'client_secret' => $this->token['client_secret']
+            )
+        );
+        $client->setAuthConfig($authConfig);
+        $client->setAccessToken($this->token);
+        $accessToken = $this->token['access_token'];
+
+        if ($client->isAccessTokenExpired()) {
+            $newToken = $client->fetchAccessTokenWithRefreshToken($this->token['refresh_token']);
+            $client->setAccessToken($newToken['access_token']);
+            $accessToken = $newToken['access_token'];
+            $refreshToken = $newToken['refresh_token'];
+            $expiresIn = $newToken['expires_in'];
+
+            $this->setEnvVar('EMAIL_CONNECTOR_GMAIL_API_ACCESS_TOKEN_' . $index, $accessToken);
+            $this->setEnvVar('EMAIL_CONNECTOR_GMAIL_API_REFRESH_TOKEN_' . $index, $refreshToken);
+            $this->setEnvVar('EMAIL_CONNECTOR_GMAIL_API_EXPIRES_IN_' . $index, $expiresIn);
+        }
+        
+        return $accessToken;
+    }
+
+    private function setEnvVar($name, $value)
+    {
+        $env = EnvironmentVariable::updateOrCreate(
+            [
+                'name' => $name,
+            ],
+            [
+                'description' => $name,
+                'value' => $value,
+            ]
+        );
     }
 }
 
