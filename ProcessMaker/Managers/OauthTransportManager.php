f29de23c2c25962d20b4174c8f153d0d1717bfc5
sanjacornelius <sanja.cornelius@processmaker.com>
2022-07-27 11:21:43 -0700

    Check if access token has expired and request new

--- ProcessMaker/Managers/OauthTransportManager.php
+++ ProcessMaker/Managers/OauthTransportManager.php
@@ -27,10 +27,32 @@ class OauthTransportManager extends TransportManager
             switch ($authMethod) {
                 case 'google':
                     $serverIndex = $this->config->get('mail.server_index');
-                    $accessToken = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_ACCESS_TOKEN_' . $serverIndex)->firstOrFail()->value;
                     $fromAddress = $this->config->get('mail.from.address');
                     
-                    // TODO:: Check if access token has expired
+                    $accessToken = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_ACCESS_TOKEN_' . $serverIndex)->firstOrFail()->value;
+                    $clientId = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_CLIENT_ID_' . $serverIndex)->firstOrFail()->value;
+                    $clientSecret = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_SECRET_' . $serverIndex)->firstOrFail()->value;
+                    $refreshToken = EnvironmentVariable::select('value')->where('name', 'EMAIL_CONNECTOR_GMAIL_API_REFRESH_TOKEN_' . $serverIndex)->firstOrFail()->value;
+                    
+                    $token = [
+                        "client_id" => $clientId,
+                        "client_secret" => $clientSecret,
+                        "access_token" => $accessToken,
+                        "refresh_token" => $refreshToken
+                    ];
+
+                    // Check if access token has expired
+                    $client = new \Google\Client();
+                    $authConfig = array(
+                        "web" => array(
+                            'client_id' => $clientId, 
+                            'client_secret' => $clientSecret
+                        )
+                    );
+                    $client->setAuthConfig($authConfig);
+                    $client->setAccessToken($token);
+                    
+                    // Update Authencation Mode
                     $transport->setAuthMode('XOAUTH2')
                     ->setUsername($fromAddress)
                     ->setPassword($accessToken);
