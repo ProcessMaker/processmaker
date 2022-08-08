fd5cc31c3bec99f7882e502fbb90e7be1df3c177
Shift <shift@laravelshift.com>
2022-08-08 16:27:14 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,37 +37,44 @@ return [
     | may even configure multiple disks of the same driver. Defaults have
     | been setup for each driver as an example of the required options.
     |
-    | Supported Drivers: "local", "ftp", "s3", "rackspace"
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
     'disks' => [
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
+
         'imports' => [
             'driver' => 'local',
             'root' => storage_path('app/imports'),
         ],
+
         'keys' => [
             'driver' => 'local',
             'root' => env('KEYS_PATH') ? base_path(env('KEYS_PATH')) : storage_path('keys'),
         ],
+
         'mailtemplates' => [
             'driver' => 'local',
             'root' => env('MAILTEMPLATES_PATH') ? base_path(env('MAILTEMPLATES_PATH')) : storage_path('mailTemplates'),
         ],
+
         'process_templates' => [
             'driver' => 'local',
             'root' => env('PROCESS_TEMPLATES_PATH') ? base_path(env('PROCESS_TEMPLATES_PATH')) : database_path('processes/templates'),
         ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
@@ -75,30 +82,51 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
         ],
+
         'profile' => [
             'driver' => 'local',
             'root' => storage_path('app/public/profile'),
             'url' => env('APP_URL').'/storage/profile',
             'visibility' => 'public',
         ],
+
         'settings' => [
             'driver' => 'local',
             'root' => storage_path('app/public/setting'),
             'url' => env('APP_URL').'/storage/setting',
             'visibility' => 'public',
         ],
+
         'private_settings' => [
             'driver' => 'local',
             'root' => storage_path('app/private/settings'),
             'visibility' => 'private',
         ],
+
         'tmp' => [
             'driver' => 'local',
             'root' => storage_path('app/public/tmp'),
             'url' => env('APP_URL').'/storage/tmp',
             'visibility' => 'public',
         ],
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Symbolic Links
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the symbolic links that will be created when the
+    | `storage:link` Artisan command is executed. The array keys should be
+    | the locations of the links and the values should be their targets.
+    |
+    */
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
