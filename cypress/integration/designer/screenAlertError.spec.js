1d6a9dddacf91b8c51e8252de8567d1f2e7ff326
Marco A. Nina Mena <marcoantonina@gmail.com>
2022-08-16 15:59:29 -0400

    add test alert error in screen

---
+++ cypress/integration/designer/screenAlertError.spec.js
@@ -0,0 +1,30 @@
+
+
+describe('screen page', () => {
+    it("should go to the screens page", function () {
+      cy.visit("/designer/screens");
+      
+      cy.title()
+        .should("include", "Screens");
+
+      cy.importScreen('screen_error_alert.json');
+      cy.wait(500);
+      cy.get('span').contains('Screen Error Alert').click();
+      cy.get('.btn-outline-secondary').click();
+      
+      //no submit form Invalid
+      cy.get(':nth-child(2) > .form-group > .btn').click();
+      cy.get('.alert-danger').should('be.visible');
+      cy.get('.alert-danger').contains('here is a validation error in your form.');
+
+      //submit form valid
+      cy.get('[data-cy="screen-field-form_input_1"]').type('12345678');
+      cy.get(':nth-child(2) > .form-group > .btn').click();
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal('Preview Form was Submitted');
+      });
+      cy.on('window:confirm', () => true);
+      
+    });
+  
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import 'cypress-file-upload';
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -29,4 +31,28 @@ Cypress.Commands.add("omitError", () => {
     // failing the test
     return false;
   });
+});
+
+Cypress.Commands.add('importScreen', (filePath) => {
+  let fileBtn = 'input[type="file"]';
+  cy.get('[data-cy="button-import-screen"]').click();
+  cy.window().then((win) => {
+    const element = win.document.getElementById("import-file");
+    element.classList.remove("d-none");
+    win.document.querySelector(fileBtn).style.visibility = "visible";
+    win.document.querySelector(fileBtn).style.display = "block";
+    win.document.querySelector(fileBtn).style.width = "200px";
+    win.document.querySelector(fileBtn).style.height = "20px";
+    win.document.querySelector(fileBtn).style.position = "fixed";
+    win.document.querySelector(fileBtn).style.overflow = "visible";
+    win.document.querySelector(fileBtn).style.zIndex = "9999999";
+    win.document.querySelector(fileBtn).style.top = "500px";
+    win.document.querySelector(fileBtn).style.left = "500px";
+    win.document.querySelector(fileBtn).style.right = "500px";
+    win.document.querySelector(fileBtn).style.bottom = "500px";
+  });
+  cy.get(fileBtn).attachFile(filePath);
+  cy.get('[data-cy="button-import"]').click();
+  cy.get('[data-cy="button-list-screen"]').should('be.visible');
+  cy.get('[data-cy="button-list-screen"]').click();
 });
