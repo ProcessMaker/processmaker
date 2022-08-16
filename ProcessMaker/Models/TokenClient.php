e7f7c9842de974ccec85d8a4abe137529bef9fe8
Nolan Ehrstrom <github@nolanpro.com>
2022-08-16 11:14:03 -0700

    Upgrade php swagger

--- ProcessMaker/Models/TokenClient.php
+++ ProcessMaker/Models/TokenClient.php
@@ -4,21 +4,21 @@ namespace ProcessMaker\Models;
 
 use Laravel\Passport\Client;
 
+/**
+ * @OA\Schema(
+ *   schema="TokenClient",
+ *   @OA\Property(property="id", type="integer"),
+ *   @OA\Property(property="user_id", type="integer"),
+ *   @OA\Property(property="name", type="string"),
+ *   @OA\Property(property="provider", type="string"),
+ *   @OA\Property(property="redirect", type="string"),
+ *   @OA\Property(property="personal_access_client", type="boolean"),
+ *   @OA\Property(property="password_client", type="boolean"),
+ *   @OA\Property(property="revoked", type="boolean"),
+ *   @OA\Property(property="created_at", type="string", format="date-time"),
+ *   @OA\Property(property="updated_at", type="string", format="date-time"),
+ * )
+ */
 class TokenClient extends Client
 {
-    /**
-     * @OA\Schema(
-     *   schema="TokenClient",
-     *   @OA\Property(property="id", type="integer"),
-     *   @OA\Property(property="user_id", type="integer"),
-     *   @OA\Property(property="name", type="string"),
-     *   @OA\Property(property="provider", type="string"),
-     *   @OA\Property(property="redirect", type="string"),
-     *   @OA\Property(property="personal_access_client", type="boolean"),
-     *   @OA\Property(property="password_client", type="boolean"),
-     *   @OA\Property(property="revoked", type="boolean"),
-     *   @OA\Property(property="created_at", type="string", format="date-time"),
-     *   @OA\Property(property="updated_at", type="string", format="date-time"),
-     * )
-     */
 }
