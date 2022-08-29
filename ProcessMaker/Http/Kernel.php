6a42cf7eed22d08259729da9c86d692535047b8e
Nolan Ehrstrom <github@nolanpro.com>
2022-08-29 10:02:54 -0700

    Update maintenance mode middleware class name

--- ProcessMaker/Http/Kernel.php
+++ ProcessMaker/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \ProcessMaker\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
