dbbd3dd97e86f8781d2a83d2c3b9e2f468819419
Nolan Ehrstrom <github@nolanpro.com>
2022-09-08 11:50:23 -0700

    Some tests expect a user to be available so seed the anonymous user

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -94,6 +94,7 @@ if (env('TEST_TOKEN')) {
 } elseif (env('POPULATE_DATABASE')) {
     Artisan::call('db:wipe', ['--database' => \DB::connection()->getName()]);
     Artisan::call('migrate:fresh', []);
+    Artisan::call('db:seed', ['--class' => 'AnonymousUserSeeder']);
 
     ScriptExecutor::firstOrCreate(
         ['language' => 'php'],
