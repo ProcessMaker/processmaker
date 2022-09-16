71bd69822223ba434326121195c38a639d10d3f1
Teisha McRae <mcraeteisha@gmail.com>
2022-09-16 18:13:04 -0400

    Clean up code and formatting; pull in processName

--- resources/js/processes/export/components/ExportManagerView.vue
+++ resources/js/processes/export/components/ExportManagerView.vue
@@ -5,65 +5,38 @@
         <div class="card text-center">
           <div class="card-header bg-light" align="left">
             <h5>{{ $t("Export Process") }}</h5>
-            <h6 class="text-muted">
-              {{ $t("Download a process model and its associated assets.") }}
-            </h6>
+            <h6 class="text-muted">{{ $t("Download a process model and its associated assets.") }}</h6>
           </div>
           <div class="card-body" align="left">
-            <h5 class="card-title export-type">
-              {{ $t("You are about to export") }}
-              <span class="font-weight-bold">{{
-                $t("[[Process Name]].")
-              }}</span>
+            <h5 class="card-title export-type">{{ $t("You are about to export") }}
+              <span class="font-weight-bold">{{ processName + "."}}</span>
             </h5>
             <div>
               <b-form-group label="Select Export Type" class="medium-font">
-                <div class="export-type">
-                  <b-form-radio
-                    v-model="selected"
-                    aria-describedby="basic-export-type"
-                    name="basic-export-option"
-                    value="basic"
-                  >
+                <div class="pb-1">
+                  <b-form-radio v-model="selected" aria-describedby="basic-export-type" name="basic-export-option" value="basic">
                     {{ $t("Basic") }}
+                    <div class="helper-text text-muted">
+                        <small id="basic-export-type">{{ $t("Download all related assets.") }}</small>
+                    </div>
                   </b-form-radio>
-                  <b-form-text id="basic-export-type" class="form-text">
-                    {{ $t("Download all related assets.") }}
-                  </b-form-text>
                 </div>
-                <div class="export-type">
-                  <b-form-radio
-                    v-model="selected"
-                    aria-describedby="custom-export-type"
-                    name="custom-export-option"
-                    value="custom"
-                  >
+                <div class="pb-1">
+                  <b-form-radio v-model="selected" aria-describedby="custom-export-type" name="custom-export-option" value="custom">
                     {{ $t("Custom") }}
+                    <div class="helper-text text-muted">
+                        <small id="custom-export-type">{{ $t("Select which assets to include in the export file for a custom export package.") }}</small>
+                    </div>
                   </b-form-radio>
-                  <b-form-text id="custom-export-type" class="form-text">
-                    {{
-                      $t(
-                        "Select which assets to include in the export file for a custom export package."
-                      )
-                    }}
-                  </b-form-text>
                 </div>
               </b-form-group>
             </div>
           </div>
           <div class="card-footer bg-light" align="right">
-            <button
-              type="button"
-              class="btn btn-outline-secondary"
-              @click="onCancel"
-            >
+            <button type="button" class="btn btn-outline-secondary" @click="onCancel">
               {{ $t("Cancel") }}
             </button>
-            <button
-              type="button"
-              class="btn btn-primary ml-2"
-              @click="onExport"
-            >
+            <button type="button" class="btn btn-primary ml-2" @click="onExport">
               {{ $t("Export") }}
             </button>
           </div>
@@ -103,13 +76,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.export-type {
-  padding-bottom: 10px;
-}
-.form-text {
-  text-indent: 2em;
-  margin-top: -5px;
-}
 .medium-font {
   font-weight: 500;
 }
