55c1f1baa72c7a21d674a6ed6d277a57ed1b4e14
sanjacornelius <sanja.cornelius@processmaker.com>
2022-09-19 13:29:23 -0700

    Initial Enter Password Modal

--- resources/js/processes/import/components/EnterPasswordModal.vue
+++ resources/js/processes/import/components/EnterPasswordModal.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
-    <!-- TODO: IMPORT/EXPORT Handle Enter Password -->
+    <modal id="enterPassword" :title="$t('Enter Password')" :ok-disabled="disabled" @ok.prevent="onSubmit" @hidden="onClose">
+      <template> 
+        <b-form-group
+          required
+          :label="$t('Password')"
+        >
+          <b-form-input
+            autofocus
+            v-model="password"
+            autocomplete="off"
+            name="password"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </template>
+    </modal>
   </div>
 </template>
 
 <script>
-export default {
+  import { FormErrorsMixin, Modal, Required } from "SharedComponents";
 
-}
+  export default {
+    components: { Modal, Required },
+    mixins: [ FormErrorsMixin ],
+    props: [''],
+    data: function() {
+      return {
+        showModal: false,
+        password: '',
+        disabled: false
+      }
+    },
+    methods: {
+      onClose () {
+        this.password = "";
+      },
+      onSubmit () {
+        console.log('HANDLE ON SUBMIT');
+      }
+    }
+  };
 </script>
-
-<style>
-
-</style>
