3c9c2af80474f73f8ce26e2b9fe0d1b5cf0a5b8c
sanjacornelius <sanja.cornelius@processmaker.com>
2022-09-21 15:21:05 -0700

    Add import process modal text

--- resources/js/processes/import/components/ImportProcessModal.vue
+++ resources/js/processes/import/components/ImportProcessModal.vue
@@ -2,18 +2,24 @@
   <div>
     <modal 
       id="importProccess" 
-      :title="$t('Import Process')" 
-      :ok-title="$t('Import As New')"
-      :ok-disabled="disabled" 
-      @ok.prevent="importFileAsNew" 
+      :title="title" 
       @hidden="onClose"
       @update="onUpdate"
-      @import="importAsNew"
+      @importAsNew="importAsNew"
       :setCustomButtons="true"
       :customButtons="customModalButtons"
     >
       <template> 
-        <h5>Caution: Process Already Exists</h5>
+        <h5>
+            <i class="fas fa-exclamation-triangle text-warning"></i> 
+            {{$t('Caution: Process Already Exists')}}
+            <div><small class="helper text-muted">{{ $t('This environment contains a process with the same ID.') }}</small></div>
+        </h5>
+        <p v-if="!userHasEditPermissions">{{ $t('You do not have permissions to update the existing process in this environment') }}</p>
+        <ul>
+            <li>{{ $t('Import As New will create a new process in this environment.') }}</li>
+            <li v-if="userHasEditPermissions">{{ $t('Update will overwrite any assets tied to the current process. This may cause unintended side effects.') }}</li>
+        </ul>
       </template>
     </modal>
   </div>
@@ -25,7 +31,7 @@
   export default {
     components: { Modal },
     mixins: [ FormErrorsMixin ],
-    props: ['userHasEditPermissions'],
+    props: ['processName', 'userHasEditPermissions'],
     data: function() {
       return {
         showModal: false,
@@ -38,7 +44,9 @@
       }
     },
     computed: {
-      
+      title() {
+        return this.$t('Import Process: {{item}}', {item: this.processName});
+      }
     },
     watch: {
       
@@ -61,7 +69,7 @@
         console.log('UPDATE PROCESS AS NEW');
       },
       importAsNew() {
-        console.log('IMPORT NEW PROCESS');
+        this.$emit('import-as-new');
       }
     }
   };
