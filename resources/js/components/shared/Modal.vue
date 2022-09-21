80eae48852f5c04ace29307b4dc89bf975e7a850
sanjacornelius <sanja.cornelius@processmaker.com>
2022-09-21 14:45:50 -0700

    Handle custom modal buttons

--- resources/js/components/shared/Modal.vue
+++ resources/js/components/shared/Modal.vue
@@ -2,12 +2,11 @@
   <b-modal
     :id="id"
     ref="pmModal"
-    :title="title"
     footer-class="pm-modal-footer"
     cancel-variant="outline-secondary"
     :cancel-title="$t('Cancel')"
-    ok-variant="secondary"
-    :ok-title="$t('Save')"
+    ok-variant="primary"
+    :ok-title="okTitle ? okTitle : $t('Save')"
     :ok-disabled="okDisabled"
     no-close-on-backdrop
     centered
@@ -20,13 +19,28 @@
     @show="onEvent('show', $event)"
     @shown="onEvent('shown', $event)"
   >
+   <template #modal-title>
+      <h5>{{title}}</h5>
+      <small v-if="subtitle" class="text-muted subtitle">{{subtitle}}</small>
+    </template>
     <slot></slot>
+    <template v-if="setCustomButtons" #modal-footer>
+      <b-button v-for="button in customButtons" 
+        :key="button.content" 
+        @click="executeFunction(button.action)" 
+        :variant="button.variant" 
+        :disabled="button.disabled"
+        :hidden="button.hidden"
+      >
+        {{ button.content }}
+      </b-button>
+    </template>
   </b-modal>
 </template>
 
 <script>
   export default {
-    props: ["id", "title", "ok-disabled"],
+    props: ["id", "title", "subtitle", "ok-disabled", 'ok-title', 'setCustomButtons', 'customButtons'],
     methods: {
       onEvent(name, event) {
         this.$emit(name, event);
@@ -36,7 +50,17 @@
       },
       hide() {
         this.$refs.pmModal.hide();
-      }
+      },
+      executeFunction(callback) {
+        if (typeof eval(`this.$refs.pmModal.${callback}`) === "function") {
+          eval(`this.$refs.pmModal.${callback}`)
+        } else {
+          this.$emit(callback);
+        }
+      },
+    },
+    mounted() {
+      
     }
   };
 </script>
@@ -45,4 +69,9 @@
   .pm-modal-footer .btn {
     margin: 0;
   }
+
+  .subtitle {
+    font-size: 70%;
+  }
+
 </style>
