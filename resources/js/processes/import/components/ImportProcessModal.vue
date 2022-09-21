a1ba29b3919f0071e3950bd82ac727378430fbad
sanjacornelius <sanja.cornelius@processmaker.com>
2022-09-21 14:47:06 -0700

    Create custom modal buttons

--- resources/js/processes/import/components/ImportProcessModal.vue
+++ resources/js/processes/import/components/ImportProcessModal.vue
@@ -7,6 +7,10 @@
       :ok-disabled="disabled" 
       @ok.prevent="importFileAsNew" 
       @hidden="onClose"
+      @update="onUpdate"
+      @import="importAsNew"
+      :setCustomButtons="true"
+      :customButtons="customModalButtons"
     >
       <template> 
         <h5>Caution: Process Already Exists</h5>
@@ -21,11 +25,16 @@
   export default {
     components: { Modal },
     mixins: [ FormErrorsMixin ],
-    props: ['userPermissions'],
+    props: ['userHasEditPermissions'],
     data: function() {
       return {
         showModal: false,
         disabled: true,
+        customModalButtons: [
+            {'content': 'Cancel', 'action': 'hide()', 'variant': 'outline-secondary', 'disabled': false, 'hidden': false},
+            {'content': 'Update', 'action': 'update', 'variant': 'secondary', 'disabled': false, 'hidden': true},
+            {'content': 'Import as New', 'action': 'import', 'variant': 'primary', 'disabled': false, 'hidden': false},
+        ],
       }
     },
     computed: {
@@ -34,6 +43,13 @@
     watch: {
       
     },
+    beforeMount() {
+        if (this.userHasEditPermissions) {
+            this.customModalButtons[1].hidden = false
+        } else {
+            this.customModalButtons[1].hidden = true;
+        }
+    },
     methods: {
       show() {
         this.$bvModal.show('importProccess');
@@ -41,6 +57,12 @@
       onClose () {
        console.log('MODAL CLOSED');
       },
+      onUpdate() {
+        console.log('UPDATE PROCESS AS NEW');
+      },
+      importAsNew() {
+        console.log('IMPORT NEW PROCESS');
+      }
     }
   };
 </script>
