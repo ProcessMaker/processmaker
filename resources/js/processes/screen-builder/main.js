6d45922b8c437dd01a20c43564e0a2318941b20d
David Callizaya <davidcallizaya@gmail.com>
2022-10-10 16:43:21 -0400

    Merge pull request #4525 from ProcessMaker/FOUR-6752
    FOUR-6752 Added ScreenBuilder modules to be initialized on app load

--- resources/js/processes/screen-builder/main.js
+++ resources/js/processes/screen-builder/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import ScreenBuilder from "./screen";
+import globalErrorsModule from "@processmaker/screen-builder/src/store/modules/globalErrorsModule";
+import undoRedoModule from "@processmaker/screen-builder/src/store/modules/undoRedoModule";
 
 Vue.use(Vuex);
-const store = new Vuex.Store({ modules: {} });
+const store = new Vuex.Store({
+  modules: {
+    globalErrorsModule,
+    undoRedoModule,
+  },
+});
 
 // Bootstrap our Designer application
 new Vue({
