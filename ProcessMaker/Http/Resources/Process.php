8b13ac3d6227a785d69eb4a9adb6885aa970868a
Agustin Busso <agustin.busso@processmaker.com>
2022-11-18 16:17:22 -0300

    Fix format

--- ProcessMaker/Http/Resources/Process.php
+++ ProcessMaker/Http/Resources/Process.php
@@ -20,9 +20,9 @@ class Process extends ApiResource
         }
         if (in_array('category', $include)) {
             $array['category'] = new ProcessCategory($this->category);
-        } 
-        if (in_array('events', $include)) { 
-            $array['events'] = $this->getStartEvents(true, $filterWithoutAssignments); 
+        }
+        if (in_array('events', $include)) {
+            $array['events'] = $this->getStartEvents(true, $filterWithoutAssignments);
         }
         if (in_array('svg', $include)) {
             $array['svg'] = $this->svg;
