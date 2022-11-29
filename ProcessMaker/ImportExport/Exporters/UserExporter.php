b6c00c6e7ac086eb68f103506bc25805422d3c13
Esteban Gallego <egallego00@gmail.com>
2022-11-28 16:59:34 -0800

    feature/FOUR-7146

--- ProcessMaker/ImportExport/Exporters/UserExporter.php
+++ ProcessMaker/ImportExport/Exporters/UserExporter.php
@@ -2,16 +2,28 @@
 
 namespace ProcessMaker\ImportExport\Exporters;
 
+use ProcessMaker\ImportExport\DependentType;
 use ProcessMaker\Models\User;
 
 class UserExporter extends ExporterBase
 {
     public function export() : void
     {
+        foreach ($this->model->groups as $group) {
+            $this->addDependent(DependentType::GROUPS, $group, GroupExporter::class);
+        }
     }
 
     public function import() : bool
     {
-        return $this->model->save();
+        $user = $this->model;
+        $user->saveOrFail();
+
+        foreach ($this->getDependents(DependentType::GROUPS) as $dependent) {
+            $group = $dependent->model;
+            $user->groups()->attach($group->id);
+        }
+
+        return true;
     }
 }
