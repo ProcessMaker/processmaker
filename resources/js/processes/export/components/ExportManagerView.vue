121dbed44aaa4f49fc07005a8c4eacae82930d04
Teisha McRae <mcraeteisha@gmail.com>
2022-11-29 15:06:52 -0500

    Update exported file name to be process name

--- resources/js/processes/export/components/ExportManagerView.vue
+++ resources/js/processes/export/components/ExportManagerView.vue
@@ -91,7 +91,7 @@ export default {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement("a");
             link.href = url;
-            link.setAttribute("download", "export.json");
+            link.setAttribute("download", this.processName + ".json");
             document.body.appendChild(link);
             link.click();
         })
