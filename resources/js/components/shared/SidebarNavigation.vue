b71728374d99d8fda9448a218d8eef72ce4c2315
Teisha McRae <mcraeteisha@gmail.com>
2022-12-02 18:09:37 -0500

    Update SidebarNavigation; Add Sidebar to CustomExportView

--- resources/js/components/shared/SidebarNavigation.vue
+++ resources/js/components/shared/SidebarNavigation.vue
@@ -1,17 +1,41 @@
 <template>
     <div>
-        <!-- TODO: IMPORT/EXPORT Create Sidebar Navigation -->
+        <span class="mb-0 font-weight-bold">{{ processName }}
+            <a
+                @click="showProcessElements = !showProcessElements"
+                :aria-expanded="showProcessElements ? 'true' : 'false'"
+            >
+                <i
+                class="fa"
+                :class="{
+                    'fa-chevron-right': !showProcessElements,
+                    'fa-chevron-down': showProcessElements,
+                }"
+                style="width: 14px"
+                ></i>
+            </a>
+        </span>
+            <div>
+            <!-- <pre
+                class="mt-1 mb-0"
+                @click="
+                showProcessElements = !showProcessElements
+                ">{{ initDockerfile.split("\n")[0] }} <template v-if="!showDockerfile">...</template></pre> -->
+            <b-collapse id="processelements" v-model="showProcessElements" :aria-hidden="showProcessElements ? 'false' : 'true'">
+                <!-- <pre>{{ initDockerfile.split("\n").slice(1).join("\n") }}</pre> -->
+            </b-collapse>
+            </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: [],
+    props: ["processName"],
     components: {},
     mixins: [],
     data() {
         return {
-
+            showProcessElements: false,
         }
     },
     methods: {      
---
+++ resources/js/processes/export/components/CustomExportView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <b-row>
+            <b-col cols="4">
+                <sidebar-navigation ref="sidebar-navigation" :processName="processName"></sidebar-navigation>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+
+import SidebarNavigation from "../../../components/shared/SidebarNavigation.vue";
+
+export default {
+    props: ["processName"],
+    components: {
+        SidebarNavigation,
+    },
+    mixins: [],
+    data() {
+        return {
+
+        }
+    },
+    methods: {      
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style>
+
+</style>
