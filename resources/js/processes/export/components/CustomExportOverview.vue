ba4c288a13b6ff69523f4334a80bd82a5b217fe2
Teisha McRae <mcraeteisha@gmail.com>
2022-12-08 16:27:07 -0500

    Add CustomExportOverview

---
+++ resources/js/processes/export/components/CustomExportOverview.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!-- TO DO: ADD IN COMPONENTS FOR PROCESS, SCRIPT, SCREENS, ETC. -->
+        <component :is="processElements"></component>
+    </div>
+</template>
+
+<script>
+
+import ProcessesView from "./process-elements/ProcessesView.vue";
+
+export default {
+    props: ["processName"],
+    components: {
+        ProcessesView,
+    },
+    mixins: [],
+    data() {
+        return {
+            processElements: "processes-view",
+        }
+    },
+    methods: {      
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style>
+
+</style>
+
--- resources/js/processes/export/components/CustomExportView.vue
+++ resources/js/processes/export/components/CustomExportView.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="container">
+    <div class="custom-export-container p-5">
         <b-row>
             <b-col cols="3" class="border-right">
                 <sidebar-navigation ref="sidebar-navigation" :processName="processName"></sidebar-navigation>
             </b-col>
             <b-col cols="6" class="data-container">
-                <h2>Export Process: {{ processName }}</h2>
-                <hr>
+                <custom-export-overview :processName="processName"></custom-export-overview>
             </b-col>
       <b-col cols="3" />
         </b-row>
@@ -15,12 +14,14 @@
 
 <script>
 
+import CustomExportOverview from "./CustomExportOverview.vue";
 import SidebarNavigation from "../../../components/shared/SidebarNavigation.vue";
 
 export default {
     props: ["processName"],
     components: {
         SidebarNavigation,
+        CustomExportOverview
     },
     mixins: [],
     data() {
@@ -38,8 +39,9 @@ export default {
 <style lang="scss" scoped>
 @import "../../../../sass/variables";
 
-.container {
-    // max-width: 1600px;
+.custom-export-container {
+    max-width: 1600px;
+    margin: auto;
     background-color: $light;
 }
 
