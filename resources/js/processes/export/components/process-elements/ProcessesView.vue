6a4ec24258e5beaeee98286766ce6fdac97938f9
Teisha McRae <mcraeteisha@gmail.com>
2022-12-08 16:25:57 -0500

    Add Process Element Views

---
+++ resources/js/processes/export/components/process-elements/ProcessesView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- TODO: Set Up Process Component -->
+        <h2>Export Process: {{ processName }}</h2>
+        <hr>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: [],
+    components: {
+    },
+    mixins: [],
+    data() {
+        return {
+
+        }
+    },
+    methods: {      
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ resources/js/processes/export/components/process-elements/SignalsView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- TODO: Set Up Component -->
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: [],
+    components: {
+    },
+    mixins: [],
+    data() {
+        return {
+
+        }
+    },
+    methods: {      
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style>
+
+</style>
+
