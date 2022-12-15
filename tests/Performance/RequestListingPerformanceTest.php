7ee073dbae6faffbf81526ba2d6bbcb6a3ae7fdd
Nolan Ehrstrom <github@nolanpro.com>
2022-12-15 15:32:51 -0800

    Cleanup

--- tests/Performance/RequestListingPerformanceTest.php
+++ tests/Performance/RequestListingPerformanceTest.php
@@ -16,23 +16,23 @@ class RequestListingPerformanceTest extends TestCase
     {
         $seeder = new RequestListingPerformanceData();
 
-        $seeder->requestCount = 100_000;
-        $seeder->processCount = 50;
-        $seeder->userCount = 20_000;
+        $seeder->requestCount = 10_000;
+        $seeder->processCount = 10;
+        $seeder->userCount = 1_000;
 
         $seeder->run();
         $seeder->associateWithUser(50, 50, 50);
         $this->user = $seeder->user;
 
-        $route = route('api.requests.index', ['limit' => 1000]);
+        $route = route('api.requests.index');
         $start = microtime(true);
         $result = $this->apiCall('GET', $route);
         $duration = microtime(true) - $start;
 
         $total = $result->json()['meta']['total'];
 
-        $this->assertEquals(150, $total);
-        $this->assertLessThan(5, $duration);
-        echo "\n----\nDuration $duration\n----\n";
+        // $this->assertEquals(150, $total);
+        // $this->assertLessThan(5, $duration);
+        echo "\n----\nDuration: $duration, $total: $total\n----\n";
     }
 }
