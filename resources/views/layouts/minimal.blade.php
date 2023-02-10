010e6facdfbc9e84d8cbb4340588a5306f65c139
Paulo Sakamoto <paas@hotmail.com>
2023-02-10 16:21:47 -0300

    Removing CSP Meta Tag - FOUR-7388

--- resources/views/layouts/minimal.blade.php
+++ resources/views/layouts/minimal.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="{{ \ProcessMaker\Http\Middleware\CSP::HEADER  }}" content="{!! \ProcessMaker\Http\Middleware\CSP::getRules() !!}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="i18n-mdate" content='{!! json_encode(ProcessMaker\i18nHelper::mdates()) !!}'>
