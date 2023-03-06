ee64743b91e95f01a16faa92544d9a05798a4555
Esteban Gallego <egallego00@gmail.com>
2023-03-06 11:20:34 -0800

    Created Templates boilerplate

---
+++ ProcessMaker/Models/Template.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ProcessMaker\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use ProcessMaker\Models\ProcessMakerModel;
+
+class Template extends ProcessMakerModel
+{
+    use HasFactory;
+}
