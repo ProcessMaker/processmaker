5efb5f57451ae63fca2dfb324420bc5f816b881c
Sanja <sanja.cornelius@processmaker.com>
2023-03-06 11:16:13 -0800

    Create migration for process_templates and process_template_categories table

---
+++ database/migrations/2023_03_06_173333_create_process_templates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProcessTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('process_templates', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique()->nullable();
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->json('manifest');
+            $table->integer('file_id');
+            $table->unsignedInteger('process_template_category_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('process_templates');
+    }
+}
