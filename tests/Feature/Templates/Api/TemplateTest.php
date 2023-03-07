262f1ccc0f30f79d46febd98a0134c8b4889ac38
Sanja <sanja.cornelius@processmaker.com>
2023-03-07 09:02:59 -0800

    Assert record is in the database; Code clean up

--- tests/Feature/Templates/Api/TemplateTest.php
+++ tests/Feature/Templates/Api/TemplateTest.php
@@ -36,9 +36,6 @@ class TemplateTest extends TestCase
         Utils::setAttributeAtXPath($process, '/bpmn:definitions/bpmn:process/bpmn:task[1]', 'pm:screenRef', $screen->id);
         $process->save();
 
-        // TODO: Export Process Model
-        //$options = new Options([$screen->uuid => ['mode' => 'discard']]);
-
         $response = $this->apiCall(
             'POST',
             route('api.template.store', [
@@ -55,34 +52,7 @@ class TemplateTest extends TestCase
 
         // Validate the header status code
         $response->assertStatus(200);
-
-        // $screen = Screen::factory()->create(['title' => 'Screen']);
-
-        // $response = $this->apiCall(
-        //     'POST',
-        //     route('api.export.download', [
-        //         'type' => 'screen',
-        //         'id' => $screen->id,
-        //     ]),
-        //     [
-        //         'password' => 'foobar',
-        //         'options' => [],
-        //     ]
-        // );
-
-        // // Ensure we can download the exported file.
-        // $response->assertStatus(200);
-        // $response->assertHeader('content-disposition', "attachment; filename={$screen->title}.json");
-
-        // // Ensure it's encrypted.
-        // $payload = json_decode($response->streamedContent(), true);
-        // $this->assertEquals(true, $payload['encrypted']);
-
-        // $headers = $response->headers;
-        // $exportInfo = json_decode($headers->get('export-info'), true)['exported'];
-        // $this->assertCount(1, $exportInfo['Screen']['ids']);
-        // $this->assertEquals($screen->id, $exportInfo['Screen']['ids'][0]);
-        // $this->assertCount(1, $exportInfo['ScreenCategory']['ids']);
-        // $this->assertEquals($screen->categories[0]->id, $exportInfo['ScreenCategory']['ids'][0]);
+        // Assert that our database has the process we need
+        $this->assertDatabaseHas('process_templates', ['name' => 'Test Template']);
     }
 }
