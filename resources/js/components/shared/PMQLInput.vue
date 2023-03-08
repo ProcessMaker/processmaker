7d43d3d3440a3b4220b06308fc3591434937601a
Agustin Busso <agustin.busso@processmaker.com>
2023-03-08 15:27:43 -0300

    Add props. Fix css. Change event name

--- resources/js/components/shared/PMQLInput.vue
+++ resources/js/components/shared/PMQLInput.vue
@@ -2,58 +2,63 @@
   <div class="search-bar-inputs flex-grow w-100">
     <div class="group">
       <input ref="search_input" type="text" class="search-input"
-        :aria-label="$t('Advanced Search (PMQL)')"
+        :aria-label="inputAreaLabel"
         v-model="pmql"
-        @keyup.enter="runSearch(true)"
+        @keyup.enter="runSearch()"
         required>
         <label class="float-label">
-          <i v-if="assistantLoading" class="fa fa-spinner fa-spin mr-1"></i> 
+          <i v-if="aiLoading" class="fa fa-spinner fa-spin mr-1"></i> 
           <span v-html="searchInputLabel"></span>
         </label>
     </div>
   </div>
 </template>
-
 <script>
 
 import isPMQL from "../../modules/isPMQL";
 
 export default {
-  props: ["type"],
+  props: ["searchType", "value", "aiEnabled", "searchLabel", "areaLabel"],
   data() {
     return {
-      assistantEnabled: false,
-      assistantLoading: false,
-      searchInputLabel: "Search using natural language or PMQL",
+      aiLoading: false,
+      searchInputLabel: "",
+      inputAreaLabel: "",
       pmql: "",
     };
   },
 
-  methods: {
-    runSearch(advanced) {
-      if (! advanced) {
-        this.buildPmql();
-      }
+  mounted() {
+    this.pmql = this.value;
+    this.searchInputLabel = this.searchLabel;
+    this.inputAreaLabel = this.areaLabel;
+    Vue.nextTick().then(() => {
+      this.$refs.search_input.focus();
+    });
+  },
 
+  methods: {
+    runSearch() {
       if (this.pmql.isPMQL()) {
-        this.$emit('submit');
-      } else {
-        this.runNLPToPMQL();
+        this.$emit("submit", this.pmql);
+      } else if (this.aiEnabled) {
+        this.runNLQToPMQL();
       }
     },
-    runNLPToPMQL() {
-      let params = { question: this.pmql, type: this.type };
+    runNLQToPMQL() {
+      const params = {
+        question: this.pmql,
+        type: this.searchType,
+      };
 
-      this.assistantLoading = true;
-      this.searchInputLabel = "Generating PMQL query for: " + "<i>"  + this.pmql + "</i>";
+      this.aiLoading = true;
+      this.searchInputLabel = `Generating PMQL query for: <i>${this.pmql}</i>`;
 
       ProcessMaker.apiClient.post("/openai/nlq-to-pmql", params).then(response => {
-        this.assistantEnabled = false;
-        this.searchInputLabel = '<i class="fa fa-check text-success"></i> ' + this.pmql;
+        this.searchInputLabel = `<i class="fa fa-check text-success"></i> ${this.pmql}`;
         this.pmql = response.data.result;
-        this.assistantLoading = false;
-        this.runSearch(true);
-        setTimeout(3000);
+        this.$emit("submit", this.pmql);
+        this.aiLoading = false;
       });
     },
   },
@@ -61,47 +66,43 @@ export default {
 </script>
 
 <style lang="scss">
-    .advanced-search {
-        .group {
-            position: relative;
-            background-color: #ffffff;
-            color: #b6bfc6;
-            border-radius: 2px;
-        }
+.group {
+    position: relative;
+    background-color: #ffffff;
+    color: #b6bfc6;
+    border-radius: 2px;
+}
 
-        input.search-input {
-            background: none;
-            color: gray;
-            padding: 0.375rem 0.75rem;
-            padding-top: 1.2rem;
-            display: block;
-            width: 100%;
-            border: none;
-            border-radius: 3px 0 0 3px;
-            border: 1px solid rgba(0, 0, 0, 0.125);
-            border-right: none;
-            height: 3.5rem;
-        }
+input.search-input {
+    background: none;
+    color: gray;
+    padding: 0.2rem 0.75rem;
+    padding-top: 1.2rem;
+    display: block;
+    width: 100%;
+    border: none;
+    border-radius: 3px;
+    border: 1px solid rgba(0, 0, 0, 0.125);
+}
 
-        input.search-input:focus {
-            outline: none;
-        }
+input.search-input:focus {
+    outline: none;
+}
 
-        input.search-input:focus ~ label, input.search-input:valid ~ label {
-            top: 0px;
-            font-size: 12px;
-            color: #0872C2;
-        }
+input.search-input:focus ~ label, input.search-input:valid ~ label {
+    top: 3px;
+    font-size: 12px;
+    color: #0872C2;
+}
 
-        .float-label {
-            color: #c6c6c6;
-            font-size: 16px;
-            font-weight: normal;
-            position: absolute;
-            pointer-events: none;
-            padding: 0.3rem 0.75rem;
-            top: 10px;
-            transition: 300ms ease all;
-        }
-    }
+.float-label {
+    color: #c6c6c6;
+    font-size: 16px;
+    font-weight: normal;
+    position: absolute;
+    pointer-events: none;
+    padding: 0.1rem 0.75rem;
+    top: 10px;
+    transition: 300ms ease all;
+}
 </style>
