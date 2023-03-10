38edd6a2a10d385ce853dc920575d272921d52bd
Teisha McRae <mcraeteisha@gmail.com>
2023-03-10 00:34:05 -0500

    Update EllipsisMenu styling; pass data from ProcessesListing

--- resources/js/components/shared/EllipsisMenu.vue
+++ resources/js/components/shared/EllipsisMenu.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="dropdown-container">
     <b-dropdown
+      variant="light"
       no-caret
       no-flip
       lazy
@@ -8,16 +9,17 @@
       <template #button-content>
         <i class="fas fa-ellipsis-h" />
       </template>
-       <b-dropdown-item href="#">Action</b-dropdown-item>
-      <!-- <b-dropdown-item
-        class="btn-white"
-        v-for="action in actions"
+      <b-dropdown-item
+        v-for="action in filterPermissions"
         :key="action.value"
-        :content="action.content"
+        class="dropdown-item pl-0 mb-1 mx-auto"
+        @click="onClick(action, data)"
       >
-        <i class="fas fa-play fa-md fa-fw"/>
-        {{ action.content }}
-      </b-dropdown-item> -->
+        <div class="dropdown-content">
+        <i class="pr-1" :class="action.icon"/>
+        <span>{{ action.content }}</span>
+        </div>
+      </b-dropdown-item>
     </b-dropdown>
   </div>
 </template>
@@ -32,23 +34,60 @@ export default {
   components: { },
   filters: { },
   mixins: [],
-  props: ["actions"],
+  props: ["actions", "permission", "data"],
   data() {
     return {
+
     };
   },
-  computed: {},
+  computed: {
+    filterPermissions() {
+    const allActions = this.actions;
+    //   console.log('allActions', allActions);
+      const userPermissions = this.permission;
+    //   console.log('userPermissions', userPermissions);
+      const result = allActions.filter(item => userPermissions.includes(item.permission));
+    //   console.log('result', result);
+      return result;
+    }
+  },
   created() {
   },
+  mounted() {
+    // console.log('data in menu', this.data);
+  },
   methods: {
+    onClick(action, data) {
+    //   console.log('action', action);
+    //   console.log('data in button', data);
+      this.$emit("navigate", action, data);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.btn-ellipsis {
+.hover-class {
+    color: #6C757D;
     background-color: #EBEEF2;
-    border-color: #EBEEF2;
+}
+
+.active-class {
+    color: #FFFFFF;
+    background-color: #104A75;
+}
+
+.dropdown-item {
+    border-radius: 4px;
+    width: 95%;
+}
+
+.dropdown-content {
+    // margin-left: 0;
+    // padding-left: 0;
+    color: #42526E;
+    font-size: 14px;
+    margin-left: -15px;
 }
 
 </style>
