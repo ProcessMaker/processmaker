67226dc46505b4b53c9208afa15555c5dbace858
Esteban Gallego <egallego00@gmail.com>
2023-03-13 18:34:49 -0700

    Fixed typo

--- resources/js/components/templates/CreateTemplateModal.vue
+++ resources/js/components/templates/CreateTemplateModal.vue
@@ -15,7 +15,7 @@
         <template>
           <b-row align-v="start">
             <b-col>
-                <p>{{ $t(`This will create a re-usuable template based on the ${this.assetName} ${this.assetType}`) }}</p>
+                <p>{{ $t(`This will create a re-usable template based on the ${this.assetName} ${this.assetType}`) }}</p>
                 <required></required>
                 <p class="mb-3" v-if="showWarning"><i class="fas fa-exclamation-triangle text-warning"></i> {{ assetExistsError }}</p>
                 <b-form-group
