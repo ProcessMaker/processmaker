94a7b9de80ce2164a773de334fe3a9769860e273
Sanja <sanja.cornelius@processmaker.com>
2023-03-16 13:20:23 -0700

    Remove templatecategory schema

--- ProcessMaker/Models/TemplateCategory.php
+++ ProcessMaker/Models/TemplateCategory.php
@@ -8,32 +8,6 @@ use ProcessMaker\Traits\Exportable;
 use ProcessMaker\Traits\HideSystemResources;
 use ProcessMaker\Traits\SerializeToIso8601;
 
-/**
- * Represents a business screen category definition.
- *
- * @property string $id
- * @property string $name
- * @property string $status
- * @property \Carbon\Carbon $updated_at
- * @property \Carbon\Carbon $created_at
- *
- * @OA\Schema(
- *   schema="ScreenCategoryEditable",
- *   @OA\Property(property="name", type="string"),
- *   @OA\Property(property="status", type="string", enum={"ACTIVE", "INACTIVE"}),
- * ),
- * @OA\Schema(
- *   schema="ScreenCategory",
- *   allOf={
- *      @OA\Schema(ref="#/components/schemas/ScreenCategoryEditable"),
- *      @OA\Schema(
- *          @OA\Property(property="id", type="string", format="id"),
- *          @OA\Property(property="created_at", type="string", format="date-time"),
- *          @OA\Property(property="updated_at", type="string", format="date-time"),
- *      )
- *   },
- * )
- */
 class TemplateCategory extends ProcessMakerModel
 {
     use SerializeToIso8601;
