9236273da1c3925c93bb0d0ca10a76fcac7e2c8c
Sanja <sanja.cornelius@processmaker.com>
2023-03-16 11:03:00 -0700

    Fix failing tests

--- tests/Feature/Templates/Api/ProcessTemplateTest.php
+++ tests/Feature/Templates/Api/ProcessTemplateTest.php
@@ -60,7 +60,6 @@ class ProcessTemplateTest extends TestCase
         );
 
         // Validate the header status code
-        // dd($response);
         $response->assertStatus(200);
         // Assert that our database has the process we need
         $this->assertDatabaseHas('process_templates', ['name' => 'Test Template']);
@@ -140,14 +139,14 @@ class ProcessTemplateTest extends TestCase
 
         // Validate the header status code
         $response->assertStatus(200);
+        $template = json_decode($response->getContent(), true)['model'];
+
         // Assert that our database has the process we need
         $this->assertDatabaseHas('process_templates', ['name' => 'Test Template']);
+        $this->assertEquals($process->id, $template['process_id']);
 
-        $template = ProcessTemplates::where('name', 'Test Template')->firstOrFail();
-        $dependents = data_get(json_decode($template->manifest, true), 'original.export.dependents');
-
-        $this->assertEquals($process->id, $template->process_id);
-        foreach ($dependents as $dependent) {
+        $dependents = data_get(json_decode($template['manifest'], true), 'original.export.*.dependents');
+        foreach ($dependents[0] as $dependent) {
             $this->assertEquals(true, $dependent['discard']);
         }
     }
