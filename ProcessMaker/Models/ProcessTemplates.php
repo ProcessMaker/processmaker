41d5cdff801ba3ce874fcbd838274d2ac34270e2
Sanja <sanja.cornelius@processmaker.com>
2023-03-24 11:30:23 -0700

    Refactor: Improve ProcessTemplates; Add Categories functionality

--- ProcessMaker/Models/ProcessTemplates.php
+++ ProcessMaker/Models/ProcessTemplates.php
@@ -3,13 +3,41 @@
 namespace ProcessMaker\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use ProcessMaker\Models\ProcessMakerModel;
+use ProcessMaker\Models\Process;
+use ProcessMaker\Models\ProcessCategory;
 use ProcessMaker\Models\Template;
-use ProcessMaker\Models\TemplateCategory;
+use ProcessMaker\Traits\HasCategories;
+use ProcessMaker\Traits\ProcessTrait;
 
 class ProcessTemplates extends Template
 {
     use HasFactory;
+    use HasCategories;
+    use ProcessTrait;
 
     protected $table = 'process_templates';
+
+    const categoryClass = ProcessCategory::class;
+
+    public $process_category_id;
+
+    /**
+     * Category of the process.
+     *
+     * @return BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(ProcessCategory::class, 'process_category_id')->withDefault();
+    }
+
+    /**
+     * Set multiple|single categories to the process
+     *
+     * @param string $value
+     */
+    public function setProcessCategoryIdAttribute($value)
+    {
+        return $this->setMultipleCategories($value, 'process_category_id');
+    }
 }
