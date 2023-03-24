b4f62cd6b7401c4c48f4e8c8efe68a99113f2420
Agustin Busso <agustin.busso@processmaker.com>
2023-03-24 15:13:47 -0300

    Improve design for condensed mode

--- resources/js/components/shared/PMQLInput.vue
+++ resources/js/components/shared/PMQLInput.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <div class="">
+      <div v-if="condensed">
+        <label>{{inputLabel ? inputLabel : "PMQL"}}</label>
+        <mustache-helper/>
+        <i v-if="aiLoading" class="fa fa-spinner fa-spin pmql-icons" :style="styles?.icons"></i>
+      </div>
+
       <div class="d-flex align-items-start">
         <div class="search-bar-buttons d-flex ml-md-0 flex-column flex-md-row">
           <slot name="left-buttons"></slot>
@@ -9,18 +15,9 @@
             <span class="text-capitalize">Filter</span>
           </button>
         </div>
-        <div class="search-bar flex-grow w-100" 
+        <div class="search-bar flex-grow w-100"
           :class="{'is-invalid': validations}"
           :style="styles?.container">
-
-          <div v-if="condensed" class="d-flex justify-content-between mb-1 mt-1">
-            <i v-if="!aiLoading" class="fa fa-search ml-2 pmql-icons" :style="styles?.icons"></i>
-            <i v-if="aiLoading" class="fa fa-spinner fa-spin ml-2 pmql-icons" :style="styles?.icons"></i> 
-            <i class="fa fa-times pl-1 pr-2 pmql-icons" role="button" @click="clearQuery" :style="styles?.icons"></i>
-          </div>
-          <div v-if="condensed">
-            <div class="separator-horizontal align-items-center"></div>
-          </div>
 
           <div class="search-bar-container d-flex align-items-center">
             <i v-if="!aiLoading && !condensed" class="fa fa-search ml-3 pmql-icons" :style="styles?.icons"></i>
@@ -44,22 +41,25 @@
             <span v-if="showAiIndicator && !condensed" class="badge badge-pill badge-success">AI</span>
 
             <div v-if="showUsage && !condensed" class="separator align-items-center" :style="styles?.separators"></div>
-            <label v-if="showUsage && !condensed" class="badge badge-primary badge-pill usage-label"
-              v-b-tooltip.hover :title="'Prompt tokens: ' + usage.promptTokens + ' - Completion tokens: ' + usage.completionTokens + ' - Total: ' + usage.totalTokens + ' tokens'">
-                {{ usage.totalTokens }} tokens
-                <i class="fa fa-info-circle ml-1 pmql-icons"></i>
+            <label 
+              v-if="showUsage && !condensed" 
+              v-b-tooltip.hover
+              class="badge badge-primary badge-pill usage-label" 
+              :title="usageText">
+              {{ usage.totalTokens }} tokens
+              <i class="fa fa-info-circle ml-1 pmql-icons" />
             </label>
 
-            <div v-if="!condensed" class="separator align-items-center" :style="styles?.separators"></div>
-            <i v-if="!condensed" class="fa fa-times pl-1 pr-3 pmql-icons" role="button" @click="clearQuery" :style="styles?.icons"></i>
-
+            <div v-if="!condensed" class="separator align-items-center" :style="styles?.separators" />
+            <i v-if="!condensed" class="fa fa-times pl-1 pr-3 pmql-icons" role="button" @click="clearQuery" :style="styles?.icons"/>
           </div>
 
           <div v-if="showPmqlSection && condensed">
             <div class="separator-horizontal align-items-center"></div>
           </div>
-          <code v-if="showPmqlSection && condensed" class="w-100 d-block input-right-section mb-1 mt-1 pr-2 pl-2" :style="styles?.pmql">{{ pmql }}</code>
-
+          <code v-if="showPmqlSection && condensed" class="w-100 d-block input-right-section mb-1 mt-1 pr-2 pl-2" :style="styles?.pmql">
+            {{ pmql }}
+          </code>
         </div>
         <div class="search-bar-buttons d-flex ml-md-0 flex-column flex-md-row">
           <slot name="right-buttons"></slot>
@@ -71,9 +71,23 @@
 <script>
 
 import isPMQL from "../../modules/isPMQL";
+import MustacheHelper from '@processmaker/screen-builder/src/components/inspector/mustache-helper'
 
 export default {
-  props: ["searchType", "value", "aiEnabled", "ariaLabel", "inputId", "validations", "styles", "condensed", "placeholder", "collectionId"],
+  components: { MustacheHelper },
+  props: [
+    "searchType",
+    "value",
+    "aiEnabled",
+    "ariaLabel",
+    "inputId",
+    "validations",
+    "styles",
+    "condensed",
+    "placeholder",
+    "collectionId",
+    "inputLabel",
+  ],
   data() {
     return {
       aiLoading: false,
@@ -97,6 +111,19 @@ export default {
     showPmqlSection() {
       return this.pmql && this.pmql.isPMQL() && !this.query.isPMQL();
     },
+    usageText() {
+      const promptTokens = `Prompt tokens: ${this.usage.promptTokens}`;
+      const completionTokens = ` - Completion tokens: ${this.usage.completionTokens}`;
+      const totalTokens = ` - Total: ${this.usage.totalTokens} tokens`;
+      return promptTokens + completionTokens + totalTokens;
+    },
+    aiEnabledLocal() {
+      if (!this.searchType || this.searchType === "") {
+        return false;
+      }
+
+      return this.aiEnabled;
+    },
   },
 
   watch: {
@@ -107,27 +134,27 @@ export default {
       if (!this.query || this.query === "") {
         this.query = this.value;
       }
-      
     },
   },
 
   mounted() {
     this.query = this.value;
-    this.inputAreaLabel = this.areaLabel;
-    Vue.nextTick().then(() => {
-      this.$refs.search_input.focus();
-    });
+    this.inputAriaLabel = this.ariaLabel;
+    // Vue.nextTick().then(() => {
+    //   this.$refs.search_input.focus();
+    // });
   },
 
   methods: {
     calcInputHeight() {
       this.$refs.search_input.style.height = "auto";
       // Font size * line height in rems (1.5)
-      let fontSize = parseFloat(getComputedStyle(this.$refs.search_input).fontSize);
-      let lineHeight = fontSize * 1.5;
+      const fontSize = parseFloat(getComputedStyle(this.$refs.search_input).fontSize);
+      const lineHeight = fontSize * 1.5;
+
       // Padding top and bottom (0.4rem each)
-      let padding = fontSize * 0.4 * 2;
-      let currentHeight = padding + (this.textAreaLines * lineHeight);
+      const padding = fontSize * 0.4 * 2;
+      const currentHeight = padding + (this.textAreaLines * lineHeight);
 
       this.showScrollbars = false;
       this.$nextTick(() => {
@@ -153,7 +180,7 @@ export default {
 
       if (this.query.isPMQL()) {
         this.$emit("submit", this.query);
-      } else if (this.aiEnabled) {
+      } else if (this.aiEnabledLocal) {
         this.runNLQToPMQL();
       }
       this.calcInputHeight();
@@ -170,7 +197,7 @@ export default {
 
       this.aiLoading = true;
 
-      ProcessMaker.apiClient.post("/openai/nlq-to-pmql", params).then(response => {
+      ProcessMaker.apiClient.post("/openai/nlq-to-pmql", params).then((response) => {
         this.pmql = response.data.result;
         this.usage = response.data.usage;
         this.$emit("submit", this.pmql);
@@ -189,6 +216,7 @@ export default {
   border-radius: 3px;
   background: #ffffff;
 }
+
 .search-bar.is-invalid {
   border-color: #E50130;
 }
@@ -254,5 +282,4 @@ input.pmql-input:focus ~ label, input.pmql-input:valid ~ label {
     color: #00875A;
     background-color: #00875a26;
 }
-
 </style>
