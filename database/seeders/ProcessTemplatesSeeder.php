b143be7af459c1828f4059ab1ddce199052c8576
Esteban Gallego <egallego00@gmail.com>
2023-03-30 18:45:32 -0700

    Update Employee Onboarding seeder

--- database/seeders/ProcessTemplatesSeeder.php
+++ database/seeders/ProcessTemplatesSeeder.php
@@ -19,19 +19,21 @@ class ProcessTemplatesSeeder extends Seeder
         //load user admin
         $admin = User::where('username', 'admin')->firstOrFail();
         $processCategory = ProcessCategory::where('name', 'Uncategorized')->firstOrFail();
+        $manifest = file_get_contents(app_path('Templates/Fixtures/process-employee-onboarding-template.json'));
+        $svg = file_get_contents(app_path('Templates/Fixtures/process-employee-onboarding-template.svg'));
 
         // create process templates seed data
-        ProcessTemplates::updateOrCreate([
-            'name' => 'Employee Onboarding 2023',
-            'description' => 'New version of the employee onboarding process from veggieDonuts',
-            'process_id' => null,
-            'user_id' => $admin->getKey(),
-            'process_category_id' => $processCategory->getKey(),
-            'manifest' => json_encode([
-
-            ]),
-            'svg' => 'svg',
-            'is_system' => 0,
-        ]);
+        ProcessTemplates::updateOrCreate(
+            ['name' => 'Employee Onboarding 2023'],
+            [
+                'name' => 'Employee Onboarding 2023',
+                'description' => 'New version of the employee onboarding process from veggieDonuts',
+                'process_id' => null,
+                'user_id' => $admin->getKey(),
+                'process_category_id' => $processCategory->getKey(),
+                'manifest' => $manifest,
+                'svg' => $svg,
+                'is_system' => 0,
+            ]);
     }
 }
