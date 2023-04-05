92e04e03f94819c58019e855e84b00da477ac506
Esteban Gallego <egallego00@gmail.com>
2023-04-05 11:35:40 -0700

    Create Default Templates Category

--- database/seeders/ProcessTemplatesSeeder.php
+++ database/seeders/ProcessTemplatesSeeder.php
@@ -21,10 +21,17 @@ class ProcessTemplatesSeeder extends Seeder
     {
         //load user admin
         $admin = User::where('username', 'admin')->firstOrFail();
-        $processCategory = ProcessCategory::where('name', 'Uncategorized')->firstOrFail();
         $manifest = file_get_contents(app_path('Templates/Fixtures/process-employee-onboarding-template.json'));
         $svg = file_get_contents(app_path('Templates/Fixtures/process-employee-onboarding-template.svg'));
 
+        ProcessCategory::updateOrCreate(
+            ['name'  => 'Default Templates'],
+            [
+                'name' => 'Default Templates',
+                'status' => 'ACTIVE',
+                'is_system' => 0,
+            ]
+        );
         // create process templates seed data
         ProcessTemplates::updateOrCreate(
             ['name' => 'Employee Onboarding 2023'],
@@ -33,7 +40,7 @@ class ProcessTemplatesSeeder extends Seeder
                 'description' => 'New version of the employee onboarding process from veggieDonuts',
                 'process_id' => null,
                 'user_id' => $admin->getKey(),
-                'process_category_id' => $processCategory->getKey(),
+                'process_category_id' => ProcessCategory::where('name', 'Default Templates')->firstOrFail()->getKey(),
                 'manifest' => $manifest,
                 'svg' => $svg,
                 'is_system' => 0,
