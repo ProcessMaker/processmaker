dcb62cb673c4f186b91955de664408f6173781d3
Esteban Gallego <egallego00@gmail.com>
2023-04-06 16:05:21 -0700

    Update process_category with the request category

--- ProcessMaker/Templates/ProcessTemplate.php
+++ ProcessMaker/Templates/ProcessTemplate.php
@@ -116,15 +116,16 @@ class ProcessTemplate implements TemplateInterface
             $postOptions[$key] = [
                 'mode' => $asset['mode'],
             ];
+
             if ($payload['root'] === $key) {
                 // Set name and description for the new process
                 $payload['export'][$key]['attributes']['name'] = $request['name'];
                 $payload['export'][$key]['attributes']['description'] = $request['description'];
-                $payload['export'][$key]['attributes']['process_category_id'] = $request['process_category_id'];
+                $payload['export'][$key]['attributes']['process_category_id'] = intval($request['process_category_id']);
 
                 $payload['export'][$key]['name'] = $request['name'];
                 $payload['export'][$key]['description'] = $request['description'];
-                $payload['export'][$key]['process_category_id'] = $request['process_category_id'];
+                $payload['export'][$key]['process_category_id'] = intval($request['process_category_id']);
                 $payload['export'][$key]['process_manager_id'] = $request['manager_id'];
             }
         }
@@ -134,6 +135,8 @@ class ProcessTemplate implements TemplateInterface
         $manifest = $importer->doImport();
         $rootLog = $manifest[$payload['root']]->log;
         $processId = $rootLog['newId'];
+
+        Process::where('id', $processId)->update(['process_category_id' => intval($request['process_category_id'])]);
 
         return response()->json(['processId' => $processId]);
     }
