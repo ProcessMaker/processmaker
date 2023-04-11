d1ad0ec3b22b9cdc9f8d088adec11e94c1dc72ff
Agustin Busso <agustin.busso@processmaker.com>
2023-04-11 17:02:16 -0300

    Add soft delete to model

--- ProcessMaker/Models/AiSearch.php
+++ ProcessMaker/Models/AiSearch.php
@@ -3,9 +3,11 @@
 namespace ProcessMaker\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use ProcessMaker\Models\ProcessMakerModel;
 
 class AiSearch extends ProcessMakerModel
 {
     use HasFactory;
+    use SoftDeletes;
 }
