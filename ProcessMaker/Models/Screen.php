20222c9127dc6379bf078f6aef08ebde07aacf4d
Agustin Busso <agustin.busso@processmaker.com>
2023-04-17 17:17:46 -0300

    Add aliases for PMQL support

--- ProcessMaker/Models/Screen.php
+++ ProcessMaker/Models/Screen.php
@@ -3,6 +3,7 @@
 namespace ProcessMaker\Models;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 use ProcessMaker\Assets\ScreensInScreen;
 use ProcessMaker\Contracts\ScreenInterface;
@@ -212,6 +213,56 @@ class Screen extends ProcessMakerModel implements ScreenInterface
     }
 
     /**
+     * @return string
+     */
+    public function fieldAliasName()
+    {
+        return 'screens.title';
+    }
+
+    /**
+     * @return string
+     */
+    public function fieldAliasStatus()
+    {
+        return 'screens.status';
+    }
+
+    /**
+     * PMQL value alias for category field
+     *
+     * @param string $value
+     *
+     * @return callable
+     */
+    public function valueAliasCategory($value, $expression)
+    {
+        return function ($query) use ($value, $expression) {
+            $categoryAssignment = DB::table('category_assignments')->leftJoin('screen_categories', function ($join) {
+                $join->on('screen_categories.id', '=', 'category_assignments.category_id');
+                $join->where('category_assignments.category_type', '=', ScreenCategory::class);
+                $join->where('category_assignments.assignable_type', '=', self::class);
+            })
+            ->where('name', $expression->operator, $value);
+            $query->whereIn('screens.id', $categoryAssignment->pluck('assignable_id'));
+        };
+    }
+
+    /**
+     * PMQL value alias for fulltext field
+     *
+     * @param string $value
+     *
+     * @return callable
+     */
+    public function valueAliasFullText($value, $expression)
+    {
+        return function ($query) use ($value) {
+            $this->scopeFilter($query, $value);
+        };
+    }
+
+    /**
      * Get a recursive list of nested screens IDs in this screen
      *
      * @return int[] nested screen IDs
@@ -227,4 +278,35 @@ class Screen extends ProcessMakerModel implements ScreenInterface
 
         return $screenIds;
     }
+
+    /**
+      * Filter screen with a string
+      *
+      * @param $query
+      *
+      * @param $filter string
+      */
+     public function scopeFilter($query, $filterStr)
+     {
+         $filter = '%' . mb_strtolower($filterStr) . '%';
+         $query->where(function ($query) use ($filter, $filterStr) {
+             $query->where('processes.name', 'like', $filter)
+                  ->orWhere('processes.description', 'like', $filter)
+                  ->orWhere('processes.status', '=', $filterStr)
+                  ->orWhere('user.firstname', 'like', $filter)
+                  ->orWhere('user.lastname', 'like', $filter)
+                  ->orWhereIn('processes.id', function ($qry) use ($filter) {
+                      $qry->select('assignable_id')
+                          ->from('category_assignments')
+                          ->leftJoin('process_categories', function ($join) {
+                              $join->on('process_categories.id', '=', 'category_assignments.category_id');
+                              $join->where('category_assignments.category_type', '=', ProcessCategory::class);
+                              $join->where('category_assignments.assignable_type', '=', self::class);
+                          })
+                          ->where('process_categories.name', 'like', $filter);
+                  });
+         });
+
+         return $query;
+     }
 }
