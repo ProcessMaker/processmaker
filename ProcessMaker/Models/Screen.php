913f1dd087affda04e04297f89de0d57fc1a7661
Agustin Busso <agustin.busso@processmaker.com>
2023-04-18 11:04:51 -0300

    Add filter and filterForSelectList scopes

--- ProcessMaker/Models/Screen.php
+++ ProcessMaker/Models/Screen.php
@@ -279,34 +279,48 @@ class Screen extends ProcessMakerModel implements ScreenInterface
         return $screenIds;
     }
 
+    /**
+     * Filter screen with a string
+     *
+     * @param $query
+     *
+     * @param $filter string
+     */
+    public function scopeFilter($query, $filterStr)
+    {
+        $filter = '%' . $filterStr . '%';
+        $query->where(function ($query) use ($filter) {
+            $query->where('title', 'like', $filter)
+                ->orWhere('description', 'like', $filter)
+                ->orWhereIn('screens.id', function ($qry) use ($filter) {
+                    $qry->select('assignable_id')
+                        ->from('category_assignments')
+                        ->leftJoin('screen_categories', function ($join) {
+                            $join->on('screen_categories.id', '=', 'category_assignments.category_id');
+                            $join->where('category_assignments.category_type', '=', ScreenCategory::class);
+                            $join->where('category_assignments.assignable_type', '=', Screen::class);
+                        })
+                        ->where('screen_categories.name', 'like', $filter);
+                });
+        });
+
+        return $query;
+    }
+
      /**
-      * Filter screen with a string
+      * Filter screen with a string for select lists
       *
       * @param $query
       *
       * @param $filter string
       */
-     public function scopeFilter($query, $filterStr)
+     public function scopeFilterForSelectList($query, $filterStr)
      {
-         $filter = '%' . mb_strtolower($filterStr) . '%';
-         $query->where(function ($query) use ($filter, $filterStr) {
-             $query->where('processes.name', 'like', $filter)
-                  ->orWhere('processes.description', 'like', $filter)
-                  ->orWhere('processes.status', '=', $filterStr)
-                  ->orWhere('user.firstname', 'like', $filter)
-                  ->orWhere('user.lastname', 'like', $filter)
-                  ->orWhereIn('processes.id', function ($qry) use ($filter) {
-                      $qry->select('assignable_id')
-                          ->from('category_assignments')
-                          ->leftJoin('process_categories', function ($join) {
-                              $join->on('process_categories.id', '=', 'category_assignments.category_id');
-                              $join->where('category_assignments.category_type', '=', ProcessCategory::class);
-                              $join->where('category_assignments.assignable_type', '=', self::class);
-                          })
-                          ->where('process_categories.name', 'like', $filter);
-                  });
-         });
+        $filter = '%' . $filterStr . '%';
+        $query->where(function ($query) use ($filter) {
+            $query->where('title', 'like', $filter);
+        });
 
-         return $query;
+        return $query;
      }
 }
