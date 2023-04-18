84bb7bd9c38c986b834afc2c91d830838aa4d7fb
Nolan Ehrstrom <github@nolanpro.com>
2023-04-18 14:28:26 -0700

    Add ability to add json column indexes in real time

---
+++ ProcessMaker/JsonColumnIndex.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ProcessMaker;
+
+use DB;
+
+class JsonColumnIndex
+{
+    public function add(string $table, string $column, string $path)
+    {
+        $indexName = $column . '_' . $path;
+
+        if ($this->indexExists($table, $indexName)) {
+            return;
+        }
+
+        $sql = <<<SQL
+            ALTER TABLE `{$table}` ADD INDEX `{$indexName}` ((
+                CAST({$column}->>"{$path}" as CHAR(255)) COLLATE utf8mb4_bin
+            )) USING BTREE;
+        SQL;
+
+        return DB::statement($sql);
+    }
+
+    public function indexExists(string $table, string $name)
+    {
+        return $this->listIndexes($table)->contains(function ($index) use ($name) {
+            return $index->Key_name === $name;
+        });
+    }
+
+    public function listIndexes(string $table)
+    {
+        return collect(DB::select("SHOW INDEXES FROM {$table}"));
+    }
+}
---
+++ tests/unit/ProcessMaker/JsonColumnIndexTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests;
+
+use DB;
+use Facades\ProcessMaker\JsonColumnIndex;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class JsonColumnIndexTest extends TestCase
+{
+    private $table = 'test_json_column_indexes';
+
+    protected function connectionsToTransact()
+    {
+        return [];
+    }
+
+    public function test()
+    {
+        Schema::dropIfExists($this->table);
+
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->id();
+            $table->json('data');
+        });
+
+        DB::table($this->table)->insert([
+            'data' => '{"foo":"bar","num":99}',
+        ]);
+        DB::table($this->table)->insert([
+            'data' => '{"foo":"baz", "num":42}',
+        ]);
+
+        JsonColumnIndex::add($this->table, 'data', '$.foo');
+        JsonColumnIndex::add($this->table, 'data', '$.num');
+
+        $indexes = DB::select("SHOW INDEXES FROM {$this->table}");
+
+        $this->assertEquals('data_$.foo', $indexes[1]->Key_name);
+        $this->assertEquals('data_$.num', $indexes[2]->Key_name);
+
+        // Attempt to add it again
+        JsonColumnIndex::add($this->table, 'data', '$.foo');
+        $indexes = DB::select("SHOW INDEXES FROM {$this->table}");
+        $this->assertEquals(3, count($indexes));
+
+        // Make sure we're using the index
+        $result = DB::select("EXPLAIN SELECT * FROM {$this->table} WHERE data->>\"$.foo\" = 'baz';");
+        $this->assertEquals('data_$.foo', $result[0]->possible_keys);
+
+        $result = DB::select("EXPLAIN SELECT * FROM {$this->table} WHERE data->>\"$.num\" = 42;");
+        $this->assertEquals('data_$.num', $result[0]->possible_keys);
+
+        Schema::dropIfExists($this->table);
+    }
+}
