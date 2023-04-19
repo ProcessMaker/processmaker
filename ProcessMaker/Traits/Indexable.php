e108c1f5b7941eebdb6eb54efaf858418b1aa3b9
Agustin Busso <agustin.busso@processmaker.com>
2023-04-19 12:08:31 -0300

    Get correct table

--- ProcessMaker/Traits/Indexable.php
+++ ProcessMaker/Traits/Indexable.php
@@ -15,14 +15,37 @@ trait Indexable
     {
         foreach ($fields as $field) {
             $exploded = explode('.', $field);
+            $table = $this->table();
 
             // Only columns in format data.field
             if (count($exploded) > 1) {
                 $column = $exploded[0];
                 array_shift($exploded);
                 $path = '$.' . implode('.', $exploded);
-                JsonColumnIndex::add($this->table, $column, $path);
+                \Log::info('Updating JSON path: ' . $path . ' indexes for table: ' . $table);
+                JsonColumnIndex::add($table, $column, $path);
             }
+        }
+    }
+
+    public function table()
+    {
+        if ($this->table !== 'saved_searches') {
+            return $this->table;
+        }
+
+        // If table is saved search, return the corresponding table
+        switch ($this->type) {
+            case 'request':
+                return 'process_requests';
+                break;
+            case 'task':
+                return 'process_request_tokens';
+                break;
+            case 'collection':
+                $collectionId = $this->meta->collection_id;
+                return 'collection_' . $collectionId;
+                break;
         }
     }
 }
