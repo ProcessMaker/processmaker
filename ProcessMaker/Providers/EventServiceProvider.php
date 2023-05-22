5ac44d73061393b9ec5c963564b962fc011317ef
luNunezProcessmaker <luciana.nunez@processmaker.com>
2023-05-22 12:11:33 -0400

    feature/FOUR-8121

--- ProcessMaker/Providers/EventServiceProvider.php
+++ ProcessMaker/Providers/EventServiceProvider.php
@@ -30,15 +30,6 @@ class EventServiceProvider extends ServiceProvider
         'Illuminate\Database\Events\MigrationsEnded' => [
             'ProcessMaker\Listeners\UpdateDataLakeViews',
         ],
-        'ProcessMaker\Events\AuthClientUpdated' => [
-            'ProcessMaker\Listeners\SecurityLogger',
-        ],
-        'ProcessMaker\Events\AuthClientCreated' => [
-            'ProcessMaker\Listeners\SecurityLogger',
-        ],
-        'ProcessMaker\Events\AuthClientDeleted' => [
-            'ProcessMaker\Listeners\SecurityLogger',
-        ]
     ];
 
     /**
