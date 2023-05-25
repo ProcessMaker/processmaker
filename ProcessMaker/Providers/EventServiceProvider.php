2fd6dfa83f909314b0631bed93ddd8d09b3e8930
luNunezProcessmaker <luciana.nunez@processmaker.com>
2023-05-25 14:03:12 -0400

    changes corrected

--- ProcessMaker/Providers/EventServiceProvider.php
+++ ProcessMaker/Providers/EventServiceProvider.php
@@ -60,6 +60,12 @@ class EventServiceProvider extends ServiceProvider
         'ProcessMaker\Events\SettingsUpdated' => [
             'ProcessMaker\Listeners\SecurityLogger',
         ],
+        'ProcessMaker\Events\TokenCreated' => [
+            'ProcessMaker\Listeners\SecurityLogger',
+        ],
+        'ProcessMaker\Events\TokenDeleted' => [
+            'ProcessMaker\Listeners\SecurityLogger',
+        ],
         'ProcessMaker\Events\UnauthorizedAccessAttempt' => [
             'ProcessMaker\Listeners\SecurityLogger',
         ],
@@ -68,13 +74,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         'ProcessMaker\Events\UserDeleted' => [
             'ProcessMaker\Listeners\SecurityLogger',
-        ],
-        'ProcessMaker\Events\TokenCreated' => [
-            'ProcessMaker\Listeners\SecurityLogger',
-        ],
-        'ProcessMaker\Events\TokenDeleted' => [
-            'ProcessMaker\Listeners\SecurityLogger',
-        ],
+        ]
     ];
 
     /**
