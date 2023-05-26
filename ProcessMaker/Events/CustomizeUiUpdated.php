a5175ab8b7800c547b4a15764669b4e4fd82876c
luNunezProcessmaker <luciana.nunez@processmaker.com>
2023-05-26 17:03:25 -0400

    changes for conflicts

--- ProcessMaker/Events/CustomizeUiUpdated.php
+++ ProcessMaker/Events/CustomizeUiUpdated.php
@@ -32,6 +32,21 @@ class CustomizeUiUpdated implements SecurityLogEventInterface
      * Building the data
      */
     public function buildData() {
+        if (isset($this->changes['variables'])){
+            $variables_changes = [];
+            $variables_original = [];
+            foreach ((array)json_decode($this->changes['variables'], true) as $variable) {
+                $variables_changes[$variable['title']] = $variable['value'];
+            }
+            foreach ((array)json_decode($this->original['variables'], true) as $variable) {
+                $variables_original[$variable['title']] = $variable['value'];
+            }
+            $variables_changes = array_diff($variables_changes, $variables_original);
+            $variables_original = array_intersect_key($variables_original, $variables_changes);
+            
+            $this->changes['variables'] = $variables_changes;
+            $this->original['variables'] = $variables_original;
+        }
         $this->data = $this->formatChanges($this->changes, $this->original);
     }
     
