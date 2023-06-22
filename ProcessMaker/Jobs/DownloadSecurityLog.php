8282051598abc9124282707a99fab288c9e75954
Paula Quispe <paula.quispe@processmaker.com>
2023-06-22 11:13:54 -0400

    FOUR-8720: Add S3 Download

--- ProcessMaker/Jobs/DownloadSecurityLog.php
+++ ProcessMaker/Jobs/DownloadSecurityLog.php
@@ -2,20 +2,28 @@
 
 namespace ProcessMaker\Jobs;
 
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use ProcessMaker\Events\SecurityLogDownloadJobCompleted;
+use ProcessMaker\Models\Media;
+use ProcessMaker\Models\SecurityLog;
 use ProcessMaker\Models\User;
+use Ramsey\Uuid\Uuid;
 
 class DownloadSecurityLog implements ShouldQueue
 {
-    use Dispatchable,
-        InteractsWithQueue,
-        Queueable,
-        SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     private User $user;
 
@@ -23,9 +31,14 @@ class DownloadSecurityLog implements ShouldQueue
 
     private ?int $userId;
 
+    public const CSV_SEPARATOR = '|';
+
+    public const FORMAT_CSV = 'csv';
+    public const FORMAT_XML = 'xml';
+
     /**
      * @param User $user
-     * @param string $format
+     * @param string $format xml|csv
      * @param int|null $userId
      */
     public function __construct(User $user, string $format, int $userId = null)
@@ -39,18 +52,161 @@ class DownloadSecurityLog implements ShouldQueue
      * Execute the job.
      *
      * @return void
+     * @throw Exception
      */
     public function handle()
     {
-        //1. Get all data from security_logs table
-        //2. Create a file in specified format: csv or xml
-        //3. Zip this file
-        //4. Store in S3, with private visibility, with 24h of lifecycle
-        //5. Send email to user with the link or error message
-        if (mt_rand(1, 10) <= 8) {
-            event(new SecurityLogDownloadJobCompleted($this->user, true, __('Click on the link and download the file. This link will be available until midnight tonight.'), 'http://processmaker.com/?download=true'));
-        } else {
-            event(new SecurityLogDownloadJobCompleted($this->user, false, __('Sorry, it was not possible to generate the log file. Please contact the administrator.')));
+        if (!Media::s3IsReady()) {
+            event(new SecurityLogDownloadJobCompleted($this->user, false, __('Sorry, this feature requires the configured AWS S3 service.. Please contact the administrator.')));
+            return;
         }
+        try {
+            $collection = $this->getCollection();
+            $contents = $this->generateContent($collection);
+            $expires = $this->getExpires();
+            $filename = $this->createTemporaryFilename();
+            $url = $this->createTemporaryUrl($filename, $contents, $expires);
+            $message = __('Click on the link and download the file. This link will be available until midnight tonight.');
+            event(new SecurityLogDownloadJobCompleted($this->user, true, $message, $url));
+        } catch (Exception $e) {
+            $message = __('Sorry, it was not possible to generate the log file. Please contact the administrator.');
+            event(new SecurityLogDownloadJobCompleted($this->user, false, $message));
+        }
+    }
+
+    /**
+     * Generate the content with the corresponding format
+     */
+    protected function generateContent(array $collection)
+    {
+        return $this->format === static::FORMAT_CSV ? $this->toCSV($collection) : $this->toXML($collection);
+    }
+
+    /**
+     * To CSV
+     */
+    protected function toCSV(array $collection)
+    {
+        $first = current($collection);
+        $headers = array_keys($first ?: []);
+        $content = '';
+        $line = [];
+        foreach ($headers as $key) {
+            $line[] = $key;
+        }
+        $content .= implode(static::CSV_SEPARATOR, $line);
+        foreach ($collection as $item) {
+            $line = [];
+            foreach ($headers as $key) {
+                if (is_object($item[$key])) {
+                    $line[] = json_encode($item[$key]);
+                } else {
+                    $line[] = isset($item[$key]) ? $item[$key] : null;
+                }
+            }
+            $content .= PHP_EOL . implode(static::CSV_SEPARATOR, $line);
+        }
+        return $content;
+    }
+
+    /**
+     * To XML
+     */
+    protected function toXML(array $collection)
+    {
+        $content = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+        $content .= '<securityLogs>';
+        $tab = "\t";
+        foreach ($collection as $item) {
+            $content .= PHP_EOL . $tab . '<securityLog>';
+            foreach ($item as $key => $value) {
+                if (is_object($value)) {
+                    $value = json_encode($value);
+                }
+                $content .= sprintf(
+                    '%s<%s>%s</%s>',
+                    PHP_EOL . $tab,
+                    $key,
+                    $value,
+                    $key
+                );
+            }
+            $content .= PHP_EOL . $tab . '</securityLog>';
+        }
+        $content .= PHP_EOL . '</securityLogs>';
+
+        return $content;
+    }
+
+    /**
+     * Get collection
+     *
+     * @return array
+     */
+    protected function getCollection()
+    {
+        $query = SecurityLog::query();
+        if ($this->userId) {
+            $query->where('user_id', $this->userId);
+        }
+        $results = [];
+        $query->chunk(1000, function (Collection $collection) use (&$results) {
+            foreach ($collection as $item) {
+                $results[] = $item->toArray();
+            }
+            unset($item);
+        });
+
+        return $results;
+    }
+
+    /**
+     * Create temp url
+     *
+     * @param string $filename
+     * @param string $content
+     * @param Carbon $expires
+     *
+     * @return string
+     */
+    protected function createTemporaryUrl(string $filename, string $content, Carbon $expires)
+    {
+        $disk = Storage::disk('s3');
+        $disk->put($filename, $content, [
+            'ACL' => 'private', // private|public-read,
+            'Expires' => $expires->toString()
+        ]);
+        $url = $disk->temporaryUrl(
+            $filename,
+            $expires,
+            [
+                'ResponseContentType' => 'application/octet-stream',
+                'ResponseContentDisposition' => 'attachment; filename=' . $filename,
+            ]
+        );
+
+        return $url;
+    }
+
+    /**
+     * Get expires time
+     *
+     * @return Carbon time
+     */
+    protected function getExpires()
+    {
+        return now()->addHours(24);
+    }
+
+    /**
+     * Create a temp file
+     *
+     * @return string
+     */
+    protected function createTemporaryFilename()
+    {
+        $uuid = Uuid::uuid4()->toString() . Str::random(8);
+
+        return 'security-logs/' . $uuid . '.'  . $this->format;
     }
 }
---
+++ tests/Feature/Jobs/DownloadSecurityLogTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature\Jobs;
+
+use Illuminate\Support\Facades\Storage;
+use ProcessMaker\Events\SecurityLogDownloadJobCompleted;
+use ProcessMaker\Jobs\DownloadSecurityLog;
+use ProcessMaker\Models\SecurityLog;
+use ReflectionMethod;
+use Tests\Feature\Shared\RequestHelper;
+use Tests\TestCase;
+
+class DownloadSecurityLogTest extends TestCase
+{
+    use RequestHelper;
+
+    protected $simpleCollection;
+
+    protected function withUserSetup(): void
+    {
+        SecurityLog::query()->delete();
+
+        $this->simpleCollection = [
+            ['id' => 1, 'event' => 'login'],
+            ['id' => 2, 'event' => 'logout'],
+        ];
+
+        SecurityLog::factory()->create(['event' => 'login', 'user_id' => $this->user->id]);
+        SecurityLog::factory()->create(['event' => 'logout', 'user_id' => $this->user->id]);
+        SecurityLog::factory()->create(['event' => 'attempt']);
+        SecurityLog::factory()->create(['event' => 'attempt']);
+    }
+
+    public function testCreateTemporaryFilename()
+    {
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_CSV);
+        $method = new ReflectionMethod($job, 'createTemporaryFilename');
+        $filename = $method->invoke($job);
+        $this->assertStringContainsString('.csv', $filename);
+
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_XML);
+        $method = new ReflectionMethod($job, 'createTemporaryFilename');
+        $filename = $method->invoke($job);
+        $this->assertStringContainsString('.xml', $filename);
+    }
+
+    public function testGetCollection()
+    {
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_CSV);
+        $results = (new ReflectionMethod($job, 'getCollection'))->invoke($job);
+        $this->assertCount(4, $results);
+
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_CSV, $this->user->id);
+        $results = (new ReflectionMethod($job, 'getCollection'))->invoke($job);
+        $this->assertCount(2, $results);
+    }
+
+    public function testToCSV()
+    {
+        $collection = $this->simpleCollection;
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_CSV);
+        $csv = (new ReflectionMethod($job, 'toCSV'))->invoke($job, $collection);
+        $this->assertEquals(
+            'id|event' . PHP_EOL .
+            '1|login' . PHP_EOL .
+            '2|logout',
+            $csv
+        );
+    }
+
+    public function testToXML()
+    {
+        $collection = $this->simpleCollection;
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_CSV);
+        $xml = (new ReflectionMethod($job, 'toXML'))->invoke($job, $collection);
+        $this->assertStringContainsString('<securityLog>', $xml);
+        $this->assertStringContainsString('<id>2</id>', $xml);
+    }
+
+    public function testCreateTemporaryUrl()
+    {
+        //The test should not be run as it requires an AWS key
+        $this->assertTrue(true, 'ignoring');
+        return;
+        $collection = $this->simpleCollection;
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_CSV);
+        $filename = (new ReflectionMethod($job, 'createTemporaryFilename'))->invoke($job);
+        $expires = (new ReflectionMethod($job, 'getExpires'))->invoke($job);
+        $csv = (new ReflectionMethod($job, 'toCSV'))->invoke($job, $collection);
+        $url = (new ReflectionMethod($job, 'createTemporaryUrl'))->invoke($job, $filename, $csv, $expires);
+        $this->assertStringContainsString('s3.amazonaws.com/security-logs', $url);
+        $disk = Storage::disk('s3');
+        $this->assertTrue($disk->exists($filename));
+    }
+
+    public function testHandleWithSuccess()
+    {
+        //The test should not be run as it requires an AWS key
+        $this->assertTrue(true, 'ignoring');
+        return;
+        $this->expectsEvents(SecurityLogDownloadJobCompleted::class);
+        $job = new DownloadSecurityLog($this->user, DownloadSecurityLog::FORMAT_CSV);
+        (new ReflectionMethod($job, 'handle'))->invoke($job);
+    }
+}
