0b83f746bb560df3b15e5cdc282167ecb5bac011
David Callizaya <davidcallizaya@gmail.com>
2023-07-05 09:38:28 -0400

    Improve upgrade script

--- upgrades/2023_06_29_110455_populate_process_requests_collaboration_uuid.php
+++ upgrades/2023_06_29_110455_populate_process_requests_collaboration_uuid.php
@@ -7,53 +7,31 @@ use ProcessMaker\Upgrades\UpgradeMigration as Upgrade;
 class PopulateProcessRequestsCollaborationUuid extends Upgrade
 {
     /**
-     * Run any validations/pre-run checks to ensure the environment, settings,
-     * packages installed, etc. are right correct to run this upgrade.
-     *
-     * Throw a \RuntimeException if the conditions are *NOT* correct for this
-     * upgrade migration to run. If this is not a required upgrade, then it
-     * will be skipped. Otherwise the exception thrown will be caught, noted,
-     * and will prevent the remaining migrations from continuing to run.
-     *
-     * Returning void or null denotes the checks were successful.
-     *
-     * @return void
-     *
-     * @throws \RuntimeException
-     */
-    public function preflightChecks()
-    {
-        //
-    }
-
-    /**
      * Run the upgrade migration.
      *
      * @return void
      */
     public function up()
     {
-        foreach (ProcessCollaboration::cursor() as $collaboration) {
-            $collaboration->uuid = ProcessCollaboration::generateUuid();
-            $collaboration->save();
-        }
-        foreach (ProcessRequest::cursor() as $processRequest) {
-            if ($processRequest->process_collaboration_id) {
-                $processRequest->collaboration_uuid = $processRequest->collaboration->uuid;
-            } else {
-                $processRequest->collaboration_uuid = ProcessCollaboration::generateUuid();
-            }
-            $processRequest->save();
-        }
-    }
+        $batchSize = 5;
+        ProcessCollaboration::whereNull('uuid')
+            ->chunkById($batchSize, function ($collaborations) {
+                foreach ($collaborations as $collaboration) {
+                    $collaboration->uuid = ProcessCollaboration::generateUuid();
+                    $collaboration->save();
+                }
+            });
 
-    /**
-     * Reverse the upgrade migration.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
+        ProcessRequest::select(['id', 'process_collaboration_id', 'collaboration_uuid'])->whereNull('collaboration_uuid')
+            ->chunkById($batchSize, function ($requests) {
+                foreach ($requests as $request) {
+                    if ($request->process_collaboration_id) {
+                        $request->collaboration_uuid = $request->collaboration->uuid;
+                    } else {
+                        $request->collaboration_uuid = ProcessCollaboration::generateUuid();
+                    }
+                    $request->save();
+                }
+            });
     }
 }
