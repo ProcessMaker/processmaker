54a60a667c3a7a991735c098a7946a0c85f3ef89
pmoskrojas <69815070+pmoskrojas@users.noreply.github.com>
2023-07-06 10:36:57 -0400

    FOUR-9157: First time selecting Script, fill Error Handling fields.

--- resources/js/processes/modeler/components/inspector/ErrorHandlingRetryAttempts.vue
+++ resources/js/processes/modeler/components/inspector/ErrorHandlingRetryAttempts.vue
@@ -45,6 +45,9 @@ export default {
         }
       } else {
         this.config.id = this.valueContent.id;
+        if (!configString) {
+          this.config.retry_attempts = this.valueContent.retry_attempts;
+        }
       }
     },
     setNodeConfig() {
