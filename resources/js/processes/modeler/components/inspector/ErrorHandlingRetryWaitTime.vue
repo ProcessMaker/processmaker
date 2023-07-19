9ec8cde89611e7e93a56f93b71242850f735e413
pmoskrojas <69815070+pmoskrojas@users.noreply.github.com>
2023-07-19 11:36:55 -0400

    Error Handling Refactor with Mixin

--- resources/js/processes/modeler/components/inspector/ErrorHandlingRetryWaitTime.vue
+++ resources/js/processes/modeler/components/inspector/ErrorHandlingRetryWaitTime.vue
@@ -1,93 +1,23 @@
 <template>
   <div role="group">
     <label for="retry_wait_time">{{ $t('Retry Wait Time') }}</label>
-    <b-form-input id="retry_wait_time" type="number" v-model="config.retry_wait_time" @input="updateConfig" min="0" max="3600"></b-form-input>
+    <b-form-input id="retry_wait_time" type="number" v-model="value" @input="userHasUpdatedValue" min="0" max="3600"></b-form-input>
     <small class="form-text text-muted">{{ helper }}</small>
   </div>
 </template>
 
 <script>
+import ErrorHandlingMixin from './ErrorHandlingMixin';
 
 export default {
-  props: ['type'],
+  mixins: [ErrorHandlingMixin],
   data() {
     return {
-      config: {
-        retry_wait_time: "",
-      },
-      valueContent: "",
+      configKey: 'retry_wait_time',
+      scriptHelperText: 'Set maximum run retry wait time in seconds. Leave empty to use script default. Set to 0 for no retry wait time.',
+      dataConnectorHelperText: 'Set maximum run retry wait time in seconds. Leave empty to use data connector default. Set to 0 for no retry wait time.'
     }
   },
-  watch: {
-    config: {
-      deep: true,
-      handler() {
-        this.setNodeConfig();
-      }
-    },
-  },
-  methods: {
-    node() {
-      return this.$root.$children[0].$refs.modeler.highlightedNode.definition;
-    },
-    getNodeConfig(newValue) {
-      this.valueContent = newValue;
-      const configString = _.get(this.node(), 'errorHandling', null);
-      if (this.config.id) {
-        if (this.config.id !== this.valueContent.id) {
-          this.config.retry_wait_time = this.valueContent.retry_wait_time;
-          this.config.id = this.valueContent.id;
-        } else {
-          if (configString) {
-            const config = JSON.parse(configString);
-            this.config.retry_wait_time = _.get(config, 'retry_wait_time');
-          }
-        }
-      } else {
-        this.config.id = this.valueContent.id;
-        if (!configString) {
-          this.config.retry_wait_time = this.valueContent.retry_wait_time;
-        }
-        if (this.valueContent.method) {
-            this.config.retry_wait_time = this.valueContent.retry_wait_time;
-            if (this.config.id !== this.valueContent.id) {
-              this.config.retry_wait_time = this.valueContent.retry_wait_time;
-              this.config.id = this.valueContent.id;
-            } else {
-              if (configString) {
-                const config = JSON.parse(configString);
-                this.config.retry_wait_time = _.get(config, 'retry_wait_time');
-              }
-            }
-        }
-      }
-    },
-    setNodeConfig() {
-      const existingSetting = JSON.parse(_.get(this.node(), 'errorHandling', '{}'));
-      const json = JSON.stringify({ ...existingSetting, retry_wait_time: this.config.retry_wait_time });
-      Vue.set(this.node(), 'errorHandling', json);
-    },
-    updateConfig() {
-      if (this.valueContent.retry_wait_time !== this.config.retry_wait_time) {
-        this.setNodeConfig();
-      }
-    },
-  },
-  mounted() {
-    this.$root.$on("contentChanged", this.getNodeConfig);
-  },
-  beforeDestroy() {
-    this.$root.$off("contentChanged", this.getNodeConfig);
-  },
-  computed: {
-    helper() {
-      if (this.type === 'script') {
-        return this.$t('Set maximum run retry wait time in seconds. Leave empty to use script default. Set to 0 for no retry wait time.');
-      } else if (this.type === 'data-connector') {
-        return this.$t('Set maximum run retry wait time in seconds. Leave empty to use data connector default. Set to 0 for no retry wait time.');
-      }
-    }
-  }
 }
 </script>
 
--- resources/js/processes/modeler/components/inspector/ErrorHandlingTimeout.vue
+++ resources/js/processes/modeler/components/inspector/ErrorHandlingTimeout.vue
@@ -7,68 +7,17 @@
 </template>
 
 <script>
+import ErrorHandlingMixin from './ErrorHandlingMixin';
 
 export default {
-  props: ['type'],
+  mixins: [ErrorHandlingMixin],
   data() {
     return {
-      value: "",
-      valueFromModel: "",
-      userHasUpdated: false,
+      configKey: 'timeout',
+      scriptHelperText: 'Set maximum run time in seconds. Leave empty to use script default. Set to 0 for no timeout.',
+      dataConnectorHelperText: 'Set maximum run time in seconds. Leave empty to use data connector default. Set to 0 for no timeout.'
     }
   },
-  watch: {
-    value: {
-      handler() {
-        this.emitValue();
-      }
-    },
-  },
-  methods: {
-    node() {
-      return this.$root.$children[0].$refs.modeler.highlightedNode.definition;
-    },
-    valueFromNode()
-    {
-      const configString = _.get(this.node(), 'errorHandling', null);
-      const config = JSON.parse(configString);
-      return _.get(config, 'timeout', null);
-    },
-    getNodeConfig(valueFromModel) {
-      if (this.valueFromNode()) {
-        return;
-      }
-
-      this.valueFromModel = this.value = valueFromModel.timeout;
-    },
-    emitValue() {
-      if (!this.userHasUpdated) {
-        return;
-      }
-      const existingSetting = JSON.parse(_.get(this.node(), 'errorHandling', '{}'));
-      const json = JSON.stringify({ ...existingSetting, timeout: this.value });
-      Vue.set(this.node(), 'errorHandling', json);
-    },
-    userHasUpdatedValue() {
-      this.userHasUpdated = true;
-    },
-  },
-  mounted() {
-    this.value = this.valueFromNode();
-    this.$root.$on("contentChanged", this.getNodeConfig);
-  },
-  beforeDestroy() {
-    this.$root.$off("contentChanged", this.getNodeConfig);
-  },
-  computed: {
-    helper() {
-      if (this.type === 'script') {
-        return this.$t('Set maximum run time in seconds. Leave empty to use script default. Set to 0 for no timeout.');
-      } else if (this.type === 'data-connector') {
-        return this.$t('Set maximum run time in seconds. Leave empty to use data connector default. Set to 0 for no timeout.');
-      }
-    }
-  }
 }
 </script>
 
