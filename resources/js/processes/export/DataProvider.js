9ddfded014d3a2f717a380ee59eee727a22ce084
Nolan Ehrstrom <github@nolanpro.com>
2023-08-02 11:37:08 -0700

    Update export filename format

--- resources/js/processes/export/DataProvider.js
+++ resources/js/processes/export/DataProvider.js
@@ -83,7 +83,7 @@ export default {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", exportInfo.name.replace(' ', '_') + ".json");
+      link.setAttribute("download", exportInfo.name.replace(/ /g, "_").toLowerCase() + ".json");
       document.body.appendChild(link);
       link.click();
       return exportInfo;
