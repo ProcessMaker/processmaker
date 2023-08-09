05bb8113802d9252ae15e499fae9f19bc617ba9c
Nolan Ehrstrom <github@nolanpro.com>
2023-08-09 16:12:33 -0700

    Backport hotfix changes from STM

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -32,13 +32,15 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'key' => env('PUSHER_APP_KEY', 'app-key'),
+            'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
+            'app_id' => env('PUSHER_APP_ID', 'app-id'),
             'options' => [
-                'cluster' => env('PUSHER_CLUSTER'),
-                'debug' => env('PUSHER_DEBUG', false),
-                'useTLS' => env('PUSHER_TLS', true),
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
+                'encrypted' => true,
+                'useTLS' => env('PUSHER_SCHEME') === 'https',
             ],
         ],
 
