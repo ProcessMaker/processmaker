a1893e28986543131c2b63effb2bf4c29e0c12f9
Gustavo Bascope <gustavo.bascope@processmaker.com>
2023-08-11 11:47:25 -0400

    Tree view toggle on the task data tab with a reusable view

---
+++ resources/js/components/TreeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <iframe id="jsoncrackEmbed" src="https://jsoncrack.com/widget?json=639b65c5a82efc29a24b2de2" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      jsonData: this.value,
+    };
+  },
+  watch: {
+    value: {
+      handler(newVal) {
+        this.jsonData = newVal;
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    handleInput() {
+      this.$emit("input", this.jsonData);
+    },
+  },
+};
+</script>
+
+<style>
+#jsoncrackEmbed {
+  flex: 1;
+  order: 2;
+  width: 100%;
+  height: 400px;
+  border: none;
+}
+</style>
---
+++ resources/js/components/common/data-tree-toggle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex-container">
+    <i class="fas fa-code"></i>
+    <div class="mb-2 custom-control custom-switch switch-padding">
+      <input id="show_tree" type="checkbox" v-model="showTree"  class="custom-control-input" :checked="value" @input="emitSwitchValue">
+      <label for="show_tree" class="custom-control-label"></label>
+    </div>
+    <i class="fas fa-project-diagram"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Boolean,
+  },
+  data() {
+    return {
+      showTree: this.value,
+    };
+  },
+  mounted() {
+    console.log(this.showTree);
+  },
+  methods: {
+    emitSwitchValue(event) {
+      this.$emit("input", event.target.checked);
+    },
+  },
+};
+</script>
+
+<style>
+.flex-container {
+  display: flex;
+  align-items: center;
+}
+.switch-padding {
+    margin-bottom: 0rem !important;
+    padding-left: 2.75rem;
+}
+</style>
