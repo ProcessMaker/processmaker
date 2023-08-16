9e025ca54aa9bf5a0538abd33a0778540269aebb
Jose Chirivella <jose.chirivella@processmaker.com>
2023-08-16 18:49:17 -0500

    FOUR-9950 Added new component to create assets quickly from Modeler

---
+++ resources/js/processes/modeler/components/inspector/ModelerAssetQuickCreate.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    id="asset-quick-create"
+    class="mt-2"
+  >
+    <a
+      class="asset-link"
+      @click="goToAsset"
+    >Create a new {{ label }} <i class="fa fa-plus" /></a>
+  </div>
+</template>
+<script>
+
+const AssetTypes = Object.freeze({
+  SCREEN: "screen",
+  SCRIPT: "script",
+  DECISION_TABLE: "decision table",
+});
+export default {
+  name: "ModelerAssetQuickCreate",
+  props: {
+    label: {
+      type: String,
+      default: AssetTypes.SCREEN,
+      validator(value) {
+        return Object.values(AssetTypes)
+          .includes(value);
+      },
+    },
+  },
+  methods: {
+    goToAsset() {
+      return window.open(`/designer/${this.label}?create`, "_blank");
+    },
+  },
+};
+</script>
+<style lang="scss">
+#asset-quick-create {
+  .asset-link {
+    &:hover {
+      color: darken(#0872C2, 5%);
+    }
+  }
+}
+</style>
