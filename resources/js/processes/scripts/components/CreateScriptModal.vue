467c7b9cc69349b87680dace650f4a9d999ae706
Jose Chirivella <jose.chirivella@processmaker.com>
2023-08-17 13:21:23 -0500

    FOUR-9949 Ability to open automatically the modal using queries in the URL * Added the Broadcast channel to send data once the form has been submitted * Re-indent both files

--- resources/js/processes/scripts/components/CreateScriptModal.vue
+++ resources/js/processes/scripts/components/CreateScriptModal.vue
@@ -1,114 +1,169 @@
 <template>
   <div>
-    <b-button :aria-label="$t('Create Script')" v-b-modal.createScript class="mb-3 mb-md-0 ml-md-2">
-      <i class="fas fa-plus"></i> {{ $t('Script') }}
+    <b-button
+      ref="createScriptModalButton"
+      v-b-modal.createScript
+      :aria-label="$t('Create Script')"
+      class="mb-3 mb-md-0 ml-md-2"
+    >
+      <i class="fas fa-plus" /> {{ $t("Script") }}
     </b-button>
-    <modal id="createScript" :title="$t('Create Script')" :ok-disabled="disabled" @ok.prevent="onSubmit" @hidden="onClose">
+    <modal
+      id="createScript"
+      :ok-disabled="disabled"
+      :title="$t('Create Script')"
+      @hidden="onClose"
+      @ok.prevent="onSubmit"
+    >
       <template v-if="countCategories">
-        <required></required>
+        <required />
         <b-form-group
-          required
-          :label="$t('Name')"
-          :description="formDescription('The script name must be unique.', 'title', addError)"
+          :description="
+            formDescription(
+              'The script name must be unique.',
+              'title',
+              addError
+            )
+          "
           :invalid-feedback="errorMessage('title', addError)"
+          :label="$t('Name')"
           :state="errorState('title', addError)"
+          required
         >
           <b-form-input
-            required
-            autofocus
             v-model="title"
-            autocomplete="off"
             :state="errorState('title', addError)"
+            autocomplete="off"
+            autofocus
             name="title"
-          ></b-form-input>
+            required
+          />
         </b-form-group>
         <b-form-group
-          required
-          :label="$t('Description')"
           :invalid-feedback="errorMessage('description', addError)"
+          :label="$t('Description')"
           :state="errorState('description', addError)"
+          required
         >
           <b-form-textarea
-            required
             v-model="description"
-            autocomplete="off"
-            rows="2"
             :state="errorState('description', addError)"
+            autocomplete="off"
             name="description"
-          ></b-form-textarea>
+            required
+            rows="2"
+          />
         </b-form-group>
-        <category-select :label="$t('Category')" api-get="script_categories"
-          api-list="script_categories" v-model="script_category_id"
+        <category-select
+          v-model="script_category_id"
           :errors="addError.script_category_id"
+          :label="$t('Category')"
+          api-get="script_categories"
+          api-list="script_categories"
           name="script_category_id"
-        ></category-select>
+        />
         <b-form-group
-          required
-          :label="$t('Language')"
           :invalid-feedback="errorMessage('script_executor_id', addError)"
+          :label="$t('Language')"
           :state="errorState('script_executor_id', addError)"
+          required
         >
           <b-form-select
-            required
             v-model="script_executor_id"
             :options="scriptExecutors"
             :state="errorState('script_executor_id', addError)"
             name="script_executor_id"
-          ></b-form-select>
+            required
+          />
         </b-form-group>
         <b-form-group
-          required
-          :label="$t('Run script as')"
-          :description="formDescription('Select a user to set the API access of the Script', 'run_as_user_id', addError)"
+          :description="
+            formDescription(
+              'Select a user to set the API access of the Script',
+              'run_as_user_id',
+              addError
+            )
+          "
           :invalid-feedback="errorMessage('run_as_user_id', addError)"
+          :label="$t('Run script as')"
           :state="errorState('run_as_user_id', addError)"
+          required
         >
           <select-user
             v-model="selectedUser"
+            :class="{
+              'is-invalid': errorState('run_as_user_id', addError) == false,
+            }"
             :multiple="false"
-            :class="{'is-invalid': errorState('run_as_user_id', addError) == false}"
             name="run_as_user_id"
-          ></select-user>
+          />
         </b-form-group>
         <slider-with-input
+          :description="
+            $t(
+              'How many seconds the script should be allowed to run (0 is unlimited).'
+            )
+          "
+          :error="
+            errorState('timeout', addError)
+              ? null
+              : errorMessage('timeout', addError)
+          "
           :label="$t('Timeout')"
-          :description="$t('How many seconds the script should be allowed to run (0 is unlimited).')"
+          :max="300"
+          :min="0"
           :value="timeout"
           @input="timeout = $event"
-          :error="errorState('timeout', addError) ? null : errorMessage('timeout', addError)"
-          :min="0"
-          :max="300"
-        ></slider-with-input>
+        />
         <slider-with-input
+          :description="
+            $t(
+              'Number of times to retry. Leave empty to use script default. Set to 0 for no retry attempts.'
+            )
+          "
+          :error="
+            errorState('retry_attempts', addError)
+              ? null
+              : errorMessage('retry_attempts', addError)
+          "
           :label="$t('Retry Attempts')"
-          :description="$t('Number of times to retry. Leave empty to use script default. Set to 0 for no retry attempts.')"
+          :max="10"
+          :min="0"
           :value="retry_attempts"
           @input="retry_attempts = $event"
-          :error="errorState('retry_attempts', addError) ? null : errorMessage('retry_attempts', addError)"
-          :min="0"
-          :max="10"
-        ></slider-with-input>
+        />
         <slider-with-input
+          :description="
+            $t(
+              'Seconds to wait before retrying. Leave empty to use script default. Set to 0 for no retry wait time.'
+            )
+          "
+          :error="
+            errorState('retry_wait_time', addError)
+              ? null
+              : errorMessage('retry_wait_time', addError)
+          "
           :label="$t('Retry Wait Time')"
-          :description="$t('Seconds to wait before retrying. Leave empty to use script default. Set to 0 for no retry wait time.')"
+          :max="3600"
+          :min="0"
           :value="retry_wait_time"
           @input="retry_wait_time = $event"
-          :error="errorState('retry_wait_time', addError) ? null : errorMessage('retry_wait_time', addError)"
-          :min="0"
-          :max="3600"
-        ></slider-with-input>
+        />
         <component
-          v-for="(cmp,index) in createScriptHooks"
-          :key="`create-script-hook-${index}`"
           :is="cmp"
-          :script="script"
+          v-for="(cmp, index) in createScriptHooks"
+          :key="`create-script-hook-${index}`"
           ref="createScriptHooks"
-        ></component>
+          :script="script"
+        />
       </template>
       <template v-else>
-        <div>{{ $t('Categories are required to create a script') }}</div>
-        <a href="/designer/scripts/categories" class="btn btn-primary container mt-2">
-            {{ $t('Add Category') }}
+        <div>{{ $t("Categories are required to create a script") }}</div>
+        <a
+          class="btn btn-primary container mt-2"
+          href="/designer/scripts/categories"
+        >
+          {{ $t("Add Category") }}
         </a>
       </template>
     </modal>
@@ -116,58 +171,68 @@
 </template>
 
 <script>
-  import { FormErrorsMixin, Modal, Required } from "SharedComponents";
-  import SliderWithInput from "../../../components/shared/SliderWithInput";
+import { FormErrorsMixin, Modal, Required } from "SharedComponents";
+import SliderWithInput from "../../../components/shared/SliderWithInput";
 
-  export default {
-    components: { Modal, Required, SliderWithInput },
-    mixins: [ FormErrorsMixin ],
-    props: ["countCategories", "scriptExecutors"],
-    data: function() {
-      return {
-        title: '',
-        language: '',
-        script_executor_id: null,
-        description: '',
-        script_category_id: '',
-        code: '',
-        addError: {},
-        selectedUser: '',
-        users: [],
-        timeout: 60,
-        retry_attempts: 0,
-        retry_wait_time: 5,
-        disabled: false,
-        createScriptHooks: [],
-        script: null,
-      }
+const channel = new BroadcastChannel("assetCreation");
+
+export default {
+  components: {
+    Modal,
+    Required,
+    SliderWithInput,
+  },
+  mixins: [FormErrorsMixin],
+  props: ["countCategories", "scriptExecutors"],
+  data() {
+    return {
+      title: "",
+      language: "",
+      script_executor_id: null,
+      description: "",
+      script_category_id: "",
+      code: "",
+      addError: {},
+      selectedUser: "",
+      users: [],
+      timeout: 60,
+      retry_attempts: 0,
+      retry_wait_time: 5,
+      disabled: false,
+      createScriptHooks: [],
+      script: null,
+    };
+  },
+  destroyed() {
+    channel.close();
+  },
+  methods: {
+    onClose() {
+      this.title = "";
+      this.language = "";
+      this.script_executor_id = null;
+      this.description = "";
+      this.script_category_id = "";
+      this.code = "";
+      this.timeout = 60;
+      this.retry_attempts = 0;
+      this.retry_wait_time = 5;
+      this.addError = {};
     },
-    methods: {
-      onClose() {
-        this.title = '';
-        this.language = '';
-        this.script_executor_id = null;
-        this.description = '';
-        this.script_category_id = '';
-        this.code = '';
-        this.timeout = 60;
-        this.retry_attempts = 0;
-        this.retry_wait_time = 5;
-        this.addError = {};
-      },
-      onSubmit() {
-        this.errors = Object.assign({}, {
-          name: null,
-          description: null,
-          status: null,
-          script_category_id: null
-        });
-        //single click
-        if (this.disabled) {
-          return
-        }
-        this.disabled = true;
-        ProcessMaker.apiClient.post("/scripts", {
+    onSubmit() {
+      this.errors = {
+        name: null,
+        description: null,
+        status: null,
+        script_category_id: null,
+      };
+      // single click
+      if (this.disabled) {
+        return;
+      }
+      this.disabled = true;
+      ProcessMaker.apiClient
+        .post("/scripts", {
           title: this.title,
           script_executor_id: this.script_executor_id,
           description: this.description,
@@ -176,24 +241,28 @@
           code: "[]",
           timeout: this.timeout,
           retry_attempts: this.retry_attempts,
-          retry_wait_time: this.retry_wait_time
+          retry_wait_time: this.retry_wait_time,
         })
-          .then(response => {
-            ProcessMaker.alert(this.$t('The script was created.'), 'success');
-            (this.$refs.createScriptHooks || []).forEach(hook => {
-              hook.onsave(response.data);
-            });
-            window.location = "/designer/scripts/" + response.data.id + "/builder";
-          })
-          .catch(error => {
-            this.disabled = false;
-            if (_.get(error, 'response.status') === 422) {
-              this.addError = error.response.data.errors;
-            } else {
-                throw error;
-            }
-          })
-      }
-    }
-  };
+        .then(({ data }) => {
+          ProcessMaker.alert(this.$t("The script was created."), "success");
+          (this.$refs.createScriptHooks || []).forEach((hook) => {
+            hook.onsave(data);
+          });
+          channel.postMessage({
+            assetType: "script",
+            id: data.id,
+          });
+          window.location = `/designer/scripts/${data.id}/builder`;
+        })
+        .catch((error) => {
+          this.disabled = false;
+          if (_.get(error, "response.status") === 422) {
+            this.addError = error.response.data.errors;
+          } else {
+            throw error;
+          }
+        });
+    },
+  },
+};
 </script>
