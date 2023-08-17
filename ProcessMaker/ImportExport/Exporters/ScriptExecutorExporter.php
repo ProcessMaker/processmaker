650800713bb6f59f6fcb6d904814dec9c8da43c5
Marco A. Nina Mena <marco.antonio.nina@processmaker.com>
2023-08-17 14:55:34 -0400

    enable export/import ScriptExecutors

--- ProcessMaker/ImportExport/Exporters/ScriptExecutorExporter.php
+++ ProcessMaker/ImportExport/Exporters/ScriptExecutorExporter.php
@@ -2,11 +2,14 @@
 
 namespace ProcessMaker\ImportExport\Exporters;
 
+use ProcessMaker\Events\ScriptExecutorUpdated;
+use ProcessMaker\Jobs\BuildScriptExecutor;
+
 class ScriptExecutorExporter extends ExporterBase
 {
     public static $fallbackMatchColumn = 'title';
 
-    public $discard = true;
+    public $discard = false;
 
     public function export() : void
     {
@@ -14,6 +17,23 @@ class ScriptExecutorExporter extends ExporterBase
 
     public function import() : bool
     {
+        switch ($this->mode) {
+            case 'copy':
+                BuildScriptExecutor::dispatch($this->model->id, auth()->user()->id);
+                break;
+            case 'update':
+                if (!empty($this->model->getChanges())) {
+                    $original = $this->model->getAttributes();
+                    ScriptExecutorUpdated::dispatch($this->model->id, $original, $this->model->getChanges());
+                    BuildScriptExecutor::dispatch($this->model->id, auth()->user()->id);
+                }
+                break;
+
+            default:
+                // code...
+                break;
+        }
+
         return true;
     }
 }
