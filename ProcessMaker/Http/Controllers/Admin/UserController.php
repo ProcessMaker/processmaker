6e058decf5ee6b041ff1c0fb3ed35321f5062623
Nolan Ehrstrom <github@nolanpro.com>
2023-08-17 16:35:07 -0700

    Update lang path

--- ProcessMaker/Http/Controllers/Admin/UserController.php
+++ ProcessMaker/Http/Controllers/Admin/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
         $permissionGroups = $all_permissions->sortBy('title')->groupBy('group')->sortKeys();
 
         $langs = [];
-        foreach (scandir(resource_path('lang')) as $file) {
+        foreach (scandir(app()->langPath()) as $file) {
             preg_match('/([a-z]{2})\\.json/', $file, $matches);
             if (!empty($matches)) {
                 $langs[] = $matches[1];
