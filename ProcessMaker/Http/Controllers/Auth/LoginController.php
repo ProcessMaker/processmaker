1b4167e8d94399d4e115adde6bc9a0e94e08e695
Roly Gutierrez <roly.gutierrez@processmaker.com>
2023-08-22 17:45:42 -0400

    FOUR-8663 Unable to login with imported LDAP users

--- ProcessMaker/Http/Controllers/Auth/LoginController.php
+++ ProcessMaker/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-     */
+    */
 
     use AuthenticatesUsers;
 
