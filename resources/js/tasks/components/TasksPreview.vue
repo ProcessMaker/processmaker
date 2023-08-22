03cb9c4baad4f249b6f5b62289082403b5cbceb2
luNunezProcessmaker <luciana.nunez@processmaker.com>
2023-08-22 11:05:52 -0400

    feature/FOUR-9987

--- resources/js/tasks/components/TasksPreview.vue
+++ resources/js/tasks/components/TasksPreview.vue
@@ -1,81 +1,82 @@
 <template>
   <div>
     <splitpanes
-        id="splitpane"
-        class="default-theme" 
-        v-if="showPreview">
+      id="splitpane"
+      class="default-theme" 
+      v-if="showPreview"
+    >
       <pane style="opacity: 0;">
         <div></div>
       </pane>
       <pane min-size="40" max-size="99">
         <div
-        id="tasks-preview"
-        ref="tasks-preview"
-        class="h-100"
+          id="tasks-preview"
+          ref="tasks-preview"
+          class="h-100"
         >
           <template>
-              <div class="ml-2 p-3">
-                <div class="d-flex w-100 h-100 mb-3">
-                  <div class="my-1">
-                    <a class="lead text-secondary font-weight-bold">
-                      {{ task.element_name }}
-                    </a>
-                  </div>
-                  <div class="ml-auto mr-0 text-right">
-                    <b-button
-                      class="btn-light text-secondary"
-                      :aria-label="$t('Previous Tasks')"
-                      :disabled="!existPrev"
-                      @click="goPrevNext('Prev')"
-                    >
-                      <i class="fas fa-chevron-left" />
-                      {{ $t("Prev") }}
-                    </b-button>
-                    <b-button
-                      class="btn-light text-secondary"
-                      :aria-label="$t('Next Tasks')"
-                      :disabled="!existNext"
-                      @click="goPrevNext('Next')"
-                    >
-                      {{ $t("Next") }}
-                      <i class="fas fa-chevron-right" />
-                    </b-button>
-                    <a class="text-secondary">|</a>
-                    <b-button
-                      class="btn-light text-secondary"
-                      :aria-label="$t('Open Task')"
-                      :href="openTask()"
-                    >
-                      <i class="fas fa-external-link-alt" />
-                    </b-button>
-                    <a class="text-secondary">|</a>
-                    <b-button
-                      class="btn-light text-secondary"
-                      :aria-label="$t('Close')"
-                      @click="onClose()"
-                    >
-                      <i class="fas fa-times" />
-                    </b-button>
-                  </div>
+            <div class="ml-2 p-3">
+              <div class="d-flex w-100 h-100 mb-3">
+                <div class="my-1">
+                  <a class="lead text-secondary font-weight-bold">
+                    {{ task.element_name }}
+                  </a>
                 </div>
-                <div>
-                  <div
-                    v-show="loading"
-                    class="text-center"
+                <div class="ml-auto mr-0 text-right">
+                  <b-button
+                    class="btn-light text-secondary"
+                    :aria-label="$t('Previous Tasks')"
+                    :disabled="!existPrev"
+                    @click="goPrevNext('Prev')"
                   >
-                    <b-spinner />
-                  </div>
-                  <b-embed
-                    v-show="!loading"
-                    id="tasksFrame"
-                    type="iframe"
-                    :class="loading ? 'loadingFrame' : ''"
-                    :src="linkTasks"
-                    @load="frameLoaded"
-                  />
+                    <i class="fas fa-chevron-left" />
+                    {{ $t("Prev") }}
+                  </b-button>
+                  <b-button
+                    class="btn-light text-secondary"
+                    :aria-label="$t('Next Tasks')"
+                    :disabled="!existNext"
+                    @click="goPrevNext('Next')"
+                  >
+                    {{ $t("Next") }}
+                    <i class="fas fa-chevron-right" />
+                  </b-button>
+                  <a class="text-secondary">|</a>
+                  <b-button
+                    class="btn-light text-secondary"
+                    :aria-label="$t('Open Task')"
+                    :href="openTask()"
+                  >
+                    <i class="fas fa-external-link-alt" />
+                  </b-button>
+                  <a class="text-secondary">|</a>
+                  <b-button
+                    class="btn-light text-secondary"
+                    :aria-label="$t('Close')"
+                    @click="onClose()"
+                  >
+                    <i class="fas fa-times" />
+                  </b-button>
                 </div>
               </div>
-            </template>
+              <div>
+                <div
+                  v-show="loading"
+                  class="text-center"
+                >
+                  <b-spinner />
+                </div>
+                <b-embed
+                  v-show="!loading"
+                  id="tasksFrame"
+                  type="iframe"
+                  :class="loading ? 'loadingFrame' : ''"
+                  :src="linkTasks"
+                  @load="frameLoaded"
+                />
+              </div>
+            </div>
+          </template>
         </div>
       </pane>
     </splitpanes>
