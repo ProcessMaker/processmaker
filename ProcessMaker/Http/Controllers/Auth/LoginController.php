1609b5203237237afb6ceeba558974d9639b3dc4
Roly Gutierrez <roly.gutierrez@processmaker.com>
2023-08-22 17:42:47 -0400

    FOUR-8663 Unable to login with imported LDAP users

--- ProcessMaker/Http/Controllers/Auth/LoginController.php
+++ ProcessMaker/Http/Controllers/Auth/LoginController.php
@@ -3,10 +3,12 @@
 namespace ProcessMaker\Http\Controllers\Auth;
 
 use App;
+use Exception;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
+use Log;
 use ProcessMaker\Events\Logout;
 use ProcessMaker\Http\Controllers\Controller;
 use ProcessMaker\Managers\LoginManager;
@@ -17,15 +19,15 @@ class LoginController extends Controller
 {
     use HasControllerAddons;
     /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+      |--------------------------------------------------------------------------
+      | Login Controller
+      |--------------------------------------------------------------------------
+      |
+      | This controller handles authenticating users for the application and
+      | redirecting them to your home screen. The controller uses a trait
+      | to conveniently provide its functionality to your applications.
+      |
+     */
 
     use AuthenticatesUsers;
 
@@ -96,7 +98,45 @@ class LoginController extends Controller
             }
         }
 
+        $redirect = $this->validateSocialiteLoginWithLDAP($user, $request);
+        if ($redirect !== false) {
+            return $redirect;
+        }
         return $this->login($request);
+    }
+
+    /**
+     * Validate Socialite Login with LDAP, if user validation is successful, it returns a redirection 
+     * to the SSOController::callback() controller; otherwise, it returns false.
+     * 
+     * @param User $user
+     * @param Request $request
+     * @return mixed
+     */
+    public function validateSocialiteLoginWithLDAP(User $user, Request $request)
+    {
+        if (!class_exists(\Laravel\Socialite\Facades\Socialite::class)) {
+            return false;
+        }
+        if (!isset($user->meta)) {
+            return false;
+        }
+        if (!isset($user->meta->authenticationType)) {
+            return false;
+        }
+        if ($user->meta->authenticationType === 'ldap') {
+            try {
+                $provider = \Laravel\Socialite\Facades\Socialite::driver('ldap');
+                if ($provider->auth($user, $request->input('password'))) {
+                    return redirect()->route('sso.callback', ['driver' => 'ldap']);
+                } else {
+                    Log::info('SocialiteProviders: ' . $provider->getErrorMessage());
+                }
+            } catch (Exception $e) {
+                Log::info('SocialiteProviders: ' . $e->getMessage());
+            }
+        }
+        return false;
     }
 
     /**
