db2d4c21fbf26a2b02ef2d9e445ca18e616f3746
Eleazar Resendez <eleazar.resendez@processmaker.com>
2023-08-23 12:06:09 -0600

    Remove dependency and add watermark

--- resources/js/components/shared/DownloadSvgButton.vue
+++ resources/js/components/shared/DownloadSvgButton.vue
@@ -18,9 +18,6 @@
   </a>
 </template>
 <script>
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Canvg } from "canvg";
-
 export default {
   props: {
     svg: {
@@ -35,6 +32,10 @@ export default {
       type: String,
       default: "Download",
     },
+    watermarkText: {
+      type: String,
+      default: "Powered by ProcessMaker",
+    },
   },
   data() {
     return {
@@ -42,26 +43,61 @@ export default {
     };
   },
   methods: {
-    async svgToCanvas(svgString) {
+    async svgToCanvas(svgString, watermarkText) {
       // Create canvas.
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d");
 
       // Set canvas dimensions from SVG attributes.
       const svgElement = new DOMParser().parseFromString(svgString, "image/svg+xml").documentElement;
-      canvas.width = svgElement.getAttribute("width");
-      canvas.height = svgElement.getAttribute("height");
+      const originalWidth = parseFloat(svgElement.getAttribute("width"));
+      const originalHeight = parseFloat(svgElement.getAttribute("height"));
 
-      // Render SVG onto canvas.
-      const v = await Canvg.fromString(ctx, svgString);
-      v.render();
+      // Set new dimensions with extra space for the watermark.
+      const watermarkHeight = 30;
+      const padding = 5;
+
+      canvas.width = originalWidth;
+      canvas.height = originalHeight + watermarkHeight + padding;
+
+      // Convert SVG string to data URL.
+      const imgSrc = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svgString)}`;
+      const img = new Image();
+      img.src = imgSrc;
+      await new Promise((resolve) => {
+        img.onload = () => resolve();
+      });
+
+      // Draw SVG on canvas using specific dimensions.
+      ctx.drawImage(img, 0, 0, originalWidth, originalHeight);
+
+      // Load the watermark image.
+      const watermarkImage = new Image();
+      watermarkImage.src = "/img/icon-pmlogo.png";
+      await new Promise((resolve) => {
+        watermarkImage.onload = () => resolve();
+      });
+
+      // Draw watermark.
+      ctx.font = "20px Arial";
+      const textWidth = ctx.measureText(watermarkText).width;
+
+      // Calculate the total width of the image + text
+      const totalWidth = watermarkImage.width + textWidth + padding;
+
+      // Draw the watermark image to the left of the text.
+      // eslint-disable-next-line max-len
+      ctx.drawImage(watermarkImage, canvas.width - totalWidth, canvas.height - watermarkHeight, watermarkImage.width, watermarkHeight);
+
+      // Adjusted the y-coordinate so the watermark sits just below the SVG content.
+      ctx.fillText(watermarkText, canvas.width - textWidth - padding, canvas.height - padding);
 
       return canvas;
     },
 
     async download() {
       this.isLoading = true;
-      const canvas = await this.svgToCanvas(this.svg);
+      const canvas = await this.svgToCanvas(this.svg, this.watermarkText);
       const dataURL = canvas.toDataURL("image/png");
 
       const downloadLink = document.createElement("a");
