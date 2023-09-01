6181ac5eb00e90a58823a9f7ab3cd8fcf6916596
Ryan Cooley <ryancooley@users.noreply.github.com>
2023-08-31 19:32:50 -0700

    Revert "Bugfix/FOUR-10277: Prompt not used on the image downloaded"

--- resources/js/components/shared/DownloadSvgButton.vue
+++ resources/js/components/shared/DownloadSvgButton.vue
@@ -22,12 +22,11 @@
     <!-- Used for the design -->
     <!-- Page -->
     <div ref="modelPrintSection" style="
-        position: fixed;
+        position: absolute;
         z-index: 0;
         color: #000;
         min-width: 90em;
-        left: 0px;
-        top: -9999999px;
+        left: -9999999px;
         padding: 1rem;
         background: #ffffff;
         text-transform: initial;
@@ -56,7 +55,7 @@
             background: #ffffff;
         ">
           <div class="w-50 p-3" style="background: #E5F1FF; border-radius: 8px 0 0 8px"><h5><b>{{ title }}</b></h5></div>
-          <div class="w-50 p-3 text-muted" style="border-radius: 0 8px 8px 0"><small>{{ prompt }}</small></div>
+          <div class="w-50 p-3 text-muted" style="border-radius: 0 8px 8px 0"><small>{{ description }}</small></div>
         </div>
       </div>
     </div>
@@ -78,10 +77,6 @@ export default {
       default: "",
     },
     description: {
-      type: String,
-      default: "",
-    },
-    prompt: {
       type: String,
       default: "",
     },
@@ -162,8 +157,3 @@ export default {
   },
 };
 </script>
-<style>
-#app-container .main {
-  overflow: hidden !important;
-}
-</style>
