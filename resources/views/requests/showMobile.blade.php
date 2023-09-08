1f9db578cb1c92f1e3049097f7515e32a955e989
Fabio Rodolfo Guachalla Blanco <frguachallab@gmail.com>
2023-09-08 16:07:47 -0400

    Update showMobile.blade.php

--- resources/views/requests/showMobile.blade.php
+++ resources/views/requests/showMobile.blade.php
@@ -7,7 +7,7 @@
 <div class="flex-fill">
   <div class="row">
     <div class="mt-1">
-      <img class="img-under" src="https://h2o-digital.com/wp-content/uploads/2015/09/websites-why-you-should-never-use-under-construction-pages.jpg" />
+      <img class="img-under" src="https://h2o-digital.com/wp-content/uploads/2015/09/websites-why-you-should-never-use-under-construction-pages.jpg" alt="to do" />
     </div>
   </div>
 </div>
