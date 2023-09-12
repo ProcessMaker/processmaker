0aa1a6e52550d7ea83c0fee3de49902478e024e9
David Callizaya <davidcallizaya@gmail.com>
2023-09-12 09:40:13 -0400

    JS Addons for controllers

--- ProcessMaker/Traits/HasControllerAddons.php
+++ ProcessMaker/Traits/HasControllerAddons.php
@@ -30,7 +30,10 @@ trait HasControllerAddons
                 $addon['content'] = isset($addon['view']) && !isset($addon['content'])
                     ? view($addon['view'], $data)->render() : (isset($addon['content'])
                     ? $addon['content'] : '');
-                $addon['script'] = isset($addon['script']) ? view($addon['script'], $data)->render() : '';
+                if (isset($addon['script']) && is_array($addon['script'])) {
+                    $addon['script_mixin'] = $addon['script'];
+                }
+                $addon['script'] = isset($addon['script']) && is_string($addon['script']) ? view($addon['script'], $data)->render() : '';
                 $addons[] = $addon;
             }
         }
