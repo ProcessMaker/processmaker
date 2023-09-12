06f3486147258da71c655fdeff3722cbd60a53ea
Paula Quispe <paula.quispe@processmaker.com>
2023-09-12 09:20:34 -0400

    FOUR-10398

--- ProcessMaker/Helpers/MobileHelper.php
+++ ProcessMaker/Helpers/MobileHelper.php
@@ -6,10 +6,11 @@ class MobileHelper
 {
     public static function isMobile($userAgent)
     {
-      $device = '/(Mobile|Android|Tablet|GoBrowser|[0-9]x[0-9]*|uZardWeb\/|Mini|Doris\/|Skyfire\/|iPhone|Fennec\/|Maemo|Iris\/|CLDC\-|Mobi\/)/uis';
-      if(preg_match($device,$userAgent) && $_COOKIE["isMobile"] === "true"){
+        $device = '/(Mobile|Android|Tablet|GoBrowser|[0-9]x[0-9]*|uZardWeb\/|Mini|Doris\/|Skyfire\/|iPhone|Fennec\/|Maemo|Iris\/|CLDC\-|Mobi\/)/uis';
+        if (isset($_COOKIE["isMobile"]) && preg_match($device, $userAgent) && $_COOKIE["isMobile"] === "true") {
           return true;
-      };
-      return false;
+        }
+
+        return false;
     }
 }
