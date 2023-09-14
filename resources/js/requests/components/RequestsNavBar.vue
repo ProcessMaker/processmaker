b68d09cb01752744bf6baecda4cef1c4a0902424
CarliPinell <carlos.pinell@processmaker.com>
2023-09-14 12:21:17 +0000

    Saving changes

--- resources/js/requests/components/RequestsNavBar.vue
+++ resources/js/requests/components/RequestsNavBar.vue
@@ -1,81 +1,168 @@
 <template>
-  <!-- <ul class="nav nav-pills">
+<div class="row">
+    <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
       <div>
-      <div class="form-group">
-        <label for="dropdown">Select Option:</label>
-        <select id="dropdown" v-model="selectedOption" @change="fetchData">
-          <option value="In Progress">In Progress</option>
-          <option value="Completed">Completed</option>
-        </select>
+        <b-nav pills>
+          <div class="dropdown" left>
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-circle text-warning"></i>
+              {{ selectedOption }}
+              <i class="fas fa-caret-down"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <!-- <a class="dropdown-item" href="#"><i class="fas fa-circle text-warning"></i> In Progress</a>
+              <a class="dropdown-item" href="#"><i class="fas fa-circle text-primary"></i> Completed</a> -->
+              <a class="dropdown-item" href="#" @click="selectOption('In Progress', 'status')"><i class="fas fa-circle text-warning"></i> In Progress</a>
+              <a class="dropdown-item" href="#" @click="selectOption('Completed', 'status')"><i class="fas fa-circle text-primary"></i> Completed</a>
+            </div>
+          </div>
+
+
+          <div class="dropdown" v-if="type === 'tab_request'">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-user"></i>
+              <i class="fas fa-caret-down"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#" @click="selectOption('Requested by Me', 'filter')"><i class="fas fa-user"></i> Requested by Me</a>
+              <a class="dropdown-item" href="#" @click="selectOption('With me as Participant', 'filter')"><i class="fas fa-users"></i> With me as Participant</a>
+            </div>
+          </div>
+
+          <div class="dropdown" v-else>
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-list"></i>
+              <i class="fas fa-caret-down"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#" @click="selectOption('By Due Date', 'orderBy')"> By Due Date</a>
+              <a class="dropdown-item" href="#" @click="selectOption('By Creation Date', 'orderBy')"> By Creation Date</a>
+            </div>
+          </div>
+
+          <!-- <button class="btn btn-primary">
+            <i class="fas fa-search"></i>
+          </button> -->
+          <div>
+    <div class="input-group">
+      <input type="text" class="form-control narrow-input" v-model="searchCriteria" placeholder="(fulltext LIKE '%someText%')">
+      <div class="input-group-append">
+        <button class="btn btn-primary" @click="performSearch">
+          <i class="fas fa-search"></i>
+        </button>
       </div>
-      <ul>
-        <li v-for="item in items" :key="item.id">Request:{{ item.name }}</li>
-      </ul>
-    </div>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Participant</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">My Requests</a>
-      <a class="dropdown-item" href="#">Participant</a>
-    </div>
-  </li>
-  <div class="input-group">
-    <input type="text" class="form-control" placeholder="Search">
-    <div class="input-group-append">
-      <button class="btn btn-secondary" type="button">
-        <i class="fa fa-search"></i>
-      </button>
     </div>
   </div>
-</ul> -->
-  <div>
-    <b-navbar
-      type="light"
-      variant="light"
-    >
-      <b-nav-form>
-        <b-form-input
-          class="mr-sm-2"
-          placeholder="Search"
-        ></b-form-input>
-        <b-button
-          variant="outline-success"
-          class="my-2 my-sm-0"
-          type="submit"
-          >Search</b-button
-        >
-      </b-nav-form>
 
-      <b-nav-item-dropdown
-        text="Lang"
-        right
-      >
-        <b-dropdown-item href="#">EN</b-dropdown-item>
-        <b-dropdown-item href="#">ES</b-dropdown-item>
-        <b-dropdown-item href="#">RU</b-dropdown-item>
-        <b-dropdown-item href="#">FA</b-dropdown-item>
-      </b-nav-item-dropdown>
-
-      <b-nav-item-dropdown
-        text="User"
-        right
-      >
-        <b-dropdown-item href="#">Account</b-dropdown-item>
-        <b-dropdown-item href="#">Settings</b-dropdown-item>
-      </b-nav-item-dropdown>
-    </b-navbar>
-  </div>
+    
+        </b-nav>
+      </div>
+    </div>  
+    
+  </div>  
 </template>
 
 <script>
 export default {
+  props: {
+    type: {
+      type: String,
+      default: 'tab_request',
+    },
+  },
   data() {
     return {
+      searchCriteria: '',
       selectedOption: "In Progress",
+      apiData: null,
       items: [],
+      orderBy: "id",
+      orderDirection: "DESC",
+      additionalParams: "",
+      sortOrder: [
+        {
+          field: "id",
+          sortField: "id",
+          direction: "desc",
+        },
+      ],
+      fields: [],
+      previousFilter: "",
+      previousPmql: "",
     };
   },
   methods: {
+    selectOption(option, controlName) {
+      this.selectedOption = option;
+      if(controlName === 'status'){
+        this.callStatus(option);
+      } else if( controlName === 'filter'){
+        this.callFilter(option);
+      } else {
+        this.callOrderBy(option);
+      }
+      
+    },
+    callStatus(param) {
+      let urlBase = "requests/";
+      if (this.type === 'tab_tasks') {
+        urlBase = "tasks/";
+      }
+      console.log('status');
+      console.log('URL BASE: ', urlBase);
+      console.log('DROPDOWN: ', param);
+      // ProcessMaker.apiClient.get(urlBase, param)
+      //   .then(response => {
+      //     this.apiData = response.data;
+      //   })
+      //   .catch(error => {
+      //     console.error('Error al hacer la solicitud a la API:', error);
+      //   });
+    },
+    callFilter(param) {
+      let urlBase = "requests/";
+      if (this.type === 'tab_tasks') {
+        urlBase = "tasks/";
+      }
+      console.log('filter');
+      console.log('URL BASE: ', urlBase);
+      console.log('DROPDOWN: ', param);
+      // ProcessMaker.apiClient.get(urlBase, param)
+      //   .then(response => {
+      //     this.apiData = response.data;
+      //   })
+      //   .catch(error => {
+      //     console.error('Error al hacer la solicitud a la API:', error);
+      //   });
+    },
+    callOrderBy(param) {
+      let urlBase = "requests/";
+      if (this.type === 'tab_tasks') {
+        urlBase = "tasks/";
+      }
+      console.log('orderBy');
+      console.log('URL BASE: ', urlBase);
+      console.log('DROPDOWN: ', param);
+      // ProcessMaker.apiClient.get(urlBase, param)
+      //   .then(response => {
+      //     this.apiData = response.data;
+      //   })
+      //   .catch(error => {
+      //     console.error('Error al hacer la solicitud a la API:', error);
+      //   });
+    },
+    performSearch() {
+     
+      console.log('Search Criteria', this.searchCriteria);
+      
+      // ProcessMaker.apiClient.get(urlBase, param)
+      //   .then(response => {
+      //     this.apiData = response.data;
+      //   })
+      //   .catch(error => {
+      //     console.error('Error al hacer la solicitud a la API:', error);
+      //   });
+    },
     fetchData() {
       // ProcessMaker.apiClient.get(`requests/${this.requestId}`)
       ProcessMaker.apiClient.get(`requests/`).then((response) => {
@@ -83,35 +170,56 @@ export default {
       });
       console.log(this.items);
     },
-    // fetchInitData() {
-    //   let pmql = "";
-    //   // Load from our api client
-    //   ProcessMaker.apiClient
-    //     .get(
-    //       `requests?page=${this.page}&per_page=${this.perPage}&include=process,participants,data` +
-    //         `&pmql=${encodeURIComponent(pmql)}&filter=${filter}&order_by=${
-    //           this.orderBy === "__slot:ids" ? "id" : this.orderBy
-    //         }&order_direction=${this.orderDirection}${this.additionalParams}`
-    //     )
-    //     .then((response) => {
-    //       this.data = this.transform(response.data);
-    //       console.log('Consulta GET');
-    //       console.log(this.data);
-    //     })
-    //     .catch((error) => {
-    //       console.log('Error');
-    //       if (_.has(error, "response.data.message")) {
-    //         ProcessMaker.alert(error.response.data.message, "danger");
-    //       } else if (_.has(error, "response.data.error")) {
-    //       } else {
-    //         throw error;
-    //       }
-    //     });
-    // },
+    fetchInitData() {
+      let pmql = "";
+
+      let { filter } = this;
+
+if (filter && filter.length) {
+  if (filter.isPMQL()) {
+    pmql = `(${pmql}) and (${filter})`;
+    filter = "";
+  }
+}
+
+if (this.previousFilter !== filter) {
+  this.page = 1;
+}
+
+this.previousFilter = filter;
+
+if (this.previousPmql !== pmql) {
+  this.page = 1;
+}
+
+this.previousPmql = pmql;
+      // Load from our api client
+      ProcessMaker.apiClient
+        .get(
+          `requests?page=1&per_page=10&include=process,participants,data` +
+            `&pmql=${encodeURIComponent(pmql)}&filter=${filter}&order_by=${
+              this.orderBy === "__slot:ids" ? "id" : this.orderBy
+            }&order_direction=${this.orderDirection}${this.additionalParams}`
+        )
+        .then((response) => {
+          this.data = this.transform(response.data);
+          console.log('Consulta GET');
+          console.log(this.data);
+        })
+        .catch((error) => {
+          console.log('Error');
+          if (_.has(error, "response.data.message")) {
+            ProcessMaker.alert(error.response.data.message, "danger");
+          } else if (_.has(error, "response.data.error")) {
+          } else {
+            throw error;
+          }
+        });
+    },
   },
   mounted() {
     // Llamar a fetchData() cuando se cargue el componente por primera vez
-    this.fetchData();
+    //this.fetchData();
     //this.fetchInitData();
   },
   formatStatus(status) {
@@ -169,5 +277,9 @@ export default {
   text-align: center;
   pointer-events: none;
   color: #aaa;
+}
+
+.narrow-input {
+  width: 100px;
 }
 </style>
--- resources/views/requests/mobile.blade.php
+++ resources/views/requests/mobile.blade.php
@@ -16,50 +16,17 @@
 
   </div>
 
-  {{-- <div class="row">
+  <div class="row">
     <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-     <requests-nav-bar :type='type'></requests-nav-bar>
+     <requests-nav-bar :type="'tab_tasks'"></requests-nav-bar>
     </div>
-  </div> --}}
+  </div>
 
   <div class="row">
     <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-      <div>
-        <b-nav pills>
-          <div class="dropdown" left>
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-circle text-warning"></i>
-              In Progress
-              <i class="fas fa-caret-down"></i>
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#"><i class="fas fa-circle text-warning"></i> In Progress</a>
-              <a class="dropdown-item" href="#"><i class="fas fa-circle text-primary"></i> Completed</a>
-            </div>
-          </div>
-
-
-          <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-user"></i>
-              <i class="fas fa-caret-down"></i>
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#"><i class="fas fa-user"></i> Requested by Me</a>
-              <a class="dropdown-item" href="#"><i class="fas fa-users"></i> With me as Participant</a>
-            </div>
-          </div>
-          <button class="btn btn-primary">
-            <i class="fas fa-search"></i>
-          </button>
-        </b-nav>
-      </div>
-    </div>  
     
-  </div>  
-  
-  
-
+    </div>
+  </div>
 
 </div>
 @endsection
