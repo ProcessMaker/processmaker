9de81e045926053135e859ea4f2ec9d38ff1ed5c
Agustín Busso <90727999+agustinbusso@users.noreply.github.com>
2023-09-15 15:16:51 -0300

    Send screen id parameter

--- resources/js/processes/screens/components/CreateScreenModal.vue
+++ resources/js/processes/screens/components/CreateScreenModal.vue
@@ -163,7 +163,7 @@ export default {
           const url = `/designer/screen-builder/${data.id}/edit`;
 
           if (this.callFromAiModeler) {
-            this.$emit("screen-created-from-modeler", url);
+            this.$emit("screen-created-from-modeler", url, data.id);
           } else {
             channel.postMessage({
               assetType: "screen",
