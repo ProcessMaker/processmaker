cf26fdacb48aaeed41c61217d2ffff7f5adab7f5
Fabio <frguachallab@gmail.com>
2023-09-18 18:19:01 +0000

    solve sonar

--- resources/js/tasks/components/MobileTasks.vue
+++ resources/js/tasks/components/MobileTasks.vue
@@ -22,10 +22,11 @@
 <script>
 import Card from "../../Mobile/Card.vue";
 import datatableMixin from "../../components/common/mixins/datatable";
-// BEGIN-NOSCAN
+import ListMixin from "./ListMixin";
+
 export default {
   components: { Card },
-  mixins: [datatableMixin],
+  mixins: [datatableMixin, ListMixin],
   data() {
     return {
       data: "",
@@ -42,81 +43,10 @@ export default {
       fields: [],
       previousFilter: "",
       previousPmql: "",
+      endpoint: "tasks",
     };
   },
   methods: {
-    fetch() {
-      Vue.nextTick(() => {
-        let pmql = "";
-
-        if (this.pmql !== undefined) {
-          pmql = this.pmql;
-        }
-
-        let { filter } = this;
-        let filterParams = "";
-
-        if (filter && filter.length) {
-          if (filter.isPMQL()) {
-            pmql = `(${pmql}) and (${filter})`;
-            filter = "";
-          } else {
-            filterParams = `&user_id=${
-              window.ProcessMaker.user.id
-            }&filter=${
-              filter
-            }&statusfilter=ACTIVE,CLOSED`;
-          }
-        }
-
-        if (this.previousFilter !== filter) {
-          this.page = 1;
-        }
-
-        this.previousFilter = filter;
-
-        if (this.previousPmql !== pmql) {
-          this.page = 1;
-        }
-
-        this.previousPmql = pmql;
-
-        // Load from our api client
-        ProcessMaker.apiClient
-          .get(
-            `tasks?page=${
-              this.page
-            }&include=process,processRequest,processRequest.user,user,data`
-              + `&pmql=${
-                encodeURIComponent(pmql)
-              }&per_page=${
-                this.perPage
-              }${filterParams
-              }${this.getSortParam()
-              }&non_system=true`,
-          )
-          .then((response) => {
-            this.data = this.transform(response.data);
-            this.$emit("in-overdue", response.data.meta.in_overdue);
-          })
-          .catch((error) => {
-            window.ProcessMaker.alert(error.response.data.message, "danger");
-            this.data = [];
-          });
-      });
-    },
-    getSortParam() {
-      if (this.sortOrder instanceof Array && this.sortOrder.length > 0) {
-        return (
-          `&order_by=${
-            this.sortOrder[0].sortField
-          }&order_direction=${
-            this.sortOrder[0].direction}`
-        );
-      }
-      return "";
-    },
   },
 };
-// END-NOSCAN
 </script>
