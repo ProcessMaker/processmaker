c591801f14cd61817b2d60b3ec0bd1e8267d0493
Paulis <42388723+pmPaulis@users.noreply.github.com>
2023-09-18 14:17:13 -0400

    FOUR-10039

--- resources/views/requests/mobile.blade.php
+++ resources/views/requests/mobile.blade.php
@@ -2,24 +2,24 @@
 @section('title')
 {{__($title)}}
 @endsection
+@php
+  $path = Request::path();
+@endphp
 @section('content_mobile')
-<div class="d-flex flex-column" style="min-height: 100vh">
+<div id="requests-listing" class="d-flex flex-column" style="min-height: 100vh">
 <div class="flex-fill">
   <div class="row">
     <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
       <div class="card card-body p-3">
           <span>Welcome Mobile ProcessMaker</span>
       </div>
-
     </div>
-
-
   </div>
 </div>
 @endsection
 
 @section('js')
-    
+
 @endsection
 
 @section('css')
