eb342b315ac8ab558d3e891ef418b323ff78b9c7
CarliPinell <carlos.pinell@processmaker.com>
2023-09-18 15:51:07 +0000

    Comments Observations fixed

--- resources/js/requests/components/FilterMobile.vue
+++ resources/js/requests/components/FilterMobile.vue
@@ -165,14 +165,14 @@ export default {
       this.showInput = !this.showInput;
       this.showDropdowns = !this.showInput;
     },
-    /* This metod receives parameters from dropdown controls
-      options selected by user
+    /** This metod receives parameters from dropdown controls
+      * options selected by user
       */
     selectOption(option, controlName, icon) {
       this.callApiFilter(this.buildApiPath(option, controlName, icon));
     },
-    /*This method builds a specific url api string depending
-      of filter used by user
+    /** This method builds a specific url api string depending
+      * of filter used by user
       */
     buildApiPath(option, controlName, icon) {
       let basePath = this.type + "?page=1&per_page=10&include=process,participants,data&";
@@ -192,8 +192,8 @@ export default {
         return basePath + 'pmql=(fulltext LIKE "%' + option + '%")';
       }
     },
-    /* This is a generic method to call API with previous builded apiPath
-      related to Filters selected by user
+    /** This is a generic method to call API with previous builded apiPath
+      * related to Filters selected by user
       */
     callApiFilter(apiPath) {
       ProcessMaker.apiClient
@@ -205,7 +205,9 @@ export default {
           console.error("Error calling API:", error);
         });
     },
-    // This method sends users's input criteria to filter specific tasks or requests
+    /** This method sends users's input criteria 
+     *  to filter specific tasks or requests 
+     */ 
     performSearch() {
       this.callApiFilter(this.buildApiPath(this.searchCriteria, "search"))
     },
@@ -232,7 +234,7 @@ export default {
     color: #aaa;
   }
   .expanded .dropdown {
-    display: none; /* Oculta los dropdowns cuando el input está expandido */
+    display: none;
   }
   .hidden-input {
     display: none;
