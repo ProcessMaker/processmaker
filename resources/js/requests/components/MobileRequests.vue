ed6640af118cff16847c7cb3414f9bee722325cb
Fabio <frguachallab@gmail.com>
2023-09-20 12:19:05 +0000

    Solve Sonar

--- resources/js/requests/components/MobileRequests.vue
+++ resources/js/requests/components/MobileRequests.vue
@@ -22,10 +22,11 @@
 <script>
 import Card from "../../Mobile/Card.vue";
 import datatableMixin from "../../components/common/mixins/datatable";
+import ListMixin from "./ListMixin";
 
 export default {
   components: { Card },
-  mixins: [datatableMixin],
+  mixins: [datatableMixin, ListMixin],
   data() {
     return {
       data: "",
@@ -47,64 +48,6 @@ export default {
     };
   },
   methods: {
-    fetch() {
-      Vue.nextTick(() => {
-        let pmql = "";
-
-        if (this.pmql !== undefined) {
-          pmql = this.pmql;
-        }
-
-        let { filter } = this;
-
-        if (filter && filter.length) {
-          if (filter.isPMQL()) {
-            pmql = `(${pmql}) and (${filter})`;
-            filter = "";
-          }
-        }
-
-        if (this.previousFilter !== filter) {
-          this.page = 1;
-        }
-
-        this.previousFilter = filter;
-
-        if (this.previousPmql !== pmql) {
-          this.page = 1;
-        }
-
-        this.previousPmql = pmql;
-
-        // Load from our api client
-        ProcessMaker.apiClient
-          .get(
-            `${this.endpoint}?page=${
-              this.page
-            }&per_page=${
-              this.perPage
-            }&include=process,participants,data`
-                  + `&pmql=${
-                    encodeURIComponent(pmql)
-                  }&filter=${
-                    filter
-                  }&order_by=${
-                    this.orderBy === "__slot:ids" ? "id" : this.orderBy
-                  }&order_direction=${
-                    this.orderDirection
-                  }${this.additionalParams}`,
-          )
-          .then((response) => {
-            this.data = this.transform(response.data);
-          }).catch((error) => {
-            if (_.has(error, "response.data.message")) {
-              ProcessMaker.alert(error.response.data.message, "danger");
-            } else if (!(_.has(error, "response.data.error"))) {
-              throw error;
-            }
-          });
-      });
-    },
   },
 };
 </script>
