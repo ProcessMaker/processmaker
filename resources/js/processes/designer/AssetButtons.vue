766001ca7d93729100a22502481760541f204577
CarliPinell <carlos.pinell@processmaker.com>
2023-09-21 22:29:04 +0000

    saving changes 3

--- resources/js/processes/designer/AssetButtons.vue
+++ resources/js/processes/designer/AssetButtons.vue
@@ -1,21 +1,24 @@
 <template>
   <b-card-text>
-  <b-card class="no-border">
-    <slot name="select-template-slot" class="custom-button custom-text"></slot>
-  </b-card>
-  <b-card class="no-border">
-    <b-button
-              variant="primary"
-              size="sm"
-              class="custom-button custom-text"
-              block
-              @click="callURL()"
-            >
-              {{ asset_name_all }}
-              <i class="fas fa-arrow-right ml-2 align-middle"></i>
-            </b-button>
-  </b-card>
-</b-card-text>
+    <b-card class="no-border">
+      <slot
+        name="select-template-slot"
+        class="custom-button custom-text"
+      ></slot>
+    </b-card>
+    <b-card class="no-border">
+      <b-button
+        variant="primary"
+        size="sm"
+        class="custom-button custom-text"
+        block
+        @click="callURL()"
+      >
+        {{ asset_name_all }}
+        <i class="fas fa-arrow-right ml-2 align-middle"></i>
+      </b-button>
+    </b-card>
+  </b-card-text>
 </template>
 
 <script>
@@ -35,26 +38,17 @@ export default {
   },
   props: {
     asset_name_all: String,
-    urlPath: String
+    urlPath: String,
   },
   data() {
-    return { 
+    return {
       countCategories: 2,
     };
   },
   methods: {
     callURL() {
-      //window.location.href = `http://172.16.3.48:9001/${assetName}`; 
-      //window.open(this.url, '_self');
-      //window.location.href = this.urlPath; 
-      //console.log('callURL:', this.urlPath);
-      //console.log('NAme:', this.asset_name_all);
-      
-      const baseURL = window.location.origin;
-      console.log('URL Base:', baseURL);
-      window.location.href = baseURL + this.urlPath; 
-    }
-    
+      window.location.href = window.location.origin + this.urlPath;
+    },
   },
 };
 </script>
@@ -64,18 +58,15 @@ export default {
   border: none !important;
 }
 
-/* Establece el ancho de los botones al 100% del card interno */
 .b-card .btn {
   width: 100% !important;
 }
 
-/* Elimina el padding y el margen de los b-card para que no afecten el tamaño del b-card padre */
 .b-card {
   padding: 0 !important;
   margin: 0 !important;
 }
 
-/* Asegura que no haya estilos heredados afectando a los botones */
 .custom-button,
 .custom-text {
   padding: 0 !important;
--- resources/js/processes/designer/Assets.vue
+++ resources/js/processes/designer/Assets.vue
@@ -7,13 +7,13 @@
     </b-navbar>
     <div class="mt-3">
       <b-card-group deck>
-        <b-card 
-           v-for="(asset, index) in assets.slice(0, 3)"
+        <b-card
+          v-for="(asset, index) in assets.slice(0, 3)"
           :key="index"
           bg-variant="light"
           class="text-center"
           @click="toggleButtons(index)"
-          >
+        >
           <div v-show="!showButtons[index]">
             <asset
               :color="asset.color"
@@ -21,61 +21,59 @@
               :asset_name="asset.asset_name"
             />
           </div>
-        
-      <div v-show="showButtons[index]" class="text-left">
-        <asset-buttons
-        :asset_name_all="asset.asset_name_all"
-          :variant="variant"
-          :urlPath="asset.urlPath">
 
-      <template v-slot:select-template-slot>
-        <div v-if="asset.flag === 1">
-            <select-template-modal 
-            :type="'Process'"
-            ref="selectTemplateModal"
-            :countCategories="countCategories"
-            :customButton="'custom-button'"
+          <div
+            v-show="showButtons[index]"
+            class="text-left"
           >
-          </select-template-modal>
-        </div>
-        <div v-if="asset.flag === 2">
-          <create-screen-modal
-                        :type="$t('Screens')"
-                        ref="create-screen-modal" 
-                        :count-categories=2
-                        :script-executors=false
-                        is-projects-installed="false"
-                    />
-        </div>
-        <div v-if="asset.flag === 3">
-          <create-script-modal
-                        :type="$t('Screen')"
-                        ref="create-script-modal" 
-                        :count-categories=2
-                        :script-executors=false
-                        is-projects-installed="false"
-                    />
-        </div>
-        
-       
-
-      </template>
-    </asset-buttons>
-      </div>
-
+            <asset-buttons
+              :asset_name_all="asset.asset_name_all"
+              :variant="variant"
+              :urlPath="asset.urlPath"
+            >
+              <template v-slot:select-template-slot>
+                <div v-if="asset.flag === 1">
+                  <select-template-modal
+                    :type="'Process'"
+                    ref="selectTemplateModal"
+                    :countCategories="countCategories"
+                    :customButton="'custom-button'"
+                  >
+                  </select-template-modal>
+                </div>
+                <div v-if="asset.flag === 2">
+                  <create-screen-modal
+                    :type="$t('Screens')"
+                    ref="create-screen-modal"
+                    :count-categories="2"
+                    :script-executors="false"
+                    is-projects-installed="false"
+                  />
+                </div>
+                <div v-if="asset.flag === 3">
+                  <create-script-modal
+                    :type="$t('Screen')"
+                    ref="create-script-modal"
+                    :count-categories="2"
+                    :script-executors="false"
+                    is-projects-installed="false"
+                  />
+                </div>
+              </template>
+            </asset-buttons>
+          </div>
         </b-card>
-          
       </b-card-group>
     </div>
     <div class="mt-3">
       <b-card-group deck>
-        <b-card 
-           v-for="(asset, index) in assets.slice(3)"
+        <b-card
+          v-for="(asset, index) in assets.slice(3)"
           :key="index"
-        bg-variant="light"
+          bg-variant="light"
           class="text-center"
           @click="toggleButtons(index + 3)"
-          >
+        >
           <div v-show="!showButtons[index + 3]">
             <asset
               :color="asset.color"
@@ -83,42 +81,51 @@
               :asset_name="asset.asset_name"
             />
           </div>
-        
-      <div v-show="showButtons[index + 3]">
-        <asset-buttons
-        :asset_name_all="asset.asset_name_all"
-          :variant="variant"
-          :urlPath="asset.urlPath">
 
-      <template v-slot:select-template-slot>
-        <div v-if="asset.flag === 4">
-            <div>
-              <b-button :aria-label="createProcess" v-b-modal.selectTemplate class="mb-3 mb-md-0 ml-md-2">
-                <i class="fas fa-plus"/> Decision Tables
-              </b-button>
-            </div>
-        </div>
-        <div v-if="asset.flag === 5">
-          <div>
-              <b-button :aria-label="createProcess" v-b-modal.selectTemplate class="mb-3 mb-md-0 ml-md-2">
-                <i class="fas fa-plus"/> Collections
-              </b-button>
-            </div>
-        </div>
-        <div v-if="asset.flag === 6">
-          <div>
-              <b-button :aria-label="createProcess" v-b-modal.createScript class="mb-3 mb-md-0 ml-md-2">
-                <i class="fas fa-plus"/> Data Connectors
-              </b-button>
-            </div>
-        </div>
-
-      </template>
-    </asset-buttons>
-      </div>
-
+          <div v-show="showButtons[index + 3]">
+            <asset-buttons
+              :asset_name_all="asset.asset_name_all"
+              :variant="variant"
+              :urlPath="asset.urlPath"
+            >
+              <template v-slot:select-template-slot>
+                <div v-if="asset.flag === 4">
+                  <div>
+                    <b-button
+                      :aria-label="createProcess"
+                      v-b-modal.selectTemplate
+                      class="mb-3 mb-md-0 ml-md-2"
+                    >
+                      <i class="fas fa-plus" /> Decision Tables
+                    </b-button>
+                  </div>
+                </div>
+                <div v-if="asset.flag === 5">
+                  <div>
+                    <b-button
+                      :aria-label="createProcess"
+                      v-b-modal.selectTemplate
+                      class="mb-3 mb-md-0 ml-md-2"
+                    >
+                      <i class="fas fa-plus" /> Collections
+                    </b-button>
+                  </div>
+                </div>
+                <div v-if="asset.flag === 6">
+                  <div>
+                    <b-button
+                      :aria-label="createProcess"
+                      v-b-modal.createScript
+                      class="mb-3 mb-md-0 ml-md-2"
+                    >
+                      <i class="fas fa-plus" /> Data Connectors
+                    </b-button>
+                  </div>
+                </div>
+              </template>
+            </asset-buttons>
+          </div>
         </b-card>
-          
       </b-card-group>
     </div>
   </div>
@@ -132,7 +139,6 @@ import ProcessesListing from "../components/ProcessesListing.vue";
 import CategorySelect from "../categories/components/CategorySelect.vue";
 import CreateScriptModal from "../../../js/processes/scripts/components/CreateScriptModal.vue";
 import CreateScreenModal from "../screens/components/CreateScreenModal.vue";
-
 
 export default {
   components: {
@@ -148,15 +154,14 @@ export default {
   data() {
     return {
       blankTemplate: true,
-        selectedTemplate: false,
-        templateData: {},
-        isProjectsInstalled: false,
-        showSelectTemplateModal: true,
-        countCategories: 2,
-        variant: 'Primary',
+      selectedTemplate: false,
+      templateData: {},
+      isProjectsInstalled: false,
+      showSelectTemplateModal: true,
+      countCategories: 2,
+      variant: "Primary",
       showModal: false,
-      //showButtons: false,
-      urlPath: '',
+      urlPath: "",
       assets: [
         {
           color: "#4DA2EB",
@@ -187,7 +192,7 @@ export default {
           icon: "fas fa-table",
           asset_name: "Decision Tables",
           asset_name_all: "See Decision Tables",
-          urlPath: "https://carlospinell.developer.processmaker.net/decision-tables",
+          urlPath: "/designer/decision-tables",
           flag: 4,
         },
         {
@@ -203,7 +208,7 @@ export default {
           icon: "fas fa-share-alt",
           asset_name: "Data Connectors",
           asset_name_all: "See All Data Conn",
-          urlPath: "https://carlospinell.developer.processmaker.net/data-connectors",
+          urlPath: "/designer/data-sources",
           flag: 6,
         },
       ],
@@ -214,43 +219,8 @@ export default {
     openSelectTemplateModal() {
       this.showSelectTemplateModal = true;
     },
-    openModal() {
-      //this.showModal = true;
-      //console.log("Refs: ", this.$refs);
-      //this.$refs.selectTemplateModalChild.openModalProcess();
-      //this.showModal = true;
-
-
-      //this.$refs["create-process-modal"].show(); //OK
-      //this.$refs["create-script-modal"].show();
-    },
     toggleButtons(index) {
       this.$set(this.showButtons, index, !this.showButtons[index]);
-      //this.showSelectTemplateModal = true;
-      //this.showButtons = true;
-      console.log('click en card index:', index);
-    },
-    handleButtonClick(action, assetName) {
-      if (action === "new") {
-        // Navegar a la ruta "New" correspondiente usando Vue Router
-        this.$router.push({ name: "new", params: { assetName } });
-      } else if (action === "seeall") {
-        // Navegar a la ruta "See All" correspondiente usando Vue Router
-        this.$router.push({ name: "seeall", params: { assetName } });
-      } else {
-        // Realizar otras acciones según sea necesario
-      }
-    },
-    getNewAssetURL(assetName) {
-      if (assetName === "Processes") {
-        this.openModal();
-        //window.location.href = `http://172.16.3.48:9001/${assetName}`;
-        //console.log('Ruta base:',window.ProcessMaker.apiClient.get);
-      }
-    },
-    getSeeAllAssetURL(assetName) {
-      // Define la lógica para obtener la URL "See All" según el nombre del activo
-      return `http://ejemplo.com/${assetName}/seeall`;
     },
   },
 };
