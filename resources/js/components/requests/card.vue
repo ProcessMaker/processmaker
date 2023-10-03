b8779d823f01e68c0ad62939a114574a371d29de
Agustín Busso <90727999+agustinbusso@users.noreply.github.com>
2023-10-03 10:42:55 -0300

    Add data-test attrs in new request modal

--- resources/js/components/requests/card.vue
+++ resources/js/components/requests/card.vue
@@ -4,16 +4,30 @@
         <div class="card-body">
           <div class="d-flex justify-content-between">
             <div>
-              <span v-uni-id="event.id.toString()">{{transformedName}}</span>
+              <span
+                v-uni-id="event.id.toString()"
+                data-test="new-request-modal-process-name"
+                :data-test-by-key="`new-request-modal-process-name-${process.id}-${event.id.toString()}`">
+                  {{transformedName}}
+              </span>
               <span v-if="process.startEvents.length > 1">: {{ event.name }}</span>
-              <a href="#" @click="showRequestDetails" :aria-expanded="ariaExpanded" :aria-controls="getComputedId(process)">...</a>
+              <a href="#"
+                @click="showRequestDetails"
+                :aria-expanded="ariaExpanded"
+                :aria-controls="getComputedId(process)"
+                data-test="new-request-modal-show-process-details-button"
+                :data-test-by-key="`new-request-modal-show-process-details-button-${process.id}-${event.id.toString()}`">
+                ...
+              </a>
             </div>
             <div class="text-right">
-              <button 
-              :href="getNewRequestLinkHref(process, event)" 
-              @click.prevent="newRequestLink(process, event);" 
+              <button
+              :href="getNewRequestLinkHref(process, event)"
+              @click.prevent="newRequestLink(process, event);"
               class="btn btn-primary btn-sm"
               v-uni-aria-describedby="event.id.toString()"
+              data-test="new-request-modal-process-start-button"
+              :data-test-by-key="`new-request-modal-process-start-button-${process.id}-${event.id.toString()}`"
               >
                 <i class="fas fa-caret-square-right mr-1"></i> {{ $t('Start') }}
             </button>
@@ -21,7 +35,13 @@
           </div>
           <div v-if="showdetail" :aria-hidden="ariaHidden" :id="getComputedId(process)">
             <hr>
-            <p class="card-text text-muted">{{ process.description }}</p>
+            <p
+              class="card-text text-muted"
+              data-test="new-request-modal-process-description"
+              :data-test-by-key="`new-request-modal-process-description-${process.id}-${event.id.toString()}`"
+            >
+              {{ process.description }}
+            </p>
           </div>
         </div>
       </div>
