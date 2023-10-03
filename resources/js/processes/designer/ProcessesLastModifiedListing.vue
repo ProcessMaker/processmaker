81b5c6ddb8c8fb54ed8d178fe4f9ad41a8590c8c
luNunezProcessmaker <luciana.nunez@processmaker.com>
2023-10-03 14:37:03 -0400

    feature/FOUR-9496

--- resources/js/processes/designer/ProcessesLastModifiedListing.vue
+++ resources/js/processes/designer/ProcessesLastModifiedListing.vue
@@ -42,7 +42,7 @@
         :no-data-template="$t('No Data Available')"
       >
         <template slot="title" slot-scope="props">
-          <a :href="`/designer/projects/${props.rowData.id}`" v-uni-id="props.rowData.id.toString()">{{props.rowData.title}}
+          <a v-bind:href="`/designer/projects/${props.rowData.id}`" v-uni-id="props.rowData.id.toString()">{{props.rowData.title}}
           </a>
         </template>
 
