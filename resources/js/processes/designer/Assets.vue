b018b925913e71770249b9d36c814282fd7507d6
CarliPinell <carlos.pinell@processmaker.com>
2023-10-04 10:50:07 -0400

    Ready to PR

--- resources/js/processes/designer/Assets.vue
+++ resources/js/processes/designer/Assets.vue
@@ -5,8 +5,7 @@
         {{ $t("Assets") }}
       </b-navbar-brand>
     </b-navbar>
-    
-    <!-- TODO: The section below must be refactored to avoid hard code in v-for -->
+
     <div class="mt-3">
       <b-card-group deck>
         <b-card
@@ -29,39 +28,11 @@
             class="text-left"
           >
             <asset-buttons
+              :asset_name="asset.asset_name"
               :asset_name_all="asset.asset_name_all"
-              :variant="variant"
-              :urlPath="asset.urlPath"
-            >
-              <template v-slot:select-template-slot>
-                <div v-if="asset.flag === 1">
-                  <select-template-modal
-                    :type="$t('Process')"
-                    ref="selectTemplateModal"
-                    :countCategories="countCategories"
-                  >
-                  </select-template-modal>
-                </div>
-                <div v-if="asset.flag === 2">
-                  <create-screen-modal
-                    :type="$t('Screens')"
-                    ref="create-screen-modal"
-                    :count-categories="2"
-                    :script-executors="false"
-                    is-projects-installed="false"
-                  />
-                </div>
-                <div v-if="asset.flag === 3">
-                  <create-script-modal
-                    :type="$t('Screen')"
-                    ref="create-script-modal"
-                    :count-categories="2"
-                    :script-executors="false"
-                    is-projects-installed="false"
-                  />
-                </div>
-              </template>
-            </asset-buttons>
+              :url-path="asset.urlPath"
+              :url-asset="asset.urlAsset"
+            />
           </div>
         </b-card>
       </b-card-group>
@@ -85,49 +56,11 @@
 
           <div v-show="showButtons[index + 3]">
             <asset-buttons
+              :asset_name="asset.asset_name"
               :asset_name_all="asset.asset_name_all"
-              :variant="variant"
-              :urlPath="asset.urlPath"
-            >
-              <template v-slot:select-template-slot>
-                <div v-if="asset.flag === 4">
-                  <div>
-                    <!-- <b-button
-                      class="mb-3 mb-md-0 ml-md-0"
-                      :style="customButtonStyle"
-                    >
-                      <i class="fas fa-plus" /> Decision Tables
-                    </b-button> -->
-                    <asset-quick-access
-                      label="decision table"
-                    />
-                  </div>
-                </div>
-                <div v-if="asset.flag === 5">
-                  <div>
-                    <!-- <b-button
-                      class="mb-3 mb-md-0 ml-md-0"
-                      :style="customButtonStyle"
-                    >
-                      <i class="fas fa-plus" /> Collections
-                    </b-button> -->
-                    <asset-quick-access
-                      label="collection"
-                    />
-                  </div>
-                </div>
-                <div v-if="asset.flag === 6">
-                  <div>
-                    <b-button
-                      class="mb-3 mb-md-0 ml-md-0"
-                      :style="customButtonStyle"
-                    >
-                      <i class="fas fa-plus" /> Data Connectors
-                    </b-button>
-                  </div>
-                </div>
-              </template>
-            </asset-buttons>
+              :url-path="asset.urlPath"
+              :url-asset="asset.urlAsset"
+            />
           </div>
         </b-card>
       </b-card-group>
@@ -137,25 +70,11 @@
 <script>
 import Asset from "./Asset.vue";
 import AssetButtons from "./AssetButtons.vue";
-import SelectTemplateModal from "../../components/templates/SelectTemplateModal.vue";
-import CreateProcessModal from "../components/CreateProcessModal.vue";
-import ProcessesListing from "../components/ProcessesListing.vue";
-import CategorySelect from "../categories/components/CategorySelect.vue";
-import CreateScriptModal from "../../../js/processes/scripts/components/CreateScriptModal.vue";
-import CreateScreenModal from "../screens/components/CreateScreenModal.vue";
-import AssetQuickAccess from "./AssetQuickAccess.vue";
 
 export default {
   components: {
     Asset,
     AssetButtons,
-    SelectTemplateModal,
-    CreateProcessModal,
-    ProcessesListing,
-    CategorySelect,
-    CreateScriptModal,
-    CreateScreenModal,
-    AssetQuickAccess,
   },
   data() {
     return {
@@ -175,7 +94,7 @@ export default {
           asset_name: "Processes",
           asset_name_all: "See All Processes",
           urlPath: "/processes",
-          flag: 1,
+          urlAsset: "/processes?create=true",
         },
         {
           color: "#8EB86F",
@@ -183,7 +102,7 @@ export default {
           asset_name: "Screens",
           asset_name_all: "See All Screens",
           urlPath: "/designer/screens",
-          flag: 2,
+          urlAsset: "/designer/screens?create=true",
         },
         {
           color: "#F7CF5D",
@@ -191,7 +110,7 @@ export default {
           asset_name: "Scripts",
           asset_name_all: "See All Scripts",
           urlPath: "/designer/scripts",
-          flag: 3,
+          urlAsset: "/designer/scripts?create=true",
         },
         {
           color: "#712F4A",
@@ -199,7 +118,7 @@ export default {
           asset_name: "Decision Tables",
           asset_name_all: "See Decision Tables",
           urlPath: "/designer/decision-tables",
-          flag: 4,
+          urlAsset: "/designer/decision-tables?create=true",
         },
         {
           color: "#D66A5F",
@@ -207,7 +126,7 @@ export default {
           asset_name: "Collections",
           asset_name_all: "See All Collections",
           urlPath: "/collections",
-          flag: 5,
+          urlAsset: "/collections?create=true",
         },
         {
           color: "#B5D3E7",
@@ -215,15 +134,10 @@ export default {
           asset_name: "Data Connectors",
           asset_name_all: "See All Data Conn",
           urlPath: "/designer/data-sources",
-          flag: 6,
+          urlAsset: "/designer/data-sources?create=true",
         },
       ],
       showButtons: new Array(6).fill(false),
-      customButtonClass: "mb-3 mb-md-0",
-      customButtonStyle: {
-        width: "100%",
-        fontSize: "14px",
-      },
     };
   },
   methods: {
