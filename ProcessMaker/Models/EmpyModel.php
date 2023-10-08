4a676c4b7aaeb17f106273700e4285f00d155995
Sanja <sanja.cornelius@processmaker.com>
2023-10-07 18:00:25 -0700

    Add project member relationship and fallback model.

---
+++ ProcessMaker/Models/EmpyModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ProcessMaker\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmpyModel extends Model
+{
+    // This model doesn't have any specific properties or table associated with it.
+}
--- ProcessMaker/Models/User.php
+++ ProcessMaker/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Validation\Rule;
 use Laravel\Passport\HasApiTokens;
+use ProcessMaker\Models\EmptyModel;
 use ProcessMaker\Notifications\ResetPassword as ResetPasswordNotification;
 use ProcessMaker\Query\Traits\PMQL;
 use ProcessMaker\Rules\StringHasAtLeastOneUpperCaseCharacter;
@@ -253,6 +254,16 @@ class User extends Authenticatable implements HasMedia
     public function groups()
     {
         return $this->morphToMany('ProcessMaker\Models\Group', 'member', 'group_members');
+    }
+
+    public function projectMembers()
+    {
+        if (class_exists('ProcessMaker\Package\Projects\Models\ProjectMember')) {
+            return $this->hasMany('ProcessMaker\Package\Projects\Models\ProjectMember', 'member_id', 'id')->where('member_type', self::class);
+        } else {
+            // Handle the case where the ProjectMember class doesn't exist.
+            return $this->hasMany(EmptyModel::class);
+        }
     }
 
     public function permissions()
