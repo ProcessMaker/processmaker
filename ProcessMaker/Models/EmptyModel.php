388385943df1757db8c95aaa78056d2e05c34c81
Sanja <sanja.cornelius@processmaker.com>
2023-10-10 08:05:15 -0700

    Resolve misspelling in model class

--- ProcessMaker/Models/EmptyModel.php
+++ ProcessMaker/Models/EmptyModel.php
@@ -4,7 +4,7 @@ namespace ProcessMaker\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EmpyModel extends Model
+class EmptyModel extends Model
 {
     // This model doesn't have any specific properties or table associated with it.
 }
