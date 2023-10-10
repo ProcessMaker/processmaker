f986c39c3e372a1510ca6f6f33ee528e00387023
Sanja <sanja.cornelius@processmaker.com>
2023-10-10 08:18:45 -0700

    Remove comments

--- resources/js/processes/screens/components/ScreenListing.vue
+++ resources/js/processes/screens/components/ScreenListing.vue
@@ -110,14 +110,6 @@ export default {
       orderBy: "title",
       screenId: null,
       assetName: " ",
-      // dupScreen: {
-      //   title: "",
-      //   type: "",
-      //   category: {},
-      //   screen_category_id: "",
-      //   description: ""
-      // },
-      // errors: [],
       sortOrder: [
         {
           field: "title",
@@ -201,52 +193,6 @@ export default {
       this.assetName = title;
       this.$refs["add-to-project-modal"].show();
     },
-  //   onAction(actionType, data, index) {
-  //     if (actionType.value) {
-  //       switch (actionType.value) {
-  //       case "duplicate-item":
-  //         this.dupScreen.title = data.title + ' ' + this.$t('Copy');
-  //         this.dupScreen.type = data.type;
-  //         this.dupScreen.category = data.category;
-  //         this.dupScreen.screen_category_id = data.screen_category_id;
-  //         this.dupScreen.description = data.description;
-  //         this.dupScreen.id = data.id;
-  //         this.showModal();
-  //         break;
-  //       case "remove-item":
-  //         let that = this;
-  //         ProcessMaker.confirmModal(
-  //           this.$t("Caution!"),
-  //            this.$t("Are you sure you want to delete the screen {{item}}? Deleting this asset will break any active tasks that are assigned.", {
-  //               item: data.title,
-  //             }),
-  //             "",
-  //           function() {
-  //             ProcessMaker.apiClient
-  //               .delete("screens/" + data.id)
-  //               .then(response => {
-  //                 ProcessMaker.alert(
-  //                   this.$t("The screen was deleted."),
-  //                   "success"
-  //                 );
-  //                 that.fetch();
-  //               });
-  //           }
-  //         );
-  //           break;
-  //           case 'add-to-project':
-  //             this.showAddToProjectModal(data.title, data.id);
-  //           break;
-  //       }
-  //   } else {
-  //       switch (actionType) {
-  //       case "edit-screen":
-  //         let link = "/designer/screen-builder/" + data.id + "/edit";
-  //         return link;
-  //         break;
-  //       }
-  //   }
-  // },
     fetch() {
       this.loading = true;
       //change method sort by slot name
--- resources/js/processes/scripts/components/ScriptListing.vue
+++ resources/js/processes/scripts/components/ScriptListing.vue
@@ -103,14 +103,6 @@ export default {
     return {
       assetId: null,
       assetName: "",
-      dupScript: {
-        title: "",
-        type: "",
-        category: {},
-        description: "",
-        script_category_id: "",
-      },
-      errors: [],
       orderBy: "title",
 
       sortOrder: [
@@ -188,36 +180,6 @@ export default {
           }
         });
     },
-    // onAction(action, data) {
-    //   switch (action.value) {
-    //     case "duplicate-item":
-    //       this.dupScript.title = data.title + " Copy";
-    //       this.dupScript.language = data.language;
-    //       this.dupScript.code = data.code;
-    //       this.dupScript.description = data.description;
-    //       this.dupScript.category = data.category;
-    //       this.dupScript.script_category_id = data.script_category_id;
-    //       this.dupScript.id = data.id;
-    //       this.dupScript.run_as_user_id = data.run_as_user_id;
-    //       this.showModal();
-    //       break;
-    //     case "remove-item":
-    //       ProcessMaker.confirmModal(
-    //         this.$t("Caution!"),
-    //         this.$t("Are you sure you want to delete {{item}}? Deleting this asset will break any active tasks that are assigned.", {
-    //           item: data.title
-    //         }),
-    //         "",
-    //         () => {
-    //           this.$emit("delete", data);
-    //         }
-    //       );
-    //       break;
-    //     case 'add-to-project':
-    //       this.showAddToProjectModal(data.title, data.id);
-    //       break;
-    //   }
-    // },
     formatLanguage(language) {
       return language;
     },
