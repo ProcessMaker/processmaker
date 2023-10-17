1f5b25de47f3f9ddc38af26b612d61ceb60bf84e
Sanja <sanja.cornelius@processmaker.com>
2023-10-16 18:36:09 -0700

    Fix issue causing project removal during updates via asset designer pages (e.g., modeler, screen builder)

--- ProcessMaker/Traits/ProjectAssetTrait.php
+++ ProcessMaker/Traits/ProjectAssetTrait.php
@@ -16,12 +16,23 @@ trait ProjectAssetTrait
     public function syncProjectAsset($request, $assetModelClass)
     {
         $projectIds = $request->input('projects', '');
+
+        // Check if the string is in the JSON array format,
+        // which happens during updates from the asset 'designer' (e.g., modeler, screen builder, etc.)
+        if (is_array($decodedProjects = json_decode($projectIds, true))) {
+            $ids = array_column($decodedProjects, 'id');
+            $projectIds = implode(',', $ids);
+        } else {
+            $projectIds = trim($projectIds, ',');
+        }
+
+        // Explode the comma-separated string, filter, and convert to integers
         $projectIds = array_filter(array_map('intval', explode(',', $projectIds)));
 
         try {
             // Sync the project assets with the prepared project IDs
             $this->projectAssets()->syncWithPivotValues($projectIds, ['asset_type' => $assetModelClass]);
-            \Log::debug('Synced project assets', $projectIds);
+            \Log::debug('Synced project assets', ['projectIds' => $projectIds]);
         } catch (Exception $e) {
             throw new ProjectAssetSyncException('Error syncing project assets: ' . $e->getMessage());
         }
