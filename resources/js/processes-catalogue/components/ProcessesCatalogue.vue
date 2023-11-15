92d823a7e494bee890dd422cc351bd1c455d50ca
luNunezProcessmaker <luciana.nunez@processmaker.com>
2023-11-15 11:18:46 -0400

    feature/FOUR-12396

---
+++ resources/js/processes-catalogue/components/ProcessesCatalogue.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="d-flex w-100">
+    <div class="w-25">
+      <h4> {{ $t('Processes Browser') }} </h4>
+    </div>
+    <div class="w-100">
+      <div v-if="!fields.length" class="d-flex justify-content-center py-5 w-100">
+        <div>
+          <div class="d-flex justify-content-center my-5">
+            <img
+              class="image d-flex"
+              src="/img/processes-catalogue-empty.svg"
+              alt="recent projects"
+            >
+          </div>
+          <h4 class="text-center">
+            {{ $t('Currently you dont have processes created') }}
+          </h4>
+          <p class="text-center">
+            {{ $t('We encourage you to create new processes using our templates') }}
+          </p>
+          <p class="text-center my-4">
+            <button
+              type="button"
+              class="btn btn-primary text-capitalize"
+            >
+              {{ $t("Show Me The Templates") }}
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      fields: [],
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
