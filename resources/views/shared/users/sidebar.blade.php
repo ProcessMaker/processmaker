b5b4d5005a0e558c78b66af5abbbd811e60fd994
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2023-11-22 14:08:24 +0000

    FOUR-11415 Password Policy Configuration

--- resources/views/shared/users/sidebar.blade.php
+++ resources/views/shared/users/sidebar.blade.php
@@ -22,8 +22,8 @@
         </div>
     </div>
     <div class="card card-body mt-3">
-        <fieldset :disabled="{{ \Auth::user()->hasPermission('edit-user-and-password') || \Auth::user()->is_administrator ? 'false' : 'true' }}">  
-            <legend> 
+        <fieldset :disabled="{{ \Auth::user()->hasPermission('edit-user-and-password') || \Auth::user()->is_administrator ? 'false' : 'true' }}">
+            <legend>
                 <h5 class="mb-3 font-weight-bold">{{__('Login Information')}}</h5>
             </legend>
             <div class="form-group">
@@ -54,7 +54,7 @@
                 {!! Form::label('confPassword', __('Confirm Password')) !!}
                 {!! Form::password('confPassword', ['id' => 'confPassword', 'rows' => 4, 'class'=> 'form-control', 'v-model'
                 => 'formData.confPassword', 'autocomplete' => 'new-password', 'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.password}']) !!}
-                <div class="invalid-feedback" :style="{display: (errors.password) ? 'block' : 'none' }" role="alert" v-if="errors.password">@{{errors.password[0]}}</div>
+                <div class="invalid-feedback" :style="{display: (errors.password) ? 'block' : 'none' }" role="alert" v-for="(error, index) in errors.password">@{{error}}</div>
             </div>
             @cannot('edit-user-and-password')
                 <div class="form-group">
