48b565d0a50b639fe2b7e36511d01a6f2e135b63
luNunezProcessmaker <luciana.nunez@processmaker.com>
2023-11-28 09:10:39 -0400

    feature/FOUR-12607

--- resources/js/processes-catalogue/components/ProcessesCatalogue.vue
+++ resources/js/processes-catalogue/components/ProcessesCatalogue.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="d-flex w-100">
-    <div class="w-25">
-      <h4> {{ $t('Processes Browser') }} </h4>
+  <div>
+    <div>
+      <breadcrumbs />
     </div>
-    <div class="w-100">
-      <div v-if="!fields.length" class="d-flex justify-content-center py-5 w-100">
-        <div>
-          <div class="d-flex justify-content-center my-5">
-            <img
-              class="image d-flex"
-              src="/img/processes-catalogue-empty.svg"
-              alt="recent projects"
-            >
+    <div class="d-flex w-100">
+      <div class="w-25">
+        <h4> {{ $t('Processes Browser') }} </h4>
+      </div>
+      <div class="w-100">
+        <div v-if="!fields.length" class="d-flex justify-content-center py-5 w-100">
+          <div>
+            <div class="d-flex justify-content-center my-5">
+              <img
+                class="image d-flex"
+                src="/img/processes-catalogue-empty.svg"
+                alt="recent projects"
+              >
+            </div>
+            <h4 class="text-center">
+              {{ $t('Currently you dont have processes created') }}
+            </h4>
+            <p class="text-center">
+              {{ $t('We encourage you to create new processes using our templates') }}
+            </p>
+            <p class="text-center my-4">
+              <button
+                type="button"
+                class="btn btn-primary text-capitalize"
+              >
+                {{ $t("Show Me The Templates") }}
+              </button>
+            </p>
           </div>
-          <h4 class="text-center">
-            {{ $t('Currently you dont have processes created') }}
-          </h4>
-          <p class="text-center">
-            {{ $t('We encourage you to create new processes using our templates') }}
-          </p>
-          <p class="text-center my-4">
-            <button
-              type="button"
-              class="btn btn-primary text-capitalize"
-            >
-              {{ $t("Show Me The Templates") }}
-            </button>
-          </p>
         </div>
       </div>
     </div>
@@ -35,7 +40,10 @@
 
 <script>
 
+import Breadcrumbs from "./Breadcrumbs.vue";
+
 export default {
+  components: { Breadcrumbs },
   data() {
     return {
       fields: [],
@@ -45,4 +53,3 @@ export default {
   },
 };
 </script>
-
