b34f7eda3aebc52ab909e178d7e5aa41bff1042c
CarliPinell <carlos.pinell@processmaker.com>
2023-11-30 18:14:00 +0000

    Saving changes

--- resources/js/processes-catalogue/components/ProcessInfo.vue
+++ resources/js/processes-catalogue/components/ProcessInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row>
     <b-col cols="2">
-      <h4> {{ $t('Processes Browser') }} </h4>
+      <h4>{{ $t("Processes Browser") }}</h4>
       <!--
       Menu Catalogue FOUR-12111
       <MenuCatologue
@@ -14,28 +14,52 @@
     <b-col cols="10">
       <div class="d-flex">
         <b-col cols="9">
-          Process Map
+          <!-- Process Map -->
+          <template>
+            <div>
+              <processes-carousel :images="carouselImages" :defaultImage="defaultImage" />
+            </div>
+          </template>
         </b-col>
-        <b-col cols="3">
-          Process Options
-        </b-col>
+        <b-col cols="3"> Process Options </b-col>
       </div>
-      <b-col cols="12">
-        Process Tab
-      </b-col>
+      <b-col cols="12"> Process Tab </b-col>
     </b-col>
   </b-row>
 </template>
 
 <script>
+import ProcessesCarousel from "../components/ProcessesCarousel.vue";
 
 export default {
+  components: { ProcessesCarousel },
   data() {
     return {
       fields: [],
+      carouselImages: [
+        { url: "https://picsum.photos/1024/480/?image=55" },
+        { url: "https://picsum.photos/1024/480/?image=54" },
+      ],
+      defaultImage: [
+        { url: "/img/launchpad-images/noImage.svg" },
+      ],
     };
   },
   methods: {
+    /**
+     * TO DO: This method gets information related to processes Launchpad Settings
+     */
+    getLaunchpadSettings() {
+      //Here call API to retrieve information
+      // ProcessMaker.apiClient
+      //   .get("query here")
+      //   .then((response) => {
+      //     this.carouselImages = response.data;
+      //   })
+      //   .catch((err) => {
+      //     console.log("Error", err);
+      //   });
+    },
   },
 };
 </script>
---
+++ resources/js/processes-catalogue/components/ProcessesCarousel.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="2000"
+      indicators
+      background="#E3E3E3"
+      img-width="593"
+      img-height="394"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <div v-if="images.length > 0">
+        <b-carousel-slide
+          v-for="(image, index) in images"
+          :key="index"
+          :img-src="image.url"
+        ></b-carousel-slide>
+      </div>
+      <div v-else>
+        <b-carousel-slide
+          v-for="index in 4"
+          :key="index"
+          :img-src="defaultImage[0].url"
+        >
+        </b-carousel-slide>
+      </div>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      required: true,
+    },
+    defaultImage: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    },
+  },
+};
+</script>
