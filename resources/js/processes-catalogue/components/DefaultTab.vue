370a07f94b0531b30a946f7d780cf81976f7d189
CarliPinell <carlos.pinell@processmaker.com>
2023-12-05 16:42:26 +0000

    Observations solved

---
+++ resources/js/processes-catalogue/components/DefaultTab.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex align-items-center justify-content-center flex-column">
+    <img
+      :src="imageSrc"
+      :alt="altText"
+    />
+    <div class="text-center">
+      <strong>{{ titleText }}</strong>
+    </div>
+    <div class="text-center">
+      {{ descriptionText }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageSrc: {
+      type: String,
+      default: "/img/launchpad-images/frame.png",
+    },
+    altText: {
+      type: String,
+      default: "",
+    },
+    titleText: {
+      type: String,
+      default: "",
+    },
+    descriptionText: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
--- resources/js/processes-catalogue/components/ProcessTab.vue
+++ resources/js/processes-catalogue/components/ProcessTab.vue
@@ -1,44 +1,38 @@
 <template>
-    <div  class="mt-3">
-        <b-tabs content-class="mt-3 text-style">
-            <b-tab title="My Requests" active>
-                <div class="d-flex align-items-center justify-content-center flex-column">
-                    <img src="/img/launchpad-images/frame.png" :alt="$t('No Image')">
-                    <div class="text-center">
-                        <strong>{{ $t('You have made no requests of this process') }}</strong>
-                    </div>
-                    <div class="text-center">
-                        {{ $t('All your requests will be shown here') }}
-                    </div>
-                </div>
-            </b-tab>
-            <b-tab title="My Tasks">
-                <div class="d-flex align-items-center justify-content-center flex-column">
-                    <img src="/img/launchpad-images/frame.png" :alt="$t('No Image')">
-                    <div class="text-center mt-3">
-                        <strong>{{ $t('You have no tasks from this process') }}</strong>
-                    </div>
-                    <div class="text-center mt-1">
-                        {{ $t('All your tasks related to this process will be shown here') }}
-                    </div>
-                </div>
-            </b-tab>
-        </b-tabs>
-    </div>
+  <div class="mt-3">
+    <b-tabs content-class="mt-3 text-style">
+      <b-tab
+        title="My Requests"
+        active
+      >
+        <default-tab
+          :altText="$t('No Image')"
+          :titleText="$t('You have made no requests of this process.')"
+          :descriptionText="$t('All your requests will be shown here')"
+        />
+      </b-tab>
+      <b-tab title="My Tasks">
+        <default-tab
+          :altText="$t('No Image')"
+          :titleText="$t('You have no tasks from this process')"
+          :descriptionText="
+            $t('All your tasks related to this process will be shown here.')
+          "
+        />
+      </b-tab>
+    </b-tabs>
+  </div>
 </template>
 
 <script>
+import DefaultTab from "./DefaultTab.vue";
 export default {
-    data() {
-    return {
-
-    }
-}
-}
+  components: { DefaultTab },
+};
 </script>
 <style>
 .text-style {
   margin-bottom: 10px;
   color: #556271;
-}    
+}
 </style>
