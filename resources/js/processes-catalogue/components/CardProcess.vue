9e4cbc363e48294ea99af3a355272985b84d10cf
Fabio <frguachallab@gmail.com>
2023-12-06 19:36:54 +0000

    FOUR-12223:Process Browser - List the cards - pagination

--- resources/js/processes-catalogue/components/CardProcess.vue
+++ resources/js/processes-catalogue/components/CardProcess.vue
@@ -1,29 +1,48 @@
 <template>
-  <div class="container processList">
-    <b-card
-      v-for="process in processList"
-      :key="process.id"
-      class="card-process"
-      @click="openProcessInfo(process)"
-    >
-      <b-card-text>
-        <img
-          class="icon-process"
-          src="/img/default-process.svg"
-          :alt="$t('Default Icon')"
-        >
-        <span class="title-process">{{ process.name }}</span>
-      </b-card-text>
-    </b-card>
+  <div>
+    <div class="container processList">
+      <b-card
+        v-for="process in processList"
+        :key="process.id"
+        class="card-process"
+        @click="openProcessInfo(process)"
+      >
+        <b-card-text>
+          <img
+            class="icon-process"
+            src="/img/default-process.svg"
+            :alt="$t('Default Icon')"
+          >
+          <span class="title-process">{{ process.name }}</span>
+        </b-card-text>
+      </b-card>
+    </div>
+    <pagination
+      :total-row="totalRow"
+      :total-pages="totalPages"
+      @onPageChanged="onPageChanged"
+    />
   </div>
 </template>
 
 <script>
+import pagination from "./utils/pagination.vue";
+
 export default {
+  components: { pagination },
   props: ["category"],
   data() {
     return {
       processList: [],
+      currentdata: [],
+      currentPage: 1,
+      limit: 7,
+      totalRow: null,
+      perPage: 9,
+      order_by: "name",
+      dir: "asc",
+      data: null,
+      totalPages: 1,
     };
   },
   watch: {
@@ -36,16 +55,31 @@ export default {
   },
   methods: {
     loadCard() {
-      /* TODO complete the new API */
       ProcessMaker.apiClient
-        .get("processes")
+        .get(
+          `processes?page=${this.currentPage}`
+          + `&per_page=${this.perPage}`
+          + `&category=${this.category.id}`
+          + "&order_by=name&order_direction=asc",
+        )
         .then((response) => {
           this.processList = response.data.data;
+          this.totalRow = response.data.meta.total;
+          this.totalPages = response.data.meta.total_pages;
         });
     },
-    /** go to process info */
+    /**
+     * Go to process info
+     */
     openProcessInfo(process) {
       this.$emit("openProcess", process);
+    },
+    /**
+     * Load Cards in the new pagination
+     */
+    onPageChanged(page) {
+      this.currentPage = page;
+      this.loadCard();
     },
   },
 };
