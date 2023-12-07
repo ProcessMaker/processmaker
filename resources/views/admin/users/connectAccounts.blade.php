cf3fac542782e15ffdfb847d6aafee2c3a050271
Teisha McRae <mcraeteisha@gmail.com>
2023-12-07 12:52:56 -0500

    Add edit button and success pill to accounts

--- resources/views/admin/users/connectAccounts.blade.php
+++ resources/views/admin/users/connectAccounts.blade.php
@@ -20,14 +20,25 @@
   <div class="container" id="connectedAccounts">
     <div class="card card-body">
       <h4 class="mt-2 pb-3 page-title">{{__('Connected Accounts')}}</h4>
-      <ul class="accounts-list w-100">
-        <li class="accounts-list-item d-flex align-items-center" v-for="account in accounts">
+      <ul class="accounts-list w-100 pl-0">
+        <li class="accounts-list-item d-flex align-items-start py-3 mt-3" v-for="account in accounts">
           <div class="d-flex align-items-start mr-3">
             <img :src="account.icon" :alt="account.name + 'icon'" width="45px"/>
           </div>
-          <div>
-            <h5 class="account-name mb-0">@{{account.name}}</h5>
-            <p class="account-description">@{{account.description}}</p>
+          <div class="d-flex flex-column flex-grow-1">
+            <div class="d-flex align-items-center justify-content-between mb-2">
+              <div>
+                <h5 class="account-name mb-0">@{{account.name}}</h5>
+                <p class="account-description mb-0">@{{account.description}}</p>
+              </div>
+                <div class="d-flex align-items-center">
+                <button class="edit-btn">Edit</button>
+                <b-badge pill variant="success" class="ml-3 connection-status">
+                    <i class="fa fa-check"></i>
+                    Connected
+                </b-badge>
+              </div>
+            </div>
           </div>
         </li>
       </ul>
@@ -35,7 +46,7 @@
   </div>
 
 
-  <pm-modal
+  {{-- <pm-modal
     ref="editConnectionModal"
     id="editConnectionModal"
     title="{{__('Edit Connection')}}"
@@ -59,7 +70,7 @@
       </vue-croppie>
     </div>
   <input id="upload-image" type="file" class="custom-file-input" accept=".gif,.jpg,.jpeg,.png,image/jpeg,image/gif,image/png" ref="customFile" @change="onFileChange" aria-label="{{__('Select a file')}}">
-  </pm-modal>
+  </pm-modal> --}}
 @endsection
 
 @section('js')
@@ -69,7 +80,6 @@
       var modalVueInstance = new Vue({
         el: '#editConnectionModal',
         data() {
-          };
         },
         methods: {
           hideModal() {
@@ -146,6 +156,10 @@
       font-size: 21px;
     }
 
+    .accounts-list-item {
+      border-bottom: 1px solid #C4C8CC;
+    }
+
     .account-name {
       font-size: 18px;
       font-weight: 600;
@@ -154,6 +168,21 @@
     .account-description {
       font-size: 14px;
       font-weight: 400;
+      color: #6C757D;
+    }
+
+    .edit-btn {
+      border: none;
+      background-color: #FFFFFF;
+      color: #6C757D;
+      font-size: 14px;
+    }
+
+    .connection-status {
+      border-radius: 8px;
+      font-size: 16px;
+      font-weight: 400;
+      padding: 0.75rem;
     }
     </style>
 @endsection
