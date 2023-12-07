5faabb9ede51a86401ebc2e6348d59519fe35e75
Teisha McRae <mcraeteisha@gmail.com>
2023-12-07 16:23:39 -0500

    Configure Edit Connection Modal

--- resources/views/admin/users/connectAccounts.blade.php
+++ resources/views/admin/users/connectAccounts.blade.php
@@ -32,7 +32,7 @@
                 <p class="account-description mb-0">@{{account.description}}</p>
               </div>
                 <div class="d-flex align-items-center">
-                <button class="edit-btn">Edit</button>
+                <button class="edit-btn" @click="showModal()">Edit</button>
                 <b-badge pill variant="success" class="ml-3 connection-status">
                     <i class="fa fa-check"></i>
                     Connected
@@ -45,144 +45,184 @@
     </div>
   </div>
 
-
-  {{-- <pm-modal
+  <pm-modal
     ref="editConnectionModal"
     id="editConnectionModal"
     title="{{__('Edit Connection')}}"
     style="display: none;"
+    :ok-disabled="disabled"
+    :set-custom-buttons="true"
+    :custom-buttons="customModalButtons"
+    @hidden="onClose"
+    @close="onClose"
+    @onSubmit="onSubmit"
   >
-    <div>
-      <div v-if="!image" class="no-avatar"
-        align="center">{{__('Click the browse button below to get started')}}</div>
-      <div align="center">
-        <button type="button" @click="browse" class="btn btn-secondary mt-5 mb-2"><i class="fas fa-upload"></i>
-          {{__('Browse')}}
-        </button>
-      </div>
-      <div align="center">
-        {{__('Image types accepted: .gif, .jpg, .jpeg, .png')}}
-      </div>
-      <vue-croppie :style="{display: (image) ? 'block' : 'none' }" ref="croppie"
-        :viewport="{ width: 380, height: 380, type: 'circle' }"
-        :boundary="{ width: 400, height: 400 }" :enable-orientation="false"
-        :enable-resize="false">
-      </vue-croppie>
+    <div class="form-group">
+      {!! Form::label('url', __('URL')) !!}
+      {!! Form::text('url', null, ['id' => 'url','class'=> 'form-control', 'v-model' =>
+      'formData.url', 'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.url}',
+      'v-bind:placeholder' => '$t("Placeholder")',
+      'required', 'aria-required' => 'true']) !!}
+      <div class="invalid-feedback" role="alert" v-for="url in errors.url">@{{url}}</div>
     </div>
-  <input id="upload-image" type="file" class="custom-file-input" accept=".gif,.jpg,.jpeg,.png,image/jpeg,image/gif,image/png" ref="customFile" @change="onFileChange" aria-label="{{__('Select a file')}}">
-  </pm-modal> --}}
+    <div class="form-group">
+      {!! Form::label('user', __('User')) !!}
+      {!! Form::text('user', null, ['id' => 'user', 'rows' => 4, 'class'=>
+      'form-control', 'v-model' => 'formData.user',
+      'v-bind:placeholder' => '$t("Placeholder")',
+      'v-bind:class' => '{\'form-control\':true,\'is-invalid\':errors.user}']) !!}
+      <div class="invalid-feedback" role="alert" v-for="user in errors.user">@{{user}}
+      </div>
+    </div>
+    <div class="form-group">
+      {!! Form::label('accessKey', __('Access Key')) !!}
+      {!! Form::text('accessKey', null, ['id' => 'accessKey', 'rows' => 4, 'class'=>
+      'form-control', 'v-model' => 'formData.accessKey',
+      'v-bind:placeholder' => '$t("Placeholder")',
+      'v-bind:class' => '{\'form-control\':true,\'is-invalid\':errors.accessKey}']) !!}
+      <div class="invalid-feedback" role="alert" v-for="accessKey in errors.accessKey">@{{accessKey}}
+      </div>
+    </div>
+  </pm-modal>
 @endsection
 
 @section('js')
-    <script src="{{mix('js/admin/users/edit.js')}}"></script>
-
-    <script>
-      var modalVueInstance = new Vue({
-        el: '#editConnectionModal',
-        data() {
-        },
-        methods: {
-          hideModal() {
-            this.$refs.editConnectionModal.hide();
-          },
-        }
-      });
-    </script>
-
-    <script>
-      var formVueInstance = new Vue({
-        el: '#connectedAccounts',
-        data() {
-          return {
-            accounts:[
-            {
-              id: 1,
-              name: 'Gmail',
-              description: 'Provide automated responses, generate drafts, and assist with email composition.',
-              icon: 'https://img.icons8.com/color/48/gmail--v1.png',
-            },
-            {
-              id: 2,
-              name: 'Slack',
-              description: 'Assist with task management, scheduling, and provide intelligent message responses.',
-              icon: 'https://img.icons8.com/color/48/slack-new.png',
-            },
+  <script>
+    var modalVueInstance = new Vue({
+      el: '#editConnectionModal',
+      data() {
+        return {
+          customModalButtons: [
+            {"content": "Cancel", "action": "close", "variant": "secondary", "size": "md"},
+            {"content": "OK", "action": "onSubmit", "variant": "primary", "size": "md"},
           ],
-            meta: @json(config('users.properties')),
-            formData: @json($user),
-            userId: @json($user->id),
-            errors: {
-              username: null,
-              firstname: null,
-              lastname: null,
-              email: null,
-              password: null,
-              status: null,
-              is_administrator: null,
-            },
-          }
-        },
-        computed: {
-        },
-        mounted() {
-        },
-        watch: {
-        },
-        methods: {
-          resetErrors() {
-            this.errors = Object.assign({}, {
-              username: null,
-              firstname: null,
-              lastname: null,
-              email: null,
-              password: null,
-              status: null
-            });
+          formData: {},
+          errors: {
+            'url': null,
+            'user': null,
+            'accessKey': null
           },
-          onClose() {
-            window.location.href = '/admin/users';
-          },
+          disabled: false
+        }
       },
-      });
-    </script>
+      methods: {
+        hideModal() {
+          modalVueInstance.$refs.editConnectionModal.hide();
+        },
+        onClose() {
+          this.hideModal();
+          this.resetFormData();
+          this.resetErrors();
+        },
+        resetFormData() {
+          this.formData = Object.assign({}, {
+            url: null,
+            user: null,
+            accessKey: null
+          });
+        },
+        resetErrors() {
+          this.errors = Object.assign({}, {
+            url: null,
+            user: null,
+            accessKey: null
+          });
+        },
+        onSubmit() {
+          this.resetErrors();
+          //single click
+          if (this.disabled) {
+            return
+          }
+          this.disabled = true;
+
+          //TODO: HANDLE CONNECTION UPDATE
+
+        }
+      }
+    });
+  </script>
+
+  <script>
+    var formVueInstance = new Vue({
+      el: '#connectedAccounts',
+      data() {
+        return {
+          //TODO: REMOVE THIS DUMMY ACCOUNT DATA
+          accounts:[
+          {
+            id: 1,
+            name: 'Gmail',
+            description: 'Provide automated responses, generate drafts, and assist with email composition.',
+            icon: 'https://img.icons8.com/color/48/gmail--v1.png',
+          },
+          {
+            id: 2,
+            name: 'Slack',
+            description: 'Assist with task management, scheduling, and provide intelligent message responses.',
+            icon: 'https://img.icons8.com/color/48/slack-new.png',
+          },
+        ],
+        }
+      },
+      methods: {
+        showModal() {
+          modalVueInstance.$refs.editConnectionModal.show();
+        },
+        resetErrors() {
+          this.errors = Object.assign({}, {
+            username: null,
+            firstname: null,
+            lastname: null,
+            email: null,
+            password: null,
+            status: null
+          });
+        },
+        onClose() {
+          window.location.href = '/admin/users';
+        },
+    },
+    });
+  </script>
 @endsection
 
 
 @section('css')
-    <style>
+  <style>
 
-    .page-title {
-      color: #556271;
-      font-size: 21px;
-    }
+  .page-title {
+    color: #556271;
+    font-size: 21px;
+  }
 
-    .accounts-list-item {
-      border-bottom: 1px solid #C4C8CC;
-    }
+  .accounts-list-item {
+    border-bottom: 1px solid #C4C8CC;
+  }
 
-    .account-name {
-      font-size: 18px;
-      font-weight: 600;
-    }
+  .account-name {
+    font-size: 18px;
+    font-weight: 600;
+  }
 
-    .account-description {
-      font-size: 14px;
-      font-weight: 400;
-      color: #6C757D;
-    }
+  .account-description {
+    font-size: 14px;
+    font-weight: 400;
+    color: #6C757D;
+  }
 
-    .edit-btn {
-      border: none;
-      background-color: #FFFFFF;
-      color: #6C757D;
-      font-size: 14px;
-    }
+  .edit-btn {
+    border: none;
+    background-color: #FFFFFF;
+    color: #6C757D;
+    font-size: 14px;
+  }
 
-    .connection-status {
-      border-radius: 8px;
-      font-size: 16px;
-      font-weight: 400;
-      padding: 0.75rem;
-    }
-    </style>
+  .connection-status {
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 400;
+    padding: 0.75rem;
+  }
+  </style>
 @endsection
