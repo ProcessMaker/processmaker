bccfe0ee7a19bcea3901c0c95d1da5f4bcdd8580
Fabio <frguachallab@gmail.com>
2023-12-08 19:51:32 +0000

    FOUR-12222:Process Browser - Search

---
+++ resources/js/processes-catalogue/components/utils/SearchCards.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <b-input-group>
+      <b-input-group-prepend>
+        <b-btn
+          class="btn-search-run px-2"
+          :title="$t('Search Processes')"
+          @click="fetch()"
+        >
+          <i class="fas fa-search search-icon" />
+        </b-btn>
+      </b-input-group-prepend>
+      <b-form-input
+        id="search-box"
+        v-model="filter"
+        class="pl-0"
+        :placeholder="$t('Search Processes')"
+      />
+    </b-input-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["filterPmql"],
+  data() {
+    return {
+      filter: "",
+    };
+  },
+  methods: {
+    fetch() {
+      this.filterPmql(this.filter);
+    },
+  },
+};
+</script>
