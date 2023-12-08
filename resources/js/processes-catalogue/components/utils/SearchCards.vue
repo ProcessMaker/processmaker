cabe420faae47652e3aa430aaa7a06230f39bac9
Fabio <frguachallab@gmail.com>
2023-12-08 20:27:48 +0000

    Styles

--- resources/js/processes-catalogue/components/utils/SearchCards.vue
+++ resources/js/processes-catalogue/components/utils/SearchCards.vue
@@ -4,6 +4,7 @@
       <b-input-group-prepend>
         <b-btn
           class="btn-search-run px-2"
+          variant="custom"
           :title="$t('Search Processes')"
           @click="fetch()"
         >
@@ -13,7 +14,7 @@
       <b-form-input
         id="search-box"
         v-model="filter"
-        class="pl-0"
+        class="ml-1"
         :placeholder="$t('Search Processes')"
       />
     </b-input-group>
@@ -35,3 +36,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.btn-custom {
+  background-color: #f7f9fb;
+}
+</style>
