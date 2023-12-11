b5abf39d401ea7c5cab2a99ba3b3c6310eddcd64
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2023-12-11 15:30:28 +0000

    FOUR-12236 Session Control - UI

--- ProcessMaker/Models/Setting.php
+++ ProcessMaker/Models/Setting.php
@@ -68,6 +68,8 @@ class Setting extends ProcessMakerModel implements HasMedia
 
     public const LOGIN_OPTIONS_GROUP = 'Log-In Options';
 
+    public const SESSION_CONTROL_GROUP = 'Session Control';
+
     /**
      * The attributes that aren't mass assignable.
      *
