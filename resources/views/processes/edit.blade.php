fcf5789b03e9037f395fcb90c7b7af89ded63af1
David Callizaya <davidcallizaya@gmail.com>
2023-12-13 18:43:46 -0400

    Show error hint for case title

--- resources/views/processes/edit.blade.php
+++ resources/views/processes/edit.blade.php
@@ -106,6 +106,7 @@
                                         'v-bind:class' => '{\'form-control\':true, \'is-invalid\':errors.case_title}'
                                     ])
                                 !!}
+                                <div class="invalid-feedback" role="alert" v-if="errors.case_title">@{{errors.case_title[0]}}</div>
                                 <small class="form-text text-muted" v-if="! errors.name">
                                     {{ __('This field has a limit of 200 characters when calculated') }}
                                 </small>
