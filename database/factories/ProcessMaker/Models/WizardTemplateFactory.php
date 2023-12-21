a838e35d74d91da02c995be8641cc5cfacd0c981
Sanja <sanja.cornelius@processmaker.com>
2023-12-20 17:39:36 -0800

    Include missing attributes

--- database/factories/ProcessMaker/Models/WizardTemplateFactory.php
+++ database/factories/ProcessMaker/Models/WizardTemplateFactory.php
@@ -15,6 +15,11 @@ class WizardTemplateFactory extends Factory
         return [
             'uuid' => $this->faker->uuid,
             'process_template_id' => null,
+            'name' => $this->faker->unique()->name(),
+            'description' => $this->faker->unique()->name(),
+            'media_collection' => $this->faker->unique()->name(),
+            'config_collection_id' => null,
+            'template_details' => '{}',
             'helper_process_id' => Process::factory()->create()->id,
         ];
     }
