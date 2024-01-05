d65ef430aa285d7a3daf819ab20a2d54839ae8d3
CarliPinell <carlos.pinell@processmaker.com>
2024-01-05 03:31:46 +0000

    Solving bugfix reported

--- resources/js/processes-catalogue/components/utils/SearchCards.vue
+++ resources/js/processes-catalogue/components/utils/SearchCards.vue
@@ -11,13 +11,18 @@
           <i class="fas fa-search search-icon" />
         </b-btn>
       </b-input-group-prepend>
-      <b-form-input
-        id="search-box"
-        v-model="filter"
-        class="ml-1"
-        :placeholder="$t('Search Processes')"
-        @keyup.enter="fetch()"
-      />
+      <div class="search-container">
+        <b-form-input
+          id="search-box"
+          v-model="filter"
+          class="ml-1 d-flex align-items-center"
+          :placeholder="$t('Search Processes')"
+          @keyup.enter="fetch()"
+        />
+        <button class="btn-clear-search" @click="clearSearch()">
+          <i class="fas fa-times"></i>
+        </button>
+      </div>
     </b-input-group>
   </div>
 </template>
@@ -34,12 +39,34 @@ export default {
     fetch() {
       this.filterPmql(this.filter);
     },
+    clearSearch() {
+      this.filter = "";
+      this.fetch();
+    },
   },
 };
 </script>
 
 <style scoped>
+.search-container {
+  position: relative;
+  width: 95%;
+}
+
 .btn-custom {
   background-color: #f7f9fb;
+}
+
+.btn-clear-search {
+  position: absolute;
+  top: 50%;
+  right: 8px;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  font-size: 16px;
+  z-index: 1;
 }
 </style>
