d73542bd2da3e4821d09e8ec60f7de0f6638e17c
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2024-01-05 17:56:26 +0000

    FOUR-13099 The message for the security code screens must be the same as the design in the mockup

--- ProcessMaker/Http/Controllers/Auth/TwoFactorAuthController.php
+++ ProcessMaker/Http/Controllers/Auth/TwoFactorAuthController.php
@@ -46,7 +46,10 @@ class TwoFactorAuthController extends Controller
             }
 
             // Set informative message
-            session()->put(self::TFA_MESSAGE, _('Enter the security code we sent you.'));
+            $methodsNames = $this->friendlyMethodsNames();
+            $message = __('Enter the security code from :methods. If incorrect, please retry with the latest code provided.',
+                ['methods' => $methodsNames]);
+            session()->put(self::TFA_MESSAGE, $message);
         } catch (Exception $error) {
             session()->put(self::TFA_ERROR, $error->getMessage());
         }
@@ -199,5 +202,33 @@ class TwoFactorAuthController extends Controller
             ], 500);
         }
         return true;
+    }
+
+    private function friendlyMethodsNames()
+    {
+        // Define the friendly names for each method
+        $friendlyNames = [
+            TwoFactorAuthentication::EMAIL => __('Email'),
+            TwoFactorAuthentication::SMS => __('SMS'),
+            TwoFactorAuthentication::AUTH_APP => __('Google Authenticator'),
+        ];
+
+        // Get enabled methods to send the code
+        $enabledMethods = config('password-policies.2fa_method', []);
+
+        // Return the friendly names for enabled methods
+        $methods = array_map(function($method) use ($friendlyNames) {
+            return $friendlyNames[$method] ?? $method;
+        }, $enabledMethods);
+
+        // Build final string
+        if (count($methods) > 1) {
+            $lastMethod = array_pop($methods);
+            $methods = implode(', ', $methods) . ' ' . __('or') . ' ' . $lastMethod;
+        } else {
+            $methods = $methods[0];
+        }
+
+        return $methods;
     }
 }
