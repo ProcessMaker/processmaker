653da4e6955661b6937d847e01cd68648a1f1a46
Roly Gutierrez <roly.gutierrez@processmaker.com>
2024-01-11 12:26:57 -0400

    FOUR-13206 New Table and filters are not translated

--- resources/js/components/shared/PmqlInput.vue
+++ resources/js/components/shared/PmqlInput.vue
@@ -92,7 +92,7 @@
 
       <div v-if="showFilters && selectedFilters.length" class="selected-filters-bar d-flex pt-2">
         <span v-for="filter in selectedFilters" class="selected-filter-item d-flex align-items-center">
-          <span class="selected-filter-key mr-1">{{ filter[0] }}: </span>
+          <span class="selected-filter-key mr-1">{{ $t(filter[0]) }}: </span>
           {{ filter[1][0].name ? filter[1][0].name : filter[1][0].fullname }} 
           <span v-if="filter[1].length > 1" class="badge badge-pill ml-2 filter-counter">
             +{{ filter[1].length -1 }}
