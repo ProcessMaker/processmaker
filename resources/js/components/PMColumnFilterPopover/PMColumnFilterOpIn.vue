ec58cbbceda99c46fddd4368b73cef181643a1b7
Roly Gutierrez <roly.gutierrez@processmaker.com>
2024-01-12 15:34:30 -0400

    FOUR-12797 Fix conflicts

--- resources/js/components/PMColumnFilterPopover/PMColumnFilterOpIn.vue
+++ resources/js/components/PMColumnFilterPopover/PMColumnFilterOpIn.vue
@@ -1,80 +1,75 @@
 <template>
   <div>
-    <div
-      v-for="(item, index) in input"
-      :key="index"
-      :data-cy="'pm-column-filter-op-in' + index"
-      class="pm-column-filter-op-in"
-    >
-      <b-form-input
-        v-model="input[index]"
-        :placeholder="$t('input ') + (index + 1)"
-        :data-cy="'pm-column-filter-op-in-input' + (index + 1)"
-        size="sm"
-        class="pm-column-filter-op-in-input"
-        @input="onInput"
-      />
-      <b-icon
-        icon="plus-square-dotted"
-        class="pm-column-filter-op-in-plus-square"
-        @click="onPlusIcon(index)"
-      />
-      <b-icon
-        icon="dash-square-dotted"
-        class="pm-column-filter-op-in-plus-square"
-        @click="onDashIcon(index)"
-      />
+    <div v-for="(item, index) in input" 
+         :key="index"
+         :data-cy="'pm-column-filter-op-in' + index"
+         class="pm-column-filter-op-in">
+      <b-form-input v-model="input[index]"
+                    :placeholder="$t('input ') + (index + 1)"
+                    :data-cy="'pm-column-filter-op-in-input' + (index + 1)"
+                    size="sm"
+                    class="pm-column-filter-op-in-input"
+                    @input="onInput">
+      </b-form-input>
+      <b-icon icon="plus-square-dotted"
+              @click="onPlusIcon(index)"
+              class="pm-column-filter-op-in-plus-square">
+      </b-icon>
+      <b-icon icon="dash-square-dotted"
+              @click="onDashIcon(index)"
+              class="pm-column-filter-op-in-plus-square">
+      </b-icon>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  components: {
-  },
-  props: [
-    "value",
-  ],
-  data() {
-    return {
-      input: [...this.value],
-    };
-  },
-  watch: {
-    value: {
-      handler(newValue) {
-        this.input = [...newValue];
-      },
-      immediate: true,
+  export default {
+    components: {
     },
-  },
-  created() {
-    this.input = [""];
-    this.$emit("input", this.input);
-  },
-  methods: {
-    onPlusIcon(index) {
-      this.addInput(index + 1);
+    props: [
+      "value"
+    ],
+    data() {
+      return {
+        input: [...this.value]
+      };
     },
-    onDashIcon(index) {
-      if (this.input.length === 1) {
-        return;
+    watch: {
+      value: {
+        handler(newValue) {
+          this.input = [...newValue];
+        },
+        immediate: true
       }
-      this.removeInput(index);
     },
-    addInput(index) {
-      this.input.splice(index, 0, "");
+    created() {
+      this.input = [""];
       this.$emit("input", this.input);
     },
-    removeInput(index) {
-      this.input.splice(index, 1);
-      this.$emit("input", this.input);
-    },
-    onInput() {
-      this.$emit("input", this.input);
-    },
-  },
-};
+    methods: {
+      onPlusIcon(index) {
+        this.addInput(index + 1);
+      },
+      onDashIcon(index) {
+        if (this.input.length === 1) {
+          return;
+        }
+        this.removeInput(index);
+      },
+      addInput(index) {
+        this.input.splice(index, 0, "");
+        this.$emit("input", this.input);
+      },
+      removeInput(index) {
+        this.input.splice(index, 1);
+        this.$emit("input", this.input);
+      },
+      onInput() {
+        this.$emit("input", this.input);
+      }
+    }
+  };
 </script>
 
 <style scoped>
