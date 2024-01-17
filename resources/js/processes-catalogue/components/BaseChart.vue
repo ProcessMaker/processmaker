87fd1388bde3b310febcadc84af6e8a4f45619f0
CarliPinell <carlos.pinell@processmaker.com>
2024-01-17 19:02:02 +0000

    Modifying base chart checking idChart

--- resources/js/processes-catalogue/components/BaseChart.vue
+++ resources/js/processes-catalogue/components/BaseChart.vue
@@ -147,7 +147,7 @@ export default {
   },
   methods: {
     fetchChart(idChart) {
-      if (!Processmaker.isSavedSearchInstalled) return;
+      if (!Processmaker.isSavedSearchInstalled || !idChart) return;
       ProcessMaker.apiClient
         .get(`saved-searches/charts/${idChart}`, { timeout: 0 })
         .then((response) => {
