61cfc386c40c27ac45e6023fbba5fc9d45dc78bf
TinCodes <tincholaguna37@gmail.com>
2024-01-18 15:02:37 -0400

    Additional optional chainings made

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -93,7 +93,7 @@ window.Vue.component("filter-table", FilterTable);
 
 let translationsLoaded = false;
 const mdates = JSON.parse(
-  document.head.querySelector("meta[name=\"i18n-mdate\"]").content,
+  document.head.querySelector("meta[name=\"i18n-mdate\"]")?.content,
 );
 
 // Make $t available to all vue instances
@@ -270,9 +270,9 @@ if (appUrl) {
 if (userID) {
   window.ProcessMaker.user = {
     id: userID.content,
-    datetime_format: formatDate.content,
-    calendar_format: formatDate.content,
-    timezone: timezone.content,
+    datetime_format: formatDate?.content,
+    calendar_format: formatDate?.content,
+    timezone: timezone?.content,
     fullName: userFullName?.content,
     avatar: userAvatar?.content,
   };
@@ -298,9 +298,9 @@ if (window.Processmaker && window.Processmaker.broadcasting) {
 if (userID) {
   // Session timeout
   const timeoutScript = document.head.querySelector("meta[name=\"timeout-worker\"]")?.content;
-  window.ProcessMaker.AccountTimeoutLength = parseInt(eval(document.head.querySelector("meta[name=\"timeout-length\"]").content));
-  window.ProcessMaker.AccountTimeoutWarnSeconds = parseInt(document.head.querySelector("meta[name=\"timeout-warn-seconds\"]").content);
-  window.ProcessMaker.AccountTimeoutEnabled = document.head.querySelector("meta[name=\"timeout-enabled\"]") ? parseInt(document.head.querySelector("meta[name=\"timeout-enabled\"]").content) : 1;
+  window.ProcessMaker.AccountTimeoutLength = parseInt(eval(document.head.querySelector("meta[name=\"timeout-length\"]")?.content));
+  window.ProcessMaker.AccountTimeoutWarnSeconds = parseInt(document.head.querySelector("meta[name=\"timeout-warn-seconds\"]")?.content);
+  window.ProcessMaker.AccountTimeoutEnabled = document.head.querySelector("meta[name=\"timeout-enabled\"]") ? parseInt(document.head.querySelector("meta[name=\"timeout-enabled\"]")?.content) : 1;
   window.ProcessMaker.AccountTimeoutWorker = new Worker(timeoutScript);
   window.ProcessMaker.AccountTimeoutWorker.addEventListener("message", (e) => {
     if (e.data.method === "countdown") {
