7e2450fbd517c92ac835e64a7466519f40fa166a
CarliPinell <carlos.pinell@processmaker.com>
2024-01-18 20:54:48 +0000

    QA Observation fixed

--- ProcessMaker/Http/Controllers/TaskController.php
+++ ProcessMaker/Http/Controllers/TaskController.php
@@ -110,7 +110,7 @@ class TaskController extends Controller
             }
 
             UserResourceView::setViewed(Auth::user(), $task);
-
+            $currentUser = Auth::user()->only(['id', 'username', 'fullname', 'firstname', 'lastname', 'avatar']);
             return view('tasks.edit', [
                 'task' => $task,
                 'dueLabels' => self::$dueLabels,
@@ -120,6 +120,7 @@ class TaskController extends Controller
                 'addons' => $this->getPluginAddons('edit', []),
                 'assignedToAddons' => $this->getPluginAddons('edit.assignedTo', []),
                 'dataActionsAddons' => $this->getPluginAddons('edit.dataActions', []),
+                'currentUser' => $currentUser,
             ]);
         }
     }
