a96380436437bef20b8acd5d12fa7c4a8c3d4e45
CarliPinell <carlos.pinell@processmaker.com>
2024-01-19 15:17:13 +0000

    Solving SonarQube observation spliting code under 120 characters

--- ProcessMaker/Http/Controllers/TaskController.php
+++ ProcessMaker/Http/Controllers/TaskController.php
@@ -110,7 +110,16 @@ class TaskController extends Controller
             }
 
             UserResourceView::setViewed(Auth::user(), $task);
-            $currentUser = Auth::user()->only(['id', 'username', 'fullname', 'firstname', 'lastname', 'avatar', 'timezone', 'datetime_format']);
+            $currentUser = Auth::user()->only([
+                'id',
+                'username',
+                'fullname',
+                'firstname',
+                'lastname',
+                'avatar',
+                'timezone',
+                'datetime_format'
+            ]);
             return view('tasks.edit', [
                 'task' => $task,
                 'dueLabels' => self::$dueLabels,
