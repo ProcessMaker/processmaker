18f74a990e75d666fee239cfb64e3f46686cd25e
Jose Chirivella <jose.chirivella@processmaker.com>
2024-01-25 14:58:49 -0600

    Added @babel/preset-env

--- webpack.mix.js
+++ webpack.mix.js
@@ -129,12 +129,12 @@ mix
 
   // Note, that this should go last for the extract to properly put the manifest and vendor in the right location
   // See: https://github.com/JeffreyWay/laravel-mix/issues/1118
-  .js("resources/js/app.js", "public/js")
-  .polyfill({
-    enabled: true,
-    useBuiltIns: false,
-    targets: "> 0.25%, not dead"
-  });
+  .js("resources/js/app.js", "public/js");
+  // .polyfill({
+  //   enabled: true,
+  //   useBuiltIns: false,
+  //   targets: "> 0.25%, not dead"
+  // });
 
 // Monaco AMD modules. Copy only the files we need to make the build faster.
 const monacoSource = "node_modules/monaco-editor/min/vs/";
