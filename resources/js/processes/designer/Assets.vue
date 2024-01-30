117199b6b998fd6802ef77d593cc434154daea98
Fabio <frguachallab@gmail.com>
2024-01-30 13:45:41 +0000

    FOUR-13377: The Sections in Designer Welcome Screens are not aligned

--- resources/js/processes/designer/Assets.vue
+++ resources/js/processes/designer/Assets.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="hasPermission()"
-    class="assets p-3 mb-3"
+    class="assets mb-3"
   >
     <b-navbar type="faded">
       <b-navbar-brand class="text-uppercase">
@@ -164,6 +164,7 @@ export default {
 <style scoped>
 .assets {
   background-color: #f9f9f9;
+  padding: 0rem 1rem 1rem 1rem;
 }
 .b-card {
   margin: 5px;
