534b4d9054efaaf0e4acb53f8848e77a607392b3
Sanja <sanja.cornelius@processmaker.com>
2024-02-14 15:44:34 -0800

    Clean code

--- tests/Feature/Templates/Api/ScreenTemplateTest.php
+++ tests/Feature/Templates/Api/ScreenTemplateTest.php
@@ -5,25 +5,16 @@ namespace Tests\Feature\Templates\Api;
 use Exception;
 use Illuminate\Foundation\Testing\WithFaker;
 use ProcessMaker\Http\Controllers\Api\ExportController;
-use ProcessMaker\ImportExport\Utils;
-use ProcessMaker\Models\Process;
-use ProcessMaker\Models\ProcessCategory;
 use ProcessMaker\Models\Screen;
 use ProcessMaker\Models\ScreenCategory;
 use ProcessMaker\Models\ScreenTemplates;
-use ProcessMaker\Models\Script;
-use ProcessMaker\Models\ScriptCategory;
-use ProcessMaker\Models\Setting;
 use ProcessMaker\Models\User;
-use ProcessMaker\Packages\Connectors\DataSources\Models\DataSourceCategory;
 use Tests\Feature\Shared\RequestHelper;
-use Tests\Feature\Templates\HelperTrait;
 use Tests\TestCase;
 
 class ScreenTemplateTest extends TestCase
 {
     use RequestHelper;
-    use HelperTrait;
     use WithFaker;
 
     public function testCreateScreenTemplate()
