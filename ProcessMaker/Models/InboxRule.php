ce75d52d6f64da1fbd5c5a240429013300ee4c6a
CarliPinell <carlos.pinell@processmaker.com>
2024-02-15 13:04:29 +0000

    Making improvements on inbox_rules model and factory

--- ProcessMaker/Models/InboxRule.php
+++ ProcessMaker/Models/InboxRule.php
@@ -4,10 +4,20 @@ namespace ProcessMaker\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use ProcessMaker\Models\ProcessMakerModel;
-
+use ProcessMaker\Models\ProcessRequestToken;
 class InboxRule extends ProcessMakerModel
 {
     use HasFactory;
 
     protected $table = 'inbox_rules';
+
+    /**
+     * Define the relationship with ProcessRequestToken model
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function task()
+    {
+        return $this->belongsTo(ProcessRequestToken::class, 'process_request_id');
+    }
 }
