cfde4a59f3aa5b360f323acb8375d99884247af7
CarliPinell <carlos.pinell@processmaker.com>
2024-02-16 21:39:32 +0000

    Part 1 improvements

--- ProcessMaker/Jobs/SmartInbox.php
+++ ProcessMaker/Jobs/SmartInbox.php
@@ -8,19 +8,22 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use ProcessMaker\InboxRules\ApplyAction;
+use ProcessMaker\InboxRules\MatchingTasks;
+use ProcessMaker\Models\InboxRule;
 use ProcessMaker\Models\ProcessRequestToken;
 
 class SmartInbox implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $taskId;
+    public $task;
     /**
      * Create a new job instance.
      */
     public function __construct(ProcessRequestToken $task)
     {
-        $this->taskId = $task->id;
+        $this->task = $task;
     }
 
     /**
@@ -28,6 +31,11 @@ class SmartInbox implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $inboxRule = new InboxRule();
+        $matchingTasks = new MatchingTasks($inboxRule);
+        $matchResult = $matchingTasks->check($this->task);
+        if($matchResult){
+            //Here calls ApplyAction Class
+        }
     }
 }
