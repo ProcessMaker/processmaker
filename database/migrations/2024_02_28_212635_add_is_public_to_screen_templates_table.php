9d684a804054466850576eb7b909a51261a43545
Sanja <sanja.cornelius@processmaker.com>
2024-02-29 09:09:00 -0800

    Resolve syntax error

--- database/migrations/2024_02_28_212635_add_is_public_to_screen_templates_table.php
+++ database/migrations/2024_02_28_212635_add_is_public_to_screen_templates_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('screen_templates', function (Blueprint $table) {
-            $table->boolean('is_public', false)->after('manifest');
+            $table->boolean('is_public')->default('false')->after('manifest');
         });
     }
 
