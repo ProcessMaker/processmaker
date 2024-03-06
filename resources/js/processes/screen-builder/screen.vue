dbf5a4f366934ef9af608470308db493476b9dbf
David Callizaya <davidcallizaya@gmail.com>
2024-03-06 06:35:55 -0400

    Adjust screen dropdown styles

--- resources/js/processes/screen-builder/screen.vue
+++ resources/js/processes/screen-builder/screen.vue
@@ -1239,14 +1239,18 @@ body {
   background-color: #ffff;
   color: #6a7888;
   padding: 8px 8px 2px 8px;
+  font-size: 1rem !important;
 }
 .btn-platform:hover {
   color: #6a7888;
 }
 .page-dropdown-menu {
   min-width: 333px;
+  max-height: 26rem;
+  overflow-y: auto;
+  scrollbar-width: thin;
   .dropdown-item {
-   font-size: 14px !important; 
+   font-size: 1rem !important;
   };
 }
 </style>
