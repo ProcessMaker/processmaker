4b02611642b9ad8952bd9948be7fb9db9fc17cc0
Agustín Busso <90727999+agustinbusso@users.noreply.github.com>
2024-03-07 13:59:59 -0300

    Disable scale in mobile

--- resources/views/layouts/ai-qr-mobile.blade.php
+++ resources/views/layouts/ai-qr-mobile.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Security-Policy" content="script-src * 'unsafe-inline' 'unsafe-eval'; object-src 'self';">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="app-url" content="{{ config('app.url') }}">
     <meta name="open-ai-nlq-to-pmql" content="{{ config('app.open_ai_nlq_to_pmql') }}">
