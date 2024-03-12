51ad9458ec680f12585b26e0e62a910e4961d47f
Ryan Cooley <ryancooley@users.noreply.github.com>
2024-03-11 18:58:48 -0700

    Revert "FOUR-13424 Decision Tables v3"

--- config/filesystems.php
+++ config/filesystems.php
@@ -114,12 +114,6 @@ return [
             'root' => storage_path() . '/samlidp',
         ],
 
-        'decision_tables' => [
-            'driver' => 'local',
-            'root' => storage_path('decision-tables'),
-            'url' => env('APP_URL') . '/storage/decision-tables',
-            'visibility' => 'private',
-        ],
     ],
 
     /*
