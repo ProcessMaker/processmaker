efd3935ae261fd3d1a2f4c1f387b0c5cfeb005b7
Luciana Nunez <luciana.nunez@processmaker.com>
2024-03-13 09:28:33 -0400

    feature/FOUR-14385

--- resources/js/processes-catalogue/components/utils/SearchCards.vue
+++ resources/js/processes-catalogue/components/utils/SearchCards.vue
@@ -1,31 +1,27 @@
 <template>
-  <div>
-    <b-input-group>
-      <b-input-group-prepend>
-        <b-btn
-          variant="outline-secondary"
-          :title="$t('Search Processes')"
-          @click="fetch()"
-        >
-          <i class="fas fa-search search-icon" />
-        </b-btn>
-      </b-input-group-prepend>
-      <b-form-input
-        id="search-box"
-        v-model="filter"
-        :placeholder="$t('Search Processes')"
-        @keyup.enter="fetch()"
-      />
-      <b-input-group-append v-if="filter">
-        <b-btn
-          class="px-1"
-          variant="outline-secondary"
-          @click="clearSearch()"
-        >
-          <b-icon icon="x" />
-        </b-btn>
-      </b-input-group-append>
-    </b-input-group>
+  <div class="search-group">
+    <b-btn
+      class="btn btn-input"
+      :title="$t('Search Processes')"
+      @click="fetch()"
+    >
+      <i class="fas fa-search search-icon" />
+    </b-btn>
+    <b-form-input
+      id="search-box"
+      v-model="filter"
+      :placeholder="$t('Search Processes')"
+      @keyup.enter="fetch()"
+    />
+    <div v-if="filter">
+      <b-btn
+        class="px-1 btn btn-input"
+        v-b-tooltip.hover.bottom="$t('Clear Search')"
+        @click="clearSearch()"
+      >
+        <b-icon class="icon-close" icon="x" />
+      </b-btn>
+    </div>
   </div>
 </template>
 
@@ -50,20 +46,42 @@ export default {
 </script>
 
 <style scoped>
-.btn-outline-secondary {
+.btn-input {
   font-size: 20px;
 }
-.btn-outline-secondary,
+.search-group {
+  display: flex;
+  align-items: center;
+}
+.btn-input,
 #search-box {
   border: none;
   background-color: #f7f9fb;
 }
 #search-box {
-  color: #B9B9B9;
   font-size: 19px;
   font-style: normal;
   font-weight: 400;
   line-height: 32.077px;
   letter-spacing: -0.38px;
+}
+.form-control:focus {
+  border: none !important;
+  box-shadow: none;
+}
+.btn-input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  font-size: 16px;
+  color: #6a7888;
+  border-radius: 4px;
+  width: 24px;
+  height: 24px;
+}
+.icon-close {
+  width: 24px;
+  height: 24px;
 }
 </style>
