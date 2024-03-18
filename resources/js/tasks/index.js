320bd527daabb6de1d1f7ea4e115fe63837bf664
Nolan Ehrstrom <github@nolanpro.com>
2024-03-18 14:27:57 -0700

    Indent

--- resources/js/tasks/index.js
+++ resources/js/tasks/index.js
@@ -49,7 +49,7 @@ new Vue({
     ProcessMaker.EventBus.$on('advanced-search-addition', (component) => {
       this.additions.push(component);
     });
-  this.onInbox();
+    this.onInbox();
   },
   created() {
     const params = new URL(document.location).searchParams;
