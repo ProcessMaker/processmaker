3d5c6a765f510b4320a2f983aa38823d9df0f647
Paula Quispe <paula.quispe@processmaker.com>
2024-03-19 12:55:35 -0400

    FOUR-14478

--- resources/js/processes-catalogue/components/ProcessesCarousel.vue
+++ resources/js/processes-catalogue/components/ProcessesCarousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-block">
+  <div class="carousel-container">
     <b-carousel
       id="processes-carousel"
       v-model="slide"
@@ -18,7 +18,7 @@
           <img
             :src="image.url"
             :alt="process.name"
-            class="d-block w-100 img-carousel"
+            class="d-block img-carousel"
           >
         </template>
       </b-carousel-slide>
@@ -112,7 +112,12 @@ export default {
   height: 400px;
 }
 .img-carousel {
-  max-width: 100%;
-  height: inherit;
+  max-width: 800px;
+  height: 400px;
+}
+.carousel-container {
+  display: flex;
+  justify-content: center;
+  background-color: #edf1f6;
 }
 </style>
