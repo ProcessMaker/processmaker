28e97873128e49633b2de7cca7aa95b508b6eba9
CarliPinell <carlos.pinell@processmaker.com>
2024-03-22 15:39:07 +0000

    Solving QA observation

--- resources/js/inbox-rules/components/Index.vue
+++ resources/js/inbox-rules/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h4>{{$t('Inbox Rules')}}</h4>
+    <div class="title-style ml-3">{{$t('Inbox Rules')}}</div>
     <b-tabs class="m-3" content-class="p-3 pm-tab-content">
       <b-tab :title="$t('Rules')" @click="$refs.inboxRules.requestData()"
              active>
@@ -32,5 +32,10 @@
     border-bottom: 1px solid #dee2e6;
     border-left: 1px solid #dee2e6;
     background-color: white;
+  }
+
+  .title-style {
+    font-size: 27px;
+    color: #556271;
   }
 </style>
