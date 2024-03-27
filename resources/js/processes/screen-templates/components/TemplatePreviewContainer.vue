49d5d3f5e61ac36c0dbc724ddea2fd93ad55b93c
Teisha McRae <mcraeteisha@gmail.com>
2024-03-27 15:27:02 -0400

    Create TemplatePreviewContainer component

---
+++ resources/js/processes/screen-templates/components/TemplatePreviewContainer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <splitpane-container
+      :size="splitpaneSize"
+    >
+      <div
+        id="template-preview"
+        ref="template-preview"
+        class="h-100 p-3"
+      >
+        <div>
+          <div class="d-flex w-100 h-100 mb-3">
+            <slot name="header" :close="onClose" :templateId="template.id">
+              <div class="ml-auto mr-0 text-right">
+                <b-button
+                  class="btn-light text-secondary"
+                  :aria-label="$t('Close')"
+                  @click="onClose()"
+                >
+                  <i class="fas fa-times" />
+                </b-button>
+              </div>
+            </slot>
+          </div>
+          <preview-template
+            v-if="showTemplatePreview"
+            ref="preview"
+            :template="getTemplate(templateId)"
+            @hide-template-preview="hidePreview"
+            @mark-selected-row="markSelectedRow"
+          />
+        </div>
+      </div>
+    </splitpane-container>
+  </div>
+</template>
+
+<script>
+import SplitpaneContainer from "../../../tasks/components/SplitpaneContainer.vue";
+import PreviewTemplate from "../../../components/templates/PreviewTemplate.vue";
+import templatePreviewMixin from "../mixins/templatePreviewMixin";
+
+export default {
+  components: {
+    SplitpaneContainer,
+    PreviewTemplate,
+  },
+  mixins: [templatePreviewMixin],
+}
+</script>
