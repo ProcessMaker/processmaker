a5043532778736dee26ac8953c6a8708d7d53780
Sanja <sanja.cornelius@processmaker.com>
2024-03-28 09:33:43 -0700

    SonarQube Fix: Move `handleEllipsisClick` to fieldsMixin to resolve duplicate code issue

--- resources/js/processes/screen-templates/components/MyTemplatesListing.vue
+++ resources/js/processes/screen-templates/components/MyTemplatesListing.vue
@@ -213,36 +213,6 @@ export default {
           this.loading = false;
         });
     },
-    handleEllipsisClick(templateColumn) {
-      this.fields.forEach(column => {
-        if (column.field !== templateColumn.field) {
-          column.direction = "none";
-          column.filterApplied = false;
-        }
-      });
-
-      if (templateColumn.direction === "asc") {
-        templateColumn.direction = "desc";
-      } else if (templateColumn.direction === "desc") {
-        templateColumn.direction = "none";
-        templateColumn.filterApplied = false;
-      } else {
-        templateColumn.direction = "asc";
-        templateColumn.filterApplied = true;
-      }
-
-      if (templateColumn.direction !== "none") {
-        const sortOrder = [
-          {
-            sortField: templateColumn.sortField || templateColumn.field,
-            direction: templateColumn.direction,
-          },
-        ];
-        this.dataManager(sortOrder);
-      } else {
-        this.fetch();
-      }
-    },
   },
 };
 </script>
--- resources/js/processes/screen-templates/mixins/fieldsMixin.js
+++ resources/js/processes/screen-templates/mixins/fieldsMixin.js
@@ -49,5 +49,35 @@ export default {
         this.commonFields.splice(index + 1, 0, newField);
       }
     },
+    handleEllipsisClick(templateColumn) {
+      this.fields.forEach(column => {
+        if (column.field !== templateColumn.field) {
+          column.direction = "none";
+          column.filterApplied = false;
+        }
+      });
+
+      if (templateColumn.direction === "asc") {
+        templateColumn.direction = "desc";
+      } else if (templateColumn.direction === "desc") {
+        templateColumn.direction = "none";
+        templateColumn.filterApplied = false;
+      } else {
+        templateColumn.direction = "asc";
+        templateColumn.filterApplied = true;
+      }
+
+      if (templateColumn.direction !== "none") {
+        const sortOrder = [
+          {
+          sortField: templateColumn.sortField || templateColumn.field,
+          direction: templateColumn.direction,
+          },
+        ];
+        this.dataManager(sortOrder);
+      } else {
+        this.fetch();
+      }
+    },
   },
 };
