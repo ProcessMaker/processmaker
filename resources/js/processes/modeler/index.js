e921968ec0a486cc6b00f436cc50260ba0cf07e3
David Callizaya <davidcallizaya@gmail.com>
2024-03-30 14:09:48 -0400

    Fix lint issue

--- resources/js/processes/modeler/index.js
+++ resources/js/processes/modeler/index.js
@@ -5,7 +5,7 @@ window.ProcessMaker.i18nPromise.then(() => {
   new Vue({
     render: (h) => h(ModelerApp, {
       props: {
-        showToolbar: !(window.document.getElementById("modeler-app").getAttribute("show-toolbar") === "false"),
+        showToolbar: window.document.getElementById("modeler-app").getAttribute("show-toolbar") !== "false",
       },
     }),
   }).$mount("#modeler-app");
