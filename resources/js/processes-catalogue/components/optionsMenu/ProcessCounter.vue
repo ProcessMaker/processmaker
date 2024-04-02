06cc78b9fce8a74a5e51730b427bf061eb45c59b
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-04-02 14:05:00 -0400

    feature/FOUR-14723

--- resources/js/processes-catalogue/components/optionsMenu/ProcessCounter.vue
+++ resources/js/processes-catalogue/components/optionsMenu/ProcessCounter.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="mt-4">
-    <div class="process-counter p-3">
-      <span class="process-counter-total"> {{ count }} </span>
-      <span class="process-counter-text">{{ $t('Requests of process') }} </span>
+  <div class="process-counter">
+    <div>
+      <p class="process-counter-text">{{ $t('Cases Started') }} </p>
+      <p class="process-counter-total"> {{ count }} </p>
     </div>
+    <img
+      class="process-counter-image"
+      src="/img/launchpad-images/iconCounter.svg"
+      alt="icon Counter"
+    >
   </div>
 </template>
 <script>
@@ -33,26 +38,38 @@
 </script>
 <style scoped>
 .process-counter {
-  width: 294px;
-  height: 52px;
-  border-radius: 8px;
-  border: 1px solid #CDDDEE;
-  background: #FFF;
-}
-.process-counter-text {
-  color: #556271;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px;
-  letter-spacing: -0.32px;
+  display: flex;
+  justify-content: space-between;
+  width: 250px;
+  height: 111px;
+  padding: 22px 24px;
+  border-radius: 16px;
+  border: 1px solid #daebf7;
+  background: #e7f9ff;
 }
 .process-counter-total {
   color: #556271;
-  font-size: 21px;
-  font-style: normal;
+  margin: 0px;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 32px;
   font-weight: 700;
-  line-height: 24px;
-  letter-spacing: -0.42px;
+  line-height: 43.58px;
+  letter-spacing: -0.02em;
+  text-align: left;
+}
+.process-counter-text {
+  color: #556271;
+  margin: 0px;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 19.07px;
+  letter-spacing: -0.02em;
+  text-align: left;
+
+}
+.process-counter-image {
+  width: 56px;
+  height: 56px;
 }
 </style>
