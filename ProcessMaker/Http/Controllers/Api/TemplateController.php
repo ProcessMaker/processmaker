0cf8c3246a3159606a37062c23b1d7f541a7acca
Esteban Gallego <egallego00@gmail.com>
2024-04-03 12:48:19 -0400

    Removing validation on CreateProcess from Templates

--- ProcessMaker/Http/Controllers/Api/TemplateController.php
+++ ProcessMaker/Http/Controllers/Api/TemplateController.php
@@ -24,8 +24,8 @@ class TemplateController extends Controller
     use ProjectAssetTrait;
 
     protected array $types = [
-        'process' => [Process::class, ProcessTemplate::class, ProcessCategory::class, 'process_category_id', 'process_templates'],
-        'screen' => [Screen::class, ScreenTemplate::class, ScreenCategory::class, 'screen_category_id', 'screen_templates'],
+        'process' => [Process::class, ProcessTemplates::class, ProcessCategory::class, 'process_category_id', 'process_templates'],
+        'screen' => [Screen::class, ScreenTemplates::class, ScreenCategory::class, 'screen_category_id', 'screen_templates'],
     ];
 
     private $template;
@@ -254,7 +254,6 @@ class TemplateController extends Controller
 
     protected function createProcess(Request $request)
     {
-        $request->validate(Process::rules($request->id));
         $postOptions = $this->checkIfAssetsExist($request);
 
         if (!empty($postOptions)) {
