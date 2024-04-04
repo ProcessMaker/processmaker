f8056e841ac5ceb4bdee82af7549e34c51c8ca1d
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-04-04 17:10:42 -0400

    feature/FOUR-14773

--- resources/views/processes-catalogue/index.blade.php
+++ resources/views/processes-catalogue/index.blade.php
@@ -12,7 +12,6 @@
   <div class="px-3 page-content mb-0" id="processes-catalogue">
     <processes-catalogue
       :process="{{$process ?? 0}}"
-      :launchpad="{{$launchpad ?? 0}}"
       :current-user-id="{{ \Auth::user()->id }}"
       :permission="{{ \Auth::user()->hasPermissionsFor('processes', 'process-templates', 'pm-blocks', 'projects') }}"
       :current-user="{{ \Auth::user() }}"
