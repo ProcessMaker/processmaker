308ec60821fa1f8d287e45f84f74ffdce4e20a67
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-04-04 14:14:18 -0400

    feature/FOUR-14787

--- ProcessMaker/Http/Controllers/ProcessesCatalogueController.php
+++ ProcessMaker/Http/Controllers/ProcessesCatalogueController.php
@@ -17,7 +17,7 @@ class ProcessesCatalogueController extends Controller
 {
     public function index(Request $request, Process $process = null)
     {
-        $currentUser = \Auth::user();
+        $currentUser = \Auth::user()->only(['id', 'username', 'fullname', 'firstname', 'lastname', 'avatar']);
         return view('processes-catalogue.index', compact('process' , 'currentUser'));
     }
 }
