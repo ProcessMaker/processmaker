9eff1aa25f94b1c1a849d4a2d64c1198846591a4
Sanja <sanja.cornelius@processmaker.com>
2024-04-09 11:16:14 -0700

    Change Make Public to Share Template

--- resources/js/templates/components/ScreenTemplateConfigurations.vue
+++ resources/js/templates/components/ScreenTemplateConfigurations.vue
@@ -73,7 +73,7 @@
                     value="true"
                     unchecked-value="false"
                 >
-                {{ $t('Make Public') }}
+                {{ $t('Share Template') }}
                 </b-form-checkbox>
             </b-form-group>
 
