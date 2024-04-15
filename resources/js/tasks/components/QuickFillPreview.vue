720b363545047a6c42d08776e82306e5cd4f2bac
Roly Gutierrez <roly.gutierrez@processmaker.com>
2024-04-15 17:04:15 -0400

    FOUR-14931 Remove unnecessary changes on file QuickFillPreview.vue.

--- resources/js/tasks/components/QuickFillPreview.vue
+++ resources/js/tasks/components/QuickFillPreview.vue
@@ -3,38 +3,38 @@
     <div
       v-if="propFromButton === 'fullTask'"
       class="header-container"
-      >
+    >
       <span class="quick-fill-text-full">{{ $t("Quick Fill") }}</span>
       <b-button
         class="close-go-back-button"
         @click="cancelAndGoBack()"
-        >
+      >
         {{ $t("Cancel And Go Back") }}
       </b-button>
     </div>
     <div
       v-if="propFromButton === 'inboxRules'"
       class="button-container"
-      >
+    >
       <span class="quick-fill-text">{{ $t("Quick Fill") }}</span>
       <b-button
-        class="close-button-prev button-cancel"
-        @click="$emit('close')"
+          class="close-button-prev button-cancel"
+          @click="$emit('close')"
         >
-        {{ $t("Cancel") }}
-      </b-button>
+          {{ $t("Cancel") }}
+        </b-button>
     </div>
 
-    <div v-if="propFromButton === 'previewTask'" 
-         class="button-container">
-
-      <b-button
-        class="close-button-prev button-cancel"
-        @click="$emit('close')"
+      <div v-if="propFromButton === 'previewTask'" 
+        class="button-container">
+        
+        <b-button
+          class="close-button-prev button-cancel"
+          @click="$emit('close')"
         >
-        {{ $t("Cancel") }}
-      </b-button>
-    </div>
+          {{ $t("Cancel") }}
+        </b-button>
+      </div>
 
 
     <div class="second-container">
@@ -55,7 +55,7 @@
           :advanced-filter-prop="quickFilter"
           :from-button="propFromButton"
           @onWatchShowPreview="onWatchShowPreview"
-          >
+        >
           <template v-slot:preview-header="{ close, screenFilteredTaskData, taskReady }">
             <div v-if="propFromButton === 'inboxRules'" style="width: 98%">
               <div class="header-container-quick">
@@ -68,19 +68,19 @@
                     :disabled="!taskReady"
                     :aria-label="$t('Use This Task Data')"
                     @click="buttonThisData(screenFilteredTaskData)"
-                    >
+                  >
                     <img
                       src="../../../img/smartinbox-images/Stroke.svg"
                       class="img-styles"
                       :alt="$t('No Image')"
-                      />{{ $t("Use This Task Data") }}
+                    />{{ $t("Use This Task Data") }}
                   </b-button>
                 </div>
                 <b-button
                   class="close-button mr-2"
                   variant="link"
                   @click="close()"
-                  >
+                >
                   <i class="fas fa-times" />
                 </b-button>
               </div>
@@ -96,12 +96,12 @@
                     variant="primary"
                     :aria-label="$t('Use This Task Data')"
                     @click="buttonThisData(screenFilteredTaskData)"
-                    >
+                  >
                     <img
                       src="../../../img/smartinbox-images/Stroke.svg"
                       class="img-styles"
                       :alt="$t('No Image')"
-                      />{{ $t("Use This Task Data") }}
+                    />{{ $t("Use This Task Data") }}
                   </b-button>
                   <b-button
                     v-if="propFromButton === 'fullTask'"
@@ -110,19 +110,19 @@
                     variant="primary"
                     :aria-label="$t('Use This Task Data')"
                     @click="buttonThisDataFromFullTask(screenFilteredTaskData)"
-                    >
+                  >
                     <img
                       src="../../../img/smartinbox-images/Stroke.svg"
                       class="img-styles"
                       :alt="$t('No Image')"
-                      />{{ $t("Use This Task Data") }}
+                    />{{ $t("Use This Task Data") }}
                   </b-button>
                 </div>
                 <b-button
                   class="close-button mr-2"
                   variant="link"
                   @click="close()"
-                  >
+                >
                   <i class="fas fa-times" />
                 </b-button>
               </div>
@@ -134,9 +134,9 @@
               class="icon-button"
               :aria-label="$t('Quick fill Preview')"
               variant="light"
-              @click="previewTasks(tooltipRowData, 93, 'previewTask');"
-              >
-              <i class="fas fa-eye" />11
+              @click="previewTasks(tooltipRowData, 93, 'previewTask')"
+            >
+              <i class="fas fa-eye" />
             </b-button>
             <b-button
               v-if="propFromButton === 'fullTask'"
@@ -144,11 +144,11 @@
               :aria-label="$t('Quick fill Preview')"
               variant="light"
               @click="
-              previewTasks(tooltipRowData, 50, 'fullTask');
-              setTask();
+                previewTasks(tooltipRowData, 50, 'fullTask');
+                setTask();
               "
-              >
-              <i class="fas fa-eye" />22
+            >
+              <i class="fas fa-eye" />
             </b-button>
             <b-button
               v-if="propFromButton === 'inboxRules'"
@@ -156,8 +156,8 @@
               :aria-label="$t('Quick fill Preview')"
               variant="light"
               @click="previewTasks(tooltipRowData, 50, 'inboxRules')"
-              >
-              <i class="fas fa-eye" />33
+            >
+              <i class="fas fa-eye" />
             </b-button>
           </template>
         </tasks-list>
@@ -166,132 +166,132 @@
   </div>
 </template>
 <script>
-  export default {
-    props: ["task", "propColumns", "propFilters", "propFromButton"],
-    data() {
-      return {
-        processName: "",
-        selectedRowQuick: 0,
-        fromQuickFill: true,
-        taskData: {},
-        pmql: `(user_id = ${ProcessMaker.user.id} and status="Completed" and process_id=${this.task.process_id})`,
-        quickFilter: null,
-        filter: {
-          order: {by: "id", direction: "desc"},
-          filters: [
-            {
-              subject: {type: "Field", value: "process_id"},
-              operator: "=",
-              value: this.task.process_id,
-            },
-            {
-              subject: {type: "Field", value: "element_id"},
-              operator: "=",
-              value: this.task.element_id,
-            },
-          ],
-        },
-        columns: [
+export default {
+  props: ["task", "propColumns", "propFilters", "propFromButton"],
+  data() {
+    return {
+      processName: "",
+      selectedRowQuick: 0,
+      fromQuickFill: true,
+      taskData: {},
+      pmql: `(user_id = ${ProcessMaker.user.id} and status="Completed" and process_id=${this.task.process_id})`,
+      quickFilter: null,
+      filter: {
+        order: { by: "id", direction: "desc" },
+        filters: [
           {
-            label: "Case #",
-            field: "case_number",
-            filter_subject: {
-              type: "Relationship",
-              value: "processRequest.case_number",
-            },
-            order_column: "process_requests.case_number",
+            subject: { type: "Field", value: "process_id" },
+            operator: "=",
+            value: this.task.process_id,
           },
           {
-            label: "Case title",
-            field: "case_title",
-            name: "__slot:case_number",
-            filter_subject: {
-              type: "Relationship",
-              value: "processRequest.case_title",
-            },
-            order_column: "process_requests.case_title",
-          },
-          {
-            label: "Completed",
-            field: "completed_at",
-            format: "datetime",
-            filter_subject: {
-              type: "Field",
-              value: "completed_at",
-            },
+            subject: { type: "Field", value: "element_id" },
+            operator: "=",
+            value: this.task.element_id,
           },
         ],
-        dataTasks: {}
-      };
-    },
-    mounted() {
-      if (this.propFilters !== "" && !this.propFilters.filters.some(filter => filter.value === null)) {
-        this.quickFilter = this.propFilters;
-      } else {
-        this.quickFilter = this.filter;
-      }
+      },
+      columns: [
+        {
+          label: "Case #",
+          field: "case_number",
+          filter_subject: {
+            type: "Relationship",
+            value: "processRequest.case_number",
+          },
+          order_column: "process_requests.case_number",
+        },
+        {
+          label: "Case title",
+          field: "case_title",
+          name: "__slot:case_number",
+          filter_subject: {
+            type: "Relationship",
+            value: "processRequest.case_title",
+          },
+          order_column: "process_requests.case_title",
+        },
+        {
+          label: "Completed",
+          field: "completed_at",
+          format: "datetime",
+          filter_subject: {
+            type: "Field",
+            value: "completed_at",
+          },
+        },
+      ],
+      dataTasks: {},
+    };
+  },
+  mounted() {
+    if (this.propFilters !== "" && !this.propFilters.filters.some(filter => filter.value === null)) {
+      this.quickFilter = this.propFilters;
+    } else {
+      this.quickFilter = this.filter;
+    }
 
-      if (this.propColumns.length > 0) {
-        this.columns = this.propColumns;
-      }
+    if (this.propColumns.length > 0) {
+      this.columns = this.propColumns;
+    }
+  },
+  methods: {
+    verifyURL(string) {
+      const currentUrl = window.location.href;
+      const isInUrl = currentUrl.includes(string);
+      return isInUrl;
     },
-    methods: {
-      verifyURL(string) {
-        const currentUrl = window.location.href;
-        const isInUrl = currentUrl.includes(string);
-        return isInUrl;
-      },
-      selected(taskData) {},
-      setTask() {
-        this.processName = this.task.process_request.case_title;
-      },
-      cancelAndGoBack() {
-        window.location.href = `/tasks/${this.task.id}/edit`;
-      },
-      buttonThisData(data) {
-        if (this.propFromButton === "inboxRules") {
-          this.$emit("quick-fill-data-inbox", data);
-        } else {
-          this.$emit("quick-fill-data", data);
-        }
-        this.$emit("close");
-      },
-      buttonThisDataFromFullTask(data) {
-        return ProcessMaker.apiClient
-                .put("drafts/" + this.task.id, data)
-                .then((response) => {
-                  this.task.draft = _.merge({}, this.task.draft, response.data);
-                  window.location.href = `/tasks/${this.task.id}/edit`;
-                  ProcessMaker.alert(this.$t("Task Filled successfully."), "success");
-                })
-                .catch((error) => {
-                  console.error("Error", error);
-                });
-      },
-      /*
-       * To do: There's a global-search-bar class with a large z-index. We added a class 
-       * that modifies this class when this panel is displayed. If it's destroyed, we 
-       * remove it to maintain compatibility.
-       */
-      reCalculateZIndex(sw) {
-        let names = [".global-search-bar", ".navbar-nav"];
-        for (let i = 0; i < names.length; i++) {
-          let searchBar = document.querySelector(names[i]);
-          if (searchBar) {
-            let obj = searchBar.classList;
-            if (sw === true) {
-              obj.add("global-search-bar-z-index");
-            } else {
-              obj.remove("global-search-bar-z-index");
-            }
+    selected(taskData) {},
+    setTask() {
+      this.processName = this.task.process_request.case_title;
+    },
+    cancelAndGoBack() {
+      window.location.href = `/tasks/${this.task.id}/edit`;
+    },
+    buttonThisData(data) {
+      if (this.propFromButton === "inboxRules") {
+        this.$emit("quick-fill-data-inbox", data);
+      } else {
+        this.$emit("quick-fill-data", data);
+      }
+      this.$emit("close");
+    },
+    buttonThisDataFromFullTask(data) {
+      return ProcessMaker.apiClient
+        .put("drafts/" + this.task.id, data)
+        .then((response) => {
+          this.task.draft = _.merge({}, this.task.draft, response.data);
+          window.location.href = `/tasks/${this.task.id}/edit`;
+          ProcessMaker.alert(this.$t("Task Filled successfully."), "success");
+        })
+        .catch((error) => {
+          console.error("Error", error);
+        });
+    },
+    /*
+     * To do: There's a global-search-bar class with a large z-index. We added a class 
+     * that modifies this class when this panel is displayed. If it's destroyed, we 
+     * remove it to maintain compatibility.
+     */
+    reCalculateZIndex(sw) {
+      let names = [".global-search-bar", ".navbar-nav"];
+      for (let i = 0; i < names.length; i++) {
+        let searchBar = document.querySelector(names[i]);
+        if (searchBar) {
+          let obj = searchBar.classList;
+          if (sw === true) {
+            obj.add("global-search-bar-z-index");
+          } else {
+            obj.remove("global-search-bar-z-index");
           }
         }
-      },
-      onWatchShowPreview(value) {
-        this.reCalculateZIndex(value);
       }
+    },
+    onWatchShowPreview(value) {
+      this.reCalculateZIndex(value);
     }
-  };
+  },
+};
 </script>
 <style>
   /*To do: Read description of method reCalculateZIndex()*/
@@ -300,176 +300,176 @@
   }
 </style>
 <style scoped>
-  .btn-cancel {
-    background-color: #fff;
-  }
-  .btn-back-quick-fill {
-    color: #888;
-    padding: 0;
-    border: none;
-  }
-  .button-container {
-    display: flex;
-    justify-content: space-between;
-    height: 64px;
-    border: 1px solid #f6f9fb;
-    padding: 0 12px;
-  }
+.btn-cancel {
+  background-color: #fff;
+}
+.btn-back-quick-fill {
+  color: #888;
+  padding: 0;
+  border: none;
+}
+.button-container {
+  display: flex;
+  justify-content: space-between;
+  height: 64px;
+  border: 1px solid #f6f9fb;
+  padding: 0 12px;
+}
 
-  .header-container {
-    display: flex;
-    align-items: center;
-    border: 1px solid #f6f9fb;
-    padding: 0 12px;
-  }
+.header-container {
+  display: flex;
+  align-items: center;
+  border: 1px solid #f6f9fb;
+  padding: 0 12px;
+}
 
-  .header-container-quick {
-    display: flex;
-    justify-content: space-between;
-    border: 1px solid #cdddee;
-    padding: 10px 12px;
-    background-color: #e8f0f9;
-  }
+.header-container-quick {
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid #cdddee;
+  padding: 10px 12px;
+  background-color: #e8f0f9;
+}
 
-  .close-go-back-button {
-    color: #fff;
-    background-color: #6a7888;
-    width: 228px;
-    height: 40px;
-    border-radius: 4px;
-    padding: 0;
-    border: none;
-    margin-left: auto;
-  }
-  .quick-fill-text-full {
-    color: #556271;
-    font-size: 27px;
-  }
-  .quick-fill-text {
-    color: #566877;
-    margin-left: 8px;
-    font-size: 16px;
-  }
+.close-go-back-button {
+  color: #fff;
+  background-color: #6a7888;
+  width: 228px;
+  height: 40px;
+  border-radius: 4px;
+  padding: 0;
+  border: none;
+  margin-left: auto;
+}
+.quick-fill-text-full {
+  color: #556271;
+  font-size: 27px;
+}
+.quick-fill-text {
+  color: #566877;
+  margin-left: 8px;
+  font-size: 16px;
+}
 
-  .button-task {
-    color: #fff;
-    background-color: #1572c2;
-    display: block;
-    width: 100%;
-    margin-top: 10px;
-  }
+.button-task {
+  color: #fff;
+  background-color: #1572c2;
+  display: block;
+  width: 100%;
+  margin-top: 10px;
+}
 
-  .button-cancel {
-    color: #fff;
-    background-color: #6a7888;
-    width: 88px;
-    height: 32px;
-    font-weight: bold;
-  }
+.button-cancel {
+  color: #fff;
+  background-color: #6a7888;
+  width: 88px;
+  height: 32px;
+  font-weight: bold;
+}
 
-  .close-button-prev {
-    color: #fff;
-    padding: 0;
-    border: none;
-  }
+.close-button-prev {
+  color: #fff;
+  padding: 0;
+  border: none;
+}
 
-  .arrow-button,
-  .close-button-prev {
-    color: #fff;
-    padding: 0;
-    border: none;
-  }
+.arrow-button,
+.close-button-prev {
+  color: #fff;
+  padding: 0;
+  border: none;
+}
 
-  .close-button {
-    color: #888;
-    padding: 0;
-    border: none;
-    margin-left: -25px;
-    margin-top: -45px;
-  }
-  .arrow-button,
-  .close-button {
-    color: #888;
-    padding: 0;
-    border: none;
-  }
-  .suggested-task {
-    display: flex;
-    align-items: center;
-    border: 1px solid #f1e4ba;
-    margin: 20px 0;
-    padding: 10px 20px;
-    background-color: #fef7e2;
-    height: 64px;
-  }
+.close-button {
+  color: #888;
+  padding: 0;
+  border: none;
+  margin-left: -25px;
+  margin-top: -45px;
+}
+.arrow-button,
+.close-button {
+  color: #888;
+  padding: 0;
+  border: none;
+}
+.suggested-task {
+  display: flex;
+  align-items: center;
+  border: 1px solid #f1e4ba;
+  margin: 20px 0;
+  padding: 10px 20px;
+  background-color: #fef7e2;
+  height: 64px;
+}
 
-  .suggested-task img {
-    margin-right: 5px;
-  }
+.suggested-task img {
+  margin-right: 5px;
+}
 
-  .suggested-task span {
-    color: #556271;
-  }
-  .content-container {
-    margin: 20px;
-  }
+.suggested-task span {
+  color: #556271;
+}
+.content-container {
+  margin: 20px;
+}
 
-  .custom-table-class {
-    background-color: #fff;
-  }
+.custom-table-class {
+  background-color: #fff;
+}
 
-  .text-container {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin-left: 10px;
-  }
+.text-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 10px;
+}
 
-  .main-text {
-    color: #556271;
-    font-size: 16px;
-  }
+.main-text {
+  color: #556271;
+  font-size: 16px;
+}
 
-  .sub-text {
-    color: #556271;
-    font-size: 14px;
-    margin-left: -30px;
-  }
+.sub-text {
+  color: #556271;
+  font-size: 14px;
+  margin-left: -30px;
+}
 
-  .span-message {
-    background-color: #f6f9fb;
-    font-size: 16px;
-    color: #556271;
-    padding: 10px;
-    margin-top: 5px;
-  }
+.span-message {
+  background-color: #f6f9fb;
+  font-size: 16px;
+  color: #556271;
+  padding: 10px;
+  margin-top: 5px;
+}
 
-  img {
-    align-self: flex-start;
-    margin-right: 2px;
-    margin-top: 3px;
-  }
+img {
+  align-self: flex-start;
+  margin-right: 2px;
+  margin-top: 3px;
+}
 
-  .img-styles {
-    margin-right: 5px;
-    margin-top: -2px;
-  }
+.img-styles {
+  margin-right: 5px;
+  margin-top: -2px;
+}
 
-  .icon-button {
-    color: #888;
-  }
+.icon-button {
+  color: #888;
+}
 
-  .span-text {
-    font-size: 16px;
-    color: #556271;
-    font-weight: bold;
-  }
+.span-text {
+  font-size: 16px;
+  color: #556271;
+  font-weight: bold;
+}
 
-  .second-container {
-    width: 99%;
-  }
+.second-container {
+  width: 99%;
+}
 
-  .third-container {
-    width: 99%;
-  }
+.third-container {
+  width: 99%;
+}
 </style>
