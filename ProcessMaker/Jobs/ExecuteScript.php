f0954dfa5c1c852c1b539e411a2e001837be28bf
David Callizaya <davidcallizaya@gmail.com>
2024-04-19 11:06:23 -0400

    Improve script tasks execution in watchers

--- ProcessMaker/Jobs/ExecuteScript.php
+++ ProcessMaker/Jobs/ExecuteScript.php
@@ -11,6 +11,7 @@ use ProcessMaker\Contracts\ScriptInterface;
 use ProcessMaker\Events\ScriptResponseEvent;
 use ProcessMaker\Models\Script;
 use ProcessMaker\Models\User;
+use ProcessMaker\Nayra\Managers\WorkflowManagerRabbitMq;
 use Throwable;
 
 class ExecuteScript implements ShouldQueue
@@ -62,7 +63,10 @@ class ExecuteScript implements ShouldQueue
      */
     public function handle()
     {
-        //throw new \Exception('This method must be overridden.');
+        $useNayraDocker = true;
+        if ($useNayraDocker && $this->sync) {
+            return $this->handleNayraDocker();
+        }
         try {
             // Just set the code but do not save the object (preview only)
             $this->script->code = $this->code;
@@ -80,6 +84,32 @@ class ExecuteScript implements ShouldQueue
                 'message' => $exception->getMessage(),
             ]);
         }
+    }
+
+    public function handleNayraDocker()
+    {
+        $engine = new WorkflowManagerRabbitMq();
+        $params = [
+            'name' => uniqid('script_', true),
+            'script' => $this->code,
+            'data' => $this->data,
+            'config' => $this->configuration,
+            'envVariables' => $engine->getEnvironmentVariables(),
+        ];
+        $body = json_encode($params);
+        // @todo config the pod address
+        $url = 'http://intembeko-nayra-svc.intembeko-ns-pm4.svc.cluster.local/run_script';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($body),
+        ]);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
     }
 
     /**
