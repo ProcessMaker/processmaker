21c4a703dfbd8c2c26f6a35ef8a7cdfb3af1397f
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-05-06 15:18:24 -0400

    bugfix/FOUR-15393

--- resources/js/processes/designer/RecentAssets.vue
+++ resources/js/processes/designer/RecentAssets.vue
@@ -118,7 +118,7 @@ export default {
     },
     showDropdown() {
       this.hideDropdown = !this.hideDropdown ;
-      performSearch();
+      this.performSearch();
     },
     /**
      * This boolean method shows or hide elements
