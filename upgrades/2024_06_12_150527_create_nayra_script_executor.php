c90dd090b87b9e97e132e65ba6a6673d1592bc6f
David Callizaya <davidcallizaya@gmail.com>
2024-06-12 13:55:50 -0400

    Add missing down method

--- upgrades/2024_06_12_150527_create_nayra_script_executor.php
+++ upgrades/2024_06_12_150527_create_nayra_script_executor.php
@@ -1,12 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
+use ProcessMaker\Models\Script;
 use ProcessMaker\Models\ScriptExecutor;
 use ProcessMaker\ScriptRunners\Base;
 use ProcessMaker\Upgrades\UpgradeMigration as Upgrade;
 
 class CreateNayraScriptExecutor extends Upgrade
 {
-
     /**
      * Run the upgrade migration.
      *
@@ -20,6 +21,25 @@ class CreateNayraScriptExecutor extends Upgrade
             $scriptExecutor->language = Base::NAYRA_LANG;
             $scriptExecutor->title = 'Nayra (µService)';
             $scriptExecutor->save();
+        }
+    }
+
+    /**
+     * Reverse the upgrade migration.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        try {
+            $existsScriptsUsingNayra = Script::where('language', Base::NAYRA_LANG)->exists();
+            if (!$existsScriptsUsingNayra) {
+                ScriptExecutor::where('language', Base::NAYRA_LANG)->delete();
+            } else {
+                Log::error('There are scripts using Nayra, so the Nayra script executor cannot be deleted.');
+            }
+        } catch (Exception $e) {
+            Log::error('Cannot delete Nayra script executor: ' . $e->getMessage());
         }
     }
 }
