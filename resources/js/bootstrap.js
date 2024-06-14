b8f822bfd7677dfb22e8216ff65b115b63855dbe
David Callizaya <davidcallizaya@gmail.com>
2024-06-14 17:28:35 -0400

    Improve the code to setup multiple version

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -232,18 +232,30 @@ if (token) {
   console.error("CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token");
 }
 
+// Setup api versions
+const apiVersionConfig = [
+  { version: "1.0", baseURL: "/api/1.0/" },
+  { version: "1.1", baseURL: "/api/1.1/" },
+];
+
 window.ProcessMaker.apiClient.defaults.baseURL = "/api/1.0/";
-// If requested url starts with "/api/1.1/" use "/api/1.1/"
 window.ProcessMaker.apiClient.interceptors.request.use((config) => {
-  if (config.url.startsWith("/api/1.1/")) {
-    config.baseURL = "/api/1.1/";
-    config.url = config.url.replace("/api/1.1/", "");
-  } else {
-    config.baseURL = "/api/1.0/";
+  if (typeof config.url !== "string" || !config.url) {
+    throw new Error("Invalid URL in the request configuration");
   }
+
+  apiVersionConfig.forEach(({ version, baseURL }) => {
+    const versionPrefix = `/api/${version}/`;
+    if (config.url.startsWith(versionPrefix)) {
+      // eslint-disable-next-line no-param-reassign
+      config.baseURL = baseURL;
+      // eslint-disable-next-line no-param-reassign
+      config.url = config.url.replace(versionPrefix, "");
+    }
+  });
+
   return config;
 });
-
 
 // Set the default API timeout
 let apiTimeout = 5000;
