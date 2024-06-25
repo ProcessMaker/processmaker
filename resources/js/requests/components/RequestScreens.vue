e2a9a75225c44817a106640a6a11e4413c24de01
Marco A. Nina <marco.antonio.nina@processmaker.com>
2024-06-25 16:56:16 -0400

    Add call api

--- resources/js/requests/components/RequestScreens.vue
+++ resources/js/requests/components/RequestScreens.vue
@@ -1,53 +1,52 @@
 <template>
   <div class="data-table">
-    <div>
-      <vuetable
-        :dataManager="dataManager"
-        :noDataTemplate="$t('No Data Available')"
-        :sortOrder="sortOrder"
-        :css="css"
-        :api-mode="false"
-        @vuetable:pagination-data="onPaginationData"
-        :fields="fields"
-        :data="data"
-        data-path="data"
-        detail-row-component="screen-detail"
-        @vuetable:cell-clicked="previewScreen"
-        ref="screens"
-        pagination-path="meta">
+    <vuetable
+      :dataManager="dataManager"
+      :noDataTemplate="$t('No Data Available')"
+      :sortOrder="sortOrder"
+      :css="css"
+      :api-mode="false"
+      @vuetable:pagination-data="onPaginationData"
+      :fields="fields"
+      :data="data"
+      data-path="data"
+      detail-row-component="screen-detail"
+      @vuetable:cell-clicked="previewScreen"
+      ref="screens"
+      pagination-path="meta">
 
-        <template slot="actions" slot-scope="props">
-          <div class="actions">
-            <div class="popout">
-              <b-btn
-                variant="link"
-                @click="previewScreen(props.rowData)"
-                v-b-tooltip.hover
-                :title="$t('Details')">
-                <i v-if="!props.rowData.view" class="fas fa-search-plus fa-lg fa-fw"></i>
-                <i v-else class="fas fa-search-minus fa-lg fa-fw"></i>
-              </b-btn>
-              <b-btn
-                variant="link"
-                @click="preview(props.rowData)"
-                v-b-tooltip.hover
-                :title="$t('Print')">
-                <i class="fas fa-print fa-lg fa-fw"></i>
-              </b-btn>
-            </div>
+      <template slot="actions" slot-scope="props">
+        <div class="actions">
+          <div class="popout">
+            <b-btn
+              variant="link"
+              @click="previewScreen(props.rowData)"
+              v-b-tooltip.hover
+              :title="$t('Details')">
+              <i v-if="!props.rowData.view" class="fas fa-search-plus fa-lg fa-fw"></i>
+              <i v-else class="fas fa-search-minus fa-lg fa-fw"></i>
+            </b-btn>
+            <b-btn
+              variant="link"
+              @click="preview(props.rowData)"
+              v-b-tooltip.hover
+              :title="$t('Print')">
+              <i class="fas fa-print fa-lg fa-fw"></i>
+            </b-btn>
           </div>
-        </template>
-      </vuetable>
-      <pagination
-        single="Screen"
-        plural="Screens"
-        :perPageSelectEnabled="true"
-        @changePerPage="changePerPage"
-        @vuetable-pagination:change-page="onPageChange"
-        ref="pagination">
-      </pagination>
-    </div>
+        </div>
+      </template>
+    </vuetable>
+    <pagination
+      :single="$t('Screen')"
+      :plural="$t('Screens')"
+      :perPageSelectEnabled="true"
+      @changePerPage="changePerPage"
+      @vuetable-pagination:change-page="onPageChange"
+      ref="pagination">
+    </pagination>
   </div>
+
 </template>
 
 <script>
@@ -59,10 +58,12 @@
 
   export default {
     mixins: [datatableMixin],
-    props: ["id", "information", "permission", "screens"],
+    props: ["id", "information", "permission"],
     data() {
       return {
-        orderBy: "title",
+        orderBy: "completed_at",
+        screens: [],
+        filter: '',
         dupScreen: {
           title: "",
           description: ""
@@ -93,7 +94,9 @@
         ]
       };
     },
-
+    mounted() {
+      this.fetch();
+    },
     methods: {
       preview(data) {
         window.open('/requests/' + this.id + '/task/' + data.id + '/screen/' + data.screen_id);
@@ -103,12 +106,41 @@
         this.$refs.screens.toggleDetailRow(data.id);
       },
       fetch() {
-        this.screens.forEach(item => {
-          item.view = false;
-          return item;
+        Vue.nextTick(() => {
+          let endpoint = `/requests/${this.id}/details-screen-request`;
+          // Load from our api client
+          ProcessMaker.apiClient
+            .get(
+              `${endpoint}?page=` +
+              this.page +
+              "&per_page=" +
+              this.perPage +
+              "&filter=" +
+              this.filter +
+              "&order_by=" +
+              this.orderBy +
+              "&order_direction=asc" 
+            )
+            .then((response) => {
+              this.data = this.transform(response.data);
+              this.screens = this.data.data;
+              this.screens.forEach(item => {
+                item.view = false;
+                return item;
+              });
+              
+            }).catch((error) => {
+              this.data = [];
+              if (_.has(error, 'response.data.message')) {
+                ProcessMaker.alert(error.response.data.message, 'danger');
+              } else if (_.has(error, 'response.data.error')) {
+                return;
+              } else {
+                throw error;
+              }
+            });
         });
-        this.data = this.screens;
-      }
+      },
     },
   };
 </script>
