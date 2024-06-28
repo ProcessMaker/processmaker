cc8151cf94f4a739f1daf11d2ee302dfcfeb5e72
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-06-28 17:45:16 -0400

    feature/FOUR-15823-Test

--- tests/Feature/Api/TaskActionByEmailTest.php
+++ tests/Feature/Api/TaskActionByEmailTest.php
@@ -19,7 +19,7 @@ class TaskActionByEmailTest extends TestCase
     {
         // Create a new process
         $this->process = Process::factory()->create();
-        $this->process->bpmn = file_get_contents($file);
+        $this->process->bpmn = $file;
 
         $task_uid = 'UserTaskUID';
         $this->task = $this->process->getDefinitions()->getActivity($task_uid);
