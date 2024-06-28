92fc0bdb83c9e7bd690c5acbda671eede62e84d2
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-06-28 17:23:58 -0400

    feature/FOUR-15823-Test

---
+++ tests/Feature/Api/TaskActionByEmailTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature\Processes;
+
+use ProcessMaker\Models\Process;
+use ProcessMaker\Models\User;
+use Tests\Feature\Shared\RequestHelper;
+use Tests\TestCase;
+
+class TaskActionByEmailTest extends TestCase
+{
+    use RequestHelper;
+
+
+    /**
+     * Create new task assignment type user successfully
+     */
+    private function loadTestProcessUserAssignment($file)
+    {
+        // Create a new process
+        $this->process = Process::factory()->create();
+        $this->process->bpmn = file_get_contents(__DIR__ . '/processes/SingleTask.bpmn');
+
+        $task_uid = 'UserTaskUID';
+        $this->task = $this->process->getDefinitions()->getActivity($task_uid);
+        $this->assigned = User::factory()->create([
+            'id' => $this->task->getProperty('assignedUsers'),
+            'status' => 'ACTIVE',
+        ]);
+        $this->process->save();
+    }
+
+    /**
+     * Execute a process with an empty Action by Email
+     */
+    public function testProcessWithEmptyActionByEmail()
+    {
+        $this->loadTestProcessUserAssignment(file_get_contents(__DIR__ . '/processes/TaskWithActionByEmailEmpty.bpmn'));
+
+        //Start a process request
+        $route = route('api.process_events.trigger',
+            [$this->process->id, 'event' => 'StartEventUID']);
+        $data = [];
+        $response = $this->apiCall('POST', $route, $data);
+
+        //Verify status
+        $response->assertStatus(201);
+    }
+
+    /**
+     * Execute a process with an Action by Email
+     */
+    public function testProcessWithActionByEmail()
+    {
+        $this->loadTestProcessUserAssignment(file_get_contents(__DIR__ . '/processes/TaskWithActionByEmail.bpmn'));
+
+        //Start a process request
+        $route = route('api.process_events.trigger',
+            [$this->process->id, 'event' => 'StartEventUID']);
+        $data = [];
+        $response = $this->apiCall('POST', $route, $data);
+
+        //Verify status
+        $response->assertStatus(201);
+    }
+}
