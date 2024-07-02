ab27da9125e9738a2008003e0af30ddbe6770eff
Sanja <sanja.cornelius@processmaker.com>
2024-07-02 12:06:23 -0700

    Implement new guided template slide

--- resources/js/components/templates/WizardTemplateDetails.vue
+++ resources/js/components/templates/WizardTemplateDetails.vue
@@ -2,39 +2,38 @@
   <div>
     <modal
       id="wizardTemplateDetails"
-      class="wizard-template-modal"
+      :content-class="!showHelperProcess ? 'first_slide' : ''"
       size="huge"
       :hide-footer="true"
       @close="close"
     >
       <b-row v-if="!showHelperProcess">
+        <b-col class="wizard-template-carousel col-5">
+          <b-carousel fade :interval="slideInterval">
+            <b-carousel-slide v-for="(image, index) in templateSlides" :key="index" :img-src="image"/>
+          </b-carousel>
+        </b-col>
         <b-col>
           <div class="wizard-details-container text-left mb-3">
-            <span>
-              <img
-                :src="templateIcon"
-                :alt="template.name + ' icon'"
-                width="45px"
-                class="mb-3 d-block"
-              >
-            </span>
-            <span>
-              <h5 class="text-uppercase mb-1 d-inline-block font-weight-bold template-name">{{ template.name | str_limit(30) }}</h5>
-            </span>
-            <div class="wizard-details-text">
-              <h2 class="wizard-details-headline">{{ templateDetails['modal-excerpt'] | str_limit(150) }}</h2>
-              <p class="wizard-details-description">{{ templateDetails['modal-description'] | str_limit(150) }}</p>
-              <button class="wizard-details-button text-uppercase"  @click.prevent="getHelperProcessStartEvent('wizard-details-modal')">
+            <div class="wizard-details-text pl-3">
+              <h1 class="mb-3 d-inline-block font-weight-bold template-name">{{ templateDetails['modal-title']| str_limit(30) }}</h1>
+              <h3 class="wizard-details-headline text-white">{{ templateDetails['modal-excerpt'] | str_limit(150) }}</h3>
+              <div v-for="item in templateModalDescriptionItems" class="mb-2 wizard-details-description text-white">
+                <span>
+                  <img
+                    :src="templateIcon"
+                    :alt="template.name + ' icon'"
+                    width="45px"
+                  >
+                </span> 
+                {{ item | str_limit(150) }}
+              </div>
+              <button class="mt-5 wizard-details-button text-uppercase"  @click.prevent="getHelperProcessStartEvent('wizard-details-modal')">
                 <i class="fas fa-play-circle mr-1" />
                 {{ $t('Get Started') }}
               </button>
             </div>
           </div>
-        </b-col>
-        <b-col>
-          <b-carousel fade :interval="slideInterval">
-            <b-carousel-slide v-for="(image, index) in templateSlides" :key="index" :img-src="image"/>
-          </b-carousel>
         </b-col>
       </b-row>
       <task
@@ -84,6 +83,9 @@ export default {
     templateSlides() {
       return this.template?.template_media?.slides;
     },
+    templateModalDescriptionItems() {
+      return this.templateDetails['modal-description'].split(';');
+    },
     slideInterval() {
       return Object.keys(this.template?.template_media?.slides).length > 1 ? 3000 : 0;
     }
@@ -116,7 +118,8 @@ export default {
   font-weight: 400;
 }
 .wizard-details-description {
-  font-size: 16px;
+  font-size: 19px;
+  line-height: 3px;
 }
 
 .wizard-details-headline {
