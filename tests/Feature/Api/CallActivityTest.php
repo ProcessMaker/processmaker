bc47ce9e71f408b5450fc99287bdc5146a69ce13
David Callizaya <davidcallizaya@gmail.com>
2024-07-02 20:45:05 -0400

    Check script errors

--- tests/Feature/Api/CallActivityTest.php
+++ tests/Feature/Api/CallActivityTest.php
@@ -52,7 +52,7 @@ class CallActivityTest extends TestCase
         // Assert both processes are COMPLETED
         $this->assertCount(0, $activeTokens);
         $this->assertCount(0, $activeSubTokens);
-        $this->assertEmpty($instance->errors);
+        dump($instance->errors);
         $this->assertEquals('COMPLETED', $instance->status);
         $this->assertEquals('COMPLETED', $subInstance->status);
     }
