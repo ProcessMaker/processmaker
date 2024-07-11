a9fa17d7c1e1d9334196ebe779bb3e6ca3a35f25
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-07-11 16:06:21 -0400

    feature/FOUR-16596

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -14,16 +14,44 @@ class TaskControllerTest extends TestCase
 
     public function testShowScreen()
     {
-        $processAbeRequest = ProcessAbeRequestToken::factory()->create();
-        $process = Process::find($processAbeRequest->process_id);
+        // $bpmn = file_get_contents(__DIR__ . '/../../Fixtures/rollback_test.bpmn');
+        // $bpmn = str_replace('[task_user_id]', $this->user->id, $bpmn);
+        // $screen = Screen::factory()->create();
+        // $processRequest = ProcessRequest::factory()->create([
+        //     'process_id' => $process->getKey(),
+        //     'bpmn' => $bpmn,
+        // ]);
 
+        // $process = Process::factory()->create();
+
+        // $processRequestToken = ProcessRequestToken::factory()->create([
+        //     'process_id' => $process->getKey(),
+        //     'process_request_id' => $processRequest->getKey()
+        // ]);
+
+        // $processAbeRequest = ProcessAbeRequestToken::factory()->create();
+
+        // ProcessTaskAssignment::factory()->create([
+        //     'process_id' => $processAbeRequest->process_id,
+        //     'process_task_id' => 'node_255',
+        //     'assignment_id' => $this->user->id,
+        //     'assignment_type' => 'ProcessMaker\Models\User',
+        // ]);
+
+        // $definitions = $process->getDefinitions();
+        // $startEvent = $definitions->getEvent('node_1');
+        // $request = WorkflowManager::triggerStartEvent($process, $startEvent, []);
+        $bpmn = file_get_contents(__DIR__ . '/../../Fixtures/rollback_test.bpmn');
+        $bpmn = str_replace('[task_user_id]', $this->user->id, $bpmn);
+        $process = Process::factory()->create([
+            'bpmn' => $bpmn,
+        ]);
         ProcessTaskAssignment::factory()->create([
-            'process_id' => $processAbeRequest->process_id,
-            'process_task_id' => 'node_255',
+            'process_id' => $process->id,
+            'process_task_id' => 'node_2',
             'assignment_id' => $this->user->id,
             'assignment_type' => 'ProcessMaker\Models\User',
         ]);
-
         $definitions = $process->getDefinitions();
         $startEvent = $definitions->getEvent('node_1');
         $request = WorkflowManager::triggerStartEvent($process, $startEvent, []);
