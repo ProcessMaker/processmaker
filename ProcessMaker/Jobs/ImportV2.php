6d5a55ca5bfcfe6c300d701a2140503759d01d2d
Rodrigo Quelca <rodrigo@processmaker.com>
2024-07-17 12:08:27 -0400

    restore importV2

--- ProcessMaker/Jobs/ImportV2.php
+++ ProcessMaker/Jobs/ImportV2.php
@@ -111,7 +111,7 @@ class ImportV2 implements ShouldQueue
 
     public static function isRunning()
     {
-        return (bool) false;
+        return (bool) Cache::lockConnection()->client()->get(Cache::getPrefix() . 'import_running');
     }
 
     private function checkHash()
