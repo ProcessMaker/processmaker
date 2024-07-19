6d9c7c34cbfd0cf1526e22942ffd7f7c113d9533
Agustín Busso <90727999+agustinbusso@users.noreply.github.com>
2024-07-19 10:09:00 -0300

    Add scope Exclude to base class model

--- ProcessMaker/Models/ProcessMakerModel.php
+++ ProcessMaker/Models/ProcessMakerModel.php
@@ -23,4 +23,26 @@ class ProcessMakerModel extends Model
     {
         return $date->format('Y-m-d H:i:s');
     }
+
+    public function scopeExclude($query, ...$columns)
+    {
+        return $columns !== [] ? $query->select(
+            array_diff(
+                $this->getTableColumns(),
+                \Illuminate\Support\Arr::flatten($columns)
+            )
+        ) : $query;
+    }
+
+    public function getTableColumns()
+    {
+        return \Illuminate\Support\Facades\Cache::rememberForever(
+            'MigrMod:'.filemtime(
+                database_path('migrations')).':'.$this->getTable(), function () {
+                    return $this->getConnection()
+                        ->getSchemaBuilder()
+                        ->getColumnListing($this->getTable());
+                }
+            );
+    }
 }
