6c09dc78742a83f7a168032c7efb82aeb4805af0
Miguel Angel <miguelangeldev@icloud.com>
2024-07-23 14:14:39 -0400

    fix(cr): use $request instead request()

--- ProcessMaker/Http/Controllers/Api/V1_1/TaskController.php
+++ ProcessMaker/Http/Controllers/Api/V1_1/TaskController.php
@@ -57,8 +57,8 @@ class TaskController extends Controller
 
     private function processFilters(Request $request, Builder $query)
     {
-        if (request()->has('user_id')) {
-            $query->where('user_id', request()->get('user_id'));
+        if ($request->has('user_id')) {
+            $query->where('user_id', $request->get('user_id'));
         }
 
         if ($request->has('process_request_id')) {
