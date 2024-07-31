2d1ae1f5cae2d94fc8f24720bf3aae2290ad194e
luNunezProcessmaker <luciana.nunez@processmaker.com>
2024-07-31 14:40:12 -0400

    feature/FOUR-17281

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -4,11 +4,12 @@ namespace Tests\Feature;
 
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\Auth;
+use ProcessMaker\Models\AnonymousUser;
 use ProcessMaker\Models\Process;
 use ProcessMaker\Models\ProcessAbeRequestToken;
 use ProcessMaker\Models\ProcessRequest;
 use ProcessMaker\Models\ProcessRequestToken;
-use ProcessMaker\Models\AnonymousUser;
+use ProcessMaker\Models\Screen;
 use ProcessMaker\Models\User;
 use Tests\Feature\Shared\RequestHelper;
 use Tests\TestCase;
@@ -17,17 +18,22 @@ class TaskControllerTest extends TestCase
 {
     use RequestHelper;
 
-    public function testShowScreen()
+    public function testActionBYEmailWithScreen()
     {
         $process = Process::factory()->create([
             'bpmn' => file_get_contents(__DIR__ . '/../Fixtures/action_by_email_process.bpmn'),
         ]);
-
         // Start a request
         $route = route('api.process_events.trigger', [$process->id, 'event' => 'node_1']);
         $this->apiCall('POST', $route, []);
 
-        $processAbeRequest = ProcessAbeRequestToken::first();
+        $instance = ProcessRequest::first();
+        $task = ProcessRequestToken::where('element_name', 'Form Task')->first();
+
+        $processAbeRequest = ProcessAbeRequestToken::factory()->create([
+            'process_request_id' => $instance->getKey(),
+            'process_request_token_id' => $task->getKey(),
+        ]);
 
         $response = $this->webCall(
             'GET',
@@ -35,18 +41,42 @@ class TaskControllerTest extends TestCase
         );
 
         // check the correct view is called
+        $response->assertViewIs('processes.screens.completedScreen');
+        $response->assertStatus(200);
+    }
+
+    public function testActionBYEmailWithoutScreen()
+    {
+        $process = Process::factory()->create([
+            'bpmn' => file_get_contents(__DIR__ . '/../Fixtures/action_by_email_process_require_login.bpmn'),
+        ]);
+        // Start a request
+        $route = route('api.process_events.trigger', [$process->id, 'event' => 'node_1']);
+        $this->apiCall('POST', $route, []);
+
+        $instance = ProcessRequest::first();
+        $task = ProcessRequestToken::where('element_name', 'Form Task')->first();
+
+        $processAbeRequest = ProcessAbeRequestToken::factory()->create([
+            'process_request_id' => $instance->getKey(),
+            'process_request_token_id' => $task->getKey(),
+            'completed_screen_id' => null,
+        ]);
+
+        $response = $this->webCall(
+            'GET',
+            'tasks/update_variable/' . $processAbeRequest->uuid . '?varName=res&varValue=yes'
+        );
+
         $response->assertSee('Your response has been submitted.');
         $response->assertStatus(200);
     }
+
     /*
      * Test Process ABE With require login
     */
-    public function testRequireLogin()
+    public function testActionByEmailWithRequireLogin()
     {
-
-        $user = User::where('username', AnonymousUser::ANONYMOUS_USERNAME)->first();
-        Auth::login($user);
-
         $process = Process::factory()->create([
             'bpmn' => file_get_contents(__DIR__ . '/../Fixtures/action_by_email_process_require_login.bpmn'),
         ]);
@@ -58,6 +88,25 @@ class TaskControllerTest extends TestCase
         $processAbeRequest = ProcessAbeRequestToken::first();
 
         $this->assertEquals(1, $processAbeRequest->require_login);
+    }
+
+    /*
+     * Test Process ABE Without require login
+    */
+    public function testActionByEmailWithoutRequireLogin()
+    {
+        $process = Process::factory()->create([
+            'bpmn' => file_get_contents(__DIR__ . '/../Fixtures/action_by_email_process.bpmn'),
+        ]);
+        Screen::factory()->create();
+
+        // Start a request
+        $route = route('api.process_events.trigger', [$process->id, 'event' => 'node_1']);
+        $this->apiCall('POST', $route, []);
+
+        $processAbeRequest = ProcessAbeRequestToken::first();
+
+        $this->assertEquals(0, $processAbeRequest->require_login);
     }
 
     public function testReturnMessageTokenNoFound()
