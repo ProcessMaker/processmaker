2e11d139bcf9adb94bc616a181bbe9f568e7836a
Sanja <sanja.cornelius@processmaker.com>
2024-08-06 12:37:51 -0700

    Remove unused cache class in user model

--- ProcessMaker/Models/User.php
+++ ProcessMaker/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
 use Laravel\Passport\HasApiTokens;
