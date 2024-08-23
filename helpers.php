5e5c044e9a9e538d110d261b1d94c314c616b6b9
Ryan Cooley <ryancooley@users.noreply.github.com>
2024-08-23 06:06:27 -0700

    Add helper functions to return color values

--- helpers.php
+++ helpers.php
@@ -50,6 +50,43 @@ if (!function_exists('settings')) {
     }
 }
 
+if (!function_exists('default_color')) {
+    /**
+     * Returns the default color value.
+     *
+     * @param $key
+     *
+     * @return string
+     */
+    function default_color($key)
+    {
+        return config('app.default_colors.' . $key);
+    }
+}
+
+if (!function_exists('color')) {
+    /**
+     * Returns the color value from the settings.
+     *
+     * @param $key
+     *
+     * @return string
+     */
+    function color($key)
+    {
+        if ($colors = config('css-override.variables')) {
+            $colors = json_decode($colors);
+            foreach ($colors as $color) {
+                if ($color->id === '$' . $key) {
+                    return $color->value;
+                }
+            }
+        }
+        
+        return default_color($key);
+    }
+}
+
 if (!function_exists('refresh_artisan_caches')) {
     /**
      * Re-caches artisan config, routes, and/or events when they are already cached.
