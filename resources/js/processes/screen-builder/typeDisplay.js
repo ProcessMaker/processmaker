8e0ec48e49ab7a97d29bec048cdb601675c736e4
CarliPinell <carlos.pinell@processmaker.com>
2024-08-29 11:37:55 -0400

    Ready for PR

--- resources/js/processes/screen-builder/typeDisplay.js
+++ resources/js/processes/screen-builder/typeDisplay.js
@@ -15,6 +15,7 @@ const FormNestedScreen = FormBuilderControls.find((control) => control.rendererB
 const FileDownloadControl = FormBuilderControls.find((control) => control.builderBinding === "FileDownload");
 const FormListTable = FormBuilderControls.find((control) => control.rendererBinding === "FormListTable");
 const FormAnalyticsChart = FormBuilderControls.find((control) => control.rendererBinding === "FormAnalyticsChart");
+const FormCollectionRecordControl = FormBuilderControls.find((control) => control.rendererBinding === "FormCollectionRecordControl");
 // Remove editable inspector props
 FormRecordList.control.inspector = FormRecordList.control.inspector.filter((prop) => prop.field !== "editable" && prop.field !== "form");
 
@@ -32,6 +33,7 @@ const controlsDisplay = [
   FileDownloadControl,
   FormListTable,
   FormAnalyticsChart,
+  FormCollectionRecordControl,
 ];
 
 ProcessMaker.EventBus.$on("screen-builder-init", (manager) => {
