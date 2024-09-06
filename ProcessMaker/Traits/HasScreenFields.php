aba583d1e251279adc403c03996ff6ec8652737f
CarliPinell <carlos.pinell@processmaker.com>
2024-09-06 15:26:00 -0400

    Adding new fields of component CollectionRecordControl to screenFields in hasScreenFields trait

--- ProcessMaker/Traits/HasScreenFields.php
+++ ProcessMaker/Traits/HasScreenFields.php
@@ -1,20 +1,15 @@
 <?php
-
 namespace ProcessMaker\Traits;
-
 use Illuminate\Support\Arr;
 use Log;
 use ProcessMaker\Models\Column;
 use ProcessMaker\Models\Screen;
-
 trait HasScreenFields
 {
     private $parsedFields;
-
     private $restrictedComponents = [
         'FormImage',
     ];
-
     public function getFieldsAttribute()
     {
         if (empty($this->parsedFields)) {
@@ -35,10 +30,8 @@ trait HasScreenFields
                 ]);
             }
         }
-
         return $this->parsedFields->unique('field');
     }
-
     public function parseNestedScreen($node)
     {
         $nested = Screen::find($node['config']['screen']);
@@ -46,16 +39,24 @@ trait HasScreenFields
             $this->parsedFields->push($field);
         }
     }
-
+    public function parseCollectionRecordControl($node)
+    {
+        $collection = Screen::find($node['config']['collection']['screen']);
+        foreach ($collection->fields as $field) {
+            $this->parsedFields->push($field);
+        }
+    }
     public function walkArray($array, $key = null)
     {
         if (!is_array($array)) {
             $array = json_decode($array);
         }
-
         foreach ($array as $subkey => $value) {
+
             if (isset($value['component']) && $value['component'] === 'FormNestedScreen') {
                 $this->parseNestedScreen($value);
+            } elseif (isset($value['component']) && $value['component'] === 'FormCollectionRecordControl') {
+                $this->parseCollectionRecordControl($value);
             } elseif ($key !== 'inspector' && is_array($value) && isset($value['config']['name'])) {
                 $this->parseItem($value);
             }
@@ -64,7 +65,6 @@ trait HasScreenFields
             }
         }
     }
-
     public function parseItem($item)
     {
         if (isset($item['component']) && !in_array($item['component'], $this->restrictedComponents)) {
@@ -79,12 +79,10 @@ trait HasScreenFields
             ]));
         }
     }
-
     public function parseItemName($item)
     {
         return $item['config']['name'];
     }
-
     public function parseItemLabel($item)
     {
         if (isset($item['config']['label'])) {
@@ -93,15 +91,12 @@ trait HasScreenFields
             return null;
         }
     }
-
     public function parseItemFormat($item)
     {
         $format = 'string';
-
         if (isset($item['config']['dataFormat'])) {
             $format = $item['config']['dataFormat'];
         }
-
         if (isset($item['component'])) {
             switch ($item['component']) {
                 case 'FileUpload':
@@ -128,22 +123,18 @@ trait HasScreenFields
                     break;
             }
         }
-
         return $format;
     }
-
     public function parseItemMask($item)
     {
         return $item['config']['dataMask'] ?? null;
         if (isset($item['config']['dataMask'])) {
         }
     }
-
     public function parseIsSubmitButton($item)
     {
         return Arr::get($item, 'config.event') === 'submit';
     }
-
     /**
      * Return an array of fields that can be included when
      * saving a draft or doing a quick fill, so as not to
