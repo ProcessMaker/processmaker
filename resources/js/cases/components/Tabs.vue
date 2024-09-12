57554df07ace161cfa7755f8b8bfa52e0474aceb
Fabio <frguachallab@gmail.com>
2024-09-12 15:01:52 -0400

    update class

--- resources/js/cases/components/Tabs.vue
+++ resources/js/cases/components/Tabs.vue
@@ -10,9 +10,9 @@
             v-if="tab.show"
             :key="index"
             :href="tab.href"
+            class="tw-whitespace-nowrap tw-border-b-2 tw-px-1 tw-py-4 tw-text-sm tw-font-medium"
             :class="[tabSelected === tab.current ? 'tw-border-blue-500 tw-text-blue-500'
-                       : 'tw-border-transparent tw-text-gray-500 hover:tw-border-gray-600 hover:tw-text-gray-600',
-                     'tw-whitespace-nowrap tw-border-b-2 tw-px-1 tw-py-4 tw-text-sm tw-font-medium'
+                       : 'tw-border-transparent tw-text-gray-500 hover:tw-border-gray-600 hover:tw-text-gray-600'
             ]"
             :aria-current="tab.current ? 'page' : undefined"
             @click="selectTab(tab)"
