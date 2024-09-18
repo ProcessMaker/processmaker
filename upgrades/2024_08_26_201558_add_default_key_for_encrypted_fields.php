5996ddb54803a7cc3152fea03d5fecab12f59303
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2024-09-18 18:30:18 +0000

    FOUR-19069 Implement command to update data already encrypted

--- upgrades/2024_08_26_201558_add_default_key_for_encrypted_fields.php
+++ upgrades/2024_08_26_201558_add_default_key_for_encrypted_fields.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Encryption\Encrypter;
 use ProcessMaker\Models\EncryptedData;
-use ProcessMaker\Models\EnvironmentVariable;
 use ProcessMaker\Upgrades\UpgradeMigration as Upgrade;
 
 
@@ -16,8 +15,8 @@ class AddDefaultKeyForEncryptedFields extends Upgrade
     public function up()
     {
         if (empty(config('app.encrypted_data_key'))) {
-            $key = $this->generateRandomKey();
-            $this->addKeyInEnvironmentFile($key);
+            $key = Encrypter::generateKey(EncryptedData::ENCRYPTION_METHOD);
+            EncryptedData::addKeyInEnvironmentFile($key);
         }
     }
 
@@ -29,59 +28,7 @@ class AddDefaultKeyForEncryptedFields extends Upgrade
     public function down()
     {
         if (!empty(config('app.encrypted_data_key'))) {
-            $this->removeKeyFromEnvironmentFile();
+            EncryptedData::removeKeyFromEnvironmentFile();
         }
     }
-
-    /**
-     * Write key in environment file with the given key.
-     *
-     * @param string $key
-     */
-    protected function addKeyInEnvironmentFile($key)
-    {
-        $content = file_get_contents(App::environmentFilePath());
-
-        $content .= "\nENCRYPTED_DATA_KEY=$key\n";
-
-        file_put_contents(App::environmentFilePath(), $content);
-    }
-
-    /**
-     * Remove key from environment file.
-     */
-    protected function removeKeyFromEnvironmentFile()
-    {
-        $replaced = preg_replace(
-            $this->keyReplacementPattern(),
-            '',
-            $input = file_get_contents(App::environmentFilePath())
-        );
-        file_put_contents(App::environmentFilePath(), $replaced);
-    }
-
-    /**
-     * Generate a random key to use for encrypt data.
-     *
-     * @return string
-     */
-    protected function generateRandomKey()
-    {
-        return 'base64:'.base64_encode(
-            Encrypter::generateKey(EncryptedData::ENCRYPTION_METHOD)
-        );
-    }
-
-    /**
-     * Get a regex pattern that will match env ENCRYPTED_DATA_KEY with any random key.
-     *
-     * @return string
-     */
-    protected function keyReplacementPattern()
-    {
-        $escaped = preg_quote('=' . config('app.encrypted_data_key'), '/');
-
-        return "/^ENCRYPTED_DATA_KEY{$escaped}/m";
-    }
 }
-
