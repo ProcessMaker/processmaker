98b26105aba92658639497168f452d7ff12880e7
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2024-10-02 15:19:17 +0000

    FOUR-18605 Count the cases and requests by user and status (EP)

--- ProcessMaker/Http/Controllers/Api/V1_1/CaseController.php
+++ ProcessMaker/Http/Controllers/Api/V1_1/CaseController.php
@@ -5,9 +5,12 @@ namespace ProcessMaker\Http\Controllers\Api\V1_1;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use ProcessMaker\Http\Controllers\Api\ProcessRequestController;
 use ProcessMaker\Http\Controllers\Controller;
 use ProcessMaker\Http\Requests\CaseListRequest;
 use ProcessMaker\Http\Resources\V1_1\CaseResource;
+use ProcessMaker\Models\User;
 use ProcessMaker\Repositories\CaseApiRepository;
 
 class CaseController extends Controller
@@ -81,6 +84,73 @@ class CaseController extends Controller
     {
         $query = $this->caseRepository->getCompletedCases($request);
         return $this->paginateResponse($query);
+    }
+
+    /**
+     * Get "my cases" counters
+     *
+     * @param CaseListRequest $request
+     *
+     * @return JsonResponse
+     */
+    public function getMyCasesCounters(CaseListRequest $request): JsonResponse
+    {
+        // Load user object
+        if ($request->filled('userId')) {
+            $userId = $request->get('userId');
+            $user = User::find($userId);
+        } else {
+            $user = Auth::user();
+        }
+
+        // Initializing variables
+        $totalAllCases = null;
+        $totalMyCases = null;
+        $totalInProgress = null;
+        $totalCompleted = null;
+        $totalMyRequest = null;
+
+        // Check permission
+        if ($user->hasPermission('view-all_cases')) {
+            // The total number of cases recorded in the platform. User Id send is overridden.
+            $request->merge(['userId' => null]);
+            $queryAllCases = $this->caseRepository->getAllCases($request);
+            $totalAllCases = $queryAllCases->count();
+        }
+
+        // Restore user id
+        $request->merge(['userId' => $user->id]);
+
+        // The total number of cases recorded by the user making the request.
+        $queryMyCases = $this->caseRepository->getAllCases($request);
+        $totalMyCases = $queryMyCases->count();
+
+        // The number of In Progress cases started by the user making the request.
+        $queryInProgressCases = $this->caseRepository->getInProgressCases($request);
+        $totalInProgress = $queryInProgressCases->count();
+
+        // The number of Completed cases started by the user making the request.
+        $queryCompletedCases = $this->caseRepository->getCompletedCases($request);
+        $totalCompleted = $queryCompletedCases->count();
+
+        // Check permission
+        if ($user->hasPermission('view-my_requests')) {
+            // Only in progress requests
+            $requestAux = new Request();
+            $requestAux->replace(['type' => 'in_progress']);
+
+            // The number of requests for user making the request.
+            $totalMyRequest = (new ProcessRequestController)->index($requestAux, true, $user);
+        }
+
+        // Build response
+        return response()->json([
+            'totalAllCases' => $totalAllCases,
+            'totalMyCases' => $totalMyCases,
+            'totalInProgress' => $totalInProgress,
+            'totalCompleted' => $totalCompleted,
+            'totalMyRequest' => $totalMyRequest,
+        ]);
     }
 
     /**
