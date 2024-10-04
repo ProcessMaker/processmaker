5f680ca54223f46933c3d7ea9224163a28bf755e
Nolan Ehrstrom <github@nolanpro.com>
2024-10-04 10:52:08 -0700

    Use tags so we don't clear all cache

--- ProcessMaker/Helpers/CachedSchema.php
+++ ProcessMaker/Helpers/CachedSchema.php
@@ -7,21 +7,27 @@ use Illuminate\Support\Facades\Schema;
 
 class CachedSchema
 {
+    const CACHE_TAG = 'schema';
+
     public function hasTable(string $table) : bool
     {
         $key = 'hasTable_' . $table;
 
-        return Cache::remember($key, 86400, function () use ($table) {
-            return Schema::hasTable($table);
-        });
+        return Cache::tags(self::CACHE_TAG)->rememberForever(
+            $key, function () use ($table) {
+                return Schema::hasTable($table);
+            }
+        );
     }
 
     public function hasColumn(string $table, string $column) : bool
     {
         $key = 'hasColumn_' . $table . '_' . $column;
 
-        return Cache::remember($key, 86400, function () use ($table, $column) {
-            return Schema::hasColumn($table, $column);
-        });
+        return Cache::tags(self::CACHE_TAG)->rememberForever(
+            $key, function () use ($table, $column) {
+                return Schema::hasColumn($table, $column);
+            }
+        );
     }
 }
