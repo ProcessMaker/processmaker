f16e915773f185a0ad75dd646ebf4f794407aac2
TinCodes <tincholaguna37@gmail.com>
2024-10-08 16:14:12 -0400

    Old profile and process config translations removed

--- resources/js/processes/edit.js
+++ resources/js/processes/edit.js
@@ -1,7 +1,3 @@
 import CategorySelect from "./categories/components/CategorySelect";
-import ProcessTranslationListing from "./translations/components/ProcessTranslationListing";
-import CreateProcessTranslationModal from "./translations/components/CreateProcessTranslationModal";
 
 Vue.component("CategorySelect", CategorySelect);
-Vue.component("ProcessTranslationListing", ProcessTranslationListing);
-Vue.component("CreateProcessTranslationModal", CreateProcessTranslationModal);
