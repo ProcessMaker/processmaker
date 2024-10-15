00553f72c50981e1fbae6ae68c1a42435b8649b5
henryjonathanquispe <56353454+henryjonathanquispe@users.noreply.github.com>
2024-10-15 15:39:18 -0400

    Update EllipsisMenu.vue
    This file is a template, it will be necessary as an example to implement an ellipsis menu in the table

--- resources/jscomposition/cases/casesDetail/components/EllipsisMenu.vue
+++ resources/jscomposition/cases/casesDetail/components/EllipsisMenu.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="tw-float-right tw-absolute tw-right-4 tw-top-0">
-    <button
-      v-for="item in menuOptions"
-      v-if="row.screenId"
-      :key="item.menu"
-      type="button"
-      class="tw-rounded-lg tw-bg-white tw-px-2 tw-py-1 tw-text-xs tw-font-semibold tw-text-gray-600 tw-shadow-sm
-        tw-ring-1 tw-ring-gray-300 hover:tw-bg-gray-50 tw-gap-2"
-      @click="executeCallback(item)">
-      <i
-        class="tw-text-xl tw-text-gray-600"
-        :class="item.icon" />
-    </button>
-  </div>
+  <div class="tw-float-right tw-absolute tw-right-4 tw-top-0" />
 </template>
 
 <script setup>
@@ -26,24 +13,4 @@ const props = defineProps({
     default: () => {},
   },
 });
-
-const onPrint = () => {
-  const { data } = props;
-  if (data.screen_version) {
-    window.open(`/requests/${props.row.process_request_id}/task/${props.row.id}/screen/${props.row.screen_version}`);
-  }
-  const msg = "There is not screen";
-  ProcessMaker.alert(msg, "danger");
-};
-
-const menuOptions = [
-  {
-    icon: "fa fa-print",
-    callback: onPrint,
-    name: "print",
-  },
-];
-
-const executeCallback = (item) => item.callback();
-
 </script>
