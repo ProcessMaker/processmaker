93818498ed3279628c1cfc675f3e021c1de14f62
Paula Quispe <paula.quispe@processmaker.com>
2024-10-16 13:13:17 -0400

    TEST

--- ProcessMaker/Http/Controllers/CasesController.php
+++ ProcessMaker/Http/Controllers/CasesController.php
@@ -2,32 +2,11 @@
 
 namespace ProcessMaker\Http\Controllers;
 
-use Facades\ProcessMaker\RollbackProcessRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use ProcessMaker\Cache\CacheRemember;
-use ProcessMaker\Events\FilesDownloaded;
-use ProcessMaker\Events\ScreenBuilderStarting;
-use ProcessMaker\Filters\SaveSession;
-use ProcessMaker\Helpers\DefaultColumns;
-use ProcessMaker\Helpers\MobileHelper;
 use ProcessMaker\Http\Controllers\Controller;
-use ProcessMaker\Managers\DataManager;
-use ProcessMaker\Managers\ScreenBuilderManager;
-use ProcessMaker\Models\Comment;
-use ProcessMaker\Models\Process;
 use ProcessMaker\Models\ProcessRequest;
-use ProcessMaker\Models\ProcessRequestToken;
-use ProcessMaker\Models\Screen;
-use ProcessMaker\Models\ScreenVersion;
-use ProcessMaker\Models\UserResourceView;
 use ProcessMaker\Package\PackageComments\PackageServiceProvider;
 use ProcessMaker\ProcessTranslations\ProcessTranslation;
-use ProcessMaker\RetryProcessRequest;
-use ProcessMaker\Traits\HasControllerAddons;
-use ProcessMaker\Traits\SearchAutocompleteTrait;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class CasesController extends Controller
 {
