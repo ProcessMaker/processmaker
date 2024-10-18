560560196a0f13ee47ca1722e83e3b94afb274ce
Gustavo Bascopé <gustavo.bascope@processmaker.com>
2024-10-18 11:30:18 -0400

    Fix access file log event

--- ProcessMaker/Http/Controllers/Api/ProcessRequestFileController.php
+++ ProcessMaker/Http/Controllers/Api/ProcessRequestFileController.php
@@ -95,7 +95,11 @@ class ProcessRequestFileController extends Controller
 
         // Register the Event
         if (!empty($filter)) {
-            foreach ($media as $singleMedia) {
+            if ($media instanceof Collection) {
+                foreach ($media as $singleMedia) {
+                    FilesAccessed::dispatch($filter, $request, $singleMedia);
+                }
+            } elseif ($media instanceof Media) {
                 FilesAccessed::dispatch($filter, $request, $media);
             }
         }
