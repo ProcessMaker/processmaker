d7bf753ec054a05bebbb4380dcde405c01900c34
David Callizaya <davidcallizaya@gmail.com>
2024-11-04 10:24:15 -0400

    Use expression to evaluate variables for self service users and groups

--- ProcessMaker/Repositories/TokenRepository.php
+++ ProcessMaker/Repositories/TokenRepository.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Log;
 use Mustache_Engine;
 use ProcessMaker\Mail\TaskActionByEmail;
 use ProcessMaker\Managers\DataManager;
+use ProcessMaker\Models\FeelExpressionEvaluator;
 use ProcessMaker\Models\ProcessAbeRequestToken;
 use ProcessMaker\Models\ProcessCollaboration;
 use ProcessMaker\Models\ProcessRequest as Instance;
@@ -131,8 +132,9 @@ class TokenRepository implements TokenRepositoryInterface
                 case 'process_variable':
                     $dataManager = new DataManager();
                     $tokenData = $dataManager->getData($token);
-                    $evaluatedUsers = $selfServiceUsers ? $tokenData[$selfServiceUsers] ?? null: [];
-                    $evaluatedGroups = $selfServiceGroups ? $tokenData[$selfServiceGroups] ?? null : [];
+                    $feel = new FeelExpressionEvaluator();
+                    $evaluatedUsers = $selfServiceUsers ? $feel->render($selfServiceUsers, $tokenData) ?? null: [];
+                    $evaluatedGroups = $selfServiceGroups ? $feel->render($selfServiceGroups, $tokenData) ?? null : [];
 
                     // If we have single values we put it inside an array
                     $evaluatedUsers = is_array($evaluatedUsers) ? $evaluatedUsers : [$evaluatedUsers];
