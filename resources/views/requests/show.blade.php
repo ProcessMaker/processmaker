7eda5ad10e9f52dcd9457c98e2efe684f2b90b2e
Henry Jonas <hjonathan.sge@gmail.com>
2024-11-05 13:07:59 -0400

    FOUR-19272 : CSS style is not rendering in Summary section

--- resources/views/requests/show.blade.php
+++ resources/views/requests/show.blade.php
@@ -120,7 +120,7 @@
                 <template v-if="showScreenSummary">
                   <div class="p-3">
                     <vue-form-renderer ref="screen" :config="screenSummary.config" v-model="dataSummary"
-                      :custom-css="screenSummary.custom_css"
+                      :custom-css="screenSummary?.custom_css"
                       :computed="screenSummary.computed" />
                   </div>
                 </template>
@@ -128,7 +128,7 @@
                   <div class="card">
                     <div class="card-body">
                       <vue-form-renderer ref="screenRequestDetail" :config="screenRequestDetail"
-                        :custom-css="screenSummary.custom_css"
+                        :custom-css="screenSummary?.custom_css"
                         v-model="dataSummary" />
                     </div>
                   </div>
