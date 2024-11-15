4554dbdd33ec67797d9867b825b1152e9ddf87f3
CarliPinell <carlos.pinell@processmaker.com>
2024-11-15 14:02:26 -0400

    fixing details

--- database/migrations/2024_02_20_193849_create_task_drafts_table.php
+++ database/migrations/2024_02_20_193849_create_task_drafts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,8 +15,6 @@ return new class extends Migration
             $table->integer('task_id');
             $table->json('data');
             $table->timestamps();
-            
-            $table->index('task_id', 'taskId');
         });
     }
 
