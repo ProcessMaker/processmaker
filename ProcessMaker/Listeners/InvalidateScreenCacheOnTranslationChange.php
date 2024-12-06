cf9db1b753360946424c20205d5f5d79d71c153a
Rodrigo Quelca <rodrigo@processmaker.com>
2024-12-06 08:12:29 -0400

    clear debug

--- ProcessMaker/Listeners/InvalidateScreenCacheOnTranslationChange.php
+++ ProcessMaker/Listeners/InvalidateScreenCacheOnTranslationChange.php
@@ -15,30 +15,9 @@ class InvalidateScreenCacheOnTranslationChange
     public function handle(TranslationChanged $event): void
     {
         try {
-            Log::debug('TranslationChanged event received', [
-                'event' => [
-                    'language' => $event->language,
-                    'changes' => $event->changes,
-                    'screenId' => $event->screenId,
-                ],
-            ]);
-
             if ($event->screenId) {
-                Log::debug('Attempting to invalidate screen cache', [
-                    'screenId' => $event->screenId,
-                    'language' => $event->language,
-                ]);
-
                 $this->invalidateScreen($event->screenId, $event->language);
-            } else {
-                Log::debug('No screenId provided, skipping cache invalidation');
             }
-
-            Log::info('Screen cache invalidated for translation changes', [
-                'language' => $event->language,
-                'changes' => array_keys($event->changes),
-                'screenId' => $event->screenId,
-            ]);
         } catch (\Exception $e) {
             Log::error('Failed to invalidate screen cache', [
                 'error' => $e->getMessage(),
@@ -56,29 +35,10 @@ class InvalidateScreenCacheOnTranslationChange
     protected function invalidateScreen(string $screenId, string $locale): void
     {
         try {
-            Log::debug('Finding screen', ['screenId' => $screenId]);
-
             $screen = Screen::find($screenId);
             if ($screen) {
-                Log::debug('Screen found, getting cache implementation', [
-                    'screen' => [
-                        'id' => $screen->id,
-                        'title' => $screen->title ?? 'N/A',
-                    ],
-                ]);
-
-                // Get cache implementation from factory
                 $cache = ScreenCacheFactory::getScreenCache();
-                Log::debug('Cache implementation obtained', [
-                    'cacheClass' => get_class($cache),
-                ]);
-
-                $result = $cache->invalidate($screen->id, $locale);
-                Log::debug('Cache invalidation completed', [
-                    'result' => $result,
-                    'screenId' => $screen->id,
-                    'locale' => $locale,
-                ]);
+                $cache->invalidate($screen->id, $locale);
             } else {
                 Log::warning('Screen not found', ['screenId' => $screenId]);
             }
