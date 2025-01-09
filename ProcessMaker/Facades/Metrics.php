8a23d30c6799e21ff46907f3a855a9e09193d4e5
David Callizaya <davidcallizaya@gmail.com>
2025-01-09 19:10:08 -0400

    Add Prometheus methods to Metrics facade and enhance MetricsService to get collection registry for tests

--- ProcessMaker/Facades/Metrics.php
+++ ProcessMaker/Facades/Metrics.php
@@ -5,6 +5,14 @@ namespace ProcessMaker\Facades;
 use Illuminate\Support\Facades\Facade;
 use ProcessMaker\Services\MetricsService;
 
+/**
+ * @method static \Prometheus\Counter counter(string $name, string $help = null, array $labels = [])
+ * @method static \Prometheus\Gauge gauge(string $name, string $help = null, array $labels = [])
+ * @method static \Prometheus\Histogram histogram(string $name, string $help = null, array $labels = [], array $buckets = [0.1, 1, 5, 10])
+ * @method static void setGauge(string $name, float $value, array $labelValues = [])
+ * @method static string renderMetrics()
+ * @method static \Prometheus\CollectorRegistry getCollectionRegistry()
+ */
 class Metrics extends Facade
 {
     /**
