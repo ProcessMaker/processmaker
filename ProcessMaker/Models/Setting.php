10c7532b5f5d9e66d4af8ff42365dd8e172472fa
Miguel Angel <me@miguelangeldev.com>
2025-04-10 09:40:36 -0400

    feat: enhance settings uiI to support new setting key for email listener configuration

--- ProcessMaker/Models/Setting.php
+++ ProcessMaker/Models/Setting.php
@@ -401,6 +401,7 @@ class Setting extends ProcessMakerModel implements HasMedia, PrometheusMetricInt
         $query = self::query()
             ->select([
                 \DB::raw('MAX(id) as id'),
+                \DB::raw('MAX(`key`) as setting_key'),
                 'group',
             ])
             ->groupBy('group')
@@ -415,6 +416,7 @@ class Setting extends ProcessMakerModel implements HasMedia, PrometheusMetricInt
                 'id' => $setting->group,
                 'name' => $setting->group,
                 'setting_id' => $setting->id,
+                'setting_key' => $setting->setting_key,
             ];
         }
 
