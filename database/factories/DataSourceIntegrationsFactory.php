836d1d7c17f561883d5004623ca65cde1d78bd51
sanja <52755494+sanjacornelius@users.noreply.github.com>
2025-04-17 12:16:06 -0700

    Create DataSourceIntegrationsFactory.php

---
+++ database/factories/DataSourceIntegrationsFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\ProcessMaker\Models\DataSourceIntegrations>
+ */
+class DataSourceIntegrationsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
