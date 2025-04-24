9d3a24ab2c7bbaeea4386320c2fcf2fed013adae
Nolan Ehrstrom <github@nolanpro.com>
2025-04-24 14:52:49 -0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -21,6 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:4200',
+        'https://legendary-adventure-2n21ppv.pages.github.io',
     ],
 
     'allowed_origins_patterns' => [],
