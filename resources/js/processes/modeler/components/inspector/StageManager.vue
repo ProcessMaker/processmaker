c822ee4eb989449e17bfc712a64152baa80864e2
Roly Gutierrez <roly.gutierrez@processmaker.com>
2025-04-25 08:07:52 -0400

    [FOUR-23473](https://processmaker.atlassian.net/browse/FOUR-23473) S1: Add stage section for a the flow elements **Description** Add Stage Section showed only when the element selected is a FLOW

---
+++ resources/js/processes/modeler/components/inspector/StageManager.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-4 bg-white rounded shadow-md max-w-md">
+    <b class="text-lg font-bold mb-2">Order of stages</b>
+    <p class="text-sm mb-4">
+      Here you have all the stages already set in this process. Define the order you prefer:
+    </p>
+    <StageList :initialStages="defaultStages" />
+  </div>
+</template>
+
+<script setup>
+import StageList from './StageList.vue';
+
+const defaultStages = [
+  { id: 1, label: 'Request Sent', selected: false },
+  { id: 2, label: 'Request Reviewed', selected: true },
+  { id: 3, label: 'Manager Reviewed', selected: false },
+];
+</script>
