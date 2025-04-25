d493942d21cad5654ca0343fe9063aff2e8e4593
David Callizaya <davidcallizaya@gmail.com>
2025-04-25 17:20:10 -0400

    Enhance Api2TypescriptCommand for improved TypeScript SDK generation

--- resources/stubs/api2typescript/composable.blade.php
+++ resources/stubs/api2typescript/composable.blade.php
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { {{ $className }} } from '../api/{{ $tagLower }}.api';
 
 /**
@@ -6,10 +7,17 @@ import { {{ $className }} } from '../api/{{ $tagLower }}.api';
  * @returns {{ $className }} instance
  */
 export const {{ $hookName }} = (apiClient: {
+  head: <T>(endpoint: string) => Promise<T>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   get: <T>(endpoint: string) => Promise<any>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   post: <T>(endpoint: string, data: Record<string, unknown>) => Promise<any>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   put: <T>(endpoint: string, data: Record<string, unknown>) => Promise<any>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   delete: <T>(endpoint: string) => Promise<any>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  patch: <T>(endpoint: string, data: Record<string, unknown>) => Promise<any>;
 }) => {
   return new {{ $className }}(apiClient);
 }; 
