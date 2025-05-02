e74350b26ffcd923b8aab5f67d3e26a9ff6a8d5c
David Callizaya <davidcallizaya@gmail.com>
2025-05-02 14:11:08 -0400

    Added support for PRAGMA_FILTERS in mustache

--- ProcessMaker/Bpmn/MustacheOptions.php
+++ ProcessMaker/Bpmn/MustacheOptions.php
@@ -21,33 +21,57 @@ class MustacheOptions
         ];
     }
 
-    public function html64($text, Mustache_LambdaHelper $helper)
+    public function html64($text, ?Mustache_LambdaHelper $helper = null)
     {
+        if (!$helper) {
+            // Support for PRAGMA_FILTERS
+            return base64_encode('<html><body>' . $text . '</body></html>');
+        }
         return base64_encode('<html><body>' . $helper->render($text) . '</body></html>');
     }
 
-    public function base64($text, Mustache_LambdaHelper $helper)
+    public function base64($text, ?Mustache_LambdaHelper $helper = null)
     {
+        if (!$helper) {
+            // Support for PRAGMA_FILTERS
+            return base64_encode($text);
+        }
         return base64_encode($helper->render($text));
     }
 
-    public function key($text, Mustache_LambdaHelper $helper)
+    public function key($text, ?Mustache_LambdaHelper $helper = null)
     {
+        if (!$helper) {
+            // Support for PRAGMA_FILTERS
+            return urlencode(Hash::make($text));
+        }
         return urlencode(Hash::make($helper->render($text)));
     }
 
-    public function json($text, Mustache_LambdaHelper $helper)
+    public function json($text, ?Mustache_LambdaHelper $helper = null)
     {
+        if (!$helper) {
+            // Support for PRAGMA_FILTERS
+            return json_encode($text);
+        }
         return json_encode($helper->render($text));
     }
 
-    public function serialize($text, Mustache_LambdaHelper $helper)
+    public function serialize($text, ?Mustache_LambdaHelper $helper = null)
     {
+        if (!$helper) {
+            // Support for PRAGMA_FILTERS
+            return serialize($text);
+        }
         return serialize($helper->render($text));
     }
 
-    public function xml($text, Mustache_LambdaHelper $helper)
+    public function xml($text, ?Mustache_LambdaHelper $helper = null)
     {
+        if (!$helper) {
+            // Support for PRAGMA_FILTERS
+            return xmlrpc_encode($text);
+        }
         return xmlrpc_encode($helper->render($text));
     }
 }
