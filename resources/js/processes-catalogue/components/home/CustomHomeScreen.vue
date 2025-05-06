a802c1aac72fa0a789fcec1c5564115f696ff67a
Henry Jonas <jonathan.quispe@processmaker.com>
2025-05-06 11:30:05 -0400

    FOUR-24086: S12: Settings - Add a select List to define the view/tce-dashboard and save

---
+++ resources/js/processes-catalogue/components/home/CustomHomeScreen.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    CUSTOM HOME SCREEN
+    <CustomHomeTableSection />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import CustomHomeTableSection from "./CustomHomeTableSection.vue";
+
+</script>
---
+++ resources/js/processes-catalogue/components/home/CustomHomeTableSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    CUSTOM TABLE SECTION
+    <CustomHomeTableSection />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+</script>
