804fd10f620c13fb64d6a75aedaaf85de1c2c1c6
David Callizaya <davidcallizaya@gmail.com>
2025-05-12 11:34:25 -0400

    Add EmailLinkClicked event class to handle email link click tracking and trigger workflow events

---
+++ ProcessMaker/Events/EmailLinkClicked.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ProcessMaker\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use jdavidbakr\MailTracker\Events\LinkClickedEvent;
+use ProcessMaker\Facades\WorkflowManager;
+use ProcessMaker\Models\ProcessRequest;
+
+class EmailLinkClicked
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function handle(LinkClickedEvent $event)
+    {
+        $tracker = $event->sent_email;
+        $emailIdentifier = $tracker->getHeader('X-ProcessMaker-Email-ID');
+        $messageEventId = $tracker->getHeader('X-ProcessMaker-Sent-Message-Event-ID');
+        $requestIdentifier = $tracker->getHeader('X-ProcessMaker-Request-ID');
+
+        if ($requestIdentifier && $messageEventId) {
+            $request = ProcessRequest::where('id', $requestIdentifier)->first();
+            if ($request) {
+                $data = [
+                    'email_id' => $emailIdentifier,
+                ];
+                WorkflowManager::triggerMessageEvent($request, $messageEventId, $data);
+            }
+        }
+    }
+}
