ba13e1fef7dfad505443694b976449c926e86e3b
danloa <danloa@gmail.com>
2025-05-28 14:09:54 -0400

    Group logic to get variables when savedsearches are used

--- ProcessMaker/Http/Controllers/Api/V1_1/ProcessVariableController.php
+++ ProcessMaker/Http/Controllers/Api/V1_1/ProcessVariableController.php
@@ -219,11 +219,22 @@ class ProcessVariableController extends Controller
         }
 
         // If the classes or tables do not exist, fallback to a saved search approach.
+
+        if ($savedSearch && $request->has('onlyAvailable')) {
+            $paginator = $this->getProcessesVariablesFrom($processIds);
+            $availableColumns = $this->mergeAvailableColumns($savedSearch);
+            $availableColumns = $this->filterActiveColumns($availableColumns, $activeColumns);
+            $paginator->setCollection(
+                $availableColumns->merge($paginator->items())
+            );
+
+            return $paginator;
+        }
+
         if (
             !class_exists(ProcessVariable::class)
             || !Schema::hasTable('process_variables')
             || !self::$useVarFinder
-            || $savedSearch
         ) {
             $paginator = $this->getProcessesVariablesFrom($processIds);
             if ($request->has('onlyAvailable')) {
