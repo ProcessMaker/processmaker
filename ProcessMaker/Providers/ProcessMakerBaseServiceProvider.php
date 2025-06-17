c9bdf779a094f3dfb99196c088ea3528df0c94a2
Rodrigo <rodrigoquelca@gmail.com>
2025-06-17 15:37:42 -0400

    create a base service provider

---
+++ ProcessMaker/Providers/ProcessMakerBaseServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ProcessMaker\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider;
+use ProcessMaker\Helpers\TenantLicense;
+use Spatie\Multitenancy\Events\MadeTenantCurrentEvent;
+
+abstract class ProcessMakerBaseServiceProvider extends ServiceProvider
+{
+    public const name = '';
+
+    public function boot()
+    {
+        Event::listen(MadeTenantCurrentEvent::class, function () {
+            if (!TenantLicense::hasFeature(static::name)) {
+                return; // Don't register anything for this package for this tenant
+            }
+            $this->bootTenant();
+        });
+    }
+
+    abstract public function bootTenant(): void;
+}
