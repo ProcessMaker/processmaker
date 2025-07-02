4bc7b66794b42892682555ac702efaa14f5744e4
Julio Cesar Laura Avendaño <contact@julio-laura.com>
2025-07-02 18:35:47 -0400

    FOUR-25078 Response time in data connector resources has increased after the upgrade to version 4.14 - Unit test

--- tests/Repositories/SettingsConfigRepositoryTest.php
+++ tests/Repositories/SettingsConfigRepositoryTest.php
@@ -20,4 +20,23 @@ class SettingsConfigRepositoryTest extends TestCase
 
         $this->assertEquals('the value', config('test.dot.notation'));
     }
+
+    public function testCachesValueSimple()
+    {
+        Setting::create([
+            'key' => 'cache-test',
+            'config' => 'foo',
+            'format' => 'text',
+        ]);
+
+        $this->assertEquals('foo', config('cache-test'));
+
+        // Update the setting directly in DB and Redis Cache to simulate external change
+        Setting::where('key', 'cache-test')->update(['config' => 'bar']);
+        $settingCache = SettingCacheFactory::getSettingsCache();
+        $settingCache->clear();
+
+        // Should still return Class cached value, not updated one
+        $this->assertEquals('foo', config('cache-test'));
+    }
 }
