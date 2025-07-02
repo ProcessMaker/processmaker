e0548ffca5d6a980eb4fe4a3124f38591c31a49d
Rodrigo <rodrigoquelca@gmail.com>
2025-07-02 13:17:28 -0400

    Fix unit tests

--- config/database.php
+++ config/database.php
@@ -49,7 +49,21 @@ return [
             'engine' => 'InnoDB',
             'timezone'  => env('DB_TIMEZONE'),
         ],
-
+        'data' => [
+            'driver' => env('DATA_DB_DRIVER', 'mysql'),
+            'host' => env('DATA_DB_HOST', 'localhost'),
+            'port' => env('DATA_DB_PORT'),
+            'database' => env('DATA_DB_DATABASE'),
+            'username' => env('DATA_DB_USERNAME'),
+            'password' => env('DATA_DB_PASSWORD'),
+            'unix_socket' => env('DATA_DB_SOCKET'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'search_path' => env('DATA_DB_SCHEMA'),
+            'engine' => env('DATA_DB_ENGINE'),
+            'date_format' => env('DATA_DB_DATE_FORMAT'),
+            'timezone'  => env('DATA_DB_TIMEZONE'),
+        ],
         'landlord' => [
             'driver' => env('DB_DRIVER', 'mysql'),
             'host' => env('DB_HOSTNAME', 'localhost'),
