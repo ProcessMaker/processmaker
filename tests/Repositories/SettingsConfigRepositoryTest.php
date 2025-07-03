44e1e8913b1e4b851e002a3242ce5275a146e6da
Julio Cesar Laura Avendaño <contact@julio-laura.com>
2025-07-03 11:04:40 -0400

    FOUR-25078 Response time in data connector resources has increased after the upgrade to version 4.14 - Fix unit test

--- tests/Repositories/SettingsConfigRepositoryTest.php
+++ tests/Repositories/SettingsConfigRepositoryTest.php
@@ -5,6 +5,7 @@ namespace Tests\Repositories;
 use Illuminate\Cache\CacheManager;
 use Illuminate\Cache\Repository;
 use Illuminate\Support\Facades\Cache;
+use ProcessMaker\Cache\Settings\SettingCacheFactory;
 use ProcessMaker\Models\Setting;
 use Tests\TestCase;
 
@@ -23,20 +24,21 @@ class SettingsConfigRepositoryTest extends TestCase
 
     public function testCachesValueSimple()
     {
+        // "collections.<id>.obfuscate" is a key generated in "Records" model in collection package
         Setting::create([
-            'key' => 'cache-test',
+            'key' => 'collections.6.obfuscate',
             'config' => 'foo',
             'format' => 'text',
         ]);
 
-        $this->assertEquals('foo', config('cache-test'));
+        $this->assertEquals('foo', config('collections.6.obfuscate'));
 
         // Update the setting directly in DB and Redis Cache to simulate external change
-        Setting::where('key', 'cache-test')->update(['config' => 'bar']);
+        Setting::where('key', 'collections.6.obfuscate')->update(['config' => 'bar']);
         $settingCache = SettingCacheFactory::getSettingsCache();
         $settingCache->clear();
 
         // Should still return Class cached value, not updated one
-        $this->assertEquals('foo', config('cache-test'));
+        $this->assertEquals('foo', config('collections.6.obfuscate'));
     }
 }
