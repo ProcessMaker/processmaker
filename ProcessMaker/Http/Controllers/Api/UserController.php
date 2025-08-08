c8e3696931e14cd64c27a5236ee4a02da89ce344
Paula Quispe <paula.quispe@processmaker.com>
2025-08-08 19:53:23 -0400

    FOUR-25586

--- ProcessMaker/Http/Controllers/Api/UserController.php
+++ ProcessMaker/Http/Controllers/Api/UserController.php
@@ -204,16 +204,8 @@ class UserController extends Controller
             $include_ids = explode(',', $include_ids_string);
         } elseif ($request->has('assignable_for_task_id')) {
             $processRequestToken = ProcessRequestToken::findOrFail($request->input('assignable_for_task_id'));
-            if (config('app.reassign_restrict_to_assignable_users', true)) {
+            if (config('app.reassign_restrict_to_assignable_users')) {
                 $include_ids = $processRequestToken->process->getAssignableUsersByAssignmentType($processRequestToken);
-            }
-
-            // Exclude the current assigned user from the list
-            $currentUserId = $processRequestToken->user_id;
-            if ($currentUserId) {
-                $include_ids = array_filter($include_ids, function ($id) use ($currentUserId) {
-                    return $id != $currentUserId;
-                });
             }
         }
 
--- resources/js/tasks/components/taskPreview/TaskPreviewAssignment.vue
+++ resources/js/tasks/components/taskPreview/TaskPreviewAssignment.vue
@@ -82,7 +82,7 @@ const disabled = computed(() => !selectedUser.value || !comments.value?.trim());
 
 // Load the reassign users
 const loadReassignUsers = async (filter) => {
-  const response = await getReassignUsers(filter, props.task.id);
+  const response = await getReassignUsers(filter, props.task.id, props.task.user_id);
 
   reassignUsers.value = [];
   response.data.forEach((user) => {
