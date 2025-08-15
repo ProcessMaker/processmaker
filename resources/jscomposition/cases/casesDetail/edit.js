b13ca73df2dddbce636f2aff587a033ccb166533
Fabio <frguachallab@gmail.com>
2025-08-15 08:24:25 -0400

    remove unused variable

--- resources/jscomposition/cases/casesDetail/edit.js
+++ resources/jscomposition/cases/casesDetail/edit.js
@@ -8,7 +8,7 @@ import {
   updateUserConfiguration, getUserConfiguration, getCommentsData, updateRequest,
 } from "./api";
 import {
-  useStore, getRequest, getRequestId, getStageName, getProgressStage, isTceCustomization,
+  useStore, getRequest, getRequestId, getStageName, getProgressStage,
 } from "./variables";
 
 Vue.globalStore.registerModule("core:cases", cases);
@@ -53,7 +53,6 @@ const caseDetail = new Vue({
       headerModel: false,
       progressStage: getProgressStage(),
       stageName: getStageName(),
-      isTceCustomization: isTceCustomization(),
     };
   },
   computed: {
