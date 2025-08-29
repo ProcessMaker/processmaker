46eefee65e3d943948637fad41bb8f0c650d714a
David Callizaya <davidcallizaya@gmail.com>
2025-08-29 15:42:50 -0400

    Mount case-details after all the modeler extensions were loaded

--- resources/jscomposition/cases/casesDetail/edit.js
+++ resources/jscomposition/cases/casesDetail/edit.js
@@ -13,170 +13,173 @@ import {
 
 Vue.globalStore.registerModule("core:cases", cases);
 
-const caseDetail = new Vue({
-  el: "#case-detail",
-  components: {
-    CaseDetail, Tabs, CollapsableContainer, Timeline, StageBar,
-  },
-  data() {
-    return {
-      data,
-      request,
-      canCancel,
-      canViewPrint,
-      status: "ACTIVE",
-      userRequested: [],
-      errorLogs,
-      packages: [],
-      processId,
-      canViewComments,
-      disabled: false,
-      retryDisabled: false,
-      tabDefault: "details",
-      collapseContainer: true,
-      tabs: [
-        {
-          name: "Details",
-          href: "#details",
-          current: "details",
-          show: true,
-          content: null,
-        },
-        {
-          name: "Comments",
-          href: "#comments",
-          current: "comments",
-          show: true,
-          content: null,
-        },
-      ],
-      headerModel: false,
-      progressStage: getProgressStage(),
-      stageName: getStageName(),
-    };
-  },
-  computed: {
-    /**
-     * Get the list of participants in the request.
-     *
-     */
-    participants() {
-      return this.request.participants;
+window.addEventListener("load", () => {
+  const caseDetail = new Vue({
+    el: "#case-detail",
+    components: {
+      CaseDetail, Tabs, CollapsableContainer, Timeline, StageBar,
     },
-    classStatusCard() {
-      const header = {
-        ACTIVE: "active-style",
-        COMPLETED: "active-style",
-        CANCELED: "canceled-style ",
-        ERROR: "canceled-style",
-      };
-      return `tw-rounded-md text-status ${header[this.request.status.toUpperCase()]}`;
-    },
-    labelDate() {
-      const label = {
-        ACTIVE: "In Progress Since",
-        COMPLETED: "Completed On",
-        CANCELED: "Canceled ",
-        ERROR: "Failed On",
-      };
-      return label[this.request.status.toUpperCase()];
-    },
-    statusDate() {
-      const status = {
-        ACTIVE: this.request.created_at,
-        COMPLETED: this.request.completed_at,
-        CANCELED: this.request.updated_at,
-        ERROR: this.request.updated_at,
-      };
-
-      return status[this.request.status.toUpperCase()];
-    },
-    statusLabel() {
-      const status = {
-        ACTIVE: this.$t("In Progress"),
-        COMPLETED: this.$t("Completed"),
-        CANCELED: this.$t("Canceled"),
-        ERROR: this.$t("Error"),
-      };
-
-      return status[this.request.status.toUpperCase()];
-    },
-    requestBy() {
-      return [this.request.user];
-    },
-    panCommentInVueOptionsComponents() {
-      return "pan-comment" in Vue.options.components;
-    },
-
-  },
-  async mounted() {
-    const store = useStore();
-    this.packages = window.ProcessMaker.requestShowPackages;
-
-    const response = await this.getUserConf();
-
-    store.commit("core:cases/updateUserConfiguration", response);
-    this.collapseContainer = store.getters["core:cases/getCollapseContainer"];
-  },
-  methods: {
-    onCancel() {
-      ProcessMaker.confirmModal(
-        this.$t("Caution!"),
-        this.$t("Are you sure you want cancel this request?"),
-        "",
-        () => {
-          this.okCancel();
-        },
-      );
-    },
-    async onToogleContainer(value) {
-      const store = useStore();
-      store.commit("core:cases/updateCollapseContainer", value);
-
-      const userConf = store.getters["core:cases/getUserConfiguration"];
-
-      const response = await updateUserConfiguration({
-        user_id: userConf.user_id,
-        ui_configuration: userConf.ui_configuration,
-      });
-    },
-    getUserConf: async () => {
-      const response = await getUserConfiguration();
-
+    data() {
       return {
-        user_id: response.user_id,
-        ui_configuration: JSON.parse(response.ui_configuration),
+        data,
+        request,
+        canCancel,
+        canViewPrint,
+        status: "ACTIVE",
+        userRequested: [],
+        errorLogs,
+        packages: [],
+        processId,
+        canViewComments,
+        disabled: false,
+        retryDisabled: false,
+        tabDefault: "details",
+        collapseContainer: true,
+        tabs: [
+          {
+            name: "Details",
+            href: "#details",
+            current: "details",
+            show: true,
+            content: null,
+          },
+          {
+            name: "Comments",
+            href: "#comments",
+            current: "comments",
+            show: true,
+            content: null,
+          },
+        ],
+        headerModel: false,
+        progressStage: getProgressStage(),
+        stageName: getStageName(),
       };
     },
-    getCommentsData: async () => {
-      const request = getRequest();
+    computed: {
+      /**
+       * Get the list of participants in the request.
+       *
+       */
+      participants() {
+        return this.request.participants;
+      },
+      classStatusCard() {
+        const header = {
+          ACTIVE: "active-style",
+          COMPLETED: "active-style",
+          CANCELED: "canceled-style ",
+          ERROR: "canceled-style",
+        };
+        return `tw-rounded-md text-status ${header[this.request.status.toUpperCase()]}`;
+      },
+      labelDate() {
+        const label = {
+          ACTIVE: "In Progress Since",
+          COMPLETED: "Completed On",
+          CANCELED: "Canceled ",
+          ERROR: "Failed On",
+        };
+        return label[this.request.status.toUpperCase()];
+      },
+      statusDate() {
+        const status = {
+          ACTIVE: this.request.created_at,
+          COMPLETED: this.request.completed_at,
+          CANCELED: this.request.updated_at,
+          ERROR: this.request.updated_at,
+        };
 
-      const response = await getCommentsData({
-        params: {
-          type: "COMMENT,REPLY",
-          order_direction: "desc",
-          case_number: request.case_number,
-        },
-      });
+        return status[this.request.status.toUpperCase()];
+      },
+      statusLabel() {
+        const status = {
+          ACTIVE: this.$t("In Progress"),
+          COMPLETED: this.$t("Completed"),
+          CANCELED: this.$t("Canceled"),
+          ERROR: this.$t("Error"),
+        };
 
-      return response;
+        return status[this.request.status.toUpperCase()];
+      },
+      requestBy() {
+        return [this.request.user];
+      },
+      panCommentInVueOptionsComponents() {
+        return "pan-comment" in Vue.options.components;
+      },
+
     },
-    async okCancel() {
-      // single click
-      if (this.disabled) {
-        return;
-      }
-      this.disabled = true;
-      try {
-        await updateRequest(getRequestId(), {
-          status: "CANCELED",
+    async mounted() {
+      const store = useStore();
+      this.packages = window.ProcessMaker.requestShowPackages;
+
+      const response = await this.getUserConf();
+
+      store.commit("core:cases/updateUserConfiguration", response);
+      this.collapseContainer = store.getters["core:cases/getCollapseContainer"];
+    },
+    methods: {
+      onCancel() {
+        ProcessMaker.confirmModal(
+          this.$t("Caution!"),
+          this.$t("Are you sure you want cancel this request?"),
+          "",
+          () => {
+            this.okCancel();
+          },
+        );
+      },
+      async onToogleContainer(value) {
+        const store = useStore();
+        store.commit("core:cases/updateCollapseContainer", value);
+
+        const userConf = store.getters["core:cases/getUserConfiguration"];
+
+        const response = await updateUserConfiguration({
+          user_id: userConf.user_id,
+          ui_configuration: userConf.ui_configuration,
+        });
+      },
+      getUserConf: async () => {
+        const response = await getUserConfiguration();
+
+        return {
+          user_id: response.user_id,
+          ui_configuration: JSON.parse(response.ui_configuration),
+        };
+      },
+      getCommentsData: async () => {
+        const request = getRequest();
+
+        const response = await getCommentsData({
+          params: {
+            type: "COMMENT,REPLY",
+            order_direction: "desc",
+            case_number: request.case_number,
+          },
         });
 
-        ProcessMaker.alert(this.$t("The request was canceled."), "success");
-        window.location.reload();
-      } catch (error) {
-        this.disabled = false;
-      }
+        return response;
+      },
+      async okCancel() {
+        // single click
+        if (this.disabled) {
+          return;
+        }
+        this.disabled = true;
+        try {
+          await updateRequest(getRequestId(), {
+            status: "CANCELED",
+          });
+
+          ProcessMaker.alert(this.$t("The request was canceled."), "success");
+          window.location.reload();
+        } catch (error) {
+          this.disabled = false;
+        }
+      },
     },
-  },
+  });
 });
+
