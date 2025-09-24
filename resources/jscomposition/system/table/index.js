12a5155ad4dae2a474c49a2b37e6ca4a387d0e68
Henry Jonas <jonathan.quispe@processmaker.com>
2025-09-24 11:03:31 -0400

    FOUR-26661: Implement cases list compoment in the Application Dashboard

--- resources/jscomposition/system/table/index.js
+++ resources/jscomposition/system/table/index.js
@@ -1,6 +1,5 @@
 import FilterableTable from "./FilterableTable.vue";
 import SortTable from "./SortTable.vue";
-import Pagination from "./Pagination.vue";
 import TablePlaceholder from "./TablePlaceholder.vue";
 
 export * from "./cell/index";
@@ -9,6 +8,5 @@ export * from "./filter/index";
 export {
   FilterableTable,
   SortTable,
-  Pagination,
   TablePlaceholder,
 };
