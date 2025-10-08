aec74e68d8be8c5512a5766a47e42fea5e41cc52
Eleazar Resendez <eleazar.resendez@processmaker.com>
2025-10-08 09:56:57 -0600

    fix(LaunchpadSettingsModal): adjust margin for selected icons in multiselect component

--- resources/js/components/shared/LaunchpadSettingsModal.vue
+++ resources/js/components/shared/LaunchpadSettingsModal.vue
@@ -1275,6 +1275,11 @@ label {
     text-align: left;
   }
 }
+.multiselect-icons.custom-multiselect {
+  .icon-selected {
+    margin-top: 0;
+  }
+}
 .modal-content-custom .column-container {
   overflow-y: auto;
 }
