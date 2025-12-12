3088c8fc92a1869a39be9d806133245d265e655b
Gustavo Silva <gustavo.silva@processmaker.com>
2025-12-12 19:38:55 -0400

    Add custom executor in microservices feature
    Fix declare http client
    
    Refactor how to get instance uuid
    
    Remove instance uuid variable
    
    Create the tenant if it does not exist
    
    Use fresh client for keycloak
    
    Implement second pusher listener to microservices
    
    Code improvements
    
    Code improvements
    
    Send tenant name whe create a custom executor
    
    Fix send tenant name which tenant create
    
    Add support to php-nayra language
    
    Verify is custom executor and is microservice when update script executor
    
    Change behavior in Script Executors when is a Custom Executor
    
    Fix the Script Microservice unit test
    
    Fix errors response
    
    Fix unit test for Script Mciroservice
    
    Add executor version to metada

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -41,6 +41,8 @@ import FilterTable from "./components/shared/FilterTable.vue";
 import PaginationTable from "./components/shared/PaginationTable.vue";
 import PMDropdownSuggest from "./components/PMDropdownSuggest";
 import "@processmaker/screen-builder/dist/vue-form-builder.css";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
 window.__ = translator;
 window._ = require("lodash");
@@ -364,6 +366,15 @@ if (window.Processmaker && window.Processmaker.broadcasting) {
   }
 
   window.Echo = new TenantAwareEcho(config);
+}
+
+if (window.Processmaker && window.Processmaker.script_microservice && window.Processmaker.script_microservice.enabled) {
+  const config = window.Processmaker.script_microservice.broadcasting;
+
+  window.ScriptMicroserviceEcho = new Echo({
+    ...config,
+    client: new Pusher(config.key, config),
+  });
 }
 
 if (userID) {
