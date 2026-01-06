f5ee85750a02025d046789e391b60912da4b2295
Shift <shift@laravelshift.com>
2026-01-06 19:39:32 +0000

    Shift cleanup

--- config/app.php
+++ config/app.php
@@ -111,7 +111,6 @@ return [
 
     'login_view' => env('LOGIN_VIEW', 'auth.newLogin'),
 
-
     'aliases' => Facade::defaultAliases()->merge([
         'Agent' => Jenssegers\Agent\Facades\Agent::class,
         'Docker' => ProcessMaker\Facades\Docker::class,
