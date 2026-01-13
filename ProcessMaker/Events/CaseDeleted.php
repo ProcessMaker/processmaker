07a3894231978972233fa93ae3e238688148ced2
Teisha McRae <mcraeteisha@gmail.com>
2026-01-13 16:40:32 -0500

    Create CaseDeleted Event

---
+++ ProcessMaker/Events/CaseDeleted.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ProcessMaker\Events;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Events\Dispatchable;
+use ProcessMaker\Contracts\SecurityLogEventInterface;
+use ProcessMaker\Traits\FormatSecurityLogChanges;
+
+class CaseDeleted implements SecurityLogEventInterface
+{
+    use Dispatchable;
+    use FormatSecurityLogChanges;
+
+    private int $caseNumber;
+
+    private string $caseTitle;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(int $caseNumber, string $caseTitle)
+    {
+        $this->caseNumber = $caseNumber;
+        $this->caseTitle = $caseTitle;
+    }
+
+    /**
+     * Get specific data related to the event
+     *
+     * @return array
+     */
+    public function getData(): array
+    {
+        return [
+            'case_title' => $this->caseTitle,
+            'case_number' => $this->caseNumber,
+            'deleted_at' => Carbon::now(),
+        ];
+    }
+
+    /**
+     * Get specific data related to the event
+     *
+     * @return array
+     */
+    public function getChanges(): array
+    {
+        return [
+            'case_number' => $this->caseNumber,
+        ];
+    }
+
+    /**
+     * Get the Event name
+     *
+     * @return string
+     */
+    public function getEventName(): string
+    {
+        return 'CaseDeleted';
+    }
+}
