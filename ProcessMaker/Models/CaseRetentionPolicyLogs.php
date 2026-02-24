dfc7d338cb148eaa667fd0497941498b797bd56b
sanja <52755494+sanjacornelius@users.noreply.github.com>
2026-02-24 15:22:29 -0400

    Create CaseRetentionPolicyLogs.php

---
+++ ProcessMaker/Models/CaseRetentionPolicyLogs.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProcessMaker\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use ProcessMaker\Models\ProcessMakerModel;
+
+class CaseRetentionPolicyLogs extends ProcessMakerModel
+{
+    use HasFactory;
+
+    protected $table = 'case_retention_policy_logs';
+
+    const UPDATED_AT = null;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+    ];
+
+    protected $fillable = [
+        'process_id',
+        'case_ids',
+        'deleted_count',
+        'total_time_taken',
+        'deleted_at',
+    ];
+}
