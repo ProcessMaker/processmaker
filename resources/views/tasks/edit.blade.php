e6f093c80b86583c2f4f9332553de28afe5b0d1f
Paula Quispe <paula.quispe@processmaker.com>
2026-05-27 15:28:39 -0400

    FOUR-31593

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -88,9 +88,9 @@
               <div id="tab-form" role="tabpanel" aria-labelledby="tab-form" class="tab-pane active show">
                 @can('update', $task)
                   @unless($hitlEnabled)
-                  <div v-if="tceEnableCaseNumberScreen" class="tw-block tw-gap-2 tw-mb-0 tw-px-2 tw-bg-white tw-border-l tw-border-l-[#d7dde5] tw-border-r tw-border-r-[#d7dde5]" v-cloak>
+                  <span v-if="tceEnableCaseNumberScreen" class="tw-block tw-gap-2 tw-mb-0 tw-px-2 tw-bg-white tw-border-l tw-border-l-[#d7dde5] tw-border-r tw-border-r-[#d7dde5]" v-cloak>
                     <span class="tw-font-medium tw-text-[#728092] tw-text-xs">Case #:</span> <span class="tw-font-normal tw-text-[#9fa8b5] tw-text-xs">{{ $caseNumber }}</span>
-                  </div>
+                  </span>
                     <task
                       ref="task"
                       class="card border-0"
