308054da436394a3c7979222f0e8350e203ce701
Eleazar Resendez <eleazar.resendez@processmaker.com>
2026-07-10 11:52:03 -0600

    Add optional importingUserId to Options class and corresponding test

--- ProcessMaker/Models/Bundle.php
+++ ProcessMaker/Models/Bundle.php
@@ -390,7 +390,7 @@ class Bundle extends ProcessMakerModel implements HasMedia
     {
         $options = new Options([
             'mode' => $mode,
-        ]);
+        ], $logger?->userId);
 
         $assets = [];
         foreach ($payloads as $payload) {
