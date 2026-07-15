91b80b6032cb45b106932fe3698e5733cdc95471
sanja <52755494+sanjacornelius@users.noreply.github.com>
2026-07-15 12:02:33 -0700

    Remove authorization badge from microsoft_graph

--- resources/js/admin/settings/components/SettingsListing.vue
+++ resources/js/admin/settings/components/SettingsListing.vue
@@ -607,9 +607,7 @@ export default {
     filterEmailServerButtons(groupName, groupData, btn) {
       const authMethod = groupData.find(data => data.key.includes("EMAIL_CONNECTOR_MAIL_AUTH_METHOD"));
       const selectedAuthMethod = authMethod ? authMethod.ui.options[authMethod.config] : null;
-      const mailDriver = groupData.find(data => data.key.includes("EMAIL_CONNECTOR_MAIL_DRIVER"));
-      const selectedDriver = mailDriver ? mailDriver.ui.options[mailDriver.config] : null;
-      const showAuthAccBtn = (selectedAuthMethod && selectedAuthMethod !== 'standard') || selectedDriver === 'microsoft_graph';
+      const showAuthAccBtn = selectedAuthMethod && selectedAuthMethod !== 'standard';
 
       if (groupName.includes('Email Server') && !showAuthAccBtn)  {
         // Returns all 'top' position buttons except the '+ Mail Server' and 'Authorize Account' button for email server tabs
