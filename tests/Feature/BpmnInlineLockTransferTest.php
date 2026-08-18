ba1c53c2b8f98b483143ea510fbba0030e32364f
David Callizaya <davidcallizaya@gmail.com>
2026-08-18 09:07:41 -0400

    test: update test

--- tests/Feature/BpmnInlineLockTransferTest.php
+++ tests/Feature/BpmnInlineLockTransferTest.php
@@ -7,7 +7,11 @@ use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\Attributes\Group as TestGroup;
 use PHPUnit\Framework\TestCase;
 use ProcessMaker\Jobs\BpmnAction;
+use ProcessMaker\Jobs\CompleteActivity;
+use ProcessMaker\Jobs\RunScriptTask;
+use ProcessMaker\Jobs\RunServiceTask;
 use ProcessMaker\Models\ProcessRequestLock;
+use ReflectionMethod;
 use ReflectionProperty;
 
 #[TestGroup('process_tests')]
@@ -20,12 +24,21 @@ class BpmnInlineLockTransferTest extends TestCase
         $source = Mockery::mock(BpmnAction::class)->makePartial();
         $target = Mockery::mock(BpmnAction::class)->makePartial();
         $lock = Mockery::mock(ProcessRequestLock::class);
-        $source->setInternalContext(['lock' => $lock]);
+        $lockProperty = new ReflectionProperty(BpmnAction::class, 'lock');
+        $lockProperty->setValue($source, $lock);
 
         $source->transferInternalContext($target);
 
-        $lockProperty = new ReflectionProperty(BpmnAction::class, 'lock');
         $this->assertNull($lockProperty->getValue($source));
         $this->assertSame($lock, $lockProperty->getValue($target));
+    }
+
+    public function testOnlyScriptAndServiceJobsEnableInlineTaskExecution(): void
+    {
+        $method = new ReflectionMethod(BpmnAction::class, 'allowsInlineTaskExecution');
+
+        $this->assertTrue($method->invoke(Mockery::mock(RunScriptTask::class)->makePartial()));
+        $this->assertTrue($method->invoke(Mockery::mock(RunServiceTask::class)->makePartial()));
+        $this->assertFalse($method->invoke(Mockery::mock(CompleteActivity::class)->makePartial()));
     }
 }
