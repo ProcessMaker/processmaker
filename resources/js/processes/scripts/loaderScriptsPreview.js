927dcea71003fd04f396f9fe4bc1b7659bfe5643
Henry Jonas <jonathan.quispe@processmaker.com>
2026-09-14 11:19:44 -0400

    Remove loaderScriptsPreview.js

--- resources/js/processes/scripts/loaderScriptsPreview.js
+++
@@ -1,8 +0,0 @@
-import { setupMain } from "../../next/setupMain";
-import monaco from "../../next/monaco";
-
-setupMain();
-monaco();
-
-window.ProcessMaker.packages = window.temporal?.packages || [];
-window.packages = window.ProcessMaker.packages;
--- vite.config.js
+++ vite.config.js
@@ -227,7 +227,6 @@ export default defineConfig(({ mode }) => {
           "resources/js/processes/scripts/editConfig.js",
           "resources/js/processes/scripts/edit.js",
 
-          "resources/js/processes/scripts/loaderScriptsPreview.js",
           "resources/js/processes/scripts/preview.js",
 
           "resources/js/processes/modeler/loaderModeler.js",
