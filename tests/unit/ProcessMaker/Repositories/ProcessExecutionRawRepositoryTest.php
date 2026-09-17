a3a3ebaf63c3c9640ec98ca0475b21edc34191eb
Paula Quispe <paula.quispe@processmaker.com>
2026-09-17 09:14:56 -0400

    FOUR-33295

--- tests/unit/ProcessMaker/Repositories/ProcessExecutionRawRepositoryTest.php
+++ tests/unit/ProcessMaker/Repositories/ProcessExecutionRawRepositoryTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit\ProcessMaker\Repositories;
 
+use ProcessMaker\Models\Group;
+use ProcessMaker\Models\GroupMember;
 use ProcessMaker\Models\ProcessRequest;
+use ProcessMaker\Models\User;
 use ProcessMaker\Repositories\ProcessExecutionRawRepository;
 use Tests\TestCase;
 
@@ -33,5 +36,34 @@ class ProcessExecutionRawRepositoryTest extends TestCase
 
         $this->assertIsArray($hydrated->data);
         $this->assertSame('persisted', $hydrated->data['marker']);
+    }
+
+    public function testRawGroupAssignmentResolvesActiveNestedGroups(): void
+    {
+        $user = User::factory()->create(['status' => 'ACTIVE']);
+        $parentGroup = Group::factory()->create(['status' => 'ACTIVE']);
+        $childGroup = Group::factory()->create(['status' => 'ACTIVE']);
+
+        GroupMember::withoutEvents(function () use ($parentGroup, $childGroup, $user): void {
+            GroupMember::create([
+                'group_id' => $parentGroup->id,
+                'member_id' => $childGroup->id,
+                'member_type' => Group::class,
+            ]);
+            GroupMember::create([
+                'group_id' => $childGroup->id,
+                'member_id' => $user->id,
+                'member_type' => User::class,
+            ]);
+        });
+
+        $repository = new ProcessExecutionRawRepository();
+        $method = new \ReflectionMethod($repository, 'mergeGroupMemberUserIdsRaw');
+        $method->setAccessible(true);
+        $users = [];
+
+        $method->invokeArgs($repository, [[$parentGroup->id], &$users]);
+
+        $this->assertSame([$user->id], array_values($users));
     }
 }
