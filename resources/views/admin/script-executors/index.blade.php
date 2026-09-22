e5a200267452f7d8f21f6b68af9f6a355e825e52
Gustavo Silva <gustavo.silva@processmaker.com>
2026-09-22 18:14:34 -0400

    Fix ScriptMicroserviceEcho issue

--- resources/views/admin/script-executors/index.blade.php
+++ resources/views/admin/script-executors/index.blade.php
@@ -27,6 +27,25 @@
 
 @section('js')
     <script>
+        if (!!{{ config('script-runner-microservice.enabled') }}) {
+            window.Processmaker.script_microservice = {
+                enabled : {{ config('script-runner-microservice.enabled', false) }},
+                broadcasting : {
+                    broadcaster: "pusher",
+                    key: "{{config('script-runner-microservice.broadcasting.app_key')}}",
+                    cluster: "{{config('script-runner-microservice.broadcasting.cluster')}}",
+                    forceTLS: {{config('script-runner-microservice.broadcasting.scheme') === 'https' ? 'true' : 'false'}},
+                    enabledTransports: ['ws', 'wss'],
+                    disableStats: true,
+                }
+            };
+            const broadcastingHost = "{{config('script-runner-microservice.broadcasting.host')}}";
+            if (broadcastingHost !== "") {
+                window.Processmaker.script_microservice.broadcasting.wsHost = "{{config('script-runner-microservice.broadcasting.host')}}";
+                window.Processmaker.script_microservice.broadcasting.wsPort = "{{config('script-runner-microservice.broadcasting.port')}}";
+                window.Processmaker.script_microservice.broadcasting.wssPort = "{{config('script-runner-microservice.broadcasting.port')}}";
+            }
+        }
         window.temporal = window.temporal || {};
         window.temporal.packages = @json(\App::make(ProcessMaker\Managers\PackageManager::class)->listPackages());
         window.packages = @json(\App::make(ProcessMaker\Managers\PackageManager::class)->listPackages());
